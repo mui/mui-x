2705fe2ee5ebc7f676ce91200d0ce33009fe7045
damien <damien.tassone@gmail.com>
2020-06-01 19:44:54 +0200

    Added stories for pagination and refactoring

--- packages/storybook/src/stories/grid-options.stories.tsx
+++ packages/storybook/src/stories/grid-options.stories.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { ColDef, Grid, GridOverlay } from '@material-ui-x/grid';
-import { LinearProgress } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { ColDef, Grid, GridOverlay, Footer, GridApiRef, gridApiRef } from '@material-ui-x/grid';
+import {Button, LinearProgress} from '@material-ui/core';
 import CodeIcon from '@material-ui/icons/Code';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
-import { GridDataSet } from '../components/grid-dataset';
+import { GridDataSet, useData } from '../components/grid-dataset';
+import { Pagination } from '@material-ui/lab';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'Grid Options',
@@ -114,6 +116,154 @@ export const withPagination = () => {
     />
   );
 };
+
+export const withPaginationNoRowCount = () => {
+  const size = { width: 800, height: 600 };
+
+  return (
+    <GridDataSet
+      nbRows={2000}
+      nbCols={200}
+      container={size}
+      options={{
+        pagination: true,
+        paginationPageSize: 100,
+        hideFooterRowCount: true,
+      }}
+    />
+  );
+};
+export const withPaginationButNotVisible = () => {
+  const size = { width: 800, height: 600 };
+
+  return (
+    <GridDataSet
+      nbRows={2000}
+      nbCols={200}
+      container={size}
+      options={{
+        pagination: true,
+        paginationPageSize: 100,
+        hideFooterPagination: true,
+      }}
+    />
+  );
+};
+export const withCustomPagination = () => {
+  const size = { width: 800, height: 600 };
+  const apiRef: GridApiRef = gridApiRef();
+  const data = useData(2000, 200);
+
+  useEffect(() => {
+    if (apiRef && apiRef.current) {
+      return apiRef.current.onPageChanged(action('pageChanged'));
+    }
+  }, [apiRef, data]);
+
+const backToFirstPage = ()=> {
+  apiRef.current.setPage(1);
+};
+  const changePageSize = ()=> {
+    apiRef.current.setPageSize(50);
+  };
+
+  return (
+    <div style={{ width: size.width, height: size.height }}>
+      <Button component={'button'} color={"primary"} variant={"outlined"} onClick={backToFirstPage}>Back to first page! </Button>
+      <Button component={'button'} color={"primary"} variant={"outlined"} onClick={changePageSize}>Change pageSize </Button>
+      <Grid
+        rows={data.rows}
+        columns={data.columns}
+        apiRef={apiRef}
+        options={{
+          pagination: true,
+          paginationPageSize: 33,
+          paginationComponent: paginationProps => (
+            <Pagination
+              className={'my-custom-pagination'}
+              page={paginationProps.page}
+              count={paginationProps.pageCount}
+              onChange={(e, value) => paginationProps.setPage(value)}
+            />
+          ),
+        }}
+      />
+    </div>
+  );
+};
+export const withCustomFooter = () => {
+  const size = { width: 800, height: 600 };
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const data = useData(2000, 200);
+  return (
+    <div style={{ width: size.width, height: size.height }}>
+      <Grid
+        rows={data.rows}
+        columns={data.columns}
+        options={{
+          pagination: true,
+          paginationPageSize: 33,
+          hideFooterPagination: true,
+          hideFooter: true,
+        }}
+      >
+        {(paginationProps, rows, columns, options, api, gridRef) => (
+          <Footer className={'my-custom-footer'}>
+            <span style={{ display: 'flex', alignItems: 'center' }}>
+              This is my custom footer and pagination here!{' '}
+            </span>
+            <Pagination
+              className={'my-custom-pagination'}
+              page={paginationProps.page}
+              count={paginationProps.pageCount}
+              onChange={(e, value) => paginationProps.setPage(value)}
+            />
+          </Footer>
+        )}
+      </Grid>
+    </div>
+  );
+};
+
+export const withCustomHeaderAndFooter = () => {
+  const size = { width: 800, height: 600 };
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const data = useData(2000, 200);
+
+  return (
+    <div style={{ width: size.width, height: size.height }}>
+      <Grid
+        rows={data.rows}
+        columns={data.columns}
+        options={{
+          pagination: true,
+          paginationPageSize: 33,
+          hideFooterPagination: true,
+          hideFooter: true,
+        }}
+      >
+        {{
+          // eslint-disable-next-line react/display-name
+          header: paginationProps => (
+            <div className={'custom-header'}>
+              <Pagination
+                className={'my-custom-pagination'}
+                page={paginationProps.page}
+                count={paginationProps.pageCount}
+                onChange={(e, value) => paginationProps.setPage(value)}
+              />
+            </div>
+          ),
+          // eslint-disable-next-line react/display-name
+          footer: paginationProps => (
+            <div className="footer my-custom-footer"> I counted {paginationProps.rowCount} row(s) </div>
+          ),
+        }}
+      </Grid>
+    </div>
+  );
+};
+
 export const withAutoPagination = () => {
   const size = { width: 800, height: 600 };
 
