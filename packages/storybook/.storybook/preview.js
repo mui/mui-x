175a4158534ee503a865e017f7598cefdadbbdea
damien <damien.tassone@gmail.com>
2020-06-11 12:12:41 +0200

    fix license

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -1,3 +1,3 @@
 import {LicenseInfo} from '@material-ui-x/license';
 
- LicenseInfo.setLicenseKey('0f94d8b65161817ca5d7f7af8ac2f042T1JERVI6TVVJLVN0b!3J5Ym9vayxFWFBJUlk9MTY1NDg1ODc1MzU1MCxLRVlWRVJTSU9OPTE=');
+ LicenseInfo.setLicenseKey('0f94d8b65161817ca5d7f7af8ac2f042T1JERVI6TVVJLVN0b3J5Ym9vayxFWFBJUlk9MTY1NDg1ODc1MzU1MCxLRVlWRVJTSU9OPTE=');
