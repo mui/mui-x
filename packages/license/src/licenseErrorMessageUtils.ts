b20645f03c8b1a525f2e688037398bfd42e1735b
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-06-17 17:27:31 +0200

    [license] Improve license message (#22)

--- packages/license/src/licenseErrorMessageUtils.ts
+++ packages/license/src/licenseErrorMessageUtils.ts
@@ -1,45 +1,45 @@
-export function showInvalidLicenseError() {
+function showError(message) {
   console.error(
-    '*************************************************************************\n' +
-      '************************* Material-UI X License *************************\n' +
-      '**************************** Invalid License ****************************\n' +
-      '*                                                                       *\n' +
-      '* Your license for Material-UI X is not valid, please contact           *\n' +
-      '* license@material-ui.com to get a valid license.                       *\n' +
-      '*                                                                       *\n' +
-      '*************************************************************************\n' +
-      '*************************************************************************\n',
+    [
+      '************************************************************',
+      '*************************************************************',
+      ,
+      '',
+      ...message,
+      '',
+      '*************************************************************',
+      '*************************************************************',
+    ].join('\n'),
   );
+}
+
+export function showInvalidLicenseError() {
+  showError([
+    'Material-UI X: Invalid license.',
+    '',
+    'Your license for Material-UI X is not valid, please contact',
+    'license@material-ui.com to get a valid license.',
+  ]);
 }
 
 export function showNotFoundLicenseError() {
-  console.error(
-    '*************************************************************************\n' +
-      '************************* Material-UI X License *************************\n' +
-      '************************* License Key Not Found *************************\n' +
-      '*                                                                       *\n' +
-      '* All Material-UI X features are unlocked. \n' +
-      '* This is an evaluation only version, it is not licensed for            *\n' +
-      '* development projects intended for production                          *\n' +
-      '*                                                                       *\n' +
-      '* If you want to hide the watermark, please email                       *\n' +
-      '* license@material-ui.com  for a trial license.                         *\n' +
-      '*                                                                       *\n' +
-      '*************************************************************************\n' +
-      '*************************************************************************',
-  );
+  showError([
+    'Material-UI X: License key not found.',
+    '',
+    'All Material-UI X features are unlocked.',
+    'This is an evaluation only version, it is not licensed for',
+    'development projects intended for production.',
+    '',
+    'If you want a license to use the features, please email',
+    'license@material-ui.com  for a trial license.',
+  ]);
 }
 
 export function showExpiredLicenseError() {
-  console.error(
-    '*************************************************************************\n' +
-      '************************* Material-UI X License *************************\n' +
-      '************************** License Key Expired **************************\n' +
-      '*                                                                       *\n' +
-      '* Please contact license@material-ui.com to renew your subscription     *\n' +
-      '* and get the latest version of Material-UI X                           *\n' +
-      '*                                                                       *\n' +
-      '*************************************************************************\n' +
-      '*************************************************************************',
-  );
+  showError([
+    'Material-UI X: License key expired.',
+    '',
+    'Please contact license@material-ui.com to renew your subscription',
+    'and get the latest version of Material-UI X.',
+  ]);
 }
