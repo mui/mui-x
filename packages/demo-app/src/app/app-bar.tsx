5fd15088514a4eb70eea6c5dbd086fb05b3df535
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-07-08 19:38:24 +0200

    react first

--- packages/demo-app/src/app/app-bar.tsx
+++ packages/demo-app/src/app/app-bar.tsx
@@ -1,9 +1,9 @@
+import * as React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
-import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
