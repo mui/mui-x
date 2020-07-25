bb2c6e1bd760870c5c8c40ac347bb5f25ad9d548
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-07-25 18:48:18 +0200

    [core] Use the classnames helper

--- packages/grid/x-grid-modules/src/components/styled-wrappers/footer.tsx
+++ packages/grid/x-grid-modules/src/components/styled-wrappers/footer.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { DivProps } from './grid-root';
+import { classnames } from '../../utils';
 
 export const Footer = React.forwardRef<HTMLDivElement, DivProps>((props, ref) => {
   const { className, ...other } = props;
-  return <div ref={ref} className={`footer ${className || ''}`} {...other} />;
+  return <div ref={ref} className={classnames('footer', className)} {...other} />;
 });
 Footer.displayName = 'Footer';
