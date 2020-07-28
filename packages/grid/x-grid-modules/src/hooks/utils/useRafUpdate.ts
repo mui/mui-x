39d5e110a4b5f8a405e9173bb1b751d2828da338
damien <damien.tassone@gmail.com>
2020-07-28 18:28:47 +0200

    testing with no raf

--- packages/grid/x-grid-modules/src/hooks/utils/useRafUpdate.ts
+++ packages/grid/x-grid-modules/src/hooks/utils/useRafUpdate.ts
@@ -4,7 +4,6 @@ import { useLogger } from './useLogger';
 type UseRafUpdateReturnType = [(...args: any[]) => void, (fn: (args: any) => void) => void];
 export function useRafUpdate(initialFn?: (...args: any) => void): UseRafUpdateReturnType {
   const logger = useLogger('useRafUpdate');
-  const rafRef = React.useRef(0);
   const fn = React.useRef(initialFn);
 
   const setUpdate = React.useCallback((updateFn: (...args: any[]) => void) => {
@@ -16,15 +15,9 @@ export function useRafUpdate(initialFn?: (...args: any) => void): UseRafUpdateRe
       if (!fn) {
         return;
       }
-      if (rafRef.current > 0) {
-        logger.debug('Skipping previous update');
-        cancelAnimationFrame(rafRef.current);
-      }
+
       logger.debug('Queuing new update');
-      rafRef.current = requestAnimationFrame(() => {
-        fn.current!(...args);
-        rafRef.current = 0;
-      });
+      fn.current!(...args);
     },
     [logger],
   );
