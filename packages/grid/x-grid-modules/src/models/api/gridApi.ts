34ad42b0373d5e19bcda2994d96812e82e56a3b8
damien <damien.tassone@gmail.com>
2020-07-29 19:10:20 +0200

    refactor split useApi

--- packages/grid/x-grid-modules/src/models/api/gridApi.ts
+++ packages/grid/x-grid-modules/src/models/api/gridApi.ts
@@ -1,45 +1,11 @@
-import { EventEmitter } from 'events';
-import { RowApi } from './rowApi';
-import { ColumnApi } from './columnApi';
-import { SelectionApi } from './selectionApi';
-import { SortApi } from './sortApi';
-import { PaginationApi } from './paginationApi';
-import { VirtualizationApi } from './virtualizationApi';
-
-/**
- * The core API interface that is available in the grid [[apiRef]].
- */
-export interface CoreApi extends EventEmitter {
-  /**
-   * Property that comes true when the grid has its EventEmitter initialised.
-   */
-  isInitialised: boolean;
-  /**
-   * Allows to register a handler for an event.
-   *
-   * @param event
-   * @param handler
-   */
-  registerEvent: (event: string, handler: (param: any) => void) => () => void;
-  /**
-   * Add a handler that will be triggered when the component unmount.
-   *
-   * @param handler
-   */
-  onUnmount: (handler: (param: any) => void) => void;
-  /**
-   * Add a handler that will be triggered when the component resize.
-   *
-   * @param handler
-   */
-  onResize: (handler: (param: any) => void) => void;
-  /**
-   * Trigger a resize of the component, and recalculation of width and height.
-   *
-   * @param handler
-   */
-  resize: () => void;
-}
+import {RowApi} from './rowApi';
+import {ColumnApi} from './columnApi';
+import {SelectionApi} from './selectionApi';
+import {SortApi} from './sortApi';
+import {PaginationApi} from './paginationApi';
+import {VirtualizationApi} from './virtualizationApi';
+import {CoreApi} from "./coreApi";
+import {EventsApi} from "./eventsApi";
 
 /**
  * The full Grid API.
@@ -50,4 +16,5 @@ export type GridApi = RowApi &
   SortApi &
   VirtualizationApi &
   CoreApi &
+	EventsApi &
   PaginationApi;
