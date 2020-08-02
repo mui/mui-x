d9b15ca7f435dfb578f03b823c294e9766850c10
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-08-02 17:59:50 +0200

    [DataGrid] Fix SSR support (#135)

--- packages/grid/x-grid-modules/src/hooks/utils/useLogger.ts
+++ packages/grid/x-grid-modules/src/hooks/utils/useLogger.ts
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import { isFunction } from '../../utils/utils';
 
-const forceDebug = localStorage.getItem('DEBUG') != null;
-const isDebugging = process.env.NODE_ENV !== 'production' || forceDebug;
+function localStorageAvailable() {
+  try {
+    // Incognito mode might reject access to the localStorage for security reasons.
+    // window isn't defined on Node.js
+    // https://stackoverflow.com/questions/16427636/check-if-localstorage-is-available
+    const key = '__some_random_key_you_are_not_going_to_use__';
+    window.localStorage.setItem(key, key);
+    window.localStorage.removeItem(key);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+const forceDebug = localStorageAvailable() && window.localStorage.getItem('DEBUG') != null;
 
 export interface Logger {
   debug: (...args: any[]) => void;
@@ -19,8 +32,10 @@ const noopLogger: Logger = {
   warn: noop,
   error: noop,
 };
+
 const LOG_LEVELS = ['debug', 'info', 'warn', 'error'];
-const getAppender = (name: string, logLevel: string, appender: Logger = console): Logger => {
+
+function getAppender(name: string, logLevel: string, appender: Logger = console): Logger {
   const minLogLevelIdx = LOG_LEVELS.indexOf(logLevel);
 
   if (minLogLevelIdx === -1) {
@@ -40,31 +55,28 @@ const getAppender = (name: string, logLevel: string, appender: Logger = console)
   }, {} as any);
 
   return logger as Logger;
-};
+}
 
 const defaultFactory: (logLevel: string) => LoggerFactoryFn = (logLevel: string) => (
   name: string,
-) => {
-  if (!isDebugging) {
-    return noopLogger;
-  }
-  return getAppender(name, logLevel);
-};
+) => getAppender(name, logLevel);
 
-export type LoggerFactoryFn = (name: string) => Logger;
+type LoggerFactoryFn = (name: string) => Logger;
 
 // TODO Refactor to allow different logger for each grid in a page...
 let factory: LoggerFactoryFn | null;
+
 export function useLoggerFactory(
   customLogger?: Logger | LoggerFactoryFn,
-  logLevel: string | boolean = 'debug',
+  logLevel: string = 'debug',
 ) {
   if (forceDebug) {
     factory = defaultFactory('debug');
     return;
   }
+
   if (!customLogger) {
-    factory = logLevel ? defaultFactory(logLevel.toString()) : null;
+    factory = logLevel ? defaultFactory(logLevel) : null;
     return;
   }
 
@@ -73,12 +85,10 @@ export function useLoggerFactory(
     return;
   }
 
-  factory = logLevel
-    ? (name: string) => getAppender(name, logLevel.toString(), customLogger)
-    : null;
+  factory = logLevel ? (name: string) => getAppender(name, logLevel, customLogger) : null;
 }
 
 export function useLogger(name: string): Logger {
-  const logger = React.useRef(factory ? factory(name) : noopLogger);
-  return logger.current;
+  const { current: logger } = React.useRef(factory ? factory(name) : noopLogger);
+  return logger;
 }
