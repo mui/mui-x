ed145353270299ab68e1884c9a2b82b57c86c68d
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-08-05 16:59:42 +0200

    [test] CI part of #150

--- test/karma.tests.js
+++ test/karma.tests.js
@@ -1,4 +1,7 @@
 import './utils/init';
+import consoleError from './utils/consoleError';
+
+consoleError();
 
 const packagesContext = require.context('../packages', true, /\.test\.tsx$/);
 packagesContext.keys().forEach(packagesContext);
