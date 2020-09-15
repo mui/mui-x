0d47c38ba95743ae9024af8fa5a22ab70a15922c
damien tassone <damien.tassone@gmail.com>
2020-09-15 17:42:06 +0200

    [DataGrid] Fix checkbox selection issue (#285)
    * Fix #283 selection issue
    
    * fix checkbox intermediate state

--- packages/grid/x-grid-modules/src/components/checkbox-renderer.tsx
+++ packages/grid/x-grid-modules/src/components/checkbox-renderer.tsx
@@ -4,9 +4,14 @@ import { SelectionChangeParams } from '../models/params/selectionChangeParams';
 import { ColParams } from '../models/params/colParams';
 import { CellParams } from '../models/params/cellParams';
 
-export const HeaderCheckbox: React.FC<ColParams> = React.memo(({ api }) => {
-  const [isChecked, setChecked] = React.useState(false);
-  const [isIndeterminate, setIndeterminate] = React.useState(false);
+export const HeaderCheckbox: React.FC<ColParams> = ({ api }) => {
+  const selectedRows = api.getSelectedRows();
+  const allRowsCount = api.getAllRowIds().length;
+  const isCurrentIndeterminate = selectedRows.length > 0 && selectedRows.length !== selectedRows;
+  const isCurrentChecked = selectedRows.length === allRowsCount || isCurrentIndeterminate;
+
+  const [isChecked, setChecked] = React.useState(isCurrentChecked);
+  const [isIndeterminate, setIndeterminate] = React.useState(isCurrentIndeterminate);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
     setChecked(checked);
@@ -37,7 +42,7 @@ export const HeaderCheckbox: React.FC<ColParams> = React.memo(({ api }) => {
       inputProps={{ 'aria-label': 'Select All Rows checkbox' }}
     />
   );
-});
+};
 HeaderCheckbox.displayName = 'HeaderCheckbox';
 
 export const CellCheckboxRenderer: React.FC<CellParams> = React.memo(({ api, rowModel, value }) => {
