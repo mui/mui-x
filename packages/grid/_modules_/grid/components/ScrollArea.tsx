88081fe5bc3eeea06630adba243fa43d263b6e86
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-10-10 01:53:43 +0200

    fix scroll smoothness

--- packages/grid/_modules_/grid/components/ScrollArea.tsx
+++ packages/grid/_modules_/grid/components/ScrollArea.tsx
@@ -2,75 +2,79 @@ import * as React from 'react';
 import { COL_REORDER_START, COL_REORDER_STOP, SCROLLING } from '../constants/eventsConstants';
 import { ScrollParams, useApiEventHandler } from '../hooks';
 import { ApiRef } from '../models';
+import { classnames } from '../utils';
 import { ApiContext } from './api-context';
 
-enum Direction {
-  Left = 'left',
-  Right = 'right',
-}
-
-type ScrollDirection = 'left' | 'right';
-
 interface ScrollAreaProps {
-  scrollDirection: ScrollDirection;
+  scrollDirection: 'left' | 'right';
 }
-export const ScrollArea: React.FC<ScrollAreaProps> = React.memo(
-  ({ scrollDirection }: ScrollAreaProps) => {
-    const api = React.useContext(ApiContext);
-    const cursorPossitionX = React.useRef<number | null>(null);
-    const cssClass = `MuiDataGrid-scrollArea ${
-      scrollDirection === Direction.Left
-        ? 'MuiDataGrid-scrollArea-left'
-        : 'MuiDataGrid-scrollArea-right'
-    }`;
-    const [isDragging, setIsDragging] = React.useState<boolean>(false);
-    const scrollStep = React.useRef<number>(2);
-    const scrollStepMultiplier = 3;
-    const currentScrollPosition = React.useRef<ScrollParams | null>({
-      left: 0,
-      top: 0,
-    });
 
-    const setCurrentScrollPosition = React.useCallback(
-      (scrollPosition) => {
-        currentScrollPosition.current = scrollPosition;
-      },
-      [currentScrollPosition],
-    );
+export const ScrollArea = React.memo(function ScrollArea(props: ScrollAreaProps) {
+  const { scrollDirection } = props;
+  const rootRef = React.useRef<HTMLDivElement>(null);
+  const api = React.useContext(ApiContext);
+  const rafRef = React.useRef(0);
+  const timeout = React.useRef<number>();
+  const [dragging, setDragging] = React.useState<boolean>(false);
+  const scrollPosition = React.useRef<ScrollParams>({
+    left: 0,
+    top: 0,
+  });
 
-    const handleDragOver = React.useCallback(
-      (event) => {
-        if (cursorPossitionX.current !== null) {
-          scrollStep.current += event.clientX - cursorPossitionX.current;
-        }
+  const handleScrolling = React.useCallback((newScrollPosition) => {
+    scrollPosition.current = newScrollPosition;
+  }, []);
 
-        cursorPossitionX.current = event.clientX;
-        let newLeftScrollPosition: number;
+  const handleDragOver = React.useCallback(
+    (event) => {
+      let diff;
 
-        if (currentScrollPosition.current!.left <= 0 && scrollDirection === Direction.Left) {
-          newLeftScrollPosition = 0;
-        } else {
-          newLeftScrollPosition =
-            currentScrollPosition.current!.left + scrollStep.current * scrollStepMultiplier;
-        }
+      if (scrollDirection === 'left') {
+        diff = event.clientX - rootRef.current!.getBoundingClientRect().right;
+      } else if (scrollDirection === 'right') {
+        diff = Math.max(1, event.clientX - rootRef.current!.getBoundingClientRect().left);
+      } else {
+        throw new Error('wrong dir');
+      }
 
-        api!.current.scroll({
-          left: newLeftScrollPosition,
-          top: currentScrollPosition.current!.top,
+      diff = (diff - 1) * 1.5 + 1;
+
+      // Throttle, max 60 Hz
+      cancelAnimationFrame(rafRef.current);
+      rafRef.current = requestAnimationFrame(() => {
+        // Wait for the frame to pain before updating the scroll position
+        clearTimeout(timeout.current);
+        timeout.current = setTimeout(() => {
+          api!.current.scroll({
+            left: scrollPosition.current.left + diff,
+            top: scrollPosition.current.top,
+          });
         });
-      },
-      [scrollDirection, currentScrollPosition, scrollStep, api],
-    );
+      });
+    },
+    [scrollDirection, api],
+  );
 
-    const toggleIsDragging = React.useCallback(() => {
-      setIsDragging(!isDragging);
-    }, [isDragging]);
+  React.useEffect(() => {
+    return () => {
+      cancelAnimationFrame(rafRef.current);
+      clearTimeout(timeout.current);
+    };
+  }, []);
 
-    useApiEventHandler(api as ApiRef, SCROLLING, setCurrentScrollPosition);
-    useApiEventHandler(api as ApiRef, COL_REORDER_START, toggleIsDragging);
-    useApiEventHandler(api as ApiRef, COL_REORDER_STOP, toggleIsDragging);
+  const toggleDragging = React.useCallback(() => {
+    setDragging((prevdragging) => !prevdragging);
+  }, []);
 
-    return isDragging ? <div className={cssClass} onDragOver={handleDragOver} /> : null;
-  },
-);
-ScrollArea.displayName = 'ScrollArea';
+  useApiEventHandler(api as ApiRef, SCROLLING, handleScrolling);
+  useApiEventHandler(api as ApiRef, COL_REORDER_START, toggleDragging);
+  useApiEventHandler(api as ApiRef, COL_REORDER_STOP, toggleDragging);
+
+  return dragging ? (
+    <div
+      ref={rootRef}
+      className={classnames('MuiDataGrid-scrollArea', `MuiDataGrid-scrollArea-${scrollDirection}`)}
+      onDragOver={handleDragOver}
+    />
+  ) : null;
+});
