4f6ca21b73a53f8221ad6df93c86ab49af6e48f1
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-10-20 19:34:13 +0200

    [DataGrid] Reduce dependency on lodash, save 1kB gzipped (#450)
    * review

--- packages/grid/_modules_/grid/utils/utils.ts
+++ packages/grid/_modules_/grid/utils/utils.ts
@@ -1,4 +1,3 @@
-import _debounce from '../lib/lodash/debounce';
 import isEqual from '../lib/lodash/isEqual';
 
 export { isEqual };
@@ -8,25 +7,26 @@ export interface DebouncedFunction extends Function {
   flush: () => void;
 }
 
-export function debounce(func: any, wait?: number, options?: any): DebouncedFunction {
-  return _debounce(func, wait, options) as DebouncedFunction;
-}
-
 export function isDate(value: any): value is Date {
   return value instanceof Date;
 }
+
 export function isArray(value: any): value is Array<any> {
   return Array.isArray(value);
 }
+
 export function isString(value: any): value is string {
   return typeof value === 'string';
 }
+
 export function isNumber(value: any): value is number {
   return typeof value === 'number';
 }
+
 export function isFunction(value: any): value is Function {
   return typeof value === 'function';
 }
+
 export function isObject(value: any): value is Record<string, any> {
   return typeof value === 'object';
 }
