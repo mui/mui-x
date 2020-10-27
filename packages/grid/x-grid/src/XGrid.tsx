e39acaf4634d00a14500aadc756125122b6af8c7
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-10-27 14:48:14 +0100

    [DataGrid] Replace require with import (#455)

--- packages/grid/x-grid/src/XGrid.tsx
+++ packages/grid/x-grid/src/XGrid.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { LicenseInfo, useLicenseVerifier } from '@material-ui/x-license';
+import { ponyfillGlobal } from '@material-ui/utils';
 import { GridComponent, GridComponentProps, classnames } from '../../_modules_/grid';
 
 // This is the package release date. Each package version should update this const
 // automatically when a new version is published on npm.
 let RELEASE_INFO = '__RELEASE_INFO__';
-
-// TODO: Refactor to import when moving to material-ui v5
-const ponyfillGlobal = require('@material-ui/utils').ponyfillGlobal;
 
 // eslint-disable-next-line no-useless-concat
 if (process.env.NODE_ENV !== 'production' && RELEASE_INFO === '__RELEASE' + '_INFO__') {
