eb6835263b45ae0286ecea55876db6ee7913345f
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-11-04 10:58:24 +0100

    [DataGrid] Avoid paint step (#531)

--- packages/grid/_modules_/grid/hooks/utils/useScrollFn.ts
+++ packages/grid/_modules_/grid/hooks/utils/useScrollFn.ts
@@ -30,8 +30,9 @@ export function useScrollFn(
         if (renderingZoneElementRef.current!.style.pointerEvents !== 'none') {
           renderingZoneElementRef.current!.style.pointerEvents = 'none';
         }
-        renderingZoneElementRef.current!.style.transform = `translate(-${v.left}px, -${v.top}px)`;
-        columnHeadersElementRef.current!.style.transform = `translate(-${v.left}px, -0px)`;
+        // Force the creation of a layer, avoid paint when changing the transform value.
+        renderingZoneElementRef.current!.style.transform = `translate3d(-${v.left}px, -${v.top}px, 0)`;
+        columnHeadersElementRef.current!.style.transform = `translate3d(-${v.left}px, 0, 0)`;
         debouncedResetPointerEvents();
         previousValue.current = v;
       }
