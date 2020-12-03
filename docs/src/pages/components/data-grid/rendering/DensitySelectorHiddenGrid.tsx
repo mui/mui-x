d9c886b01c2a255ff63b03884ade372ffd745f55
Danail Hadjiatanasov <hadjiatanasov@gmail.com>
2020-12-03 17:27:14 +0100

    Update docs/src/pages/components/data-grid/rendering/DensitySelectorHiddenGrid.tsx
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>

--- docs/src/pages/components/data-grid/rendering/DensitySelectorHiddenGrid.tsx
+++ docs/src/pages/components/data-grid/rendering/DensitySelectorHiddenGrid.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 import { useDemoData } from '@material-ui/x-grid-data-generator';
 
-export default function SizePickerHiddenGrid() {
+export default function DensitySelectorHiddenGrid() {
   const { data } = useDemoData({
     dataSet: 'Commodity',
     rowLength: 4,
