e1a7fb907463cf535bc6fbdc74a3f0e98d618138
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-12-14 16:03:36 +0100

    [core] Update monorepository (#725)

--- docs/src/pages/components/data-grid/rendering/AntDesignGrid.js
+++ docs/src/pages/components/data-grid/rendering/AntDesignGrid.js
@@ -54,9 +54,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     border: 0,
     color:
-      theme.palette.type === 'light'
-        ? 'rgba(0,0,0,.85)'
-        : 'rgba(255,255,255,0.85)',
+      theme.palette.type === 'light' ? 'rgba(0,0,0,.85)' : 'rgba(255,255,255,0.85)',
     fontFamily: [
       '-apple-system',
       'BlinkMacSystemFont',
