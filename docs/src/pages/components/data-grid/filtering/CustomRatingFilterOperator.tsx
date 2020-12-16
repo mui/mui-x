b5a2e929b17ce18bebfcfd76c537d8f01cffce27
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-12-16 19:38:51 +0100

    yarn prettier

--- docs/src/pages/components/data-grid/filtering/CustomRatingFilterOperator.tsx
+++ docs/src/pages/components/data-grid/filtering/CustomRatingFilterOperator.tsx
@@ -1,81 +1,89 @@
 import * as React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { Rating } from '@material-ui/lab';
-import { PreferencePanelsValue, DataGrid, FilterInputValueProps, NUMERIC_OPERATORS } from '@material-ui/data-grid';
+import {
+  PreferencePanelsValue,
+  DataGrid,
+  FilterInputValueProps,
+  NUMERIC_OPERATORS,
+} from '@material-ui/data-grid';
 import { useDemoData } from '@material-ui/x-grid-data-generator';
 
 const useStyles = makeStyles({
-	ratingContainer: {
-		display: 'inline-flex',
-		flexDirection: 'row',
-		alignItems: 'center',
-		height: 48,
-		paddingLeft: 20
-	}
+  ratingContainer: {
+    display: 'inline-flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 48,
+    paddingLeft: 20,
+  },
 });
 
 function RatingInputValue(props: FilterInputValueProps) {
-	const classes = useStyles();
-	const {item, applyValue} = props;
-	const [filterValueState, setFilterValueState] = React.useState<number>(Number(item.value));
+  const classes = useStyles();
+  const { item, applyValue } = props;
+  const [filterValueState, setFilterValueState] = React.useState<number>(
+    Number(item.value),
+  );
 
-	const onFilterChange = React.useCallback(
-		(event) => {
-			const value = event.target.value;
-			setFilterValueState(value);
-			applyValue({...item, value});
-		},
-		[applyValue, item],
-	);
+  const onFilterChange = React.useCallback(
+    (event) => {
+      const value = event.target.value;
+      setFilterValueState(value);
+      applyValue({ ...item, value });
+    },
+    [applyValue, item],
+  );
 
-	React.useEffect(() => {
-		setFilterValueState(Number(item.value));
-	}, [item.value]);
+  React.useEffect(() => {
+    setFilterValueState(Number(item.value));
+  }, [item.value]);
 
-	return (
-		<div className={classes.ratingContainer}>
-			<Rating
-				placeholder={'Filter value'}
-				value={filterValueState}
-				onChange={onFilterChange}
-				precision={0.1}
-			/>
-		</div>
-	);
+  return (
+    <div className={classes.ratingContainer}>
+      <Rating
+        placeholder={'Filter value'}
+        value={filterValueState}
+        onChange={onFilterChange}
+        precision={0.1}
+      />
+    </div>
+  );
 }
 
 const filterModel = {
-	items: [
-		{columnField: 'rating', value: '3.5', operatorValue: '>='},
-	],
+  items: [{ columnField: 'rating', value: '3.5', operatorValue: '>=' }],
 };
 
 export default function CustomFilterOperator() {
-	const {data} = useDemoData({dataSet: 'Employee', rowLength: 100});
+  const { data } = useDemoData({ dataSet: 'Employee', rowLength: 100 });
 
-	React.useEffect(()=> {
-		if(data.columns.length > 0) {
-			const ratingColumn = data.columns.find(col => col.field === 'rating');
-			const ratingOperators = [...NUMERIC_OPERATORS].map(operator => {
-				operator.InputComponent = RatingInputValue;
-				return operator;
-			});
-			ratingColumn!.filterOperators = ratingOperators;
+  React.useEffect(() => {
+    if (data.columns.length > 0) {
+      const ratingColumn = data.columns.find((col) => col.field === 'rating');
+      const ratingOperators = [...NUMERIC_OPERATORS].map((operator) => {
+        operator.InputComponent = RatingInputValue;
+        return operator;
+      });
+      ratingColumn!.filterOperators = ratingOperators;
 
-			// Just hidding some columns for demo clarity
-			data.columns.filter(col => col.field === 'phone' || col.field === 'email' || col.field === 'username').forEach(col => {
-				col.hide = true;
-			});
-		}
-	}, [data.columns])
+      // Just hidding some columns for demo clarity
+      data.columns
+        .filter(
+          (col) =>
+            col.field === 'phone' ||
+            col.field === 'email' ||
+            col.field === 'username',
+        )
+        .forEach((col) => {
+          col.hide = true;
+        });
+    }
+  }, [data.columns]);
 
-	return (
-		<div style={{ height: 400, width: '100%' }}>
-		<DataGrid
-				rows={data.rows}
-				columns={data.columns}
-				filterModel={filterModel}
-			/>
-		</div>
-	);
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid rows={data.rows} columns={data.columns} filterModel={filterModel} />
+    </div>
+  );
 }
--- docs/src/pages/components/data-grid/filtering/filtering.md
+++ docs/src/pages/components/data-grid/filtering/filtering.md
@@ -7,17 +7,16 @@ components: DataGrid, XGrid
 
 <p class="description">Filtering helps view particular or related records in the Data Grid.</p>
 
-
 ## Basic Filtering
 
-Column filters can be set using the column menu and clicking the filter menu item. 
+Column filters can be set using the column menu and clicking the filter menu item.
 Alternatively, if the grid has the toolbar displayed, just need to click on the filter button.
 
 A filtered column can be can pre-configured using the filterModel prop:
 
 ```ts
 const filterModel: FilterModel = {
-  items:[{columnField: 'commodity', operatorValue: 'contains', value: 'rice'}]
+  items: [{ columnField: 'commodity', operatorValue: 'contains', value: 'rice' }],
 };
 ```
 
@@ -30,21 +29,22 @@ const filterModel: FilterModel = {
 Filters are enabled by default, but you can easily disable this feature by setting the `disableColumnFilter` prop.
 
 ```ts
-<DataGrid disableColumnFilter /> 
+<DataGrid disableColumnFilter />
 ```
 
 #### Per column
- 
+
 You can disable the filter on a column by setting the `filterable` prop of the `ColDef` to `false`;
 
-```ts 
-const columns = [{field: 'image', filterable: false}];
+```ts
+const columns = [{ field: 'image', filterable: false }];
 ```
+
 {{"demo": "pages/components/data-grid/filtering/DisableFilteringGrid.js", "bg": "inline"}}
 
 ### Custom filter operator
 
-We currently support different operators for the native column types. 
+We currently support different operators for the native column types.
 However, you can extend the operator and add your own, customize the input component or set your own operator for a new column type.
 
 In this demo, you will see how to reuse the numeric filter and customize the input filter value component.
