cfeeb535c72940cc0a2f96dbe1631fa0f1d2aae0
damien <damien.tassone@gmail.com>
2021-01-14 12:29:41 +0100

    Fix column menu focus and keyboard navigation

--- packages/grid/_modules_/grid/components/menu/GridMenu.tsx
+++ packages/grid/_modules_/grid/components/menu/GridMenu.tsx
@@ -42,13 +42,35 @@ export const GridMenu: React.FC<MenuProps> = ({
   position,
   ...other
 }) => {
+  const menuListRef = React.useRef<HTMLUListElement | null>(null);
+  const focusTimeout = React.useRef<any>();
+
+  React.useEffect(()=> {
+    focusTimeout.current = setTimeout(()=> {
+        if(open && menuListRef.current && target) {
+          const firstItem = menuListRef.current.querySelector('li:first-child');
+          if(firstItem) {
+            (firstItem as HTMLLIElement).focus();
+          } else {
+            menuListRef.current.focus();
+          }
+        }
+      }, 0);
+    }, [target, open, menuListRef]);
+
+  React.useEffect(()=> {
+    return ()=> {
+     clearTimeout(focusTimeout.current);
+    };
+  }, []);
+
   return (
     <Popper open={open} anchorEl={target as any} transition placement={position} {...other}>
       {({ TransitionProps, placement }) => (
         <Grow {...TransitionProps} style={{ transformOrigin: transformOrigin[placement] }}>
           <Paper>
             <ClickAwayListener onClickAway={onClickAway}>
-              <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={onKeyDown}>
+              <MenuList ref={menuListRef} id="menu-list-grow" onKeyDown={onKeyDown}>
                 {children}
               </MenuList>
             </ClickAwayListener>
