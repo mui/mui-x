137e81e478d1277f5accb8df1f36fdcff593d896
尹程 <zj9495@gmail.com>
2021-01-25 18:26:08 +0800

    format code

--- packages/grid/_modules_/grid/components/menu/columnMenu/GridColumnHeaderMenuItems.tsx
+++ packages/grid/_modules_/grid/components/menu/columnMenu/GridColumnHeaderMenuItems.tsx
@@ -23,7 +23,13 @@ export function GridColumnHeaderMenuItems(props: GridColumnHeaderMenuItemsProps)
     [hideMenu],
   );
   return (
-    <MenuList aria-labelledby={`menu-button-${currentColumn.field}`} autoFocus autoFocusItem id="menu-list-grow" onKeyDown={handleListKeyDown}>
+    <MenuList
+      aria-labelledby={`menu-button-${currentColumn.field}`}
+      autoFocus
+      autoFocusItem
+      id="menu-list-grow"
+      onKeyDown={handleListKeyDown}
+    >
       <SortMenuItems onClick={hideMenu} column={currentColumn!} />
       <FilterMenuItem onClick={hideMenu} column={currentColumn!} />
       <HideColMenuItem onClick={hideMenu} column={currentColumn!} />
