b64f8b5378803002c5bd6f936f3a21b021924d7d
damien <damien.tassone@gmail.com>
2021-01-28 18:48:49 +0100

    add missing filter doc

---
+++ docs/src/pages/components/data-grid/filtering/ColumnTypeFilteringGrid.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { InputAdornment } from '@material-ui/core';
+import {
+  DataGrid,
+  getNumericColumnOperators,
+  PreferencePanelsValue,
+} from '@material-ui/data-grid';
+import { useDemoData } from '@material-ui/x-grid-data-generator';
+
+const priceColumnType = {
+  extendType: 'number',
+  filterOperators: getNumericColumnOperators()
+    .filter((operator) => operator.value === '>' || operator.value === '<')
+    .map((operator) => {
+      return {
+        ...operator,
+        InputComponentProps: {
+          inputProps: {
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          },
+        },
+      };
+    }),
+};
+
+export default function NewColumnTypes() {
+  const { data } = useDemoData({ dataSet: 'Commodity', rowLength: 100 });
+  const [cols, setCols] = React.useState(data.columns);
+
+  React.useEffect(() => {
+    if (data.columns.length > 0) {
+      const visibleFields = ['desk', 'commodity', 'totalPrice'];
+      const demoCols = data.columns.map((col) => {
+        const newCol = { ...col, hide: visibleFields.indexOf(col.field) === -1 };
+        if (newCol.field === 'totalPrice') {
+          newCol.type = 'price';
+        }
+        return newCol;
+      });
+      setCols(demoCols);
+    }
+  }, [data]);
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={data.rows}
+        columns={cols}
+        columnTypes={{ price: priceColumnType }}
+        filterModel={{
+          items: [
+            { columnField: 'totalPrice', value: '3000000', operatorValue: '>' },
+          ],
+        }}
+        state={{
+          preferencePanel: {
+            open: true,
+            openedPanelValue: PreferencePanelsValue.filters,
+          },
+        }}
+      />
+    </div>
+  );
+}
---
+++ docs/src/pages/components/data-grid/filtering/ServerFilterGrid.js
@@ -0,0 +1,60 @@
+import { DataGrid } from '@material-ui/data-grid';
+import * as React from 'react';
+
+function getRowsFromServer(commodityFilterValue) {
+  const serverRows = [
+    { id: '1', commodity: 'rice' },
+    { id: '2', commodity: 'soybeans' },
+    { id: '3', commodity: 'milk' },
+    { id: '4', commodity: 'wheat' },
+    { id: '5', commodity: 'oats' },
+  ];
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if (!commodityFilterValue) {
+        resolve(serverRows);
+      }
+      resolve(
+        serverRows.filter(
+          (row) => row.commodity.toLowerCase().indexOf(commodityFilterValue) > -1,
+        ),
+      );
+    }, 500);
+  });
+}
+export default function ServerFilterGrid() {
+  const [columns] = React.useState([{ field: 'commodity', width: 150 }]);
+  const [rows, setRows] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  const fetchRows = React.useCallback(async (filterValue) => {
+    setLoading(true);
+    const serverRows = await getRowsFromServer(filterValue);
+    setRows(serverRows);
+    setLoading(false);
+  }, []);
+
+  const onFilterChange = React.useCallback(
+    async (params) => {
+      await fetchRows(params.filterModel.items[0].value);
+    },
+    [fetchRows],
+  );
+
+  React.useEffect(() => {
+    fetchRows();
+  }, [fetchRows]);
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        filterMode={'server'}
+        onFilterModelChange={onFilterChange}
+        loading={loading}
+      />
+    </div>
+  );
+}
