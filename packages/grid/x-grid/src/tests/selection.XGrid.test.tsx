e16185e61b860549fb1738f9d195fedc39746776
damien tassone <damien.tassone@gmail.com>
2021-02-01 16:01:59 +0100

    Update packages/grid/x-grid/src/tests/selection.XGrid.test.tsx
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>

--- packages/grid/x-grid/src/tests/selection.XGrid.test.tsx
+++ packages/grid/x-grid/src/tests/selection.XGrid.test.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { XGrid, useApiRef, ApiRef } from '@material-ui/x-grid';
-// @ts-expect-error need to migrate helpers to TypeScript
-import { fireEvent, createClientRenderStrictMode } from 'test/utils';
+import { createClientRenderStrictMode } from 'test/utils';
 import { expect } from 'chai';
-import { getCell, getColumnHeaders, getRow } from 'test/utils/helperFn';
+import { getColumnHeaders } from 'test/utils/helperFn';
 
-describe('<XGrid /> - Selection', () => {
+describe('<XGrid /> - Reorder', () => {
   // TODO v5: replace with createClientRender
   const render = createClientRenderStrictMode();
 
@@ -16,17 +15,13 @@ describe('<XGrid /> - Selection', () => {
     }
   });
 
-  it('should not reset the column order when onSelectionChange is binded', () => {
+  it('should not reset the column order when a prop change', () => {
     let apiRef: ApiRef;
+    const rows = [{ id: 0, brand: 'Nike' }];
+    const columns = [{ field: 'brand' }, { field: 'desc' }, { field: 'type' }];
+
     const Test = () => {
       apiRef = useApiRef();
-      const [rows] = React.useState([{ id: 0, brand: 'Nike' }]);
-      const [columns] = React.useState([{ field: 'brand' }, { field: 'desc' }, { field: 'type' }]);
-
-      const [myState, setMyState] = React.useState(false);
-      const handleSelection = React.useCallback(() => {
-        setMyState(!myState);
-      }, [myState]);
 
       return (
         <div style={{ width: 300, height: 300 }}>
@@ -34,20 +29,17 @@ describe('<XGrid /> - Selection', () => {
             apiRef={apiRef}
             rows={rows}
             columns={columns}
-            onSelectionChange={handleSelection}
+            onPageChange={() => {}}
           />
         </div>
       );
     };
 
-    render(<Test />);
+    const { forceUpdate } = render(<Test />);
     expect(getColumnHeaders()).to.deep.equal(['brand', 'desc', 'type']);
     apiRef!.current.moveColumn('brand', 2);
     expect(getColumnHeaders()).to.deep.equal(['desc', 'type', 'brand']);
-    fireEvent.click(getCell(0, 0));
-
-    const row = getRow(0);
-    expect(row.classList.contains('Mui-selected')).to.equal(true);
+    forceUpdate(); // test stability
     expect(getColumnHeaders()).to.deep.equal(['desc', 'type', 'brand']);
   });
 });
