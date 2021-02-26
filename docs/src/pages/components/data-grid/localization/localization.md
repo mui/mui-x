e2eaedb4878a9a42eeae684835f2e5f85324707f
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-26 18:48:14 +0100

    yarn prettier

--- docs/src/pages/components/data-grid/localization/localization.md
+++ docs/src/pages/components/data-grid/localization/localization.md
@@ -52,6 +52,7 @@ The same import works with `XGrid` as it's an extension of `DataGrid`.
 | Bulgarian               | bg-BG               | `bgBG`      |
 | English (United States) | en-US               | `enUS`      |
 | French                  | fr-FR               | `frFR`      |
+| German                  | de-DE               | `deDE`      |
 | Portuguese (Brazil)     | pt-BR               | `ptBR`      |
 
 You can [find the source](https://github.com/mui-org/material-ui-x/blob/HEAD/packages/grid/_modules_/grid/locales) in the GitHub repository.
--- packages/grid/_modules_/grid/locales/deDE.ts
+++ packages/grid/_modules_/grid/locales/deDE.ts
@@ -3,79 +3,82 @@ import { GridLocaleText } from '../models/api/gridLocaleTextApi';
 import { getGridLocalization, Localization } from '../utils';
 
 const deDEGrid: Partial<GridLocaleText> = {
-    // Root
-    rootGridLabel: 'grid',
-    noRowsLabel: 'Keine Einträge',
-    errorOverlayDefaultLabel: 'Ein unvorhergesehener Fehler ist passiert.',
+  // Root
+  rootGridLabel: 'grid',
+  noRowsLabel: 'Keine Einträge',
+  errorOverlayDefaultLabel: 'Ein unvorhergesehener Fehler ist passiert.',
 
-    // Density selector toolbar button text
-    toolbarDensity: 'Zeilenhöhe',
-    toolbarDensityLabel: 'Zeilenhöhe',
-    toolbarDensityCompact: 'Kompakt',
-    toolbarDensityStandard: 'Standard',
-    toolbarDensityComfortable: 'Breit',
+  // Density selector toolbar button text
+  toolbarDensity: 'Zeilenhöhe',
+  toolbarDensityLabel: 'Zeilenhöhe',
+  toolbarDensityCompact: 'Kompakt',
+  toolbarDensityStandard: 'Standard',
+  toolbarDensityComfortable: 'Breit',
 
-    // Columns selector toolbar button text
-    toolbarColumns: 'Spalten',
-    toolbarColumnsLabel: 'Zeige Spaltenauswahl',
+  // Columns selector toolbar button text
+  toolbarColumns: 'Spalten',
+  toolbarColumnsLabel: 'Zeige Spaltenauswahl',
 
-    // Filters toolbar button text
-    toolbarFilters: 'Filter',
-    toolbarFiltersLabel: 'Zeige Filter',
-    toolbarFiltersTooltipHide: 'Verstecke Filter',
-    toolbarFiltersTooltipShow: 'Zeige Filter',
-    toolbarFiltersTooltipActive: (count) => (count !== 1) ? `${count} aktive Filter` : `${count} aktiver Filter`,
-    
-    // Columns panel text
-    columnsPanelTextFieldLabel: 'Finde Spalte',
-    columnsPanelTextFieldPlaceholder: 'Spaltenüberschrift',
-    columnsPanelDragIconLabel: 'Spalte umsortieren',
-    columnsPanelShowAllButton: 'Zeige alle',
-    columnsPanelHideAllButton: 'Verstecke alle',
+  // Filters toolbar button text
+  toolbarFilters: 'Filter',
+  toolbarFiltersLabel: 'Zeige Filter',
+  toolbarFiltersTooltipHide: 'Verstecke Filter',
+  toolbarFiltersTooltipShow: 'Zeige Filter',
+  toolbarFiltersTooltipActive: (count) =>
+    count !== 1 ? `${count} aktive Filter` : `${count} aktiver Filter`,
 
-    // Filter panel text
-    filterPanelAddFilter: 'Filter hinzufügen',
-    filterPanelDeleteIconLabel: 'Löschen',
-    filterPanelOperators: 'Operatoren',
-    filterPanelOperatorAnd: 'Und',
-    filterPanelOperatorOr: 'Oder',
-    filterPanelColumns: 'Spalten',
-    filterPanelInputLabel: 'Wert',
-    filterPanelInputPlaceholder: 'Wert filtern',
+  // Columns panel text
+  columnsPanelTextFieldLabel: 'Finde Spalte',
+  columnsPanelTextFieldPlaceholder: 'Spaltenüberschrift',
+  columnsPanelDragIconLabel: 'Spalte umsortieren',
+  columnsPanelShowAllButton: 'Zeige alle',
+  columnsPanelHideAllButton: 'Verstecke alle',
 
-    // Filter operators text
-    filterOperatorContains: 'beinhaltet',
-    filterOperatorEquals: 'ist gleich',
-    filterOperatorStartsWith: 'beginnt mit',
-    filterOperatorEndsWith: 'endet mit',
-    filterOperatorIs: 'ist',
-    filterOperatorNot: 'ist nicht',
-    filterOperatorOnOrAfter: 'ist an oder nach',
-    filterOperatorBefore: 'ist vor',
-    filterOperatorOnOrBefore: 'ist an oder vor',
-    filterOperatorAfter: 'ist nach',
+  // Filter panel text
+  filterPanelAddFilter: 'Filter hinzufügen',
+  filterPanelDeleteIconLabel: 'Löschen',
+  filterPanelOperators: 'Operatoren',
+  filterPanelOperatorAnd: 'Und',
+  filterPanelOperatorOr: 'Oder',
+  filterPanelColumns: 'Spalten',
+  filterPanelInputLabel: 'Wert',
+  filterPanelInputPlaceholder: 'Wert filtern',
 
-    // Column menu text
-    columnMenuLabel: 'Menu',
-    columnMenuShowColumns: 'Zeige alle Spalten',
-    columnMenuFilter: 'Filter',
-    columnMenuHideColumn: 'Verstecken',
-    columnMenuUnsort: 'Sortierung deaktivieren',
-    columnMenuSortAsc: 'Sortiere aufsteigend',
-    columnMenuSortDesc: 'Sortiere absteigend',
+  // Filter operators text
+  filterOperatorContains: 'beinhaltet',
+  filterOperatorEquals: 'ist gleich',
+  filterOperatorStartsWith: 'beginnt mit',
+  filterOperatorEndsWith: 'endet mit',
+  filterOperatorIs: 'ist',
+  filterOperatorNot: 'ist nicht',
+  filterOperatorOnOrAfter: 'ist an oder nach',
+  filterOperatorBefore: 'ist vor',
+  filterOperatorOnOrBefore: 'ist an oder vor',
+  filterOperatorAfter: 'ist nach',
 
-    // Column header text
-    columnHeaderFiltersTooltipActive: (count) => (count !== 1) ? `${count} aktive Filter` : `${count} aktiver Filter`,
-    columnHeaderFiltersLabel: 'Zeige Filter',
-    columnHeaderSortIconLabel: 'Sortieren',
+  // Column menu text
+  columnMenuLabel: 'Menu',
+  columnMenuShowColumns: 'Zeige alle Spalten',
+  columnMenuFilter: 'Filter',
+  columnMenuHideColumn: 'Verstecken',
+  columnMenuUnsort: 'Sortierung deaktivieren',
+  columnMenuSortAsc: 'Sortiere aufsteigend',
+  columnMenuSortDesc: 'Sortiere absteigend',
 
-    // Rows selected footer text
-    footerRowSelected: (count) => (count !== 1
-    ? `${count.toLocaleString()} Einträge ausgewählt`
-    : `${count.toLocaleString()} Eintrag ausgewählt`),
+  // Column header text
+  columnHeaderFiltersTooltipActive: (count) =>
+    count !== 1 ? `${count} aktive Filter` : `${count} aktiver Filter`,
+  columnHeaderFiltersLabel: 'Zeige Filter',
+  columnHeaderSortIconLabel: 'Sortieren',
 
-    // Total rows footer text
-    footerTotalRows: 'Gesamt:',
+  // Rows selected footer text
+  footerRowSelected: (count) =>
+    count !== 1
+      ? `${count.toLocaleString()} Einträge ausgewählt`
+      : `${count.toLocaleString()} Eintrag ausgewählt`,
+
+  // Total rows footer text
+  footerTotalRows: 'Gesamt:',
 };
 
 export const deDE: Localization = getGridLocalization(deDEGrid, deDECore);
