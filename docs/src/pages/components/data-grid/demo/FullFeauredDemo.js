cdfdb4553deac288198012411f0b492f80ca244a
Danail H <hadjiatanasov@gmail.com>
2021-03-03 14:38:49 +0100

    Fix performance related issue

--- docs/src/pages/components/data-grid/demo/FullFeauredDemo.js
+++ docs/src/pages/components/data-grid/demo/FullFeauredDemo.js
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import { XGrid, GridToolbar } from '@material-ui/x-grid';
-import {
-  useDemoData,
-  getCommodityColumns,
-  getEmployeeColumns,
-  getRealData,
-} from '@material-ui/x-grid-data-generator';
+import { useDemoData } from '@material-ui/x-grid-data-generator';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
@@ -97,15 +92,14 @@ export default function FullFeaturedDemo() {
   const classes = useStyles();
   const [type, setType] = React.useState('Commodity');
   const [size, setSize] = React.useState(100);
-  const { data } = useDemoData({
+  const { data, setRowLength, loadNewData } = useDemoData({
     dataSet: type,
     rowLength: size,
     maxColumns: 20,
   });
-  const [gridData, setGridData] = React.useState({});
   const [pagination, setPagination] = React.useState({});
 
-  const handleApplyClick = async (settings) => {
+  const handleApplyClick = (settings) => {
     if (size !== settings.size) {
       setSize(settings.size);
     }
@@ -114,13 +108,9 @@ export default function FullFeaturedDemo() {
       setType(settings.type.toLowerCase());
     }
 
-    const newData = await getRealData(
-      settings.size,
-      settings.type === 'Commodity' ? getCommodityColumns() : getEmployeeColumns(),
-    );
-
-    if (newData.columns.length && newData.rows.length) {
-      setGridData(newData);
+    if (size !== settings.size || type !== settings.type) {
+      setRowLength(settings.size);
+      loadNewData();
     }
 
     const newPaginationSettings = {
@@ -146,8 +136,7 @@ export default function FullFeaturedDemo() {
     <div className={classes.root}>
       <SettingsPanel onApply={handleApplyClick} size={size} type={type} />
       <XGrid
-        columns={gridData.columns || data.columns}
-        rows={gridData.rows || data.rows}
+        {...data}
         components={{
           Toolbar: GridToolbar,
         }}
