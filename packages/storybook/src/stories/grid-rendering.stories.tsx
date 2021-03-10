0fa88ca781a58bbae7e25a8d58be05d2ff097e67
Danail H <hadjiatanasov@gmail.com>
2021-03-10 11:10:09 +0100

    Add storybook demo

--- packages/storybook/src/stories/grid-rendering.stories.tsx
+++ packages/storybook/src/stories/grid-rendering.stories.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
-import { XGrid } from '@material-ui/x-grid';
+import { XGrid, GridOverlay, useGridApiRef } from '@material-ui/x-grid';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { useDemoData } from '@material-ui/x-grid-data-generator';
 import '../style/grid-stories.css';
 import { action } from '@storybook/addon-actions';
+
+function CustomLoadingOverlay() {
+  return (
+    <GridOverlay>
+      <div style={{ position: 'absolute', top: 0, width: '100%' }}>
+        <LinearProgress />
+      </div>
+    </GridOverlay>
+  );
+}
 
 export default {
   title: 'X-Grid Tests/Rendering',
@@ -37,6 +49,36 @@ export const RenderInputInCell = () => {
   return (
     <div className="grid-container">
       <XGrid rows={rows} columns={columns} />
+    </div>
+  );
+};
+export const InfiniteLoading = () => {
+  const apiRef = useGridApiRef();
+  const [size, setSize] = React.useState(50);
+  const { loading, data, setRowLength, loadNewData } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: size,
+    maxColumns: 20,
+  });
+
+  const handleOnScrollBottom = () => {
+    const newRowLength = size + 20;
+    setSize(newRowLength);
+    setRowLength(newRowLength);
+    loadNewData();
+  };
+
+  return (
+    <div className="grid-container">
+      <XGrid
+        {...data}
+        apiRef={apiRef}
+        loading={loading}
+        onScrollBottom={handleOnScrollBottom}
+        components={{
+          LoadingOverlay: CustomLoadingOverlay,
+        }}
+      />
     </div>
   );
 };
