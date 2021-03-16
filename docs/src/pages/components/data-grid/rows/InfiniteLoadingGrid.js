182456d7f093e1e3d2fe43531d8c712bdaba6a99
Danail H <hadjiatanasov@gmail.com>
2021-03-16 13:53:43 +0100

    Polish demos

--- docs/src/pages/components/data-grid/rows/InfiniteLoadingGrid.js
+++ docs/src/pages/components/data-grid/rows/InfiniteLoadingGrid.js
@@ -3,6 +3,8 @@ import { XGrid, GridOverlay } from '@material-ui/x-grid';
 import { useDemoData } from '@material-ui/x-grid-data-generator';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
+const MAX_ROW_LENGTH = 500;
+
 function CustomLoadingOverlay() {
   return (
     <GridOverlay>
@@ -15,6 +17,7 @@ function CustomLoadingOverlay() {
 
 export default function InfiniteLoadingGrid() {
   const [loading, setLoading] = React.useState(false);
+  const timeout = React.useRef(null);
   const { data, setRowLength } = useDemoData({
     dataSet: 'Commodity',
     rowLength: 20,
@@ -22,17 +25,27 @@ export default function InfiniteLoadingGrid() {
   });
 
   const loadServerRows = (newRowLength) => {
+    clearTimeout(timeout.current);
     setLoading(true);
-    setTimeout(() => {
+
+    timeout.current = setTimeout(() => {
       setRowLength(newRowLength);
       setLoading(false);
+      // Simulate network throttle
     }, Math.random() * 500 + 100);
   };
 
   const handleOnRowsScrollEnd = (params) => {
     const newRowLength = params.virtualRowsCount + params.viewportPageSize;
-    loadServerRows(newRowLength);
+
+    if (newRowLength <= MAX_ROW_LENGTH) {
+      loadServerRows(newRowLength);
+    }
   };
+
+  React.useEffect(() => {
+    return () => clearTimeout(timeout.current);
+  }, []);
 
   return (
     <div style={{ height: 400, width: '100%' }}>
