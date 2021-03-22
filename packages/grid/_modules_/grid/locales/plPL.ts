176623c26e18457cb4ada8c2159e39d7baf5982b
Michal Lukowski <michal.lukowski@imgw.pl>
2021-03-22 12:25:27 +0100

    Code formatted with Prettier

--- packages/grid/_modules_/grid/locales/plPL.ts
+++ packages/grid/_modules_/grid/locales/plPL.ts
@@ -1,86 +1,84 @@
-import { plPL as plPLCore } from "@material-ui/core/locale";
+import { plPL as plPLCore } from '@material-ui/core/locale';
 import { GridLocaleText } from '../models/api/gridLocaleTextApi';
 import { getGridLocalization, Localization } from '../utils';
 
 export const plPLGrid: Partial<GridLocaleText> = {
   // Root
-  rootGridLabel: "grid",
-  noRowsLabel: "Brak danych",
-  errorOverlayDefaultLabel: "Wystąpił błąd.",
+  rootGridLabel: 'grid',
+  noRowsLabel: 'Brak danych',
+  errorOverlayDefaultLabel: 'Wystąpił błąd.',
 
   // Density selector toolbar button text
-  toolbarDensity: "Wysokość rzędu",
-  toolbarDensityLabel: "Wysokość rzędu",
-  toolbarDensityCompact: "Kompakt",
-  toolbarDensityStandard: "Standard",
-  toolbarDensityComfortable: "Komfort",
+  toolbarDensity: 'Wysokość rzędu',
+  toolbarDensityLabel: 'Wysokość rzędu',
+  toolbarDensityCompact: 'Kompakt',
+  toolbarDensityStandard: 'Standard',
+  toolbarDensityComfortable: 'Komfort',
 
   // Columns selector toolbar button text
-  toolbarColumns: "Kolumny",
-  toolbarColumnsLabel: "Zaznacz kolumny",
+  toolbarColumns: 'Kolumny',
+  toolbarColumnsLabel: 'Zaznacz kolumny',
 
   // Filters toolbar button text
-  toolbarFilters: "Filtry",
-  toolbarFiltersLabel: "Pokaż filtry",
-  toolbarFiltersTooltipHide: "Ukryj filtry",
-  toolbarFiltersTooltipShow: "Pokaż filtry",
+  toolbarFilters: 'Filtry',
+  toolbarFiltersLabel: 'Pokaż filtry',
+  toolbarFiltersTooltipHide: 'Ukryj filtry',
+  toolbarFiltersTooltipShow: 'Pokaż filtry',
   toolbarFiltersTooltipActive: (count) => `Ilość aktywnych filtrów: ${count}`,
 
   // Export selector toolbar button text
-  toolbarExport: "Eksportuj",
-  toolbarExportLabel: "Eksportuj",
-  toolbarExportCSV: "Pobierz jako plik CSV",
+  toolbarExport: 'Eksportuj',
+  toolbarExportLabel: 'Eksportuj',
+  toolbarExportCSV: 'Pobierz jako plik CSV',
 
   // Columns panel text
-  columnsPanelTextFieldLabel: "Znajdź kolumnę",
-  columnsPanelTextFieldPlaceholder: "Tytuł kolumny",
-  columnsPanelDragIconLabel: "Zmień kolejność kolumn",
-  columnsPanelShowAllButton: "Pokaż wszystko",
-  columnsPanelHideAllButton: "Ukryj wszystko",
+  columnsPanelTextFieldLabel: 'Znajdź kolumnę',
+  columnsPanelTextFieldPlaceholder: 'Tytuł kolumny',
+  columnsPanelDragIconLabel: 'Zmień kolejność kolumn',
+  columnsPanelShowAllButton: 'Pokaż wszystko',
+  columnsPanelHideAllButton: 'Ukryj wszystko',
 
   // Filter panel text
-  filterPanelAddFilter: "Dodaj filtr",
-  filterPanelDeleteIconLabel: "Usuń",
-  filterPanelOperators: "Operator",
-  filterPanelOperatorAnd: "Dodaj",
-  filterPanelOperatorOr: "Lub",
-  filterPanelColumns: "Kolumny",
-  filterPanelInputLabel: "Wartość",
-  filterPanelInputPlaceholder: "Filtrowana wartość",
+  filterPanelAddFilter: 'Dodaj filtr',
+  filterPanelDeleteIconLabel: 'Usuń',
+  filterPanelOperators: 'Operator',
+  filterPanelOperatorAnd: 'Dodaj',
+  filterPanelOperatorOr: 'Lub',
+  filterPanelColumns: 'Kolumny',
+  filterPanelInputLabel: 'Wartość',
+  filterPanelInputPlaceholder: 'Filtrowana wartość',
 
   // Filter operators text
-  filterOperatorContains: "zawiera",
-  filterOperatorEquals: "równa się",
-  filterOperatorStartsWith: "zaczyna się od",
-  filterOperatorEndsWith: "kończy się na",
-  filterOperatorIs: "równa się",
-  filterOperatorNot: "różne",
-  filterOperatorAfter: "większe niż",
-  filterOperatorOnOrAfter: "większe lub równe",
-  filterOperatorBefore: "mniejsze niż",
-  filterOperatorOnOrBefore: "mniejsze lub równe",
+  filterOperatorContains: 'zawiera',
+  filterOperatorEquals: 'równa się',
+  filterOperatorStartsWith: 'zaczyna się od',
+  filterOperatorEndsWith: 'kończy się na',
+  filterOperatorIs: 'równa się',
+  filterOperatorNot: 'różne',
+  filterOperatorAfter: 'większe niż',
+  filterOperatorOnOrAfter: 'większe lub równe',
+  filterOperatorBefore: 'mniejsze niż',
+  filterOperatorOnOrBefore: 'mniejsze lub równe',
 
   // Column menu text
-  columnMenuLabel: "Menu",
-  columnMenuShowColumns: "Pokaż wszystkie kolumny",
-  columnMenuFilter: "Filtr",
-  columnMenuHideColumn: "Ukryj",
-  columnMenuUnsort: "Anuluj sortowanie",
-  columnMenuSortAsc: "Sortuj rosnąco",
-  columnMenuSortDesc: "Sortuj malejąco",
+  columnMenuLabel: 'Menu',
+  columnMenuShowColumns: 'Pokaż wszystkie kolumny',
+  columnMenuFilter: 'Filtr',
+  columnMenuHideColumn: 'Ukryj',
+  columnMenuUnsort: 'Anuluj sortowanie',
+  columnMenuSortAsc: 'Sortuj rosnąco',
+  columnMenuSortDesc: 'Sortuj malejąco',
 
   // Column header text
-  columnHeaderFiltersTooltipActive: (count) =>
-    `Ilość aktywnych filtrów: ${count}`,
-  columnHeaderFiltersLabel: "Pokaż filtry",
-  columnHeaderSortIconLabel: "Sortuj",
+  columnHeaderFiltersTooltipActive: (count) => `Ilość aktywnych filtrów: ${count}`,
+  columnHeaderFiltersLabel: 'Pokaż filtry',
+  columnHeaderSortIconLabel: 'Sortuj',
 
   // Rows selected footer text
-  footerRowSelected: (count) =>
-    `Ilość wybranych wierszy: ${count.toLocaleString()}`,
+  footerRowSelected: (count) => `Ilość wybranych wierszy: ${count.toLocaleString()}`,
 
   // Total rows footer text
-  footerTotalRows: "Łączna liczba wierszy:",
+  footerTotalRows: 'Łączna liczba wierszy:',
 };
 
 export const plPL: Localization = getGridLocalization(plPLGrid, plPLCore);
