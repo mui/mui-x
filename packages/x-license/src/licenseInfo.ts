3f9197f429353c95317867f9808ee4a9da2f37db
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-04-11 00:49:21 +0200

    fix build

--- packages/x-license/src/licenseInfo.ts
+++ packages/x-license/src/licenseInfo.ts
@@ -5,7 +5,7 @@ import { ponyfillGlobal } from '@material-ui/utils';
 // when module duplication occurs. The duplication of the modules can happen
 // if using multiple version of Material-UI X at the same time of the bundler
 // decide to duplicate to improve the size of the chunks.
-ponyfillGlobal.__MUI_LICENSE_INFO__ ||= {
+ponyfillGlobal.__MUI_LICENSE_INFO__ = ponyfillGlobal.__MUI_LICENSE_INFO__ || {
   key: undefined as undefined | string,
   releaseInfo: undefined as undefined | string,
 };
