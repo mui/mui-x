24d068c1cbc5672fb06541b80354c9c06ca34d00
Danail H <hadjiatanasov@gmail.com>
2021-04-12 16:39:13 +0200

    Fix type

--- packages/grid/_modules_/grid/components/toolbar/GridFilterToolbarButton.tsx
+++ packages/grid/_modules_/grid/components/toolbar/GridFilterToolbarButton.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Badge from '@material-ui/core/Badge';
-import Button from '@material-ui/core/Button';
+import Button, { ButtonProps } from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
 import { capitalize } from '@material-ui/core/utils';
 import { gridColumnLookupSelector } from '../../hooks/features/columns/gridColumnsSelector';
@@ -26,77 +26,76 @@ const useStyles = makeStyles(
   { name: 'MuiDataGridFilterToolbarButton' },
 );
 
-export const GridFilterToolbarButton = React.forwardRef<
-  HTMLButtonElement,
-  React.HTMLAttributes<HTMLButtonElement>
->(function GridFilterToolbarButton(props, ref) {
-  const classes = useStyles();
-  const apiRef = React.useContext(GridApiContext);
-  const options = useGridSelector(apiRef, optionsSelector);
-  const counter = useGridSelector(apiRef, filterGridItemsCounterSelector);
-  const activeFilters = useGridSelector(apiRef, activeGridFilterItemsSelector);
-  const lookup = useGridSelector(apiRef, gridColumnLookupSelector);
-  const preferencePanel = useGridSelector(apiRef, gridPreferencePanelStateSelector);
+export const GridFilterToolbarButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  function GridFilterToolbarButton(props, ref) {
+    const classes = useStyles();
+    const apiRef = React.useContext(GridApiContext);
+    const options = useGridSelector(apiRef, optionsSelector);
+    const counter = useGridSelector(apiRef, filterGridItemsCounterSelector);
+    const activeFilters = useGridSelector(apiRef, activeGridFilterItemsSelector);
+    const lookup = useGridSelector(apiRef, gridColumnLookupSelector);
+    const preferencePanel = useGridSelector(apiRef, gridPreferencePanelStateSelector);
 
-  const tooltipContentNode = React.useMemo(() => {
-    if (preferencePanel.open) {
-      return apiRef!.current.getLocaleText('toolbarFiltersTooltipHide') as React.ReactElement;
-    }
-    if (counter === 0) {
-      return apiRef!.current.getLocaleText('toolbarFiltersTooltipShow') as React.ReactElement;
-    }
-    return (
-      <div>
-        {apiRef!.current.getLocaleText('toolbarFiltersTooltipActive')(counter)}
-        <ul className={classes.list}>
-          {activeFilters.map((item) => ({
-            ...(lookup[item.columnField!] && (
-              <li key={item.id}>
-                {`${lookup[item.columnField!].headerName || item.columnField}
+    const tooltipContentNode = React.useMemo(() => {
+      if (preferencePanel.open) {
+        return apiRef!.current.getLocaleText('toolbarFiltersTooltipHide') as React.ReactElement;
+      }
+      if (counter === 0) {
+        return apiRef!.current.getLocaleText('toolbarFiltersTooltipShow') as React.ReactElement;
+      }
+      return (
+        <div>
+          {apiRef!.current.getLocaleText('toolbarFiltersTooltipActive')(counter)}
+          <ul className={classes.list}>
+            {activeFilters.map((item) => ({
+              ...(lookup[item.columnField!] && (
+                <li key={item.id}>
+                  {`${lookup[item.columnField!].headerName || item.columnField}
                   ${apiRef!.current.getLocaleText(
                     `filterOperator${capitalize(item.operatorValue!)}` as GridTranslationKeys,
                   )}
                   ${item.value}`}
-              </li>
-            )),
-          }))}
-        </ul>
-      </div>
-    );
-  }, [apiRef, preferencePanel.open, counter, activeFilters, lookup, classes]);
+                </li>
+              )),
+            }))}
+          </ul>
+        </div>
+      );
+    }, [apiRef, preferencePanel.open, counter, activeFilters, lookup, classes]);
 
-  const toggleFilter = React.useCallback(() => {
-    const { open, openedPanelValue } = preferencePanel;
-    if (open && openedPanelValue === GridPreferencePanelsValue.filters) {
-      apiRef!.current.hideFilterPanel();
-    } else {
-      apiRef!.current.showFilterPanel();
+    const toggleFilter = React.useCallback(() => {
+      const { open, openedPanelValue } = preferencePanel;
+      if (open && openedPanelValue === GridPreferencePanelsValue.filters) {
+        apiRef!.current.hideFilterPanel();
+      } else {
+        apiRef!.current.showFilterPanel();
+      }
+    }, [apiRef, preferencePanel]);
+
+    // Disable the button if the corresponding is disabled
+    if (options.disableColumnFilter) {
+      return null;
     }
-  }, [apiRef, preferencePanel]);
 
-  // Disable the button if the corresponding is disabled
-  if (options.disableColumnFilter) {
-    return null;
-  }
-
-  const OpenFilterButtonIcon = apiRef!.current.components!.OpenFilterButtonIcon!;
-  return (
-    <Tooltip title={tooltipContentNode} enterDelay={1000}>
-      <Button
-        ref={ref}
-        {...props}
-        onClick={toggleFilter}
-        size="small"
-        color="primary"
-        aria-label={apiRef!.current.getLocaleText('toolbarFiltersLabel')}
-        startIcon={
-          <Badge badgeContent={counter} color="primary">
-            <OpenFilterButtonIcon />
-          </Badge>
-        }
-      >
-        {apiRef!.current.getLocaleText('toolbarFilters')}
-      </Button>
-    </Tooltip>
-  );
-});
+    const OpenFilterButtonIcon = apiRef!.current.components!.OpenFilterButtonIcon!;
+    return (
+      <Tooltip title={tooltipContentNode} enterDelay={1000}>
+        <Button
+          ref={ref}
+          {...props}
+          onClick={toggleFilter}
+          size="small"
+          color="primary"
+          aria-label={apiRef!.current.getLocaleText('toolbarFiltersLabel')}
+          startIcon={
+            <Badge badgeContent={counter} color="primary">
+              <OpenFilterButtonIcon />
+            </Badge>
+          }
+        >
+          {apiRef!.current.getLocaleText('toolbarFilters')}
+        </Button>
+      </Tooltip>
+    );
+  },
+);
