27c626618173bf5f10540f776a54d4b84298b3ed
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-04-13 15:53:54 +0200

    [DataGrid] Ignore event from portal in cells (#1324)

--- packages/grid/data-grid/src/tests/rows.DataGrid.test.tsx
+++ packages/grid/data-grid/src/tests/rows.DataGrid.test.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
-import { createClientRenderStrictMode } from 'test/utils';
+import {
+  createClientRenderStrictMode,
+  // @ts-expect-error need to migrate helpers to TypeScript
+  fireEvent,
+} from 'test/utils';
 import { expect } from 'chai';
+import { spy } from 'sinon';
+import Portal from '@material-ui/core/Portal';
 import { DataGrid } from '@material-ui/data-grid';
 import { getColumnValues } from 'test/utils/helperFn';
 
@@ -46,5 +52,38 @@ describe('<DataGrid /> - Rows', () => {
       );
       expect(getColumnValues()).to.deep.equal(['c1', 'c2', 'c3']);
     });
+  });
+
+  it('should ignore events coming from a portal in the cell', () => {
+    const handleRowClick = spy();
+    const InputCell = () => <input type="text" name="input" />;
+    const PortalCell = () => (
+      <Portal>
+        <input type="text" name="portal-input" />
+      </Portal>
+    );
+
+    render(
+      <div style={{ width: 300, height: 300 }}>
+        <DataGrid
+          rows={[{ id: '1' }]}
+          onRowClick={handleRowClick}
+          columns={[
+            {
+              field: 'id',
+              renderCell: () => <PortalCell />,
+            },
+            {
+              field: 'input',
+              renderCell: () => <InputCell />,
+            },
+          ]}
+        />
+      </div>,
+    );
+    fireEvent.click(document.querySelector('input[name="portal-input"]'));
+    expect(handleRowClick.callCount).to.equal(0);
+    fireEvent.click(document.querySelector('input[name="input"]'));
+    expect(handleRowClick.callCount).to.equal(1);
   });
 });
