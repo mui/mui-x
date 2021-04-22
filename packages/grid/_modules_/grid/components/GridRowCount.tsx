3a5793d1b5fe8a2cd742401be83aad018727643c
Danail H <hadjiatanasov@gmail.com>
2021-04-22 13:58:41 +0200

    Forward props for the Footer components

--- packages/grid/_modules_/grid/components/GridRowCount.tsx
+++ packages/grid/_modules_/grid/components/GridRowCount.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
+import { classnames } from '../utils/classnames';
 import { GridApiContext } from './GridApiContext';
 
-export const GridRowCount: React.FC<{ rowCount: number }> = ({ rowCount }) => {
-  const apiRef = React.useContext(GridApiContext);
+interface RowCountProps {
+  rowCount: number;
+}
 
-  if (rowCount === 0) {
-    return null;
-  }
-  return (
-    <div className="MuiDataGrid-rowCount">
-      {`${apiRef!.current.getLocaleText('footerTotalRows')} ${rowCount.toLocaleString()}`}
-    </div>
-  );
-};
+type GridRowCountProps = React.HTMLAttributes<HTMLDivElement> & RowCountProps;
+
+export const GridRowCount = React.forwardRef<HTMLDivElement, GridRowCountProps>(
+  function GridRowCount(props, ref) {
+    const { className, rowCount, ...other } = props;
+    const apiRef = React.useContext(GridApiContext);
+
+    if (rowCount === 0) {
+      return null;
+    }
+
+    return (
+      <div ref={ref} className={classnames('MuiDataGrid-rowCount', className)} {...other}>
+        {`${apiRef!.current.getLocaleText('footerTotalRows')} ${rowCount.toLocaleString()}`}
+      </div>
+    );
+  },
+);
