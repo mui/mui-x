6cb6a323ad3b6defedcd974ad8712177f3ebc226
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-04-25 12:35:22 +0200

    remove jest

--- packages/x-license/src/generateLicense.test.ts
+++ packages/x-license/src/generateLicense.test.ts
@@ -1,8 +1,11 @@
-import { generateLicence } from '../generateLicense';
+import { expect } from 'chai';
+import { generateLicence } from './generateLicense';
 
 describe('License: generateLicense', () => {
   it('should generate License properly', () => {
-    expect(generateLicence({ expiryDate: new Date(1591723879062), orderNumber: 'MUI-123' })).toBe(
+    expect(
+      generateLicence({ expiryDate: new Date(1591723879062), orderNumber: 'MUI-123' }),
+    ).to.equal(
       'e34253b37166e7a4a85189b91b653e63T1JERVI6TVVJLTEyMyxFWFBJUlk9MTU5MTcyMzg3OTA2MixLRVlWRVJTSU9OPTE=',
     );
   });
