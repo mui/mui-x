01b8e85e0eab402d6f154f163743463ba23b64f1
damien <damien.tassone@gmail.com>
2021-05-06 18:13:51 +0200

    fix issue

--- packages/grid/_modules_/grid/components/cell/GridEditInputCell.tsx
+++ packages/grid/_modules_/grid/components/cell/GridEditInputCell.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import InputBase, { InputBaseProps } from '@material-ui/core/InputBase';
 import { GRID_CELL_EDIT_PROPS_CHANGE } from '../../constants/eventsConstants';
 import { GridCellParams } from '../../models/params/gridCellParams';
-import { formatDateToLocalInputDate, isDate, mapColDefTypeToInputType } from '../../utils/utils';
+import { formatDateToLocalInputDate, isDate, isDateValid, mapColDefTypeToInputType } from '../../utils/utils';
 
 export function GridEditInputCell(props: GridCellParams & InputBaseProps) {
   const {
@@ -22,27 +22,36 @@ export function GridEditInputCell(props: GridCellParams & InputBaseProps) {
   } = props;
 
   const [valueState, setValueState] = React.useState(value);
+  const inputType = mapColDefTypeToInputType(colDef.type);
 
   const handleChange = React.useCallback(
     (event) => {
       const newValue = event.target.value;
+      let isValid = false;
+      setValueState(prevState => {
+        isValid = !isDate(prevState) || !!newValue; //if the input date is invalid, it triggers on change ''
+        return isValid ? newValue : prevState;
+      });
+
+      if(!isValid) {
+        return;
+      }
+
       const editProps = {
         value: colDef.type === 'date' || colDef.type === 'dateTime' ? new Date(newValue) : newValue,
       };
-      setValueState(newValue);
       api.publishEvent(GRID_CELL_EDIT_PROPS_CHANGE, { id, field, props: editProps }, event);
     },
     [api, colDef.type, field, id],
   );
 
-  const inputType = mapColDefTypeToInputType(colDef.type);
   const inputFormattedValue =
     valueState && isDate(valueState)
       ? formatDateToLocalInputDate({ value: valueState, withTime: colDef.type === 'dateTime' })
       : valueState;
 
   React.useEffect(() => {
-    setValueState(value);
+      setValueState(value);
   }, [value]);
 
   return (
@@ -53,6 +62,7 @@ export function GridEditInputCell(props: GridCellParams & InputBaseProps) {
       type={inputType}
       value={inputFormattedValue}
       onChange={handleChange}
+
       {...other}
     />
   );
