cec3d3b814930605d70c694f989f0297bb017b64
Zeeshan T <zeeshan.tamboli@gmail.com>
2021-05-08 23:28:59 +0530

    fix: removed checked argument from Checkbox onChange

--- packages/grid/_modules_/grid/components/columnSelection/GridCellCheckboxRenderer.tsx
+++ packages/grid/_modules_/grid/components/columnSelection/GridCellCheckboxRenderer.tsx
@@ -12,7 +12,8 @@ const GridCellCheckboxForwardRef = React.forwardRef<HTMLInputElement, GridCellPa
     const apiRef = React.useContext(GridApiContext);
     const tabIndexState = useGridSelector(apiRef, gridTabIndexCellSelector);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const checked = event.target.checked;
       apiRef!.current.selectRow(id, checked, true);
     };
     const tabIndex =
