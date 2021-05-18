df11fa850d086d46013455cace4d3bd25d15d2a1
Marija Najdova <mnajdova@gmail.com>
2021-05-18 11:15:17 +0200

    merge conflicts

--- docs/src/pages/components/data-grid/style/AntDesignGrid.tsx
+++ docs/src/pages/components/data-grid/style/AntDesignGrid.tsx
@@ -94,48 +94,12 @@ const useStyles = makeStyles(
           color:
             theme.palette.type === 'light'
               ? 'rgba(0,0,0,.85)'
-              : 'rgba(255,255,255,0.85)',
-          fontFamily: [
-            '-apple-system',
-            'BlinkMacSystemFont',
-            '"Segoe UI"',
-            'Roboto',
-            '"Helvetica Neue"',
-            'Arial',
-            'sans-serif',
-            '"Apple Color Emoji"',
-            '"Segoe UI Emoji"',
-            '"Segoe UI Symbol"',
-          ].join(','),
-          WebkitFontSmoothing: 'auto',
-          letterSpacing: 'normal',
-          '& .MuiDataGrid-columnsContainer': {
-            backgroundColor: theme.palette.type === 'light' ? '#fafafa' : '#1d1d1d',
-          },
-          '& .MuiDataGrid-iconSeparator': {
-            display: 'none',
-          },
-          '& .MuiDataGrid-colCell, .MuiDataGrid-cell': {
-            borderRight: `1px solid ${
-              theme.palette.type === 'light' ? '#f0f0f0' : '#303030'
-            }`,
-          },
-          '& .MuiDataGrid-columnsContainer, .MuiDataGrid-cell': {
-            borderBottom: `1px solid ${
-              theme.palette.type === 'light' ? '#f0f0f0' : '#303030'
-            }`,
-          },
-          '& .MuiDataGrid-cell': {
-            color:
-              theme.palette.type === 'light'
-                ? 'rgba(0,0,0,.85)'
-                : 'rgba(255,255,255,0.65)',
-          },
-          '& .MuiPaginationItem-root': {
-            borderRadius: 0,
-          },
-          ...customCheckbox(theme),
+              : 'rgba(255,255,255,0.65)',
         },
+        '& .MuiPaginationItem-root': {
+          borderRadius: 0,
+        },
+        ...customCheckbox(theme),
       },
     }),
   { defaultTheme },
