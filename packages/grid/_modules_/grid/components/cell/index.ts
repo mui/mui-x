1f94832fb7374277fe3b67fdef729ef27024ab37
Danail H <hadjiatanasov@gmail.com>
2021-05-24 14:28:55 +0300

    Make new skeleton components public

--- packages/grid/_modules_/grid/components/cell/index.ts
+++ packages/grid/_modules_/grid/components/cell/index.ts
@@ -2,3 +2,5 @@ export * from './GridCell';
 export * from './GridEditInputCell';
 export * from './GridEmptyCell';
 export * from './GridRowCells';
+export * from './GridSkeletonRowCells';
+export * from './GridSkeletonCell';
