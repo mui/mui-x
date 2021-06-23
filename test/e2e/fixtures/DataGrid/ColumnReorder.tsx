3f0be4981eab3d7e637c2c579d4b8146c5de2aef
Matheus Wichman <matheushw@outlook.com>
2021-06-22 23:51:49 -0300

    add e2e test

---
+++ test/e2e/fixtures/DataGrid/ColumnReorder.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { XGrid } from '@material-ui/x-grid';
+
+const baselineProps = {
+  rows: [
+    {
+      id: 0,
+      brand: 'Nike',
+      year: 1990,
+    },
+    {
+      id: 1,
+      brand: 'Adidas',
+      year: 1995,
+    },
+    {
+      id: 2,
+      brand: 'Puma',
+      year: 1993,
+    },
+  ],
+  columns: [
+    { field: 'brand', width: 120 },
+    { field: 'year', width: 120 },
+  ],
+};
+
+export default function ColumnReorder() {
+  return (
+    <React.Fragment>
+      <div style={{ width: 300, height: 300 }}>
+        <XGrid {...baselineProps} />
+      </div>
+    </React.Fragment>
+  );
+}
