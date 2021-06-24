8464bd4f73aab8335c2151e3f810dd519a69da78
Danail Hadjiatanasov <hadjiatanasov@gmail.com>
2021-06-24 15:54:13 +0300

    Update docs/src/pages/components/data-grid/columns/ColumnTypesGrid.js
    Co-authored-by: Matheus Wichman <matheushw@outlook.com>

--- docs/src/pages/components/data-grid/columns/ColumnTypesGrid.js
+++ docs/src/pages/components/data-grid/columns/ColumnTypesGrid.js
@@ -55,7 +55,7 @@ export default function ColumnTypesGrid() {
               'France',
               'United Kingdom',
               'Spain',
-              'Brasil',
+              'Brazil',
             ],
           },
         ]}
