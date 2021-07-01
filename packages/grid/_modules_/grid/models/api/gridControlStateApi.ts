7bbcc2bd38a752aa5d400f1d57c97c439ef3fbe1
damien <damien.tassone@gmail.com>
2021-07-01 10:12:02 +0200

    Apply suggestions from code review
    Co-authored-by: Matheus Wichman <matheushw@outlook.com>

--- packages/grid/_modules_/grid/models/api/gridControlStateApi.ts
+++ packages/grid/_modules_/grid/models/api/gridControlStateApi.ts
@@ -8,12 +8,14 @@ export interface GridControlStateApi {
   /**
    * Updates a control state that binds the model, the onChange prop, and the grid state together.
    * @param {ControlStateItem<TModel>} controlState The [[ControlStateItem]] to be registered.
+   * @ignore - do not document.
    */
   updateControlState: <TModel>(controlState: ControlStateItem<TModel>) => void;
   /**
    * Allows the internal grid state to apply the registered control state constraint.
    * @param {GridState} state The new modified state that would be the next if the state is not controlled.
    * @returns {shouldUpdate: boolean, postUpdate: () => void}, shouldUpdate let the state know if it should update, and postUpdate is a callback function triggered if the state has updated.
+   * @ignore - do not document.
    */
   applyControlStateConstraint: (state: GridState) => {
     shouldUpdate: boolean;
