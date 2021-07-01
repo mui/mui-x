8cde463713edbd6be389443409af8f65321ba0de
delangle <flaviendelangle@gmail.com>
2021-07-01 10:22:18 +0200

    Run prettier

--- packages/grid/x-grid/src/tests/export.XGrid.test.tsx
+++ packages/grid/x-grid/src/tests/export.XGrid.test.tsx
@@ -1,4 +1,4 @@
-import {GridApiRef, GridColumns, useGridApiRef, XGrid} from '@material-ui/x-grid';
+import { GridApiRef, GridColumns, useGridApiRef, XGrid } from '@material-ui/x-grid';
 import { expect } from 'chai';
 import * as React from 'react';
 import { createClientRenderStrictMode } from 'test/utils';
@@ -18,270 +18,268 @@ describe('<XGrid /> - Export', () => {
   const columns: GridColumns = [{ field: 'id' }, { field: 'brand', headerName: 'Brand' }];
 
   describe('getDataAsCsv', () => {
-      it('should work with basic strings', () => {
-          const TestCaseCSVExport = () => {
-              apiRef = useGridApiRef();
-              return (
-                  <div style={{ width: 300, height: 300 }}>
-                      <XGrid
-                          {...baselineProps}
-                          apiRef={apiRef}
-                          columns={columns}
-                          rows={[
-                              {
-                                  id: 0,
-                                  brand: 'Nike',
-                              },
-                              {
-                                  id: 1,
-                                  brand: 'Adidas',
-                              },
-                              {
-                                  id: 2,
-                                  brand: 'Puma',
-                              },
-                          ]}
-                      />
-                  </div>
-              );
-          };
-
-          render(<TestCaseCSVExport />);
-          expect(apiRef.current.getDataAsCsv()).to.equal(
-              ['id,Brand', '0,Nike', '1,Adidas', '2,Puma'].join('\r\n'),
-          );
-          apiRef.current.updateRows([
-              {
+    it('should work with basic strings', () => {
+      const TestCaseCSVExport = () => {
+        apiRef = useGridApiRef();
+        return (
+          <div style={{ width: 300, height: 300 }}>
+            <XGrid
+              {...baselineProps}
+              apiRef={apiRef}
+              columns={columns}
+              rows={[
+                {
+                  id: 0,
+                  brand: 'Nike',
+                },
+                {
                   id: 1,
-                  brand: 'Adidas,Reebok',
-              },
-          ]);
-          expect(apiRef.current.getDataAsCsv()).to.equal(
-              ['id,Brand', '0,Nike', '1,"Adidas,Reebok"', '2,Puma'].join('\r\n'),
-          );
-      });
+                  brand: 'Adidas',
+                },
+                {
+                  id: 2,
+                  brand: 'Puma',
+                },
+              ]}
+            />
+          </div>
+        );
+      };
 
-      it('should work with comma', () => {
-          const TestCaseCSVExport = () => {
-              apiRef = useGridApiRef();
-              return (
-                  <div style={{ width: 300, height: 300 }}>
-                      <XGrid
-                          {...baselineProps}
-                          apiRef={apiRef}
-                          columns={columns}
-                          rows={[
-                              {
-                                  id: 0,
-                                  brand: 'Nike',
-                              },
-                              {
-                                  id: 1,
-                                  brand: 'Adidas,Puma',
-                              },
-                          ]}
-                      />
-                  </div>
-              );
-          };
+      render(<TestCaseCSVExport />);
+      expect(apiRef.current.getDataAsCsv()).to.equal(
+        ['id,Brand', '0,Nike', '1,Adidas', '2,Puma'].join('\r\n'),
+      );
+      apiRef.current.updateRows([
+        {
+          id: 1,
+          brand: 'Adidas,Reebok',
+        },
+      ]);
+      expect(apiRef.current.getDataAsCsv()).to.equal(
+        ['id,Brand', '0,Nike', '1,"Adidas,Reebok"', '2,Puma'].join('\r\n'),
+      );
+    });
 
-          render(<TestCaseCSVExport />);
-          expect(apiRef.current.getDataAsCsv()).to.equal(
-              ['id,Brand', '0,Nike', '1,"Adidas,Puma"'].join('\r\n'),
-          );
-      });
+    it('should work with comma', () => {
+      const TestCaseCSVExport = () => {
+        apiRef = useGridApiRef();
+        return (
+          <div style={{ width: 300, height: 300 }}>
+            <XGrid
+              {...baselineProps}
+              apiRef={apiRef}
+              columns={columns}
+              rows={[
+                {
+                  id: 0,
+                  brand: 'Nike',
+                },
+                {
+                  id: 1,
+                  brand: 'Adidas,Puma',
+                },
+              ]}
+            />
+          </div>
+        );
+      };
 
-      it('should apply valueFormatter correctly', () => {
-          const TestCaseCSVExport = () => {
-              apiRef = useGridApiRef();
-              return (
-                  <div style={{ width: 300, height: 300 }}>
-                      <XGrid
-                          {...baselineProps}
-                          apiRef={apiRef}
-                          columns={[
-                              { field: 'id' },
-                              {
-                                  field: 'brand',
-                                  headerName: 'Brand',
-                                  valueFormatter: (params) => (params.value === 'Nike' ? 'Jordan' : params.value),
-                              },
-                          ]}
-                          rows={[
-                              {
-                                  id: 0,
-                                  brand: 'Nike',
-                              },
-                              {
-                                  id: 1,
-                                  brand: 'Adidas',
-                              },
-                          ]}
-                      />
-                  </div>
-              );
-          };
+      render(<TestCaseCSVExport />);
+      expect(apiRef.current.getDataAsCsv()).to.equal(
+        ['id,Brand', '0,Nike', '1,"Adidas,Puma"'].join('\r\n'),
+      );
+    });
 
-          render(<TestCaseCSVExport />);
-          expect(apiRef.current.getDataAsCsv()).to.equal(
-              ['id,Brand', '0,Jordan', '1,Adidas'].join('\r\n'),
-          );
-      });
+    it('should apply valueFormatter correctly', () => {
+      const TestCaseCSVExport = () => {
+        apiRef = useGridApiRef();
+        return (
+          <div style={{ width: 300, height: 300 }}>
+            <XGrid
+              {...baselineProps}
+              apiRef={apiRef}
+              columns={[
+                { field: 'id' },
+                {
+                  field: 'brand',
+                  headerName: 'Brand',
+                  valueFormatter: (params) => (params.value === 'Nike' ? 'Jordan' : params.value),
+                },
+              ]}
+              rows={[
+                {
+                  id: 0,
+                  brand: 'Nike',
+                },
+                {
+                  id: 1,
+                  brand: 'Adidas',
+                },
+              ]}
+            />
+          </div>
+        );
+      };
 
-      it('should work with double quotes', () => {
-          const TestCaseCSVExport = () => {
-              apiRef = useGridApiRef();
-              return (
-                  <div style={{ width: 300, height: 300 }}>
-                      <XGrid
-                          {...baselineProps}
-                          apiRef={apiRef}
-                          columns={columns}
-                          rows={[
-                              {
-                                  id: 0,
-                                  brand: 'Nike',
-                              },
-                              {
-                                  id: 1,
-                                  brand: 'Samsung 24" (inches)',
-                              },
-                          ]}
-                      />
-                  </div>
-              );
-          };
+      render(<TestCaseCSVExport />);
+      expect(apiRef.current.getDataAsCsv()).to.equal(
+        ['id,Brand', '0,Jordan', '1,Adidas'].join('\r\n'),
+      );
+    });
 
-          render(<TestCaseCSVExport />);
-          expect(apiRef.current.getDataAsCsv()).to.equal(
-              ['id,Brand', '0,Nike', '1,Samsung 24"" (inches)'].join('\r\n'),
-          );
-      });
+    it('should work with double quotes', () => {
+      const TestCaseCSVExport = () => {
+        apiRef = useGridApiRef();
+        return (
+          <div style={{ width: 300, height: 300 }}>
+            <XGrid
+              {...baselineProps}
+              apiRef={apiRef}
+              columns={columns}
+              rows={[
+                {
+                  id: 0,
+                  brand: 'Nike',
+                },
+                {
+                  id: 1,
+                  brand: 'Samsung 24" (inches)',
+                },
+              ]}
+            />
+          </div>
+        );
+      };
 
-      it('should allow to change the delimiter', () => {
-          const TestCaseCSVExport = () => {
-              apiRef = useGridApiRef();
-              return (
-                  <div style={{ width: 300, height: 300 }}>
-                      <XGrid
-                          {...baselineProps}
-                          apiRef={apiRef}
-                          columns={columns}
-                          rows={[
-                              {
-                                  id: 0,
-                                  brand: 'Nike',
-                              },
-                              {
-                                  id: 1,
-                                  brand: 'Adidas',
-                              },
-                          ]}
-                      />
-                  </div>
-              );
-          };
+      render(<TestCaseCSVExport />);
+      expect(apiRef.current.getDataAsCsv()).to.equal(
+        ['id,Brand', '0,Nike', '1,Samsung 24"" (inches)'].join('\r\n'),
+      );
+    });
 
-          render(<TestCaseCSVExport />);
-          expect(
-              apiRef.current.getDataAsCsv({
-                  delimiter: ';',
-              }),
-          ).to.equal(['id;Brand', '0;Nike', '1;Adidas'].join('\r\n'));
-      });
+    it('should allow to change the delimiter', () => {
+      const TestCaseCSVExport = () => {
+        apiRef = useGridApiRef();
+        return (
+          <div style={{ width: 300, height: 300 }}>
+            <XGrid
+              {...baselineProps}
+              apiRef={apiRef}
+              columns={columns}
+              rows={[
+                {
+                  id: 0,
+                  brand: 'Nike',
+                },
+                {
+                  id: 1,
+                  brand: 'Adidas',
+                },
+              ]}
+            />
+          </div>
+        );
+      };
 
-      it('should not export hidden column', () => {
-          const TestCaseCSVExport = () => {
-              apiRef = useGridApiRef();
-              return (
-                  <div style={{ width: 300, height: 300 }}>
-                      <XGrid
-                          {...baselineProps}
-                          apiRef={apiRef}
-                          columns={[{ field: 'id' }, { field: 'brand', headerName: 'Brand', hide: true }]}
-                          rows={[
-                              {
-                                  id: 0,
-                                  brand: 'Nike',
-                              },
-                              {
-                                  id: 1,
-                                  brand: 'Adidas',
-                              },
-                          ]}
-                      />
-                  </div>
-              );
-          };
+      render(<TestCaseCSVExport />);
+      expect(
+        apiRef.current.getDataAsCsv({
+          delimiter: ';',
+        }),
+      ).to.equal(['id;Brand', '0;Nike', '1;Adidas'].join('\r\n'));
+    });
 
-          render(<TestCaseCSVExport />);
-          expect(apiRef.current.getDataAsCsv()).to.equal(
-              ['id', '0', '1'].join('\r\n'),
-          );
-      })
+    it('should not export hidden column', () => {
+      const TestCaseCSVExport = () => {
+        apiRef = useGridApiRef();
+        return (
+          <div style={{ width: 300, height: 300 }}>
+            <XGrid
+              {...baselineProps}
+              apiRef={apiRef}
+              columns={[{ field: 'id' }, { field: 'brand', headerName: 'Brand', hide: true }]}
+              rows={[
+                {
+                  id: 0,
+                  brand: 'Nike',
+                },
+                {
+                  id: 1,
+                  brand: 'Adidas',
+                },
+              ]}
+            />
+          </div>
+        );
+      };
 
-      it('should export hidden column if params.allColumns = true', () => {
-          const TestCaseCSVExport = () => {
-              apiRef = useGridApiRef();
-              return (
-                  <div style={{ width: 300, height: 300 }}>
-                      <XGrid
-                          {...baselineProps}
-                          apiRef={apiRef}
-                          columns={[{ field: 'id' }, { field: 'brand', headerName: 'Brand', hide: true }]}
-                          rows={[
-                              {
-                                  id: 0,
-                                  brand: 'Nike',
-                              },
-                              {
-                                  id: 1,
-                                  brand: 'Adidas',
-                              },
-                          ]}
-                      />
-                  </div>
-              );
-          };
+      render(<TestCaseCSVExport />);
+      expect(apiRef.current.getDataAsCsv()).to.equal(['id', '0', '1'].join('\r\n'));
+    });
 
-          render(<TestCaseCSVExport />);
-          expect(
-              apiRef.current.getDataAsCsv({
-                  allColumns: true,
-              }),
-          ).to.equal(['id,Brand', '0,Nike', '1,Adidas'].join('\r\n'));
-      })
+    it('should export hidden column if params.allColumns = true', () => {
+      const TestCaseCSVExport = () => {
+        apiRef = useGridApiRef();
+        return (
+          <div style={{ width: 300, height: 300 }}>
+            <XGrid
+              {...baselineProps}
+              apiRef={apiRef}
+              columns={[{ field: 'id' }, { field: 'brand', headerName: 'Brand', hide: true }]}
+              rows={[
+                {
+                  id: 0,
+                  brand: 'Nike',
+                },
+                {
+                  id: 1,
+                  brand: 'Adidas',
+                },
+              ]}
+            />
+          </div>
+        );
+      };
 
-      it('should only export columns in params.columnKeys if defined', () => {
-          const TestCaseCSVExport = () => {
-              apiRef = useGridApiRef();
-              return (
-                  <div style={{ width: 300, height: 300 }}>
-                      <XGrid
-                          {...baselineProps}
-                          apiRef={apiRef}
-                          columns={[{ field: 'id' }, { field: 'brand', headerName: 'Brand', hide: true }]}
-                          rows={[
-                              {
-                                  id: 0,
-                                  brand: 'Nike',
-                              },
-                              {
-                                  id: 1,
-                                  brand: 'Adidas',
-                              },
-                          ]}
-                      />
-                  </div>
-              );
-          };
+      render(<TestCaseCSVExport />);
+      expect(
+        apiRef.current.getDataAsCsv({
+          allColumns: true,
+        }),
+      ).to.equal(['id,Brand', '0,Nike', '1,Adidas'].join('\r\n'));
+    });
 
-          render(<TestCaseCSVExport />);
-          expect(
-              apiRef.current.getDataAsCsv({
-                  columnKeys: ['brand'],
-              }),
-          ).to.equal(['Brand', 'Nike', 'Adidas'].join('\r\n'));
-      })
-  })
+    it('should only export columns in params.columnKeys if defined', () => {
+      const TestCaseCSVExport = () => {
+        apiRef = useGridApiRef();
+        return (
+          <div style={{ width: 300, height: 300 }}>
+            <XGrid
+              {...baselineProps}
+              apiRef={apiRef}
+              columns={[{ field: 'id' }, { field: 'brand', headerName: 'Brand', hide: true }]}
+              rows={[
+                {
+                  id: 0,
+                  brand: 'Nike',
+                },
+                {
+                  id: 1,
+                  brand: 'Adidas',
+                },
+              ]}
+            />
+          </div>
+        );
+      };
+
+      render(<TestCaseCSVExport />);
+      expect(
+        apiRef.current.getDataAsCsv({
+          columnKeys: ['brand'],
+        }),
+      ).to.equal(['Brand', 'Nike', 'Adidas'].join('\r\n'));
+    });
+  });
 });
