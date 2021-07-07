ef14e9ee8fde442576ba32279ad7a625dc99c4d2
Matheus Wichman <matheushw@outlook.com>
2021-07-07 09:06:15 -0300

    [DataGrid] Fix manual entry in date fields (#2051)

---
+++ packages/grid/_modules_/grid/components/cell/GridEditDateCell.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import InputBase, { InputBaseProps } from '@material-ui/core/InputBase';
+import { GridCellParams } from '../../models/params/gridCellParams';
+
+export function GridEditDateCell(props: GridCellParams & InputBaseProps) {
+  const {
+    id,
+    value,
+    formattedValue,
+    api,
+    field,
+    row,
+    colDef,
+    cellMode,
+    isEditable,
+    hasFocus,
+    getValue,
+    ...other
+  } = props;
+
+  const [valueState, setValueState] = React.useState(value);
+
+  const handleChange = React.useCallback(
+    (event) => {
+      const newValue = event.target.value;
+      setValueState(newValue);
+
+      if (newValue === '') {
+        const editProps = { value: null };
+        api.setEditCellProps({ id, field, props: editProps }, event);
+        return;
+      }
+
+      const [date, time] = newValue.split('T');
+      const [year, month, day] = date.split('-');
+      const dateObj = new Date();
+      dateObj.setFullYear(Number(year));
+      dateObj.setMonth(Number(month) - 1);
+      dateObj.setDate(Number(day));
+      dateObj.setHours(0, 0, 0, 0);
+
+      if (time) {
+        const [hours, minutes] = time.split(':');
+        dateObj.setHours(Number(hours), Number(minutes), 0, 0);
+      }
+
+      const editProps = { value: dateObj };
+      api.setEditCellProps({ id, field, props: editProps }, event);
+    },
+    [api, field, id],
+  );
+
+  const isDateTime = colDef.type === 'dateTime';
+
+  React.useEffect(() => {
+    setValueState(value);
+  }, [value]);
+
+  let valueToDisplay = valueState || '';
+  if (valueState instanceof Date) {
+    const offset = valueState.getTimezoneOffset();
+    const localDate = new Date(valueState.getTime() - offset * 60 * 1000);
+    valueToDisplay = localDate.toISOString().substr(0, isDateTime ? 16 : 10);
+  }
+
+  return (
+    <InputBase
+      autoFocus
+      fullWidth
+      className="MuiDataGrid-editInputCell"
+      type={isDateTime ? 'datetime-local' : 'date'}
+      value={valueToDisplay}
+      onChange={handleChange}
+      {...other}
+    />
+  );
+}
+export const renderEditDateCell = (params) => <GridEditDateCell {...params} />;
--- packages/grid/_modules_/grid/models/colDef/gridDateColDef.ts
+++ packages/grid/_modules_/grid/models/colDef/gridDateColDef.ts
@@ -1,19 +1,19 @@
-import { isDate } from '../../utils/utils';
 import { gridDateComparer } from '../../utils/sortingUtils';
 import { GridCellValue } from '../gridCell';
 import { getGridDateOperators } from './gridDateOperators';
 import { GRID_STRING_COL_DEF } from './gridStringColDef';
 import { GridColTypeDef } from './gridColDef';
+import { renderEditDateCell } from '../../components/cell/GridEditDateCell';
 
 export function gridDateFormatter({ value }: { value: GridCellValue }) {
-  if (isDate(value)) {
+  if (value instanceof Date) {
     return value.toLocaleDateString();
   }
   return value;
 }
 
 export function gridDateTimeFormatter({ value }: { value: GridCellValue }) {
-  if (isDate(value)) {
+  if (value instanceof Date) {
     return value.toLocaleString();
   }
   return value;
@@ -25,6 +25,7 @@ export const GRID_DATE_COL_DEF: GridColTypeDef = {
   sortComparator: gridDateComparer,
   valueFormatter: gridDateFormatter,
   filterOperators: getGridDateOperators(),
+  renderEditCell: renderEditDateCell,
 };
 
 export const GRID_DATETIME_COL_DEF: GridColTypeDef = {
@@ -33,4 +34,5 @@ export const GRID_DATETIME_COL_DEF: GridColTypeDef = {
   sortComparator: gridDateComparer,
   valueFormatter: gridDateTimeFormatter,
   filterOperators: getGridDateOperators(true),
+  renderEditCell: renderEditDateCell,
 };
