55972c6631773a70b04a1cdcf21ac758955b04ee
Flavien DELANGLE <flaviendelangle@gmail.com>
2021-07-07 11:41:29 +0200

    [XGrid] Close column header menu when resizing column (#1989)
    * [XGrid] Close column header menu when resizing column
    
    * Run prettier
    
    * Fix test
    
    * Remove dummy line
    
    * Run Prettier
    
    * Fix browser tests
    
    * Run prettier
    
    * Fix
    
    * Fix comments
    
    * Remove it.only
    
    * Remove useless expect
    
    * Remove 'raf'
    
    * Clean listeners when unmounting the grid in useGridColumnResize
    
    * Remove commented test lines
    
    * Code review
    
    * Add test to check behavior when resizing another column
    
    * Remove 'only'
    
    * Move test in describe
    
    * Remove async
    
    * Code review
    
    * Code review

--- packages/grid/_modules_/grid/components/columnHeaders/GridColumnHeaderSeparator.tsx
+++ packages/grid/_modules_/grid/components/columnHeaders/GridColumnHeaderSeparator.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import { useGridSelector } from '../../hooks/features/core/useGridSelector';
 import { optionsSelector } from '../../hooks/utils/optionsSelector';
 import { useGridApiContext } from '../../hooks/root/useGridApiContext';
+import { GRID_COLUMN_HEADER_SEPARATOR_RESIZABLE_CSS_CLASS } from '../../constants';
 
 export interface GridColumnHeaderSeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
   resizable: boolean;
@@ -27,7 +28,7 @@ export const GridColumnHeaderSeparator = React.memo(function GridColumnHeaderSep
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
     <div
       className={clsx('MuiDataGrid-columnSeparator', {
-        'MuiDataGrid-columnSeparator--resizable': resizable,
+        [GRID_COLUMN_HEADER_SEPARATOR_RESIZABLE_CSS_CLASS]: resizable,
         'Mui-resizing': resizing,
       })}
       style={{ minHeight: height, opacity: showColumnRightBorder ? 0 : 1 }}
