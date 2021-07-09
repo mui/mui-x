9859b6a4f37316274e62ef2101f565914b5fe068
delangle <flaviendelangle@gmail.com>
2021-07-09 12:09:26 +0200

    Clean pagination tests

--- packages/grid/data-grid/src/tests/pagination.DataGrid.test.tsx
+++ packages/grid/data-grid/src/tests/pagination.DataGrid.test.tsx
@@ -21,65 +21,55 @@ import { useData } from 'packages/storybook/src/hooks/useData';
 
 const isJSDOM = /jsdom/.test(window.navigator.userAgent);
 
-describe('<DataGrid /> - Pagination', () => {
+describe.only('<DataGrid /> - Pagination', () => {
   // TODO v5: replace with createClientRender
   const render = createClientRenderStrictMode();
 
-  const baselineProps = {
-    rows: [
-      {
-        id: 0,
-        brand: 'Nike',
-      },
-      {
-        id: 1,
-        brand: 'Adidas',
-      },
-      {
-        id: 2,
-        brand: 'Puma',
-      },
-    ],
-    columns: [{ field: 'brand' }],
-  };
+  const BaselineTestCase = (props: Omit<DataGridProps, 'rows' | 'columns'>) => (
+    <div style={{ width: 300, height: 300 }}>
+      <DataGrid
+        rows={[
+          {
+            id: 0,
+            brand: 'Element 0',
+          },
+          {
+            id: 1,
+            brand: 'Element 1',
+          },
+          {
+            id: 2,
+            brand: 'Element 2',
+          },
+        ]}
+        columns={[{ field: 'brand' }]}
+        pagination
+        {...props}
+      />
+    </div>
+  );
 
-  describe('pagination', () => {
-    it('should apply the page prop correctly', () => {
-      render(
-        <div style={{ width: 300, height: 300 }}>
-          <DataGrid {...baselineProps} page={1} pageSize={1} />
-        </div>,
-      );
-      expect(getColumnValues()).to.deep.equal(['Adidas']);
+  describe('prop: page and onPageChange', () => {
+    it('should display the rows of page given in props', () => {
+      render(<BaselineTestCase page={1} pageSize={1} />);
+      expect(getColumnValues()).to.deep.equal(['Element 1']);
     });
 
-    it('should react to an update of rowCount', () => {
-      const Test = (props) => {
-        return (
-          <div style={{ width: 300, height: 300 }}>
-            <DataGrid {...baselineProps} rowCount={5} {...props} />
-          </div>
-        );
-      };
-
-      const { setProps } = render(<Test />);
-      expect(document.querySelector('.MuiTablePagination-root')).to.have.text(
-        'Rows per page:1001-5 of 5',
-      );
-      setProps({ rowCount: 21 });
-      expect(document.querySelector('.MuiTablePagination-root')).to.have.text(
-        'Rows per page:1001-21 of 21',
-      );
-    });
-
-    it('should trigger onPageChange when clicking on next page', () => {
+    it('should not call onPageChange on initialisation or on page prop change', () => {
       const onPageChange = spy();
 
-      render(
-        <div style={{ width: 300, height: 300 }}>
-          <DataGrid {...baselineProps} onPageChange={onPageChange} pageSize={1} />
-        </div>,
+      const { setProps } = render(
+        <BaselineTestCase page={1} pageSize={1} onPageChange={onPageChange} />,
       );
+      expect(onPageChange.callCount).to.equal(0);
+      setProps({ page: 2 });
+      expect(onPageChange.callCount).to.equal(0);
+    });
+
+    it('should call onPageChange when clicking on next / previous button', () => {
+      const onPageChange = spy();
+
+      render(<BaselineTestCase onPageChange={onPageChange} pageSize={1} />);
       fireEvent.click(screen.getByRole('button', { name: /next page/i }));
       expect(onPageChange.callCount).to.equal(1);
 
@@ -87,14 +77,10 @@ describe('<DataGrid /> - Pagination', () => {
       expect(onPageChange.callCount).to.equal(2);
     });
 
-    it('should trigger onPageChange with correct page param when page prop is supplied i.e in controlled mode', () => {
+    it('should call onPageChange with correct page when clicking on next / previous button when page is controlled', () => {
       const onPageChange = spy();
 
-      render(
-        <div style={{ width: 300, height: 300 }}>
-          <DataGrid {...baselineProps} page={1} onPageChange={onPageChange} pageSize={1} />
-        </div>,
-      );
+      render(<BaselineTestCase page={1} onPageChange={onPageChange} pageSize={1} />);
 
       fireEvent.click(screen.getByRole('button', { name: /next page/i }));
       expect(onPageChange.lastCall.args[0]).to.equal(2);
@@ -102,59 +88,192 @@ describe('<DataGrid /> - Pagination', () => {
       expect(onPageChange.lastCall.args[0]).to.equal(0);
     });
 
-    it('should trigger onPageChange when clicking on next page in Server mode', () => {
+    it('should call onPageChange when clicking on next / previous button in "server" mode', () => {
       const onPageChange = spy();
 
-      render(
-        <div style={{ width: 300, height: 300 }}>
-          <DataGrid
-            {...baselineProps}
-            onPageChange={onPageChange}
-            pageSize={1}
-            paginationMode="server"
-          />
-        </div>,
-      );
+      render(<BaselineTestCase onPageChange={onPageChange} pageSize={1} paginationMode="server" />);
       fireEvent.click(screen.getByRole('button', { name: /next page/i }));
       expect(onPageChange.callCount).to.equal(1);
       fireEvent.click(screen.getByRole('button', { name: /previous page/i }));
       expect(onPageChange.callCount).to.equal(2);
     });
 
-    it('should not trigger onPageChange on initialisation or on page prop change', () => {
-      const onPageChange = spy();
+    it('should not change the page when clicking on next button and a page prop is provided', () => {
+      render(<BaselineTestCase page={0} pageSize={1} />);
+      expect(getColumnValues()).to.deep.equal(['Element 0']);
+      fireEvent.click(screen.getByRole('button', { name: /next page/i }));
+      expect(getColumnValues()).to.deep.equal(['Element 0']);
+    });
 
-      function Test(props: Partial<DataGridProps>) {
+    it('should control page state when the prop and the onChange are set', () => {
+      const ControlCase = () => {
+        const [page, setPage] = React.useState(0);
+
         return (
-          <div style={{ width: 300, height: 300 }}>
-            <DataGrid {...baselineProps} {...props} />
-          </div>
+          <BaselineTestCase page={page} onPageChange={(newPage) => setPage(newPage)} pageSize={1} />
         );
-      }
+      };
 
-      const { setProps } = render(<Test page={1} pageSize={1} onPageChange={onPageChange} />);
-      expect(onPageChange.callCount).to.equal(0);
-      setProps({ page: 2 });
-      expect(onPageChange.callCount).to.equal(0);
+      render(<ControlCase />);
+
+      fireEvent.click(screen.getByRole('button', { name: /next page/i }));
+      expect(getColumnValues()).to.deep.equal(['Element 1']);
+
+      fireEvent.click(screen.getByRole('button', { name: /previous page/i }));
+      expect(getColumnValues()).to.deep.equal(['Element 0']);
+    });
+  });
+
+  describe('prop: pageSize and onPageSizeChange', () => {
+    it('should display the amount of rows given in props', () => {
+      render(<BaselineTestCase page={0} pageSize={2} />);
+      expect(getColumnValues()).to.deep.equal(['Element 0', 'Element 1']);
     });
 
     it('should not trigger onPageSizeChange on initialisation or on pageSize prop change', () => {
       const onPageSizeChange = spy();
 
-      function Test(props: Partial<DataGridProps>) {
-        return (
-          <div style={{ width: 300, height: 300 }}>
-            <DataGrid {...baselineProps} {...props} />
-          </div>
-        );
-      }
-
       const { setProps } = render(
-        <Test onPageSizeChange={onPageSizeChange} pageSize={1} page={1} />,
+        <BaselineTestCase onPageSizeChange={onPageSizeChange} pageSize={1} page={1} />,
       );
       expect(onPageSizeChange.callCount).to.equal(0);
       setProps({ pageSize: 2 });
       expect(onPageSizeChange.callCount).to.equal(0);
+    });
+  });
+
+  describe('prop: autoPageSize', () => {
+    before(function beforeHook() {
+      if (isJSDOM) {
+        // Need layouting
+        this.skip();
+      }
+    });
+
+    const TestCaseAutoPageSize = (
+      props: Omit<DataGridProps, 'rows' | 'columns'> & { height: number; nbRows: number },
+    ) => {
+      const { height, nbRows, ...other } = props;
+
+      const data = useData(nbRows, 10);
+
+      return (
+        <div style={{ width: 300, height: props.height }}>
+          <DataGrid columns={data.columns} rows={data.rows} autoPageSize pagination {...other} />
+        </div>
+      );
+    };
+
+    it('should always render the same amount of rows and fit the viewport', () => {
+      const nbRows = 27;
+      const height = 780;
+      render(<TestCaseAutoPageSize nbRows={nbRows} height={height} />);
+
+      const footerHeight = document.querySelector('.MuiDataGrid-footerContainer')!.clientHeight;
+      const expectedFullPageRowsLength = Math.floor(
+        (height - DEFAULT_GRID_OPTIONS.headerHeight - footerHeight) /
+          DEFAULT_GRID_OPTIONS.rowHeight,
+      );
+
+      let rows = getRows();
+      expect(rows.length).to.equal(Math.min(expectedFullPageRowsLength, nbRows));
+
+      fireEvent.click(screen.getByRole('button', { name: /next page/i }));
+      rows = getRows();
+      expect(rows.length).to.equal(
+        Math.min(expectedFullPageRowsLength, nbRows - expectedFullPageRowsLength),
+      );
+
+      fireEvent.click(screen.getByRole('button', { name: /previous page/i }));
+      rows = getRows();
+      expect(rows.length).to.equal(Math.min(expectedFullPageRowsLength, nbRows));
+
+      fireEvent.click(screen.getByRole('button', { name: /next page/i }));
+      fireEvent.click(screen.getByRole('button', { name: /next page/i }));
+      rows = getRows();
+
+      expect(rows.length).to.equal(
+        Math.min(expectedFullPageRowsLength, nbRows - 2 * expectedFullPageRowsLength),
+      );
+
+      // make sure there is no more pages.
+      const nextPageBtn = document.querySelector('.MuiTablePagination-actions button:last-child');
+      expect(nextPageBtn!.getAttribute('disabled')).to.not.equal(
+        null,
+        'next page should be disabled.',
+      );
+    });
+
+    it('should be compatible with controlled page', () => {
+      const rows = [
+        { id: 1, x: 1 },
+        { id: 2, x: 2 },
+        { id: 3, x: 3 },
+        { id: 4, x: 4 },
+        { id: 5, x: 5 },
+        { id: 6, x: 6 },
+        { id: 7, x: 7 },
+        { id: 8, x: 8 },
+      ];
+      const columns = [{ field: 'x', type: 'number' }];
+      render(
+        <div style={{ height: 300, width: 400 }}>
+          <DataGrid pagination autoPageSize rows={rows} columns={columns} page={2} />
+        </div>,
+      );
+      expect(getColumnValues(0)).to.deep.equal(['7', '8']);
+    });
+
+    it('should update the amount of rows rendered and call onPageSizeChange when changing the table height', async () => {
+      const onPageSizeChange = spy();
+
+      const nbRows = 27;
+
+      const heightBefore = 780;
+      const heightAfter = 360;
+
+      const { setProps } = render(
+        <TestCaseAutoPageSize
+          nbRows={nbRows}
+          height={heightBefore}
+          onPageSizeChange={onPageSizeChange}
+        />,
+      );
+
+      const footerHeight = document.querySelector('.MuiDataGrid-footerContainer')!.clientHeight;
+      const expectedViewportRowsLengthBefore = Math.floor(
+        (heightBefore - DEFAULT_GRID_OPTIONS.headerHeight - footerHeight) /
+          DEFAULT_GRID_OPTIONS.rowHeight,
+      );
+      const expectedViewportRowsLengthAfter = Math.floor(
+        (heightAfter - DEFAULT_GRID_OPTIONS.headerHeight - footerHeight) /
+          DEFAULT_GRID_OPTIONS.rowHeight,
+      );
+
+      let rows = document.querySelectorAll('.MuiDataGrid-viewport [role="row"]');
+      expect(rows.length).to.equal(expectedViewportRowsLengthBefore);
+
+      setProps({ height: heightAfter });
+
+      await waitFor(() =>
+        expect(document.querySelector('.MuiTablePagination-caption')!.innerHTML).to.equal(
+          `1-${expectedViewportRowsLengthAfter} of ${nbRows}`,
+        ),
+      );
+
+      rows = document.querySelectorAll('.MuiDataGrid-viewport [role="row"]');
+      expect(rows.length).to.equal(expectedViewportRowsLengthAfter);
+
+      expect(onPageSizeChange.lastCall.args[0]).to.equal(expectedViewportRowsLengthAfter);
+    });
+  });
+
+  describe('other props', () => {
+    it('should react to an update of rowCount', () => {
+      const { setProps } = render(<BaselineTestCase rowCount={5} pageSize={1} page={0} />);
+      expect(document.querySelector('.MuiTablePagination-root')).to.have.text('1-1 of 5');
+      setProps({ rowCount: 21 });
+      expect(document.querySelector('.MuiTablePagination-root')).to.have.text('1-1 of 21');
     });
 
     it('should support server side pagination', () => {
@@ -173,7 +292,6 @@ describe('<DataGrid /> - Pagination', () => {
             const newRows = [
               {
                 id: page,
-                brand: `Nike ${page}`,
               },
             ];
 
@@ -192,7 +310,7 @@ describe('<DataGrid /> - Pagination', () => {
         return (
           <div style={{ height: 300, width: 300 }}>
             <DataGrid
-              {...baselineProps}
+              columns={[{ field: 'id' }]}
               rows={rows}
               pagination
               pageSize={1}
@@ -205,20 +323,9 @@ describe('<DataGrid /> - Pagination', () => {
       };
 
       render(<ServerPaginationGrid />);
-      expect(getColumnValues()).to.deep.equal(['Nike 0']);
+      expect(getColumnValues()).to.deep.equal(['0']);
       fireEvent.click(screen.getByRole('button', { name: /next page/i }));
-      expect(getColumnValues()).to.deep.equal(['Nike 1']);
-    });
-
-    it('should not change the page when clicking on the next page and a page prop is provided', () => {
-      render(
-        <div style={{ width: 300, height: 300 }}>
-          <DataGrid {...baselineProps} page={0} pageSize={1} />
-        </div>,
-      );
-      expect(getColumnValues()).to.deep.equal(['Nike']);
-      fireEvent.click(screen.getByRole('button', { name: /next page/i }));
-      expect(getColumnValues()).to.deep.equal(['Nike']);
+      expect(getColumnValues()).to.deep.equal(['1']);
     });
 
     it('should show filtered data if the user applies filter on an intermediate page and the resulted filter data is less than the rows per page', () => {
@@ -251,136 +358,6 @@ describe('<DataGrid /> - Pagination', () => {
       };
       render(<TestCasePaginationFilteredData />);
       expect(getColumnValues(1)).to.deep.equal(['BTCUSD', 'BTCUSD', 'BTCUSD', 'BTCUSD']);
-    });
-
-    describe('prop: autoPageSize', () => {
-      before(function beforeHook() {
-        if (isJSDOM) {
-          // Need layouting
-          this.skip();
-        }
-      });
-
-      it('should always render the same amount of rows and fit the viewport', () => {
-        const TestCaseAutoPageSize = (props: { nbRows: number; height?: number }) => {
-          const data = useData(props.nbRows, 10);
-
-          return (
-            <div style={{ width: 300, height: props.height }}>
-              <DataGrid columns={data.columns} rows={data.rows} autoPageSize pagination />
-            </div>
-          );
-        };
-        const nbRows = 27;
-        const height = 780;
-        render(<TestCaseAutoPageSize nbRows={nbRows} height={height} />);
-
-        const footerHeight = document.querySelector('.MuiDataGrid-footerContainer')!.clientHeight;
-        const expectedFullPageRowsLength = Math.floor(
-          (height - DEFAULT_GRID_OPTIONS.headerHeight - footerHeight) /
-            DEFAULT_GRID_OPTIONS.rowHeight,
-        );
-
-        let rows = getRows();
-        expect(rows.length).to.equal(Math.min(expectedFullPageRowsLength, nbRows));
-
-        fireEvent.click(screen.getByRole('button', { name: /next page/i }));
-        rows = getRows();
-        expect(rows.length).to.equal(
-          Math.min(expectedFullPageRowsLength, nbRows - expectedFullPageRowsLength),
-        );
-
-        fireEvent.click(screen.getByRole('button', { name: /previous page/i }));
-        rows = getRows();
-        expect(rows.length).to.equal(Math.min(expectedFullPageRowsLength, nbRows));
-
-        fireEvent.click(screen.getByRole('button', { name: /next page/i }));
-        fireEvent.click(screen.getByRole('button', { name: /next page/i }));
-        rows = getRows();
-
-        expect(rows.length).to.equal(
-          Math.min(expectedFullPageRowsLength, nbRows - 2 * expectedFullPageRowsLength),
-        );
-
-        // make sure there is no more pages.
-        const nextPageBtn = document.querySelector('.MuiTablePagination-actions button:last-child');
-        expect(nextPageBtn!.getAttribute('disabled')).to.not.equal(
-          null,
-          'next page should be disabled.',
-        );
-      });
-
-      it('should be compatible with controlled page', () => {
-        const rows = [
-          { id: 1, x: 1 },
-          { id: 2, x: 2 },
-          { id: 3, x: 3 },
-          { id: 4, x: 4 },
-          { id: 5, x: 5 },
-          { id: 6, x: 6 },
-          { id: 7, x: 7 },
-          { id: 8, x: 8 },
-        ];
-        const columns = [{ field: 'x', type: 'number' }];
-        render(
-          <div style={{ height: 300, width: 400 }}>
-            <DataGrid pagination autoPageSize rows={rows} columns={columns} page={2} />
-          </div>,
-        );
-        expect(getColumnValues(0)).to.deep.equal(['7', '8']);
-      });
-
-      it('should update the amount of rows rendered and call onPageSizeChange when changing the table height', async () => {
-        const onPageSizeChange = spy();
-
-        const TestCaseAutoPageSize = (props: { nbRows: number; height?: number }) => {
-          const data = useData(props.nbRows, 10);
-
-          return (
-            <div style={{ width: 300, height: props.height }}>
-              <DataGrid
-                columns={data.columns}
-                rows={data.rows}
-                autoPageSize
-                pagination
-                onPageSizeChange={onPageSizeChange}
-              />
-            </div>
-          );
-        };
-
-        const nbRows = 27;
-        const heightBefore = 780;
-        const heightAfter = 360;
-
-        const { setProps } = render(<TestCaseAutoPageSize nbRows={nbRows} height={heightBefore} />);
-
-        const footerHeight = document.querySelector('.MuiDataGrid-footerContainer')!.clientHeight;
-        const expectedViewportRowsLengthBefore = Math.floor(
-          (heightBefore - DEFAULT_GRID_OPTIONS.headerHeight - footerHeight) /
-            DEFAULT_GRID_OPTIONS.rowHeight,
-        );
-        const expectedViewportRowsLengthAfter = Math.floor(
-          (heightAfter - DEFAULT_GRID_OPTIONS.headerHeight - footerHeight) /
-            DEFAULT_GRID_OPTIONS.rowHeight,
-        );
-
-        let rows = document.querySelectorAll('.MuiDataGrid-viewport [role="row"]');
-        expect(rows.length).to.equal(expectedViewportRowsLengthBefore);
-
-        setProps({ height: heightAfter });
-
-        await waitFor(() =>
-          expect(document.querySelector('.MuiTablePagination-caption')!.innerHTML).to.equal(
-            `1-${expectedViewportRowsLengthAfter} of ${nbRows}`,
-          ),
-        );
-
-        rows = document.querySelectorAll('.MuiDataGrid-viewport [role="row"]');
-        expect(rows.length).to.equal(expectedViewportRowsLengthAfter);
-
-        expect(onPageSizeChange.lastCall.args[0]).to.equal(expectedViewportRowsLengthAfter);
-      });
     });
   });
 });
