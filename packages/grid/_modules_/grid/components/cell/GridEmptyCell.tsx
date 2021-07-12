a74a1ddc263767805ce1e2fe4d2e656b69f69087
delangle <flaviendelangle@gmail.com>
2021-07-12 13:07:07 +0200

    Undo padding modification

--- packages/grid/_modules_/grid/components/cell/GridEmptyCell.tsx
+++ packages/grid/_modules_/grid/components/cell/GridEmptyCell.tsx
@@ -22,7 +22,6 @@ export const GridEmptyCell = React.memo(function GridEmptyCell({
         lineHeight: `${height - 1}px`,
         minHeight: height,
         maxHeight: height,
-        padding: 0,
       }}
       className={GRID_CELL_CSS_CLASS}
     />
