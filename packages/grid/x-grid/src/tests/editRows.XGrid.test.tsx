f1538f0cc386c147a5919e540773b1deecff3d35
Matheus Wichman <matheushw@outlook.com>
2021-07-23 16:08:28 -0300

    [DataGrid] Fix support for singleSelect with numeric values (#2112)

--- packages/grid/x-grid/src/tests/editRows.XGrid.test.tsx
+++ packages/grid/x-grid/src/tests/editRows.XGrid.test.tsx
@@ -475,79 +475,81 @@ describe('<XGrid /> - Edit Rows', () => {
     });
   });
 
-  it('should change cell value correctly when column type is singleSelect and valueOptions is array of strings', () => {
-    render(
-      <div style={{ width: 300, height: 300 }}>
-        <XGrid
-          columns={[
-            {
-              field: 'brand',
-              type: 'singleSelect',
-              valueOptions: ['Nike', 'Adidas'],
-              editable: true,
-            },
-          ]}
-          rows={[
-            {
-              id: 0,
-              brand: 'Nike',
-            },
-          ]}
-        />
-      </div>,
-    );
-
-    const cell = getCell(0, 0);
-    fireEvent.doubleClick(cell);
-    fireEvent.click(screen.queryAllByRole('option')[1]);
-
-    expect(cell).not.to.have.class('MuiDataGrid-cell--editing');
-    expect(cell).to.have.text('Adidas');
-  });
-
-  it('should change cell value correctly when column type is singleSelect and valueOptions is array of objects', () => {
-    const countries = [
-      {
-        value: 'fr',
-        label: 'France',
-      },
-      {
-        value: 'it',
-        label: 'Italy',
-      },
-    ];
-
-    render(
-      <div style={{ width: 300, height: 300 }}>
-        <XGrid
-          columns={[
-            {
-              field: 'country',
-              type: 'singleSelect',
-              valueOptions: countries,
-              valueFormatter: (params) => {
-                const result = countries.find((country) => country.value === params.value);
-                return result!.label;
+  describe('column type: singleSelect', () => {
+    it('should change cell value correctly when the valueOptions is array of strings', () => {
+      render(
+        <div style={{ width: 300, height: 300 }}>
+          <XGrid
+            columns={[
+              {
+                field: 'brand',
+                type: 'singleSelect',
+                valueOptions: ['Nike', 'Adidas'],
+                editable: true,
               },
-              editable: true,
-            },
-          ]}
-          rows={[
-            {
-              id: 0,
-              country: 'fr',
-            },
-          ]}
-        />
-      </div>,
-    );
+            ]}
+            rows={[
+              {
+                id: 0,
+                brand: 'Nike',
+              },
+            ]}
+          />
+        </div>,
+      );
 
-    const cell = getCell(0, 0);
-    fireEvent.doubleClick(cell);
-    fireEvent.click(screen.queryAllByRole('option')[1]);
+      const cell = getCell(0, 0);
+      fireEvent.doubleClick(cell);
+      fireEvent.click(screen.queryAllByRole('option')[1]);
 
-    expect(cell).not.to.have.class('MuiDataGrid-cell--editing');
-    expect(cell).to.have.text('Italy');
+      expect(cell).not.to.have.class('MuiDataGrid-cell--editing');
+      expect(cell).to.have.text('Adidas');
+    });
+
+    it('should change cell value correctly when the valueOptions is array of objects', () => {
+      const countries = [
+        {
+          value: 'fr',
+          label: 'France',
+        },
+        {
+          value: 'it',
+          label: 'Italy',
+        },
+      ];
+
+      render(
+        <div style={{ width: 300, height: 300 }}>
+          <XGrid
+            columns={[
+              {
+                field: 'country',
+                type: 'singleSelect',
+                valueOptions: countries,
+                valueFormatter: (params) => {
+                  const result = countries.find((country) => country.value === params.value);
+                  return result!.label;
+                },
+                editable: true,
+              },
+            ]}
+            rows={[
+              {
+                id: 0,
+                country: 'fr',
+              },
+            ]}
+          />
+        </div>,
+      );
+
+      const cell = getCell(0, 0);
+      fireEvent.doubleClick(cell);
+      fireEvent.click(screen.queryAllByRole('option')[1]);
+
+      expect(cell).not.to.have.class('MuiDataGrid-cell--editing');
+      expect(cell).to.have.text('Italy');
+    });
   });
 
   it('should keep the right type', () => {
