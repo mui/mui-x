512caf92e2dd43f0b3a3218a066370212308d91f
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-30 15:32:06 +0700

    fix type boolean renderCell

--- packages/grid/_modules_/grid/models/colDef/gridBooleanColDef.tsx
+++ packages/grid/_modules_/grid/models/colDef/gridBooleanColDef.tsx
@@ -4,6 +4,14 @@ import { renderBooleanCell } from '../../components/cell/GridBooleanCell';
 import { renderEditBooleanCell } from '../../components/cell/GridEditBooleanCell';
 import { gridNumberComparer } from '../../utils/sortingUtils';
 import { getGridBooleanOperators } from './gridBooleanOperators';
+import { GridValueFormatterParams } from '../params/gridCellParams';
+
+function gridBooleanFormatter({ value }: GridValueFormatterParams) {
+  if (typeof value === 'boolean') {
+    return value.toString();
+  }
+  return value;
+}
 
 export const GRID_BOOLEAN_COL_DEF: GridColTypeDef = {
   ...GRID_STRING_COL_DEF,
@@ -14,4 +22,5 @@ export const GRID_BOOLEAN_COL_DEF: GridColTypeDef = {
   renderEditCell: renderEditBooleanCell,
   sortComparator: gridNumberComparer,
   filterOperators: getGridBooleanOperators(),
+  valueFormatter: gridBooleanFormatter,
 };
--- packages/grid/data-grid/src/tests/cells.DataGrid.test.tsx
+++ packages/grid/data-grid/src/tests/cells.DataGrid.test.tsx
@@ -63,4 +63,66 @@ describe('<DataGrid /> - Cells', () => {
     );
     expect(getCell(0, 0)).to.have.class('foobar');
   });
+
+  describe('type `boolean`', () => {
+    it('able to render number from provided `renderCell`', () => {
+      render(
+        <div style={{ width: 300, height: 500 }}>
+          <DataGrid
+            autoHeight={isJSDOM}
+            columns={[
+              {
+                field: 'number',
+                headerName: 'Number',
+                type: 'boolean',
+                renderCell: (params) => (params.value ? 1 : 0),
+              },
+            ]}
+            rows={[
+              {
+                id: 1,
+                number: false,
+              },
+              {
+                id: 2,
+                number: true,
+              },
+            ]}
+          />
+        </div>,
+      );
+      expect(getCell(0, 0)).to.have.text('0');
+      expect(getCell(1, 0)).to.have.text('1');
+    });
+
+    it('formattedValue to string by default', () => {
+      render(
+        <div style={{ width: 300, height: 500 }}>
+          <DataGrid
+            autoHeight={isJSDOM}
+            columns={[
+              {
+                field: 'text',
+                headerName: 'Text',
+                type: 'boolean',
+                renderCell: (params) => params.formattedValue,
+              },
+            ]}
+            rows={[
+              {
+                id: 1,
+                text: false,
+              },
+              {
+                id: 2,
+                text: true,
+              },
+            ]}
+          />
+        </div>,
+      );
+      expect(getCell(0, 0)).to.have.text('false');
+      expect(getCell(1, 0)).to.have.text('true');
+    });
+  });
 });
