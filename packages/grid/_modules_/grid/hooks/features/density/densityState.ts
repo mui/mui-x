f64a4818d8f40fbfc71b74d37b756e65f69e3ed5
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-07-31 21:57:52 +0200

    remove useless variable

--- packages/grid/_modules_/grid/hooks/features/density/densityState.ts
+++ packages/grid/_modules_/grid/hooks/features/density/densityState.ts
@@ -6,16 +6,10 @@ export interface GridGridDensity {
   headerHeight: number;
 }
 
-const DEFAULT_GRID_DENSITY = GridDensityTypes.Standard;
-
-const DEFAULT_GRID_ROW_HEIGHT = 52;
-
-const DEFAULT_GRID_HEADER_HEIGHT = 56;
-
 export function getInitialGridDensityState(): GridGridDensity {
   return {
-    value: DEFAULT_GRID_DENSITY,
-    rowHeight: DEFAULT_GRID_ROW_HEIGHT,
-    headerHeight: DEFAULT_GRID_HEADER_HEIGHT,
+    value: GridDensityTypes.Standard,
+    rowHeight: 52,
+    headerHeight: 56,
   };
 }
