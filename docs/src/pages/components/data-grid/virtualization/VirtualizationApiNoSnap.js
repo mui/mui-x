655698d15475e70d2c7b5b2917210ddff4d54516
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-07-31 17:02:45 +0200

    absolute path

--- docs/src/pages/components/data-grid/virtualization/VirtualizationApiNoSnap.js
+++ docs/src/pages/components/data-grid/virtualization/VirtualizationApiNoSnap.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ApiDocs from 'docsx/src/modules/components/ApiDocs';
-import api from '../../../../../pages/api-docs/data-grid/grid-virtualization-api.json';
+import api from 'docsx/pages/api-docs/data-grid/grid-virtualization-api.json';
 
 export default function VirtualizationApiNoSnap() {
   return <ApiDocs api={api} />;
