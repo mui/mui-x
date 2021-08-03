0c7fb787bd4bf6e62bc0d031fcf41d633fbb87f0
delangle <flaviendelangle@gmail.com>
2021-08-03 10:59:18 +0200

    Prettier

---
+++ docs/pages/api-docs/data-grid/grid-events.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
+import { prepareMarkdown } from 'docs/src/modules/utils/parseMarkdown';
+
+const pageFilename = 'api/grid-events';
+const requireRaw = require.context('!raw-loader!./', false, /\/grid-events\.md$/);
+
+export default function Page({ docs }) {
+  return <MarkdownDocs docs={docs} />;
+}
+
+Page.getInitialProps = () => {
+  const { demos, docs } = prepareMarkdown({ pageFilename, requireRaw });
+  return { demos, docs };
+};
---
+++ docs/pages/api-docs/data-grid/grid-events.md
@@ -0,0 +1,16 @@
+# GridEvents Interface
+
+<p class="description"></p>
+
+## Import
+
+```js
+import { GridEvents } from '@material-ui/x-grid';
+// or
+import { GridEvents } from '@material-ui/data-grid';
+```
+
+## Properties
+
+| Name | Type | Description |
+| :--- | :--- | :---------- |
