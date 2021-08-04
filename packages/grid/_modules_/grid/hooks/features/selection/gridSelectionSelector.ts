3a28278fca0d988e840cacd9d8149e0d7219ab9f
delangle <flaviendelangle@gmail.com>
2021-08-04 10:25:36 +0200

    Prettier

--- packages/grid/_modules_/grid/hooks/features/selection/gridSelectionSelector.ts
+++ packages/grid/_modules_/grid/hooks/features/selection/gridSelectionSelector.ts
@@ -6,34 +6,34 @@ import { GridRowId, GridRowModel } from '../../../models/gridRows';
 
 export const gridSelectionStateSelector = (state: GridState) => state.selection;
 export const selectedGridRowsCountSelector: OutputSelector<
-    GridState,
-    number,
-    (res: GridSelectionState) => number
-    > = createSelector<GridState, GridSelectionState, number>(
-    gridSelectionStateSelector,
-    (selection) => selection.length,
+  GridState,
+  number,
+  (res: GridSelectionState) => number
+> = createSelector<GridState, GridSelectionState, number>(
+  gridSelectionStateSelector,
+  (selection) => selection.length,
 );
 
 export const selectedGridRowsSelector = createSelector<
-    GridState,
-    GridSelectionState,
-    GridRowsLookup,
-    Map<GridRowId, GridRowModel>
-    >(
-    gridSelectionStateSelector,
-    gridRowsLookupSelector,
-    (selectedRows, rowsLookup) => new Map(selectedRows.map((id) => [id, rowsLookup[id]])),
+  GridState,
+  GridSelectionState,
+  GridRowsLookup,
+  Map<GridRowId, GridRowModel>
+>(
+  gridSelectionStateSelector,
+  gridRowsLookupSelector,
+  (selectedRows, rowsLookup) => new Map(selectedRows.map((id) => [id, rowsLookup[id]])),
 );
 
 export const selectedIdsLookupSelector: OutputSelector<
-    GridState,
-    Record<string, GridRowId>,
-    (res: GridSelectionState) => Record<string, GridRowId>
-    > = createSelector<GridState, GridSelectionState, Record<string, GridRowId>>(
-    gridSelectionStateSelector,
-    (selection) =>
-        selection.reduce((lookup, rowId) => {
-            lookup[rowId] = rowId;
-            return lookup;
-        }, {}),
+  GridState,
+  Record<string, GridRowId>,
+  (res: GridSelectionState) => Record<string, GridRowId>
+> = createSelector<GridState, GridSelectionState, Record<string, GridRowId>>(
+  gridSelectionStateSelector,
+  (selection) =>
+    selection.reduce((lookup, rowId) => {
+      lookup[rowId] = rowId;
+      return lookup;
+    }, {}),
 );
