3dd9f41e93628f27e026b91c230344897a5f3883
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2021-08-08 12:39:15 +0530

    add module augmentation for theme style overrides

---
+++ packages/grid/_modules_/grid/themeAugmentation/overrides.ts
@@ -0,0 +1,9 @@
+import { GridClassKey } from '../models/gridClasses';
+
+export interface GridComponentNameToClassKey {
+  MuiDataGrid: GridClassKey;
+}
+
+declare module '@material-ui/core/styles/overrides' {
+  interface ComponentNameToClassKey extends GridComponentNameToClassKey {}
+}
