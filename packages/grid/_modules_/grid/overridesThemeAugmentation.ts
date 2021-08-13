6f169d3275fad2d5d86134436766a1f1107d69f2
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-08-14 00:48:32 +0200

    leave a note for later

--- packages/grid/_modules_/grid/overridesThemeAugmentation.ts
+++ packages/grid/_modules_/grid/overridesThemeAugmentation.ts
@@ -4,6 +4,7 @@ export interface GridComponentNameToClassKey {
   MuiDataGrid: GridClassKey;
 }
 
+// TODO v5 use '@material-ui/core/styles'
 declare module '@material-ui/core/styles/overrides' {
   interface ComponentNameToClassKey extends GridComponentNameToClassKey {}
 }
