24422fa186331592dd6ee3715813f5a537a62fc3
delangle <flaviendelangle@gmail.com>
2021-09-01 13:31:45 +0200

    Fix

--- packages/grid/_modules_/grid/hooks/features/pagination/gridPaginationSelector.ts
+++ packages/grid/_modules_/grid/hooks/features/pagination/gridPaginationSelector.ts
@@ -2,21 +2,15 @@ import { createSelector } from 'reselect';
 import { GridState } from '../core/gridState';
 import { visibleSortedGridRowIdsSelector } from '../filter/gridFilterSelector';
 import { GridPaginationState } from './gridPaginationState';
-import { gridContainerSizesSelector } from '../../root/gridContainerSizesSelector';
 
 export const gridPaginationSelector = (state: GridState): GridPaginationState => state.pagination;
 
 export const gridPaginatedVisibleSortedGridRowIdsSelector = createSelector(
   gridPaginationSelector,
   visibleSortedGridRowIdsSelector,
-  gridContainerSizesSelector,
-  (pagination, visibleSortedRows, containerSizes) => {
-    if (!containerSizes) {
-      return [];
-    }
-
+  (pagination, visibleSortedRows) => {
     const firstSelectedRowIndex = pagination.page * pagination.pageSize;
-    const lastSelectedRowIndex = firstSelectedRowIndex + containerSizes.virtualRowsCount;
+    const lastSelectedRowIndex = firstSelectedRowIndex + pagination.pageSize;
 
     return visibleSortedRows.slice(firstSelectedRowIndex, lastSelectedRowIndex);
   },
