a6dcd52c7f64a85d76ef30d061c8b7f5984163c1
Matheus Wichman <matheushw@outlook.com>
2021-09-03 10:05:24 -0300

    Unwrap createStyles

--- docs/src/pages/components/data-grid/components/CustomEmptyOverlayGrid.tsx
+++ docs/src/pages/components/data-grid/components/CustomEmptyOverlayGrid.tsx
@@ -2,35 +2,34 @@ import * as React from 'react';
 import { GridOverlay, DataGrid } from '@mui/x-data-grid';
 import { useDemoData } from '@mui/x-data-grid-generator';
 import { createTheme, Theme } from '@mui/material/styles';
-import { createStyles, makeStyles } from '@mui/styles';
+import { makeStyles } from '@mui/styles';
 
 const defaultTheme = createTheme();
 const useStyles = makeStyles(
-  (theme: Theme) =>
-    createStyles({
-      root: {
-        flexDirection: 'column',
-        '& .ant-empty-img-1': {
-          fill: theme.palette.mode === 'light' ? '#aeb8c2' : '#262626',
-        },
-        '& .ant-empty-img-2': {
-          fill: theme.palette.mode === 'light' ? '#f5f5f7' : '#595959',
-        },
-        '& .ant-empty-img-3': {
-          fill: theme.palette.mode === 'light' ? '#dce0e6' : '#434343',
-        },
-        '& .ant-empty-img-4': {
-          fill: theme.palette.mode === 'light' ? '#fff' : '#1c1c1c',
-        },
-        '& .ant-empty-img-5': {
-          fillOpacity: theme.palette.mode === 'light' ? '0.8' : '0.08',
-          fill: theme.palette.mode === 'light' ? '#f5f5f5' : '#fff',
-        },
+  (theme: Theme) => ({
+    root: {
+      flexDirection: 'column',
+      '& .ant-empty-img-1': {
+        fill: theme.palette.mode === 'light' ? '#aeb8c2' : '#262626',
       },
-      label: {
-        marginTop: theme.spacing(1),
+      '& .ant-empty-img-2': {
+        fill: theme.palette.mode === 'light' ? '#f5f5f7' : '#595959',
       },
-    }),
+      '& .ant-empty-img-3': {
+        fill: theme.palette.mode === 'light' ? '#dce0e6' : '#434343',
+      },
+      '& .ant-empty-img-4': {
+        fill: theme.palette.mode === 'light' ? '#fff' : '#1c1c1c',
+      },
+      '& .ant-empty-img-5': {
+        fillOpacity: theme.palette.mode === 'light' ? '0.8' : '0.08',
+        fill: theme.palette.mode === 'light' ? '#f5f5f5' : '#fff',
+      },
+    },
+    label: {
+      marginTop: theme.spacing(1),
+    },
+  }),
   { defaultTheme },
 );
 
