52006b829fd52d0f6146fdd70f92453048b585f6
Matheus Wichman <matheushw@outlook.com>
2021-09-06 10:24:57 -0300

    add new example action

--- docs/src/pages/components/data-grid/columns/ColumnTypesGrid.js
+++ docs/src/pages/components/data-grid/columns/ColumnTypesGrid.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
 import DeleteIcon from '@material-ui/icons/Delete';
 import SecurityIcon from '@material-ui/icons/Security';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 import { randomCreatedDate, randomUpdatedDate } from '@mui/x-data-grid-generator';
 
 const initialRows = [
@@ -55,6 +56,18 @@ export default function ColumnTypesGrid() {
     [],
   );
 
+  const duplicateUser = React.useCallback(
+    (id) => () => {
+      setRows((prevRows) => {
+        const rowToDuplicate = prevRows.find((row) => row.id === id);
+        const newRows = [...prevRows, { ...rowToDuplicate, id: Date.now() }];
+        console.log(newRows);
+        return newRows;
+      });
+    },
+    [],
+  );
+
   const columns = React.useMemo(
     () => [
       { field: 'name', type: 'string' },
@@ -91,10 +104,16 @@ export default function ColumnTypesGrid() {
             onClick={toggleAdmin(params.id)}
             showInMenu
           />,
+          <GridActionsCellItem
+            icon={<FileCopyIcon />}
+            label="Duplicate User"
+            onClick={duplicateUser(params.id)}
+            showInMenu
+          />,
         ],
       },
     ],
-    [deleteUser, toggleAdmin],
+    [deleteUser, toggleAdmin, duplicateUser],
   );
 
   return (
