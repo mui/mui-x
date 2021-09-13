943879f3db91bfc4e1e73305642aa08cb387fb4d
delangle <flaviendelangle@gmail.com>
2021-09-13 11:26:26 +0200

    yarn docs:api:build

--- packages/grid/_modules_/grid/models/gridOptions.tsx
+++ packages/grid/_modules_/grid/models/gridOptions.tsx
@@ -219,7 +219,7 @@ export interface GridSimpleOptions {
    */
   sortingMode: GridFeatureMode;
   /**
-   * If positive, the Grid will throttle updates coming from `props.rows`, `apiRef.current.updateRows` or `apiRef.current.setRows`.
+   * If positive, the Grid will throttle updates coming from `apiRef.current.updateRows` and `apiRef.current.setRows`.
    * It can be useful if you have a high update rate but do not want to do heavy work like filtering / sorting or rendering on each  individual update.
    * @default 50
    */
