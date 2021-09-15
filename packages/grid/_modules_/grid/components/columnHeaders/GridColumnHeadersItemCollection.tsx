b29874529efbff0a2fffa5b7456412fe00d70f35
delangle <flaviendelangle@gmail.com>
2021-09-15 12:54:52 +0200

    Fix

--- packages/grid/_modules_/grid/components/columnHeaders/GridColumnHeadersItemCollection.tsx
+++ packages/grid/_modules_/grid/components/columnHeaders/GridColumnHeadersItemCollection.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { gridResizingColumnFieldSelector } from '../../hooks/features/columnResize/columnResizeSelector';
 import { useGridSelector } from '../../hooks/features/core/useGridSelector';
 import { filterGridColumnLookupSelector } from '../../hooks/features/filter/gridFilterSelector';
 import {
