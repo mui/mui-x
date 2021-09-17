c7bf76e4c1e5cfd2824a520607cb307f4daaf901
delangle <flaviendelangle@gmail.com>
2021-09-17 13:08:41 +0200

    Add pagination treeData test

--- packages/grid/x-grid/src/tests/treeData.DataGridPro.test.tsx
+++ packages/grid/x-grid/src/tests/treeData.DataGridPro.test.tsx
@@ -1,4 +1,9 @@
-import { createClientRenderStrictMode, fireEvent, screen } from 'test/utils';
+import {
+  createClientRenderStrictMode,
+  // @ts-expect-error need to migrate helpers to TypeScript
+  fireEvent,
+  screen,
+} from 'test/utils';
 import { getCell, getColumnHeadersTextContent, getColumnValues } from 'test/utils/helperFn';
 import * as React from 'react';
 import { expect } from 'chai';
@@ -6,18 +11,15 @@ import { DataGridPro, DataGridProProps } from '@mui/x-data-grid-pro';
 
 const baselineProps: DataGridProProps = {
   rows: [
-    { id: 0, name: 'A' },
-    { id: 1, name: 'A.A' },
-    { id: 2, name: 'B' },
-    { id: 3, name: 'B.A' },
-    { id: 4, name: 'B.B' },
-    { id: 5, name: 'B.B.A' },
-    { id: 6, name: 'C' },
+    { name: 'A' },
+    { name: 'A.A' },
+    { name: 'B' },
+    { name: 'B.A' },
+    { name: 'B.B' },
+    { name: 'B.B.A' },
+    { name: 'C' },
   ],
   columns: [
-    {
-      field: 'id',
-    },
     {
       field: 'name',
       width: 200,
@@ -25,15 +27,16 @@ const baselineProps: DataGridProProps = {
   ],
   treeData: true,
   getTreeDataPath: (row) => row.name.split('.'),
+  getRowId: (row) => row.name,
 };
 
 describe.only('<DataGridPro /> - Pagination', () => {
   // TODO v5: replace with createClientRender
   const render = createClientRenderStrictMode();
 
-  const Test = () => (
+  const Test = (props: Partial<DataGridProProps>) => (
     <div style={{ width: 300, height: 300 }}>
-      <DataGridPro {...baselineProps} />
+      <DataGridPro {...baselineProps} {...props} />
     </div>
   );
 
@@ -42,30 +45,41 @@ describe.only('<DataGridPro /> - Pagination', () => {
       render(<Test />);
 
       const columnsHeader = getColumnHeadersTextContent();
-      expect(columnsHeader).to.have.length(3);
+      expect(columnsHeader).to.have.length(baselineProps.columns.length + 1);
       expect(columnsHeader[0]).to.equal('Group');
     });
 
     it('should toggle expansion when clicking on grouping column icon', () => {
       render(<Test />);
 
-      expect(getColumnValues(1)).to.deep.equal(['0', '2', '6']);
+      expect(getColumnValues(1)).to.deep.equal(['A', 'B', 'C']);
       fireEvent.click(getCell(0, 0).querySelector('button'));
-      expect(getColumnValues(1)).to.deep.equal(['0', '1', '2', '6']);
+      expect(getColumnValues(1)).to.deep.equal(['A', 'A.A', 'B', 'C']);
       fireEvent.click(getCell(0, 0).querySelector('button'));
-      expect(getColumnValues(1)).to.deep.equal(['0', '2', '6']);
+      expect(getColumnValues(1)).to.deep.equal(['A', 'B', 'C']);
     });
 
     it('should toggle expansion when pressing Space while focusing grouping column', () => {
       render(<Test />);
 
-      expect(getColumnValues(1)).to.deep.equal(['0', '2', '6']);
+      expect(getColumnValues(1)).to.deep.equal(['A', 'B', 'C']);
       getCell(0, 0).focus();
-      expect(getColumnValues(1)).to.deep.equal(['0', '2', '6']);
+      expect(getColumnValues(1)).to.deep.equal(['A', 'B', 'C']);
       fireEvent.keyDown(getCell(0, 0), { key: ' ' });
-      expect(getColumnValues(1)).to.deep.equal(['0', '1', '2', '6']);
+      expect(getColumnValues(1)).to.deep.equal(['A', 'A.A', 'B', 'C']);
       fireEvent.keyDown(getCell(0, 0), { key: ' ' });
-      expect(getColumnValues(1)).to.deep.equal(['0', '2', '6']);
+      expect(getColumnValues(1)).to.deep.equal(['A', 'B', 'C']);
+    });
+  });
+
+  describe('pagination', () => {
+    it('should respect the pageSize when toggling children expansion', () => {
+      render(<Test pagination pageSize={2} rowsPerPageOptions={[2]} />);
+      expect(getColumnValues(1)).to.deep.equal(['A', 'B']);
+      fireEvent.click(getCell(0, 0).querySelector('button'));
+      expect(getColumnValues(1)).to.deep.equal(['A', 'A.A']);
+      fireEvent.click(screen.getByRole('button', { name: /next page/i }));
+      expect(getColumnValues(1)).to.deep.equal(['B', 'C']);
     });
   });
 });
