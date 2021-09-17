d4323fdb9b0729f466267243d777b4469f95d80d
Danail H <hadjiatanasov@gmail.com>
2021-09-17 14:52:10 +0200

    Add docs

--- packages/grid/_modules_/grid/hooks/features/export/useGridPrintExport.tsx
+++ packages/grid/_modules_/grid/hooks/features/export/useGridPrintExport.tsx
@@ -65,7 +65,7 @@ export const useGridPrintExport = (
               return column;
             }
 
-            column.hide = !fields?.includes(column.field);
+            column.hide = !fields?.includes(column.field) || column.disableExport;
 
             return column;
           }),
