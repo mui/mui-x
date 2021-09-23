ce7728fae636ea5e2b90c602d16624987ac93b33
Michał Dudak <michal@dudak.me>
2021-09-23 15:11:31 +0100

    [BarChart] Use Github stats as the basic demo's data

--- docs/src/pages/components/charts/bar-chart/BasicBarChart.js
+++ docs/src/pages/components/charts/bar-chart/BasicBarChart.js
@@ -6,23 +6,44 @@ import XAxis from '@mui/charts/XAxis';
 import YAxis from '@mui/charts/YAxis';
 import Grid from '@mui/charts/Grid';
 
-const barData = [
-  { x: new Date(2015, 0, 1), y: 4 },
-  { x: new Date(2016, 0, 1), y: 14 },
-  { x: new Date(2017, 0, 1), y: 36 },
-  { x: new Date(2018, 0, 1), y: 38 },
-  { x: new Date(2019, 0, 1), y: 54 },
-  { x: new Date(2020, 0, 1), y: 47 },
-  { x: new Date(2021, 0, 1), y: 70 },
-];
+async function getData() {
+  const response = await fetch(
+    'https://api.github.com/repos/mui-org/material-ui/stats/punch_card',
+  );
+  const rawData = await response.json();
+
+  const dataPoints = {};
+  rawData.forEach((record) => {
+    dataPoints[record[1]] = (dataPoints[record[1]] ?? 0) + record[2];
+  });
+
+  console.log(dataPoints);
+  return Object.keys(dataPoints).map((hour) => ({ x: hour, y: dataPoints[hour] }));
+}
 
 export default function BasicBarChart() {
+  const [chartData, setChartData] = React.useState([]);
+
+  React.useEffect(() => {
+    async function loadData() {
+      setChartData(await getData());
+    }
+
+    loadData();
+  }, []);
+
   const theme = useTheme();
   return (
-    <BarChart data={barData} xScaleType="time">
+    <BarChart
+      data={chartData}
+      xScaleType="linear"
+      xDomain={[0, 23]}
+      label="Commits to the MUI repo per hour of day"
+      pixelsPerTick={30}
+    >
       <Grid disableX />
       <XAxis />
-      <YAxis suffix="kg" />
+      <YAxis />
       <Bar stroke="rgb(235,97,97)" fill={theme.palette.primary.main} />
     </BarChart>
   );
