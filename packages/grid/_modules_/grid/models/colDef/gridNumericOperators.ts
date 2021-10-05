84b5677a37244f6efbe88874b226afc1899ee0e2
Matheus Wichman <matheushw@outlook.com>
2021-10-05 14:55:12 -0300

    [DataGrid] Don't filter numeric column when value is empty (#2780)

--- packages/grid/_modules_/grid/models/colDef/gridNumericOperators.ts
+++ packages/grid/_modules_/grid/models/colDef/gridNumericOperators.ts
@@ -7,7 +7,7 @@ export const getGridNumericColumnOperators = (): GridFilterOperator[] => [
     label: '=',
     value: '=',
     getApplyFilterFn: (filterItem: GridFilterItem) => {
-      if (filterItem.value == null) {
+      if (filterItem.value == null || Number.isNaN(filterItem.value)) {
         return null;
       }
 
@@ -22,7 +22,7 @@ export const getGridNumericColumnOperators = (): GridFilterOperator[] => [
     label: '!=',
     value: '!=',
     getApplyFilterFn: (filterItem: GridFilterItem) => {
-      if (filterItem.value == null) {
+      if (filterItem.value == null || Number.isNaN(filterItem.value)) {
         return null;
       }
 
@@ -37,7 +37,7 @@ export const getGridNumericColumnOperators = (): GridFilterOperator[] => [
     label: '>',
     value: '>',
     getApplyFilterFn: (filterItem: GridFilterItem) => {
-      if (filterItem.value == null) {
+      if (filterItem.value == null || Number.isNaN(filterItem.value)) {
         return null;
       }
 
@@ -52,7 +52,7 @@ export const getGridNumericColumnOperators = (): GridFilterOperator[] => [
     label: '>=',
     value: '>=',
     getApplyFilterFn: (filterItem: GridFilterItem) => {
-      if (filterItem.value == null) {
+      if (filterItem.value == null || Number.isNaN(filterItem.value)) {
         return null;
       }
 
@@ -67,7 +67,7 @@ export const getGridNumericColumnOperators = (): GridFilterOperator[] => [
     label: '<',
     value: '<',
     getApplyFilterFn: (filterItem: GridFilterItem) => {
-      if (filterItem.value == null) {
+      if (filterItem.value == null || Number.isNaN(filterItem.value)) {
         return null;
       }
 
@@ -82,7 +82,7 @@ export const getGridNumericColumnOperators = (): GridFilterOperator[] => [
     label: '<=',
     value: '<=',
     getApplyFilterFn: (filterItem: GridFilterItem) => {
-      if (filterItem.value == null) {
+      if (filterItem.value == null || Number.isNaN(filterItem.value)) {
         return null;
       }
 
