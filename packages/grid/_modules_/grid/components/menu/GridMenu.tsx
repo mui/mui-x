b2832d6c93d4a2f6cecd1098f7d52e2b67759042
Danail H <hadjiatanasov@gmail.com>
2021-10-06 14:35:58 +0200

    Remove commented out code

--- packages/grid/_modules_/grid/components/menu/GridMenu.tsx
+++ packages/grid/_modules_/grid/components/menu/GridMenu.tsx
@@ -49,7 +49,6 @@ const GridMenu = (props: GridMenuProps) => {
   const { open, target, onClickAway, children, position, ...other } = props;
   const prevTarget = React.useRef(target);
   const prevOpen = React.useRef(open);
-  // const classes = useStyles();
 
   React.useEffect(() => {
     if (prevOpen.current && prevTarget.current) {
