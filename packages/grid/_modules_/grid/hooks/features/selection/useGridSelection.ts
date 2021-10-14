14df18549d1b547e7f88ac3f827d9cce68626b32
Flavien DELANGLE <flaviendelangle@gmail.com>
2021-10-14 09:04:04 +0200

    [DataGrid] The header selection checkbox should work with prop.checkboxSelectionVisibleOnly (#2781)

--- packages/grid/_modules_/grid/hooks/features/selection/useGridSelection.ts
+++ packages/grid/_modules_/grid/hooks/features/selection/useGridSelection.ts
@@ -16,7 +16,9 @@ import {
   selectedGridRowsSelector,
   selectedIdsLookupSelector,
 } from './gridSelectionSelector';
+import { gridPaginatedVisibleSortedGridRowIdsSelector } from '../pagination';
 import { visibleSortedGridRowIdsSelector } from '../filter';
+import { GridHeaderSelectionCheckboxParams } from '../../../models/params/gridHeaderSelectionCheckboxParams';
 import { GridCellParams } from '../../../models/params/gridCellParams';
 import { GridRowSelectionCheckboxParams } from '../../../models/params/gridRowSelectionCheckboxParams';
 import { useGridStateInit } from '../../utils/useGridStateInit';
@@ -267,11 +269,30 @@ export const useGridSelection = (
     [apiRef, expandRowRangeSelection],
   );
 
+  const handleHeaderSelectionCheckboxChange = React.useCallback(
+    (params: GridHeaderSelectionCheckboxParams) => {
+      const shouldLimitSelectionToCurrentPage =
+        props.checkboxSelectionVisibleOnly && props.pagination;
+
+      const rowsToBeSelected = shouldLimitSelectionToCurrentPage
+        ? gridPaginatedVisibleSortedGridRowIdsSelector(apiRef.current.state)
+        : visibleSortedGridRowIdsSelector(apiRef.current.state);
+
+      apiRef.current.selectRows(rowsToBeSelected, params.value);
+    },
+    [apiRef, props.checkboxSelectionVisibleOnly, props.pagination],
+  );
+
   useGridApiEventHandler(apiRef, GridEvents.rowClick, handleRowClick);
   useGridApiEventHandler(
     apiRef,
     GridEvents.rowSelectionCheckboxChange,
     handleRowSelectionCheckboxChange,
+  );
+  useGridApiEventHandler(
+    apiRef,
+    GridEvents.headerSelectionCheckboxChange,
+    handleHeaderSelectionCheckboxChange,
   );
   useGridApiEventHandler(apiRef, GridEvents.cellMouseDown, preventSelectionOnShift);
 
