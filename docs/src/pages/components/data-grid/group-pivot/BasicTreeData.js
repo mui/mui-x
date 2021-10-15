f15a828e6679c1608d38bf9bec6a7cfabab8c08b
delangle <flaviendelangle@gmail.com>
2021-10-15 15:00:52 +0200

    Improve basic tree data example

--- docs/src/pages/components/data-grid/group-pivot/BasicTreeData.js
+++ docs/src/pages/components/data-grid/group-pivot/BasicTreeData.js
@@ -1,16 +1,40 @@
 import * as React from 'react';
 import { DataGridPro } from '@mui/x-data-grid-pro';
-import { useDemoTreeData } from '@mui/x-data-grid-generator';
+
+const rows = [
+  { path: ['Sarah'], jobTitle: 'CEO' },
+  { path: ['Sarah', 'Thomas'], jobTitle: 'Head of Sales' },
+  { path: ['Sarah', 'Thomas', 'Robert'], jobTitle: 'Sales Person' },
+  { path: ['Sarah', 'Thomas', 'Karen'], jobTitle: 'Sales Person' },
+  { path: ['Sarah', 'Thomas', 'Nancy'], jobTitle: 'Sales Person' },
+  { path: ['Sarah', 'Thomas', 'Daniel'], jobTitle: 'Sales Person' },
+  { path: ['Sarah', 'Thomas', 'Christopher'], jobTitle: 'Sales Person' },
+  { path: ['Sarah', 'Thomas', 'Donald'], jobTitle: 'Sales Person' },
+  { path: ['Sarah', 'Mary'], jobTitle: 'Head of Engineering' },
+  { path: ['Sarah', 'Mary', 'Jennifer'], jobTitle: 'Tech lead front' },
+  { path: ['Sarah', 'Mary', 'Jennifer', 'Linda'], jobTitle: 'Front-end developer' },
+  { path: ['Sarah', 'Mary', 'Michael'], jobTitle: 'Tech lead devops' },
+  { path: ['Sarah', 'Mary', 'Linda'], jobTitle: 'Tech lead back' },
+  { path: ['Sarah', 'Mary', 'Linda', 'Elizabeth'], jobTitle: 'Back-end developer' },
+  { path: ['Sarah', 'Mary', 'Linda', 'William'], jobTitle: 'Back-end developer' },
+];
+
+const columns = [{ field: 'jobTitle', width: 250 }];
+
+const getRowId = (row) => row.path.join('-');
+const getTreeDataPath = (row) => row.path;
 
 export default function BasicTreeData() {
-  const { data, loading } = useDemoTreeData({
-    rowLength: [10, 5, 3],
-    randomLength: true,
-  });
-
   return (
     <div style={{ height: 300, width: '100%' }}>
-      <DataGridPro loading={loading} treeData disableSelectionOnClick {...data} />
+      <DataGridPro
+        treeData
+        rows={rows}
+        columns={columns}
+        disableSelectionOnClick
+        getRowId={getRowId}
+        getTreeDataPath={getTreeDataPath}
+      />
     </div>
   );
 }
