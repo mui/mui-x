c201de0febe45199cf0dee2ee5eb3528a9b4ccef
delangle <flaviendelangle@gmail.com>
2021-10-20 13:04:55 +0200

    Work

--- packages/grid/_modules_/grid/components/cell/GridEditDateCell.tsx
+++ packages/grid/_modules_/grid/components/cell/GridEditDateCell.tsx
@@ -5,6 +5,7 @@ import { composeClasses, useEnhancedEffect } from '../../utils/material-ui-utils
 import { getDataGridUtilityClass } from '../../gridClasses';
 import { useGridRootProps } from '../../hooks/utils/useGridRootProps';
 import { GridComponentProps } from '../../GridComponentProps';
+import { GridCellValue } from '../../models/gridCell';
 
 type OwnerState = { classes: GridComponentProps['classes'] };
 
@@ -16,6 +17,31 @@ const useUtilityClasses = (ownerState: OwnerState) => {
   };
 
   return composeClasses(slots, getDataGridUtilityClass, classes);
+};
+
+const parseDate = (value: GridCellValue, isDateTime: boolean) => {
+  let date: Date;
+
+  if (value instanceof Date) {
+    date = value;
+  } else {
+    date = new Date((value ?? '').toString());
+  }
+
+  date.setMilliseconds(0);
+  date.setSeconds(0);
+
+  if (!isDateTime) {
+    date.setMinutes(0);
+    date.setHours(0);
+  }
+
+  return date;
+};
+
+const stringifyDate = (date: Date, isDateTime: boolean) => {
+  const localeDate = new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
+  return localeDate.toISOString().substr(0, isDateTime ? 16 : 10);
 };
 
 export function GridEditDateCell(props: GridRenderEditCellParams & InputBaseProps) {
@@ -35,45 +61,51 @@ export function GridEditDateCell(props: GridRenderEditCellParams & InputBaseProp
     ...other
   } = props;
 
+  const isDateTime = colDef.type === 'dateTime';
   const inputRef = React.useRef<HTMLInputElement>();
-  const [valueState, setValueState] = React.useState(value);
+  const [valueState, setValueState] = React.useState(() =>
+    stringifyDate(parseDate(value, isDateTime), isDateTime),
+  );
   const rootProps = useGridRootProps();
   const ownerState = { classes: rootProps.classes };
   const classes = useUtilityClasses(ownerState);
 
-  const handleChange = React.useCallback(
-    (event) => {
-      const newValue = event.target.value;
-      setValueState(newValue);
+  const handleChange: InputBaseProps['onChange'] = (event) => {
+    const newValue = event.target.value;
+    setValueState(newValue);
 
-      if (newValue === '') {
-        api.setEditCellValue({ id, field, value: null }, event);
-        return;
-      }
+    if (newValue === '') {
+      api.setEditCellValue({ id, field, value: null }, event);
+      return;
+    }
 
-      const [date, time] = newValue.split('T');
-      const [year, month, day] = date.split('-');
-      const dateObj = new Date();
-      dateObj.setFullYear(Number(year));
-      dateObj.setMonth(Number(month) - 1);
-      dateObj.setDate(Number(day));
-      dateObj.setHours(0, 0, 0, 0);
+    const [date, time] = newValue.split('T');
+    const [year, month, day] = date.split('-');
+    const dateObj = new Date();
+    dateObj.setFullYear(Number(year));
+    dateObj.setMonth(Number(month) - 1);
+    dateObj.setDate(Number(day));
+    dateObj.setHours(0, 0, 0, 0);
 
-      if (time) {
-        const [hours, minutes] = time.split(':');
-        dateObj.setHours(Number(hours), Number(minutes), 0, 0);
-      }
+    if (time) {
+      const [hours, minutes] = time.split(':');
+      dateObj.setHours(Number(hours), Number(minutes), 0, 0);
+    }
 
-      api.setEditCellValue({ id, field, value: dateObj }, event);
-    },
-    [api, field, id],
-  );
-
-  const isDateTime = colDef.type === 'dateTime';
+    api.setEditCellValue({ id, field, value: dateObj }, event);
+  };
 
   React.useEffect(() => {
-    setValueState(value);
-  }, [value]);
+    const parsedPropValue = parseDate(value, isDateTime);
+    const parsedStateValue = new Date(valueState);
+
+    const propTimestamp = parsedPropValue.getTime();
+    const stateTimestamp = parsedStateValue.getTime();
+
+    if (propTimestamp !== stateTimestamp && !Number.isNaN(propTimestamp)) {
+      setValueState(stringifyDate(parsedPropValue, isDateTime));
+    }
+  }, [value, isDateTime]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEnhancedEffect(() => {
     if (hasFocus) {
@@ -81,20 +113,13 @@ export function GridEditDateCell(props: GridRenderEditCellParams & InputBaseProp
     }
   }, [hasFocus]);
 
-  let valueToDisplay = valueState || '';
-  if (valueState instanceof Date) {
-    const offset = valueState.getTimezoneOffset();
-    const localDate = new Date(valueState.getTime() - offset * 60 * 1000);
-    valueToDisplay = localDate.toISOString().substr(0, isDateTime ? 16 : 10);
-  }
-
   return (
     <InputBase
       inputRef={inputRef}
       fullWidth
       className={classes.root}
       type={isDateTime ? 'datetime-local' : 'date'}
-      value={valueToDisplay}
+      value={valueState}
       onChange={handleChange}
       {...other}
     />
