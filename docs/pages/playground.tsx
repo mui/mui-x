5b47ae1034c9c530849f0323ce7f7cac705253c3
delangle <flaviendelangle@gmail.com>
2021-10-29 11:58:04 +0200

    Fix

--- docs/pages/playground.tsx
+++ docs/pages/playground.tsx
@@ -1,5 +1,33 @@
+import { useDemoData } from '@mui/x-data-grid-generator';
+import { DataGridPro } from '@mui/x-data-grid-pro';
 import * as React from 'react';
 
-export default function Playground() {
-  return <div>This file is listed in `.gitignore`</div>;
+export default function FlexLayoutGridSnap() {
+  const { data, loading } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 15,
+    maxColumns: 10,
+  });
+
+  const tmpData = JSON.parse(JSON.stringify(data));
+  tmpData.columns.forEach((item) => {
+    item.flex = 1;
+  });
+  return (
+    <div style={{ height: '400px', width: '400px' }}>
+      <div style={{ display: 'flex', height: '100%' }}>
+        <div style={{ flexGrow: 1 }}>
+          <DataGridPro
+            autoHeight
+            showCellRightBorder
+            showColumnRightBorder
+            disableExtendRowFullWidth
+            rows={tmpData.rows}
+            columns={tmpData.columns}
+            loading={loading}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
