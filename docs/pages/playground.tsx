96ac8ef661403b8538d3bd6197ca8f1481bbd1bb
delangle <flaviendelangle@gmail.com>
2021-11-03 13:50:29 +0100

    Test

--- docs/pages/playground.tsx
+++ docs/pages/playground.tsx
@@ -1,26 +1,5 @@
 import * as React from 'react';
-import { DataGridPro } from '@mui/x-data-grid-pro';
-import { useDemoData } from '@mui/x-data-grid-generator';
-import { GridPreferencePanelsValue } from '../../packages/grid/_modules_';
 
 export default function Playground() {
-  const { data } = useDemoData({
-    dataSet: 'Commodity',
-    rowLength: 1000,
-    maxColumns: 15,
-  });
-
-  return (
-    <div style={{ width: '100%', height: 600 }}>
-      <DataGridPro
-        {...data}
-        initialState={{
-          preferencePanel: {
-            open: true,
-            openedPanelValue: GridPreferencePanelsValue.filters,
-          },
-        }}
-      />
-    </div>
-  );
+  return <div>This file is listed in `.gitignore`</div>;
 }
