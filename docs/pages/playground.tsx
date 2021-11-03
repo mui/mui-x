6a5c1f4f6d200fecbd122db225bf38a0070a79a2
delangle <flaviendelangle@gmail.com>
2021-11-03 11:45:12 +0100

    Test

--- docs/pages/playground.tsx
+++ docs/pages/playground.tsx
@@ -1,13 +1,5 @@
-import { useData } from 'storybook/src/hooks/useData';
-import { DataGridPro } from '@mui/x-data-grid-pro';
 import * as React from 'react';
 
-export default function Page2PropSnap() {
-  const data = useData(2000, 200);
-
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGridPro rows={data.rows} columns={data.columns} pagination pageSize={50} page={1} />
-    </div>
-  );
+export default function Playground() {
+  return <div>This file is listed in `.gitignore`</div>;
 }
