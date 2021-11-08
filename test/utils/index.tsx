e99c3b274a8f593421e0d8e7ba75f79aa02737eb
delangle <flaviendelangle@gmail.com>
2021-11-08 10:54:43 +0100

    [core] Replace createClientRender with new createRenderer API

--- test/utils/index.tsx
+++ test/utils/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { ThemeProvider, unstable_createMuiStrictModeTheme } from '@mui/material/styles';
-import { createClientRender } from '@material-ui/monorepo/test/utils';
+import { createRenderer } from '@material-ui/monorepo/test/utils';
 
 export * from '@material-ui/monorepo/test/utils';
 
+
 export const createClientRenderStrictMode = () => {
-  const render = createClientRender();
+  const { render } = createRenderer();
   const strictTheme = unstable_createMuiStrictModeTheme();
   const Wrapper = (props) => <ThemeProvider theme={strictTheme} {...props} />;
 
