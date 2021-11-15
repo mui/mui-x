84f840b7b369e5fc943c35b6a9660feabe677ea7
delangle <flaviendelangle@gmail.com>
2021-11-15 10:26:07 +0100

    Fix overlay

--- packages/grid/_modules_/grid/components/containers/GridOverlay.tsx
+++ packages/grid/_modules_/grid/components/containers/GridOverlay.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import clsx from 'clsx';
 import { unstable_composeClasses as composeClasses } from '@mui/material';
 import { alpha, styled } from '@mui/material/styles';
-import { useGridSelector } from '../../hooks/utils/useGridSelector';
-import { gridDensityHeaderHeightSelector } from '../../hooks/features/density/densitySelector';
 import { useGridApiContext } from '../../hooks/utils/useGridApiContext';
 import { getDataGridUtilityClass } from '../../gridClasses';
 import { useGridRootProps } from '../../hooks/utils/useGridRootProps';
 import { GridComponentProps } from '../../GridComponentProps';
+import { useGridApiEventHandler } from '../../hooks/utils/useGridApiEventHandler';
+import { GridEvents } from '../../constants';
 
 export type GridOverlayProps = React.HTMLAttributes<HTMLDivElement>;
 
@@ -49,20 +49,36 @@ export const GridOverlay = React.forwardRef<HTMLDivElement, GridOverlayProps>(fu
   const rootProps = useGridRootProps();
   const ownerState = { classes: rootProps.classes };
   const classes = useUtilityClasses(ownerState);
-  const headerHeight = useGridSelector(apiRef, gridDensityHeaderHeightSelector);
 
-  const windowRef = apiRef.current.windowRef?.current;
-  const verticalScrollbarSize = windowRef ? windowRef.offsetWidth - windowRef.clientWidth : 0;
-  const horizontalScrollbarSize = windowRef ? windowRef.offsetHeight - windowRef.clientHeight : 0;
+  const getOverlayPosition = React.useCallback((): React.CSSProperties | null => {
+    const dimensions = apiRef.current.getRootDimensions();
+    if (!dimensions) {
+      return null;
+    }
+
+    return {
+      height: dimensions.viewportInnerSize.height,
+      width: dimensions.viewportInnerSize.width,
+      bottom: 0,
+      left: 0,
+    };
+  }, [apiRef]);
+
+  const [position, setPosition] = React.useState(() => getOverlayPosition());
+
+  const handleViewportSizeChange = React.useCallback(
+    () => setPosition(getOverlayPosition()),
+    [getOverlayPosition],
+  );
+
+  useGridApiEventHandler(apiRef, GridEvents.viewportInnerSizeChange, handleViewportSizeChange);
 
   return (
     <GridOverlayRoot
       ref={ref}
       className={clsx(classes.root, className)}
       style={{
-        top: headerHeight,
-        right: verticalScrollbarSize,
-        bottom: horizontalScrollbarSize,
+        ...position,
         ...style,
       }}
       {...other}
