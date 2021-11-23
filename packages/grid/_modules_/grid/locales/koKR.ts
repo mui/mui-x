53a82d229c8e445f6bd3c59f96455c09df57bb9f
zzossig <zzossig@gmail.com>
2021-11-24 08:54:34 +0900

    [l10n] Improve ko_KR locale

--- packages/grid/_modules_/grid/locales/koKR.ts
+++ packages/grid/_modules_/grid/locales/koKR.ts
@@ -30,7 +30,7 @@ const koKRGrid: Partial<GridLocaleText> = {
   toolbarExport: '내보내기',
   toolbarExportLabel: '내보내기',
   toolbarExportCSV: 'CSV다운로드',
-  // toolbarExportPrint: 'Print',
+  toolbarExportPrint: '프린트',
 
   // Columns panel text
   columnsPanelTextFieldLabel: '열 검색',
@@ -93,14 +93,14 @@ const koKRGrid: Partial<GridLocaleText> = {
     `${visibleCount.toLocaleString()} / ${totalCount.toLocaleString()}`,
 
   // Checkbox selection text
-  // checkboxSelectionHeaderName: 'Checkbox selection',
+  checkboxSelectionHeaderName: '선택',
 
   // Boolean cell text
   booleanCellTrueLabel: '참',
   booleanCellFalseLabel: '거짓',
 
   // Actions cell more text
-  // actionsCellMore: 'more',
+  actionsCellMore: '더보기',
 };
 
 export const koKR: Localization = getGridLocalization(koKRGrid, koKRCore);
