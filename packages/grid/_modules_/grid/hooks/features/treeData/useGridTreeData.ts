c44ce9ce4f8a1491a9e70811cc42b2d9d98cfb18
delangle <flaviendelangle@gmail.com>
2021-11-24 17:01:59 +0100

    Fix

--- packages/grid/_modules_/grid/hooks/features/treeData/useGridTreeData.ts
+++ packages/grid/_modules_/grid/hooks/features/treeData/useGridTreeData.ts
@@ -7,19 +7,14 @@ import {
 } from './gridTreeDataGroupColDef';
 import { useGridApiEventHandler } from '../../utils/useGridApiEventHandler';
 import { GridEvents } from '../../../constants';
-import {
-  GridCellParams,
-  GridColDef,
-  GridColDefOverrideParams,
-  GridColumns,
-  MuiEvent,
-} from '../../../models';
+import { GridCellParams, GridColDef, GridColDefOverrideParams, MuiEvent } from '../../../models';
 import { isSpaceKey } from '../../../utils/keyboardUtils';
 import { useFirstRender } from '../../utils/useFirstRender';
 import { buildRowTree } from '../../../utils/rowTreeUtils';
 import { GridRowGroupingPreProcessing } from '../../core/rowGroupsPerProcessing';
 import { gridFilteredDescendantCountLookupSelector } from '../filter';
 import { GridPreProcessingGroup, useGridRegisterPreProcessor } from '../../core/preProcessing';
+import { GridColumnsRawState } from '../columns/gridColumnsState';
 
 /**
  * Only available in DataGridPro
@@ -59,15 +54,25 @@ export const useGridTreeData = (
     };
   }, [apiRef, props.groupingColDef]);
 
-  const addGroupingColumn = React.useCallback(
-    (columns: GridColumns) => {
-      if (!props.treeData) {
-        return columns;
+  const updateGroupingColumn = React.useCallback(
+    (columnsState: GridColumnsRawState) => {
+      const shouldHaveGroupingColumn = props.treeData;
+      const haveGroupingColumn = columnsState.lookup[groupingColDef.field] != null;
+      const index = columnsState.all[0] === '__check__' ? 1 : 0;
+
+      if (shouldHaveGroupingColumn && !haveGroupingColumn) {
+        columnsState.lookup[groupingColDef.field] = groupingColDef;
+        columnsState.all = [
+          ...columnsState.all.slice(0, index),
+          groupingColDef.field,
+          ...columnsState.all.slice(index),
+        ];
+      } else if (!shouldHaveGroupingColumn && haveGroupingColumn) {
+        delete columnsState.lookup[groupingColDef.field];
+        columnsState.all = columnsState.all.filter((field) => field !== groupingColDef.field);
       }
 
-      const index = columns[0]?.field === '__check__' ? 1 : 0;
-
-      return [...columns.slice(0, index), groupingColDef, ...columns.slice(index)];
+      return columnsState;
     },
     [props.treeData, groupingColDef],
   );
@@ -113,7 +118,7 @@ export const useGridTreeData = (
     updateRowGrouping();
   }, [updateRowGrouping]);
 
-  useGridRegisterPreProcessor(apiRef, GridPreProcessingGroup.hydrateColumns, addGroupingColumn);
+  useGridRegisterPreProcessor(apiRef, GridPreProcessingGroup.hydrateColumns, updateGroupingColumn);
 
   const handleCellKeyDown = React.useCallback(
     (params: GridCellParams, event: MuiEvent<React.KeyboardEvent>) => {
