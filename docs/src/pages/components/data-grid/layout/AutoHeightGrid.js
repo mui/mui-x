570b58dc952f13bd49328cdb064d6d341c2a5b3b
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2021-11-30 14:59:33 +0100

    [docs] Allows to experiment `autoHeight` behavior (#3216)

--- docs/src/pages/components/data-grid/layout/AutoHeightGrid.js
+++ docs/src/pages/components/data-grid/layout/AutoHeightGrid.js
@@ -1,18 +1,28 @@
 import * as React from 'react';
+import Button from '@mui/material/Button';
 import { DataGrid } from '@mui/x-data-grid';
 import { useDemoData } from '@mui/x-data-grid-generator';
 
 export default function AutoHeightGrid() {
+  const [nbRows, setNbRows] = React.useState(5);
+  const removeRow = () => setNbRows((x) => Math.max(0, x - 1));
+  const addRow = () => setNbRows((x) => Math.min(100, x + 1));
+
   const { data } = useDemoData({
     dataSet: 'Commodity',
-    rowLength: 4,
+    rowLength: 100,
     maxColumns: 6,
   });
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid autoHeight {...data} />
-      <p>more content</p>
+    <div style={{ width: '100%' }}>
+      <Button variant="outlined" onClick={removeRow}>
+        Remove a row
+      </Button>
+      <Button variant="outlined" onClick={addRow}>
+        Add a row
+      </Button>
+      <DataGrid autoHeight {...data} rows={data.rows.slice(0, nbRows)} />
     </div>
   );
 }
