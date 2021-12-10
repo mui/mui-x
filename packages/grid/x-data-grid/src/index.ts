914ef44bccd32d1a5cc3e25ce60f7dea325f5010
delangle <flaviendelangle@gmail.com>
2021-12-10 17:34:55 +0100

    Fix

--- packages/grid/x-data-grid/src/index.ts
+++ packages/grid/x-data-grid/src/index.ts
@@ -2,3 +2,4 @@ export * from '../../_modules_';
 export * from './DataGrid';
 export type { DataGridProps } from '../../_modules_/grid/models/props/DataGridProps';
 export * from './useDataGridComponent';
+export { MAX_PAGE_SIZE } from './useDataGridProps';
