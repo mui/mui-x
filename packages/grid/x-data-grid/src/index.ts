b63cd69bc5ca349ad0505abbf4b33901b4dd8fdb
delangle <flaviendelangle@gmail.com>
2021-12-10 17:33:57 +0100

    Fix

--- packages/grid/x-data-grid/src/index.ts
+++ packages/grid/x-data-grid/src/index.ts
@@ -1,4 +1,4 @@
 export * from '../../_modules_';
 export * from './DataGrid';
-export * from '../../_modules_/grid/models/props/DataGridProps';
+export type { DataGridProps } from '../../_modules_/grid/models/props/DataGridProps';
 export * from './useDataGridComponent';
