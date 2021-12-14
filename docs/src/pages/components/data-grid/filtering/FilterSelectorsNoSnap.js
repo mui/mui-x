5cd69189331d7f4bea9273237506be9c874245fd
delangle <flaviendelangle@gmail.com>
2021-12-14 13:55:00 +0100

    Work

--- docs/src/pages/components/data-grid/filtering/FilterSelectorsNoSnap.js
+++ docs/src/pages/components/data-grid/filtering/FilterSelectorsNoSnap.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import SelectorsDocs from 'docsx/src/modules/components/SelectorsDocs';
-import selectors from 'docsx/pages/api-docs/data-grid/selectors.json';
 
 export default function FilterSelectorsNoSnap() {
-  return (
-    <SelectorsDocs
-      selectors={selectors.filter((selector) => selector.category === 'Filtering')}
-    />
-  );
+  return <SelectorsDocs category="Filtering" />;
 }
--- docs/src/pages/components/data-grid/sorting/SortingSelectorsNoSnap.js
+++ docs/src/pages/components/data-grid/sorting/SortingSelectorsNoSnap.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import SelectorsDocs from 'docsx/src/modules/components/SelectorsDocs';
-import selectors from 'docsx/pages/api-docs/data-grid/selectors.json';
 
 export default function SortingSelectorsNoSnap() {
-  return (
-    <SelectorsDocs
-      selectors={selectors.filter((selector) => selector.category === 'Sorting')}
-    />
-  );
+  return <SelectorsDocs category="Sorting" />;
 }
