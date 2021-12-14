32593abeed9630825e8391a53cf91d70c1dcca45
delangle <flaviendelangle@gmail.com>
2021-12-14 17:38:43 +0100

    Work

--- packages/grid/_modules_/grid/hooks/features/sorting/gridSortingUtils.ts
+++ packages/grid/_modules_/grid/hooks/features/sorting/gridSortingUtils.ts
@@ -1,11 +1,11 @@
 import { GridSortingModelApplier } from './gridSortingState';
 import {
-  GridApiRef,
-  GridComparatorFn,
-  GridRowId,
-  GridRowTreeNodeConfig,
-  GridSortCellParams,
-  GridSortModel,
+    GridApiRef,
+    GridComparatorFn,
+    GridRowId,
+    GridRowTreeNodeConfig,
+    GridSortCellParams, GridSortItem,
+    GridSortModel,
 } from '../../../models';
 import { isDesc } from '../../../utils/sortingUtils';
 
@@ -14,60 +14,81 @@ type GridSortingFieldComparator = {
   comparator: GridComparatorFn;
 };
 
+interface GridParsedSortItem {
+    comparator: GridComparatorFn;
+    getSortCellParams: (id: GridRowId) => GridSortCellParams
+}
+
+/**
+ * Transform an item of the sorting model into a method comparing two rows.
+ * @param {GridSortItem} sortItem The sort item we want to apply.
+ * @param {GridApiRef} apiRef The API of the grid.
+ * @returns {GridParsedSortItem | null} The parsed sort item. Returns `null` is the sort item is not valid.
+ */
+const parseSortItem = (sortItem: GridSortItem, apiRef: GridApiRef): GridParsedSortItem | null => {
+    const column = apiRef.current.getColumn(sortItem.field);
+    if (!column) {
+        return null;
+    }
+
+    const comparator: GridComparatorFn = isDesc(sortItem.sort)
+        ? (...args) => -1 * column.sortComparator!(...args)
+        : column.sortComparator!;
+
+    const getSortCellParams = (id: GridRowId): GridSortCellParams => ({
+        id,
+        field: column.field,
+        value: apiRef.current.getCellValue(id, column.field),
+        api: apiRef.current,
+    });
+
+    return { getSortCellParams, comparator };
+}
+
+/**
+ * Compare two rows according to a list of valid sort items.
+ * The `row1Params` and `row2Params` must have the same length as `parsedSortItems`,
+ * and each of their index must contain the `GridSortCellParams` of the sort item with the same index.
+ * @param {GridParsedSortItem[]} parsedSortItems All the sort items with which we want to compare the rows.
+ * @param {GridSortCellParams} row1Params The params of the 1st row for each sort item.
+ * @param {GridSortCellParams} row2Params The params of the 2nd row for each sort item.
+ */
+const compareRows = (parsedSortItems: GridParsedSortItem[], row1Params: GridSortCellParams[], row2Params: GridSortCellParams[]) => {
+    return parsedSortItems.reduce((res, item, index) => {
+        if (res !== 0) {
+            // return the results of the first comparator which distinguish the two rows
+            return res;
+        }
+
+        const sortCellParams1 = row1Params[index];
+        const sortCellParams2 = row2Params[index];
+        res = item.comparator(
+            sortCellParams1.value,
+            sortCellParams2.value,
+            sortCellParams1,
+            sortCellParams2,
+        );
+        return res;
+    }, 0);
+};
+
 /**
  * Generates a method to easily sort a list of rows according to the current sort model.
  * @param {GridSortModel} sortModel The model with which we want to sort the rows.
  * @param {GridApiRef} apiRef The API of the grid.
- * @returns {GridSortingModelApplier | null} A method that sorts a list of rows according to the current sort model. If `null`, we consider that the rows should remain in the order there were provided.
+ * @returns {GridSortingModelApplier | null} A method that generates a list of sorted row ids from a list of rows according to the current sort model. If `null`, we consider that the rows should remain in the order there were provided.
  */
 export const buildAggregatedSortingApplier = (
   sortModel: GridSortModel,
   apiRef: GridApiRef,
 ): GridSortingModelApplier | null => {
   const comparatorList = sortModel
-    .map((item) => {
-      const column = apiRef.current.getColumn(item.field);
-      if (!column) {
-        return null;
-      }
-
-      const comparator: GridComparatorFn = isDesc(item.sort)
-        ? (...args) => -1 * column.sortComparator!(...args)
-        : column.sortComparator!;
-
-      const getSortCellParams = (id: GridRowId): GridSortCellParams => ({
-        id,
-        field: column.field,
-        value: apiRef.current.getCellValue(id, column.field),
-        api: apiRef.current,
-      });
-
-      return { getSortCellParams, comparator };
-    })
+    .map(item => parseSortItem(item, apiRef))
     .filter((comparator): comparator is GridSortingFieldComparator => !!comparator);
 
   if (comparatorList.length === 0) {
     return null;
   }
-
-  const aggregatedComparator = (row1: GridSortCellParams[], row2: GridSortCellParams[]) => {
-    return comparatorList.reduce((res, colComparator, index) => {
-      if (res !== 0) {
-        return res;
-      }
-
-      const { comparator } = colComparator;
-      const sortCellParams1 = row1[index];
-      const sortCellParams2 = row2[index];
-      res = comparator(
-        sortCellParams1.value,
-        sortCellParams2.value,
-        sortCellParams1,
-        sortCellParams2,
-      );
-      return res;
-    }, 0);
-  };
 
   return (rowList: GridRowTreeNodeConfig[]) =>
     rowList
@@ -75,6 +96,6 @@ export const buildAggregatedSortingApplier = (
         value,
         params: comparatorList.map((el) => el.getSortCellParams(value.id)),
       }))
-      .sort((a, b) => aggregatedComparator(a.params, b.params))
+      .sort((a, b) => compareRows(comparatorList, a.params, b.params))
       .map((row) => row.value.id);
 };
