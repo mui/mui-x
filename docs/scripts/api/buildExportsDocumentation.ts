44538ee27473763f35a183c191511c9f4c42f2b4
delangle <flaviendelangle@gmail.com>
2021-12-14 09:15:31 +0100

    [core] Generate exports snapshot for both x-data-grid and x-data-grid-pro packages

--- docs/scripts/api/buildExportsDocumentation.ts
+++ docs/scripts/api/buildExportsDocumentation.ts
@@ -3,29 +3,36 @@ import * as ts from 'typescript';
 import { Project, writePrettifiedFile } from './utils';
 
 interface BuildExportsDocumentationOptions {
-  project: Project;
+  dataGridProject: Project;
+  dataGridProProject: Project;
 }
 
-export default function buildExportsDocumentation(options: BuildExportsDocumentationOptions) {
-  const { project } = options;
-
+const buildPackageExports = (project: Project) => {
   const syntaxKindToSyntaxName = {};
   Object.entries(ts.SyntaxKind).forEach(([syntaxName, syntaxKind]) => {
     syntaxKindToSyntaxName[syntaxKind] = syntaxName.replace('Declaration', '');
   });
 
   const exports = Object.entries(project.exports)
-    .map(([name, symbol]) => {
-      return {
-        name,
-        kind: syntaxKindToSyntaxName[symbol.declarations?.[0].kind!],
-      };
-    })
-    .sort((a, b) => a.name.localeCompare(b.name));
+      .map(([name, symbol]) => {
+        return {
+          name,
+          kind: syntaxKindToSyntaxName[symbol.declarations?.[0].kind!],
+        };
+      })
+      .sort((a, b) => a.name.localeCompare(b.name));
 
   writePrettifiedFile(
-    path.resolve(project.workspaceRoot, 'scripts/exportsSnapshot.json'),
-    JSON.stringify(exports),
-    project,
+      path.resolve(project.workspaceRoot, `scripts/${project.name}.exports.json`),
+      JSON.stringify(exports),
+      project,
   );
+}
+
+export default function buildExportsDocumentation(options: BuildExportsDocumentationOptions) {
+  const { dataGridProject, dataGridProProject } = options;
+
+
+  buildPackageExports(dataGridProject)
+  buildPackageExports(dataGridProProject)
 }
