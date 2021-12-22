129e93927dd08ddc41448ecac4233c0bb1352209
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-12-22 17:05:12 +0100

    add doc

---
+++ docs/src/pages/components/data-grid/filtering/CustomFilterPanel.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { DataGridPro, GridLinkOperator } from '@mui/x-data-grid-pro';
+import { useDemoData } from '@mui/x-data-grid-generator';
+
+export default function DisableSortingGrid() {
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 10,
+    maxColumns: 6,
+  });
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGridPro
+        {...data}
+        // components={{
+        //   FilterPanel: MyCustomFilterPanel,
+        // }}
+        componentsProps={{
+          filterPanel: {
+            linkOperators: [GridLinkOperator.And],
+            columnsSort: 'asc',
+            deleteIconContainerSx: { display: 'none' },
+            valueContainerSx: { width: 200 },
+            // linkOperatorContainerSx: {},
+            // columnContainerSx: {},
+            // operatorContainerSx: {}
+          },
+        }}
+      />
+    </div>
+  );
+}
