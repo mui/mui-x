0cdaf385ffc11230fce7502d14aedbb9bd9fa61c
delangle <flaviendelangle@gmail.com>
2022-01-25 14:12:55 +0100

    Fix

--- packages/grid/_modules_/grid/models/api/gridApiRef.ts
+++ packages/grid/_modules_/grid/models/api/gridApiRef.ts
@@ -2,12 +2,18 @@ import * as React from 'react';
 import { GridApiCommon, GridApiCommunity, GridApiPro } from './gridApi';
 
 /**
- * The apiRef component prop type.
+ * The reference storing the api of the grid.
  * TODO v6: Make the `GridApi` generic mandatory. This interface should only be used when accessing dynamically api methods or state based on the plan.
  */
 export type GridApiRef<GridApi extends GridApiCommon = GridApiCommunity> =
   React.MutableRefObject<GridApi>;
 
+/**
+ * The reference storing the api of the Community-plan grid.
+ */
 export type GridApiRefCommunity = React.MutableRefObject<GridApiCommunity>;
 
+/**
+ * The reference storing the api of the Pro-plan grid.
+ */
 export type GridApiRefPro = React.MutableRefObject<GridApiPro>;
