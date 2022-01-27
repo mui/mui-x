876eac13f120745f3b3f9fc178abc0412085c7a9
delangle <flaviendelangle@gmail.com>
2022-01-27 12:59:52 +0100

    Work

--- packages/grid/_modules_/grid/components/base/GridHeaderPlaceholder.tsx
+++ packages/grid/_modules_/grid/components/base/GridHeaderPlaceholder.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { useGridApiContext } from '../../hooks/utils/useGridApiContext';
+import { useGridRootProps } from '../../hooks/utils/useGridRootProps';
 
 export function GridHeaderPlaceholder() {
   const apiRef = useGridApiContext();
+  const rootProps = useGridRootProps();
   const headerRef = React.useRef<HTMLDivElement>(null);
   apiRef.current.headerRef = headerRef;
+
+  console.log(rootProps);
 
   return <div ref={headerRef}>TEST</div>;
 }
