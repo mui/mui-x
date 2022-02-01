5c104eb2f35a03f6372b64f4adb698a4ac67dee9
siriwatknp <siriwatkunaporn@gmail.com>
2022-02-01 15:22:49 +0700

    Revert "run migration"
    This reverts commit a94eddf3df2d70a2de6f49a79e1a7908e5c8aa02.

--- docs/pages/components/data-grid/accessibility.js
+++ docs/pages/components/data-grid/accessibility.js
@@ -4,7 +4,7 @@ import {
   demos,
   docs,
   demoComponents,
-} from 'docsx/data/data-grid/accessibility/accessibility.md?@mui/markdown';
+} from 'docsx/src/pages/components/data-grid/accessibility/accessibility.md?@mui/markdown';
 
 export default function Page() {
   return <MarkdownDocs demos={demos} docs={docs} demoComponents={demoComponents} />;
