5d84e80a059827b92f54f60890b7f2413c5fd50d
Olivier Tassinari <olivier.tassinari@gmail.com>
2022-02-07 22:52:10 +0100

    [docs] Clarify a bit the migration to v5 (#3847)

--- docs/src/pages/components/data-grid/getting-started/CoreV5WithCoreV4.js
+++ docs/src/pages/components/data-grid/getting-started/CoreV5WithCoreV4.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
-export default function DataGridV5WithCoreV4() {
+export default function CoreV5WithCoreV4() {
   return (
     <iframe
       title="codesandbox"
-      src="https://codesandbox.io/embed/datagrid-v5-with-mui-core-v4-1pmwj?hidenavigation=1&fontsize=14&view=preview"
+      src="https://codesandbox.io/embed/mui-core-v5-with-mui-core-v4-1pmwj?hidenavigation=1&fontsize=14&view=preview"
       style={{
         width: '100%',
-        height: 350,
+        height: 520,
         border: 0,
       }}
       sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
