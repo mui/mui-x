86a8e7f4ecfe333c9333d10de2c5ce30ed32e4c4
siriwatknp <siriwatkunaporn@gmail.com>
2022-02-07 09:54:24 +0700

    Revert "run migration"
    This reverts commit 0e643412ba35c735f4b1d00a867e2e9f7fdfc5f9.

--- docs/data/data-grid/group-pivot/DetailPanelApiNoSnap.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ApiDocs from 'docsx/src/modules/components/ApiDocs';
-import api from 'docsx/pages/x/api/data-grid/grid-detail-panel-api.json';
-
-export default function DetailPanelApiNoSnap() {
-  return <ApiDocs api={api} />;
-}
