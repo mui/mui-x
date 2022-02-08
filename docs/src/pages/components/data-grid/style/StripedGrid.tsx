e6dd0dfa954276dfb3d467556bc3bc6bc710dbd6
flavien <flaviendelangle@gmail.com>
2022-02-08 09:42:19 +0100

    Fix

--- docs/src/pages/components/data-grid/style/StripedGrid.tsx
+++ docs/src/pages/components/data-grid/style/StripedGrid.tsx
@@ -1,27 +1,50 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import { alpha, styled } from '@mui/material/styles';
 import { DataGrid, GridRowProps, GridRow, gridClasses } from '@mui/x-data-grid';
 import { useDemoData } from '@mui/x-data-grid-generator';
 import clsx from 'clsx';
 
-const StripedDataGrid = styled(DataGrid)({
-  [`& .${gridClasses.row}.odd`]: {
+const ODD_OPACITY = 0.2;
+
+const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
+  [`& .${gridClasses.row}.even`]: {
     backgroundColor: '#EEEEEE',
     '&:hover, &.Mui-hovered': {
-      backgroundColor: '#DDDDDD',
+      backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY),
       '@media (hover: none)': {
         backgroundColor: 'transparent',
       },
     },
+    '&.Mui-selected': {
+      backgroundColor: alpha(
+        theme.palette.primary.main,
+        ODD_OPACITY + theme.palette.action.selectedOpacity,
+      ),
+      '&:hover, &.Mui-hovered': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          ODD_OPACITY +
+            theme.palette.action.selectedOpacity +
+            theme.palette.action.hoverOpacity,
+        ),
+        // Reset on touch devices, it doesn't add specificity
+        '@media (hover: none)': {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            ODD_OPACITY + theme.palette.action.selectedOpacity,
+          ),
+        },
+      },
+    },
   },
-});
+}));
 
 const CustomRow = (props: GridRowProps) => (
   <GridRow
     {...props}
     className={clsx(
       props.className,
-      props.indexes.pageRows % 2 === 1 ? 'odd' : undefined,
+      props.indexes.fromPageRows % 2 === 0 ? 'even' : undefined,
     )}
   />
 );
