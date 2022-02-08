74af52a3f9c39faa90ddedcff17b6f8d4f8daf57
Siriwat K <siriwatkunaporn@gmail.com>
2022-02-08 19:18:13 +0700

    [docs] Migrate content to the new location (#3730)

---
+++ docs/data/data-grid/filtering/FilterApiNoSnap.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ApiDocs from 'docsx/src/modules/components/ApiDocs';
+import api from 'docsx/pages/x/api/data-grid/grid-filter-api.json';
+
+export default function FilterApiNoSnap() {
+  return <ApiDocs api={api} />;
+}
---
+++ docs/data/data-grid/group-pivot/DetailPanelApiNoSnap.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ApiDocs from 'docsx/src/modules/components/ApiDocs';
+import api from 'docsx/pages/x/api/data-grid/grid-detail-panel-api.json';
+
+export default function DetailPanelApiNoSnap() {
+  return <ApiDocs api={api} />;
+}
