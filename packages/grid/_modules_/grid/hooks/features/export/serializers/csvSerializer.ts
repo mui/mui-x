7fb7aa6b3e3619ccae9db5281e0773bf5dd94c5b
Danail Hadjiatanasov <hadjiatanasov@gmail.com>
2022-02-11 11:37:06 +0100

    Update packages/grid/_modules_/grid/hooks/features/export/serializers/csvSerializer.ts
    Co-authored-by: Matheus Wichman <matheushw@outlook.com>

--- packages/grid/_modules_/grid/hooks/features/export/serializers/csvSerializer.ts
+++ packages/grid/_modules_/grid/hooks/features/export/serializers/csvSerializer.ts
@@ -35,7 +35,7 @@ const serialiseRow = (
       if (!warnedOnce && String(cellParams.formattedValue) === '[object Object]') {
         console.warn(
           [
-            'MUI: When value of a field is an object or a `renderCell` is provided, the CSV export might not display the value correctly.',
+            'MUI: When the value of a field is an object or a `renderCell` is provided, the CSV export might not display the value correctly.',
             'You can provide a `valueFormatter` with a string representation to be used.',
           ].join('\n'),
         );
