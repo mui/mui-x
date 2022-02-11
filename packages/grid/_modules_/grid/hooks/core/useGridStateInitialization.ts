4efedb58957676891aee73b9bc4637421a083eca
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-02-11 14:10:12 +0100

    clarify error message

--- packages/grid/_modules_/grid/hooks/core/useGridStateInitialization.ts
+++ packages/grid/_modules_/grid/hooks/core/useGridStateInitialization.ts
@@ -69,11 +69,11 @@ export const useGridStateInitialization = (apiRef: GridApiRef, props: DataGridPr
         // Events are here to forward to other hooks and apply changes.
         // You are trying to update several states in a no isolated way.
         throw new Error(
-          `You're not allowed to update several sub-state in one transaction. You already updated ${
-            updatedControlStateIds[0]
-          }, therefore, you're not allowed to update ${updatedControlStateIds.join(
-            ', ',
-          )} in the same transaction.`,
+          `You're not allowed to update several sub-state in one transaction. You already updated ${JSON.stringify(
+            updatedControlStateIds[0],
+          )}, therefore, you're not allowed to update ${updatedControlStateIds
+            .map((x) => JSON.stringify(x))
+            .join(', ')} in the same transaction.`,
         );
       }
 
