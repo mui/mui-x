f716d18c46399e7f308f3919ef0b4ebc7f05bc96
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-03-01 14:03:19 +0100

    prettier

--- packages/grid/x-data-grid/src/hooks/features/export/utils.ts
+++ packages/grid/x-data-grid/src/hooks/features/export/utils.ts
@@ -4,25 +4,25 @@ import { GridExportOptions } from '../../../models/gridExport';
 import { GridStateColDef } from '../../../models/colDef/gridColDef';
 
 interface GridGetColumnsToExportParams {
-    /**
-     * The API of the grid.
-     */
-    apiRef: React.MutableRefObject<GridApiCommunity>;
-    options: GridExportOptions;
+  /**
+   * The API of the grid.
+   */
+  apiRef: React.MutableRefObject<GridApiCommunity>;
+  options: GridExportOptions;
 }
 
 export const getColumnsToExport = ({
-    apiRef,
-    options,
+  apiRef,
+  options,
 }: GridGetColumnsToExportParams): GridStateColDef[] => {
-    const columns = gridColumnDefinitionsSelector(apiRef);
+  const columns = gridColumnDefinitionsSelector(apiRef);
 
-    if (options.fields) {
-        return options.fields
-            .map((field) => columns.find((column) => column.field === field))
-            .filter((column): column is GridStateColDef => !!column);
-    }
+  if (options.fields) {
+    return options.fields
+      .map((field) => columns.find((column) => column.field === field))
+      .filter((column): column is GridStateColDef => !!column);
+  }
 
-    const validColumns = options.allColumns ? columns : gridVisibleColumnDefinitionsSelector(apiRef);
-    return validColumns.filter((column) => !column.disableExport);
+  const validColumns = options.allColumns ? columns : gridVisibleColumnDefinitionsSelector(apiRef);
+  return validColumns.filter((column) => !column.disableExport);
 };
