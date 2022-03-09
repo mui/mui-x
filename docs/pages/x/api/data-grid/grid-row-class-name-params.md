1ac64ba17a25565d3e186cd80b04218f41b84be7
Matheus Wichman <matheushw@outlook.com>
2022-03-09 15:12:56 -0300

    [DataGrid] Add support for margin between rows (#3848)

---
+++ docs/pages/x/api/data-grid/grid-row-class-name-params.md
@@ -0,0 +1,22 @@
+# GridRowClassNameParams Interface
+
+<p class="description">Object passed as parameter in the row `getRowClassName` callback prop.</p>
+
+## Import
+
+```js
+import { GridRowClassNameParams } from '@mui/x-data-grid-pro';
+// or
+import { GridRowClassNameParams } from '@mui/x-data-grid';
+```
+
+## Properties
+
+| Name                                          | Type                                                                              | Description                                                |
+| :-------------------------------------------- | :-------------------------------------------------------------------------------- | :--------------------------------------------------------- |
+| <span class="prop-name">columns</span>        | <span class="prop-type">GridColumns</span>                                        | All grid columns.                                          |
+| <span class="prop-name">getValue</span>       | <span class="prop-type">(id: GridRowId, field: string) =&gt; GridCellValue</span> | Get the cell value of a row and field.                     |
+| <span class="prop-name">id</span>             | <span class="prop-type">GridRowId</span>                                          | The grid row id.                                           |
+| <span class="prop-name">isFirstVisible</span> | <span class="prop-type">boolean</span>                                            | Whether this row is the first visible or not.              |
+| <span class="prop-name">isLastVisible</span>  | <span class="prop-type">boolean</span>                                            | Whether this row is the last visible or not.               |
+| <span class="prop-name">row</span>            | <span class="prop-type">R</span>                                                  | The row model of the row that the current cell belongs to. |
--- packages/grid/x-data-grid/src/components/GridRow.tsx
+++ packages/grid/x-data-grid/src/components/GridRow.tsx
@@ -20,6 +20,8 @@ import { GridStateColDef } from '../models/colDef/gridColDef';
 import { GridCellIdentifier } from '../hooks/features/focus/gridFocusState';
 import { gridColumnsTotalWidthSelector } from '../hooks/features/columns/gridColumnsSelector';
 import { useGridSelector } from '../hooks/utils/useGridSelector';
+import { GridRowClassNameParams } from '../models/params/gridRowParams';
+import { useCurrentPageRows } from '../hooks/utils/useCurrentPageRows';
 import { findParentElementFromClassName } from '../utils/domUtils';
 import { GRID_CHECKBOX_SELECTION_COL_DEF } from '../colDef/gridCheckboxSelectionColDef';
 import { GRID_ACTIONS_COLUMN_TYPE } from '../colDef/gridActionsColDef';
@@ -104,6 +106,7 @@ function GridRow(props: React.HTMLAttributes<HTMLDivElement> & GridRowProps) {
   const ariaRowIndex = index + 2; // 1 for the header row and 1 as it's 1-based
   const apiRef = useGridApiContext();
   const rootProps = useGridRootProps();
+  const currentPage = useCurrentPageRows(apiRef, rootProps);
   const columnsTotalWidth = useGridSelector(apiRef, gridColumnsTotalWidthSelector);
   const { hasScrollX, hasScrollY } = apiRef.current.getRootDimensions() ?? {
     hasScrollX: false,
@@ -186,14 +189,35 @@ function GridRow(props: React.HTMLAttributes<HTMLDivElement> & GridRowProps) {
   );
 
   const style = {
+    ...styleProp,
     maxHeight: rowHeight,
     minHeight: rowHeight,
-    ...styleProp,
   };
 
-  const rowClassName =
-    typeof rootProps.getRowClassName === 'function' &&
-    rootProps.getRowClassName(apiRef.current.getRowParams(rowId));
+  const sizes = apiRef.current.unstable_getRowInternalSizes(rowId);
+
+  if (sizes?.spacingTop) {
+    const property = rootProps.rowSpacingType === 'border' ? 'borderTopWidth' : 'marginTop';
+    style[property] = sizes.spacingTop;
+  }
+
+  if (sizes?.spacingBottom) {
+    const property = rootProps.rowSpacingType === 'border' ? 'borderBottomWidth' : 'marginBottom';
+    style[property] = sizes.spacingBottom;
+  }
+
+  let rowClassName: string | null = null;
+
+  if (typeof rootProps.getRowClassName === 'function') {
+    const indexRelativeToCurrentPage = index - currentPage.range!.firstRowIndex;
+    const rowParams: GridRowClassNameParams = {
+      ...apiRef.current.getRowParams(rowId),
+      isFirstVisible: indexRelativeToCurrentPage === 0,
+      isLastVisible: indexRelativeToCurrentPage === currentPage.rows.length - 1,
+    };
+
+    rowClassName = rootProps.getRowClassName(rowParams);
+  }
 
   const cells: JSX.Element[] = [];
 
