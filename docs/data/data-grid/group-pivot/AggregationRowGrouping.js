d0fa7c87a10c3568495fb44a082df6aeef746397
delangle <flaviendelangle@gmail.com>
2022-03-17 14:49:20 +0100

    Improve example

--- docs/data/data-grid/group-pivot/AggregationRowGrouping.js
+++ docs/data/data-grid/group-pivot/AggregationRowGrouping.js
@@ -1,102 +1,39 @@
 import { useDemoData } from '@mui/x-data-grid-generator';
 import * as React from 'react';
-import {
-  DataGridPro,
-  gridColumnVisibilityModelSelector,
-  GridEvents,
-  useGridApiRef,
-} from '@mui/x-data-grid-pro';
+import { DataGridPro, useGridApiRef } from '@mui/x-data-grid-pro';
 
-const INITIAL_GROUPING_COLUMN_MODEL = ['commodity'];
-
-const useKeepGroupingColumnsHidden = (apiRef, columns, initialModel, leafField) => {
-  const prevModel = React.useRef(initialModel);
-
-  React.useEffect(() => {
-    apiRef.current.subscribeEvent(GridEvents.rowGroupingModelChange, (newModel) => {
-      const columnVisibilityModel = {
-        ...gridColumnVisibilityModelSelector(apiRef),
-      };
-
-      newModel.forEach((field) => {
-        if (!prevModel.current.includes(field)) {
-          columnVisibilityModel[field] = false;
-        }
-      });
-      prevModel.current.forEach((field) => {
-        if (!newModel.includes(field)) {
-          columnVisibilityModel[field] = true;
-        }
-      });
-      apiRef.current.setColumnVisibilityModel(columnVisibilityModel);
-      prevModel.current = newModel;
-    });
-  }, [apiRef]);
-
-  return React.useMemo(
-    () =>
-      columns.map((colDef) =>
-        initialModel.includes(colDef.field) ||
-        (leafField && colDef.field === leafField)
-          ? { ...colDef, hide: true }
-          : colDef,
-      ),
-    [columns, initialModel, leafField],
-  );
-};
+const VISIBLE_FIELDS = ['quantity', 'filledQuantity', 'totalPrice'];
 
 export default function RowGroupingFullExample() {
   const { data, loading } = useDemoData({
     dataSet: 'Commodity',
     rowLength: 100,
     maxColumns: 25,
-    visibleFields: ['traderEmail', 'quantity', 'filledQuantity', 'totalPrice'],
+    visibleFields: VISIBLE_FIELDS,
   });
 
   const apiRef = useGridApiRef();
-
-  const columns = useKeepGroupingColumnsHidden(
-    apiRef,
-    data.columns,
-    INITIAL_GROUPING_COLUMN_MODEL,
-  );
 
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGridPro
         {...data}
         apiRef={apiRef}
-        columns={columns}
         loading={loading}
         disableSelectionOnClick
         aggregationPosition="inline"
         initialState={{
           ...data.initialState,
-          columns: {
-            ...data.initialState?.columns,
-            columnVisibilityModel: {
-              ...data.initialState?.columns?.columnVisibilityModel,
-              ...Object.fromEntries(
-                INITIAL_GROUPING_COLUMN_MODEL.map((field) => [field, false]),
-              ),
-            },
-          },
           rowGrouping: {
-            model: INITIAL_GROUPING_COLUMN_MODEL,
+            model: ['commodity'],
           },
           aggregation: {
             model: {
               quantity: { method: 'sum' },
-              filledQuantity: { method: 'avg' },
+              filledQuantity: { method: 'size' },
               totalPrice: { method: 'max' },
             },
           },
-          sorting: {
-            sortModel: [{ field: '__row_group_by_columns_group__', sort: 'asc' }],
-          },
-        }}
-        groupingColDef={{
-          leafField: 'traderEmail',
         }}
         experimentalFeatures={{
           rowGrouping: true,
