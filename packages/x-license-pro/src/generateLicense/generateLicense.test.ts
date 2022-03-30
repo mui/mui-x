1212c1699de842b2ef2920e11e97b61badc8a794
Danail H <hadjiatanasov@gmail.com>
2022-03-30 16:26:01 +0200

    Update pro license to handle premium as well

--- packages/x-license-pro/src/generateLicense/generateLicense.test.ts
+++ packages/x-license-pro/src/generateLicense/generateLicense.test.ts
@@ -1,12 +1,32 @@
 import { expect } from 'chai';
-import { generateLicence } from './generateLicense';
+import { generateLicense } from './generateLicense';
 
 describe('License: generateLicense', () => {
-  it('should generate License properly', () => {
+  it('should generate DataGridPro License properly when "type" is not provided', () => {
     expect(
-      generateLicence({ expiryDate: new Date(1591723879062), orderNumber: 'MUI-123' }),
+      generateLicense({ expiryDate: new Date(1591723879062), orderNumber: 'MUI-123' }),
     ).to.equal(
-      'e34253b37166e7a4a85189b91b653e63T1JERVI6TVVJLTEyMyxFWFBJUlk9MTU5MTcyMzg3OTA2MixLRVlWRVJTSU9OPTE=',
+      '821e1299774f9b5de8d7e638c4a40649T1JERVI6TVVJLTEyMyxFWFBJUlk9MTU5MTcyMzg3OTA2MixLRVlWRVJTSU9OPTEsdHlwZT1wcm8=',
+    );
+  });
+
+  it('should generate DataGridPro License properly when `type: "pro"`', () => {
+    expect(
+      generateLicense({ expiryDate: new Date(1591723879062), orderNumber: 'MUI-123', type: 'pro' }),
+    ).to.equal(
+      '821e1299774f9b5de8d7e638c4a40649T1JERVI6TVVJLTEyMyxFWFBJUlk9MTU5MTcyMzg3OTA2MixLRVlWRVJTSU9OPTEsdHlwZT1wcm8=',
+    );
+  });
+
+  it('should generate DataGridPremium License properly when `type: "premium"`', () => {
+    expect(
+      generateLicense({
+        expiryDate: new Date(1591723879062),
+        orderNumber: 'MUI-123',
+        type: 'premium',
+      }),
+    ).to.equal(
+      '8e3a29d6dc4f46c8a9c32980bee6484dT1JERVI6TVVJLTEyMyxFWFBJUlk9MTU5MTcyMzg3OTA2MixLRVlWRVJTSU9OPTEsdHlwZT1wcmVtaXVt',
     );
   });
 });
--- packages/x-license-pro/src/generateLicense/generateLicense.ts
+++ packages/x-license-pro/src/generateLicense/generateLicense.ts
@@ -2,18 +2,29 @@ import { md5 } from '../encoding/md5';
 import { base64Encode } from '../encoding/base64';
 
 const licenseVersion = '1';
+
+export type LicenseType = 'pro' | 'premium';
+
 export interface LicenseDetails {
   orderNumber: string;
   expiryDate: Date;
+  // TODO: to be made required once the store is updated
+  type?: LicenseType;
 }
 
 function getClearLicenseString(details: LicenseDetails) {
   return `ORDER:${
     details.orderNumber
-  },EXPIRY=${details.expiryDate.getTime()},KEYVERSION=${licenseVersion}`;
+  },EXPIRY=${details.expiryDate.getTime()},KEYVERSION=${licenseVersion},TYPE=${details.type}`;
 }
 
-export function generateLicence(details: LicenseDetails) {
-  const clearLicense = getClearLicenseString(details);
+export function generateLicense(details: LicenseDetails) {
+  let clearLicense;
+  if (details.type) {
+    clearLicense = getClearLicenseString(details);
+  } else {
+    clearLicense = getClearLicenseString({ ...details, type: 'pro' });
+  }
+
   return `${md5(base64Encode(clearLicense))}${base64Encode(clearLicense)}`;
 }
