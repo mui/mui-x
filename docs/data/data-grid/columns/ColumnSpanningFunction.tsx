af9f0e39ea1f31c0d3001cd42c8808bfa2f8ae38
Andrew Cherniavskyi <andrew@mui.com>
2022-04-11 13:00:33 +0200

    fix ts errors

--- docs/data/data-grid/columns/ColumnSpanningFunction.tsx
+++ docs/data/data-grid/columns/ColumnSpanningFunction.tsx
@@ -1,17 +1,44 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { DataGridPro } from '@mui/x-data-grid-pro';
+import { DataGridPro, DataGridProProps, GridColDef } from '@mui/x-data-grid-pro';
 
-const rows = [
+const items = [
   { id: 1, item: 'Paperclip', quantity: 100, price: 1.99 },
   { id: 2, item: 'Paper', quantity: 10, price: 30 },
   { id: 3, item: 'Pencil', quantity: 100, price: 1.25 },
+];
+
+type Item = typeof items[number];
+
+interface SubtotalHeader {
+  id: 'SUBTOTAL';
+  label: string;
+  subtotal: 624;
+}
+
+interface TaxHeader {
+  id: 'TAX';
+  label: string;
+  taxRate: number;
+  taxTotal: number;
+}
+
+interface TotalHeader {
+  id: 'TOTAL';
+  label: string;
+  total: number;
+}
+
+type Row = Item | SubtotalHeader | TaxHeader | TotalHeader;
+
+const rows: Row[] = [
+  ...items,
   { id: 'SUBTOTAL', label: 'Subtotal', subtotal: 624 },
   { id: 'TAX', label: 'Tax', taxRate: 10, taxTotal: 62.4 },
   { id: 'TOTAL', label: 'Total', total: 686.4 },
 ];
 
-const columns = [
+const columns: GridColDef<Row>[] = [
   {
     field: 'item',
     headerName: 'Item/Description',
@@ -66,14 +93,14 @@ const columns = [
   },
 ];
 
-function getCellClassName({ row, field }) {
+const getCellClassName: DataGridProProps['getCellClassName'] = ({ row, field }) => {
   if (row.id === 'SUBTOTAL' || row.id === 'TOTAL' || row.id === 'TAX') {
     if (field === 'item') {
       return 'bold';
     }
   }
   return '';
-}
+};
 
 export default function ColumnSpanningFunction() {
   return (
