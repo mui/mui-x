b396c9dfa172528397c331d70a8ba9723104e99c
Danail H <hadjiatanasov@gmail.com>
2022-04-20 16:08:50 +0200

    update demo

--- docs/data/data-grid/rows/RowOrderingGrid.js
+++ docs/data/data-grid/rows/RowOrderingGrid.js
@@ -2,6 +2,17 @@ import * as React from 'react';
 import { DataGridPro } from '@mui/x-data-grid-pro';
 import { useDemoData } from '@mui/x-data-grid-generator';
 
+function updateRowPossition(initialIndex, newIndex, rows) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const rowsClone = [...rows];
+      const row = rowsClone.splice(initialIndex, 1)[0];
+      rowsClone.splice(newIndex, 0, row);
+      resolve(rowsClone);
+    }, Math.random() * 500 + 100); // simulate network latency
+  });
+}
+
 export default function RowOrderingGrid() {
   const { data } = useDemoData({
     dataSet: 'Commodity',
@@ -9,9 +20,34 @@ export default function RowOrderingGrid() {
     maxColumns: 20,
   });
 
+  const [rows, setRows] = React.useState(data.rows);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setRows(data.rows);
+  }, [data]);
+
+  const handleRowOrderChange = async (params) => {
+    setLoading(true);
+    const newRows = await updateRowPossition(
+      params.oldIndex,
+      params.targetIndex,
+      rows,
+    );
+
+    setRows(newRows);
+    setLoading(false);
+  };
+
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGridPro {...data} rowReordering />
+      <DataGridPro
+        loading={loading}
+        rows={rows}
+        columns={data.columns}
+        rowReordering
+        onRowOrderChange={handleRowOrderChange}
+      />
     </div>
   );
 }
