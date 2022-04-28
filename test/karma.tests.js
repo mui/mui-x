aefd2fc9b63bf0537a526ccb702e003691193f72
Matheus Wichman <matheushw@outlook.com>
2022-04-28 11:33:07 -0300

    [test] Reset cleanup tracking on Karma tests (#4679)

--- test/karma.tests.js
+++ test/karma.tests.js
@@ -1,6 +1,13 @@
 /* eslint-env mocha */
 import './utils/init';
 import '@mui/monorepo/test/utils/setupKarma';
+import { unstable_resetCleanupTracking } from '@mui/x-data-grid';
+import { unstable_resetCreateSelectorCache } from '@mui/x-data-grid/internals';
 
 const packagesContext = require.context('../packages', true, /\.test\.tsx$/);
 packagesContext.keys().forEach(packagesContext);
+
+afterEach(function afterEach() {
+  unstable_resetCleanupTracking();
+  unstable_resetCreateSelectorCache();
+});
