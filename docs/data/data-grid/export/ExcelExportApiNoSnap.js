00b36b40243346467844b920220ee5b806a756cd
Matheus Wichman <matheushw@outlook.com>
2022-04-29 17:21:07 -0300

    [docs] Remove remaining pages in `docs/pages/api-docs` folder (#4709)

--- docs/data/data-grid/export/ExcelExportApiNoSnap.js
+++ docs/data/data-grid/export/ExcelExportApiNoSnap.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ApiDocs from 'docsx/src/modules/components/ApiDocs';
-import api from 'docsx/pages/api-docs/data-grid/grid-excel-export-api.json';
+import api from 'docsx/pages/x/api/data-grid/grid-excel-export-api.json';
 
 export default function ExcelExportApiNoSnap() {
   return <ApiDocs api={api} />;
