17cbaaa81f9c2fac15bf1b0da658dd721010390b
Olivier Tassinari <olivier.tassinari@gmail.com>
2022-05-15 01:45:40 +0200

    [core] Small code style edit

--- docs/data/data-grid/filtering/QuickFilteringGrid.js
+++ docs/data/data-grid/filtering/QuickFilteringGrid.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-
 import { useDemoData } from '@mui/x-data-grid-generator';
 
 const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
