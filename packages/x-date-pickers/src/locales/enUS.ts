6840befc70c5e0bbcdf18e33cecb762d74169d86
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-05-20 00:10:20 +0200

    group pickers locales

--- packages/x-date-pickers/src/locales/enUS.ts
+++ packages/x-date-pickers/src/locales/enUS.ts
@@ -3,16 +3,23 @@ import { getPickersLocalization } from './utils/getPickersLocalization';
 
 // This object is not Partial<PickersLocaleText> because it is the default values
 const enUSPickers: PickersLocaleText = {
+  // Calendar navigation
   previousMonth: 'Previous month',
   nextMonth: 'Next month',
+
+  // View navigation
   openPreviousView: 'open previous view',
   openNextView: 'open next view',
+
+  // DateRange placeholders
+  start: 'Start',
+  end: 'End',
+
+  // Action bar
   cancelButtonLabel: 'Cancel',
   clearButtonLabel: 'Clear',
   okButtonLabel: 'OK',
   todayButtonLabel: 'Today',
-  start: 'Start',
-  end: 'End',
 };
 
 export const DEFAULT_LOCALE = enUSPickers;
--- packages/x-date-pickers/src/locales/frFR.ts
+++ packages/x-date-pickers/src/locales/frFR.ts
@@ -2,16 +2,23 @@ import { PickersLocaleText } from './utils/pickersLocaleTextApi';
 import { getPickersLocalization } from './utils/getPickersLocalization';
 
 const frFRPickers: Partial<PickersLocaleText> = {
+  // Calendar navigation
   previousMonth: 'Mois précédent',
   nextMonth: 'Mois suivant',
+
+  // View navigation
   openPreviousView: 'Ouvrir la vue précédente',
   openNextView: 'Ouvrir la vue suivante',
+
+  // DateRange placeholders
+  start: 'Début',
+  end: 'Fin',
+
+  // Action bar
   cancelButtonLabel: 'Annuler',
   clearButtonLabel: 'Vider',
   okButtonLabel: 'OK',
   todayButtonLabel: "Aujourd'hui",
-  start: 'Début',
-  end: 'Fin',
 };
 
 export const frFR = getPickersLocalization(frFRPickers);
