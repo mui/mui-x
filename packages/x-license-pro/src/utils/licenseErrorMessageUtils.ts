845f3e4282c203410b8050e7de97028abb8b707e
Olivier Tassinari <olivier.tassinari@gmail.com>
2022-05-25 01:09:42 +0200

    polish

--- packages/x-license-pro/src/utils/licenseErrorMessageUtils.ts
+++ packages/x-license-pro/src/utils/licenseErrorMessageUtils.ts
@@ -16,9 +16,9 @@ export function showInvalidLicenseError() {
   showError([
     'MUI: Invalid license key.',
     '',
-    "Your MUI X license key isn't valid. Please check",
-    'your license key, or visit',
-    'https://mui.com/r/x-get-license to purchase a license.',
+    "Your MUI X license key isn't valid. Please check your license key installation https://mui.com/r/x-license-key-installation.",
+    '',
+    'To purchase a license, please visit https://mui.com/r/x-get-license.',
   ]);
 }
 
@@ -27,11 +27,9 @@ export function showNotFoundLicenseError() {
     'MUI: License key not found.',
     '',
     'This is a trial-only version of MUI X.',
-    'See the conditons here:',
-    'https://mui.com/r/x-license-eula#evaluation-trial-licenses.',
+    'See the conditons here: https://mui.com/r/x-license-eula#evaluation-trial-licenses.',
     '',
-    'To purchase a license, please visit',
-    'https://mui.com/r/x-get-license.',
+    'To purchase a license, please visit https://mui.com/r/x-get-license.',
   ]);
 }
 
@@ -39,7 +37,6 @@ export function showExpiredLicenseError() {
   showError([
     'MUI: License key expired.',
     '',
-    'Please visit https://mui.com/r/x-get-license to renew',
-    'your subscription of MUI X.',
+    'Please visit https://mui.com/r/x-get-license to renew your subscription of MUI X.',
   ]);
 }
