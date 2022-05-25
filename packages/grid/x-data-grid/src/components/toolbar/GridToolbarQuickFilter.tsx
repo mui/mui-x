3153ab979c8324a23282df01a313c3c86d8776a2
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-05-25 12:18:29 +0200

    proptypes

--- packages/grid/x-data-grid/src/components/toolbar/GridToolbarQuickFilter.tsx
+++ packages/grid/x-data-grid/src/components/toolbar/GridToolbarQuickFilter.tsx
@@ -131,6 +131,12 @@ GridToolbarQuickFilter.propTypes = {
    */
   debounceMs: PropTypes.number,
   /**
+   * Function responsible for formatting values of quick filter in a string when the model is modified
+   * @param {any[]} values The new values passed to the quick filter model
+   * @returns {string} The string to display in the text field
+   */
+  quickFilterFormatter: PropTypes.func,
+  /**
    * Function responsible for parsing text input in an array of independent values for quick filtering.
    * @param {string} input The value entered by the user
    * @returns {any[]} The array of value on which quick filter is applied
