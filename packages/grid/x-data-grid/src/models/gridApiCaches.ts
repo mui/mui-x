764953e92840d2bd0711adcd0dfc34c78899f2cf
Flavien DELANGLE <flaviendelangle@gmail.com>
2022-05-31 11:03:30 +0200

    [data grid] Use column visibility model on Hide All / Show All when enabled (#5050)

--- packages/grid/x-data-grid/src/models/gridApiCaches.ts
+++ packages/grid/x-data-grid/src/models/gridApiCaches.ts
@@ -1,5 +1,7 @@
 import { GridRowsInternalCache } from '../hooks/features/rows/gridRowsState';
+import { GridColumnsInternalCache } from '../hooks/features/columns/gridColumnsInterfaces';
 
 export interface GridApiCaches {
   rows: GridRowsInternalCache;
+  columns: GridColumnsInternalCache;
 }
