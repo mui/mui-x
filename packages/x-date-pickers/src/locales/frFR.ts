c0082c415ca2fa46e42d3b0b5709cae21d662882
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-06-01 11:35:31 +0200

    run l10n

--- packages/x-date-pickers/src/locales/frFR.ts
+++ packages/x-date-pickers/src/locales/frFR.ts
@@ -1,5 +1,6 @@
 import { PickersLocaleText } from './utils/pickersLocaleTextApi';
 import { getPickersLocalization } from './utils/getPickersLocalization';
+// import { CalendarPickerView } from '../internals/models';
 
 const frFRPickers: Partial<PickersLocaleText<any>> = {
   // Calendar navigation
@@ -9,6 +10,7 @@ const frFRPickers: Partial<PickersLocaleText<any>> = {
   // View navigation
   openPreviousView: 'Ouvrir la vue précédente',
   openNextView: 'Ouvrir la vue suivante',
+  // calendarViewSwitchingButtonAriaLabel: (view: CalendarPickerView) => view === 'year' ? 'year view is open, switch to calendar view' : 'calendar view is open, switch to year view',
 
   // DateRange placeholders
   start: 'Début',
@@ -19,6 +21,20 @@ const frFRPickers: Partial<PickersLocaleText<any>> = {
   clearButtonLabel: 'Vider',
   okButtonLabel: 'OK',
   todayButtonLabel: "Aujourd'hui",
+
+  // Clock labels
+  // clockLabelText: (view, time, adapter) => `Select ${view}. ${time === null ? 'No time selected' : `Selected time is ${adapter.format(time, 'fullTime')}`}`,
+  // hoursClockNumberText: hours => `${hours} hours`,
+  // minutesClockNumberText: minutes => `${minutes} minutes`,
+  // secondsClockNumberText: seconds => `${seconds} seconds`,
+
+  // Open picker labels
+  // openDatePickerDialogue: (rawValue, utils) => rawValue && utils.isValid(utils.date(rawValue)) ? `Choose date, selected date is ${utils.format(utils.date(rawValue)!, 'fullDate')}` : 'Choose date',
+  // openTimePickerDialogue: (rawValue, utils) => rawValue && utils.isValid(utils.date(rawValue)) ? `Choose time, selected time is ${utils.format(utils.date(rawValue)!, 'fullTime')}` : 'Choose time',
+
+  // Table labels
+  // timeTableLabel: 'pick time',
+  // dateTableLabel: 'pick date',
 };
 
 export const frFR = getPickersLocalization(frFRPickers);
