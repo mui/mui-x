5c36969961680c0a5fbcbac4c227a63130022c56
Matheus Wichman <matheushw@outlook.com>
2022-06-10 17:23:57 -0300

    [DataGrid] Hide vertical scrollbar if `autoHeight` is enabled (#5164)

--- packages/grid/x-data-grid/src/hooks/features/virtualization/useGridVirtualScroller.tsx
+++ packages/grid/x-data-grid/src/hooks/features/virtualization/useGridVirtualScroller.tsx
@@ -472,6 +472,9 @@ export const useGridVirtualScroller = (props: UseGridVirtualScrollerProps) => {
   if (!needsHorizontalScrollbar) {
     rootStyle.overflowX = 'hidden';
   }
+  if (rootProps.autoHeight) {
+    rootStyle.overflowY = 'hidden';
+  }
 
   const getRenderContext = React.useCallback((): GridRenderContext => {
     return prevRenderContext.current!;
