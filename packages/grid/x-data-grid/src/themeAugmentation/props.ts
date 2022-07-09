c51cad8433dd8ee231af83d634facd744808af7c
Rafael Mariano <toruticas@gmail.com>
2022-07-09 12:54:57 +0400

    [data grid] add base theme to the components theme augmentation

--- packages/grid/x-data-grid/src/themeAugmentation/props.ts
+++ packages/grid/x-data-grid/src/themeAugmentation/props.ts
@@ -5,10 +5,10 @@ export interface DataGridComponentsPropsList {
   MuiDataGrid: DataGridProps;
 }
 
-export interface DataGridComponents {
+export interface DataGridComponents<Theme = unknown> {
   MuiDataGrid?: {
     defaultProps?: ComponentsProps['MuiDataGrid'];
-    styleOverrides?: ComponentsOverrides['MuiDataGrid'];
+    styleOverrides?: ComponentsOverrides<Theme>['MuiDataGrid'];
   };
 }
 
@@ -17,5 +17,5 @@ declare module '@mui/material/styles' {
 }
 
 declare module '@mui/material/styles/components' {
-  interface Components extends DataGridComponents {}
+  interface Components<Theme = unknown> extends DataGridComponents<Theme> {}
 }
