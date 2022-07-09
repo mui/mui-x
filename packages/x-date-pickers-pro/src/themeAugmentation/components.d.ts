1126940e9dfc059819bd65b3237e4f581a5bd179
Rafael Mariano <toruticas@gmail.com>
2022-07-09 12:55:31 +0400

    [date picker pro] add base theme to the components theme augmentation

--- packages/x-date-pickers-pro/src/themeAugmentation/components.d.ts
+++ packages/x-date-pickers-pro/src/themeAugmentation/components.d.ts
@@ -1,13 +1,13 @@
 import { ComponentsProps, ComponentsOverrides, ComponentsVariants } from '@mui/material/styles';
 
-export interface PickersProComponents {
+export interface PickersProComponents<Theme = unknown> {
   MuiDateRangePickerDay?: {
     defaultProps?: ComponentsProps['MuiDateRangePickerDay'];
-    styleOverrides?: ComponentsOverrides['MuiDateRangePickerDay'];
+    styleOverrides?: ComponentsOverrides<Theme>['MuiDateRangePickerDay'];
     variants?: ComponentsVariants['MuiDateRangePickerDay'];
   };
 }
 
 declare module '@mui/material/styles' {
-  interface Components extends PickersProComponents {}
+  interface Components<Theme = unknown> extends PickersProComponents<Theme> {}
 }
