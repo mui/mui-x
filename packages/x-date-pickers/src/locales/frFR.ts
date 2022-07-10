b70b76b6df816ec5b4fa171de59d5ad0f48927b5
Zenoo <perso@franckdemoute.fr>
2022-07-10 22:04:35 +0200

    [DatePickers] Fix prettier for frFR

--- packages/x-date-pickers/src/locales/frFR.ts
+++ packages/x-date-pickers/src/locales/frFR.ts
@@ -16,7 +16,10 @@ const frFRPickers: Partial<PickersLocaleText<any>> = {
   // View navigation
   openPreviousView: 'Ouvrir la vue précédente',
   openNextView: 'Ouvrir la vue suivante',
-  calendarViewSwitchingButtonAriaLabel: (view: CalendarPickerView) => view === 'year' ? 'La vue année est ouverte, ouvrir la vue calendrier' : 'La vue calendrier est ouverte, ouvrir la vue année',
+  calendarViewSwitchingButtonAriaLabel: (view: CalendarPickerView) =>
+    view === 'year'
+      ? 'La vue année est ouverte, ouvrir la vue calendrier'
+      : 'La vue calendrier est ouverte, ouvrir la vue année',
 
   // DateRange placeholders
   start: 'Début',
@@ -29,17 +32,34 @@ const frFRPickers: Partial<PickersLocaleText<any>> = {
   todayButtonLabel: "Aujourd'hui",
 
   // Clock labels
-  clockLabelText: (view, time, adapter) => `Choix des ${views[view]}. ${time === null ? 'Aucune heure choisie' : `L'heure choisie est ${adapter.format(time, 'fullTime')}`}`,
-  hoursClockNumberText: hours => `${hours} heures`,
-  minutesClockNumberText: minutes => `${minutes} minutes`,
-  secondsClockNumberText: seconds => `${seconds} secondes`,
+  clockLabelText: (view, time, adapter) =>
+    `Choix des ${views[view]}. ${
+      time === null
+        ? 'Aucune heure choisie'
+        : `L'heure choisie est ${adapter.format(time, 'fullTime')}`
+    }`,
+  hoursClockNumberText: (hours) => `${hours} heures`,
+  minutesClockNumberText: (minutes) => `${minutes} minutes`,
+  secondsClockNumberText: (seconds) => `${seconds} secondes`,
 
   // Open picker labels
-  openDatePickerDialogue: (rawValue, utils) => rawValue && utils.isValid(utils.date(rawValue)) ? `Choisir la date, la date sélectionnée est ${utils.format(utils.date(rawValue)!, 'fullDate')}` : 'Choisir la date',
-  openTimePickerDialogue: (rawValue, utils) => rawValue && utils.isValid(utils.date(rawValue)) ? `Choisir l'heure, l'heure sélectionnée est ${utils.format(utils.date(rawValue)!, 'fullTime')}` : 'Choisir l\'heure',
+  openDatePickerDialogue: (rawValue, utils) =>
+    rawValue && utils.isValid(utils.date(rawValue))
+      ? `Choisir la date, la date sélectionnée est ${utils.format(
+          utils.date(rawValue)!,
+          'fullDate',
+        )}`
+      : 'Choisir la date',
+  openTimePickerDialogue: (rawValue, utils) =>
+    rawValue && utils.isValid(utils.date(rawValue))
+      ? `Choisir l'heure, l'heure sélectionnée est ${utils.format(
+          utils.date(rawValue)!,
+          'fullTime',
+        )}`
+      : "Choisir l'heure",
 
   // Table labels
-  timeTableLabel: 'choix de l\'heure',
+  timeTableLabel: "choix de l'heure",
   dateTableLabel: 'choix de la date',
 };
 
