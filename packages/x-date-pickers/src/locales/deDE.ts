1c0c7a04abd74963eeb96aa6b56d10e4c095b808
Sebastian Frey <sebastian.frey@outlook.com>
2022-07-11 07:24:03 +0200

    [DatePickers][l10n] Fix linter errors

--- packages/x-date-pickers/src/locales/deDE.ts
+++ packages/x-date-pickers/src/locales/deDE.ts
@@ -1,10 +1,9 @@
 import { PickersLocaleText } from './utils/pickersLocaleTextApi';
 import { getPickersLocalization } from './utils/getPickersLocalization';
-import { ClockPickerView } from '../ClockPicker';
 import { CalendarPickerView } from '../internals/models';
 
 // maps ClockPickerView to its translation
-const views: Record<ClockPickerView, string> = {
+const views = {
   hours: 'Stunden',
   minutes: 'Minuten',
   seconds: 'Sekunden',
@@ -18,7 +17,10 @@ const deDEPickers: Partial<PickersLocaleText<any>> = {
   // View navigation
   openPreviousView: 'Letzte Ansicht öffnen',
   openNextView: 'Nächste Ansicht öffnen',
-  calendarViewSwitchingButtonAriaLabel: (view: CalendarPickerView) => view === 'year' ? 'Jahresansicht ist geöffnet, zur Kalenderansicht wechseln' : 'Kalenderansicht ist geöffnet, zur Jahresansicht wechseln',
+  calendarViewSwitchingButtonAriaLabel: (view: CalendarPickerView) =>
+    view === 'year'
+      ? 'Jahresansicht ist geöffnet, zur Kalenderansicht wechseln'
+      : 'Kalenderansicht ist geöffnet, zur Jahresansicht wechseln',
 
   // DateRange placeholders
   start: 'Beginn',
@@ -31,14 +33,31 @@ const deDEPickers: Partial<PickersLocaleText<any>> = {
   todayButtonLabel: 'Heute',
 
   // Clock labels
-  clockLabelText: (view, time, adapter) => `${views[view] ?? view} auswählen. ${time === null ? 'Keine Zeit ausgewählt' : `Ausgewählte Zeit ist ${adapter.format(time, 'fullTime')}`}`,
-  hoursClockNumberText: hours => `${hours} ${views['hours']}`,
-  minutesClockNumberText: minutes => `${minutes} ${views['minutes']}`,
-  secondsClockNumberText: seconds => `${seconds}  ${views['seconds']}`,
+  clockLabelText: (view, time, adapter) =>
+    `${views[view] ?? view} auswählen. ${
+      time === null
+        ? 'Keine Zeit ausgewählt'
+        : `Ausgewählte Zeit ist ${adapter.format(time, 'fullTime')}`
+    }`,
+  hoursClockNumberText: (hours) => `${hours} ${views.hours}`,
+  minutesClockNumberText: (minutes) => `${minutes} ${views.minutes}`,
+  secondsClockNumberText: (seconds) => `${seconds}  ${views.seconds}`,
 
   // Open picker labels
-  openDatePickerDialogue: (rawValue, utils) => rawValue && utils.isValid(utils.date(rawValue)) ? `Datum auswählen, das selektierte Datum ist ${utils.format(utils.date(rawValue)!, 'fullDate')}` : 'Datum auswählen',
-  openTimePickerDialogue: (rawValue, utils) => rawValue && utils.isValid(utils.date(rawValue)) ? `Zeit auswählen, die selektierte Zeit ist ${utils.format(utils.date(rawValue)!, 'fullTime')}` : 'Zeit auswählen',
+  openDatePickerDialogue: (rawValue, utils) =>
+    rawValue && utils.isValid(utils.date(rawValue))
+      ? `Datum auswählen, das selektierte Datum ist ${utils.format(
+          utils.date(rawValue)!,
+          'fullDate',
+        )}`
+      : 'Datum auswählen',
+  openTimePickerDialogue: (rawValue, utils) =>
+    rawValue && utils.isValid(utils.date(rawValue))
+      ? `Zeit auswählen, die selektierte Zeit ist ${utils.format(
+          utils.date(rawValue)!,
+          'fullTime',
+        )}`
+      : 'Zeit auswählen',
 
   // Table labels
   timeTableLabel: 'Zeit auswählen',
