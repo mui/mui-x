d7ebcbb561d30a274b3df126d41610c2d2d4ce3d
Jose Rodolfo Freitas <joserodolfo.freitas@gmail.com>
2022-08-03 09:43:18 +0200

    Merge features list file into PopularFeaturesDemo to fix regression tests

--- docs/data/data-grid/demo/PopularFeaturesDemo.tsx
+++ docs/data/data-grid/demo/PopularFeaturesDemo.tsx
@@ -12,7 +12,150 @@ import Chip from '@mui/material/Chip';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { featuresSet } from './features';
+import AggregationRowGrouping from '../aggregation/AggregationRowGrouping';
+import BasicColumnPinning from '../column-pinning/BasicColumnPinning';
+import ColumnSelectorGrid from '../column-visibility/ColumnSelectorGrid';
+import EditingWithDatePickers from '../editing/EditingWithDatePickers';
+import ExcelExport from '../export/ExcelExport';
+import QuickFilteringGrid from '../filtering/QuickFilteringGrid';
+import BasicDetailPanels from '../master-detail/BasicDetailPanels';
+import CursorPaginationGrid from '../pagination/CursorPaginationGrid';
+import RowGroupingInitialState from '../row-grouping/RowGroupingInitialState';
+import RowOrderingGrid from '../row-ordering/RowOrderingGrid';
+import RowPinningWithPagination from '../row-pinning/RowPinningWithPagination';
+import RestoreStateInitialState from '../state/RestoreStateInitialState';
+import TreeDataFullExample from '../tree-data/TreeDataFullExample';
+import ColumnVirtualizationGrid from '../virtualization/ColumnVirtualizationGrid';
+import FullFeaturedDemo from './FullFeaturedDemo';
+
+export const featuresSet = [
+  {
+    id: 1,
+    name: 'Master detail',
+    description: 'A parent row with a collapsible child panel',
+    plan: 'Pro',
+    detailPage: '/master-detail/',
+    demo: <BasicDetailPanels />,
+  },
+  {
+    id: 2,
+    name: 'Inline editing',
+    description:
+      'Edit your data inside the cells by double-clicking or pressing Enter',
+    plan: 'Community',
+    detailPage: '/editing/',
+    demo: <EditingWithDatePickers />,
+  },
+  {
+    id: 3,
+    name: 'Pagination',
+    description: 'Easily paginate your rows and only fetch what you need',
+    plan: 'Community',
+    detailPage: '/pagination/',
+    demo: <CursorPaginationGrid />,
+  },
+  {
+    id: 4,
+    name: 'Save and restore state',
+    description:
+      'Save and restore internal state and configurations like active filters and sorting',
+    plan: 'Community',
+    detailPage: '/state/#save-and-restore-the-state',
+    demo: <RestoreStateInitialState />,
+  },
+  {
+    id: 5,
+    name: 'Row Grouping',
+    description: 'Group rows with repeating column values',
+    plan: 'Premium',
+    detailPage: '/row-grouping/',
+    demo: <RowGroupingInitialState />,
+  },
+  {
+    id: 6,
+    name: 'Excel export',
+    description:
+      'Easily export the rows in various file formats such as CSV, PDF or Excel',
+    plan: 'Premium',
+    detailPage: '/export/#excel-export',
+    demo: <ExcelExport />,
+  },
+  {
+    id: 7,
+    name: 'Quick filter',
+    description: 'Use a single text input to filter multiple fields',
+    plan: 'Community',
+    detailPage: '/filtering/#quick-filter',
+    demo: <QuickFilteringGrid />,
+  },
+  {
+    id: 8,
+    name: 'Row reorder',
+    description: 'Drag and drop to reorder your data',
+    plan: 'Pro',
+    detailPage: '/row-ordering/',
+    demo: <RowOrderingGrid />,
+  },
+  {
+    id: 9,
+    name: 'Column Pinning',
+    description: 'Pin your columns to the left or right',
+    plan: 'Pro',
+    detailPage: '/column-pinning/',
+    demo: <BasicColumnPinning />,
+  },
+  {
+    id: 10,
+    name: 'Row Pinning',
+    description: 'Pin your rows up or down',
+    plan: 'Pro',
+    detailPage: '/row-pinning/',
+    demo: <RowPinningWithPagination />,
+    newBadge: true,
+  },
+  {
+    id: 11,
+    name: 'Aggretation and Summary rows',
+    description: 'Set summary footer rows or inline summaries with row grouping',
+    plan: 'Premium',
+    detailPage: '/aggregation/',
+    demo: <AggregationRowGrouping />,
+    newBadge: true,
+  },
+  {
+    id: 12,
+    name: 'Column Visibility',
+    description:
+      'Display different columns in different use cases by defining which columns are visible',
+    plan: 'Community',
+    detailPage: '"/column-visibility/',
+    demo: <ColumnSelectorGrid />,
+  },
+  {
+    id: 13,
+    name: 'Column Virtualization',
+    description: 'High performance support for thousands of columns',
+    plan: 'Community',
+    detailPage: '/virtualization/#column-virtualization',
+    demo: <ColumnVirtualizationGrid />,
+  },
+  {
+    id: 14,
+    name: 'Row Virtualization',
+    description: 'High performance support for vast volume of data',
+    plan: 'Pro',
+    detailPage: '/virtualization/#row-virtualization',
+    demo: <FullFeaturedDemo />,
+  },
+  {
+    id: 15,
+    name: 'Tree data',
+    description: 'Support rows with parent / child relationship',
+    plan: 'Pro',
+    detailPage: '/tree-data/',
+    demo: <TreeDataFullExample />,
+  },
+];
 
 const getChipProperties = (plan: string) => {
   switch (plan.toLowerCase()) {
