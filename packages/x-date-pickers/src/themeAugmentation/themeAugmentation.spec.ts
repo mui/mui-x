4378413b6ebecfd43a68d7309c8d1f2c63b0a2a0
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-08-05 15:43:23 +0200

    extend tests

--- packages/x-date-pickers/src/themeAugmentation/themeAugmentation.spec.ts
+++ packages/x-date-pickers/src/themeAugmentation/themeAugmentation.spec.ts
@@ -1,13 +1,75 @@
 import { createTheme } from '@mui/material/styles';
 
 createTheme({
-  components: {
-    MuiDatePicker: {
-      defaultProps: {
-        open: true,
-        // @ts-expect-error invalid MuiDatePicker prop
-        someRandomProp: true,
-      },
+    components: {
+        MuiCalendarPicker: {
+            defaultProps: {
+                view: 'day',
+                // @ts-expect-error invalid MuiCalendarPicker prop
+                someRandomProp: true,
+            }
+        },
+        MuiCalendarPickerSkeleton: {
+            defaultProps: {
+                className: 'class',
+                // @ts-expect-error invalid MuiCalendarPickerSkeleton prop
+                someRandomProp: true,
+            }
+        },
+        MuiClockPicker: {
+            defaultProps: {
+                view: 'hours',
+                // @ts-expect-error invalid MuiClockPicker prop
+                someRandomProp: true,
+            }
+        },
+        MuiDatePicker: {
+            defaultProps: {
+                open: true,
+                // @ts-expect-error invalid MuiDatePicker prop
+                someRandomProp: true,
+            }
+        },
+        MuiDateTimePicker: {
+            defaultProps: {
+                open: true,
+                // @ts-expect-error invalid MuiDateTimePicker prop
+                someRandomProp: true,
+            }
+        },
+        MuiMonthPicker: {
+            defaultProps: {
+                disableFuture: true,
+                // @ts-expect-error invalid MuiMonthPicker prop
+                someRandomProp: true,
+            }
+        },
+        MuiPickersDay: {
+            defaultProps: {
+                disabled: true,
+                // @ts-expect-error invalid MuiPickersDay prop
+                someRandomProp: true,
+            }
+        },
+        MuiStaticDatePicker: {
+            defaultProps: {
+                open: true,
+                someRandomProp: true,
+            }
+        },
+        MuiYearPicker: {
+            defaultProps: {
+                disableFuture: true,
+                // @ts-expect-error invalid MuiYearPicker prop
+                someRandomProp: true,
+            }
+        },
+        MuiPickerStaticWrapper: {
+            defaultProps: {
+                open: true,
+                // @ts-expect-error invalid MuiPickerStaticWrapper prop
+                someRandomProp: true,
+            }
+        },
     },
-  },
 });
