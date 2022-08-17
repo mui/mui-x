0ebc8f1c22893d082561bca57351437e8d43c9b5
Olivier Tassinari <olivier.tassinari@gmail.com>
2022-08-17 17:27:31 +0200

    [core] Clarify the scope of this license key

--- test/regressions/index.js
+++ test/regressions/index.js
@@ -9,7 +9,8 @@ import addons, { mockChannel } from '@storybook/addons';
 // See https://storybook.js.org/docs/react/workflows/faq#why-is-there-no-addons-channel
 addons.setChannel(mockChannel());
 
-// Remove the license warning from demonstration purposes
+// This license key is only valid for use with Material UI SAS's projects
+// See the terms: https://mui.com/r/x-license-eula
 LicenseInfo.setLicenseKey(
   '61628ce74db2c1b62783a6d438593bc5Tz1NVUktRG9jLEU9MTY4MzQ0NzgyMTI4NCxTPXByZW1pdW0sTE09c3Vic2NyaXB0aW9uLEtWPTI=',
 );
