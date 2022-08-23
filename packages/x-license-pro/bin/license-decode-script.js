45489f81c7583161dd2e535d893ab88ec02d9df9
delangle <flaviendelangle@gmail.com>
2022-08-23 09:46:52 +0200

    Drop cli

--- packages/x-license-pro/bin/license-decode-script.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-/* eslint-disable */
-require = require('esm')(module);
-
-if (process.env.NODE_ENV === 'dev') {
-  require('../build/node/cli/license-cli').licenseDecodeCli();
-} else {
-  require('../node/cli/license-cli').licenseDecodeCli();
-}
