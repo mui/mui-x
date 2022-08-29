6af605542cc881863255e201970bd7b22016c558
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-08-29 16:09:05 +0200

    simplify focus management

--- packages/x-date-pickers/src/internals/components/CalendarOrClockPicker/useFocusManagement.tsx
+++ packages/x-date-pickers/src/internals/components/CalendarOrClockPicker/useFocusManagement.tsx
@@ -1,60 +1,29 @@
 import * as React from 'react';
 import { CalendarPickerView } from '../../models/views';
 
-interface FocusState {
-  focusedView: CalendarPickerView | null;
-}
-
-type FocusModificationPayload = { view: CalendarPickerView };
-
-type ReducerAction<TType, TAdditional = {}> = { type: TType } & TAdditional;
-
-export const createCalendarStateReducer =
-  () =>
-  (
-    state: FocusState,
-    action:
-      | ReducerAction<'focus', FocusModificationPayload>
-      | ReducerAction<'blur', FocusModificationPayload>,
-  ): FocusState => {
-    const { type, view } = action;
-    switch (type) {
-      case 'focus':
-        return {
-          focusedView: view,
-        };
-      case 'blur':
-        if (view === state.focusedView) {
-          return { focusedView: null };
-        }
-        return state;
-
-      default:
-        throw new Error('missing support');
-    }
-  };
-
 interface FocusStateInput {
   autoFocus?: boolean;
   openView: any;
 }
 
 export const useFocusManagement = ({ autoFocus, openView }: FocusStateInput) => {
-  const reducerFn = React.useRef(createCalendarStateReducer()).current;
+  const [focusedView, setFocusedView] = React.useState<CalendarPickerView | null>(
+    autoFocus ? openView : null,
+  );
 
-  const [viewFocusState, dispatch] = React.useReducer(reducerFn, {
-    focusedView: autoFocus ? openView : null,
-  });
-
-  const setFocusedView = React.useCallback(
+  const setFocusedViewCallback = React.useCallback(
     (view: CalendarPickerView) => (newHasFocus: boolean) => {
-      dispatch({ type: newHasFocus ? 'focus' : 'blur', view });
+      if (newHasFocus) {
+        setFocusedView(view);
+      } else {
+        setFocusedView((prevFocusedView) => (view === prevFocusedView ? null : prevFocusedView));
+      }
     },
     [],
   );
 
   return {
-    focusedView: viewFocusState.focusedView,
-    setFocusedView,
+    focusedView,
+    setFocusedView: setFocusedViewCallback,
   };
 };
