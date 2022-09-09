62d413340c0eb463e056a7aa1ba94f102727c5d4
Lukas Tyla <llukas.tyla@gmail.com>
2022-09-09 10:20:39 +0300

    Add `pro` package theme augmentation spec

---
+++ packages/x-date-pickers-pro/src/themeAugmentation/themeAugmentation.spec.ts
@@ -0,0 +1,55 @@
+import { createTheme } from '@mui/material/styles';
+
+createTheme({
+  components: {
+    MuiDateRangePicker: {
+      defaultProps: {
+        calendars: 2,
+        // @ts-expect-error invalid MuiDateRangePicker prop
+        someRandomProp: true,
+      },
+    },
+    MuiDateRangePickerDay: {
+      defaultProps: {
+        color: 'red',
+        // @ts-expect-error invalid MuiDateRangePickerDay prop
+        someRandomProp: true,
+      },
+    },
+    MuiDateRangePickerInput: {
+      defaultProps: {
+        disabled: true,
+        // @ts-expect-error invalid MuiDateRangePickerInput prop
+        someRandomProp: true,
+      },
+    },
+    MuiDateRangePickerToolbar: {
+      defaultProps: {
+        toolbarTitle: 'test',
+        // @ts-expect-error invalid MuiDateRangePickerToolbar prop
+        someRandomProp: true,
+      },
+    },
+    MuiDesktopDateRangePicker: {
+      defaultProps: {
+        open: true,
+        // @ts-expect-error invalid MuiDesktopDateRangePicker prop
+        someRandomProp: true,
+      },
+    },
+    MuiMobileDateRangePicker: {
+      defaultProps: {
+        disableFuture: true,
+        // @ts-expect-error invalid MuiMobileDateRangePicker prop
+        someRandomProp: true,
+      },
+    },
+    MuiStaticDateRangePicker: {
+      defaultProps: {
+        disabled: true,
+        // @ts-expect-error invalid MuiStaticDateRangePicker prop
+        someRandomProp: true,
+      },
+    },
+  },
+});
