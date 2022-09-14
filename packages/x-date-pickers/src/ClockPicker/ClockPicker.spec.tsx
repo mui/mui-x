5c4fa778a5cbac598e8e4ec320576a717009db6c
Flavien DELANGLE <flaviendelangle@gmail.com>
2022-09-14 12:56:35 +0200

    [pickers] Rename the `date` prop `value` on `MonthPicker` / `YearPicker`, `ClockPicker` and `CalendarPicker` (#6128)

--- packages/x-date-pickers/src/ClockPicker/ClockPicker.spec.tsx
+++ packages/x-date-pickers/src/ClockPicker/ClockPicker.spec.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import { ClockPicker } from '@mui/x-date-pickers/ClockPicker';
 
 // External components are generic
-<ClockPicker<Date> view="hours" date={null} onChange={(date) => date?.getDate()} />;
+<ClockPicker<Date> view="hours" value={null} onChange={(date) => date?.getDate()} />;
