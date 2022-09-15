f2796f68f21cd1a1688829ef3a9def7cd4c17641
Lukas Tyla <llukas.tyla@gmail.com>
2022-09-15 10:50:51 +0300

    Fix revert export

--- packages/x-date-pickers/src/ClockPicker/index.ts
+++ packages/x-date-pickers/src/ClockPicker/index.ts
@@ -1,13 +1,13 @@
 export { ClockPicker } from './ClockPicker';
 export type { ClockPickerProps } from './ClockPicker';
 
-export { clockClasses, getClockUtilityClass } from './clockClasses';
+export { clockClasses } from './clockClasses';
 export type { ClockClasses, ClockClassKey } from './clockClasses';
 export type { ClockProps } from './Clock';
 export { clockNumberClasses } from './clockNumberClasses';
 export type { ClockNumberClasses, ClockNumberClassKey } from './clockNumberClasses';
 export type { ClockNumberProps } from './ClockNumber';
-export { clockPickerClasses } from './clockPickerClasses';
+export { clockPickerClasses, getClockPickerUtilityClass } from './clockPickerClasses';
 export type { ClockPickerClasses, ClockPickerClassKey } from './clockPickerClasses';
 export { clockPointerClasses } from './clockPointerClasses';
 export type { ClockPointerClasses, ClockPointerClassKey } from './clockPointerClasses';
