7fc1ef361b06459158fee03fdd3b357b1a0c8cd5
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-09-21 12:32:42 +0200

    adapt doc

--- docs/data/date-pickers/date-picker/AddWeekNumber.js
+++ docs/data/date-pickers/date-picker/AddWeekNumber.js
@@ -3,27 +3,14 @@ import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import dayjsInstance from 'dayjs';
-// plugins mandatory for the AdapterDayjS
-import customParseFormatPlugin from 'dayjs/plugin/customParseFormat';
-import localizedFormatPlugin from 'dayjs/plugin/localizedFormat';
-import isBetweenPlugin from 'dayjs/plugin/isBetween';
-// plugins to get week number
-import weekOfYearPlugin from 'dayjs/plugin/weekOfYear';
-
-dayjsInstance.extend(customParseFormatPlugin);
-dayjsInstance.extend(localizedFormatPlugin);
-dayjsInstance.extend(isBetweenPlugin);
-dayjsInstance.extend(weekOfYearPlugin);
 
 export default function AddWeekNumber() {
   const [value, setValue] = React.useState(null);
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs} instance={dayjsInstance}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <StaticDatePicker
         displayWeekNumber
-        getWeekNumber={(date) => `${date.week()}`}
         label="Basic example"
         value={value}
         onChange={(newValue) => {
--- docs/data/date-pickers/date-picker/AddWeekNumber.tsx
+++ docs/data/date-pickers/date-picker/AddWeekNumber.tsx
@@ -3,27 +3,15 @@ import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import dayjsInstance, { Dayjs } from 'dayjs';
-// plugins mandatory for the AdapterDayjS
-import customParseFormatPlugin from 'dayjs/plugin/customParseFormat';
-import localizedFormatPlugin from 'dayjs/plugin/localizedFormat';
-import isBetweenPlugin from 'dayjs/plugin/isBetween';
-// plugins to get week number
-import weekOfYearPlugin from 'dayjs/plugin/weekOfYear';
-
-dayjsInstance.extend(customParseFormatPlugin);
-dayjsInstance.extend(localizedFormatPlugin);
-dayjsInstance.extend(isBetweenPlugin);
-dayjsInstance.extend(weekOfYearPlugin);
+import { Dayjs } from 'dayjs';
 
 export default function AddWeekNumber() {
   const [value, setValue] = React.useState<Dayjs | null>(null);
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs} instance={dayjsInstance}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <StaticDatePicker
         displayWeekNumber
-        getWeekNumber={(date: Dayjs): string => `${date.week()}`}
         label="Basic example"
         value={value}
         onChange={(newValue) => {
