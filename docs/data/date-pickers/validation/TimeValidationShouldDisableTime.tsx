f9277d6f940e68577a7aa29070a5643f137c1bfc
delangle <flaviendelangle@gmail.com>
2022-09-23 18:26:47 +0200

    Fix

--- docs/data/date-pickers/validation/TimeValidationShouldDisableTime.tsx
+++ docs/data/date-pickers/validation/TimeValidationShouldDisableTime.tsx
@@ -9,7 +9,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker, TimePickerProps } from '@mui/x-date-pickers/TimePicker';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
-const shouldDisableTime: TimePickerProps<Dayjs, Dayjs>['shouldDisableTime'] = (
+const shouldDisableTime: TimePickerProps<Dayjs>['shouldDisableTime'] = (
   timeValue,
   view,
 ) => view === 'minutes' && timeValue >= 45;
