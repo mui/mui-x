fd1f44956c2108d420b39e3bfded8436552635da
Arai Shota <me@arsho.dev>
2022-10-04 20:12:48 +0900

    Add translations

--- packages/x-date-pickers/src/locales/jaJP.ts
+++ packages/x-date-pickers/src/locales/jaJP.ts
@@ -2,19 +2,35 @@ import { PickersLocaleText } from './utils/pickersLocaleTextApi';
 import { getPickersLocalization } from './utils/getPickersLocalization';
 import { CalendarPickerView } from '../internals/models';
 
+// maps ClockPickerView to its translation
+const clockViews = {
+  hours: '時間',
+  minutes: '分',
+  seconds: '秒',
+};
+
+// maps PickersToolbar["viewType"] to its translation
+const pickerViews = {
+  calendar: 'カレンダー表示',
+  clock: '時計表示',
+};
+
 const jaJPPickers: Partial<PickersLocaleText<any>> = {
   // Calendar navigation
   previousMonth: '先月',
   nextMonth: '来月',
 
   // View navigation
-  openPreviousView: '前の画面を開く',
-  openNextView: '次の画面を開く',
+  openPreviousView: '前の表示を開く',
+  openNextView: '次の表示を開く',
   calendarViewSwitchingButtonAriaLabel: (view: CalendarPickerView) =>
     view === 'year'
-      ? '年選択画面からカレンダー画面に切り替える'
-      : 'カレンダー画面から年選択画面に切り替える',
-  // inputModeToggleButtonAriaLabel: (isKeyboardInputOpen: boolean, viewType: 'calendar' | 'clock') => isKeyboardInputOpen ? `text input view is open, go to ${viewType} view` : `${viewType} view is open, go to text input view`,
+      ? '年選択表示からカレンダー表示に切り替える'
+      : 'カレンダー表示から年選択表示に切り替える',
+  inputModeToggleButtonAriaLabel: (isKeyboardInputOpen: boolean, viewType: 'calendar' | 'clock') =>
+    isKeyboardInputOpen
+      ? `テキスト入力表示から${pickerViews[viewType]}に切り替える`
+      : `${pickerViews[viewType]}からテキスト入力表示に切り替える`,
 
   // DateRange placeholders
   start: '開始',
@@ -23,25 +39,25 @@ const jaJPPickers: Partial<PickersLocaleText<any>> = {
   // Action bar
   cancelButtonLabel: 'キャンセル',
   clearButtonLabel: 'クリア',
-  okButtonLabel: '確認',
+  okButtonLabel: '確定',
   todayButtonLabel: '今日',
 
   // Toolbar titles
-  // datePickerDefaultToolbarTitle: 'Select date',
-  // dateTimePickerDefaultToolbarTitle: 'Select date & time',
-  // timePickerDefaultToolbarTitle: 'Select time',
-  // dateRangePickerDefaultToolbarTitle: 'Select date range',
+  datePickerDefaultToolbarTitle: '日付を選択',
+  dateTimePickerDefaultToolbarTitle: '日時を選択',
+  timePickerDefaultToolbarTitle: '時間を選択',
+  dateRangePickerDefaultToolbarTitle: '日付の範囲を選択',
 
   // Clock labels
   clockLabelText: (view, time, adapter) =>
-    `Select ${view}. ${
+    `${clockViews[view] ?? view}を選択してください ${
       time === null
         ? '時間が選択されていません'
         : `選択した時間は ${adapter.format(time, 'fullTime')} です`
     }`,
-  hoursClockNumberText: (hours) => `${hours} 時間`,
-  minutesClockNumberText: (minutes) => `${minutes} 分`,
-  secondsClockNumberText: (seconds) => `${seconds} 秒`,
+  hoursClockNumberText: (hours) => `${hours} ${clockViews.hours}`,
+  minutesClockNumberText: (minutes) => `${minutes} ${clockViews.minutes}`,
+  secondsClockNumberText: (seconds) => `${seconds} ${clockViews.seconds}`,
 
   // Open picker labels
   openDatePickerDialogue: (value, utils) =>
