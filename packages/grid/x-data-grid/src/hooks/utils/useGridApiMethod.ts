95f296899aeb389819d4f3d4e3f836ae84070d72
Andrew Cherniavskyi <andrew@mui.com>
2022-10-05 19:19:51 +0200

    improve useGridApiMethod typing to avoid accidental private/public error

--- packages/grid/x-data-grid/src/hooks/utils/useGridApiMethod.ts
+++ packages/grid/x-data-grid/src/hooks/utils/useGridApiMethod.ts
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import { GridPrivateApiCommon } from '../../models/api/gridApiCommon';
 
+type GetPublicApiType<PrivateApi> = PrivateApi extends { getPublicApi: () => infer PublicApi }
+  ? PublicApi
+  : never;
+
 export function useGridApiMethod<
   PrivateApi extends GridPrivateApiCommon,
-  T extends Partial<PrivateApi>,
->(
-  privateApiRef: React.MutableRefObject<PrivateApi>,
-  apiMethods: T,
-  visibility: 'public' | 'private',
-) {
+  PublicApi extends GetPublicApiType<PrivateApi>,
+  PrivateOnlyApi extends Omit<PrivateApi, keyof PublicApi>,
+  V extends 'public' | 'private',
+  T extends V extends 'public' ? Partial<PublicApi> : Partial<PrivateOnlyApi>,
+>(privateApiRef: React.MutableRefObject<PrivateApi>, apiMethods: T, visibility: V) {
   const apiMethodsRef = React.useRef(apiMethods);
-  const [apiMethodsNames] = React.useState(Object.keys(apiMethods));
+  const [apiMethodsNames] = React.useState(Object.keys(apiMethods) as Array<keyof T>);
 
   const installMethods = React.useCallback(() => {
     if (!privateApiRef.current) {
@@ -18,8 +21,10 @@ export function useGridApiMethod<
     }
     apiMethodsNames.forEach((methodName) => {
       privateApiRef.current.register(visibility, {
-        [methodName]: (...args: any[]) =>
-          apiMethodsRef.current[methodName as keyof GridPrivateApiCommon](...args),
+        [methodName]: (...args: any[]) => {
+          const fn = apiMethodsRef.current[methodName] as Function;
+          return fn(...args);
+        },
       });
     });
   }, [apiMethodsNames, privateApiRef, visibility]);
