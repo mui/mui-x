2cc4f98e4bc323e8aa649e4eb738211012ce554e
Bilal Shafi <bilal945@gmail.com>
2022-10-08 10:47:50 +0500

    [data grid] Allow to limit to one filter per column (#6333)
    Signed-off-by: Bilal Shafi <bilal945@gmail.com>
    Co-authored-by: Matheus Wichman <matheushw@outlook.com>

---
+++ docs/data/data-grid/filtering/DisableMultiFiltersDataGridPro.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import {
+  DataGridPro,
+  GridToolbar,
+  FilterColumnsArgs,
+  GetColumnForNewFilterArgs,
+} from '@mui/x-data-grid-pro';
+import { useDemoData } from '@mui/x-data-grid-generator';
+
+const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
+
+export default function DisableMultiFiltersDataGridPro() {
+  const { data } = useDemoData({
+    dataSet: 'Employee',
+    visibleFields: VISIBLE_FIELDS,
+    rowLength: 100,
+  });
+
+  const filterColumns = ({ field, columns, currentFilters }: FilterColumnsArgs) => {
+    // remove already filtered fields from list of columns
+    const filteredFields = currentFilters?.map((item) => item.columnField);
+    return columns
+      .filter(
+        (colDef) =>
+          colDef.filterable &&
+          (colDef.field === field || !filteredFields.includes(colDef.field)),
+      )
+      .map((column) => column.field);
+  };
+
+  const getColumnForNewFilter = ({
+    currentFilters,
+    columns,
+  }: GetColumnForNewFilterArgs) => {
+    const filteredFields = currentFilters?.map(({ columnField }) => columnField);
+    const columnForNewFilter = columns
+      .filter(
+        (colDef) => colDef.filterable && !filteredFields.includes(colDef.field),
+      )
+      .find((colDef) => colDef.filterOperators?.length);
+    return columnForNewFilter?.field;
+  };
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGridPro
+        {...data}
+        components={{ Toolbar: GridToolbar }}
+        componentsProps={{
+          filterPanel: {
+            filterFormProps: {
+              filterColumns,
+            },
+            getColumnForNewFilter,
+          },
+        }}
+      />
+    </div>
+  );
+}
