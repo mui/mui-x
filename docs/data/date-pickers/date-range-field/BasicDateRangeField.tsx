963c46ab382d3cef7d42b77e9cc3b22bd9e3e0af
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-10-11 11:41:06 +0200

    scripts

--- docs/data/date-pickers/date-range-field/BasicDateRangeField.tsx
+++ docs/data/date-pickers/date-range-field/BasicDateRangeField.tsx
@@ -4,18 +4,20 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Unstable_MultiInputDateRangeField as MultiInputDateRangeField } from '@mui/x-date-pickers-pro/MultiInputDateRangeField';
 import { Unstable_SingleInputDateRangeField as SingleInputDateRangeField } from '@mui/x-date-pickers-pro/SingleInputDateRangeField';
-import { Unstable_MultiInputDateTimeRangeField as MultiInputDateTimeRangeField } from '@mui/x-date-pickers-pro/MultiInputDateTimeRangeField';
-import { Unstable_MultiInputTimeRangeField as MultiInputTimeRangeField } from '@mui/x-date-pickers-pro/MultiInputTimeRangeField';
 
 export default function BasicDateRangeField() {
+  const [value, setValue] = React.useState<any>([null, null]);
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Stack spacing={4}>
-        <MultiInputDateRangeField />
-        <SingleInputDateRangeField />
-        {/* For experiment reasons (will be removed) */}
-        <MultiInputDateTimeRangeField />
-        <MultiInputTimeRangeField />
+        <MultiInputDateRangeField
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        />
+        <SingleInputDateRangeField
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        />
       </Stack>
     </LocalizationProvider>
   );
