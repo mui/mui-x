7170582ac000f21fbec33b3ae4f228ad9a902ab5
delangle <flaviendelangle@gmail.com>
2022-10-12 10:50:49 +0200

    Fix

--- docs/data/date-pickers/date-field/ControlledSelectedSections.tsx
+++ docs/data/date-pickers/date-field/ControlledSelectedSections.tsx
@@ -3,9 +3,8 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { MuiDateSectionName } from '@mui/x-date-pickers';
+import { MuiDateSectionName, FieldSelectedSections } from '@mui/x-date-pickers';
 import { Unstable_DateField as DateField } from '@mui/x-date-pickers/DateField';
-import { FieldSelectedSections } from '@mui/x-date-pickers';
 
 export default function ControlledSelectedSections() {
   const [selectedSections, setSelectedSections] =
