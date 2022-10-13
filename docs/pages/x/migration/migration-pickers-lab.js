25ed31f5eabca019aeedd65fae25504fa7e3e3de
Flavien DELANGLE <flaviendelangle@gmail.com>
2022-10-13 15:50:21 +0200

    [docs] Organize migration pages (#6480)

--- docs/pages/x/migration/migration-pickers-lab.js
+++ docs/pages/x/migration/migration-pickers-lab.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import * as pageProps from 'docsx/data/date-pickers/migration-lab/migration-lab.md?@mui/markdown';
+import * as pageProps from 'docsx/data/migration/migration-pickers-lab/migration-pickers-lab.md?@mui/markdown';
 
 export default function Page() {
   return <MarkdownDocs {...pageProps} />;
