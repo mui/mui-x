efbaff2295f40a3428fe44a71c5e5668c8b7a044
Bilal Shafi <bilal945@gmail.com>
2022-10-29 02:50:22 +0500

    Removes localization change

--- docs/data/data-grid/column-menu/ColumnMenuGridPro.js
+++ docs/data/data-grid/column-menu/ColumnMenuGridPro.js
@@ -1,20 +1,17 @@
-import * as React from 'react';
-import { DataGridPremium, frFR } from '@mui/x-data-grid-premium';
-import { useDemoData } from '@mui/x-data-grid-generator';
+import * as React from 'react'
+import { DataGridPremium } from '@mui/x-data-grid-premium'
+import { useDemoData } from '@mui/x-data-grid-generator'
 
-export default function ColumnMenuGridPro() {
+export default function ColumnMenuGridPro () {
   const { data } = useDemoData({
     dataSet: 'Commodity',
     rowLength: 20,
     maxColumns: 5,
-  });
+  })
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGridPremium
-        {...data}
-        localeText={frFR.components.MuiDataGrid.defaultProps.localeText}
-      />
+      <DataGridPremium {...data} />
     </div>
-  );
+  )
 }
