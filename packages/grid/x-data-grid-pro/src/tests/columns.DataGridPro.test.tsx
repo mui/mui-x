c9f29bc7a5c24713df598aaea16db9db4b613174
Andrew Cherniavskyi <andrew@mui.com>
2022-10-31 21:09:10 +0100

    uncomment tests

--- packages/grid/x-data-grid-pro/src/tests/columns.DataGridPro.test.tsx
+++ packages/grid/x-data-grid-pro/src/tests/columns.DataGridPro.test.tsx
@@ -8,8 +8,11 @@ import {
   useGridApiRef,
   DataGridPro,
   gridClasses,
+  gridColumnLookupSelector,
+  gridColumnFieldsSelector,
   GridApi,
 } from '@mui/x-data-grid-pro';
+import { useGridPrivateApiContext } from '@mui/x-data-grid-pro/internals';
 import { getColumnHeaderCell, getCell } from 'test/utils/helperFn';
 
 const isJSDOM = /jsdom/.test(window.navigator.userAgent);
@@ -344,34 +347,56 @@ describe('<DataGridPro /> - Columns', () => {
     });
   });
 
-  // `requestPipeProcessorsApplication` is now private
-  //
-  // describe('column pipe processing', () => {
-  //   it('should not loose column width when re-applying pipe processing', () => {
-  //     render(<Test checkboxSelection />);
-  //     act(() => apiRef.current.setColumnWidth('brand', 300));
-  //     expect(gridColumnLookupSelector(apiRef).brand.computedWidth).to.equal(300);
-  //     act(() => apiRef.current.requestPipeProcessorsApplication('hydrateColumns'));
-  //     expect(gridColumnLookupSelector(apiRef).brand.computedWidth).to.equal(300);
-  //   });
+  describe('column pipe processing', () => {
+    it('should not loose column width when re-applying pipe processing', () => {
+      let privateApi: ReturnType<typeof useGridPrivateApiContext>;
+      const Footer = () => {
+        privateApi = useGridPrivateApiContext();
+        return null;
+      };
+      render(<Test checkboxSelection components={{ Footer }} />);
 
-  //   it('should not loose column index when re-applying pipe processing', () => {
-  //     render(<Test checkboxSelection columns={[{ field: 'id' }, { field: 'brand' }]} />);
-  //     expect(gridColumnFieldsSelector(apiRef).indexOf('brand')).to.equal(2);
-  //     act(() => apiRef.current.setColumnIndex('brand', 1));
-  //     expect(gridColumnFieldsSelector(apiRef).indexOf('brand')).to.equal(1);
-  //     act(() => apiRef.current.requestPipeProcessorsApplication('hydrateColumns'));
-  //     expect(gridColumnFieldsSelector(apiRef).indexOf('brand')).to.equal(1);
-  //   });
+      act(() => apiRef.current.setColumnWidth('brand', 300));
+      expect(gridColumnLookupSelector(apiRef).brand.computedWidth).to.equal(300);
+      act(() => privateApi.current.requestPipeProcessorsApplication('hydrateColumns'));
+      expect(gridColumnLookupSelector(apiRef).brand.computedWidth).to.equal(300);
+    });
 
-  //   it('should not loose imperatively added columns when re-applying pipe processing', () => {
-  //     render(<Test checkboxSelection />);
-  //     act(() => apiRef.current.updateColumn({ field: 'id' }));
-  //     expect(gridColumnFieldsSelector(apiRef)).to.deep.equal(['__check__', 'brand', 'id']);
-  //     act(() => apiRef.current.requestPipeProcessorsApplication('hydrateColumns'));
-  //     expect(gridColumnFieldsSelector(apiRef)).to.deep.equal(['__check__', 'brand', 'id']);
-  //   });
-  // });
+    it('should not loose column index when re-applying pipe processing', () => {
+      let privateApi: ReturnType<typeof useGridPrivateApiContext>;
+      const Footer = () => {
+        privateApi = useGridPrivateApiContext();
+        return null;
+      };
+      render(
+        <Test
+          checkboxSelection
+          columns={[{ field: 'id' }, { field: 'brand' }]}
+          components={{ Footer }}
+        />,
+      );
+
+      expect(gridColumnFieldsSelector(apiRef).indexOf('brand')).to.equal(2);
+      act(() => apiRef.current.setColumnIndex('brand', 1));
+      expect(gridColumnFieldsSelector(apiRef).indexOf('brand')).to.equal(1);
+      act(() => privateApi.current.requestPipeProcessorsApplication('hydrateColumns'));
+      expect(gridColumnFieldsSelector(apiRef).indexOf('brand')).to.equal(1);
+    });
+
+    it('should not loose imperatively added columns when re-applying pipe processing', () => {
+      let privateApi: ReturnType<typeof useGridPrivateApiContext>;
+      const Footer = () => {
+        privateApi = useGridPrivateApiContext();
+        return null;
+      };
+      render(<Test checkboxSelection components={{ Footer }} />);
+
+      act(() => apiRef.current.updateColumn({ field: 'id' }));
+      expect(gridColumnFieldsSelector(apiRef)).to.deep.equal(['__check__', 'brand', 'id']);
+      act(() => privateApi.current.requestPipeProcessorsApplication('hydrateColumns'));
+      expect(gridColumnFieldsSelector(apiRef)).to.deep.equal(['__check__', 'brand', 'id']);
+    });
+  });
 
   it('should warn if unsupported column type is used', () => {
     const singleColumns = [{ field: 'brand', type: 'str' }];
