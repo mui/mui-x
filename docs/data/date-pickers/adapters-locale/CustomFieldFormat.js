9182d3930d378cfb0b5b2520f77749214a573126
delangle <flaviendelangle@gmail.com>
2022-11-03 09:27:00 +0100

    Fix

--- docs/data/date-pickers/adapters-locale/CustomFieldFormat.js
+++ docs/data/date-pickers/adapters-locale/CustomFieldFormat.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { Unstable_DateField as DateField } from '@mui/x-date-pickers/DateField';
@@ -9,8 +10,16 @@ export default function CustomFieldFormat() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Stack spacing={3}>
-        <DateField label="Date Field" format="MM - DD - YYYY" />
-        <NextDatePicker label="Date Picker" inputFormat="MM - DD - YYYY" />
+        <DateField
+          label="Date Field"
+          format="MM - DD - YYYY"
+          defaultValue={dayjs('2022-04-07')}
+        />
+        <NextDatePicker
+          label="Date Picker"
+          inputFormat="MM - DD - YYYY"
+          defaultValue={dayjs('2022-04-07')}
+        />
       </Stack>
     </LocalizationProvider>
   );
