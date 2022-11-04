f5e7dd4a5d62b7a7573abc0c04a5b46521de0d30
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-11-04 11:43:32 +0100

    support digit update

--- packages/x-date-pickers/src/internals/hooks/useField/useField.utils.ts
+++ packages/x-date-pickers/src/internals/hooks/useField/useField.utils.ts
@@ -245,9 +245,10 @@ export const getSectionVisibleValue = (
   // If you press `1`, on the first section, the new value is also `1/dd/yyyy`,
   // So the browser will not fire the input `onChange`.
   // Adding the ltr mark is not a problem because it's only for digit (which are always ltr)
-  // if (willBeRenderedInInput && section.contentType === 'digit' && !section.hasTrailingZeroes) {
-  //   return `${value}\u200e`;
-  // }
+  // The \u2068 and \u2069 are cleaned, but not the \u200e to notice that an update with same digit occures
+  if (willBeRenderedInInput && section.contentType === 'digit' && !section.hasTrailingZeroes) {
+    return `\u2068${value}\u200e\u2069`;
+  }
 
   if (willBeRenderedInInput) {
     return `\u2068${value}\u2069`;
