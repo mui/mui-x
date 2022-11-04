e5772c2be5c5f8569ff6950f878acd3c417f7f11
Gavin Brennan <gavbrennan@hotmail.com>
2022-11-04 18:04:34 +0200

    [DataGridPro] Opt-out for column jump back on re-order (#6697)
    Signed-off-by: Gavin Brennan <gavbrennan@hotmail.com>

--- packages/grid/x-data-grid/src/DataGrid/useDataGridProps.ts
+++ packages/grid/x-data-grid/src/DataGrid/useDataGridProps.ts
@@ -26,6 +26,7 @@ const DATA_GRID_FORCED_PROPS: { [key in DataGridForcedPropsKey]?: DataGridProces
   checkboxSelectionVisibleOnly: false,
   disableColumnReorder: true,
   disableColumnResize: true,
+  keepColumnPositionIfDraggedOutside: false,
   signature: 'DataGrid',
 };
 
@@ -77,6 +78,7 @@ export const DATA_GRID_PROPS_DEFAULT_VALUES: DataGridPropsWithDefaultValues = {
   disableColumnReorder: false,
   disableColumnResize: false,
   keepNonExistentRowsSelected: false,
+  keepColumnPositionIfDraggedOutside: false,
 };
 
 export const useDataGridProps = <R extends GridValidRowModel>(inProps: DataGridProps<R>) => {
