a15597917760b349f56ba8753bfe4dff5b618ced
Andrew Cherniavskii <andrew@mui.com>
2022-11-11 18:31:58 +0100

    [core] Import from `@mui/utils` instead of `@mui/material/utils` (#6816)

--- packages/x-date-pickers/src/internals/components/wrappers/DesktopWrapper.tsx
+++ packages/x-date-pickers/src/internals/components/wrappers/DesktopWrapper.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useForkRef } from '@mui/material/utils';
+import { unstable_useForkRef as useForkRef } from '@mui/utils';
 import { WrapperVariantContext } from './WrapperVariantContext';
 import {
   PickersPopper,
