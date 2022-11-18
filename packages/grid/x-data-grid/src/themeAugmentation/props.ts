b6f903f127889c6e7a3b89fe4f0cb9562038f152
Matheus Wichman <matheushw@outlook.com>
2022-11-18 20:41:55 -0300

    Remove unused Theme

--- packages/grid/x-data-grid/src/themeAugmentation/props.ts
+++ packages/grid/x-data-grid/src/themeAugmentation/props.ts
@@ -1,4 +1,4 @@
-import { ComponentsOverrides, ComponentsProps, Theme } from '@mui/material/styles';
+import { ComponentsOverrides, ComponentsProps } from '@mui/material/styles';
 import { DataGridProps } from '../models/props/DataGridProps';
 
 export interface DataGridComponentsPropsList {
