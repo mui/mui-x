ae5e89f4872b504cd9722c870017ae473c938443
delangle <flaviendelangle@gmail.com>
2022-11-24 10:58:36 +0100

    Refacto

--- packages/x-date-pickers/src/tests/describe.types.ts
+++ packages/x-date-pickers/src/tests/describe.types.ts
@@ -6,3 +6,8 @@ export type PickerComponentFamily =
   | 'clock'
   | 'new-static-picker'
   | 'legacy-static-picker';
+
+export type PickerV6ComponentFamily = Exclude<
+  PickerComponentFamily,
+  'legacy-picker' | 'legacy-static-picker'
+>;
