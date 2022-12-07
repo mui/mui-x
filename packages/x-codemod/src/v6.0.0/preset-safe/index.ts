3b7496a6b76872886cbf64ebc1e670f66156f4d0
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2022-12-07 10:22:38 +0100

    [core] Add codemod to move l10n translation (#7027)

--- packages/x-codemod/src/v6.0.0/preset-safe/index.ts
+++ packages/x-codemod/src/v6.0.0/preset-safe/index.ts
@@ -1,8 +1,12 @@
 import transformLocalizationProviderRenameLocale from '../localization-provider-rename-locale';
+import transformTextPropsToLocaleText from '../text-props-to-localeText';
 import { JsCodeShiftAPI, JsCodeShiftFileInfo } from '../../types';
 
 export default function transformer(file: JsCodeShiftFileInfo, api: JsCodeShiftAPI, options: any) {
   file.source = transformLocalizationProviderRenameLocale(file, api, options);
+
+  // Should be run before the renaming of components such as `ClockPicker` to `TimeClock`.
+  file.source = transformTextPropsToLocaleText(file, api, options);
 
   return file.source;
 }
