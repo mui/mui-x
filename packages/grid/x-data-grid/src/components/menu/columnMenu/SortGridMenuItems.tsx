26fc29378ceace1d6078e843d2a95f8780f87fd6
hanbin9775 <dnffkrktyd@gmail.com>
2022-12-07 22:57:03 +0900

    [DataGrid] Display sort column menu items as per sortingOrder prop

--- packages/grid/x-data-grid/src/components/menu/columnMenu/SortGridMenuItems.tsx
+++ packages/grid/x-data-grid/src/components/menu/columnMenu/SortGridMenuItems.tsx
@@ -6,11 +6,14 @@ import { gridSortModelSelector } from '../../../hooks/features/sorting/gridSorti
 import { GridSortDirection } from '../../../models/gridSortModel';
 import { useGridApiContext } from '../../../hooks/utils/useGridApiContext';
 import { GridFilterItemProps } from './GridFilterItemProps';
+import { useGridRootProps } from '../../../hooks/utils/useGridRootProps';
 
-const SortGridMenuItems = (props: GridFilterItemProps) => {
+function SortGridMenuItems(props: GridFilterItemProps) {
   const { column, onClick } = props;
   const apiRef = useGridApiContext();
   const sortModel = useGridSelector(apiRef, gridSortModelSelector);
+  const rootProps = useGridRootProps();
+  const sortingOrder: GridSortDirection[] = column.sortingOrder ?? rootProps.sortingOrder;
 
   const sortDirection = React.useMemo(() => {
     if (!column) {
@@ -35,18 +38,28 @@ const SortGridMenuItems = (props: GridFilterItemProps) => {
 
   return (
     <React.Fragment>
-      <MenuItem onClick={onSortMenuItemClick} disabled={sortDirection == null}>
-        {apiRef.current.getLocaleText('columnMenuUnsort')}
-      </MenuItem>
-      <MenuItem onClick={onSortMenuItemClick} data-value="asc" disabled={sortDirection === 'asc'}>
-        {apiRef.current.getLocaleText('columnMenuSortAsc')}
-      </MenuItem>
-      <MenuItem onClick={onSortMenuItemClick} data-value="desc" disabled={sortDirection === 'desc'}>
-        {apiRef.current.getLocaleText('columnMenuSortDesc')}
-      </MenuItem>
+      {sortingOrder.includes(null) ? (
+        <MenuItem onClick={onSortMenuItemClick} disabled={sortDirection == null}>
+          {apiRef.current.getLocaleText('columnMenuUnsort')}
+        </MenuItem>
+      ) : null}
+      {sortingOrder.includes('asc') ? (
+        <MenuItem onClick={onSortMenuItemClick} data-value="asc" disabled={sortDirection === 'asc'}>
+          {apiRef.current.getLocaleText('columnMenuSortAsc')}
+        </MenuItem>
+      ) : null}
+      {sortingOrder.includes('desc') ? (
+        <MenuItem
+          onClick={onSortMenuItemClick}
+          data-value="desc"
+          disabled={sortDirection === 'desc'}
+        >
+          {apiRef.current.getLocaleText('columnMenuSortDesc')}
+        </MenuItem>
+      ) : null}
     </React.Fragment>
   );
-};
+}
 
 SortGridMenuItems.propTypes = {
   // ----------------------------- Warning --------------------------------
