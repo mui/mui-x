e3adfccb5929139d9e4b697fa5f747eff04617af
Nick Lucas <me@nicklucas.co.uk>
2022-12-13 12:54:51 +0000

    Add LiteralAnyString type to support autocomplete on GridColType while also supporting free-text strings

--- packages/grid/x-data-grid/src/models/colDef/gridColType.ts
+++ packages/grid/x-data-grid/src/models/colDef/gridColType.ts
@@ -7,4 +7,15 @@ export type GridNativeColTypes =
   | 'singleSelect'
   | 'actions';
 
-export type GridColType = GridNativeColTypes | string;
+/**
+ * Workaround for Typescript losing type information when a string literal is opened up to any string
+ * 
+ * Can be removed if this issue is ever resolved:
+ * https://github.com/Microsoft/TypeScript/issues/29729
+ */
+type LiteralAnyString = string & Record<never, never>
+
+// eslint-disable-next-line no-template-curly-in-string
+type LiteralAnyStringPrompt = '${string}'
+
+export type GridColType = GridNativeColTypes | LiteralAnyStringPrompt | LiteralAnyString;
