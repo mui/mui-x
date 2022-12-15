25cf94710cf3312efcbda9fd40f8fae17268fe65
Andrew Cherniavskyi <andrew@mui.com>
2022-12-15 15:25:18 +0100

    fix failing tests
    fix failing tests

--- packages/grid/x-data-grid/src/tests/keyboard.DataGrid.test.tsx
+++ packages/grid/x-data-grid/src/tests/keyboard.DataGrid.test.tsx
@@ -716,13 +716,13 @@ describe('<DataGrid /> - Keyboard', () => {
         <DataGrid rows={rows} columns={columns} />
       </div>,
     );
-    expect(renderCell.callCount).to.equal(4);
+    expect(renderCell.callCount).to.equal(6);
     const input = screen.getByTestId('custom-input');
     input.focus();
     fireEvent.keyDown(input, { key: 'a' });
-    expect(renderCell.callCount).to.equal(6);
+    expect(renderCell.callCount).to.equal(8);
     fireEvent.keyDown(input, { key: 'b' });
-    expect(renderCell.callCount).to.equal(6);
+    expect(renderCell.callCount).to.equal(8);
   });
 
   it('should not scroll horizontally when cell is wider than viewport', () => {
