4e78711df26a522100777cf9e5f6931742b4934e
alexandre <alex.fauquette@gmail.com>
2022-12-15 13:34:12 +0100

    add demo

--- docs/data/date-pickers/date-time-range-field/BasicDateTimeRangeField.js
+++ docs/data/date-pickers/date-time-range-field/BasicDateTimeRangeField.js
@@ -3,12 +3,21 @@ import { DemoContainer } from 'docsx/src/modules/components/DemoContainer';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Unstable_MultiInputDateTimeRangeField as MultiInputDateTimeRangeField } from '@mui/x-date-pickers-pro/MultiInputDateTimeRangeField';
+import { Unstable_SingleInputDateTimeRangeField as SingleInputDateTimeRangeField } from '@mui/x-date-pickers-pro/SingleInputDateTimeRangeField';
 
 export default function BasicDateTimeRangeField() {
+  const [value, setValue] = React.useState([null, null]);
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer>
-        <MultiInputDateTimeRangeField />
+        <MultiInputDateTimeRangeField
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        />
+        <SingleInputDateTimeRangeField
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        />
       </DemoContainer>
     </LocalizationProvider>
   );
