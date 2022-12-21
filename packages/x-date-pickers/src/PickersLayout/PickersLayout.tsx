282c8d1ebf0ef77ab2f575edfabca2e64edb53c6
alexandre <alex.fauquette@gmail.com>
2022-12-21 11:52:51 +0100

    scripts

--- packages/x-date-pickers/src/PickersLayout/PickersLayout.tsx
+++ packages/x-date-pickers/src/PickersLayout/PickersLayout.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { styled, useThemeProps } from '@mui/material/styles';
 import { PickersLayoutProps } from './PickersLayout.types';
 import { pickersLayoutClasses } from './pickersLayoutClasses';
 import usePickerLayout from './usePickerLayout';
 
-export const PickersLayoutRoot = styled('div', {
+const PickersLayoutRoot = styled('div', {
   name: 'MuiPickersLayout',
   slot: 'Root',
   overridesResolver: (props, styles) => styles.root,
@@ -13,7 +14,6 @@ export const PickersLayoutRoot = styled('div', {
   display: 'grid',
   gridAutoColumns: 'max-content auto max-content',
   gridAutoRows: 'max-content auto max-content',
-
   [`& .${pickersLayoutClasses.toolbar}`]: ownerState.isLandscape
     ? {
         gridColumn: theme.direction === 'rtl' ? 3 : 1,
@@ -22,6 +22,24 @@ export const PickersLayoutRoot = styled('div', {
     : { gridColumn: '1 / 4', gridRow: 1 },
   [`& .${pickersLayoutClasses.actionBar}`]: { gridColumn: '1 / 4', gridRow: 3 },
 }));
+
+PickersLayoutRoot.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  as: PropTypes.elementType,
+  ownerState: PropTypes.shape({
+    isLandscape: PropTypes.bool.isRequired,
+  }).isRequired,
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+} as any;
+
+export { PickersLayoutRoot };
 
 export const PickersLayoutContentWrapper = styled('div', {
   name: 'MuiPickersLayout',
@@ -34,7 +52,7 @@ export const PickersLayoutContentWrapper = styled('div', {
   flexDirection: 'column',
 });
 
-export const PickersLayout = React.forwardRef(function PickersLayout(
+const PickersLayout = React.forwardRef(function PickersLayout(
   inProps: PickersLayoutProps<any, any>,
   ref: React.Ref<HTMLDivElement>,
 ) {
@@ -59,3 +77,51 @@ export const PickersLayout = React.forwardRef(function PickersLayout(
     </PickersLayoutRoot>
   );
 });
+
+PickersLayout.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  children: PropTypes.node,
+  classes: PropTypes.object,
+  className: PropTypes.string,
+  /**
+   * Overrideable components.
+   * @default {}
+   */
+  components: PropTypes.object,
+  /**
+   * The props used for each component slot.
+   * @default {}
+   */
+  componentsProps: PropTypes.object,
+  disabled: PropTypes.bool,
+  isLandscape: PropTypes.bool.isRequired,
+  onAccept: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onClear: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onDismiss: PropTypes.func.isRequired,
+  onOpen: PropTypes.func.isRequired,
+  onSetToday: PropTypes.func.isRequired,
+  onViewChange: PropTypes.func.isRequired,
+  /**
+   * Force rendering in particular orientation.
+   */
+  orientation: PropTypes.oneOf(['landscape', 'portrait']),
+  readOnly: PropTypes.bool,
+  showToolbar: PropTypes.bool,
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  value: PropTypes.any.isRequired,
+  view: PropTypes.any.isRequired,
+  views: PropTypes.array.isRequired,
+  wrapperVariant: PropTypes.oneOf(['desktop', 'mobile']),
+} as any;
+
+export { PickersLayout };
