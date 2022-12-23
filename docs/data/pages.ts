48bea39f938b10c10a989c2d91f3b660a78df609
alexandre <alex.fauquette@gmail.com>
2022-12-23 12:05:49 +0100

    Jose feedbacks

--- docs/data/pages.ts
+++ docs/data/pages.ts
@@ -280,7 +280,7 @@ const pages: MuiPage[] = [
         ],
       },
       { pathname: '/x/react-date-pickers/fields', title: 'Field components', newFeature: true },
-      { pathname: '/x/react-date-pickers/custom-components', title: 'Custom sub-components' },
+      { pathname: '/x/react-date-pickers/custom-components', title: 'Custom subcomponents' },
       { pathname: '/x/react-date-pickers/custom-layout', title: 'Custom layout' },
       {
         pathname: '/x/api/date-pickers',
