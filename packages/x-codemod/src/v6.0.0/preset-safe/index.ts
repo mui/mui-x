0f6c7223bf512bd36aae4ffd3d9de2426a65fc85
delangle <flaviendelangle@gmail.com>
2022-12-29 10:05:42 +0100

    [codemod] Add new codemod for the value prop renaming on the view components

--- packages/x-codemod/src/v6.0.0/preset-safe/index.ts
+++ packages/x-codemod/src/v6.0.0/preset-safe/index.ts
@@ -1,13 +1,15 @@
 import transformLocalizationProviderRenameLocale from '../localization-provider-rename-locale';
 import transformTextPropsToLocaleText from '../text-props-to-localeText';
 import transformViewComponentsRename from '../view-components-rename';
+import transformViewComponentsRenameValueProp from '../view-components-rename-value-prop';
 import { JsCodeShiftAPI, JsCodeShiftFileInfo } from '../../types';
 
 export default function transformer(file: JsCodeShiftFileInfo, api: JsCodeShiftAPI, options: any) {
   file.source = transformLocalizationProviderRenameLocale(file, api, options);
 
-  // Should be run before the renaming of components such as `ClockPicker` to `TimeClock`.
+  // All the codemods impacting the view components should be run before rename these components
   file.source = transformTextPropsToLocaleText(file, api, options);
+  file.source = transformViewComponentsRenameValueProp(file, api, options);
 
   file.source = transformViewComponentsRename(file, api, options);
 
