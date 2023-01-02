1f50adef72d8f47a19559bff1d6f5e40babee877
Andrew Cherniavskyi <andrew@mui.com>
2023-01-02 21:12:23 +0100

    add default values for public types

--- packages/grid/x-data-grid/src/models/gridCellClass.ts
+++ packages/grid/x-data-grid/src/models/gridCellClass.ts
@@ -3,9 +3,9 @@ import { GridCellParams } from './params/gridCellParams';
 /**
  * A function used to process cellClassName params.
  */
-export type GridCellClassFn<V> = (params: GridCellParams<V>) => string;
+export type GridCellClassFn<V = any> = (params: GridCellParams<V>) => string;
 
 /**
  * The union type representing the [[GridColDef]] cell class type.
  */
-export type GridCellClassNamePropType<V> = string | GridCellClassFn<V>;
+export type GridCellClassNamePropType<V = any> = string | GridCellClassFn<V>;
