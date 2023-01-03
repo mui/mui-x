058b9434676735c2487cdc0716e2c33977d70540
delangle <flaviendelangle@gmail.com>
2023-01-03 14:25:16 +0100

    Regen doc

--- packages/x-date-pickers/src/DatePicker/DatePickerToolbar.tsx
+++ packages/x-date-pickers/src/DatePicker/DatePickerToolbar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
 import { styled, SxProps, Theme, useThemeProps } from '@mui/material/styles';
 import { unstable_composeClasses as composeClasses } from '@mui/utils';
@@ -45,6 +46,125 @@ const DatePickerToolbarTitle = styled(Typography, {
   }),
 }));
 
+DatePickerToolbarTitle.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * Set the text-align on the component.
+   * @default 'inherit'
+   */
+  align: PropTypes.oneOf(['center', 'inherit', 'justify', 'left', 'right']),
+  as: PropTypes.elementType,
+  className: PropTypes.string,
+  /**
+   * If `true`, the text will have a bottom margin.
+   * @default false
+   */
+  gutterBottom: PropTypes.bool,
+  /**
+   * If `true`, the text will not wrap, but instead will truncate with a text overflow ellipsis.
+   *
+   * Note that text overflow can only happen with block or inline-block level elements
+   * (the element needs to have a width in order to overflow).
+   * @default false
+   */
+  noWrap: PropTypes.bool,
+  ownerState: PropTypes.shape({
+    classes: PropTypes.object,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    isLandscape: PropTypes.bool.isRequired,
+    isMobileKeyboardViewOpen: PropTypes.bool,
+    onChange: PropTypes.func.isRequired,
+    onViewChange: PropTypes.func.isRequired,
+    readOnly: PropTypes.bool,
+    sx: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+      PropTypes.func,
+      PropTypes.object,
+    ]),
+    toggleMobileKeyboardView: PropTypes.func,
+    toolbarFormat: PropTypes.string,
+    toolbarPlaceholder: PropTypes.node,
+    value: PropTypes.any.isRequired,
+    view: PropTypes.oneOf(['day', 'month', 'year']).isRequired,
+    views: PropTypes.arrayOf(
+      PropTypes.oneOf(['day', 'hours', 'minutes', 'month', 'seconds', 'year']).isRequired,
+    ).isRequired,
+  }).isRequired,
+  /**
+   * If `true`, the element will be a paragraph element.
+   * @default false
+   */
+  paragraph: PropTypes.bool,
+  style: PropTypes.object,
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  /**
+   * Applies the theme typography styles.
+   * @default 'body1'
+   */
+  variant: PropTypes.oneOf([
+    'body1',
+    'body2',
+    'button',
+    'caption',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'inherit',
+    'overline',
+    'subtitle1',
+    'subtitle2',
+  ]),
+  /**
+   * The component maps the variant prop to a range of different HTML element types.
+   * For instance, subtitle1 to `<h6>`.
+   * If you wish to change that mapping, you can provide your own.
+   * Alternatively, you can use the `component` prop.
+   * @default {
+   *   h1: 'h1',
+   *   h2: 'h2',
+   *   h3: 'h3',
+   *   h4: 'h4',
+   *   h5: 'h5',
+   *   h6: 'h6',
+   *   subtitle1: 'h6',
+   *   subtitle2: 'h6',
+   *   body1: 'p',
+   *   body2: 'p',
+   *   inherit: 'p',
+   * }
+   */
+  variantMapping: PropTypes.shape({
+    body1: PropTypes.string,
+    body2: PropTypes.string,
+    button: PropTypes.string,
+    caption: PropTypes.string,
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+    h3: PropTypes.string,
+    h4: PropTypes.string,
+    h5: PropTypes.string,
+    h6: PropTypes.string,
+    inherit: PropTypes.string,
+    overline: PropTypes.string,
+    subtitle1: PropTypes.string,
+    subtitle2: PropTypes.string,
+  }),
+} as any;
+
 type DatePickerToolbarComponent = (<TDate>(
   props: DatePickerToolbarProps<TDate> & React.RefAttributes<HTMLDivElement>,
 ) => JSX.Element) & { propTypes?: any };
@@ -52,7 +172,7 @@ type DatePickerToolbarComponent = (<TDate>(
 /**
  * @ignore - internal component.
  */
-export const DatePickerToolbar = React.forwardRef(function DatePickerToolbar<TDate>(
+const DatePickerToolbar = React.forwardRef(function DatePickerToolbar<TDate>(
   inProps: DatePickerToolbarProps<TDate>,
   ref: React.Ref<HTMLDivElement>,
 ) {
@@ -121,3 +241,51 @@ export const DatePickerToolbar = React.forwardRef(function DatePickerToolbar<TDa
     </DatePickerToolbarRoot>
   );
 }) as DatePickerToolbarComponent;
+
+DatePickerToolbar.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  classes: PropTypes.object,
+  /**
+   * className applied to the root component.
+   */
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  isLandscape: PropTypes.bool.isRequired,
+  isMobileKeyboardViewOpen: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+  /**
+   * Callback called when a toolbar is clicked
+   * @template TView
+   * @param {TView} view The view to open
+   */
+  onViewChange: PropTypes.func.isRequired,
+  readOnly: PropTypes.bool,
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  toggleMobileKeyboardView: PropTypes.func,
+  /**
+   * Toolbar date format.
+   */
+  toolbarFormat: PropTypes.string,
+  /**
+   * Toolbar value placeholder—it is displayed when the value is empty.
+   * @default "––"
+   */
+  toolbarPlaceholder: PropTypes.node,
+  value: PropTypes.any,
+  /**
+   * Currently visible picker view.
+   */
+  view: PropTypes.oneOf(['day', 'month', 'year']).isRequired,
+  views: PropTypes.arrayOf(
+    PropTypes.oneOf(['day', 'hours', 'minutes', 'month', 'seconds', 'year']).isRequired,
+  ).isRequired,
+} as any;
+
+export { DatePickerToolbar };
