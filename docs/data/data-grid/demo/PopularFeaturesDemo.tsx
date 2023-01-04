7535fc09783a3d34b5f96b0ef45a668798aa9535
Matheus Wichman <matheushw@outlook.com>
2023-01-04 09:37:59 -0300

    Fix feature showcase demo

--- docs/data/data-grid/demo/PopularFeaturesDemo.tsx
+++ docs/data/data-grid/demo/PopularFeaturesDemo.tsx
@@ -393,7 +393,7 @@ export default function PopularFeaturesDemo() {
           [`& .${gridClasses.detailPanel}`]: {
             background: 'transparent',
           },
-          [`& .${gridClasses.cell}:focus, & .${gridClasses.cell}:focus-within`]: {
+          [`& .${gridClasses['cell--outlined']}`]: {
             outline: 'none',
           },
           [`& .${gridClasses.columnHeader}:focus, & .${gridClasses.columnHeader}:focus-within`]:
