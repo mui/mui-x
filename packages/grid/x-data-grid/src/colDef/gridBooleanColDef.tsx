292215ebb37a219aaeab96804b49c6e79cf70689
Andrew Cherniavskyi <andrew@mui.com>
2023-01-04 13:40:09 +0100

    serialize boolean to csv as true/false rather than yes/no

--- packages/grid/x-data-grid/src/colDef/gridBooleanColDef.tsx
+++ packages/grid/x-data-grid/src/colDef/gridBooleanColDef.tsx
@@ -6,10 +6,8 @@ import { gridNumberComparator } from '../hooks/features/sorting/gridSortingUtils
 import { getGridBooleanOperators } from './gridBooleanOperators';
 import { GridValueFormatterParams } from '../models/params/gridCellParams';
 
-function gridBooleanFormatter({ value, api }: GridValueFormatterParams) {
-  return value
-    ? api.getLocaleText('booleanCellTrueLabel')
-    : api.getLocaleText('booleanCellFalseLabel');
+function gridBooleanFormatter({ value }: GridValueFormatterParams) {
+  return String(value);
 }
 
 export const GRID_BOOLEAN_COL_DEF: GridColTypeDef<boolean | null, any> = {
