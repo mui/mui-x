e9474f738e99682933cd427278459e20766ef911
Lukas Tyla <llukas.tyla@gmail.com>
2023-01-11 00:23:00 +0200

    cleanup

--- docs/utils.js
+++ docs/utils.js
@@ -1,57 +1,32 @@
-const childProcess = require('child_process');
+const { Octokit } = require('@octokit/rest');
+const { retry } = require('@octokit/plugin-retry');
 
-/**
- * @param {string} command A shell command to execute
- * @return {Promise<string>} A promise that resolve to the output of the shell command, or an error
- * @example const output = await execute("ls -alh");
- */
-function execute(command) {
-  /**
-   * @param {Function} resolve A function that resolves the promise
-   * @param {Function} reject A function that fails the promise
-   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-   */
-  return new Promise((resolve, reject) => {
-    /**
-     * @param {Error} error An error triggered during the execution of the childProcess.exec command
-     * @param {string|Buffer} standardOutput The result of the shell command execution
-     * @param {string|Buffer} standardError The error resulting of the shell command execution
-     * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
-     */
-    childProcess.exec(command, (error, standardOutput, standardError) => {
-      if (error) {
-        reject();
-
-        return;
-      }
-
-      if (standardError) {
-        reject(standardError);
-
-        return;
-      }
-
-      resolve(standardOutput);
-    });
-  });
-}
+const MyOctokit = Octokit.plugin(retry);
 
 async function resolveGitRemoteUrl() {
-  try {
-    const result = await execute('git config --get remote.origin.url');
-    // eslint-disable-next-line no-console
-    console.info(result);
-    if (result.includes('git@')) {
-      return result.replace('git@', 'https://').replace('github.com:', 'github.com/');
-    }
-    return result;
-  } catch (err) {
-    console.warn('Unable to resolve git remote:', err);
-    return process.env.REPOSITORY_URL;
+  let remoteUrl = null;
+  if (!process.env.PULL_REQUEST_ID) {
+    console.warn(`Returning ${remoteUrl}, because PULL_REQUEST_ID=${process.env.PULL_REQUEST_ID}`)
+    return remoteUrl;
   }
+  const octokit = new MyOctokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+  try {
+    const prInfo = await octokit.request(`GET /repos/{owner}/{repo}/pulls/{pull_number}`, {
+      owner: 'mui',
+      repo: 'mui-x',
+      pull_number: process.env.PULL_REQUEST_ID,
+    });
+    console.warn(prInfo);
+    console.warn(prInfo.data.head.repo);
+    remoteUrl = `${prInfo.data.head.repo.owner.html_url}/${prInfo.data.head.repo.name}`;
+  } catch (err) {
+    console.warn('Failed to resolve remote URL from PR:', err);
+  }
+  return remoteUrl;
 }
 
 module.exports = {
-  execute,
   resolveGitRemoteUrl,
 };
