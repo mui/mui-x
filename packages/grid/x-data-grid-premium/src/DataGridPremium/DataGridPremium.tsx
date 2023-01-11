96225fd168c5d74ef7b9f952e09ecdcced8d72c1
Matheus Wichman <matheushw@outlook.com>
2023-01-11 10:01:39 -0300

    Revert "[DataGrid] Store the outlined cell in the state (#7111)"
    This reverts commit 0203346f5cb38b280026326dbd91254d584ecdc6.

--- packages/grid/x-data-grid-premium/src/DataGridPremium/DataGridPremium.tsx
+++ packages/grid/x-data-grid-premium/src/DataGridPremium/DataGridPremium.tsx
@@ -281,6 +281,7 @@ DataGridPremiumRaw.propTypes = {
     columnGrouping: PropTypes.bool,
     lazyLoading: PropTypes.bool,
     rowPinning: PropTypes.bool,
+    warnIfFocusStateIsNotSynced: PropTypes.bool,
   }),
   /**
    * Filtering can be processed on the server or client-side.
