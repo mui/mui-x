499c2888362fbacbce2c158cbc29a235d7430bc9
alexandre <alex.fauquette@gmail.com>
2023-01-12 13:17:00 +0100

    remove useless props

--- packages/x-date-pickers/src/PickersShortcuts/PickersShortcuts.tsx
+++ packages/x-date-pickers/src/PickersShortcuts/PickersShortcuts.tsx
@@ -2,53 +2,38 @@ import * as React from 'react';
 import List, { ListProps } from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Chip from '@mui/material/Chip';
-import { DateOrTimeView, MuiPickersAdapter } from '../internals';
-import { useUtils } from '../internals/hooks/useUtils';
 import { VIEW_HEIGHT } from '../internals/constants/dimensions';
-// import { useLocaleText } from '@mui/x-date-pickers/src/internals/hooks/useUtils';
 
-interface PickersShortcutsItemGetValueParams<TValue, TDate> {
-  value: TValue;
-  view: DateOrTimeView;
+interface PickersShortcutsItemGetValueParams<TValue> {
   isValid: (value: TValue) => boolean;
-  adapter: MuiPickersAdapter<TDate>;
 }
 
-export interface PickersShortcutsItem<TValue, TDate> {
+export interface PickersShortcutsItem<TValue> {
   label: string;
-  getValue: (params: PickersShortcutsItemGetValueParams<TValue, TDate>) => TValue;
+  getValue: (params: PickersShortcutsItemGetValueParams<TValue>) => TValue;
 }
 
-export interface PickersShortcutsProps<TValue, TDate, TView extends DateOrTimeView>
-  extends Omit<ListProps, 'onChange'> {
+export interface PickersShortcutsProps<TValue> extends Omit<ListProps, 'onChange'> {
   /**
    * Ordered array of shortcuts to display.
    * If empty, does not display the shortcuts.
    * @default `[]`
    */
-  shortcuts?: PickersShortcutsItem<TValue, TDate>[];
+  shortcuts?: PickersShortcutsItem<TValue>[];
   isLandscape: boolean;
   onChange: (newValue: TValue) => void;
-  value: TValue;
-  view: TView;
-  onViewChange?: (view: TView) => void;
-  views: readonly TView[];
   isValid: (value: TValue) => boolean;
 }
 
-export function PickersShortcuts<TValue, TDate, TView extends DateOrTimeView>(
-  props: PickersShortcutsProps<TValue, TDate, TView>,
-) {
-  const { shortcuts, isLandscape, onChange, value, view, onViewChange, isValid, views, ...other } =
-    props;
-  const utils = useUtils<TDate>();
+export function PickersShortcuts<TValue>(props: PickersShortcutsProps<TValue>) {
+  const { shortcuts, isLandscape, onChange, isValid, ...other } = props;
 
   if (shortcuts == null || shortcuts.length === 0) {
     return null;
   }
 
   const items = shortcuts.map((item) => {
-    const newValue = item.getValue({ value, view, isValid, adapter: utils });
+    const newValue = item.getValue({ isValid });
 
     return {
       label: item.label,
