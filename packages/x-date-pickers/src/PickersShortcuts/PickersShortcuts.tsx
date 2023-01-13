1197c75c0ad10accc3113207a8c8ac5b839b0501
alexandre <alex.fauquette@gmail.com>
2023-01-13 10:08:09 +0100

    add API page for shortcuts

--- packages/x-date-pickers/src/PickersShortcuts/PickersShortcuts.tsx
+++ packages/x-date-pickers/src/PickersShortcuts/PickersShortcuts.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import List, { ListProps } from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Chip from '@mui/material/Chip';
@@ -25,7 +26,7 @@ export interface PickersShortcutsProps<TValue> extends Omit<ListProps, 'onChange
   isValid: (value: TValue) => boolean;
 }
 
-export function PickersShortcuts<TValue>(props: PickersShortcutsProps<TValue>) {
+function PickersShortcuts<TValue>(props: PickersShortcutsProps<TValue>) {
   const { items, isLandscape, onChange, isValid, ...other } = props;
 
   if (items == null || items.length === 0) {
@@ -63,3 +64,52 @@ export function PickersShortcuts<TValue>(props: PickersShortcutsProps<TValue>) {
     </List>
   );
 }
+
+PickersShortcuts.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  className: PropTypes.string,
+  /**
+   * If `true`, compact vertical padding designed for keyboard and mouse input is used for
+   * the list and list items.
+   * The prop is available to descendant components as the `dense` context.
+   * @default false
+   */
+  dense: PropTypes.bool,
+  /**
+   * If `true`, vertical padding is removed from the list.
+   * @default false
+   */
+  disablePadding: PropTypes.bool,
+  isLandscape: PropTypes.bool.isRequired,
+  isValid: PropTypes.func.isRequired,
+  /**
+   * Ordered array of shortcuts to display.
+   * If empty, does not display the shortcuts.
+   * @default `[]`
+   */
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      getValue: PropTypes.func.isRequired,
+      label: PropTypes.string.isRequired,
+    }),
+  ),
+  onChange: PropTypes.func.isRequired,
+  style: PropTypes.object,
+  /**
+   * The content of the subheader, normally `ListSubheader`.
+   */
+  subheader: PropTypes.node,
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+} as any;
+
+export { PickersShortcuts };
