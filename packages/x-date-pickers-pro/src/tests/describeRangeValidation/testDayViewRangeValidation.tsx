f59879744876a61eb0ed38b931cb956edee46847
Matheus Wichman <matheushw@outlook.com>
2023-01-17 20:48:00 -0300

    Revert "[test] Fix date range picker tests to work with western time zones"
    This reverts commit 26bc17f595ea6dbf4622f5c1ebbb5fb5a1098c1a.

--- packages/x-date-pickers-pro/src/tests/describeRangeValidation/testDayViewRangeValidation.tsx
+++ packages/x-date-pickers-pro/src/tests/describeRangeValidation/testDayViewRangeValidation.tsx
@@ -157,11 +157,7 @@ export function testDayViewRangeValidation(ElementToTest, getOptions) {
       const yesterday = adapterToUse.addDays(now, -1);
 
       testDisabledDate(adapterToUse.format(now, 'dayOfMonth'), [false, false], isDesktop);
-      testDisabledDate(
-        adapterToUse.format(tomorrow, 'dayOfMonth'),
-        [!adapterToUse.isSameMonth(now, tomorrow), false],
-        isDesktop,
-      );
+      testDisabledDate(adapterToUse.format(tomorrow, 'dayOfMonth'), [false, false], isDesktop);
 
       if (!adapterToUse.isSameMonth(yesterday, tomorrow)) {
         setProps({ value: [yesterday, null] });
@@ -185,11 +181,7 @@ export function testDayViewRangeValidation(ElementToTest, getOptions) {
       const yesterday = adapterToUse.addDays(now, -1);
 
       testDisabledDate(adapterToUse.format(now, 'dayOfMonth'), [false, true], isDesktop);
-      testDisabledDate(
-        adapterToUse.format(tomorrow, 'dayOfMonth'),
-        [adapterToUse.isSameMonth(now, tomorrow), true],
-        isDesktop,
-      );
+      testDisabledDate(adapterToUse.format(tomorrow, 'dayOfMonth'), [true, true], isDesktop);
 
       if (!adapterToUse.isSameMonth(yesterday, tomorrow)) {
         setProps({ value: [yesterday, null] });
