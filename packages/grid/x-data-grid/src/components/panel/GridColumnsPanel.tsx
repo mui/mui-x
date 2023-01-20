3ae00cef7e22be6185cae388dc688ec6961c1c96
Andrew Cherniavskyi <andrew@mui.com>
2023-01-20 15:05:43 +0100

    Revert "fix failing unit tests"
    This reverts commit 722a68c9a798f0bc056e74429a716eda04ccb48a.

--- packages/grid/x-data-grid/src/components/panel/GridColumnsPanel.tsx
+++ packages/grid/x-data-grid/src/components/panel/GridColumnsPanel.tsx
@@ -138,21 +138,15 @@ function GridColumnsPanel(props: GridColumnsPanelProps) {
 
   const toggleAllColumns = React.useCallback(
     (isVisible: boolean) => {
-      const currentModel = gridColumnVisibilityModelSelector(apiRef);
-      const newModel = { ...currentModel };
+      if (isVisible) {
+        return apiRef.current.setColumnVisibilityModel({});
+      }
 
-      columns.forEach((col) => {
-        if (col.hideable !== false) {
-          if (isVisible === true) {
-            // delete the key from the model instead of setting it to `true`
-            delete newModel[col.field];
-          } else {
-            newModel[col.field] = false;
-          }
-        }
-      });
-
-      return apiRef.current.setColumnVisibilityModel(newModel);
+      return apiRef.current.setColumnVisibilityModel(
+        Object.fromEntries(
+          columns.filter((col) => col.hideable !== false).map((col) => [col.field, false]),
+        ),
+      );
     },
     [apiRef, columns],
   );
