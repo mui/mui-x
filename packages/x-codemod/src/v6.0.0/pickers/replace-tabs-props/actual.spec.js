17543313b2209c80306363721b57c576ecb2d0ff
alexandre <alex.fauquette@gmail.com>
2023-01-23 13:51:24 +0100

    [codemod] Update props for pickers tabs

---
+++ packages/x-codemod/src/v6.0.0/pickers/replace-tabs-props/actual.spec.js
@@ -0,0 +1,19 @@
+<DateTimePicker
+  componentsProps={{
+    tabs: {
+      dateRangeIcon: <LightModeIcon />,
+    },
+  }}
+/>;
+
+<DateTimePicker
+  componentsProps={{
+    tabs: {
+      dateRangeIcon: <LightModeIcon />,
+    },
+  }}
+>
+  ChildrenString
+</DateTimePicker>;
+
+<DateTimePicker hideTabs={false} dateRangeIcon={<LightModeIcon />} timeIcon={<AcUnitIcon />} />;
---
+++ packages/x-codemod/src/v6.0.0/pickers/replace-tabs-props/expected.spec.js
@@ -0,0 +1,26 @@
+<DateTimePicker
+  componentsProps={{
+    tabs: {
+      dateIcon: <LightModeIcon />,
+    },
+  }}
+/>;
+
+<DateTimePicker
+  componentsProps={{
+    tabs: {
+      dateIcon: <LightModeIcon />,
+    },
+  }}
+>
+  ChildrenString
+</DateTimePicker>;
+
+<DateTimePicker
+  componentsProps={{
+    tabs: {
+      hidden: false,
+      dateIcon: <LightModeIcon />,
+      timeIcon: <AcUnitIcon />,
+    },
+  }} />;
