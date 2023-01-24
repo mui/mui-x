4498995189774212e3f7057d64f94f1bd4fabe46
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2023-01-24 14:52:09 +0100

    [codemod] Update `DateTimePicker` component `tabs` props (#7639)
    Co-authored-by: Lukas <llukas.tyla@gmail.com>

--- packages/x-codemod/src/v6.0.0/pickers/preset-safe/expected.spec.js
+++ packages/x-codemod/src/v6.0.0/pickers/preset-safe/expected.spec.js
@@ -3,6 +3,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 
 const theme = createTheme({});
@@ -16,6 +17,13 @@ function App() {
           localeText={{
             cancelButtonLabel: custom_cancelText,
             okButtonLabel: "string_okText"
+          }} />
+        <DateTimePicker
+          componentsProps={{
+            tabs: {
+              hidden: false,
+              dateIcon: <LightModeIcon />,
+            },
           }} />
       </LocalizationProvider>
     </ThemeProvider>
