4e512a4b0b4f29ba7abe0fde1cf4e0959cf216fb
Vivek-Prajapatii <prajapativivek415@gmail.com>
2023-01-27 22:08:21 +0530

    issues fixed

--- packages/grid/x-data-grid/src/components/panel/GridPanelWrapper.tsx
+++ packages/grid/x-data-grid/src/components/panel/GridPanelWrapper.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import clsx from 'clsx';
-import TrapFocus from '@mui/material/Unstable_TrapFocus';
+import TrapFocus, { TrapFocusProps } from '@mui/material/Unstable_TrapFocus';
 import { styled, Theme } from '@mui/material/styles';
 import { MUIStyledCommonProps } from '@mui/system';
 import { unstable_composeClasses as composeClasses } from '@mui/utils';
@@ -36,18 +36,20 @@ const GridPanelWrapperRoot = styled('div', {
 const isEnabled = () => true;
 
 export interface GridPanelWrapperProps
-  extends React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>,
+  extends React.PropsWithChildren<
+      React.HTMLAttributes<HTMLDivElement> & { TrapFocusProps: Omit<TrapFocusProps, 'open'> }
+    >,
     MUIStyledCommonProps<Theme> {}
 
 const GridPanelWrapper = React.forwardRef<HTMLDivElement, GridPanelWrapperProps>(
   function GridPanelWrapper(props, ref) {
-    const { className, ...other } = props;
+    const { className, TrapFocusProps: TrapFocusPropsProps, ...other } = props;
     const rootProps = useGridRootProps();
     const ownerState = { classes: rootProps.classes };
     const classes = useUtilityClasses(ownerState);
 
     return (
-      <TrapFocus open disableEnforceFocus isEnabled={isEnabled}>
+      <TrapFocus open disableEnforceFocus isEnabled={isEnabled} {...TrapFocusPropsProps}>
         <GridPanelWrapperRoot
           ref={ref}
           tabIndex={-1}
