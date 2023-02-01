13fc5d50470a56aaa9d5e60f3b092a59ac687c56
Bilal Shafi <bilalshafidev@gmail.com>
2023-02-01 16:15:03 +0500

    [codemod] Replace onCellFocusOut prop for Data Grid

---
+++ packages/x-codemod/src/v6.0.0/data-grid/replace-onCellFocusOut-prop/actual.spec.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { DataGridPro, GridEventListener } from '@mui/x-data-grid-pro';
+import { DataGridPremium } from '@mui/x-data-grid-premium';
+
+function App() {
+  const handleCellFocusOut: GridEventListener<'cellFocusOut'> = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+  return (
+    <React.Fragment>
+      <DataGrid 
+        onCellFocusOut={(event) => {
+          const cellElement = event.currentTarget;
+          const field = cellElement.getAttribute('data-field');
+          const rowId = cell.parentElement.getAttribute('data-id');
+        }}
+      />
+      <DataGridPro onCellFocusOut={handleCellFocusOut} />
+      <DataGridPremium onCellFocusOut={handleCellFocusOut} />
+    </React.Fragment>
+  );
+}
+
+export default App;
