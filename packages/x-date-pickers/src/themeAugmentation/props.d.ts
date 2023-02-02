efa0fbe8b17af459dc7083a2b776bf2cb46d0c14
Lukas <llukas.tyla@gmail.com>
2023-02-02 16:19:51 +0200

    [pickers] Fix theme augmentation issues (#7800)

--- packages/x-date-pickers/src/themeAugmentation/props.d.ts
+++ packages/x-date-pickers/src/themeAugmentation/props.d.ts
@@ -12,7 +12,6 @@ import { DateFieldProps } from '../DateField';
 import { LocalizationProviderProps } from '../LocalizationProvider';
 import { PickersLayoutProps } from '../PickersLayout';
 import {
-  CalendarOrClockPickerProps,
   DayCalendarProps,
   ExportedPickersArrowSwitcherProps,
   PickerPopperProps,
@@ -43,7 +42,6 @@ import { MobileTimePickerProps } from '../MobileTimePicker';
 import { StaticTimePickerProps } from '../StaticTimePicker';
 
 export interface PickersComponentsPropsList {
-  MuiCalendarOrClockPicker: CalendarOrClockPickerProps<unknown, DateOrTimeView>;
   MuiClock: ClockProps<unknown>;
   MuiClockNumber: ClockNumberProps;
   MuiClockPointer: ClockPointerProps;
@@ -63,7 +61,7 @@ export interface PickersComponentsPropsList {
   MuiPickersMonth: ExportedPickersMonthProps;
   MuiPickersPopper: PickerPopperProps;
   MuiPickersSlideTransition: ExportedSlideTransitionProps;
-  MuiPickersToolbar: PickersToolbarProps<unknown, unknown>;
+  MuiPickersToolbar: PickersToolbarProps<unknown, DateOrTimeView>;
   MuiPickersToolbarButton: PickersToolbarButtonProps;
   MuiPickersToolbarText: ExportedPickersToolbarTextProps;
   MuiPickersLayout: PickersLayoutProps<unknown, unknown, DateOrTimeView>;
