59283a555f084cfa0aec5405d44b83e261250cc0
Bilal Shafi <bilalshafidev@gmail.com>
2023-02-06 19:46:59 +0500

    [codemod] Codemod to remove unnecessary `experimentalFeatures` flag (#7836)

--- packages/x-codemod/src/v6.0.0/data-grid/preset-safe/actual.spec.js
+++ packages/x-codemod/src/v6.0.0/data-grid/preset-safe/actual.spec.js
@@ -15,6 +15,7 @@ function App({ column, hideMenu, apiRef, handleEvent }) {
         showCellRightBorder
         disableExtendRowFullWidth
         rowsPerPageOptions={[5]}
+        experimentalFeatures={{ newEditingApi: true }}
         onCellFocusOut={handleCellFocusOut}
       />
       <SortGridMenuItems column={column} onClick={hideMenu} />
