5cc8deb519a1538ab9e19e2d7a0ec9ed99d3afa6
alexandre <alex.fauquette@gmail.com>
2023-02-07 13:48:58 +0100

    add dager scipt preview

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,14 +1,23 @@
-const { markdown, message } = require('danger');
+const { danger, markdown } = require('danger');
 const fse = require('fs-extra');
 const path = require('path');
 const prettier = require('prettier');
 
 const dangerCommand = process.env.DANGER_COMMAND;
 
+type SnapshotType = {
+  [name: string]: {
+    min: number;
+    max: number;
+    mean: number;
+    median: number;
+    stdDev: number;
+  };
+};
 async function reportBundleSize() {
   const snapshotPath = path.join(__dirname, './performance-snapshot.json');
   const prettierConfigPath = path.join(__dirname, './prettier.config.js');
-  const snapshot = await fse.readJSON(snapshotPath);
+  const snapshot: SnapshotType = await fse.readJSON(snapshotPath);
 
   const headers = `
 | Test case | Unit | Min | Max | Median | Mean | σ |
@@ -35,9 +44,51 @@ ${headers}\n`;
   markdown(prettier.format(text, { prettierConfig, parser: 'markdown' }));
 }
 
+function addDeployPreviewUrls() {
+  /**
+   * The incoming docsPath from danger does not start with `/`
+   * e.g. ['docs/data/data-grid/editing/editing.md']
+   */
+  function formatFileToLink(docsPath: string) {
+    const url = docsPath.replace('docs/data', 'x/').replace(/\/[^/]+\.md$/, '/');
+
+    return url
+      .replace('data-grid/', 'react-data-grid/')
+      .replace('date-pickers/', 'react-date-pickers')
+      .replace(/\/[^/]+\.md$/, '/');
+  }
+
+  const netlifyPreview = `https://deploy-preview-${danger.github.pr.number}--material-ui-x.netlify.app/`;
+
+  const files = [...danger.git.created_files, ...danger.git.modified_files];
+
+  // limit to the first 5 docs
+  const docs = files
+    .filter((file) => file.startsWith('docs/data') && file.endsWith('.md'))
+    .slice(0, 5);
+
+  markdown(`
+## Netlify deploy preview
+
+Netlify deploy preview: <a href="${netlifyPreview}">${netlifyPreview}</a>
+
+### Update pages
+
+${
+  docs.length
+    ? docs
+        .map((docsPath) => {
+          const formattedUrl = formatFileToLink(docsPath);
+          return `- [${docsPath}](${netlifyPreview}${formattedUrl})`;
+        })
+        .join('\n')
+    : 'No updates.'
+}
+`);
+}
+
 async function run() {
-  const netlifyPreview = `https://deploy-preview-${process.env.CIRCLE_PR_NUMBER}--material-ui-x.netlify.app/`;
-  message(`Netlify deploy preview: <a href="${netlifyPreview}">${netlifyPreview}</a>`);
+  addDeployPreviewUrls();
 
   switch (dangerCommand) {
     case 'reportPerformance':
