4e26fbcd304ba4cabc0c6568968ea963884c7c22
alexandre <alex.fauquette@gmail.com>
2023-02-09 10:28:28 +0100

    [core] Add test on value timezone

---
+++ packages/x-date-pickers/src/DateTimeField/tests/timezone.DateTimeField.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { DateTimeField } from '@mui/x-date-pickers/DateTimeField';
+import { screen, act, userEvent, fireEvent } from '@mui/monorepo/test/utils';
+import { createPickerRenderer, expectInputValue } from 'test/utils/pickers-utils';
+
+describe('<DateTimeField /> - TimeZone', () => {
+  describe('Value time-zone modification - Luxon', () => {
+    const { render, adapter } = createPickerRenderer({ clock: 'fake', adapterName: 'luxon' });
+    it('should update the field when dayjs', () => {
+      const { setProps } = render(<DateTimeField />);
+      const input = screen.getByRole('textbox');
+
+      const date = adapter.date(new Date(2020, 5, 18, 16, 30, 10));
+      setProps({ value: date });
+
+      expectInputValue(input, '6 / 18 / 2020 04:30 PM');
+
+      setProps({ value: date.setZone('America/Los_Angeles') });
+
+      expectInputValue(input, '6 / 18 / 2020 07:30 AM');
+    });
+  });
+});
