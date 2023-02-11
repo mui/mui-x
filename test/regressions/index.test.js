499ef28cc518517f6b9ab989486948da7d5ecc40
Bilal Shafi <bilalshafidev@gmail.com>
2023-02-11 15:31:57 +0500

    Try to fix regression

--- test/regressions/index.test.js
+++ test/regressions/index.test.js
@@ -174,6 +174,8 @@ async function main() {
         link.click();
       });
 
+      await sleep(1000);
+
       // Click the export button in the toolbar.
       await page.$eval(`button[aria-label="Export"]`, (exportButton) => {
         exportButton.click();
