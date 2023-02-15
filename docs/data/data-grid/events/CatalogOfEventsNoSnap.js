abc7f7a6555df4e98fddd29df1db5a4e0c04cd8c
Flavien DELANGLE <flaviendelangle@gmail.com>
2023-02-15 12:44:21 +0100

    [docs] Add missing param in `useGridApiEventHandler` examples (#7939)

--- docs/data/data-grid/events/CatalogOfEventsNoSnap.js
+++ docs/data/data-grid/events/CatalogOfEventsNoSnap.js
@@ -58,7 +58,7 @@ apiRef.current.subscribeEvent(
 );
 
 // Hook subscription (only available inside the scope of the grid)
-useGridApiEventHandler('${event.name}', handleEvent);
+useGridApiEventHandler(apiRef, '${event.name}', handleEvent);
 ${propExample}
 `;
   }, [event]);
