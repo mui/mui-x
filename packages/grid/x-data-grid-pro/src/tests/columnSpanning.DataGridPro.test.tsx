6ca0e1a766b4d16bd1330cc5385375dfbaa3790a
Andrew Cherniavskyi <andrew@mui.com>
2023-03-02 02:58:00 +0100

    avoid fake clock

--- packages/grid/x-data-grid-pro/src/tests/columnSpanning.DataGridPro.test.tsx
+++ packages/grid/x-data-grid-pro/src/tests/columnSpanning.DataGridPro.test.tsx
@@ -7,7 +7,7 @@ import { getActiveCell, getCell, getColumnHeaderCell } from 'test/utils/helperFn
 const isJSDOM = /jsdom/.test(window.navigator.userAgent);
 
 describe('<DataGridPro /> - Column Spanning', () => {
-  const { render } = createRenderer({ clock: 'fake' });
+  const { render } = createRenderer();
 
   const baselineProps = {
     rows: [
