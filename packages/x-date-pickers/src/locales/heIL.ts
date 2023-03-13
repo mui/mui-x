61dc75c92294db9e124174b1796650adab361d05
alexandre <alex.fauquette@gmail.com>
2023-03-13 11:28:47 +0100

    fixes

--- packages/x-date-pickers/src/locales/heIL.ts
+++ packages/x-date-pickers/src/locales/heIL.ts
@@ -1,47 +1,45 @@
-import { PickersLocaleText } from "./utils/pickersLocaleTextApi";
-import { getPickersLocalization } from "./utils/getPickersLocalization";
+import { PickersLocaleText } from './utils/pickersLocaleTextApi';
+import { getPickersLocalization } from './utils/getPickersLocalization';
 
 const views = {
-  hours: "שעות",
-  minutes: "דקות",
-  seconds: "שניות",
+  hours: 'שעות',
+  minutes: 'דקות',
+  seconds: 'שניות',
 };
 
 const heILPickers: Partial<PickersLocaleText<any>> = {
   // Calendar navigation
-  previousMonth: "חודש קודם",
-  nextMonth: "חודש הבא",
+  previousMonth: 'חודש קודם',
+  nextMonth: 'חודש הבא',
 
   // View navigation
-  openPreviousView: "תצוגה קודמת",
-  openNextView: "תצוגה הבאה",
+  openPreviousView: 'תצוגה קודמת',
+  openNextView: 'תצוגה הבאה',
   calendarViewSwitchingButtonAriaLabel: (view) =>
-    view === "year"
-      ? "תצוגת שנה פתוחה, מעבר לתצוגת לוח שנה"
-      : "תצוגת לוח שנה פתוחה, מעבר לתצוגת שנה",
+    view === 'year'
+      ? 'תצוגת שנה פתוחה, מעבר לתצוגת לוח שנה'
+      : 'תצוגת לוח שנה פתוחה, מעבר לתצוגת שנה',
 
   // DateRange placeholders
-  start: "תחילה",
-  end: "סיום",
+  start: 'תחילה',
+  end: 'סיום',
 
   // Action bar
-  cancelButtonLabel: "ביטול",
-  clearButtonLabel: "ניקוי",
-  okButtonLabel: "אישור",
-  todayButtonLabel: "היום",
+  cancelButtonLabel: 'ביטול',
+  clearButtonLabel: 'ניקוי',
+  okButtonLabel: 'אישור',
+  todayButtonLabel: 'היום',
 
   // Toolbar titles
-  datePickerToolbarTitle: "בחירת תאריך",
-  dateTimePickerToolbarTitle: "בחירת תאריך ושעה",
-  timePickerToolbarTitle: "בחירת שעה",
-  dateRangePickerToolbarTitle: "בחירת טווח תאריכים",
+  datePickerToolbarTitle: 'בחירת תאריך',
+  dateTimePickerToolbarTitle: 'בחירת תאריך ושעה',
+  timePickerToolbarTitle: 'בחירת שעה',
+  dateRangePickerToolbarTitle: 'בחירת טווח תאריכים',
 
   // Clock labels
   clockLabelText: (view, time, adapter) =>
     `בחירת ${views[view]}. ${
-      time === null
-        ? "לא נבחרה שעה"
-        : `השעה הנבחרת היא ${adapter.format(time, "fullTime")}`
+      time === null ? 'לא נבחרה שעה' : `השעה הנבחרת היא ${adapter.format(time, 'fullTime')}`
     }`,
   hoursClockNumberText: (hours) => `${hours} שעות`,
   minutesClockNumberText: (minutes) => `${minutes} דקות`,
@@ -56,16 +54,16 @@ const heILPickers: Partial<PickersLocaleText<any>> = {
   // Open picker labels
   openDatePickerDialogue: (value, utils) =>
     value !== null && utils.isValid(value)
-      ? `בחירת תאריך, התאריך שנבחר הוא ${utils.format(value, "fullDate")}`
-      : "בחירת תאריך",
+      ? `בחירת תאריך, התאריך שנבחר הוא ${utils.format(value, 'fullDate')}`
+      : 'בחירת תאריך',
   openTimePickerDialogue: (value, utils) =>
     value !== null && utils.isValid(value)
-      ? `בחירת שעה, השעה שנבחרה היא ${utils.format(value, "fullTime")}`
-      : "בחירת שעה",
+      ? `בחירת שעה, השעה שנבחרה היא ${utils.format(value, 'fullTime')}`
+      : 'בחירת שעה',
 
   // Table labels
-  timeTableLabel: "בחירת שעה",
-  dateTableLabel: "בחירת תאריך",
+  timeTableLabel: 'בחירת שעה',
+  dateTableLabel: 'בחירת תאריך',
 
   // Field section placeholders
   // fieldYearPlaceholder: params => 'Y'.repeat(params.digitAmount),
