14656076f03130ffc3b19407c9c305eaee5017c4
Lukas <llukas.tyla@gmail.com>
2023-03-17 08:57:13 +0200

    Play with digital clock width

--- packages/x-date-pickers/src/DigitalClock/DigitalClock.tsx
+++ packages/x-date-pickers/src/DigitalClock/DigitalClock.tsx
@@ -32,6 +32,7 @@ const DigitalClockRoot = styled(PickerViewRoot, {
   overridesResolver: (props, styles) => styles.root,
 })<{ ownerState: DigitalClockProps<any> }>({
   overflowY: 'auto',
+  width: '100%'
 });
 
 const DigitalClockList = styled(MenuList, {
