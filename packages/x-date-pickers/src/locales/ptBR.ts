96602feff5ffeab609dbf94a30a15129af73e0d8
Axspeo <13906720+Pwnedev@users.noreply.github.com>
2023-04-02 17:58:47 +0100

    x-date-pickers: ran prettier

--- packages/x-date-pickers/src/locales/ptBR.ts
+++ packages/x-date-pickers/src/locales/ptBR.ts
@@ -44,8 +44,8 @@ const ptBRPickers: Partial<PickersLocaleText<any>> = {
   // Calendar labels
   calendarWeekNumberHeaderLabel: 'Número da semana',
   calendarWeekNumberHeaderText: '#',
-  calendarWeekNumberAriaLabelText: weekNumber => `Semana ${weekNumber}`,
-  calendarWeekNumberText: weekNumber => `${weekNumber}`,
+  calendarWeekNumberAriaLabelText: (weekNumber) => `Semana ${weekNumber}`,
+  calendarWeekNumberText: (weekNumber) => `${weekNumber}`,
 
   // Open picker labels
   openDatePickerDialogue: (value, utils) =>
@@ -62,10 +62,10 @@ const ptBRPickers: Partial<PickersLocaleText<any>> = {
   dateTableLabel: 'escolha uma data',
 
   // Field section placeholders
-  fieldYearPlaceholder: params => 'A'.repeat(params.digitAmount),
-  fieldMonthPlaceholder: params => params.contentType === 'letter' ? 'MMMM' : 'MM',
+  fieldYearPlaceholder: (params) => 'A'.repeat(params.digitAmount),
+  fieldMonthPlaceholder: (params) => (params.contentType === 'letter' ? 'MMMM' : 'MM'),
   fieldDayPlaceholder: () => 'DD',
-  fieldWeekDayPlaceholder: params => params.contentType === 'letter' ? 'SSSS' : 'SS',
+  fieldWeekDayPlaceholder: (params) => (params.contentType === 'letter' ? 'SSSS' : 'SS'),
   fieldHoursPlaceholder: () => 'hh',
   fieldMinutesPlaceholder: () => 'mm',
   fieldSecondsPlaceholder: () => 'ss',
