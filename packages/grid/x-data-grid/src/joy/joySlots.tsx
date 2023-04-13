687d6a2ba33016f8bdea0a1a34de04971349c6a1
siriwatknp <siriwatkunaporn@gmail.com>
2023-04-13 13:06:50 +0700

    add joy button slot

--- packages/grid/x-data-grid/src/joy/joySlots.tsx
+++ packages/grid/x-data-grid/src/joy/joySlots.tsx
@@ -1,10 +1,45 @@
 import * as React from 'react';
+import { SxProps } from '@mui/system';
+import { ColorPaletteProp, Theme, VariantProp } from '@mui/joy/styles';
 import JoyCheckbox from '@mui/joy/Checkbox';
 import JoyInput from '@mui/joy/Input';
 import JoyFormControl from '@mui/joy/FormControl';
 import JoyFormLabel from '@mui/joy/FormLabel';
+import JoyButton from '@mui/joy/Button';
 import type { UncapitalizeObjectKeys } from '../internals/utils';
 import type { GridSlotsComponent, GridSlotsComponentsProps } from '../models';
+
+function convertColor<
+  T extends 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning' | 'inherit',
+>(color: T | undefined) {
+  if (color === 'secondary') {
+    return 'primary';
+  }
+  if (color === 'error') {
+    return 'danger';
+  }
+  return color as ColorPaletteProp;
+}
+
+function convertSize<T extends 'small' | 'medium' | 'large'>(size: T | undefined) {
+  return (size ? { small: 'sm', medium: 'md', large: 'lg' }[size] : size) as
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | undefined;
+}
+
+function convertVariant<T extends 'outlined' | 'contained' | 'text'>(variant: T | undefined) {
+  return (
+    variant
+      ? {
+          outlined: 'outlined',
+          contained: 'solid',
+          text: 'plain',
+        }[variant]
+      : variant
+  ) as VariantProp;
+}
 
 const Checkbox = React.forwardRef<
   HTMLElement,
@@ -45,13 +80,31 @@ const TextField = React.forwardRef<
   );
 });
 
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  NonNullable<GridSlotsComponentsProps['baseButton']>
+>(function Button({ startIcon, color, endIcon, size, sx, variant, ...props }, ref) {
+  return (
+    <JoyButton
+      {...props}
+      size={convertSize(size)}
+      color={convertColor(color)}
+      variant={convertVariant(variant)}
+      ref={ref}
+      startDecorator={startIcon}
+      endDecorator={endIcon}
+      sx={sx as SxProps<Theme>}
+    />
+  );
+});
+
 const joySlots: UncapitalizeObjectKeys<Partial<GridSlotsComponent>> = {
   baseCheckbox: Checkbox,
   baseTextField: TextField,
+  baseButton: Button,
   // BaseFormControl: MUIFormControl,
   // BaseSelect: MUISelect,
   // BaseSwitch: MUISwitch,
-  // BaseButton: MUIButton,
   // BaseIconButton: MUIIconButton,
   // BaseTooltip: MUITooltip,
   // BasePopper: MUIPopper,
