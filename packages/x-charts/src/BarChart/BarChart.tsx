c2ee17249bd2577cfc0e0df746231aae34a24189
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2023-04-18 09:56:13 +0200

    [charts] Support Tooltip (#8356)
    Co-authored-by: Lukas <llukas.tyla@gmail.com>

--- packages/x-charts/src/BarChart/BarChart.tsx
+++ packages/x-charts/src/BarChart/BarChart.tsx
@@ -2,56 +2,40 @@ import * as React from 'react';
 import { BarPlot } from './BarPlot';
 import { XAxis } from '../XAxis/XAxis';
 import { YAxis } from '../YAxis/YAxis';
-import {
-  SeriesContextProvider,
-  SeriesContextProviderProps,
-} from '../context/SeriesContextProvider';
-import { DrawingProvider } from '../context/DrawingProvider';
-import {
-  CartesianContextProvider,
-  CartesianContextProviderProps,
-} from '../context/CartesianContextProvider';
-import Surface from '../Surface';
 import { DEFAULT_X_AXIS_KEY, DEFAULT_Y_AXIS_KEY } from '../constants';
-import { LayoutConfig } from '../models/layout';
+import { ChartContainer, ChartContainerProps } from '../ChartContainer';
 
-export function BarChart(
-  props: Omit<
-    LayoutConfig & SeriesContextProviderProps & CartesianContextProviderProps,
-    'children'
-  >,
-) {
-  const { xAxis, yAxis, series, colors, width, height, margin } = props;
+export function BarChart(props: Omit<ChartContainerProps, 'children'>) {
+  const { xAxis, yAxis, series, width, height, margin, colors, tooltip } = props;
 
   return (
-    <DrawingProvider width={width} height={height} margin={margin}>
-      <SeriesContextProvider series={series} colors={colors}>
-        <CartesianContextProvider xAxis={xAxis} yAxis={yAxis}>
-          <Surface width={width} height={height}>
-            <BarPlot />
-            <XAxis
-              label="Bottom X axis"
-              position="bottom"
-              axisId={xAxis?.[0]?.id ?? DEFAULT_X_AXIS_KEY}
-            />
-            <XAxis
-              label="Top X axis"
-              position="top"
-              axisId={xAxis?.[1]?.id ?? xAxis?.[0]?.id ?? DEFAULT_X_AXIS_KEY}
-            />
-            <YAxis
-              label="Left Y axis"
-              position="left"
-              axisId={yAxis?.[0]?.id ?? DEFAULT_Y_AXIS_KEY}
-            />
-            <YAxis
-              label="Right Y axis"
-              position="right"
-              axisId={yAxis?.[1]?.id ?? yAxis?.[0]?.id ?? DEFAULT_Y_AXIS_KEY}
-            />
-          </Surface>
-        </CartesianContextProvider>
-      </SeriesContextProvider>
-    </DrawingProvider>
+    <ChartContainer
+      series={series}
+      width={width}
+      height={height}
+      margin={margin}
+      xAxis={xAxis}
+      yAxis={yAxis}
+      colors={colors}
+      tooltip={tooltip}
+    >
+      <BarPlot />
+      <XAxis
+        label="Bottom X axis"
+        position="bottom"
+        axisId={xAxis?.[0]?.id ?? DEFAULT_X_AXIS_KEY}
+      />
+      <XAxis
+        label="Top X axis"
+        position="top"
+        axisId={xAxis?.[1]?.id ?? xAxis?.[0]?.id ?? DEFAULT_X_AXIS_KEY}
+      />
+      <YAxis label="Left Y axis" position="left" axisId={yAxis?.[0]?.id ?? DEFAULT_Y_AXIS_KEY} />
+      <YAxis
+        label="Right Y axis"
+        position="right"
+        axisId={yAxis?.[1]?.id ?? yAxis?.[0]?.id ?? DEFAULT_Y_AXIS_KEY}
+      />
+    </ChartContainer>
   );
 }
