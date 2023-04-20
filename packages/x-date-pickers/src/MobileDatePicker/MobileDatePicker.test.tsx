adf1801c34037df3c36067e62a63524ba0b5d723
Bilal Shafi <bilalshafidev@gmail.com>
2023-04-20 18:41:13 +0500

    Fix failed CI

--- packages/x-date-pickers/src/MobileDatePicker/MobileDatePicker.test.tsx
+++ packages/x-date-pickers/src/MobileDatePicker/MobileDatePicker.test.tsx
@@ -14,7 +14,7 @@ import {
 } from 'test/utils/pickers-utils';
 
 describe('<MobileDatePicker />', () => {
-  const { render } = createPickerRenderer({ clock: 'fake' });
+  const { render, clock } = createPickerRenderer({ clock: 'fake' });
 
   it('allows to change only year', () => {
     const onChangeMock = spy();
