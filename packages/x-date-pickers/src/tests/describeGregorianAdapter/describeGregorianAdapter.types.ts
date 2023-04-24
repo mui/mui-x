970124e990bb677cd867fcc49842fb7454fa9cb2
Flavien DELANGLE <flaviendelangle@gmail.com>
2023-04-24 11:46:05 +0200

    [pickers] Migrate `AdapterMoment` to our repository (#8700)

--- packages/x-date-pickers/src/tests/describeGregorianAdapter/describeGregorianAdapter.types.ts
+++ packages/x-date-pickers/src/tests/describeGregorianAdapter/describeGregorianAdapter.types.ts
@@ -2,6 +2,7 @@ import { MuiPickersAdapter } from '@mui/x-date-pickers/models';
 
 export interface DescribeGregorianAdapterParams {
   formatDateTime: string;
+  locale: string;
 }
 
 export type DescribeGregorianAdapterTestSuite = <TDate>(params: {
