7de2b7534170e4a596cd441ed8c729b38dce98e4
Rom Grk <romgrk.cc@gmail.com>
2023-05-03 01:32:29 -0400

    lint

--- packages/grid/x-data-grid/src/internals/utils/groupDataProps.ts
+++ packages/grid/x-data-grid/src/internals/utils/groupDataProps.ts
@@ -7,10 +7,10 @@ export function groupDataProps<T extends {
 }>(
   props: T
 ): T {
-  const newProps = {} as Partial<T>;
-  let dataProps: Record<string, any> | undefined = props.dataProps ?? undefined;
+  const newProps = {} as Record<string, unknown>;
+  let dataProps: Record<string, unknown> | undefined = props.dataProps ?? undefined;
 
-  for (let key in props) {
+  for (const key of Object.keys(props)) {
     if (key.startsWith('data-')) {
       dataProps ??= {}
       dataProps[key] = props[key]
