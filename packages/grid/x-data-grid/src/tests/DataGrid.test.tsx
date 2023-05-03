7daa742a287ce63185d8f5b09d82165ed3df17a9
Rom Grk <romgrk.cc@gmail.com>
2023-05-03 04:00:04 -0400

    fix: test

--- packages/grid/x-data-grid/src/tests/DataGrid.test.tsx
+++ packages/grid/x-data-grid/src/tests/DataGrid.test.tsx
@@ -3,15 +3,35 @@ import { createRenderer } from '@mui/monorepo/test/utils';
 import { expect } from 'chai';
 import { DataGrid } from '@mui/x-data-grid';
 
+const isJSDOM = /jsdom/.test(window.navigator.userAgent);
+
 describe('<DataGrid />', () => {
   const { render } = createRenderer({ clock: 'fake' });
+
+  const baselineProps = {
+    autoHeight: isJSDOM,
+    rows: [
+      {
+        id: 0,
+        brand: 'Nike',
+      },
+      {
+        id: 1,
+        brand: 'Adidas',
+      },
+      {
+        id: 2,
+        brand: 'Puma',
+      },
+    ],
+  };
 
   it('should accept data attributes props', () => {
     render(
       <div style={{ width: 300, height: 500 }}>
         <DataGrid
-          rows={[{ name: 'Bob' }]}
-          columns={[{ field: 'name' }]}
+          {...baselineProps}
+          columns={[{ field: 'brand' }]}
           data-custom-id="grid-1"
         />
       </div>,
