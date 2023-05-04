7e6205b2ed4379bae5c36d050ba167ab5da55937
Lukas <llukas.tyla@gmail.com>
2023-05-04 17:14:50 +0300

    Add export

--- packages/x-date-pickers/src/internals/index.ts
+++ packages/x-date-pickers/src/internals/index.ts
@@ -49,7 +49,7 @@ export type {
 } from './components/pickersPopperClasses';
 export { PickersToolbarButton } from './components/PickersToolbarButton';
 
-export { DAY_MARGIN, DIALOG_WIDTH } from './constants/dimensions';
+export { DAY_MARGIN, DIALOG_WIDTH, DAY_SIZE } from './constants/dimensions';
 
 export type { DesktopOnlyPickerProps } from './hooks/useDesktopPicker';
 export {
