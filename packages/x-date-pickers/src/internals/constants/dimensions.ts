42d83f52e5a8981cbc0c5e7d4bf23de6b196e44d
Lukas <llukas.tyla@gmail.com>
2023-05-08 10:21:09 +0300

    Reduce week number width to correctly fit it on mobile

--- packages/x-date-pickers/src/internals/constants/dimensions.ts
+++ packages/x-date-pickers/src/internals/constants/dimensions.ts
@@ -1,7 +1,7 @@
 export const DAY_SIZE = 36;
-export const WEEK_NUMBER_SIZE = 30;
+export const WEEK_NUMBER_SIZE = 28;
 export const DAY_MARGIN = 1;
-export const DIALOG_WIDTH = 298;
+export const DIALOG_WIDTH = 296;
 export const RANGE_CALENDAR_WIDTH = 273;
 export const CALENDAR_MARGIN = 16;
 export const VIEW_HEIGHT = 370;
