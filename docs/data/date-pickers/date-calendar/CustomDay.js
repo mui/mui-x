e2e78c8fc5bfbaea4b8d821e3295c53f0d296a88
Lukas <llukas.tyla@gmail.com>
2023-05-08 13:59:58 +0300

    Revert no longer relevant change done in #8541

--- docs/data/date-pickers/date-calendar/CustomDay.js
+++ docs/data/date-pickers/date-calendar/CustomDay.js
@@ -50,7 +50,7 @@ function Day(props) {
     <CustomPickersDay
       {...other}
       day={day}
-      sx={dayIsBetween ? { px: 2.5, mx: 0 } : {}}
+      disableMargin
       dayIsBetween={dayIsBetween}
       isFirstDay={isFirstDay}
       isLastDay={isLastDay}
