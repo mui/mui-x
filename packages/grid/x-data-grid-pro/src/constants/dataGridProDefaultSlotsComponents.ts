1ee8896347bac0c6f32d3c5d71d0939095f71603
Bilal Shafi <bilalshafidev@gmail.com>
2023-05-10 12:44:59 +0500

    Cleanup

--- packages/grid/x-data-grid-pro/src/constants/dataGridProDefaultSlotsComponents.ts
+++ packages/grid/x-data-grid-pro/src/constants/dataGridProDefaultSlotsComponents.ts
@@ -2,7 +2,6 @@ import { DATA_GRID_DEFAULT_SLOTS_COMPONENTS } from '@mui/x-data-grid/internals';
 import type { GridProSlotsComponent } from '../models';
 import { GridProColumnMenu } from '../components/GridProColumnMenu';
 import { GridColumnHeaders } from '../components/GridColumnHeaders';
-import { GridHeaderFilterAdornment } from '../components/headerFiltering/GridHeaderFilterAdornment';
 import { GridHeaderFilterMenu } from '../components/headerFiltering/GridHeaderFilterMenu';
 import { GridHeaderFilterCell } from '../components/headerFiltering/GridHeaderFilterCell';
 import materialSlots from '../material';
