3c03ee44edc2abc2b6f9031c9a3387e156a1d266
noraleonte <noraleonte00@gmail.com>
2023-05-16 14:57:01 +0300

    fix failng test for letter editing

--- packages/x-date-pickers/src/TimeField/tests/editing.TimeField.test.tsx
+++ packages/x-date-pickers/src/TimeField/tests/editing.TimeField.test.tsx
@@ -304,7 +304,7 @@ describe('<TimeField /> - Editing', () => {
     it('should set meridiem to AM when pressing "a" and no value is provided', () => {
       testFieldChange({
         format: adapter.formats.fullTime12h,
-        cursorPosition: 17,
+        cursorPosition: 16,
         // Press "a"
         keyStrokes: [{ value: 'hh:mm a', expected: 'hh:mm AM' }],
       });
@@ -313,7 +313,7 @@ describe('<TimeField /> - Editing', () => {
     it('should set meridiem to PM when pressing "p" and no value is provided', () => {
       testFieldChange({
         format: adapter.formats.fullTime12h,
-        cursorPosition: 17,
+        cursorPosition: 16,
         // Press "p"
         keyStrokes: [{ value: 'hh:mm p', expected: 'hh:mm PM' }],
       });
@@ -323,7 +323,7 @@ describe('<TimeField /> - Editing', () => {
       testFieldChange({
         format: adapter.formats.fullTime12h,
         defaultValue: adapter.date(new Date(2022, 5, 15, 14, 12, 25)),
-        cursorPosition: 17,
+        cursorPosition: 16,
         // Press "a"
         keyStrokes: [{ value: '02:12 a', expected: '02:12 AM' }],
       });
@@ -333,7 +333,7 @@ describe('<TimeField /> - Editing', () => {
       testFieldChange({
         format: adapter.formats.fullTime12h,
         defaultValue: adapter.date(new Date(2022, 5, 15, 14, 12, 25)),
-        cursorPosition: 17,
+        cursorPosition: 16,
         // Press "p"
         keyStrokes: [{ value: '02:12 p', expected: '02:12 PM' }],
       });
