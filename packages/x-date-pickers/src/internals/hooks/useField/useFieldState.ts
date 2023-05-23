c60f97f68e2bb4c23decf6435c5c0f2e37f74897
delangle <flaviendelangle@gmail.com>
2023-05-23 09:09:28 +0200

    Fix

--- packages/x-date-pickers/src/internals/hooks/useField/useFieldState.ts
+++ packages/x-date-pickers/src/internals/hooks/useField/useFieldState.ts
@@ -65,7 +65,7 @@ export const useFieldState = <
     internalProps: {
       value: valueProp,
       defaultValue,
-      referenceDate,
+      referenceDate: referenceDateProp,
       onChange,
       format,
       formatDensity = 'dense',
@@ -118,7 +118,7 @@ export const useFieldState = <
 
     const granularity = getSectionTypeGranularity(sections);
     const referenceValue = valueManager.getInitialReferenceValue({
-      referenceDate,
+      referenceDate: referenceDateProp,
       value: valueFromTheOutside,
       valueType,
       utils,
