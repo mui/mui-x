47a2491e9cd56b4637b9dfcd69bf7f9bf0c97bb1
alexandre <alex.fauquette@gmail.com>
2023-05-24 11:15:10 +0200

    add classes

--- packages/x-charts/src/Legend/Legend.tsx
+++ packages/x-charts/src/Legend/Legend.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { unstable_composeClasses as composeClasses } from '@mui/utils';
+import { useThemeProps, useTheme, Theme } from '@mui/material/styles';
 import { DrawingContext } from '../context/DrawingProvider';
 import {
   AnchorPosition,
@@ -8,22 +10,50 @@ import {
   getTranslateValues,
 } from './utils';
 import { SeriesContext } from '../context/SeriesContextProvider';
-
+import { LegendClasses, getLegendUtilityClass } from './legendClasses';
+import { DefaultizedProps } from '../models/helpers';
 
 export type LegendProps = {
   position: AnchorPosition;
   offset?: Partial<{ x: number; y: number }>;
+  /**
+   * Override or extend the styles applied to the component.
+   */
+  classes?: Partial<LegendClasses>;
 } & SizingParams;
 
-export function Legend(props: LegendProps) {
-  const {
-    position = { horizontal: 'middle', vertical: 'top' },
-    offset,
-    direction = 'row',
-    markSize = 20,
-    itemWidth = 100,
-    spacing = 2,
-  } = props;
+type DefaultizedLegendProps = DefaultizedProps<LegendProps, 'direction' | 'position'>;
+
+const useUtilityClasses = (ownerState: DefaultizedLegendProps & { theme: Theme }) => {
+  const { classes, direction } = ownerState;
+  const slots = {
+    root: ['root', direction],
+    mark: ['mark'],
+    label: ['label'],
+    series: ['series'],
+  };
+
+  return composeClasses(slots, getLegendUtilityClass, classes);
+};
+
+const defaultProps = {
+  position: { horizontal: 'middle', vertical: 'top' },
+  direction: 'row',
+  markSize: 20,
+  itemWidth: 100,
+  spacing: 2,
+} as const;
+
+export function Legend(inProps: LegendProps) {
+  const props: DefaultizedLegendProps = useThemeProps({
+    props: { ...defaultProps, ...inProps },
+    name: 'MuiLegend',
+  });
+
+  const { position, direction, markSize, itemWidth, spacing, offset } = props;
+  const theme = useTheme();
+  const classes = useUtilityClasses({ ...props, theme });
+
   const { left, top, width, height } = React.useContext(DrawingContext);
   const series = React.useContext(SeriesContext);
 
@@ -45,17 +75,32 @@ export function Legend(props: LegendProps) {
   const offsetX = offset?.x ?? 0;
   const offsetY = offset?.y ?? 0;
   return (
-    <g transform={`translate(${left + translateLeft + offsetX},${top + translateTop + offsetY})`}>
-      {/* <rect x={0} y={0} width={contentWidth} height={contentHeight} /> */}
+    <g
+      transform={`translate(${left + translateLeft + offsetX},${top + translateTop + offsetY})`}
+      className={classes.root}
+    >
       {seriesToDisplay.map(({ id, label, color }, seriesIndex) => (
         <g
           key={id}
           transform={`translate(${direction === 'row' ? seriesIndex * (itemWidth + spacing) : 0}, ${
             direction === 'column' ? seriesIndex * (markSize + spacing) : 0
           })`}
+          className={classes.series}
         >
-          <rect x={0} y={0} width={markSize} height={markSize} fill={color} />
-          <text x={markSize + 5} y={markSize / 2} alignmentBaseline="central">
+          <rect
+            x={0}
+            y={0}
+            width={markSize}
+            height={markSize}
+            fill={color}
+            className={classes.mark}
+          />
+          <text
+            x={markSize + 5}
+            y={markSize / 2}
+            alignmentBaseline="central"
+            className={classes.label}
+          >
             {label}
           </text>
         </g>
