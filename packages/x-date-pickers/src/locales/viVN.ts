0cd80f964626f57abafad427fe54ddad58a27cab
Nguyễn Trung Nhẫn <trungnhan21.12@gmail.com>
2023-05-24 11:06:31 +0700

    Create viVN.ts
    Add vietnamese language
    
    Signed-off-by: Nguyễn Trung Nhẫn <trungnhan21.12@gmail.com>

---
+++ packages/x-date-pickers/src/locales/viVN.ts
@@ -0,0 +1,80 @@
+import { PickersLocaleText } from './utils/pickersLocaleTextApi';
+import { getPickersLocalization } from './utils/getPickersLocalization';
+
+// This object is not Partial<PickersLocaleText> because it is the default values
+
+const viVNPickers: PickersLocaleText<any> = {
+  // Calendar navigation
+  previousMonth: 'Previous month',
+  nextMonth: 'Next month',
+
+  // View navigation
+  openPreviousView: 'open previous view',
+  openNextView: 'open next view',
+  calendarViewSwitchingButtonAriaLabel: (view) =>
+    view === 'year'
+      ? 'year view is open, switch to calendar view'
+      : 'calendar view is open, switch to year view',
+
+  // DateRange placeholders
+  start: 'Start',
+  end: 'End',
+
+  // Action bar
+  cancelButtonLabel: 'Cancel',
+  clearButtonLabel: 'Clear',
+  okButtonLabel: 'OK',
+  todayButtonLabel: 'Today',
+
+  // Toolbar titles
+  datePickerToolbarTitle: 'Select date',
+  dateTimePickerToolbarTitle: 'Select date & time',
+  timePickerToolbarTitle: 'Select time',
+  dateRangePickerToolbarTitle: 'Select date range',
+
+  // Clock labels
+  clockLabelText: (view, time, adapter) =>
+    `Select ${view}. ${
+      time === null ? 'No time selected' : `Selected time is ${adapter.format(time, 'fullTime')}`
+    }`,
+  hoursClockNumberText: (hours) => `${hours} hours`,
+  minutesClockNumberText: (minutes) => `${minutes} minutes`,
+  secondsClockNumberText: (seconds) => `${seconds} seconds`,
+
+  // Digital clock labels
+  selectViewText: (view) => `Select ${view}`,
+
+  // Calendar labels
+  calendarWeekNumberHeaderLabel: 'Week number',
+  calendarWeekNumberHeaderText: '#',
+  calendarWeekNumberAriaLabelText: (weekNumber) => `Week ${weekNumber}`,
+  calendarWeekNumberText: (weekNumber) => `${weekNumber}`,
+
+  // Open picker labels
+  openDatePickerDialogue: (value, utils) =>
+    value !== null && utils.isValid(value)
+      ? `Choose date, selected date is ${utils.format(value, 'fullDate')}`
+      : 'Choose date',
+  openTimePickerDialogue: (value, utils) =>
+    value !== null && utils.isValid(value)
+      ? `Choose time, selected time is ${utils.format(value, 'fullTime')}`
+      : 'Choose time',
+
+  // Table labels
+  timeTableLabel: 'pick time',
+  dateTableLabel: 'pick date',
+
+  // Field section placeholders
+  fieldYearPlaceholder: (params) => 'Y'.repeat(params.digitAmount),
+  fieldMonthPlaceholder: (params) => (params.contentType === 'letter' ? 'MMMM' : 'MM'),
+  fieldDayPlaceholder: () => 'DD',
+  fieldWeekDayPlaceholder: (params) => (params.contentType === 'letter' ? 'EEEE' : 'EE'),
+  fieldHoursPlaceholder: () => 'hh',
+  fieldMinutesPlaceholder: () => 'mm',
+  fieldSecondsPlaceholder: () => 'ss',
+  fieldMeridiemPlaceholder: () => 'aa',
+};
+
+export const DEFAULT_LOCALE = enUSPickers;
+
+export const enUS = getPickersLocalization(enUSPickers);
