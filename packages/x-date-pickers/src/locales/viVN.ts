7ae8290293626b76a3e301c00987b71faf20ef18
Nguyễn Trung Nhẫn <trungnhan21.12@gmail.com>
2023-05-24 16:35:16 +0700

    [l10n] Improve Vietnamese (vi-VN) locale
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
    Signed-off-by: Nguyễn Trung Nhẫn <trungnhan21.12@gmail.com>

--- packages/x-date-pickers/src/locales/viVN.ts
+++ packages/x-date-pickers/src/locales/viVN.ts
@@ -1,9 +1,7 @@
 import { PickersLocaleText } from './utils/pickersLocaleTextApi';
 import { getPickersLocalization } from './utils/getPickersLocalization';
 
-// This object is not Partial<PickersLocaleText> because it is the default values
-
-const viVNPickers: PickersLocaleText<any> = {
+const viVNPickers: Partial<PickersLocaleText<any>> = {
   // Calendar navigation
   previousMonth: 'Tháng trước',
   nextMonth: 'Tháng sau',
