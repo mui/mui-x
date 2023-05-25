e70c982c7bbd57c036a927d43f35b2d84ccdf346
Rom Grk <romgrk.cc@gmail.com>
2023-05-25 14:22:56 -0400

    lint

--- packages/grid/x-data-grid/src/utils/fastShallowCompare.ts
+++ packages/grid/x-data-grid/src/utils/fastShallowCompare.ts
@@ -1,6 +1,6 @@
 const is = Object.is;
 
-export function fastShallowCompare<T extends Record<string, unknown> | null | undefined>(
+export function fastShallowCompare<T extends Record<string, any> | null>(
   a: T,
   b: T,
 ) {
