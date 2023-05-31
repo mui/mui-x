135ec6a1e9c9792b78e9ecfd4d85325d4a3f24dd
alexandre <alex.fauquette@gmail.com>
2023-05-31 15:30:20 +0200

    renaming

--- docs/pages/x/react-charts/line-demo.js
+++ docs/pages/x/react-charts/line-demo.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import * as pageProps from 'docsx/data/charts/lineDemo/lineDemo.md?@mui/markdown';
+import * as pageProps from 'docsx/data/charts/line-demo/line-demo.md?@mui/markdown';
 
 export default function Page() {
   return <MarkdownDocs {...pageProps} />;
