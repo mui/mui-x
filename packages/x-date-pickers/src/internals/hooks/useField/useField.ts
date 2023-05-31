a5df0c870c7b4983ed5f1c94c16253829be550e7
noraleonte <72460825+noraleonte@users.noreply.github.com>
2023-05-31 13:06:55 +0300

    Update packages/x-date-pickers/src/internals/hooks/useField/useField.ts
    Co-authored-by: Lukas <llukas.tyla@gmail.com>
    Signed-off-by: noraleonte <72460825+noraleonte@users.noreply.github.com>

--- packages/x-date-pickers/src/internals/hooks/useField/useField.ts
+++ packages/x-date-pickers/src/internals/hooks/useField/useField.ts
@@ -489,7 +489,7 @@ export const useField = <
   }));
 
   const handleClearValue = useEventCallback((event: React.MouseEvent, ...args) => {
-    // the click event of the endAdornmnet propagates to the input and triggers the `handleInputClick` handler.
+    // the click event on the endAdornmnet would propagate to the input and trigger the `handleInputClick` handler.
     event.stopPropagation();
     event.preventDefault();
     onClear?.(event, ...(args as []));
