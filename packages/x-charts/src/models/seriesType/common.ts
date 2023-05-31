724d8bedd7496d9a7fe7c270856428dd8bd96254
alexandre <alex.fauquette@gmail.com>
2023-05-31 15:00:02 +0200

    fix series types

--- packages/x-charts/src/models/seriesType/common.ts
+++ packages/x-charts/src/models/seriesType/common.ts
@@ -1,19 +1,17 @@
 import type { StackOffset, StackOrder } from '../../internals/stackSeries';
 
-export type CommonSeriesType = {
+export type CommonSeriesType<TValue> = {
   id?: string;
-};
-
-export type DefaultizedCommonSeriesType<TValue> = {
-  id: string;
-  color: string;
+  color?: string;
   /**
    * Formatter used to render values in tooltip or other data display.
    * @param {TValue} value The series' value to render.
    * @returns {string} The string to dispaly.
    */
-  valueFormatter: (value: TValue) => string;
+  valueFormatter?: (value: TValue) => string;
 };
+
+export type CommonDefaultizedProps = 'id' | 'color' | 'valueFormatter';
 
 export type CartesianSeriesType = {
   xAxisKey?: string;
