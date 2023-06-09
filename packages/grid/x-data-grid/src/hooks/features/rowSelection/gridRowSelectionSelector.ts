e1edd11b0ca18be3905af3236d114a4e3c2a7b8c
Rom Grk <romgrk.cc@gmail.com>
2023-06-09 10:57:19 -0400

    feat: migrate packages/grid/x-data-grid/src/hooks/features/rowSelection/gridRowSelectionSelector.ts

--- packages/grid/x-data-grid/src/hooks/features/rowSelection/gridRowSelectionSelector.ts
+++ packages/grid/x-data-grid/src/hooks/features/rowSelection/gridRowSelectionSelector.ts
@@ -1,4 +1,7 @@
-import { createSelector } from '../../../utils/createSelector';
+import {
+  createSelectorUnmemoized as createSelector,
+  createSelectorMemoized,
+} from '../../../utils/createSelector';
 import { gridRowsLookupSelector } from '../rows/gridRowsSelector';
 import { GridStateCommunity } from '../../../models/gridStateCommunity';
 import { GridRowId, GridRowModel } from '../../../models/gridRows';
@@ -10,14 +13,14 @@ export const selectedGridRowsCountSelector = createSelector(
   (selection) => selection.length,
 );
 
-export const selectedGridRowsSelector = createSelector(
+export const selectedGridRowsSelector = createSelectorMemoized(
   gridRowSelectionStateSelector,
   gridRowsLookupSelector,
   (selectedRows, rowsLookup) =>
     new Map<GridRowId, GridRowModel>(selectedRows.map((id) => [id, rowsLookup[id]])),
 );
 
-export const selectedIdsLookupSelector = createSelector(
+export const selectedIdsLookupSelector = createSelectorMemoized(
   gridRowSelectionStateSelector,
   (selection) =>
     selection.reduce<Record<GridRowId, GridRowId>>((lookup, rowId) => {
