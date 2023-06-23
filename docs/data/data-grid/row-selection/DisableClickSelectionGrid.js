cba66f279d068006e1290ede72189711c1c381b5
Gautam Jayaraman <gautam@joingreenwork.com>
2023-06-23 10:41:59 -0700

    Update DisableClickSelectionGrid.js
    Signed-off-by: Gautam Jayaraman <gautam@joingreenwork.com>

--- docs/data/data-grid/row-selection/DisableClickSelectionGrid.js
+++ docs/data/data-grid/row-selection/DisableClickSelectionGrid.js
@@ -11,7 +11,7 @@ export default function DisableClickSelectionGrid() {
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGrid checkboxSelection disableRowSelectionOnClick {...data} />
+      <DataGrid checkboxSelection disableSelectionOnClick {...data} />
     </div>
   );
 }
