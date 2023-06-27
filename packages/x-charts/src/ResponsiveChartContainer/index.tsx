576600cb20b962cf2dd1f5c7ea7922b41bbdb39e
alexandre <alex.fauquette@gmail.com>
2023-06-27 14:47:17 +0200

    [charts] Picke resizing listener from data grid

--- packages/x-charts/src/ResponsiveChartContainer/index.tsx
+++ packages/x-charts/src/ResponsiveChartContainer/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { ResizeObserver } from '@juggle/resize-observer';
+import useEnhancedEffect from '@mui/utils/useEnhancedEffect';
+import ownerWindow from '@mui/utils/ownerWindow';
 import { ChartContainer, ChartContainerProps } from '../ChartContainer';
 import { MakeOptional } from '../models/helpers';
 
@@ -7,34 +8,84 @@ const useChartDimensions = (
   inWidth?: number,
   inHeight?: number,
 ): [React.RefObject<HTMLDivElement>, number, number] => {
-  const ref = React.useRef<HTMLDivElement>(null);
+  const rootRef = React.useRef<HTMLDivElement>(null);
+  const displayError = React.useRef<boolean>(false);
 
   const [width, setWidth] = React.useState(0);
   const [height, setHeight] = React.useState(0);
 
+  const computeSizeAndPublishResizeEvent = React.useCallback(() => {
+    const mainEl = rootRef?.current;
+
+    if (!mainEl) {
+      return;
+    }
+
+    const win = ownerWindow(mainEl);
+    const computedStyle = win.getComputedStyle(mainEl);
+
+    const newHeight = parseFloat(computedStyle.height) || 0;
+    const newWidth = parseFloat(computedStyle.width) || 0;
+
+    setWidth(newWidth);
+    setHeight(newHeight);
+  }, []);
+
   React.useEffect(() => {
-    const element = ref.current;
-    if (element === null || (inHeight !== undefined && inWidth !== undefined)) {
+    // Ensure the error detection occures after first rendering
+    displayError.current = true;
+  }, []);
+
+  useEnhancedEffect(() => {
+    if (inWidth !== undefined && inHeight !== undefined) {
+      return () => {};
+    }
+    computeSizeAndPublishResizeEvent();
+
+    const elementToObserve = rootRef.current;
+    if (typeof ResizeObserver === 'undefined') {
       return () => {};
     }
 
-    const resizeObserver = new ResizeObserver((entries) => {
-      if (Array.isArray(entries) && entries.length) {
-        const entry = entries[0];
-        if (inWidth === undefined) {
-          setWidth(entry.contentRect.width);
-        }
-        if (inHeight === undefined) {
-          setHeight(entry.contentRect.height);
-        }
-      }
+    let animationFrame: number;
+    const observer = new ResizeObserver(() => {
+      // See https://github.com/mui/mui-x/issues/8733
+      animationFrame = window.requestAnimationFrame(() => {
+        computeSizeAndPublishResizeEvent();
+      });
     });
-    resizeObserver.observe(element);
 
-    return () => resizeObserver.disconnect();
-  }, [height, inHeight, inWidth, width]);
+    if (elementToObserve) {
+      observer.observe(elementToObserve);
+    }
 
-  return [ref, inWidth ?? width, inHeight ?? height];
+    return () => {
+      if (animationFrame) {
+        window.cancelAnimationFrame(animationFrame);
+      }
+
+      if (elementToObserve) {
+        observer.unobserve(elementToObserve);
+      }
+    };
+  }, [computeSizeAndPublishResizeEvent, inHeight, inWidth]);
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (displayError.current && inWidth === undefined && width === 0) {
+      console.error(
+        `MUI: Charts does not have \`width\` prop, and its container has no \`width\` defined.`,
+      );
+      displayError.current = false;
+    }
+    if (displayError.current && inHeight === undefined && height === 0) {
+      console.error(
+        `MUI: Charts does not have \`height\` prop, and its container has no \`height\` defined.`,
+      );
+      displayError.current = false;
+    }
+  }
+
+  return [rootRef, inWidth ?? width, inHeight ?? height];
 };
 
 export type ResponsiveChartContainerProps = MakeOptional<ChartContainerProps, 'width' | 'height'>;
@@ -45,7 +96,17 @@ export function ResponsiveChartContainer(props: ResponsiveChartContainerProps) {
   return (
     <div
       ref={containerRef}
-      style={{ width: props.width ?? '100%', height: props.height ?? '100%', padding: 0 }}
+      style={{
+        width: props.width ?? '100%',
+        height: props.height ?? '100%',
+        display: 'flex',
+        position: 'relative',
+        flexGrow: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+      }}
     >
       <ChartContainer {...props} width={width} height={height} />
     </div>
