5744a57b92ff2ca7492700ec62fe8a291dfd399e
Andrew Cherniavskyi <andrew@mui.com>
2023-07-03 19:07:00 +0200

    fix rangeError with duplicated flex fields

--- packages/grid/x-data-grid/src/tests/layout.DataGrid.test.tsx
+++ packages/grid/x-data-grid/src/tests/layout.DataGrid.test.tsx
@@ -1161,4 +1161,29 @@ describe('<DataGrid /> - Layout & Warnings', () => {
       </div>,
     );
   });
+
+  // See https://github.com/mui/mui-x/issues/9550
+  it('should not exceed maximum call stack size with duplicated flex fields', function test() {
+    if (/jsdom/.test(window.navigator.userAgent)) {
+      // Need layouting
+      this.skip();
+    }
+
+    expect(() => {
+      render(
+        <div style={{ height: 200, width: 400 }}>
+          <DataGrid
+            rows={[{ id: 1 }]}
+            columns={[
+              { field: 'id', flex: 1 },
+              { field: 'id', flex: 1 },
+            ]}
+          />
+        </div>,
+      );
+    }).toErrorDev([
+      'Warning: Encountered two children with the same key, `id`. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version.',
+      'Warning: Encountered two children with the same key, `id`. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version.',
+    ]);
+  });
 });
