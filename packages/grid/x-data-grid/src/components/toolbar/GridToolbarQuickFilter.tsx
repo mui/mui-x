ffba4344d00ba70cb7effa00594ef4f36997a869
giladappsforce <gilad.varsano@appsforce.io>
2023-07-10 17:59:37 +0300

    fix

--- packages/grid/x-data-grid/src/components/toolbar/GridToolbarQuickFilter.tsx
+++ packages/grid/x-data-grid/src/components/toolbar/GridToolbarQuickFilter.tsx
@@ -149,7 +149,7 @@ function GridToolbarQuickFilter(props: GridToolbarQuickFilterProps) {
             <rootProps.slots.quickFilterClearIcon fontSize="small" />
           </rootProps.slots.baseIconButton>
         ),
-        {...other.InputProps, ...rootProps.slotProps?.baseTextField?.InputProps}
+        ...other.InputProps
       }}
       {...rootProps.slotProps?.baseTextField}
     />
