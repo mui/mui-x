e99445a7b0ec5f6746af44b95b40b73768cb0bd3
Lukas <llukas.tyla@gmail.com>
2023-07-28 10:33:49 +0300

    [test] Add pickers e2e tests (#9747)

---
+++ test/e2e/fixtures/DatePicker/BasicDesktopDateRangePicker.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+export default function BasicDesktopDateRangePicker() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateRangePicker />
+    </LocalizationProvider>
+  );
+}
