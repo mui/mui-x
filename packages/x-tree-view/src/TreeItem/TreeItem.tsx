211b63ff187f601fc2ad16ec724ca8bf57e81920
delangle <flaviendelangle@gmail.com>
2023-07-28 07:57:20 +0200

    Work

--- packages/x-tree-view/src/TreeItem/TreeItem.tsx
+++ packages/x-tree-view/src/TreeItem/TreeItem.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import Collapse from '@mui/material/Collapse';
 import { alpha, styled, useThemeProps } from '@mui/material/styles';
 import ownerDocument from '@mui/utils/ownerDocument';
 import useForkRef from '@mui/utils/useForkRef';
+import unsupportedProp from '@mui/utils/unsupportedProp';
+import elementTypeAcceptingRef from '@mui/utils/elementTypeAcceptingRef';
 import { unstable_composeClasses as composeClasses } from '@mui/base';
 import { useTreeViewContext } from '../TreeView/TreeViewContext';
 import { DescendantProvider, TreeItemDescendant, useDescendant } from '../TreeView/descendants';
@@ -356,3 +359,80 @@ export const TreeItem = React.forwardRef(function TreeItem(
     </TreeItemRoot>
   );
 });
+
+TreeItem.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * The content of the component.
+   */
+  children: PropTypes.node,
+  /**
+   * Override or extend the styles applied to the component.
+   */
+  classes: PropTypes.object,
+  /**
+   * The icon used to collapse the node.
+   */
+  collapseIcon: PropTypes.node,
+  /**
+   * The component used for the content node.
+   * @default TreeItemContent
+   */
+  ContentComponent: elementTypeAcceptingRef,
+  /**
+   * Props applied to ContentComponent
+   */
+  ContentProps: PropTypes.object,
+  /**
+   * If `true`, the node is disabled.
+   * @default false
+   */
+  disabled: PropTypes.bool,
+  /**
+   * The icon displayed next to a end node.
+   */
+  endIcon: PropTypes.node,
+  /**
+   * The icon used to expand the node.
+   */
+  expandIcon: PropTypes.node,
+  /**
+   * The icon to display next to the tree node's label.
+   */
+  icon: PropTypes.node,
+  /**
+   * The tree node label.
+   */
+  label: PropTypes.node,
+  /**
+   * The id of the node.
+   */
+  nodeId: PropTypes.string.isRequired,
+  /**
+   * This prop isn't supported.
+   * Use the `onNodeFocus` callback on the tree if you need to monitor a node's focus.
+   */
+  onFocus: unsupportedProp,
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  /**
+   * The component used for the transition.
+   * [Follow this guide](/material-ui/transitions/#transitioncomponent-prop) to learn more about the requirements for this component.
+   * @default Collapse
+   */
+  TransitionComponent: PropTypes.elementType,
+  /**
+   * Props applied to the transition element.
+   * By default, the element is based on this [`Transition`](http://reactcommunity.org/react-transition-group/transition/) component.
+   */
+  TransitionProps: PropTypes.object,
+} as any;
