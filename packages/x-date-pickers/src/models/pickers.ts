a8f2dcbaf840cacf97c5d9c49eda5a72bbc8a5d9
delangle <flaviendelangle@gmail.com>
2023-08-10 13:55:01 +0200

    Fix

--- packages/x-date-pickers/src/models/pickers.ts
+++ packages/x-date-pickers/src/models/pickers.ts
@@ -3,7 +3,7 @@ import type { PickersShortcutsItem } from '../PickersShortcuts';
 export interface PickerChangeHandlerContext<TError> {
   validationError: TError;
   /**
-   * Label of the shortcut causing this `onChange` call.
+   * Shortcut causing this `onChange` call.
    * If the call has not been caused by a shortcut selection, it will equal `null`.
    */
   shortcut: Omit<PickersShortcutsItem<unknown>, 'getValue'> | null;
