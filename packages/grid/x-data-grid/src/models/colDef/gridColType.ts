1d01679ca8e4ecc1b7b788c1abe35199802ab65e
Milan <milan.dhar@gmail.com>
2023-08-21 15:39:29 -0700

    Formatting changes

--- packages/grid/x-data-grid/src/models/colDef/gridColType.ts
+++ packages/grid/x-data-grid/src/models/colDef/gridColType.ts
@@ -12,25 +12,25 @@ export type GridNativeColTypes =
 // Use `LiteralUnion` to get autocompletion for literal types.
 export type GridColType = LiteralUnion<GridNativeColTypes, string>;
 
-export type GridNativeColOperator = 
-| 'is'
-| 'not'
-| 'after'
-| 'onOrAfter'
-| 'before'
-| 'onOrBefore'
-| 'isEmpty'
-| 'isNotEmpty'
-| '='
-| '!='
-| '>'
-| '>='
-| '<'
-| '<='
-| 'isAnyOf'
-| 'contains'
-| 'equals'
-| 'startsWith'
-| 'endsWith';
+export type GridNativeColOperator =
+  | 'is'
+  | 'not'
+  | 'after'
+  | 'onOrAfter'
+  | 'before'
+  | 'onOrBefore'
+  | 'isEmpty'
+  | 'isNotEmpty'
+  | '='
+  | '!='
+  | '>'
+  | '>='
+  | '<'
+  | '<='
+  | 'isAnyOf'
+  | 'contains'
+  | 'equals'
+  | 'startsWith'
+  | 'endsWith';
 
 export type GridColTypeOperator = GridNativeColOperator | string;
