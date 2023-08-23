7190f36212f7df61b6958326859ce3f2f2318a7f
Danail H <hadjiatanasov@gmail.com>
2023-08-23 14:24:31 +0300

    Add a recipe showcasing how to render components outside of the grid context

---
+++ docs/data/data-grid/filtering-recipes/QuickFilterOutsideOfGrid.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { DataGrid, GridToolbarQuickFilter, GridToolbar } from '@mui/x-data-grid';
+import { useDemoData } from '@mui/x-data-grid-generator';
+
+function MyCustomToolbar(props) {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <GridToolbarQuickFilter />,
+        document.getElementById('filter-panel'),
+      )}
+      <GridToolbar {...props} />
+    </React.Fragment>
+  );
+}
+
+const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
+
+export default function QuickFilterOutsideOfGrid() {
+  const { data } = useDemoData({
+    dataSet: 'Employee',
+    rowLength: 1000,
+  });
+
+  // Otherwise filter will be applied on fields such as the hidden column id
+  const columns = React.useMemo(
+    () => data.columns.filter((column) => VISIBLE_FIELDS.includes(column.field)),
+    [data.columns],
+  );
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <Box id="filter-panel" />
+      </Grid>
+      <Grid item style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          {...data}
+          columns={columns}
+          slots={{
+            toolbar: MyCustomToolbar,
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+}
