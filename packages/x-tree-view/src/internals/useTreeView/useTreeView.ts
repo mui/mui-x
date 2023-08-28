7fd203650331be88af5378f63596fd722f25ce08
delangle <flaviendelangle@gmail.com>
2023-08-28 09:56:45 +0200

    Fix

--- packages/x-tree-view/src/internals/useTreeView/useTreeView.ts
+++ packages/x-tree-view/src/internals/useTreeView/useTreeView.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import useForkRef from '@mui/utils/useForkRef';
 import { EventHandlers } from '@mui/base/utils';
-import { TreeViewInstance, TreeViewPlugin, TreeViewState, TreeViewModels } from '../../models';
+import { TreeViewInstance, TreeViewPlugin, TreeViewState } from '../../models';
 import { useTreeViewNodes } from './useTreeViewNodes';
 import { useTreeViewSelection } from './useTreeViewSelection';
 import { useTreeViewFocus } from './useTreeViewFocus';
@@ -10,6 +10,7 @@ import { useTreeViewKeyboardNavigation } from './useTreeViewKeyboardNavigation';
 import { useTreeViewContext } from './useTreeViewContext';
 import { UseTreeViewDefaultizedProps, UseTreeViewProps } from './useTreeView.types';
 import { DEFAULT_TREE_VIEW_CONTEXT_VALUE } from '../TreeViewProvider/TreeViewContext';
+import { useTreeViewModels } from './useTreeViewModels';
 
 const plugins: TreeViewPlugin<UseTreeViewDefaultizedProps<any>>[] = [
   useTreeViewNodes,
@@ -19,104 +20,6 @@ const plugins: TreeViewPlugin<UseTreeViewDefaultizedProps<any>>[] = [
   useTreeViewKeyboardNavigation,
   useTreeViewContext,
 ];
-
-/**
- * Implements the same behavior as `useControlled` but for several models.
- * The controlled models are never stored in the state and the state is only updated if the model is not controlled.
- */
-const useTreeViewModels = <Multiple extends boolean>(
-  props: UseTreeViewDefaultizedProps<Multiple>,
-) => {
-  const modelsRef = React.useRef<{
-    [modelName: string]: {
-      controlledProp: keyof UseTreeViewDefaultizedProps<true>;
-      defaultProp: keyof UseTreeViewDefaultizedProps<true>;
-      isControlled: boolean;
-    };
-  }>({});
-
-  const [modelsState, setModelsState] = React.useState<{ [modelName: string]: any }>(() => {
-    const initialState: { [modelName: string]: any } = {};
-
-    plugins.forEach((plugin) => {
-      plugin.models?.forEach((model) => {
-        modelsRef.current[model.name] = {
-          controlledProp: model.controlledProp,
-          defaultProp: model.defaultProp,
-          isControlled: props[model.controlledProp] !== undefined,
-        };
-        initialState[model.name] = props[model.defaultProp];
-      });
-    });
-
-    return initialState;
-  });
-
-  const models = Object.fromEntries(
-    Object.entries(modelsRef.current).map(([modelName, model]) => {
-      const value = model.isControlled ? props[model.controlledProp] : modelsState[modelName];
-
-      return [
-        modelName,
-        {
-          value,
-          setValue: (newValue: any) => {
-            if (!model.isControlled) {
-              setModelsState((prevState) => ({
-                ...prevState,
-                [modelName]: newValue,
-              }));
-            }
-          },
-        },
-      ];
-    }),
-  ) as unknown as TreeViewModels<Multiple>;
-
-  // We know that `modelsRef` do not vary across renders.
-  /* eslint-disable react-hooks/rules-of-hooks, react-hooks/exhaustive-deps */
-  if (process.env.NODE_ENV !== 'production') {
-    Object.entries(modelsRef.current).forEach(([modelName, model]) => {
-      const controlled = props[model.controlledProp];
-      const defaultProp = props[model.defaultProp];
-
-      React.useEffect(() => {
-        if (model.isControlled !== (controlled !== undefined)) {
-          console.error(
-            [
-              `MUI: A component is changing the ${
-                model.isControlled ? '' : 'un'
-              }controlled ${modelName} state of TreeView to be ${
-                model.isControlled ? 'un' : ''
-              }controlled.`,
-              'Elements should not switch from uncontrolled to controlled (or vice versa).',
-              `Decide between using a controlled or uncontrolled ${modelName} ` +
-                'element for the lifetime of the component.',
-              "The nature of the state is determined during the first render. It's considered controlled if the value is not `undefined`.",
-              'More info: https://fb.me/react-controlled-components',
-            ].join('\n'),
-          );
-        }
-      }, [controlled]);
-
-      const { current: defaultValue } = React.useRef(defaultProp);
-
-      React.useEffect(() => {
-        if (!model.isControlled && defaultValue !== defaultProp) {
-          console.error(
-            [
-              `MUI: A component is changing the default ${modelName} state of an uncontrolled TreeView after being initialized. ` +
-                `To suppress this warning opt to use a controlled TreeView.`,
-            ].join('\n'),
-          );
-        }
-      }, [JSON.stringify(defaultValue)]);
-    });
-  }
-  /* eslint-enable react-hooks/rules-of-hooks, react-hooks/exhaustive-deps */
-
-  return models;
-};
 
 const defaultDefaultExpanded: string[] = [];
 const defaultDefaultSelected: string[] = [];
@@ -147,7 +50,7 @@ export const useTreeView = <Multiple extends boolean | undefined>(
     }, {} as TreeViewState),
   );
 
-  const models = useTreeViewModels(props);
+  const models = useTreeViewModels(plugins, props);
 
   const instanceRef = React.useRef<TreeViewInstance>({ nodeMap: {} } as TreeViewInstance);
   const instance = instanceRef.current;
---
+++ packages/x-tree-view/src/internals/useTreeView/useTreeViewModels.ts
@@ -0,0 +1,102 @@
+import * as React from 'react';
+import { TreeViewModels, TreeViewPlugin } from '../../models';
+import { UseTreeViewDefaultizedProps } from './useTreeView.types';
+
+/**
+ * Implements the same behavior as `useControlled` but for several models.
+ * The controlled models are never stored in the state and the state is only updated if the model is not controlled.
+ */
+export const useTreeViewModels = <Multiple extends boolean>(
+  plugins: TreeViewPlugin<UseTreeViewDefaultizedProps<any>>[],
+  props: UseTreeViewDefaultizedProps<Multiple>,
+) => {
+  const modelsRef = React.useRef<{
+    [modelName: string]: {
+      controlledProp: keyof UseTreeViewDefaultizedProps<true>;
+      defaultProp: keyof UseTreeViewDefaultizedProps<true>;
+      isControlled: boolean;
+    };
+  }>({});
+
+  const [modelsState, setModelsState] = React.useState<{ [modelName: string]: any }>(() => {
+    const initialState: { [modelName: string]: any } = {};
+
+    plugins.forEach((plugin) => {
+      plugin.models?.forEach((model) => {
+        modelsRef.current[model.name] = {
+          controlledProp: model.controlledProp,
+          defaultProp: model.defaultProp,
+          isControlled: props[model.controlledProp] !== undefined,
+        };
+        initialState[model.name] = props[model.defaultProp];
+      });
+    });
+
+    return initialState;
+  });
+
+  const models = Object.fromEntries(
+    Object.entries(modelsRef.current).map(([modelName, model]) => {
+      const value = model.isControlled ? props[model.controlledProp] : modelsState[modelName];
+
+      return [
+        modelName,
+        {
+          value,
+          setValue: (newValue: any) => {
+            if (!model.isControlled) {
+              setModelsState((prevState) => ({
+                ...prevState,
+                [modelName]: newValue,
+              }));
+            }
+          },
+        },
+      ];
+    }),
+  ) as unknown as TreeViewModels<Multiple>;
+
+  // We know that `modelsRef` do not vary across renders.
+  /* eslint-disable react-hooks/rules-of-hooks, react-hooks/exhaustive-deps */
+  if (process.env.NODE_ENV !== 'production') {
+    Object.entries(modelsRef.current).forEach(([modelName, model]) => {
+      const controlled = props[model.controlledProp];
+      const defaultProp = props[model.defaultProp];
+
+      React.useEffect(() => {
+        if (model.isControlled !== (controlled !== undefined)) {
+          console.error(
+            [
+              `MUI: A component is changing the ${
+                model.isControlled ? '' : 'un'
+              }controlled ${modelName} state of TreeView to be ${
+                model.isControlled ? 'un' : ''
+              }controlled.`,
+              'Elements should not switch from uncontrolled to controlled (or vice versa).',
+              `Decide between using a controlled or uncontrolled ${modelName} ` +
+                'element for the lifetime of the component.',
+              "The nature of the state is determined during the first render. It's considered controlled if the value is not `undefined`.",
+              'More info: https://fb.me/react-controlled-components',
+            ].join('\n'),
+          );
+        }
+      }, [controlled]);
+
+      const { current: defaultValue } = React.useRef(defaultProp);
+
+      React.useEffect(() => {
+        if (!model.isControlled && defaultValue !== defaultProp) {
+          console.error(
+            [
+              `MUI: A component is changing the default ${modelName} state of an uncontrolled TreeView after being initialized. ` +
+                `To suppress this warning opt to use a controlled TreeView.`,
+            ].join('\n'),
+          );
+        }
+      }, [JSON.stringify(defaultValue)]);
+    });
+  }
+  /* eslint-enable react-hooks/rules-of-hooks, react-hooks/exhaustive-deps */
+
+  return models;
+};
