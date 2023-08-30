b78b323c5998588266a7e8d3de72c685cae1f27e
alexandre <alex.fauquette@gmail.com>
2023-08-30 18:02:55 +0200

    WIP legend wrapping

--- packages/x-charts/src/ChartsLegend/ChartsLegend.tsx
+++ packages/x-charts/src/ChartsLegend/ChartsLegend.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import { useSlotProps } from '@mui/base/utils';
+import { NoSsr } from '@mui/base/NoSsr';
 import { unstable_composeClasses as composeClasses } from '@mui/utils';
 import { useThemeProps, useTheme, Theme, styled } from '@mui/material/styles';
 import { DrawingArea, DrawingContext } from '../context/DrawingProvider';
 import { AnchorPosition, SizingParams, getSeriesToDisplay } from './utils';
 import { FormattedSeries, SeriesContext } from '../context/SeriesContextProvider';
-import { ChartsLegendClasses, getChartsLegendUtilityClass } from './chartsLegendClasses';
+import {
+  ChartsLegendClasses,
+  getChartsLegendUtilityClass,
+  legendClasses,
+} from './chartsLegendClasses';
 import { DefaultizedProps } from '../models/helpers';
-import { ChartSeriesDefaultized, LegendParams } from '../models/seriesType/config';
+import { LegendParams } from '../models/seriesType/config';
+import { getStringSize } from '../internals/domUtils';
+import { Text } from '../internals/components/Text';
 
 export interface ChartsLegendSlotsComponent {
   legend?: React.JSXElementConstructor<LegendRendererProps>;
@@ -42,9 +49,6 @@ export type ChartsLegendProps = {
 
 type DefaultizedChartsLegendProps = DefaultizedProps<ChartsLegendProps, 'direction' | 'position'>;
 
-type SeriesLegendOwnerState = ChartSeriesDefaultized<any> &
-  Pick<DefaultizedChartsLegendProps, 'direction'> & { seriesIndex: number };
-
 const useUtilityClasses = (ownerState: DefaultizedChartsLegendProps & { theme: Theme }) => {
   const { classes, direction } = ownerState;
   const slots = {
@@ -66,113 +70,23 @@ export type ChartsLegendRootOwnerState = {
   seriesNumber: number;
 };
 
-function getTranslePosition({
-  position,
-  drawingArea,
-}: Omit<ChartsLegendRootOwnerState, 'direction' | 'seriesNumber'>) {
-  let xValue: string;
-  switch (position.horizontal) {
-    case 'left':
-      xValue = `calc(var(--ChartsLegend-rootOffsetX, 0px) + ${drawingArea.left}px - var(--ChartsLegend-rootWidth))`;
-      break;
-    case 'middle':
-      xValue = `calc(var(--ChartsLegend-rootOffsetX, 0px) + ${
-        drawingArea.left + drawingArea.width / 2
-      }px - 0.5 * var(--ChartsLegend-rootWidth))`;
-      break;
-    default:
-      xValue = `calc(var(--ChartsLegend-rootOffsetX, 0px) + ${
-        drawingArea.left + drawingArea.width
-      }px)`;
-      break;
-  }
-  let yValue: string;
-  switch (position.vertical) {
-    case 'top':
-      yValue = `calc(var(--ChartsLegend-rootOffsetY, 0px) + ${drawingArea.top}px - var(--ChartsLegend-rootHeight))`;
-      break;
-    case 'middle':
-      yValue = `calc(var(--ChartsLegend-rootOffsetY, 0px) + ${
-        drawingArea.top + drawingArea.height / 2
-      }px - 0.5 * var(--ChartsLegend-rootHeight))`;
-      break;
-    default:
-      yValue = `calc(var(--ChartsLegend-rootOffsetY, 0px) + ${
-        drawingArea.top + drawingArea.height
-      }px)`;
-      break;
-  }
-  return { transform: `translate(${xValue}, ${yValue})` };
-}
-
 export const ChartsLegendRoot = styled('g', {
   name: 'MuiChartsLegend',
   slot: 'Root',
   overridesResolver: (props, styles) => styles.root,
-})<{ ownerState: ChartsLegendRootOwnerState }>(({ ownerState }) => {
-  const { direction, drawingArea, offsetX, offsetY, seriesNumber, position } = ownerState;
-
+})(({ theme }) => {
   return {
-    '--ChartsLegend-rootOffsetX': typeof offsetX === 'number' ? `${offsetX}px` : undefined,
-    '--ChartsLegend-rootOffsetY': typeof offsetY === 'number' ? `${offsetY}px` : undefined,
-    '--ChartsLegend-rootWidth':
-      direction === 'row'
-        ? `calc(var(--ChartsLegend-itemWidth) * ${seriesNumber} + var(--ChartsLegend-rootSpacing) * ${
-            seriesNumber - 1
-          } )`
-        : 'var(--ChartsLegend-itemWidth)',
-    '--ChartsLegend-rootHeight':
-      direction === 'row'
-        ? 'var(--ChartsLegend-itemMarkSize)'
-        : `calc(var(--ChartsLegend-itemMarkSize) * ${seriesNumber} + var(--ChartsLegend-rootSpacing) * ${
-            seriesNumber - 1
-          } )`,
-    ...getTranslePosition({ position, drawingArea, offsetX, offsetY }),
+    [`& .${legendClasses.label}`]: {
+      ...theme.typography.body1,
+      color: 'inherit',
+      fill: theme.palette.text.primary,
+    },
+    [`& .${legendClasses.mark}`]: {
+      x: 0,
+      y: 0,
+    },
   };
 });
-
-export const ChartsSeriesLegendGroup = styled('g', {
-  name: 'MuiChartsLegend',
-  slot: 'ChartsSeriesLegendGroup',
-  overridesResolver: (props, styles) => styles.series,
-})<{ ownerState: SeriesLegendOwnerState }>(({ ownerState }) => {
-  const { direction, seriesIndex } = ownerState;
-
-  if (direction === 'row') {
-    return {
-      transform: `translate(calc(${seriesIndex} * (var(--ChartsLegend-itemWidth) + var(--ChartsLegend-rootSpacing))), 0)`,
-    };
-  }
-  return {
-    transform: `translate(0, calc(${seriesIndex} * (var(--ChartsLegend-itemMarkSize) + var(--ChartsLegend-rootSpacing))))`,
-  };
-});
-
-export const ChartsLegendMark = styled('rect', {
-  name: 'MuiChartsLegend',
-  slot: 'Mark',
-  overridesResolver: (props, styles) => styles.mark,
-})<{ ownerState: { color: string } }>(({ ownerState }) => ({
-  x: 0,
-  y: 0,
-  width: 'var(--ChartsLegend-itemMarkSize)',
-  height: 'var(--ChartsLegend-itemMarkSize)',
-  fill: ownerState.color,
-}));
-export const ChartsLegendLabel = styled('text', {
-  name: 'MuiChartsLegend',
-  slot: 'Label',
-  overridesResolver: (props, styles) => styles.label,
-})(({ theme }) => ({
-  ...theme.typography.body1,
-  color: 'inherit',
-  transform: `translate(
-      calc(var(--ChartsLegend-itemMarkSize) + var(--ChartsLegend-labelSpacing)),
-      calc(0.5 * var(--ChartsLegend-itemMarkSize))
-      )`,
-  fill: theme.palette.text.primary,
-  dominantBaseline: 'central',
-}));
 
 const defaultProps = {
   position: { horizontal: 'middle', vertical: 'top' },
@@ -188,38 +102,103 @@ export interface LegendRendererProps
   seriesToDisplay: LegendParams[];
   drawingArea: DrawingArea;
   classes: Record<'label' | 'mark' | 'series' | 'root', string>;
+  /**
+   * Width of the item mark (in px)
+   * @default 20
+   */
+  itemMarkWidth?: number;
+  /**
+   * Height of the item mark (in px)
+   * @default 20
+   */
+  itemMarkHeight?: number;
+  /**
+   * Space between the mark and the label (in px)
+   * @default 5
+   */
+  labelSpacing?: number;
 }
 
+function getPosition(
+  seriesToDisplay: LegendParams,
+  direction: LegendRendererProps['direction'],
+  position: LegendRendererProps['position'],
+){
+  
+};
 function DefaultChartsLegend(props: LegendRendererProps) {
-  const { hidden, position, direction, offset, series, seriesToDisplay, drawingArea, classes } =
-    props;
+  const {
+    hidden,
+    position,
+    direction,
+    offset,
+    seriesToDisplay,
+    drawingArea,
+    classes,
+    itemMarkWidth = 20,
+    itemMarkHeight = 20,
+    labelSpacing = 5,
+  } = props;
+
+  const seriesWithPosition = React.useMemo(() => {
+    let x = drawingArea.left + (offset?.x ?? 0);
+    let y = offset?.y ?? 0;
+
+    return seriesToDisplay.map(({ label, ...other }) => {
+      const rep = {
+        ...other,
+        label,
+        size: getStringSize(label),
+        positionX: x,
+        positionY: y,
+      };
+
+      if (x + rep.size.width > drawingArea.left + drawingArea.width) {
+        x = drawingArea.left + (offset?.x ?? 0);
+        y += itemMarkSize + labelSpacing;
+        rep.positionX = x;
+        rep.positionY = y;
+      }
+
+      x += itemMarkSize + 3 * labelSpacing + rep.size.width;
+      return rep;
+    });
+  }, [
+    drawingArea.left,
+    drawingArea.width,
+    itemMarkSize,
+    labelSpacing,
+    offset?.x,
+    offset?.y,
+    seriesToDisplay,
+  ]);
 
   if (hidden) {
     return null;
   }
   return (
-    <ChartsLegendRoot
-      ownerState={{
-        direction,
-        offsetX: offset?.x,
-        offsetY: offset?.y,
-        seriesNumber: seriesToDisplay.length,
-        position,
-        drawingArea,
-      }}
-      className={classes.root}
-    >
-      {seriesToDisplay.map(({ id, label, color }, seriesIndex) => (
-        <ChartsSeriesLegendGroup
-          key={id}
-          ownerState={{ direction, seriesIndex, ...series }}
-          className={classes.series}
-        >
-          <ChartsLegendMark ownerState={{ color }} className={classes.mark} />
-          <ChartsLegendLabel className={classes.label}>{label}</ChartsLegendLabel>
-        </ChartsSeriesLegendGroup>
-      ))}
-    </ChartsLegendRoot>
+    <NoSsr>
+      <ChartsLegendRoot className={classes.root}>
+        {seriesWithPosition.map(({ id, label, color, positionX, positionY }) => (
+          <g key={id} className={classes.series} transform={`translate(${positionX} ${positionY})`}>
+            <rect
+              className={classes.mark}
+              width={itemMarkWidth}
+              height={itemMarkHeight}
+              fill={color}
+            />
+            <Text
+              className={classes.label}
+              dominantBaseline="central"
+              textAnchor="start"
+              text={label}
+              x={itemMarkWidth + labelSpacing}
+              y={itemMarkHeight / 2}
+            />
+          </g>
+        ))}
+      </ChartsLegendRoot>
+    </NoSsr>
   );
 }
 
