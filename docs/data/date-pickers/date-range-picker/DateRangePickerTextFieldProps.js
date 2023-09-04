62aba5eea991ee181a989ee7e97fdccb1c272d61
Lukas <llukas.tyla@gmail.com>
2023-09-04 18:59:05 +0300

    [docs] Add conditional range picker props example

---
+++ docs/data/date-pickers/date-range-picker/DateRangePickerTextFieldProps.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import dayjs from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+
+export default function DateRangePickerTextFieldProps() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateRangePicker']}>
+        <DateRangePicker
+          slotProps={{
+            textField: ({ position }) => ({
+              color: position === 'start' ? 'success' : 'warning',
+              focused: true,
+            }),
+          }}
+          defaultValue={[dayjs('2022-04-17'), null]}
+        />
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
