f809feab7d971bd2919f1be56f70ec13e7029fce
delangle <flaviendelangle@gmail.com>
2023-09-06 09:43:42 +0200

    [TreeView] Add missing export

--- docs/data/tree-view-component-api-pages.ts
+++ docs/data/tree-view-component-api-pages.ts
@@ -2,5 +2,6 @@ import type { MuiPage } from '@mui/monorepo/docs/src/MuiPage';
 
 export default [
   { pathname: '/x/api/tree-view/tree-item', title: 'TreeItem' },
+  { pathname: '/x/api/tree-view/tree-item-content', title: 'TreeItemContent' },
   { pathname: '/x/api/tree-view/tree-view', title: 'TreeView' },
 ] as MuiPage[];
---
+++ docs/pages/x/api/tree-view/tree-item-content.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import ApiPage from 'docs/src/modules/components/ApiPage';
+import mapApiPageTranslations from 'docs/src/modules/utils/mapApiPageTranslations';
+import jsonPageContent from './tree-item-content.json';
+
+export default function Page(props) {
+  const { descriptions, pageContent } = props;
+  return <ApiPage descriptions={descriptions} pageContent={pageContent} />;
+}
+
+Page.getInitialProps = () => {
+  const req = require.context(
+    'docsx/translations/api-docs/tree-view',
+    false,
+    /\.\/tree-item-content(-[a-z]{2})?\.json$/,
+  );
+  const descriptions = mapApiPageTranslations(req);
+
+  return {
+    descriptions,
+    pageContent: jsonPageContent,
+  };
+};
