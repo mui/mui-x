e725d5a4eecccec57fa890486769b062f409bacb
Rom Grk <romgrk.cc@gmail.com>
2023-09-20 16:10:09 -0400

    lint

--- templates/x-data-grid/index.tsx
+++ templates/x-data-grid/index.tsx
@@ -3,10 +3,10 @@ import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Demo from './demo';
 
-ReactDOM.createRoot(document.querySelector("#root")).render(
+ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <Demo />
     </StyledEngineProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
