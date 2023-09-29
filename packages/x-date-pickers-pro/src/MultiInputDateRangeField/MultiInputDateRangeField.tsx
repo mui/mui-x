64abb122c2c7ccfd510b55ba8c4d372267717c73
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2023-09-29 08:02:55 +0200

    [pickers] Add warning to `shouldDisableDate` validation (#10502)

--- packages/x-date-pickers-pro/src/MultiInputDateRangeField/MultiInputDateRangeField.tsx
+++ packages/x-date-pickers-pro/src/MultiInputDateRangeField/MultiInputDateRangeField.tsx
@@ -331,6 +331,9 @@ MultiInputDateRangeField.propTypes = {
   ]),
   /**
    * Disable specific date.
+   *
+   * Warning: This function can be called multiple times (e.g. when rendering date calendar, checking if focus can be moved to a certain date, etc.). Expensive computations can impact performance.
+   *
    * @template TDate
    * @param {TDate} day The date to test.
    * @param {string} position The date to test, 'start' or 'end'.
