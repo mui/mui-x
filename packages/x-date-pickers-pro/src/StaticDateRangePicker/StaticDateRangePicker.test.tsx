e67f6d38e9f72fe767a3f2e18fb8d3a5c26e9cfd
Lukas <llukas.tyla@gmail.com>
2023-10-09 13:52:48 +0300

    Remove redundant test

--- packages/x-date-pickers-pro/src/StaticDateRangePicker/StaticDateRangePicker.test.tsx
+++ packages/x-date-pickers-pro/src/StaticDateRangePicker/StaticDateRangePicker.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { expect } from 'chai';
 import { isWeekend } from 'date-fns';
 import { StaticDateRangePicker } from '@mui/x-date-pickers-pro/StaticDateRangePicker';
-import { describeConformance, screen, fireEvent } from '@mui/monorepo/test/utils';
+import { describeConformance, screen } from '@mui/monorepo/test/utils';
 import { wrapPickerMount, createPickerRenderer, adapterToUse } from 'test/utils/pickers';
 import { describeRangeValidation } from '@mui/x-date-pickers-pro/tests/describeRangeValidation';
 
@@ -75,52 +75,5 @@ describe('<StaticDateRangePicker />', () => {
         '[role="grid"] [role="rowgroup"] > [role="row"] button[role="gridcell"]',
       ),
     ).to.have.text('1');
-  });
-
-  it('should be able to set same date twice as range', () => {
-    before(function beforeHook() {
-      if (/jsdom/.test(window.navigator.userAgent)) {
-        // Needs layout
-        this.skip();
-      }
-    });
-
-    // arrange
-    render(
-      <StaticDateRangePicker
-        defaultValue={[
-          adapterToUse.date(new Date(2022, 0, 17)),
-          adapterToUse.date(new Date(2022, 0, 21)),
-        ]}
-      />,
-    );
-
-    const toolbarButtons = screen.getAllByMuiTest('toolbar-button');
-    const startDay = screen.getAllByMuiTest('DateRangeHighlight')[16];
-    const { x, y } = startDay.getBoundingClientRect();
-
-    expect(startDay).to.have.text('17');
-    expect(toolbarButtons[0]).to.have.text('Jan 17');
-    expect(toolbarButtons[1]).to.have.text('Jan 21');
-
-    const now = Date.now();
-    // act
-    fireEvent.touchStart(startDay, {
-      changedTouches: [new Touch({ identifier: now, target: startDay, clientX: x, clientY: y })],
-    });
-    fireEvent.touchMove(startDay, {
-      changedTouches: [
-        new Touch({ identifier: now, target: startDay, clientX: x + 10, clientY: y }),
-      ],
-    });
-    fireEvent.touchEnd(startDay, {
-      changedTouches: [
-        new Touch({ identifier: now, target: startDay, clientX: x + 10, clientY: y }),
-      ],
-    });
-
-    // assert
-    expect(toolbarButtons[0]).to.have.text('Jan 17');
-    expect(toolbarButtons[1]).to.have.text('Jan 17');
   });
 });
