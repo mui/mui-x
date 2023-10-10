b41f3a6859d465998bf39cdadd1219ebdb6a23d1
michel <jsnerdic@gmail.com>
2023-10-10 10:44:40 +0200

    added reference links to GridToolbarQuickFilter component

--- packages/grid/x-data-grid/src/components/toolbar/GridToolbarQuickFilter.tsx
+++ packages/grid/x-data-grid/src/components/toolbar/GridToolbarQuickFilter.tsx
@@ -182,4 +182,11 @@ GridToolbarQuickFilter.propTypes = {
   quickFilterParser: PropTypes.func,
 } as any;
 
+/**
+ * Demos:
+ * - [Filtering - overview](https://mui.com/x/react-data-grid/filtering/)
+ *
+ * API:
+ * - [GridToolbarQuickFilter API](https://mui.com/x/api/data-grid/grid-toolbar-quick-filter/)
+ */
 export { GridToolbarQuickFilter };
