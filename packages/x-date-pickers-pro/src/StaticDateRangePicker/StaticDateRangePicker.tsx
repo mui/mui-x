d25238b5cc1f52c66408594110384e1dae59e924
michel <jsnerdic@gmail.com>
2023-10-10 15:04:10 +0200

    dded reference link to StaticDateRangePicker component

--- packages/x-date-pickers-pro/src/StaticDateRangePicker/StaticDateRangePicker.tsx
+++ packages/x-date-pickers-pro/src/StaticDateRangePicker/StaticDateRangePicker.tsx
@@ -308,4 +308,12 @@ StaticDateRangePicker.propTypes = {
   }),
 } as any;
 
+/**
+ * Demos:
+ * - [DateRangePicker](https://mui.com/x/react-date-pickers/date-range-picker/)
+ * - [Validation](https://mui.com/x/react-date-pickers/validation/)
+ *
+ * API:
+ * - [StaticDateRangePicker API](https://mui.com/x/api/date-pickers/static-date-range-picker/)
+ */
 export { StaticDateRangePicker };
