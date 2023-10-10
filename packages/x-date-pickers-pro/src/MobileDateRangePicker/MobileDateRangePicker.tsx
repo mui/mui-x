27838d3dd834969c07553c90b83e62b48298b029
michel <jsnerdic@gmail.com>
2023-10-10 15:03:29 +0200

    dded reference link to MobileDateRangePicker component

--- packages/x-date-pickers-pro/src/MobileDateRangePicker/MobileDateRangePicker.tsx
+++ packages/x-date-pickers-pro/src/MobileDateRangePicker/MobileDateRangePicker.tsx
@@ -383,4 +383,12 @@ MobileDateRangePicker.propTypes = {
   }),
 } as any;
 
+/**
+ * Demos:
+ * - [DateRangePicker](https://mui.com/x/react-date-pickers/date-range-picker/)
+ * - [Validation](https://mui.com/x/react-date-pickers/validation/)
+ *
+ * API:
+ * - [MobileDateRangePicker API](https://mui.com/x/api/date-pickers/mobile-date-range-picker/)
+ */
 export { MobileDateRangePicker };
