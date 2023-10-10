598738cd4a4d0344eb021db522764dc4e63efa89
michel <jsnerdic@gmail.com>
2023-10-10 15:16:18 +0200

    added reference links to TimeField component

--- packages/x-date-pickers/src/TimeField/TimeField.tsx
+++ packages/x-date-pickers/src/TimeField/TimeField.tsx
@@ -383,4 +383,12 @@ TimeField.propTypes = {
   variant: PropTypes.oneOf(['filled', 'outlined', 'standard']),
 } as any;
 
+/**
+ * Demos:
+ * - [TimeField](http://mui.com/x/react-date-pickers/time-field/)
+ * - [Fields](https://mui.com/x/react-date-pickers/fields/)
+ *
+ * API:
+ * - [TimeField API](https://mui.com/x/api/date-pickers/time-field/)
+ */
 export { TimeField };
