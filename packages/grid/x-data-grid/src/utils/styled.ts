65ff50982940813a9fb443c0efc0db31c2fb4d5f
Bilal Shafi <bilalshafidev@gmail.com>
2023-10-10 16:06:40 +0500

    Update packages/grid/x-data-grid/src/utils/styled.ts
    Signed-off-by: Bilal Shafi <bilalshafidev@gmail.com>

--- packages/grid/x-data-grid/src/utils/styled.ts
+++ packages/grid/x-data-grid/src/utils/styled.ts
@@ -1,6 +1,6 @@
 import { createStyled } from '@mui/system';
 
-// hardcode theme id to avoid importing from @mui/material
+// hardcode theme id to avoid importing from `@mui/material`
 const styled = createStyled({ themeId: '$$material' });
 
 export default styled;
