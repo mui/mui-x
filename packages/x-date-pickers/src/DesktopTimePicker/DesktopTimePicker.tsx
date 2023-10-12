56e27c49c4a5393e8ad9312988386adf6d5cbbc0
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2023-10-12 16:27:22 +0200

    [docs] Added reference links to TimePicker components (#10627)

--- packages/x-date-pickers/src/DesktopTimePicker/DesktopTimePicker.tsx
+++ packages/x-date-pickers/src/DesktopTimePicker/DesktopTimePicker.tsx
@@ -24,6 +24,16 @@ type DesktopTimePickerComponent = (<TDate>(
   props: DesktopTimePickerProps<TDate> & React.RefAttributes<HTMLDivElement>,
 ) => React.JSX.Element) & { propTypes?: any };
 
+/**
+ * Demos:
+ *
+ * - [TimePicker](https://mui.com/x/react-date-pickers/time-picker/)
+ * - [Validation](https://mui.com/x/react-date-pickers/validation/)
+ *
+ * API:
+ *
+ * - [DesktopTimePicker API](https://mui.com/x/api/date-pickers/desktop-time-picker/)
+ */
 const DesktopTimePicker = React.forwardRef(function DesktopTimePicker<TDate>(
   inProps: DesktopTimePickerProps<TDate>,
   ref: React.Ref<HTMLDivElement>,
