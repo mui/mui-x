1ea719d792f9edc54a923746b7e7df625dda0a16
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2023-10-12 16:28:04 +0200

    [docs] Add reference links to DateRangePicker components (#10629)

--- packages/x-date-pickers-pro/src/DateRangePicker/DateRangePicker.tsx
+++ packages/x-date-pickers-pro/src/DateRangePicker/DateRangePicker.tsx
@@ -11,6 +11,16 @@ type DatePickerComponent = (<TDate>(
   props: DateRangePickerProps<TDate> & React.RefAttributes<HTMLDivElement>,
 ) => React.JSX.Element) & { propTypes?: any };
 
+/**
+ * Demos:
+ *
+ * - [DateRangePicker](https://mui.com/x/react-date-pickers/date-range-picker/)
+ * - [Validation](https://mui.com/x/react-date-pickers/validation/)
+ *
+ * API:
+ *
+ * - [DateRangePicker API](https://mui.com/x/api/date-pickers/date-range-picker/)
+ */
 const DateRangePicker = React.forwardRef(function DateRangePicker<TDate>(
   inProps: DateRangePickerProps<TDate>,
   ref: React.Ref<HTMLDivElement>,
