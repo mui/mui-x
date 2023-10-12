a86f2422c7bf512b0c352d96183a963d10a64cb5
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2023-10-12 16:28:26 +0200

    [docs] Add reference links to picker field components (#10631)

--- packages/x-date-pickers-pro/src/MultiInputTimeRangeField/MultiInputTimeRangeField.tsx
+++ packages/x-date-pickers-pro/src/MultiInputTimeRangeField/MultiInputTimeRangeField.tsx
@@ -63,6 +63,16 @@ type MultiInputTimeRangeFieldComponent = (<TDate>(
   props: MultiInputTimeRangeFieldProps<TDate> & React.RefAttributes<HTMLDivElement>,
 ) => React.JSX.Element) & { propTypes?: any };
 
+/**
+ * Demos:
+ *
+ * - [TimeRangeField](http://mui.com/x/react-date-pickers/time-range-field/)
+ * - [Fields](https://mui.com/x/react-date-pickers/fields/)
+ *
+ * API:
+ *
+ * - [MultiInputTimeRangeField API](https://mui.com/x/api/multi-input-time-range-field/)
+ */
 const MultiInputTimeRangeField = React.forwardRef(function MultiInputTimeRangeField<TDate>(
   inProps: MultiInputTimeRangeFieldProps<TDate>,
   ref: React.Ref<HTMLDivElement>,
