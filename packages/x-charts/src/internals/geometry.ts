a072c6df08c30c8b225707f28abdbf7a03705a9a
alexandre <alex.fauquette@gmail.com>
2023-10-16 11:47:11 +0200

    fix geometry

--- packages/x-charts/src/internals/geometry.ts
+++ packages/x-charts/src/internals/geometry.ts
@@ -2,12 +2,17 @@ const ANGLE_APPROX = 5; // Angle (in deg) for which we approximate the rectangle
 
 /**
  * Return the minimal translation along x-axis to avoid overflow of a rectangle of a given width, height, rotation.
+ * This assume that all rectangles have the same heightand angle are in -90, 90.
+ * Otherwise it woul dbe problematic because you need the height/width of the next rectangle to do the correct computation
  * @param width the side along the x axis.
  * @param height the side along the y axis.
  * @param angle the rotation in degree.
  */
 export function getMinXTranslation(width: number, height: number, angle: number = 0) {
-  const standardAngle = Math.min(Math.abs(angle) % 180, Math.abs(Math.abs(angle) - 180) % 180); // Map from R to [0, 90]
+  const standardAngle = Math.min(
+    Math.abs(angle) % 180,
+    Math.abs((Math.abs(angle) % 180) - 180) % 180,
+  ); // Map from R to [0, 90]
 
   if (standardAngle < ANGLE_APPROX) {
     // It's nearly horizontal
@@ -19,7 +24,7 @@ export function getMinXTranslation(width: number, height: number, angle: number 
   }
 
   const radAngle = (standardAngle * Math.PI) / 180;
-  const angleSwich = Math.atan2(width, height);
+  const angleSwich = Math.atan2(height, width);
 
   if (radAngle < angleSwich) {
     return width / Math.cos(radAngle);
