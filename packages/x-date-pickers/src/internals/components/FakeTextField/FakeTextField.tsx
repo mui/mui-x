235c4e2cd470b966dd242d131d6e221b545b0ac3
delangle <flaviendelangle@gmail.com>
2023-10-16 11:08:54 +0200

    [pickers] Change the props received by the FakeTextField component

--- packages/x-date-pickers/src/internals/components/FakeTextField/FakeTextField.tsx
+++ packages/x-date-pickers/src/internals/components/FakeTextField/FakeTextField.tsx
@@ -1,23 +1,32 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
-import { FieldSection } from '../../../models';
 
-interface FakeTextFieldProps {
-  sections: FieldSection[];
+export interface FakeTextFieldElement extends React.HTMLAttributes<HTMLDivElement> {
+  before: string;
+  after: string;
 }
 
-export function FakeTextField(props: FakeTextFieldProps) {
-  const { sections } = props;
+export interface A {}
+
+interface FakeTextFieldProps {
+  elements: FakeTextFieldElement[];
+}
+
+export const FakeTextField = React.forwardRef(function FakeTextField(
+  props: FakeTextFieldProps,
+  ref: React.Ref<HTMLDivElement>,
+) {
+  const { elements } = props;
 
   return (
-    <Stack direction="row" spacing={1}>
-      {sections.map((section) => (
-        <React.Fragment>
-          {section.startSeparator}
-          <input value={section.value} onChange={() => {}} />
-          {section.endSeparator}
+    <Stack direction="row" spacing={1} ref={ref}>
+      {elements.map(({ before, after, ...otherElementProps }, elementIndex) => (
+        <React.Fragment key={elementIndex}>
+          {before}
+          <input {...otherElementProps} />
+          {after}
         </React.Fragment>
       ))}
     </Stack>
   );
-}
+});
