3e3b16c1e48a3f8e2a0ae1cd35388a6114c250a6
Lukas <llukas.tyla@gmail.com>
2023-11-09 13:20:05 +0200

    Fix l10n script to correctly work with 2 letter locale

--- scripts/l10n.ts
+++ scripts/l10n.ts
@@ -67,7 +67,7 @@ function plugin(existingTranslations: Translations): babel.PluginObj {
           }
 
           // Test if the variable name follows the pattern xxXXGrid or xxXXPickers
-          if (!/[a-z]{2}[A-Z]{2}(Grid|Pickers)/.test(node.id.name)) {
+          if (!/[a-z]{2}[A-Z]{2}|[a-z]{2}(Grid|Pickers)/.test(node.id.name)) {
             visitorPath.skip();
             return;
           }
@@ -164,7 +164,7 @@ function extractTranslations(translationsPath: string): [TranslationsByGroup, Tr
 function findLocales(localesDirectory: string, constantsPath: string) {
   const items = fse.readdirSync(localesDirectory);
   const locales: any[] = [];
-  const localeRegex = /^[a-z]{2}([A-Z]{2})?/;
+  const localeRegex = /^[a-z]{2}[A-Z]{2}|^[a-z]{2}(?=.ts)/;
 
   items.forEach((item) => {
     const match = item.match(localeRegex);
@@ -176,7 +176,7 @@ function findLocales(localesDirectory: string, constantsPath: string) {
     if (fse.lstatSync(localePath).isDirectory()) {
       return;
     }
-    const code = match[0];
+    const code = match[0] || match[1];
     if (constantsPath !== localePath) {
       // Ignore the locale used as a reference
       locales.push([localePath, code]);
@@ -412,6 +412,9 @@ async function run(argv: yargs.ArgumentsCamelCase<HandlerArgv>) {
         extractAndReplaceTranslations(localePath);
 
       if (!transformedCode || Object.keys(existingTranslations).length === 0) {
+        console.info('returning:', localeCode, localePath, {
+          translationsLength: Object.keys(existingTranslations).length,
+        });
         return;
       }
 
