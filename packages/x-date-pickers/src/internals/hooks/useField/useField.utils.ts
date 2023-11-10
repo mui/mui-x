ca76bc9d342479b07cb147799fd4b6de515db5ff
delangle <flaviendelangle@gmail.com>
2023-11-10 09:23:53 +0100

    Add test

--- packages/x-date-pickers/src/internals/hooks/useField/useField.utils.ts
+++ packages/x-date-pickers/src/internals/hooks/useField/useField.utils.ts
@@ -824,7 +824,7 @@ export const validateSections = <TSection extends FieldSection>(
 ) => {
   if (process.env.NODE_ENV !== 'production') {
     if (!warnedOnceInvalidSection) {
-      const supportedSections: FieldSectionType[] = [];
+      const supportedSections: FieldSectionType[] = ['empty'];
       if (['date', 'date-time'].includes(valueType)) {
         supportedSections.push('weekDay', 'day', 'month', 'year');
       }
@@ -836,7 +836,7 @@ export const validateSections = <TSection extends FieldSection>(
 
       if (invalidSection) {
         console.warn(
-          `MUI: The field component you are using is not compatible with the "${invalidSection.type} date section.`,
+          `MUI: The field component you are using is not compatible with the "${invalidSection.type}" date section.`,
           `The supported date sections are ["${supportedSections.join('", "')}"]\`.`,
         );
         warnedOnceInvalidSection = true;
