3011df011681cd1fd36863596e70933e915adb0a
delangle <flaviendelangle@gmail.com>
2023-11-10 17:35:38 +0100

    Fix

--- packages/x-date-pickers/src/models/adapters.ts
+++ packages/x-date-pickers/src/models/adapters.ts
@@ -273,23 +273,12 @@ export interface MuiPickersAdapter<TDate, TLocale = any> {
    */
   formatNumber(numberToFormat: string): string;
   /**
-   * Compute the difference between the two dates in the unit provided.
-   * @deprecated  Will be removed in v7.
-   * @template TDate
-   * @param {TDate} value The reference date.
-   * @param {TDate | string} comparing The date to compare with the reference date.
-   * @param {AdapterUnits} unit The unit in which we want to the result to be.
-   * @returns {number} The diff between the two dates.
-   */
-  getDiff(value: TDate, comparing: TDate | string, unit?: AdapterUnits): number;
-  // TODO v7: Type `value` and `comparing` to be `TDate | null`.
-  /**
    * Check if the two dates are equal (e.g: they represent the same timestamp).
-   * @param {any} value The reference date.
-   * @param {any} comparing The date to compare with the reference date.
+   * @param {TDate | null} value The reference date.
+   * @param {TDate | null} comparing The date to compare with the reference date.
    * @returns {boolean} `true` if the two dates are equal.
    */
-  isEqual(value: any, comparing: any): boolean;
+  isEqual(value: TDate | null, comparing: TDate | null): boolean;
   /**
    * Check if the two dates are in the same year (using the timezone of the reference date).
    * @template TDate
