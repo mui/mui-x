4833e73ef025101acd8ac0584b081dabedaf0abd
Lukas <llukas.tyla@gmail.com>
2023-11-10 18:01:22 +0200

    Use direct link to avoid redirect

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -232,8 +232,8 @@ function AppWrapper(props) {
             text: `v${process.env.DATA_GRID_VERSION}`,
             current: true,
           },
-          { text: 'v6', href: `https://mui.com${languagePrefix}/components/data-grid/` },
-          { text: 'v5', href: `https://v5.mui.com${languagePrefix}/components/data-grid/` },
+          { text: 'v6', href: `https://mui.com${languagePrefix}/x/react-data-grid/` },
+          { text: 'v5', href: `https://v5.mui.com${languagePrefix}/x/react-data-grid/` },
           { text: 'v4', href: `https://v4.mui.com${languagePrefix}/components/data-grid/` },
         ],
       };
