878b02908a3c9aa28fd3480ca566c90f25745037
delangle <flaviendelangle@gmail.com>
2023-11-21 13:25:54 +0100

    Fix

--- docs/data/tree-view-component-api-pages.ts
+++ docs/data/tree-view-component-api-pages.ts
@@ -1,7 +1,7 @@
 import type { MuiPage } from '@mui/monorepo/docs/src/MuiPage';
 
 export default [
+  { pathname: '/x/api/tree-view/simple-tree-item', title: 'SimpleTreeItem' },
   { pathname: '/x/api/tree-view/simple-tree-view', title: 'SimpleTreeView' },
-  { pathname: '/x/api/tree-view/tree-item', title: 'TreeItem' },
   { pathname: '/x/api/tree-view/tree-view', title: 'TreeView' },
 ] as MuiPage[];
