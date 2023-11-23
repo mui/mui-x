b9b78c7a02fbaaf889ab5c6bdbacf5556027c6d2
Flavien DELANGLE <flaviendelangle@gmail.com>
2023-11-23 16:00:46 +0100

    [grid] Remove unused utilities (#11156)

--- packages/grid/x-data-grid/src/utils/domUtils.ts
+++ packages/grid/x-data-grid/src/utils/domUtils.ts
@@ -9,22 +9,6 @@ export function findParentElementFromClassName(elem: Element, className: string)
   return elem.closest(`.${className}`);
 }
 
-export function getRowEl(cell?: Element | null): HTMLElement | null {
-  if (!cell) {
-    return null;
-  }
-  return findParentElementFromClassName(cell as HTMLDivElement, gridClasses.row)! as HTMLElement;
-}
-
-// TODO remove
-export function isGridCellRoot(elem: Element | null): boolean {
-  return elem != null && elem.classList.contains(gridClasses.cell);
-}
-
-export function isGridHeaderCellRoot(elem: Element | null): boolean {
-  return elem != null && elem.classList.contains(gridClasses.columnHeader);
-}
-
 function escapeOperandAttributeSelector(operand: string): string {
   return operand.replace(/["\\]/g, '\\$&');
 }
