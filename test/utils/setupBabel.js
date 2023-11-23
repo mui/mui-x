473ad45c191a95d951be00526db47a734b8a5039
Lukas <llukas.tyla@gmail.com>
2023-11-23 15:30:53 +0200

    Add necessary plugin in tests

--- test/utils/setupBabel.js
+++ test/utils/setupBabel.js
@@ -1,4 +1,5 @@
 require('@babel/register')({
   extensions: ['.js', '.ts', '.tsx'],
   ignore: [/node_modules\/(?!@mui\/monorepo)/],
+  plugins: ['@babel/plugin-transform-export-namespace-from'],
 });
