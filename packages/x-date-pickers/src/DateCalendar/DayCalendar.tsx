8ea8e61743f2401f1bb93097a3edb5cc501e5ea8
Flavien DELANGLE <flaviendelangle@gmail.com>
2023-11-24 14:31:25 +0100

    Update packages/x-date-pickers/src/DateCalendar/DayCalendar.tsx
    Co-authored-by: Lukas <llukas.tyla@gmail.com>
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>

--- packages/x-date-pickers/src/DateCalendar/DayCalendar.tsx
+++ packages/x-date-pickers/src/DateCalendar/DayCalendar.tsx
@@ -72,7 +72,7 @@ export interface ExportedDayCalendarProps<TDate> extends ExportedPickersDayProps
   displayWeekNumber?: boolean;
   /**
    * The day view will show as many weeks as needed after the end of the current month to match this value.
-   * Put it to 6 to have a fix number of weeks in Gregorian calendars
+   * Put it to 6 to have a fixed number of weeks in Gregorian calendars
    * @default undefined
    */
   fixedWeekNumber?: number;
