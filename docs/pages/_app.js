3bf5dc976efaae0b86c139e47b544370ffe3f1b1
Lukas <llukas.tyla@gmail.com>
2023-11-24 09:56:50 +0200

    Avoid redirect

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -227,10 +227,10 @@ function AppWrapper(props) {
         versions: [
           {
             text: 'v7 (next)',
-            href: `https://next.mui.com${languagePrefix}/components/data-grid/`,
+            href: `https://next.mui.com${languagePrefix}/x/react-data-grid/`,
           },
           { text: `v${process.env.DATA_GRID_VERSION}`, current: true },
-          { text: 'v5', href: `https://v5.mui.com${languagePrefix}/components/data-grid/` },
+          { text: 'v5', href: `https://v5.mui.com${languagePrefix}/x/react-data-grid/` },
           { text: 'v4', href: `https://v4.mui.com${languagePrefix}/components/data-grid/` },
         ],
       };
