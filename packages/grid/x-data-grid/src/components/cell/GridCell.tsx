d3c725e277ef7de1c1af16841958a2fbd27fda65
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-11-27 18:41:20 +0100

    [core] Remove dead prop

--- packages/grid/x-data-grid/src/components/cell/GridCell.tsx
+++ packages/grid/x-data-grid/src/components/cell/GridCell.tsx
@@ -267,7 +267,6 @@ const GridCell = React.forwardRef<HTMLDivElement, GridCellProps>((props, ref) =>
     className,
     showRightBorder,
     extendRowFullWidth,
-    row,
     colSpan,
     disableDragEvents,
     isNotVisible,
