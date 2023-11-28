dd32f6fe2334513eef70a925ddfd27beaefafa1b
Andrew Cherniavskyi <andrew@mui.com>
2023-11-28 16:46:37 +0100

    stop exporting GridPreferencesPanel component

--- packages/grid/x-data-grid/src/components/GridHeader.tsx
+++ packages/grid/x-data-grid/src/components/GridHeader.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useGridRootProps } from '../hooks/utils/useGridRootProps';
-import { GridPreferencesPanel } from './panel';
+import { GridPreferencesPanel } from './panel/GridPreferencesPanel';
 
 export function GridHeader() {
   const rootProps = useGridRootProps();
