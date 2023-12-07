7091c184d6d074032fe38c4ebbe28f6613397dd1
alexandre <alex.fauquette@gmail.com>
2023-12-07 18:13:45 +0100

    fixes

--- packages/x-date-pickers/src/internals/components/PickersTextField/Outline.tsx
+++ packages/x-date-pickers/src/internals/components/PickersTextField/Outline.tsx
@@ -85,7 +85,7 @@ export default function Outline(props: OutlineProps) {
           <span>{label}</span>
         ) : (
           // notranslate needed while Google Translate will not fix zero-width space issue
-          <span className="notranslate">​</span>
+          <span className="notranslate">&#8203;</span>
         )}
       </OutlineLegend>
     </OutlineRoot>
