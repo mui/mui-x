3035400a68a99f13c387b6c74136e4126991d6cf
alexandre <alex.fauquette@gmail.com>
2023-12-08 16:36:57 +0100

    last update

--- packages/grid/x-data-grid-pro/src/tests/columnPinning.DataGridPro.test.tsx
+++ packages/grid/x-data-grid-pro/src/tests/columnPinning.DataGridPro.test.tsx
@@ -880,7 +880,7 @@ describe('<DataGridPro /> - Column pinning', () => {
     const rows = [
       { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
       { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-      { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 42 },
+      { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
       { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
       { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
       { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
