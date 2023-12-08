d21f79f7ef70bc138f7afee0609ea9ab243f0d06
Bilal Shafi <bilalshafidev@gmail.com>
2023-12-08 22:02:37 +0500

    [codemod] Add cellSelection codemod and update migration guide

--- packages/x-codemod/src/v7.0.0/data-grid/preset-safe/actual.spec.js
+++ packages/x-codemod/src/v7.0.0/data-grid/preset-safe/actual.spec.js
@@ -38,6 +38,7 @@ export default function App() {
             },
           },
         }}
+        unstable_cellSelection
       />
     </div>
   );
--- packages/x-codemod/src/v7.0.0/data-grid/preset-safe/index.ts
+++ packages/x-codemod/src/v7.0.0/data-grid/preset-safe/index.ts
@@ -1,9 +1,11 @@
-import transformRenameComponentsToSlots from '../../../v6.0.0/data-grid/rename-components-to-slots';
+import transformRenameComponentsToSlots from '../rename-components-to-slots';
+import renameCellSelectionProps from '../rename-cell-selection-props';
 
 import { JsCodeShiftAPI, JsCodeShiftFileInfo } from '../../../types';
 
 export default function transformer(file: JsCodeShiftFileInfo, api: JsCodeShiftAPI, options: any) {
   file.source = transformRenameComponentsToSlots(file, api, options);
+  file.source = renameCellSelectionProps(file, api, options);
 
   return file.source;
 }
