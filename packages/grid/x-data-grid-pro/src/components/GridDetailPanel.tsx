165f092eda03a2c3743d9291eba84c5c1946a194
Rom Grk <romgrk.cc@gmail.com>
2023-12-08 03:14:46 -0500

    fix: detail panel autoheight with resize observer

--- packages/grid/x-data-grid-pro/src/components/GridDetailPanel.tsx
+++ packages/grid/x-data-grid-pro/src/components/GridDetailPanel.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import { GridRowId } from '@mui/x-data-grid';
+import { useResizeObserver } from '@mui/x-data-grid/internals';
 import { useGridRootProps } from '../hooks/utils/useGridRootProps';
 import { useGridPrivateApiContext } from '../hooks/utils/useGridPrivateApiContext';
 import { DataGridProProcessedProps } from '../models/dataGridProProps';
@@ -37,21 +38,18 @@ function GridDetailPanel(props: GridDetailPanelProps) {
   const ref = React.useRef<HTMLDivElement>();
   const rootProps = useGridRootProps();
   const ownerState = rootProps;
+  const hasAutoHeight = height === 'auto';
 
   React.useLayoutEffect(() => {
-    if (height === 'auto' && typeof ResizeObserver === 'undefined') {
+    if (hasAutoHeight && typeof ResizeObserver === 'undefined') {
       // Fallback for IE
       apiRef.current.storeDetailPanelHeight(rowId, ref.current!.clientHeight);
     }
   }, [apiRef, height, rowId]);
 
-  React.useLayoutEffect(() => {
-    const hasFixedHeight = height !== 'auto';
-    if (hasFixedHeight || typeof ResizeObserver === 'undefined') {
-      return undefined;
-    }
-
-    const resizeObserver = new ResizeObserver((entries) => {
+  useResizeObserver(
+    ref,
+    (entries) => {
       const [entry] = entries;
       const observedHeight =
         entry.borderBoxSize && entry.borderBoxSize.length > 0
@@ -59,12 +57,9 @@ function GridDetailPanel(props: GridDetailPanelProps) {
           : entry.contentRect.height;
 
       apiRef.current.storeDetailPanelHeight(rowId, observedHeight);
-    });
-
-    resizeObserver.observe(ref.current!);
-
-    return () => resizeObserver.disconnect();
-  }, [apiRef, height, rowId]);
+    },
+    hasAutoHeight,
+  );
 
   return (
     <DetailPanel
--- packages/grid/x-data-grid/src/hooks/utils/useResizeObserver.ts
+++ packages/grid/x-data-grid/src/hooks/utils/useResizeObserver.ts
@@ -1,22 +1,34 @@
 import * as React from 'react';
 import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/utils';
 
-export function useResizeObserver(ref: React.MutableRefObject<HTMLElement | null>, fn: Function) {
-  useEnhancedEffect(() => {
-    fn();
+const isDevEnvironment = process.env.NODE_ENV === 'development';
 
-    if (typeof ResizeObserver === 'undefined') {
-      return () => {};
+const noop = () => {};
+
+export function useResizeObserver(
+  ref: React.MutableRefObject<HTMLElement | undefined | null>,
+  fn: (entries: ResizeObserverEntry[]) => void,
+  enabled?: boolean,
+) {
+  useEnhancedEffect(() => {
+    if (enabled === false || typeof ResizeObserver === 'undefined') {
+      return noop;
     }
 
     let frameID = 0;
 
     const target = ref.current;
-    const observer = new ResizeObserver(() => {
+    const observer = new ResizeObserver((entries) => {
       // See https://github.com/mui/mui-x/issues/8733
-      frameID = requestAnimationFrame(() => {
-        fn();
-      });
+      // In dev, we avoid the React warning by moving the task to the next frame.
+      // In prod, we want the task to run in the same frame as to avoid tear.
+      if (isDevEnvironment) {
+        frameID = requestAnimationFrame(() => {
+          fn(entries);
+        });
+      } else {
+        fn(entries);
+      }
     });
 
     if (target) {
@@ -31,5 +43,5 @@ export function useResizeObserver(ref: React.MutableRefObject<HTMLElement | null
       observer.disconnect();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [enabled]);
 }
