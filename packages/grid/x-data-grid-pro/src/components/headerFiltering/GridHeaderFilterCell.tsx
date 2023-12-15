30db955f1969bbdbfb68a9120b9e7d0a6ab5537d
Andrew Cherniavskyi <andrew@mui.com>
2023-12-15 13:29:43 +0100

    simplify focus condition

--- packages/grid/x-data-grid-pro/src/components/headerFiltering/GridHeaderFilterCell.tsx
+++ packages/grid/x-data-grid-pro/src/components/headerFiltering/GridHeaderFilterCell.tsx
@@ -194,8 +194,8 @@ const GridHeaderFilterCell = React.forwardRef<HTMLDivElement, GridHeaderFilterCe
     const onMouseDown = React.useCallback(
       (event: React.MouseEvent) => {
         if (!hasFocus) {
-          if (inputRef.current && inputRef.current.contains?.(event.target as HTMLElement)) {
-            inputRef.current?.focus();
+          if (inputRef.current?.contains?.(event.target as HTMLElement)) {
+            inputRef.current.focus();
           }
           apiRef.current.setColumnHeaderFilterFocus(colDef.field, event);
         }
