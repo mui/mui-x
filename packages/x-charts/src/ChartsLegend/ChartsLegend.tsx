5a5882262c66e4eb4a80dd6fb3df043e97ced180
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2023-12-18 09:42:37 +0100

    [charts] Make the helper `ChartsText` component public (#11370)
    Co-authored-by: Lukas <llukas.tyla@gmail.com>

--- packages/x-charts/src/ChartsLegend/ChartsLegend.tsx
+++ packages/x-charts/src/ChartsLegend/ChartsLegend.tsx
@@ -9,8 +9,9 @@ import { FormattedSeries, SeriesContext } from '../context/SeriesContextProvider
 import { ChartsLegendClasses, getLegendUtilityClass } from './chartsLegendClasses';
 import { DefaultizedProps } from '../models/helpers';
 import { LegendParams } from '../models/seriesType/config';
-import { ChartsText, ChartsTextStyle, getWordsByLines } from '../internals/components/ChartsText';
+import { ChartsText, ChartsTextStyle } from '../ChartsText';
 import { CardinalDirections } from '../models/layout';
+import { getWordsByLines } from '../internals/getWordsByLines';
 
 export interface ChartsLegendSlotsComponent {
   legend?: React.JSXElementConstructor<LegendRendererProps>;
