eb6ebb4e163474497d2bbead89062ce6aa0fea42
alexandre <alex.fauquette@gmail.com>
2023-12-19 15:39:05 +0100

    handle the case of default axis id

--- packages/x-charts/src/LineChart/AreaPlot.tsx
+++ packages/x-charts/src/LineChart/AreaPlot.tsx
@@ -6,6 +6,7 @@ import { CartesianContext } from '../context/CartesianContextProvider';
 import { AreaElement, AreaElementProps } from './AreaElement';
 import { getValueToPositionMapper } from '../hooks/useScale';
 import getCurveFactory from '../internals/getCurve';
+import { DEFAULT_X_AXIS_KEY } from '../constants';
 
 export interface AreaPlotSlots {
   area?: React.JSXElementConstructor<AreaElementProps>;
@@ -63,7 +64,11 @@ function AreaPlot(props: AreaPlotProps) {
           if (process.env.NODE_ENV !== 'production') {
             if (xData === undefined) {
               throw new Error(
-                `Axis of id "${xAxisKey}" should have data property to be able to display a line plot.`,
+                `MUI-Charts: ${
+                  xAxisKey === DEFAULT_X_AXIS_KEY
+                    ? 'The first `xAxis`'
+                    : `The x-axis with id "${xAxisKey}"`
+                } should have data property to be able to display a line plot.`,
               );
             }
             if (xData.length < stackedData.length) {
--- packages/x-charts/src/LineChart/LinePlot.tsx
+++ packages/x-charts/src/LineChart/LinePlot.tsx
@@ -6,6 +6,7 @@ import { CartesianContext } from '../context/CartesianContextProvider';
 import { LineElement, LineElementProps } from './LineElement';
 import { getValueToPositionMapper } from '../hooks/useScale';
 import getCurveFactory from '../internals/getCurve';
+import { DEFAULT_X_AXIS_KEY } from '../constants';
 
 export interface LinePlotSlots {
   line?: React.JSXElementConstructor<LineElementProps>;
@@ -61,7 +62,11 @@ function LinePlot(props: LinePlotProps) {
           if (process.env.NODE_ENV !== 'production') {
             if (xData === undefined) {
               throw new Error(
-                `Axis of id "${xAxisKey}" should have data property to be able to display a line plot`,
+                `MUI-Charts: ${
+                  xAxisKey === DEFAULT_X_AXIS_KEY
+                    ? 'The first `xAxis`'
+                    : `The x-axis with id "${xAxisKey}"`
+                } should have data property to be able to display a line plot`,
               );
             }
             if (xData.length < stackedData.length) {
