188ded68286bb25abf15f3284ebc2c987980d1ce
delangle <flaviendelangle@gmail.com>
2024-01-02 12:12:21 +0100

    Add codemod

--- packages/x-codemod/src/v7.0.0/tree-view/preset-safe/index.ts
+++ packages/x-codemod/src/v7.0.0/tree-view/preset-safe/index.ts
@@ -1,8 +1,10 @@
+import transformRenameExpansionProps from '../rename-expansion-props';
 import transformRenameTreeViewSimpleTreeView from '../rename-tree-view-simple-tree-view';
 
 import { JsCodeShiftAPI, JsCodeShiftFileInfo } from '../../../types';
 
 export default function transformer(file: JsCodeShiftFileInfo, api: JsCodeShiftAPI, options: any) {
+  file.source = transformRenameExpansionProps(file, api, options);
   file.source = transformRenameTreeViewSimpleTreeView(file, api, options);
 
   return file.source;
