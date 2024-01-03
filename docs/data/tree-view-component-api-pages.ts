93002fde338e87a0960f2fd31c5067658795c8ee
alexandre <alex.fauquette@gmail.com>
2024-01-03 13:07:20 +0100

    generate tree view api

--- docs/data/tree-view-component-api-pages.ts
+++ docs/data/tree-view-component-api-pages.ts
@@ -1,8 +1,20 @@
 import type { MuiPage } from '@mui/monorepo/docs/src/MuiPage';
 
 export default [
-  { pathname: '/x/api/tree-view/rich-tree-view', title: 'RichTreeView' },
-  { pathname: '/x/api/tree-view/simple-tree-view', title: 'SimpleTreeView' },
-  { pathname: '/x/api/tree-view/tree-item', title: 'TreeItem' },
-  { pathname: '/x/api/tree-view/tree-view', title: 'TreeView' },
+  {
+    pathname: '/x/api/tree-view/rich-tree-view/',
+    title: 'RichTreeView',
+  },
+  {
+    pathname: '/x/api/tree-view/simple-tree-view/',
+    title: 'SimpleTreeView',
+  },
+  {
+    pathname: '/x/api/tree-view/tree-item/',
+    title: 'TreeItem',
+  },
+  {
+    pathname: '/x/api/tree-view/tree-view/',
+    title: 'TreeView',
+  },
 ] as MuiPage[];
