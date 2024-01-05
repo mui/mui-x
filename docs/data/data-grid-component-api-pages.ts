44a097dedb2b7538cde19a76aca291cf328ba95a
Andrew Cherniavskyi <andrew@mui.com>
2024-01-05 13:26:15 +0100

    fix side navigation collapse when opening an api page

--- docs/data/data-grid-component-api-pages.ts
+++ docs/data/data-grid-component-api-pages.ts
@@ -2,29 +2,29 @@ import type { MuiPage } from '@mui/monorepo/docs/src/MuiPage';
 
 export default [
   {
-    pathname: '/x/api/data-grid/data-grid/',
+    pathname: '/x/api/data-grid/data-grid',
     title: 'DataGrid',
   },
   {
-    pathname: '/x/api/data-grid/data-grid-premium/',
+    pathname: '/x/api/data-grid/data-grid-premium',
     title: 'DataGridPremium',
     plan: 'premium',
   },
   {
-    pathname: '/x/api/data-grid/data-grid-pro/',
+    pathname: '/x/api/data-grid/data-grid-pro',
     title: 'DataGridPro',
     plan: 'pro',
   },
   {
-    pathname: '/x/api/data-grid/grid-filter-form/',
+    pathname: '/x/api/data-grid/grid-filter-form',
     title: 'GridFilterForm',
   },
   {
-    pathname: '/x/api/data-grid/grid-filter-panel/',
+    pathname: '/x/api/data-grid/grid-filter-panel',
     title: 'GridFilterPanel',
   },
   {
-    pathname: '/x/api/data-grid/grid-toolbar-quick-filter/',
+    pathname: '/x/api/data-grid/grid-toolbar-quick-filter',
     title: 'GridToolbarQuickFilter',
   },
 ] as MuiPage[];
--- docs/data/tree-view-component-api-pages.ts
+++ docs/data/tree-view-component-api-pages.ts
@@ -2,19 +2,19 @@ import type { MuiPage } from '@mui/monorepo/docs/src/MuiPage';
 
 export default [
   {
-    pathname: '/x/api/tree-view/rich-tree-view/',
+    pathname: '/x/api/tree-view/rich-tree-view',
     title: 'RichTreeView',
   },
   {
-    pathname: '/x/api/tree-view/simple-tree-view/',
+    pathname: '/x/api/tree-view/simple-tree-view',
     title: 'SimpleTreeView',
   },
   {
-    pathname: '/x/api/tree-view/tree-item/',
+    pathname: '/x/api/tree-view/tree-item',
     title: 'TreeItem',
   },
   {
-    pathname: '/x/api/tree-view/tree-view/',
+    pathname: '/x/api/tree-view/tree-view',
     title: 'TreeView',
   },
 ] as MuiPage[];
