b1c759ef10985e912a9811e218f50c4c43f5a674
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-01-06 00:17:09 +0100

    [charts][docs] Improve landing page (#11570)

--- docs/src/modules/components/ChartComponentsGrid.js
+++ docs/src/modules/components/ChartComponentsGrid.js
@@ -80,7 +80,7 @@ export default function ChartComponentsGrid() {
               })}
             />
             <Typography
-              component="h2"
+              component="h3"
               variant="body2"
               fontWeight="semiBold"
               sx={{ px: 2, py: 1.5 }}
