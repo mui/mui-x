0746e769912cbd407a8f787f40381bc8641f8618
Andrew Cherniavskyi <andrew@mui.com>
2024-01-05 23:11:14 +0100

    lint

--- packages/grid/x-data-grid/src/components/panel/GridPanel.test.tsx
+++ packages/grid/x-data-grid/src/components/panel/GridPanel.test.tsx
@@ -5,7 +5,6 @@ import {
   gridPanelClasses as classes,
   useGridApiRef,
   GridApiContext,
-  gridClasses,
 } from '@mui/x-data-grid';
 import { GridRootPropsContext } from '@mui/x-data-grid/context/GridRootPropsContext';
 import Popper from '@mui/material/Popper';
