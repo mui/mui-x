0ed71686c2c2a9db7b31c9971b72444e76a304d6
Muhammed Mustafa <MuhammedElruby@gmail.com>
2024-01-05 16:13:08 +0200

    [DataGrid] follow warning message guideline for `autoPageSize` and `autoHeight` (#11585)

--- packages/grid/x-data-grid-pro/src/DataGridPro/DataGridPro.tsx
+++ packages/grid/x-data-grid-pro/src/DataGridPro/DataGridPro.tsx
@@ -99,7 +99,12 @@ DataGridProRaw.propTypes = {
   autoPageSize: chainPropTypes(PropTypes.bool, (props: any) => {
     if (props.autoHeight && props.autoPageSize) {
       return new Error(
-        'MUI: The `autoPageSize` prop will conflict with `autoHeight` prop when both of them are enabled.',
+        [
+          'MUI: `<DataGrid autoPageSize={true} autoHeight={true} />` are not valid props.',
+          'You can not use both the `autoPageSize` and `autoHeight` props at the same time because `autoHeight` scales the height of the Data Grid according to the `pageSize`.',
+          '',
+          'Please remove one of these two props.',
+        ].join('\n'),
       );
     }
     return null;
