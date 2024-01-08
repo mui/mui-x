13ed31cb361ff86f47bcdb07f9156931a5b06fdc
Pascal Corpet <pascal@co2ai.com>
2024-01-08 22:25:40 +0100

    [DataGrid] Allow passing readonly arrays to `pageSizeOptions` prop
    Contributes to #10685

--- packages/x-data-grid/src/components/GridPagination.tsx
+++ packages/x-data-grid/src/components/GridPagination.tsx
@@ -26,6 +26,10 @@ const GridPaginationRoot = styled(TablePagination)(({ theme }) => ({
   },
 })) as typeof TablePagination;
 
+// A mutable version of a readonly array.
+
+type MutableArray<A> = A extends readonly (infer T)[] ? T[] : never;
+
 export const GridPagination = React.forwardRef<unknown, Partial<TablePaginationProps>>(
   function GridPagination(props, ref) {
     const apiRef = useGridApiContext();
@@ -103,7 +107,10 @@ export const GridPagination = React.forwardRef<unknown, Partial<TablePaginationP
         component="div"
         count={rowCount}
         page={paginationModel.page <= lastPage ? paginationModel.page : lastPage}
-        rowsPerPageOptions={pageSizeOptions}
+        // TODO: Remove the cast once the type is fixed in MUI and that the min MUI version
+        // for x-data-grid is past the fix.
+        // Note that MUI will not mutate the array, so this is safe.
+        rowsPerPageOptions={pageSizeOptions as MutableArray<typeof pageSizeOptions>}
         rowsPerPage={paginationModel.pageSize}
         onPageChange={handlePageChange}
         onRowsPerPageChange={handlePageSizeChange}
