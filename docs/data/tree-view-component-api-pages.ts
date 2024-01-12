4c83aef8a4766bc8f956166de78b7bde81c963c6
alexandre <alex.fauquette@gmail.com>
2024-01-12 11:30:46 +0100

    docs:api

--- docs/data/tree-view-component-api-pages.ts
+++ docs/data/tree-view-component-api-pages.ts
@@ -1,6 +1,12 @@
 import type { MuiPage } from '@mui/monorepo/docs/src/MuiPage';
 
 export default [
-  { pathname: '/x/api/tree-view/tree-item', title: 'TreeItem' },
-  { pathname: '/x/api/tree-view/tree-view', title: 'TreeView' },
+  {
+    pathname: '/x/api/tree-view/tree-item/',
+    title: 'TreeItem',
+  },
+  {
+    pathname: '/x/api/tree-view/tree-view/',
+    title: 'TreeView',
+  },
 ] as MuiPage[];
