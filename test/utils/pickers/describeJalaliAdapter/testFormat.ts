74b3ec94d1004103f0d727cae65d09a7e2b0e2b1
delangle <flaviendelangle@gmail.com>
2024-01-12 14:30:49 +0100

    Fix

--- test/utils/pickers/describeJalaliAdapter/testFormat.ts
+++ test/utils/pickers/describeJalaliAdapter/testFormat.ts
@@ -4,6 +4,7 @@ import { DescribeJalaliAdapterTestSuite } from './describeJalaliAdapter.types';
 export const testFormat: DescribeJalaliAdapterTestSuite = ({ adapter }) => {
   it.only('should format the seconds without leading zeroes for format "s"', () => {
     const date = adapter.date('2020-01-01T23:44:09.000Z')!;
-    expect(adapter.formatByString(date, 's')).to.equal('9');
+    // Should be either ۹ or 9 depending on the config of the adapter.
+    expect(adapter.formatByString(date, 's').replace('9', '۹')).to.equal('۹');
   });
 };
