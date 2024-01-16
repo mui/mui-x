3adaad4717a23c9ec5c1bfabf48d504904b9fa66
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-01-16 01:01:12 +0100

    forgot this one

--- packages/x-date-pickers/src/TimePicker/TimePickerToolbar.tsx
+++ packages/x-date-pickers/src/TimePicker/TimePickerToolbar.tsx
@@ -272,7 +272,7 @@ TimePickerToolbar.propTypes = {
   ampmInClock: PropTypes.bool,
   classes: PropTypes.object,
   /**
-   * Class name applied to the root component.
+   * @ignore
    */
   className: PropTypes.string,
   disabled: PropTypes.bool,
