c0d46025d65befb2379acd0b154c731d9bd6d4e9
Dave Hulme <dhulme@outlook.com>
2024-01-17 07:58:01 +0000

    [TreeView] Pass through `Theme` generic to variants (#11480)
    Signed-off-by: Dave Hulme <dhulme@outlook.com>
    Co-authored-by: Dave Hulme <dhu@redwoodtech.com>

--- packages/x-tree-view/src/themeAugmentation/components.d.ts
+++ packages/x-tree-view/src/themeAugmentation/components.d.ts
@@ -4,22 +4,22 @@ export interface TreeViewComponents<Theme = unknown> {
   MuiSimpleTreeView?: {
     defaultProps?: ComponentsProps['MuiSimpleTreeView'];
     styleOverrides?: ComponentsOverrides<Theme>['MuiSimpleTreeView'];
-    variants?: ComponentsVariants['MuiSimpleTreeView'];
+    variants?: ComponentsVariants<Theme>['MuiSimpleTreeView'];
   };
   MuiRichTreeView?: {
     defaultProps?: ComponentsProps['MuiRichTreeView'];
     styleOverrides?: ComponentsOverrides<Theme>['MuiRichTreeView'];
-    variants?: ComponentsVariants['MuiRichTreeView'];
+    variants?: ComponentsVariants<Theme>['MuiRichTreeView'];
   };
   MuiTreeView?: {
     defaultProps?: ComponentsProps['MuiTreeView'];
     styleOverrides?: ComponentsOverrides<Theme>['MuiTreeView'];
-    variants?: ComponentsVariants['MuiTreeView'];
+    variants?: ComponentsVariants<Theme>['MuiTreeView'];
   };
   MuiTreeItem?: {
     defaultProps?: ComponentsProps['MuiTreeItem'];
     styleOverrides?: ComponentsOverrides<Theme>['MuiTreeItem'];
-    variants?: ComponentsVariants['MuiTreeItem'];
+    variants?: ComponentsVariants<Theme>['MuiTreeItem'];
   };
 }
 
