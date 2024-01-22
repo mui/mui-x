95fd0f79b1e38d298c49c8d6e64dadd8114331db
Lukas <llukas.tyla@gmail.com>
2024-01-22 11:12:27 +0200

    proptypes

--- packages/x-date-pickers-pro/src/MobileDateRangePicker/MobileDateRangePicker.tsx
+++ packages/x-date-pickers-pro/src/MobileDateRangePicker/MobileDateRangePicker.tsx
@@ -88,6 +88,9 @@ MobileDateRangePicker.propTypes = {
    * - the `input` element if there is a field rendered.
    */
   autoFocus: PropTypes.bool,
+  /**
+   * Class name applied to the root element.
+   */
   className: PropTypes.string,
   /**
    * If `true`, the popover or modal will close after submitting the full date.
