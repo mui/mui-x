e73d30bed1f048bbe6406f4f67b5c4c178f40a6f
alexandre <alex.fauquette@gmail.com>
2024-01-31 14:26:19 +0100

    [docs-infra] Remove randomized API page layout

--- docs/pages/x/api/data-grid/grid-filter-panel.js
+++ docs/pages/x/api/data-grid/grid-filter-panel.js
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import ApiPage from 'docs/src/modules/components/ApiPage';
 import mapApiPageTranslations from 'docs/src/modules/utils/mapApiPageTranslations';
+import { dataGridLayoutKeys } from 'docsx/src/modules/utils/layoutStorageKey';
 import jsonPageContent from './grid-filter-panel.json';
 
 export default function Page(props) {
   const { descriptions, pageContent } = props;
-  return <ApiPage descriptions={descriptions} pageContent={pageContent} />;
+  return (
+    <ApiPage
+      defaultLayout="expanded"
+      layoutStorageKey={dataGridLayoutKeys}
+      descriptions={descriptions}
+      pageContent={pageContent}
+    />
+  );
 }
 
 Page.getInitialProps = () => {
---
+++ docs/src/modules/utils/layoutStorageKey.tsx
@@ -0,0 +1,5 @@
+export const dataGridLayoutKeys = {
+  slots: 'dataGrid_apiPage_slots',
+  props: 'dataGrid_apiPage_props',
+  classes: 'dataGrid_apiPage_classes',
+} as const;
