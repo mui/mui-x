57c6f9dcc43bbb5c284150fe40b6c4c60676f92b
Michał Dudak <michal@dudak.me>
2024-02-02 11:45:11 +0100

    [code-infra] Integrate changes from Core's #40842 (#11801)

--- docs/scripts/generateProptypes.ts
+++ docs/scripts/generateProptypes.ts
@@ -2,11 +2,8 @@ import * as yargs from 'yargs';
 import * as path from 'path';
 import * as fse from 'fs-extra';
 import * as prettier from 'prettier';
-import {
-  getPropTypesFromFile,
-  injectPropTypesInFile,
-} from '@mui/monorepo/packages/typescript-to-proptypes';
-import { fixBabelGeneratorIssues, fixLineEndings } from '@mui-internal/docs-utilities';
+import { getPropTypesFromFile, injectPropTypesInFile } from '@mui-internal/typescript-to-proptypes';
+import { fixBabelGeneratorIssues, fixLineEndings } from '@mui-internal/docs-utils';
 import { createXTypeScriptProjects, XTypeScriptProject } from './createXTypeScriptProjects';
 
 async function generateProptypes(project: XTypeScriptProject, sourceFile: string) {
