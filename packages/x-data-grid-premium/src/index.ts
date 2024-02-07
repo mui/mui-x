3a9d81cb7278dd7f3fde67b6f85bb7274b803686
Andrew Cherniavskyi <andrew@mui.com>
2024-02-07 13:09:13 +0100

    keep LicenseInfo as class

--- packages/x-data-grid-premium/src/index.ts
+++ packages/x-data-grid-premium/src/index.ts
@@ -5,7 +5,7 @@ import { LicenseInfo as LicenseInfoExport } from '@mui/x-license';
  * @deprecated Use `@mui/x-license` package instead:
  * @example import { LicenseInfo } from '@mui/x-license';
  */
-export const LicenseInfo = LicenseInfoExport;
+export class LicenseInfo extends LicenseInfoExport {}
 
 export * from '@mui/x-data-grid/components';
 export * from '@mui/x-data-grid-pro/components';
