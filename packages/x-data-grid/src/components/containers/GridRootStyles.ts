907646b4d8d8755eff56a61d7d2dca6bf45794bc
Rom Grk <romgrk@users.noreply.github.com>
2024-02-08 10:50:43 -0500

    [DataGrid] Vertical scrolling performance (#11924)
    Co-authored-by: Andrew Cherniavskyi <andrew@mui.com>

--- packages/x-data-grid/src/components/containers/GridRootStyles.ts
+++ packages/x-data-grid/src/components/containers/GridRootStyles.ts
@@ -572,8 +572,10 @@ export const GridRootStyles = styled('div', {
         },
       },
     },
-    [`& .${c.cell}:not(.${c['cell--pinnedLeft']}):not(.${c['cell--pinnedRight']})`]: {
-      transform: 'translate3d(var(--DataGrid-offsetLeft), 0, 0)',
+    [`& .${c.cellOffsetLeft}`]: {
+      flex: '0 0 auto',
+      display: 'inline-block',
+      width: 'var(--DataGrid-offsetLeft)',
     },
     [`& .${c.columnHeaderDraggableContainer}`]: {
       display: 'flex',
