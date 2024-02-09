e8cd79db02ec41c7ce88d56dbc07072cf423c374
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-02-09 14:03:04 +0100

    Update packages/x-date-pickers/src/internals/hooks/useField/useFieldV7TextField.ts
    Co-authored-by: Lukas <llukas.tyla@gmail.com>
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>

--- packages/x-date-pickers/src/internals/hooks/useField/useFieldV7TextField.ts
+++ packages/x-date-pickers/src/internals/hooks/useField/useFieldV7TextField.ts
@@ -445,7 +445,7 @@ export const useFieldV7TextField: UseFieldTextField<true> = (params) => {
     if (sectionListRef.current == null) {
       throw new Error(
         [
-          'MUI: The `sectionListRef` prop has not been initialized by `PickersSectionList`',
+          'MUI X: The `sectionListRef` prop has not been initialized by `PickersSectionList`',
           'You probably tried to pass a component to the `textField` slot that contains an `<input />` element instead of a `PickersSectionList`.',
           '',
           'If you want to keep using an `<input />` HTML element for the editing, please remove the `enableAccessibleFieldDOMStructure` prop from your picker or field component:',
