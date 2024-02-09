a9e35553adc8bcc94d1c5080900722f5d9f1c69f
Bilal Shafi <bilalshafidev@gmail.com>
2024-02-09 18:01:13 +0500

    Refactor

--- packages/x-data-grid/src/components/GridPagination.tsx
+++ packages/x-data-grid/src/components/GridPagination.tsx
@@ -40,7 +40,7 @@ export const GridPagination = React.forwardRef<unknown, Partial<TablePaginationP
 
     const lastPage = React.useMemo(() => {
       const calculatedValue = Math.ceil(rowCount / (paginationModel.pageSize || 1)) - 1;
-      return calculatedValue < 0 ? 0 : calculatedValue;
+      return Math.max(0, calculatedValue);
     }, [rowCount, paginationModel.pageSize]);
 
     const handlePageSizeChange = React.useCallback(
