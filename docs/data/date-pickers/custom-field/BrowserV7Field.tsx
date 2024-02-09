98578b5465fddbdcc8857e7167d69ad417c4faea
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-02-09 14:03:28 +0100

    Update docs/data/date-pickers/custom-field/BrowserV7Field.tsx
    Co-authored-by: Lukas <llukas.tyla@gmail.com>
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>

--- docs/data/date-pickers/custom-field/BrowserV7Field.tsx
+++ docs/data/date-pickers/custom-field/BrowserV7Field.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Dayjs } from 'dayjs';
-import { unstable_useForkRef as useForkRef } from '@mui/utils';
+import useForkRef from '@mui/utils/useForkRef';
 import styled from '@mui/system/styled';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
