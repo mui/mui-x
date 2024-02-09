bb43dd78a045aef02dc18db6bd50ca002b55badb
delangle <flaviendelangle@gmail.com>
2024-02-09 15:08:33 +0100

    Fix

--- docs/data/date-pickers/custom-field/BrowserV7MultiInputRangeField.js
+++ docs/data/date-pickers/custom-field/BrowserV7MultiInputRangeField.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { unstable_useForkRef as useForkRef } from '@mui/utils';
 import { useSlotProps } from '@mui/base/utils';
-import styled from '@mui/material/styled';
+import styled from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
