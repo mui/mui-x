fa266b48fe071f470e98ea28012b781d4732408d
delangle <flaviendelangle@gmail.com>
2024-02-09 15:02:36 +0100

    Code review: Lukas

--- packages/x-date-pickers-pro/src/DateRangePicker/DateRangePicker.test.tsx
+++ packages/x-date-pickers-pro/src/DateRangePicker/DateRangePicker.test.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 import { fireEvent, screen } from '@mui-internal/test-utils/createRenderer';
 import { expect } from 'chai';
-import { createPickerRenderer, getFieldInputRoot, stubMatchMedia } from 'test/utils/pickers';
+import {
+  buildFieldInteractions,
+  createPickerRenderer,
+  getFieldInputRoot,
+  stubMatchMedia,
+} from 'test/utils/pickers';
 
 describe('<DateRangePicker />', () => {
   const { render, clock } = createPickerRenderer({
@@ -10,9 +15,26 @@ describe('<DateRangePicker />', () => {
     clockConfig: new Date(2018, 0, 1, 0, 0, 0, 0),
   });
 
+  const { renderWithProps } = buildFieldInteractions({
+    render,
+    clock,
+    Component: DateRangePicker,
+  });
+
   it('should not open mobile picker dialog when clicked on input', () => {
-    render(<DateRangePicker enableAccessibleFieldDOMStructure />);
+    // Test with v7 input
+    const v7Response = renderWithProps({ enableAccessibleFieldDOMStructure: true });
     fireEvent.click(getFieldInputRoot());
+    clock.runToLast();
+
+    expect(screen.queryByRole('tooltip')).not.to.equal(null);
+    expect(screen.queryByRole('dialog')).to.equal(null);
+
+    v7Response.unmount();
+
+    // Test with v6 input
+    renderWithProps({ enableAccessibleFieldDOMStructure: false });
+    fireEvent.click(screen.getAllByRole('textbox')[0]);
     clock.runToLast();
 
     expect(screen.queryByRole('tooltip')).not.to.equal(null);
