01ea1dac35a7cab3e605e5f19d5dab30933f21e7
bmairlot <141404435+bmairlot@users.noreply.github.com>
2024-02-11 15:12:47 +0100

    Fix for a small import mistake

--- docs/data/charts/overview/ChartsOverviewDemo.tsx
+++ docs/data/charts/overview/ChartsOverviewDemo.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BarChart } from '@mui/x-charts/BarChart';
+import { BarChart } from '@mui/x-charts';
 
 export default function ChartsOverviewDemo() {
   return (
