3dcebaeeb046beddefe08f2eca5b0c52a47a5ffb
alexandre <alex.fauquette@gmail.com>
2024-02-12 14:25:32 +0100

    Remove useless stack

--- docs/data/charts/gauge/ArcDesign.js
+++ docs/data/charts/gauge/ArcDesign.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
 import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
 
 const settings = {
@@ -7,24 +6,23 @@ const settings = {
   height: 200,
   value: 60,
 };
+
 export default function ArcDesign() {
   return (
-    <Stack direction={{ xs: 'column', md: 'row' }} spacing={{ xs: 1, md: 3 }}>
-      <Gauge
-        {...settings}
-        cornerRadius="50%"
-        sx={(theme) => ({
-          [`& .${gaugeClasses.valueText}`]: {
-            fontSize: 40,
-          },
-          [`& .${gaugeClasses.valueArc}`]: {
-            fill: '#52b202',
-          },
-          [`& .${gaugeClasses.referenceArc}`]: {
-            fill: theme.palette.text.disabled,
-          },
-        })}
-      />
-    </Stack>
+    <Gauge
+      {...settings}
+      cornerRadius="50%"
+      sx={(theme) => ({
+        [`& .${gaugeClasses.valueText}`]: {
+          fontSize: 40,
+        },
+        [`& .${gaugeClasses.valueArc}`]: {
+          fill: '#52b202',
+        },
+        [`& .${gaugeClasses.referenceArc}`]: {
+          fill: theme.palette.text.disabled,
+        },
+      })}
+    />
   );
 }
