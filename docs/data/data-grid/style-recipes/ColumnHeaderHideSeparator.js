38da7077d5be235a881479752abacfc35479f247
michel <jsnerdic@gmail.com>
2024-02-19 15:05:36 +0100

    added a new recipe for hiding the column header separators when not resizable

---
+++ docs/data/data-grid/style-recipes/ColumnHeaderHideSeparator.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { DataGridPro, gridClasses } from '@mui/x-data-grid-pro';
+
+const rows = [
+  {
+    id: 1,
+    username: '@MUI',
+    age: 20,
+  },
+];
+
+const columns = [
+  {
+    field: 'id',
+  },
+  {
+    field: 'username',
+    width: 125,
+    minWidth: 150,
+    maxWidth: 200,
+    resizable: false,
+  },
+  { field: 'age', resizable: false },
+];
+
+export default function ColumnHeaderHideSeparator() {
+  return (
+    <div style={{ height: 250, width: '100%' }}>
+      <DataGridPro
+        columns={columns}
+        rows={rows}
+        sx={{
+          [`& .${gridClasses.columnSeparator}`]: {
+            [`&:not(.${gridClasses['columnSeparator--resizable']})`]: {
+              display: 'none',
+            },
+          },
+        }}
+      />
+    </div>
+  );
+}
