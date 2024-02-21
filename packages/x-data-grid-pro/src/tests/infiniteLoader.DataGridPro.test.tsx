9878e009ef3bae3ca15276787f5af0e068ef3899
Andrew Cherniavskyi <andrew@mui.com>
2024-02-21 21:35:39 +0100

    expand test case

--- packages/x-data-grid-pro/src/tests/infiniteLoader.DataGridPro.test.tsx
+++ packages/x-data-grid-pro/src/tests/infiniteLoader.DataGridPro.test.tsx
@@ -33,13 +33,28 @@ describe('<DataGridPro /> - Infnite loader', () => {
         </div>
       );
     }
-    const { container } = render(<TestCase rows={baseRows} />);
+    const { container, setProps } = render(<TestCase rows={baseRows} />);
     const virtualScroller = container.querySelector('.MuiDataGrid-virtualScroller')!;
     // arbitrary number to make sure that the bottom of the grid window is reached.
     virtualScroller.scrollTop = 12345;
     virtualScroller.dispatchEvent(new Event('scroll'));
     await waitFor(() => {
       expect(handleRowsScrollEnd.callCount).to.equal(1);
+    });
+    setProps({
+      rows: baseRows.concat(
+        { id: 6, brand: 'Gucci' },
+        { id: 7, brand: "Levi's" },
+        { id: 8, brand: 'Ray-Ban' },
+      ),
+    });
+    // Trigger a scroll again to notify the grid that we're not in the bottom area anymore
+    virtualScroller.dispatchEvent(new Event('scroll'));
+    expect(handleRowsScrollEnd.callCount).to.equal(1);
+    virtualScroller.scrollTop = 12345;
+    virtualScroller.dispatchEvent(new Event('scroll'));
+    await waitFor(() => {
+      expect(handleRowsScrollEnd.callCount).to.equal(2);
     });
   });
 });
