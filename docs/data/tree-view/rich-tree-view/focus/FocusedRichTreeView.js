67b84360a6890a76fa9584c299e9a8af5fd719c1
Nora <72460825+noraleonte@users.noreply.github.com>
2024-02-28 14:26:52 +0200

    [TreeView] Add public API and expose focus method (#12143)
    Signed-off-by: Nora <72460825+noraleonte@users.noreply.github.com>
    Co-authored-by: Flavien DELANGLE <flaviendelangle@gmail.com>

---
+++ docs/data/tree-view/rich-tree-view/focus/FocusedRichTreeView.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
+
+import { useTreeViewApiRef } from '@mui/x-tree-view/hooks/useTreeViewApiRef';
+
+const MUI_X_PRODUCTS = [
+  {
+    id: 'grid',
+    label: 'Data Grid',
+    children: [
+      { id: 'grid-community', label: '@mui/x-data-grid' },
+      { id: 'grid-pro', label: '@mui/x-data-grid-pro' },
+      { id: 'grid-premium', label: '@mui/x-data-grid-premium' },
+    ],
+  },
+  {
+    id: 'pickers',
+    label: 'Date and Time Pickers',
+    children: [
+      { id: 'pickers-community', label: '@mui/x-date-pickers' },
+      { id: 'pickers-pro', label: '@mui/x-date-pickers-pro' },
+    ],
+  },
+  {
+    id: 'charts',
+    label: 'Charts',
+    children: [{ id: 'charts-community', label: '@mui/x-charts' }],
+  },
+  {
+    id: 'tree-view',
+    label: 'Tree View',
+    children: [{ id: 'tree-view-community', label: '@mui/x-tree-view' }],
+  },
+];
+
+export default function FocusedRichTreeView() {
+  const apiRef = useTreeViewApiRef();
+  const handleButtonClick = (event) => {
+    apiRef.current?.focusNode(event, 'pickers');
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1, maxWidth: 400 }}>
+      <Box sx={{ mb: 1 }}>
+        <Button onClick={handleButtonClick}>Focus pickers node</Button>
+      </Box>
+      <Box sx={{ height: 264, flexGrow: 1 }}>
+        <RichTreeView items={MUI_X_PRODUCTS} apiRef={apiRef} />
+      </Box>
+    </Box>
+  );
+}
---
+++ packages/x-tree-view/src/internals/utils/utils.ts
@@ -0,0 +1,13 @@
+export const getActiveElement = (root: Document | ShadowRoot = document): Element | null => {
+  const activeEl = root.activeElement;
+
+  if (!activeEl) {
+    return null;
+  }
+
+  if (activeEl.shadowRoot) {
+    return getActiveElement(activeEl.shadowRoot);
+  }
+
+  return activeEl;
+};
