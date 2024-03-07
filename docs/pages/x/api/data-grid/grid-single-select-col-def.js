1f3d51751af8ca7bf006eeab0655e632fcd83f3b
alexandre <alex.fauquette@gmail.com>
2024-03-07 12:14:00 +0100

    update markdown import

--- docs/pages/x/api/data-grid/grid-single-select-col-def.js
+++ docs/pages/x/api/data-grid/grid-single-select-col-def.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import MarkdownDocs from '@mui/monorepo/docs/src/modules/components/MarkdownDocs';
-import * as pageProps from './grid-single-select-col-def.md?@mui/markdown';
+import * as pageProps from './grid-single-select-col-def.md?muiMarkdown';
 
 export default function Page() {
   return <MarkdownDocs {...pageProps} />;
