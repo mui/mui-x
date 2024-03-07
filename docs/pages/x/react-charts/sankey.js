544026210e1287b318876c782c1dfb47b49eab56
alexandre <alex.fauquette@gmail.com>
2024-03-07 14:14:27 +0100

    fix

--- docs/pages/x/react-charts/sankey.js
+++ docs/pages/x/react-charts/sankey.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import * as pageProps from 'docsx/data/charts/sankey/sankey.md?@muiMarkdown';
+import * as pageProps from 'docsx/data/charts/sankey/sankey.md?muiMarkdown';
 
 export default function Page() {
   return <MarkdownDocs {...pageProps} disableAd />;
