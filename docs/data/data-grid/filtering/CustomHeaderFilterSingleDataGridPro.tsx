5c15ef01e38552a7cdb139faf66ed1a2bccf5245
Bilal Shafi <bilalshafidev@gmail.com>
2024-03-09 10:58:54 +0500

    Update the demo

--- docs/data/data-grid/filtering/CustomHeaderFilterSingleDataGridPro.tsx
+++ docs/data/data-grid/filtering/CustomHeaderFilterSingleDataGridPro.tsx
@@ -5,7 +5,7 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import {
   DataGridPro,
-  RenderHeaderFilterProps,
+  GridRenderHeaderFilterProps,
   gridFilterModelSelector,
   useGridSelector,
   useGridApiContext,
@@ -14,7 +14,7 @@ import { useDemoData } from '@mui/x-data-grid-generator';
 
 const getDefaultFilter = (field: string) => ({ field, operator: 'is' });
 
-function AdminFilter(props: RenderHeaderFilterProps) {
+function AdminFilter(props: GridRenderHeaderFilterProps) {
   const { colDef } = props;
   const apiRef = useGridApiContext();
   const filterModel = useGridSelector(apiRef, gridFilterModelSelector);
@@ -76,7 +76,7 @@ export default function CustomHeaderFilterSingleDataGridPro() {
           return {
             ...colDef,
             width: 200,
-            renderHeaderFilter: (params: RenderHeaderFilterProps) => (
+            renderHeaderFilter: (params: GridRenderHeaderFilterProps) => (
               <AdminFilter {...params} />
             ),
           };
