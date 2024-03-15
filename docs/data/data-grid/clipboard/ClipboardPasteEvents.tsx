563f41000dbed98031d9da4f3887dcd938b3b2bd
Andrew Cherniavskyi <andrew@mui.com>
2024-03-15 22:28:04 +0100

    use Dialog component instead of window.confirm

--- docs/data/data-grid/clipboard/ClipboardPasteEvents.tsx
+++ docs/data/data-grid/clipboard/ClipboardPasteEvents.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { DataGridPremium, DataGridPremiumProps } from '@mui/x-data-grid-premium';
 import { useDemoData } from '@mui/x-data-grid-generator';
-
-const confirmPaste = async () => {
-  const confirmed = window.confirm('Are you sure you want to paste?');
-  if (!confirmed) {
-    throw new Error('The paste was cancelled');
-  }
-};
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 
 export default function ClipboardPasteEvents() {
   const { data } = useDemoData({
@@ -37,6 +36,19 @@ export default function ClipboardPasteEvents() {
     },
   };
 
+  const confirm = useConfirm();
+  const confirmPaste = React.useCallback<() => Promise<void>>(() => {
+    return new Promise((resolve, reject) => {
+      confirm.open((confirmed) => {
+        if (confirmed) {
+          resolve();
+        } else {
+          reject();
+        }
+      });
+    });
+  }, [confirm]);
+
   return (
     <div style={{ width: '100%', height: 400 }}>
       <DataGridPremium
@@ -51,6 +63,57 @@ export default function ClipboardPasteEvents() {
         ignoreValueFormatterDuringExport
         disableRowSelectionOnClick
       />
+
+      <Dialog
+        open={confirm.isOpen}
+        onClose={confirm.cancel}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {'Are you sure you want to paste?'}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This will overwrite the selected cells.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={confirm.cancel}>Cancel</Button>
+          <Button onClick={confirm.confirm} autoFocus>
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
+
+const useConfirm = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const callbackRef = React.useRef<((confirmed: boolean) => void) | null>(null);
+
+  const open = React.useCallback((callback: (confirmed: boolean) => void) => {
+    setIsOpen(true);
+    callbackRef.current = callback;
+  }, []);
+
+  const cancel = React.useCallback(() => {
+    setIsOpen(false);
+    callbackRef.current?.(false);
+    callbackRef.current = null;
+  }, []);
+
+  const confirm = React.useCallback(() => {
+    setIsOpen(false);
+    callbackRef.current?.(true);
+    callbackRef.current = null;
+  }, []);
+
+  return {
+    open,
+    isOpen,
+    cancel,
+    confirm,
+  };
+};
