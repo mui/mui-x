719dc04c55b7397ce6851061eba32e5ba87f4c15
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-03-20 08:44:27 +0100

    [fields] Allow to override the separator between the start and the end date in all range fields (#12174)
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>
    Co-authored-by: Lukas <llukas.tyla@gmail.com>

--- packages/x-date-pickers-pro/src/MultiInputDateRangeField/MultiInputDateRangeField.tsx
+++ packages/x-date-pickers-pro/src/MultiInputDateRangeField/MultiInputDateRangeField.tsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import Stack, { StackProps } from '@mui/material/Stack';
 import MuiTextField from '@mui/material/TextField';
-import Typography, { TypographyProps } from '@mui/material/Typography';
+import Typography from '@mui/material/Typography';
 import { styled, useThemeProps } from '@mui/material/styles';
 import { useSlotProps } from '@mui/base/utils';
 import {
@@ -54,14 +54,11 @@ const MultiInputDateRangeFieldRoot = styled(
   },
 )({});
 
-const MultiInputDateRangeFieldSeparator = styled(
-  (props: TypographyProps) => <Typography {...props}>{props.children ?? ' – '}</Typography>,
-  {
-    name: 'MuiMultiInputDateRangeField',
-    slot: 'Separator',
-    overridesResolver: (props, styles) => styles.separator,
-  },
-)({
+const MultiInputDateRangeFieldSeparator = styled(Typography, {
+  name: 'MuiMultiInputDateRangeField',
+  slot: 'Separator',
+  overridesResolver: (props, styles) => styles.separator,
+})({
   lineHeight: '1.4375em', // 23px
 });
 
@@ -156,6 +153,9 @@ const MultiInputDateRangeField = React.forwardRef(function MultiInputDateRangeFi
   const separatorProps = useSlotProps({
     elementType: Separator,
     externalSlotProps: slotProps?.separator,
+    additionalProps: {
+      children: ` ${internalProps.dateSeparator ?? '–'} `,
+    },
     ownerState,
     className: classes.separator,
   });
@@ -199,6 +199,11 @@ MultiInputDateRangeField.propTypes = {
   classes: PropTypes.object,
   className: PropTypes.string,
   component: PropTypes.elementType,
+  /**
+   * String displayed between the start and the end dates.
+   * @default "–"
+   */
+  dateSeparator: PropTypes.string,
   /**
    * The default value. Use when the component is not controlled.
    */
