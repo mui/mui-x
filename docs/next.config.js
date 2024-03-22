8f90a299729ffd0735df33df88f6e3d025d15f27
alexandre <alex.fauquette@gmail.com>
2024-03-22 11:31:22 +0100

    [docs] fix links

--- docs/next.config.js
+++ docs/next.config.js
@@ -25,7 +25,7 @@ module.exports = withDocsInfra({
     FEEDBACK_URL: process.env.FEEDBACK_URL,
     CONTEXT: process.env.CONTEXT,
     // #default-branch-switch
-    SOURCE_GITHUB_BRANCH: 'master',
+    SOURCE_GITHUB_BRANCH: 'v6.x',
     SOURCE_CODE_REPO: 'https://github.com/mui/mui-x',
     GITHUB_TEMPLATE_DOCS_FEEDBACK: '6.docs-feedback.yml',
   },
