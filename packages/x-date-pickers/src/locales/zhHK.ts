81665361e6efe5a629914868d83bd44071269301
samchiu90 <51096971+samchiu90@users.noreply.github.com>
2024-03-26 14:21:57 +0800

    [l10n] Improve Chinese (Hong Kong) (zh-HK) locale (#12547)
    Co-authored-by: Sam Chiu <341975@haeco.com>
    Co-authored-by: Lukas <llukas.tyla@gmail.com>

--- packages/x-date-pickers/src/locales/zhHK.ts
+++ packages/x-date-pickers/src/locales/zhHK.ts
@@ -23,10 +23,10 @@ const zhHKPickers: Partial<PickersLocaleText<any>> = {
   // DateRange labels
   start: '開始',
   end: '結束',
-  // startDate: 'Start date',
-  // startTime: 'Start time',
-  // endDate: 'End date',
-  // endTime: 'End time',
+  startDate: '開始日期',
+  startTime: '開始時間',
+  endDate: '結束日期',
+  endTime: '結束時間',
 
   // Action bar
   cancelButtonLabel: '取消',
@@ -82,17 +82,17 @@ const zhHKPickers: Partial<PickersLocaleText<any>> = {
   fieldMeridiemPlaceholder: () => 'aa',
 
   // View names
-  // year: 'Year',
-  // month: 'Month',
-  // day: 'Day',
-  // weekDay: 'Week day',
-  // hours: 'Hours',
-  // minutes: 'Minutes',
-  // seconds: 'Seconds',
-  // meridiem: 'Meridiem',
+  year: '年',
+  month: '月',
+  day: '日',
+  weekDay: '星期',
+  hours: '小時',
+  minutes: '分鐘',
+  seconds: '秒',
+  meridiem: '子午線',
 
   // Common
-  // empty: 'Empty',
+  empty: '空值',
 };
 
 export const zhHK = getPickersLocalization(zhHKPickers);
