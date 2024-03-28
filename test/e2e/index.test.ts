c047aeb2663b4ef9d341dd91776bc438b731f27f
Lukas <llukas.tyla@gmail.com>
2024-03-28 10:00:14 +0200

    Fix test

--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -563,7 +563,7 @@ async function initializeEnvironment(
           await page.locator(`.${pickersSectionListClasses.root}`).click();
           await input.fill('02/12/2020');
 
-          expect(await page.getByRole('button').getAttribute('aria-label')).to.equal(
+          expect(await page.getByRole('button', { name: /Choose date/ }).getAttribute('aria-label')).to.equal(
             'Choose date, selected date is Feb 12, 2020',
           );
         });
