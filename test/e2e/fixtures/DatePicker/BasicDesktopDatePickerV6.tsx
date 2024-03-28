dcb658239c22e58cf06f05defae2eee944379c1f
Lukas <llukas.tyla@gmail.com>
2024-03-28 11:09:48 +0200

    [fields] Fix input refocusing after clearing a value (#12587)

--- test/e2e/fixtures/DatePicker/BasicDesktopDatePickerV6.tsx
+++ test/e2e/fixtures/DatePicker/BasicDesktopDatePickerV6.tsx
@@ -3,11 +3,10 @@ import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
-export default function BasicDesktopDatePicker() {
+export default function BasicDesktopDatePickerV6() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DesktopDatePicker
-        enableAccessibleFieldDOMStructure
         label="Desktop Date Picker"
         className="test-date-picker"
         slotProps={{ field: { clearable: true } }}
--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -563,9 +563,9 @@ async function initializeEnvironment(
           await page.locator(`.${pickersSectionListClasses.root}`).click();
           await input.fill('02/12/2020');
 
-          expect(await page.getByRole('button').getAttribute('aria-label')).to.equal(
-            'Choose date, selected date is Feb 12, 2020',
-          );
+          expect(
+            await page.getByRole('button', { name: /Choose date/ }).getAttribute('aria-label'),
+          ).to.equal('Choose date, selected date is Feb 12, 2020');
         });
 
         it('should allow pasting a section', async () => {
@@ -611,6 +611,38 @@ async function initializeEnvironment(
           // check that the search query has been cleared after pasting
           await monthSection.press('2');
           expect(await input.inputValue()).to.equal('02/11/2022');
+        });
+
+        it('should focus the first field section after clearing a value', async () => {
+          await renderFixture('DatePicker/BasicDesktopDatePicker');
+
+          const monthSection = page.getByRole('spinbutton', { name: 'Month' });
+          await monthSection.press('2');
+          await page.getByRole('button', { name: 'Clear value' }).click();
+
+          expect(await page.evaluate(() => document.activeElement?.textContent)).to.equal('MM');
+        });
+
+        it('should focus the first field section after clearing a value in v6 input', async () => {
+          await renderFixture('DatePicker/BasicDesktopDatePickerV6');
+
+          await page.getByRole('textbox').fill('2');
+          await page.getByRole('button', { name: 'Clear value' }).click();
+
+          // firefox does not support document.getSelection().toString() on input elements
+          if (browserType.name() === 'firefox') {
+            expect(
+              await page.evaluate(() => {
+                return (
+                  document.activeElement?.tagName === 'INPUT' &&
+                  // only focused input has value set
+                  (document.activeElement as HTMLInputElement)?.value === 'MM/DD/YYYY'
+                );
+              }),
+            ).to.equal(true);
+          } else {
+            expect(await page.evaluate(() => document.getSelection()?.toString())).to.equal('MM');
+          }
         });
       });
 
