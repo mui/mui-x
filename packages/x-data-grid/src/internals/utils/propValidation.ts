bb24b9e4bde6f1ad3ba3f6e1ca7dd792ee19e346
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2024-04-04 09:27:05 +0200

    [DataGrid] Log error if `rowCount` is used with client-side pagination (#12448)
    Co-authored-by: Bilal Shafi <bilalshafidev@gmail.com>
    Co-authored-by: Andrew Cherniavskyi <andrew@mui.com>

--- packages/x-data-grid/src/internals/utils/propValidation.ts
+++ packages/x-data-grid/src/internals/utils/propValidation.ts
@@ -1,4 +1,6 @@
+import { isNumber } from '../../utils/utils';
 import { DataGridProcessedProps } from '../../models/props/DataGridProps';
+import { GridSignature } from '../../hooks/utils/useGridApiEventHandler';
 
 export type PropValidator<TProps> = (props: TProps) => string | undefined;
 
@@ -13,13 +15,19 @@ export const propValidatorsDataGrid: PropValidator<DataGridProcessedProps>[] = [
         'Please remove one of these two props.',
       ].join('\n')) ||
     undefined,
+  (props) =>
+    (props.signature === GridSignature.DataGrid &&
+      props.paginationMode === 'client' &&
+      isNumber(props.rowCount) &&
+      'MUI X: Usage of the `rowCount` prop with client side pagination (`paginationMode="client"`) has no effect. `rowCount` is only meant to be used with `paginationMode="server"`.') ||
+    undefined,
 ];
 
-const warnedOnceMap = new Set();
+const warnedOnceCache = new Set();
 const warnOnce = (message: string) => {
-  if (!warnedOnceMap.has(message)) {
+  if (!warnedOnceCache.has(message)) {
     console.error(message);
-    warnedOnceMap.add(message);
+    warnedOnceCache.add(message);
   }
 };
 
@@ -33,4 +41,8 @@ export const validateProps = <TProps>(props: TProps, validators: PropValidator<T
       warnOnce(warning);
     }
   });
+};
+
+export const clearWarningsCache = () => {
+  warnedOnceCache.clear();
 };
