08aa1cd726101169edbef0d533053cb73d6f9c86
Jose C Quintas Jr <juniorquintas@gmail.com>
2024-04-04 16:53:40 +0200

    [charts] Fix typescript error when using `sx` property on ChartsTooltip (#12659)

--- packages/x-charts/src/ChartsTooltip/ChartsTooltip.tsx
+++ packages/x-charts/src/ChartsTooltip/ChartsTooltip.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import composeClasses from '@mui/utils/composeClasses';
-import { styled, useThemeProps } from '@mui/material/styles';
-import { Popper, PopperProps } from '@mui/base/Popper';
+import { styled, useThemeProps, SxProps, Theme } from '@mui/material/styles';
+import { Popper, PopperProps as BasePopperProps } from '@mui/base/Popper';
 import { NoSsr } from '@mui/base/NoSsr';
 import { useSlotProps } from '@mui/base/utils';
 import {
@@ -20,6 +20,13 @@ import { ChartSeriesType } from '../models/seriesType/config';
 import { ChartsItemContentProps, ChartsItemTooltipContent } from './ChartsItemTooltipContent';
 import { ChartsAxisContentProps, ChartsAxisTooltipContent } from './ChartsAxisTooltipContent';
 import { ChartsTooltipClasses, getChartsTooltipUtilityClass } from './chartsTooltipClasses';
+
+export type PopperProps = BasePopperProps & {
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
+  sx?: SxProps<Theme>;
+};
 
 export interface ChartsTooltipSlots {
   /**
