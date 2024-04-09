a971535a9605ecdfb76e5c2768a74673424e63cf
Lukas <llukas.tyla@gmail.com>
2024-04-09 14:42:19 +0300

    Fix to successfully build `x-charts` dynamically imported types

--- packages/x-charts/typing.d.ts
+++ packages/x-charts/typing.d.ts
@@ -1,2 +1,4 @@
 // After pnpm migration the dynamically imported types have to be defined explicitly
 /// <reference types="@emotion/styled" />
+/// <reference types="@react-spring/shared" />
+/// <reference types="csstype" />
