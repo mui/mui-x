4db77b1b65bae492737d8a85be12892068a41772
Andrew Cherniavskii <andrew@mui.com>
2024-04-10 17:18:15 +0200

    [DataGrid] Do not escape double quotes when copying to clipboard (#12734)

--- packages/grid/x-data-grid-premium/src/hooks/features/cellSelection/useGridCellSelection.ts
+++ packages/grid/x-data-grid-premium/src/hooks/features/cellSelection/useGridCellSelection.ts
@@ -565,7 +565,7 @@ export const useGridCellSelection = (
             cellData = serializeCellValue(cellParams, {
               delimiterCharacter: clipboardCopyCellDelimiter,
               ignoreValueFormatter,
-              shouldAppendQuotes: true,
+              shouldAppendQuotes: false,
             });
           } else {
             cellData = '';
