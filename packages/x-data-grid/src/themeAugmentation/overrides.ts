e0b54a510407b999165faebe0b9e900039d1ad3e
Rom Grk <romgrk@users.noreply.github.com>
2024-04-10 20:19:43 -0400

    [DataGrid] Make colors customizable (#12614)
    Signed-off-by: Rom Grk <romgrk@users.noreply.github.com>
    Co-authored-by: Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>

--- packages/x-data-grid/src/themeAugmentation/overrides.ts
+++ packages/x-data-grid/src/themeAugmentation/overrides.ts
@@ -6,4 +6,11 @@ export interface DataGridComponentNameToClassKey {
 
 declare module '@mui/material/styles' {
   interface ComponentNameToClassKey extends DataGridComponentNameToClassKey {}
+
+  interface Mixins {
+    MuiDataGrid?: {
+      containerBackground?: string;
+      pinnedBackground?: string;
+    };
+  }
 }
