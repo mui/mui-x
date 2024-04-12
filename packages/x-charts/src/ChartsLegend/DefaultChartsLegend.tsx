0e8f42c5add651041f2e812248a18c8deed179cd
Jose Quintas <juniorquintas@gmail.com>
2024-04-12 13:49:26 +0200

    Refactor calculating label positions to its own function

--- packages/x-charts/src/ChartsLegend/DefaultChartsLegend.tsx
+++ packages/x-charts/src/ChartsLegend/DefaultChartsLegend.tsx
@@ -12,6 +12,7 @@ import { ChartsText, ChartsTextStyle } from '../ChartsText';
 import { CardinalDirections } from '../models/layout';
 import { getWordsByLines } from '../internals/getWordsByLines';
 import type { ChartsLegendProps } from './ChartsLegend';
+import { calculateLabelPositions } from './calculateLabelPositions';
 
 export type ChartsLegendRootOwnerState = {
   position: AnchorPosition;
@@ -150,95 +151,27 @@ function DefaultChartsLegend(props: LegendRendererProps) {
   const availableWidth = totalWidth - padding.left - padding.right;
   const availableHeight = totalHeight - padding.top - padding.bottom;
 
-  const [seriesWithPosition, legendWidth, legendHeight] = React.useMemo(() => {
-    // Start at 0, 0. Will be modified later by padding and position.
-    let x = 0;
-    let y = 0;
-
-    // total values used to align legend later.
-    let totalWidthUsed = 0;
-    let totalHeightUsed = 0;
-    let rowIndex = 0;
-    const rowMaxHeight = [0];
-
-    const seriesWithRawPosition = seriesToDisplay.map(({ label, ...other }) => {
-      const itemSpace = getItemSpace(label, labelStyle);
-      const rep = {
-        ...other,
-        label,
-        positionX: x,
-        positionY: y,
-        innerHeight: itemSpace.innerHeight,
-        innerWidth: itemSpace.innerWidth,
-        outerHeight: itemSpace.outerHeight,
-        outerWidth: itemSpace.outerWidth,
-        rowIndex,
-      };
-
-      if (direction === 'row') {
-        if (x + itemSpace.innerWidth > availableWidth) {
-          // This legend item would create overflow along the x-axis, so we start a new row.
-          x = 0;
-          y += rowMaxHeight[rowIndex];
-          rowIndex += 1;
-          if (rowMaxHeight.length <= rowIndex) {
-            rowMaxHeight.push(0);
-          }
-          rep.positionX = x;
-          rep.positionY = y;
-          rep.rowIndex = rowIndex;
-        }
-        totalWidthUsed = Math.max(totalWidthUsed, x + itemSpace.outerWidth);
-        totalHeightUsed = Math.max(totalHeightUsed, y + itemSpace.outerHeight);
-        rowMaxHeight[rowIndex] = Math.max(rowMaxHeight[rowIndex], itemSpace.outerHeight);
-
-        x += itemSpace.outerWidth;
-      }
-
-      if (direction === 'column') {
-        if (y + itemSpace.innerHeight > availableHeight) {
-          // This legend item would create overflow along the y-axis, so we start a new column.
-          x = totalWidthUsed + itemGap;
-          y = 0;
-          rowIndex = 0;
-          rep.positionX = x;
-          rep.positionY = y;
-          rep.rowIndex = rowIndex;
-        }
-        if (rowMaxHeight.length <= rowIndex) {
-          rowMaxHeight.push(0);
-        }
-        totalWidthUsed = Math.max(totalWidthUsed, x + itemSpace.outerWidth);
-        totalHeightUsed = Math.max(totalHeightUsed, y + itemSpace.outerHeight);
-
-        rowIndex += 1;
-        y += itemSpace.outerHeight;
-      }
-
-      return rep;
-    });
-
-    return [
-      seriesWithRawPosition.map((item) => ({
-        ...item,
-        positionY:
-          item.positionY +
-          (direction === 'row'
-            ? rowMaxHeight[item.rowIndex] / 2 // Get the center of the entire row
-            : item.outerHeight / 2), // Get the center of the item
-      })),
-      totalWidthUsed,
-      totalHeightUsed,
-    ];
-  }, [
-    seriesToDisplay,
-    getItemSpace,
-    labelStyle,
-    direction,
-    availableWidth,
-    availableHeight,
-    itemGap,
-  ]);
+  const [seriesWithPosition, legendWidth, legendHeight] = React.useMemo(
+    () =>
+      calculateLabelPositions(
+        seriesToDisplay,
+        getItemSpace,
+        availableWidth,
+        availableHeight,
+        direction,
+        itemGap,
+        labelStyle,
+      ),
+    [
+      seriesToDisplay,
+      getItemSpace,
+      availableWidth,
+      availableHeight,
+      direction,
+      itemGap,
+      labelStyle,
+    ],
+  );
 
   const gapX = React.useMemo(() => {
     switch (position.horizontal) {
