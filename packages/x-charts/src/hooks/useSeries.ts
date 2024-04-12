6ddb4f118a23454f8108425cc6633e10345b1ba6
alexandre <alex.fauquette@gmail.com>
2024-04-12 16:16:10 +0200

    add series specific hooks

--- packages/x-charts/src/hooks/useSeries.ts
+++ packages/x-charts/src/hooks/useSeries.ts
@@ -21,3 +21,55 @@ export function useSeries() {
 
   return series;
 }
+
+/**
+ * Get access to the internal state of pie series.
+ * The returned object contains:
+ * - series: a mapping from ids to series attributes.
+ * - seriesOrder: the array of series ids.
+ * @returns { series: Record<SeriesId, DefaultizedPieSeriesType>; seriesOrder: SeriesId[]; } | undefined pieSeries
+ */
+export function usePieSeries() {
+  const series = useSeries();
+
+  return React.useMemo(() => series.pie, [series.pie]);
+}
+
+/**
+ * Get access to the internal state of line series.
+ * The returned object contains:
+ * - series: a mapping from ids to series attributes.
+ * - seriesOrder: the array of series ids.
+ * @returns { series: Record<SeriesId, DefaultizedLineSeriesType>; seriesOrder: SeriesId[]; } | undefined lineSeries
+ */
+export function useLineSeries() {
+  const series = useSeries();
+
+  return React.useMemo(() => series.line, [series.line]);
+}
+
+/**
+ * Get access to the internal state of bar series.
+ * The returned object contains:
+ * - series: a mapping from ids to series attributes.
+ * - seriesOrder: the array of series ids.
+ * @returns { series: Record<SeriesId, DefaultizedBarSeriesType>; seriesOrder: SeriesId[]; } | undefined barSeries
+ */
+export function useBarSeries() {
+  const series = useSeries();
+
+  return React.useMemo(() => series.bar, [series.bar]);
+}
+
+/**
+ * Get access to the internal state of scatter series.
+ * The returned object contains:
+ * - series: a mapping from ids to series attributes.
+ * - seriesOrder: the array of series ids.
+ * @returns { series: Record<SeriesId, DefaultizedScatterSeriesType>; seriesOrder: SeriesId[]; } | undefined scatterSeries
+ */
+export function useScatterSeries() {
+  const series = useSeries();
+
+  return React.useMemo(() => series.scatter, [series.scatter]);
+}
