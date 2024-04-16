28ad040f1de760a288033838142cf11ae9691dce
Lukas <llukas.tyla@gmail.com>
2024-04-16 11:47:43 +0300

    Fix `@mui/x-charts` types build

--- packages/x-charts/typing.d.ts
+++ packages/x-charts/typing.d.ts
@@ -1,4 +1,5 @@
 // After pnpm migration the dynamically imported types have to be defined explicitly
 /// <reference types="@emotion/styled" />
 /// <reference types="@react-spring/shared" />
+/// <reference types="@react-spring/core" />
 /// <reference types="csstype" />
