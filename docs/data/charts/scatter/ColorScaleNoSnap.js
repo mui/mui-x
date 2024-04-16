63763b5fb8be4e737174693d733de62035687b46
alexandre <alex.fauquette@gmail.com>
2024-04-16 15:00:11 +0200

    exports and documentation

--- docs/data/charts/scatter/ColorScaleNoSnap.js
+++ docs/data/charts/scatter/ColorScaleNoSnap.js
@@ -9,11 +9,13 @@ import HighlightedCode from 'docs/src/modules/components/HighlightedCode';
 
 import { Chance } from 'chance';
 
+const POINTS_NUMBER = 50;
 const chance = new Chance(42);
 
 export default function ColorScaleNoSnap() {
   const [colorX, setColorX] = React.useState('piecewise');
   const [colorY, setColorY] = React.useState('None');
+  const [colorZ, setColorZ] = React.useState('None');
 
   return (
     <Stack direction="column" spacing={1} sx={{ width: '100%', maxWidth: 600 }}>
@@ -39,6 +41,18 @@ export default function ColorScaleNoSnap() {
           <MenuItem value="None">None</MenuItem>
           <MenuItem value="piecewise">piecewise</MenuItem>
           <MenuItem value="continuous">continuous</MenuItem>
+        </TextField>
+        <TextField
+          select
+          sx={{ minWidth: 150 }}
+          label="z-axis colorMap"
+          value={colorZ}
+          onChange={(event) => setColorZ(event.target.value)}
+        >
+          <MenuItem value="None">None</MenuItem>
+          <MenuItem value="piecewise">piecewise</MenuItem>
+          <MenuItem value="continuous">continuous</MenuItem>
+          <MenuItem value="ordinal">ordinal</MenuItem>
         </TextField>
       </Stack>
 
@@ -85,6 +99,37 @@ export default function ColorScaleNoSnap() {
               (colorX === 'piecewise' && {
                 type: 'piecewise',
                 thresholds: [-1.5, 0, 1.5],
+                colors: ['#d01c8b', '#f1b6da', '#b8e186', '#4dac26'],
+              }) ||
+              undefined,
+          },
+        ]}
+        zAxis={[
+          {
+            data:
+              colorZ === 'ordinal'
+                ? [
+                    ...[...Array(POINTS_NUMBER)].map(() => 'A'),
+                    ...[...Array(POINTS_NUMBER)].map(() => 'B'),
+                    ...[...Array(POINTS_NUMBER)].map(() => 'C'),
+                    ...[...Array(POINTS_NUMBER)].map(() => 'D'),
+                  ]
+                : undefined,
+            colorMap:
+              (colorZ === 'continuous' && {
+                type: 'continuous',
+                min: -2,
+                max: 2,
+                color: ['green', 'orange'],
+              }) ||
+              (colorZ === 'piecewise' && {
+                type: 'piecewise',
+                thresholds: [-1.5, 0, 1.5],
+                colors: ['#d01c8b', '#f1b6da', '#b8e186', '#4dac26'],
+              }) ||
+              (colorZ === 'ordinal' && {
+                type: 'ordinal',
+                values: ['A', 'B', 'C', 'D'],
                 colors: ['#d01c8b', '#f1b6da', '#b8e186', '#4dac26'],
               }) ||
               undefined,
@@ -150,12 +195,21 @@ export default function ColorScaleNoSnap() {
   );
 }
 
-const series = [{ data: getGaussianSeriesData([0, 0], [1, 1], 200) }].map((s) => ({
+const series = [
+  {
+    data: [
+      ...getGaussianSeriesData([-1, -1]),
+      ...getGaussianSeriesData([-1, 1]),
+      ...getGaussianSeriesData([1, 1]),
+      ...getGaussianSeriesData([1, -1]),
+    ],
+  },
+].map((s) => ({
   ...s,
   valueFormatter: (v) => `(${v.x.toFixed(1)}, ${v.y.toFixed(1)})`,
 }));
 
-function getGaussianSeriesData(mean, stdev = [0.3, 0.4], N = 50) {
+function getGaussianSeriesData(mean, stdev = [0.5, 0.5], N = 50) {
   return [...Array(N)].map((_, i) => {
     const x =
       Math.sqrt(-2.0 * Math.log(1 - chance.floating({ min: 0, max: 0.99 }))) *
@@ -167,6 +221,6 @@ function getGaussianSeriesData(mean, stdev = [0.3, 0.4], N = 50) {
         Math.cos(2.0 * Math.PI * chance.floating({ min: 0, max: 0.99 })) *
         stdev[1] +
       mean[1];
-    return { x, y, id: i };
+    return { x, y, z: x + y, id: i };
   });
 }
--- packages/x-charts/src/context/index.ts
+++ packages/x-charts/src/context/index.ts
@@ -1,1 +1,2 @@
 export type { HighlightOptions, FadeOptions, HighlightScope } from './HighlightProvider';
+export { ZAxisContextProvider, ZAxisContextProviderProps } from './ZAxisContextProvider';
