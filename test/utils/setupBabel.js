6287b784abf66207a9dae5a4c20113740773b9c0
Lukas <llukas.tyla@gmail.com>
2024-04-17 18:22:19 +0300

    prettier

--- test/utils/setupBabel.js
+++ test/utils/setupBabel.js
@@ -1,9 +1,11 @@
 require('@babel/register')({
   extensions: ['.js', '.ts', '.tsx'],
-  ignore: [function ignore(filePath) {
-    if (filePath.includes('/node_modules/')) {
-      return !filePath.includes('/node_modules/@mui/monorepo');
-    }
-    return false;
-  }]
+  ignore: [
+    function ignore(filePath) {
+      if (filePath.includes('/node_modules/')) {
+        return !filePath.includes('/node_modules/@mui/monorepo');
+      }
+      return false;
+    },
+  ],
 });
