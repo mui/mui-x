55870b4b5b8b8a3db86f6d294cdeeff47b155830
Lukas <llukas.tyla@gmail.com>
2024-04-17 16:02:55 +0300

    [test] Increase timeout for test that sometimes fail on DateTimeRangePicker (#12786)

--- test/utils/pickers/describeValue/testPickerOpenCloseLifeCycle.tsx
+++ test/utils/pickers/describeValue/testPickerOpenCloseLifeCycle.tsx
@@ -180,7 +180,9 @@ export const testPickerOpenCloseLifeCycle: DescribeValueTestSuite<any, 'picker'>
       expect(onClose.callCount).to.equal(1);
     });
 
-    it('should not call onClose or onAccept when selecting a date and `props.closeOnSelect` is false', () => {
+    it('should not call onClose or onAccept when selecting a date and `props.closeOnSelect` is false', function test() {
+      // increase the timeout of this test as it tends to sometimes fail on CI with `DesktopDateTimeRangePicker` or `MobileDateTimeRangePicker`
+      this.timeout(10000);
       const onChange = spy();
       const onAccept = spy();
       const onClose = spy();
