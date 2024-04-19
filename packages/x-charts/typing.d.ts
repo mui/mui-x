dcdd4e35ce7edc0affdb2a1ee72bc062ebd4cc39
Lukas <llukas.tyla@gmail.com>
2024-04-19 16:51:18 +0300

    Simplify Charts tsconfig

--- packages/x-charts/typing.d.ts
+++
@@ -1,5 +0,0 @@
-// After pnpm migration the dynamically imported types have to be defined explicitly
-/// <reference types="@emotion/styled" />
-/// <reference types="@react-spring/shared" />
-/// <reference types="@react-spring/core" />
-/// <reference types="csstype" />
