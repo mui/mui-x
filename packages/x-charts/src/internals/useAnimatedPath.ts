17abb38aa3e7b43a67e6bf3a89566c26d63be233
alexandre <alex.fauquette@gmail.com>
2024-04-24 16:03:10 +0200

    ximplify the modification

--- packages/x-charts/src/internals/useAnimatedPath.ts
+++ packages/x-charts/src/internals/useAnimatedPath.ts
@@ -1,27 +1,18 @@
 import * as React from 'react';
 import { interpolateString } from 'd3-interpolate';
-import { useSpringRef, useSpring } from '@react-spring/web';
+import { useSpring } from '@react-spring/web';
 
 // Taken from Nivo
 export const useAnimatedPath = (path: string, skipAnimation?: boolean) => {
   const previousPathRef = React.useRef<string | null>(null);
   const currentPathRef = React.useRef<string | null>(path);
 
-  const api = useSpringRef();
-
   React.useEffect(() => {
-    if (previousPathRef.current !== path) {
-      // Only start the animation if the previouse path is different.
-      // Avoid re-animating if the props is updated with the same value.
-      api.start();
-    }
-
     previousPathRef.current = currentPathRef.current;
     currentPathRef.current = path;
-  }, [api, path]);
+  }, [path]);
 
   const spring = useSpring({
-    ref: api,
     from: { value: 0 },
     to: { value: 1 },
     reset: true,
