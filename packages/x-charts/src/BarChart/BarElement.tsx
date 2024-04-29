7f900261432801dcddce5f45ed2e6dfb893b0ebc
Jose Quintas <juniorquintas@gmail.com>
2024-04-29 15:46:21 +0200

    Bar element no need to be animated

--- packages/x-charts/src/BarChart/BarElement.tsx
+++ packages/x-charts/src/BarChart/BarElement.tsx
@@ -6,7 +6,6 @@ import generateUtilityClass from '@mui/utils/generateUtilityClass';
 import { styled } from '@mui/material/styles';
 import { color as d3Color } from 'd3-color';
 import generateUtilityClasses from '@mui/utils/generateUtilityClasses';
-import { animated } from '@react-spring/web';
 import {
   getIsFaded,
   getIsHighlighted,
@@ -49,7 +48,7 @@ const useUtilityClasses = (ownerState: BarElementOwnerState) => {
   return composeClasses(slots, getBarElementUtilityClass, classes);
 };
 
-export const BarElementPath = styled(animated.rect, {
+export const BarElementPath = styled('rect', {
   name: 'MuiBarElement',
   slot: 'Root',
   overridesResolver: (_, styles) => styles.root,
