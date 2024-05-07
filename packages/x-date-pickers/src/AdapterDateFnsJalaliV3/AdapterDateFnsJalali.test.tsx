d99d2dff546174d83afa01c10899fb614b9ca382
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-05-07 05:35:28 +0330

    copy AdapterDateFnsJalali to AdapterDateFnsJalaliV3

---
+++ packages/x-date-pickers/src/AdapterDateFnsJalaliV3/AdapterDateFnsJalali.test.tsx
@@ -0,0 +1,95 @@
+import { expect } from 'chai';
+import { DateTimeField } from '@mui/x-date-pickers';
+import { AdapterDateFnsJalali } from '@mui/x-date-pickers/AdapterDateFnsJalali';
+import {
+  createPickerRenderer,
+  expectFieldValueV7,
+  describeJalaliAdapter,
+  buildFieldInteractions,
+} from 'test/utils/pickers';
+import { enUS } from 'date-fns/locale';
+import faIR from 'date-fns-jalali/locale/fa-IR';
+import faJalaliIR from 'date-fns-jalali/locale/fa-jalali-IR';
+import { AdapterMomentJalaali } from '@mui/x-date-pickers/AdapterMomentJalaali';
+import { AdapterFormats } from '@mui/x-date-pickers/models';
+
+describe('<AdapterDateFnsJalali />', () => {
+  describeJalaliAdapter(AdapterDateFnsJalali, {});
+
+  describe('Adapter localization', () => {
+    it('Formatting', () => {
+      const adapter = new AdapterMomentJalaali();
+
+      const expectDate = (format: keyof AdapterFormats, expectedWithFaIR: string) => {
+        const date = adapter.date('2020-02-01T23:44:00.000Z')!;
+
+        expect(adapter.format(date, format)).to.equal(expectedWithFaIR);
+      };
+
+      expectDate('fullDate', '۱۳۹۸، Bahman ۱م');
+      expectDate('keyboardDate', '۱۳۹۸/۱۱/۱۲');
+      expectDate('keyboardDateTime', '۱۳۹۸/۱۱/۱۲ ۲۳:۴۴');
+      expectDate('keyboardDateTime12h', '۱۳۹۸/۱۱/۱۲ ۱۱:۴۴ بعد از ظهر');
+      expectDate('keyboardDateTime24h', '۱۳۹۸/۱۱/۱۲ ۲۳:۴۴');
+    });
+  });
+
+  describe('Picker localization', () => {
+    const testDate = '2018-05-15T09:35:00';
+    const localizedTexts = {
+      enUS: {
+        placeholder: 'MM/DD/YYYY hh:mm aa',
+        value: '02/25/1397 09:35 AM',
+      },
+      faIR: {
+        placeholder: 'YYYY/MM/DD hh:mm aa',
+        value: '1397/02/25 09:35 ق.ظ.',
+      },
+      faJalaliIR: {
+        // Not sure about what's the difference between this and fa-IR
+        placeholder: 'YYYY/MM/DD hh:mm aa',
+        value: '1397/02/25 09:35 ق.ظ.',
+      },
+    };
+
+    Object.keys(localizedTexts).forEach((localeKey) => {
+      const localeObject = {
+        faIR,
+        faJalaliIR,
+        enUS,
+      }[localeKey];
+
+      describe(`test with the "${localeKey}" locale`, () => {
+        const { render, adapter, clock } = createPickerRenderer({
+          clock: 'fake',
+          adapterName: 'date-fns-jalali',
+          locale: localeObject,
+        });
+
+        const { renderWithProps } = buildFieldInteractions({
+          render,
+          clock,
+          Component: DateTimeField,
+        });
+
+        it('should have correct placeholder', () => {
+          const v7Response = renderWithProps({ enableAccessibleFieldDOMStructure: true });
+
+          expectFieldValueV7(
+            v7Response.getSectionsContainer(),
+            localizedTexts[localeKey].placeholder,
+          );
+        });
+
+        it('should have well formatted value', () => {
+          const v7Response = renderWithProps({
+            enableAccessibleFieldDOMStructure: true,
+            value: adapter.date(testDate),
+          });
+
+          expectFieldValueV7(v7Response.getSectionsContainer(), localizedTexts[localeKey].value);
+        });
+      });
+    });
+  });
+});
---
+++ packages/x-date-pickers/src/AdapterDateFnsJalaliV3/index.ts
@@ -0,0 +1,1 @@
+export { AdapterDateFnsJalali } from './AdapterDateFnsJalali';
