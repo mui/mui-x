df5db67c0e3f755e651c118f50151488fe8a01b2
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2024-05-08 20:14:37 +0330

    [pickers] Add `AdapterDateFnsJalaliV3` adapter (#12891)
    Co-authored-by: Lukas <llukas.tyla@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -91,8 +91,19 @@ module.exports = function getBabelConfig(api) {
       {
         test: /date-fns/i,
         replacer: 'date-fns-v3',
-        ignoreFilenames: 'AdapterDateFns.ts',
+        // This option is provided by the `patches/babel-plugin-replace-imports@1.0.2.patch` patch
+        filenameIncludes: 'src/AdapterDateFnsV3/',
       },
+    ]);
+    plugins.push([
+      'babel-plugin-replace-imports',
+      {
+        test: /date-fns-jalali/i,
+        replacer: 'date-fns-jalali-v3',
+        // This option is provided by the `patches/babel-plugin-replace-imports@1.0.2.patch` patch
+        filenameIncludes: 'src/AdapterDateFnsJalaliV3/',
+      },
+      'replace-date-fns-jalali-imports',
     ]);
   }
 
