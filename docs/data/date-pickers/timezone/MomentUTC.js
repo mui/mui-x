7768bfc890b8b35a72e399daf94af67282820ef8
Lukas <llukas.tyla@gmail.com>
2024-05-16 12:43:34 +0300

    [pickers] Fix `AdapterMomentJalaali` regression

--- docs/data/date-pickers/timezone/MomentUTC.js
+++ docs/data/date-pickers/timezone/MomentUTC.js
@@ -6,11 +6,13 @@ import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
+moment.locale('en');
+
 export default function MomentUTC() {
   const [value, setValue] = React.useState(moment.utc('2022-04-17T15:30'));
 
   return (
-    <LocalizationProvider dateAdapter={AdapterMoment}>
+    <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en">
       <Stack spacing={2}>
         <DateTimePicker value={value} onChange={setValue} />
         <Typography>
