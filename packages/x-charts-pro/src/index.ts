41f28ec409a542fa7e652726bed8ecb81cd282ad
alexandre <alex.fauquette@gmail.com>
2024-05-20 10:26:08 +0200

    package files

---
+++ packages/x-charts-pro/src/index.ts
@@ -0,0 +1,8 @@
+import { LicenseInfo as LicenseInfoExport } from '@mui/x-license';
+/**
+ * @deprecated Use `@mui/x-license` package instead:
+ * @example import { LicenseInfo } from '@mui/x-license';
+ */
+export class LicenseInfo extends LicenseInfoExport {}
+
+export * from '@mui/x-charts';
