da0105a2e16720b8eb8027785af791c02f3062ff
Jose Quintas <juniorquintas@gmail.com>
2024-05-22 18:48:47 +0200

    Add useItemHighlighted

---
+++ packages/x-charts/src/context/HighlightedProvider/useItemHighlighted.ts
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { HighlightedContext, HighlightedItemData } from './HighlightedContext';
+
+export function useItemHighlighted(item: HighlightedItemData | null): {
+  isHighlighted: boolean;
+  isFaded: boolean;
+} {
+  const highlighted = React.useContext(HighlightedContext);
+
+  if (highlighted === undefined) {
+    throw new Error(
+      [
+        'MUI X: Could not find the highlighted ref context.',
+        'It looks like you rendered your component outside of a ChartsContainer parent component.',
+      ].join('\n'),
+    );
+  }
+
+  if (!item) {
+    return {
+      isHighlighted: false,
+      isFaded: false,
+    };
+  }
+
+  const isHighlighted = highlighted.isHighlighted(item);
+  const isFaded = !isHighlighted && highlighted.isFaded(item);
+
+  return { isHighlighted, isFaded };
+}
