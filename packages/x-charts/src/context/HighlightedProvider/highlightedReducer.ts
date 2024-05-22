90bf8f45c19a7ee1449234c90cdfdc760c5e739c
Jose Quintas <juniorquintas@gmail.com>
2024-05-22 18:48:39 +0200

    isHighlighted logic for series same-series

--- packages/x-charts/src/context/HighlightedProvider/highlightedReducer.ts
+++ packages/x-charts/src/context/HighlightedProvider/highlightedReducer.ts
@@ -19,6 +19,12 @@ const createIsHighlighted =
       return false;
     }
 
+    if (highlightedOptions.type === 'series') {
+      return (
+        input.seriesId === highlightedOptions.seriesId && input.itemId === highlightedOptions.itemId
+      );
+    }
+
     return false;
   };
 
