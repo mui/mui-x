69b295b8d510cf4f7923975cd7dd5d63084a3063
Jose Quintas <juniorquintas@gmail.com>
2024-05-23 13:56:35 +0200

    HighlightItemData docs

--- packages/x-charts/src/context/HighlightedProvider/HighlightedContext.ts
+++ packages/x-charts/src/context/HighlightedProvider/HighlightedContext.ts
@@ -1,6 +1,22 @@
 import * as React from 'react';
 import { SeriesId } from '../../models/seriesType/common';
 
+/**
+ * The data of the highlighted item.
+ * To highlight an item, you need to provide the series id and the item id.
+ * If targeting the whole series, you can omit the item id.
+ * To clear the highlight, set the value to an empty object.
+ *
+ * @example
+ * // Highlight the item with the series id 'london' and the item id 0.
+ * { seriesId: 'london', itemId: 0 }
+ *
+ * // Highlight the whole series with the series id 'london'.
+ * { seriesId: 'london' }
+ *
+ * // Clear the highlight.
+ * {}
+ */
 export type HighlightItemData = {
   /**
    * The series id of the highlighted item.
