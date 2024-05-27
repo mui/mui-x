9723a8abaa46767b2e80c00e5a98489eb217d456
Jose Quintas <juniorquintas@gmail.com>
2024-05-27 21:05:29 +0200

    Revert "Add deprecation warning to `useInteractionItemProps` signature"
    This reverts commit 2d37f7ca95f9ee3c9b5bd5940714c9bb832798bb.

--- packages/x-charts/src/hooks/useInteractionItemProps.ts
+++ packages/x-charts/src/hooks/useInteractionItemProps.ts
@@ -1,30 +1,11 @@
 import * as React from 'react';
 import { InteractionContext } from '../context/InteractionProvider';
 import { SeriesItemIdentifier } from '../models';
-import { HighlightScope, HighlightedContext } from '../context';
+import { HighlightedContext } from '../context';
 
-type UseInteractionItemFunction = (data: SeriesItemIdentifier) => {
-  onMouseEnter?: () => void;
-  onMouseLeave?: () => void;
-};
-export function useInteractionItemProps(skip?: boolean): UseInteractionItemFunction;
-/**
- * @deprecated The `scope` parameter was deprecated and will be removed in the next major version. Simply remove it from the call.
- */
-export function useInteractionItemProps(
-  scope?: Partial<HighlightScope>,
-  skip?: boolean,
-): UseInteractionItemFunction;
-export function useInteractionItemProps(
-  scope?: Partial<HighlightScope> | boolean,
-  skip?: boolean,
-): UseInteractionItemFunction {
+export const useInteractionItemProps = (skip?: boolean) => {
   const { dispatch: dispatchInteraction } = React.useContext(InteractionContext);
   const { setHighlighted, clearHighlighted } = React.useContext(HighlightedContext);
-
-  if (typeof scope === 'boolean') {
-    skip = scope;
-  }
 
   if (skip) {
     return () => ({});
@@ -50,4 +31,4 @@ export function useInteractionItemProps(
     };
   };
   return getInteractionItemProps;
-}
+};
