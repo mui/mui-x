771fef9657af98f7b153b89a430257d17a33529b
Jose C Quintas Jr <juniorquintas@gmail.com>
2024-05-28 12:22:47 +0200

    Update packages/x-charts/src/context/HighlightedProvider/useItemHighlighted.ts
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
    Signed-off-by: Jose C Quintas Jr <juniorquintas@gmail.com>

--- packages/x-charts/src/context/HighlightedProvider/useItemHighlighted.ts
+++ packages/x-charts/src/context/HighlightedProvider/useItemHighlighted.ts
@@ -16,7 +16,7 @@ export type ItemHighlightedState = {
  * A hook to check the highlighted state of the item.
  * This function already calculates that an item is not faded if it is highlighted.
  *
- * if you need fine control over the state, use the `useHighlighted` hook instead.
+ * If you need fine control over the state, use the `useHighlighted` hook instead.
  *
  * @param {HighlightItemData} item is the item to check
  * @returns {ItemHighlightedState} the state of the item
