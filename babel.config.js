45543161bd4de0e5567ca44a3b90e727b4486b09
Aleksey Kozlenkov <nidecker95@gmail.com>
2024-05-31 15:23:57 +0200

    (POC) Explicitly replace imports for AdapterDateFnsV3

--- babel.config.js
+++ babel.config.js
@@ -84,15 +84,15 @@ module.exports = function getBabelConfig(api) {
     // We replace `date-fns` imports with an aliased `date-fns@v3` version installed as `date-fns-v3` for tests.
     // The plugin is patched to only run on `AdapterDateFnsV3.ts`.
     // TODO: remove when we upgrade to date-fns v3 by default.
-    plugins.push([
-      'babel-plugin-replace-imports',
-      {
-        test: /date-fns/i,
-        replacer: 'date-fns-v3',
-        // This option is provided by the `patches/babel-plugin-replace-imports@1.0.2.patch` patch
-        filenameIncludes: 'src/AdapterDateFnsV3/',
-      },
-    ]);
+    // plugins.push([
+    //   'babel-plugin-replace-imports',
+    //   {
+    //     test: /date-fns/i,
+    //     replacer: 'date-fns-v3',
+    //     // This option is provided by the `patches/babel-plugin-replace-imports@1.0.2.patch` patch
+    //     filenameIncludes: 'src/AdapterDateFnsV3/',
+    //   },
+    // ]);
     plugins.push([
       'babel-plugin-replace-imports',
       {
