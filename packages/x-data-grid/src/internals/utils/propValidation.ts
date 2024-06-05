0b185a34f9c8ca3bac847b886ee064f0eac82ece
Bilal Shafi <bilalshafidev@gmail.com>
2024-06-05 18:55:13 +0500

    Fix prop validation

--- packages/x-data-grid/src/internals/utils/propValidation.ts
+++ packages/x-data-grid/src/internals/utils/propValidation.ts
@@ -35,6 +35,7 @@ export const propValidatorsDataGrid: PropValidator<DataGridProcessedProps>[] = [
   (props) =>
     (props.paginationMode === 'server' &&
       props.rowCount == null &&
+      !props.unstable_dataSource &&
       [
         "MUI X: The `rowCount` prop must be passed using `paginationMode='server'`",
         'For more detail, see http://mui.com/components/data-grid/pagination/#index-based-pagination',
