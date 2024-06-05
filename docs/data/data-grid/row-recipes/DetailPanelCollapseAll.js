7c3710f6b119202e490793b35c402f840da79cbc
michel <jsnerdic@gmail.com>
2024-06-05 15:59:37 +0200

    added a new advanced recipe for the detail panel custom header renderer

---
+++ docs/data/data-grid/row-recipes/DetailPanelCollapseAll.js
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import UnfoldLessIcon from '@mui/icons-material/UnfoldLess';
+import {
+  DataGridPro,
+  useGridApiContext,
+  useGridSelector,
+  gridDetailPanelExpandedRowIdsSelector,
+  GRID_DETAIL_PANEL_TOGGLE_COL_DEF,
+} from '@mui/x-data-grid-pro';
+import {
+  randomCreatedDate,
+  randomCurrency,
+  randomEmail,
+  randomPrice,
+} from '@mui/x-data-grid-generator';
+
+export default function DetailPanelCollapseAll() {
+  const getDetailPanelContent = React.useCallback(
+    ({ row }) => <Box sx={{ p: 2 }}>{`Order #${row.id}`}</Box>,
+    [],
+  );
+
+  const getDetailPanelHeight = React.useCallback(() => 50, []);
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGridPro
+        rows={rows}
+        columns={columns}
+        getDetailPanelContent={getDetailPanelContent}
+        getDetailPanelHeight={getDetailPanelHeight}
+      />
+    </div>
+  );
+}
+
+function CustomDetailPanelHeader() {
+  const apiRef = useGridApiContext();
+  const expandedRowIds = useGridSelector(
+    apiRef,
+    gridDetailPanelExpandedRowIdsSelector,
+  );
+  const areDetailPanelsOpen = expandedRowIds.length > 0;
+
+  const closeAllDetailPanels = () => apiRef.current.setExpandedDetailPanels([]);
+
+  return (
+    <IconButton
+      size="small"
+      tabIndex={-1}
+      disabled={!areDetailPanelsOpen}
+      onClick={closeAllDetailPanels}
+      aria-label={'Close All'}
+    >
+      <UnfoldLessIcon
+        sx={{
+          opacity: `${areDetailPanelsOpen ? 1 : 0}`,
+          transition: (theme) =>
+            theme.transitions.create('opacity', {
+              duration: theme.transitions.duration.shortest,
+            }),
+        }}
+        fontSize="inherit"
+      />
+    </IconButton>
+  );
+}
+
+const columns = [
+  {
+    ...GRID_DETAIL_PANEL_TOGGLE_COL_DEF,
+    renderHeader: () => <CustomDetailPanelHeader />,
+  },
+  { field: 'id', headerName: 'Order ID' },
+  { field: 'customer', headerName: 'Customer', width: 200 },
+  { field: 'date', type: 'date', headerName: 'Placed at' },
+  { field: 'currency', headerName: 'Currency' },
+  { field: 'total', type: 'number', headerName: 'Total' },
+];
+
+const rows = [
+  {
+    id: 1,
+    customer: 'Matheus',
+    email: randomEmail(),
+    date: randomCreatedDate(),
+    currency: randomCurrency(),
+    total: randomPrice(1, 1000),
+  },
+  {
+    id: 2,
+    customer: 'Olivier',
+    email: randomEmail(),
+    date: randomCreatedDate(),
+    currency: randomCurrency(),
+    total: randomPrice(1, 1000),
+  },
+  {
+    id: 3,
+    customer: 'Flavien',
+    email: randomEmail(),
+    date: randomCreatedDate(),
+    currency: randomCurrency(),
+    total: randomPrice(1, 1000),
+  },
+  {
+    id: 4,
+    customer: 'Danail',
+    email: randomEmail(),
+    date: randomCreatedDate(),
+    currency: randomCurrency(),
+    total: randomPrice(1, 1000),
+  },
+  {
+    id: 5,
+    customer: 'Alexandre',
+    email: randomEmail(),
+    date: randomCreatedDate(),
+    currency: randomCurrency(),
+    total: randomPrice(1, 1000),
+  },
+];
--- docs/data/data-grid/row-recipes/row-recipes.md
+++ docs/data/data-grid/row-recipes/row-recipes.md
@@ -1,4 +1,4 @@
----
+--
 title: Data Grid - Row customization recipes
 ---
 
@@ -13,3 +13,15 @@ By default, the [Master detail <span class="plan-pro" />](/x/react-data-grid/mas
 However, you can [control the expanded detail panels](/x/react-data-grid/master-detail/#controlling-expanded-detail-panels) to have only one detail panel expanded at a time.
 
 {{"demo": "DetailPanelOneExpandedRow.js", "bg": "inline", "defaultCodeOpen": false}}
+
+## Collapse all detail panels with a custom header
+
+This feature allows you to create a custom header element that collapses all currently expanded detail panels at once.
+
+Here is how it works:
+
+- Clicking the custom header triggers a collapse action on all open detail panels.
+- It checks the status of open panels using the [`useGridSelector` hook](/x/react-data-grid/state/#with-usegridselector) to access the grid's state.
+- The example leverages [`setExpandedDetailPanels`](/x/api/data-grid/grid-api/#grid-api-prop-setExpandedDetailPanels) from the [Grid API](/x/react-data-grid/api-object/#how-to-use-the-api-object).
+
+{{"demo": "DetailPanelCollapseAll.js", "bg": "inline", "defaultCodeOpen": false}}
