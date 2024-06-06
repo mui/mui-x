87966c1d6801396f3fa983db29d4d4342922d633
Jose Quintas <juniorquintas@gmail.com>
2024-06-06 11:18:12 +0200

    Simpler plugin types

--- packages/x-charts/src/ChartContainer/defaultPlugins.ts
+++ packages/x-charts/src/ChartContainer/defaultPlugins.ts
@@ -2,5 +2,12 @@ import { plugin as barPlugin } from '../BarChart/plugin';
 import { plugin as scatterPlugin } from '../ScatterChart/plugin';
 import { plugin as linePlugin } from '../LineChart/plugin';
 import { plugin as piePlugin } from '../PieChart/plugin';
+import { ChartSeriesType } from '../models/seriesType/config';
+import { ChartsPluginType } from '../models';
 
-export const defaultPlugins = [barPlugin, scatterPlugin, linePlugin, piePlugin];
+export const defaultPlugins: ChartsPluginType<ChartSeriesType>[] = [
+  barPlugin,
+  scatterPlugin,
+  linePlugin,
+  piePlugin,
+];
