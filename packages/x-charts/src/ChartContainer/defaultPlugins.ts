4c3eda46b6183f318b37550122570d73ac0e5356
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2024-06-06 13:21:07 +0200

    Update packages/x-charts/src/ChartContainer/defaultPlugins.ts
    Signed-off-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>

--- packages/x-charts/src/ChartContainer/defaultPlugins.ts
+++ packages/x-charts/src/ChartContainer/defaultPlugins.ts
@@ -2,7 +2,6 @@ import { plugin as barPlugin } from '../BarChart/plugin';
 import { plugin as scatterPlugin } from '../ScatterChart/plugin';
 import { plugin as linePlugin } from '../LineChart/plugin';
 import { plugin as piePlugin } from '../PieChart/plugin';
-import { ChartSeriesType } from '../models/seriesType/config';
 import { ChartsPluginType } from '../models';
 
 export const defaultPlugins: ChartsPluginType<'bar' | 'scatter' | 'line' | 'pie'>[] = [
