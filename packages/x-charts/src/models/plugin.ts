a03a5c353bff9cf3871240eadb0795b8faeb0c61
Jose C Quintas Jr <juniorquintas@gmail.com>
2024-06-06 13:39:29 +0200

    [charts] Simplify plugin types (#13396)
    Signed-off-by: Jose C Quintas Jr <juniorquintas@gmail.com>
    Signed-off-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>

--- packages/x-charts/src/models/plugin.ts
+++ packages/x-charts/src/models/plugin.ts
@@ -14,14 +14,12 @@ export type ColorProcessorsConfig<T extends ChartSeriesType> = {
   [Key in T]?: ColorProcessor<Key>;
 };
 
-export type ChartsPluginType<T extends ChartSeriesType> = {
-  seriesType: T;
-  seriesFormatter: Formatter<T>;
-  colorProcessor: ColorProcessor<T>;
-  xExtremumGetter?: ExtremumGetter<T>;
-  yExtremumGetter?: ExtremumGetter<T>;
-};
-
-export type ChartsPluginTypes<T extends ChartSeriesType> = {
-  [Key in T]: ChartsPluginType<Key>;
-}[T];
+export type ChartsPluginType<T> = T extends ChartSeriesType
+  ? {
+      seriesType: T;
+      seriesFormatter: Formatter<T>;
+      colorProcessor: ColorProcessor<T>;
+      xExtremumGetter?: ExtremumGetter<T>;
+      yExtremumGetter?: ExtremumGetter<T>;
+    }
+  : never;
