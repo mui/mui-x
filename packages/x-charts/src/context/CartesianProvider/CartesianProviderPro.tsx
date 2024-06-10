637881ff17b192da2020a2ac1c8a07c74aa2f073
Jose Quintas <juniorquintas@gmail.com>
2024-06-10 12:13:35 +0200

    Remove CartesianContextProviderProps from pro

--- packages/x-charts/src/context/CartesianProvider/CartesianProviderPro.tsx
+++ packages/x-charts/src/context/CartesianProvider/CartesianProviderPro.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import { AxisConfig, ChartsXAxisProps, ChartsYAxisProps, ScaleName } from '../../models/axis';
-import { DatasetType } from '../../models/seriesType/config';
-import { MakeOptional } from '../../models/helpers';
 import { useDrawingArea } from '../../hooks/useDrawingArea';
 import { useSeries } from '../../hooks/useSeries';
 import { CartesianContext } from './CartesianContext';
@@ -9,34 +6,7 @@ import { normalizeAxis } from './normalizeAxis';
 import { computeValue } from './computeValue';
 import { ExtremumGettersConfig } from '../../models';
 import { useZoom } from '../ZoomProvider/useZoom';
-
-export type CartesianContextProviderProps = {
-  /**
-   * The configuration of the x-axes.
-   * If not provided, a default axis config is used.
-   * An array of [[AxisConfig]] objects.
-   */
-  xAxis?: MakeOptional<AxisConfig<ScaleName, any, ChartsXAxisProps>, 'id'>[];
-  /**
-   * The configuration of the y-axes.
-   * If not provided, a default axis config is used.
-   * An array of [[AxisConfig]] objects.
-   */
-  yAxis?: MakeOptional<AxisConfig<ScaleName, any, ChartsYAxisProps>, 'id'>[];
-  /**
-   * An array of objects that can be used to populate series and axes data using their `dataKey` property.
-   */
-  dataset?: DatasetType;
-  /**
-   * An object with x-axis extremum getters per series type.
-   */
-  xExtremumGetters: ExtremumGettersConfig;
-  /**
-   * An object with y-axis extremum getters per series type.
-   */
-  yExtremumGetters: ExtremumGettersConfig;
-  children: React.ReactNode;
-};
+import { CartesianContextProviderProps } from './CartesianProvider';
 
 function CartesianContextProviderPro(props: CartesianContextProviderProps) {
   const prevContext = React.useContext(CartesianContext);
