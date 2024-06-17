c16372492a81b385967edb9017fc6f7c93c17bb1
Jacob Rowland <109928237+jacrowland1@users.noreply.github.com>
2024-06-18 04:29:48 +1200

    [l10n] Improve Danish (da-DK) locale (#13375)
    Signed-off-by: Gene Arch <genepaularch@gmail.com>
    Co-authored-by: Gene Arch <genepaularch@gmail.com>
    Co-authored-by: Rom Grk <romgrk.cc@gmail.com>

--- packages/x-date-pickers/src/locales/daDK.ts
+++ packages/x-date-pickers/src/locales/daDK.ts
@@ -26,10 +26,10 @@ const daDKPickers: Partial<PickersLocaleText<any>> = {
   // DateRange labels
   start: 'Start',
   end: 'Slut',
-  // startDate: 'Start date',
-  // startTime: 'Start time',
-  // endDate: 'End date',
-  // endTime: 'End time',
+  startDate: 'Start dato',
+  startTime: 'Start tid',
+  endDate: 'Slut date',
+  endTime: 'Slut tid',
 
   // Action bar
   cancelButtonLabel: 'Annuller',
@@ -68,7 +68,7 @@ const daDKPickers: Partial<PickersLocaleText<any>> = {
     value !== null && utils.isValid(value)
       ? `Vælg tidspunkt, valgte tidspunkt er ${utils.format(value, 'fullTime')}`
       : 'Vælg tidspunkt',
-  // fieldClearLabel: 'Clear value',
+  fieldClearLabel: 'ryd felt',
 
   // Table labels
   timeTableLabel: 'vælg tidspunkt',
@@ -85,17 +85,17 @@ const daDKPickers: Partial<PickersLocaleText<any>> = {
   fieldMeridiemPlaceholder: () => 'aa',
 
   // View names
-  // year: 'Year',
-  // month: 'Month',
-  // day: 'Day',
-  // weekDay: 'Week day',
-  // hours: 'Hours',
-  // minutes: 'Minutes',
-  // seconds: 'Seconds',
-  // meridiem: 'Meridiem',
+  year: 'år',
+  month: 'måned',
+  day: 'dag',
+  weekDay: 'ugedag',
+  hours: 'timer',
+  minutes: 'minutter',
+  seconds: 'sekunder',
+  meridiem: 'middag',
 
   // Common
-  // empty: 'Empty',
+  empty: 'tom',
 };
 
 export const daDK = getPickersLocalization(daDKPickers);
