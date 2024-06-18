fcf666460564b707bd84cea91d76662a9e463137
noraleonte <noraleonte00@gmail.com>
2024-06-18 18:58:50 +0300

    update docs

--- docs/data/tree-view/simple-tree-view/customization/IconExpansionTreeView.tsx
+++ docs/data/tree-view/simple-tree-view/customization/IconExpansionTreeView.tsx
@@ -1,59 +1,27 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
-import { useTreeItem2Utils } from '@mui/x-tree-view/hooks';
-import { UseTreeItem2ContentSlotOwnProps } from '@mui/x-tree-view/useTreeItem2';
-import { TreeItem2, TreeItem2Props } from '@mui/x-tree-view/TreeItem2';
-
-const CustomTreeItem = React.forwardRef(function MyTreeItem(
-  props: TreeItem2Props,
-  ref: React.Ref<HTMLLIElement>,
-) {
-  const { interactions } = useTreeItem2Utils({
-    itemId: props.itemId,
-    children: props.children,
-  });
-
-  const handleContentClick: UseTreeItem2ContentSlotOwnProps['onClick'] = (event) => {
-    event.defaultMuiPrevented = true;
-    interactions.handleSelection(event);
-  };
-
-  const handleIconContainerClick = (event: React.MouseEvent) => {
-    interactions.handleExpansion(event);
-  };
-
-  return (
-    <TreeItem2
-      {...props}
-      ref={ref}
-      slotProps={{
-        content: { onClick: handleContentClick },
-        iconContainer: { onClick: handleIconContainerClick },
-      }}
-    />
-  );
-});
+import { TreeItem2 } from '@mui/x-tree-view/TreeItem2';
 
 export default function IconExpansionTreeView() {
   return (
     <Box sx={{ minHeight: 352, minWidth: 250 }}>
-      <SimpleTreeView aria-label="icon expansion">
-        <CustomTreeItem itemId="grid" label="Data Grid">
-          <CustomTreeItem itemId="grid-community" label="@mui/x-data-grid" />
-          <CustomTreeItem itemId="grid-pro" label="@mui/x-data-grid-pro" />
-          <CustomTreeItem itemId="grid-premium" label="@mui/x-data-grid-premium" />
-        </CustomTreeItem>
-        <CustomTreeItem itemId="pickers" label="Date and Time Pickers">
-          <CustomTreeItem itemId="pickers-community" label="@mui/x-date-pickers" />
-          <CustomTreeItem itemId="pickers-pro" label="@mui/x-date-pickers-pro" />
-        </CustomTreeItem>
-        <CustomTreeItem itemId="charts" label="Charts">
-          <CustomTreeItem itemId="charts-community" label="@mui/x-charts" />
-        </CustomTreeItem>
-        <CustomTreeItem itemId="tree-view" label="Tree View">
-          <CustomTreeItem itemId="tree-view-community" label="@mui/x-tree-view" />
-        </CustomTreeItem>
+      <SimpleTreeView aria-label="icon expansion" expansionTrigger="iconContainer">
+        <TreeItem2 itemId="grid" label="Data Grid">
+          <TreeItem2 itemId="grid-community" label="@mui/x-data-grid" />
+          <TreeItem2 itemId="grid-pro" label="@mui/x-data-grid-pro" />
+          <TreeItem2 itemId="grid-premium" label="@mui/x-data-grid-premium" />
+        </TreeItem2>
+        <TreeItem2 itemId="pickers" label="Date and Time Pickers">
+          <TreeItem2 itemId="pickers-community" label="@mui/x-date-pickers" />
+          <TreeItem2 itemId="pickers-pro" label="@mui/x-date-pickers-pro" />
+        </TreeItem2>
+        <TreeItem2 itemId="charts" label="Charts">
+          <TreeItem2 itemId="charts-community" label="@mui/x-charts" />
+        </TreeItem2>
+        <TreeItem2 itemId="tree-view" label="Tree View">
+          <TreeItem2 itemId="tree-view-community" label="@mui/x-tree-view" />
+        </TreeItem2>
       </SimpleTreeView>
     </Box>
   );
