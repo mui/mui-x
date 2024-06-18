872f99767984fd2e697255d61c0c84809ec27a7c
Jose Quintas <juniorquintas@gmail.com>
2024-06-18 13:13:07 +0200

    update internals

--- packages/x-charts/src/internals/index.ts
+++ packages/x-charts/src/internals/index.ts
@@ -11,3 +11,19 @@ export { useSeries } from '../hooks/useSeries';
 // utils
 export * from './defaultizeValueFormatter';
 export * from './configInit';
+
+// contexts
+
+export * from '../context/CartesianProvider';
+export * from '../context/DrawingProvider';
+export * from '../context/InteractionProvider';
+export * from '../context/SeriesContextProvider';
+export * from '../context/ZAxisContextProvider';
+
+// series configuration
+export * from '../models/seriesType/config';
+export * from '../models/seriesType/common';
+
+export * from '../models/helpers';
+export * from '../models/z-axis';
+export * from '../models/axis';
