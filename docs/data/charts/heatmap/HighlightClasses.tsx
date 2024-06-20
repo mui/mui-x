fdefab3aa83254784502381eb7d3d9b9793d74c3
alexandre <alex.fauquette@gmail.com>
2024-06-20 13:39:19 +0200

    share data accross example

--- docs/data/charts/heatmap/HighlightClasses.tsx
+++ docs/data/charts/heatmap/HighlightClasses.tsx
@@ -2,33 +2,9 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import '@mui/x-charts-pro/typeOverloads';
 import { UnstableHeatmap, heatmapClasses } from '@mui/x-charts-pro/Heatmap';
-import { HeatmapValueType } from '@mui/x-charts-pro/models';
-
-const data: HeatmapValueType[] = [
-  [0, 0, 1],
-  [0, 1, 2],
-  [0, 2, 4],
-  [0, 3, 5],
-  [0, 4, 2],
-  [1, 0, 3],
-  [1, 1, 5],
-  [1, 2, 1],
-  [1, 3, 2],
-  [1, 4, 4],
-  [2, 0, 5],
-  [2, 1, 2],
-  [2, 2, 3],
-  [2, 3, 1],
-  [2, 4, 2],
-  [3, 0, 4],
-  [3, 1, 5],
-  [3, 2, 2],
-  [3, 3, 3],
-  [3, 4, 5],
-];
+import { data } from './dumbData';
 
 export default function HighlightClasses() {
-  console.log([`& .${heatmapClasses.cell}.${heatmapClasses.highlighted}`]);
   return (
     <Box sx={{ width: '100%', maxWidth: 400 }}>
       <UnstableHeatmap
--- docs/data/charts/heatmap/HighlightHeatmap.js
+++ docs/data/charts/heatmap/HighlightHeatmap.js
@@ -2,29 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import '@mui/x-charts-pro/typeOverloads';
 import { UnstableHeatmap } from '@mui/x-charts-pro/Heatmap';
-
-const data = [
-  [0, 0, 1],
-  [0, 1, 2],
-  [0, 2, 4],
-  [0, 3, 5],
-  [0, 4, 2],
-  [1, 0, 3],
-  [1, 1, 5],
-  [1, 2, 1],
-  [1, 3, 2],
-  [1, 4, 4],
-  [2, 0, 5],
-  [2, 1, 2],
-  [2, 2, 3],
-  [2, 3, 1],
-  [2, 4, 2],
-  [3, 0, 4],
-  [3, 1, 5],
-  [3, 2, 2],
-  [3, 3, 3],
-  [3, 4, 5],
-];
+import { data } from './dumbData';
 
 export default function HighlightHeatmap() {
   return (
