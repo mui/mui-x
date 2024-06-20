f4b4175c4e3d84e4bf5fd718f22c3d7dfa4009ed
michel <jsnerdic@gmail.com>
2024-06-20 16:25:27 +0200

    adjusted the licesning error

--- packages/x-license/src/utils/licenseErrorMessageUtils.ts
+++ packages/x-license/src/utils/licenseErrorMessageUtils.ts
@@ -36,7 +36,9 @@ export function showProductNotCoveredError() {
   showError([
     'MUI X: Product not covered by plan.',
     '',
-    'The component you are trying to use is not included in the Pro Plan your purchased. You are using a license that is only compatible with the `@mui/x-data-grid-pro` and `@mui/x-date-pickers-pro` commercial packages.',
+    'The component you are trying to use is not included in the Pro Plan your purchased.',
+    '',
+    'Your license is from an old version of the pro plan that is only compatible with the `@mui/x-data-grid-pro` and `@mui/x-date-pickers-pro` commercial packages.',
     '',
     'To start using another Pro package, please consider reaching to our sales team to upgrade your license or visit https://mui.com/r/x-get-license to get a new license key.',
   ]);
