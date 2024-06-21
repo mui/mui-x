72c47ba7b904d79ce593a7fc78c7b9e62d4f2491
delangle <flaviendelangle@gmail.com>
2024-06-21 13:52:40 +0200

    Work

--- packages/x-tree-view-pro/src/internals/plugins/useTreeViewVirtualization/useTreeViewVirtualization.ts
+++ packages/x-tree-view-pro/src/internals/plugins/useTreeViewVirtualization/useTreeViewVirtualization.ts
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { TreeViewPlugin } from '@mui/x-tree-view/internals';
 import { UseTreeViewVirtualizationSignature } from './useTreeViewVirtualization.types';
 
-export const useTreeViewVirtualization: TreeViewPlugin<UseTreeViewVirtualizationSignature> = () => {
+export const useTreeViewVirtualization: TreeViewPlugin<UseTreeViewVirtualizationSignature> = ({ state }) => {
   const virtualScrollerRef = React.useRef<HTMLUListElement>(null);
 
   return {
     instance: {
       virtualScrollerRef,
-      getDimensions: () => ({}) as any,
+      getDimensions: () => state.virtualization,
     },
   };
 };
