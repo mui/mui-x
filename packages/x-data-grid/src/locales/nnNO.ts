0b7866358a7823ac97ad8ac73ab300f9c2c16d86
Anders Lund <a_lund_01@hotmail.com>
2024-06-23 00:50:04 +0200

    Fixes localization PR requirements

--- packages/x-data-grid/src/locales/nnNO.ts
+++ packages/x-data-grid/src/locales/nnNO.ts
@@ -176,4 +176,4 @@ const nnNOGrid: Partial<GridLocaleText> = {
   aggregationFunctionLabelSize: 'størrelse',
 };
 
-export const nbNO: Localization = getGridLocalization(nnNOGrid, nnNOCore);
+export const nnNO: Localization = getGridLocalization(nnNOGrid, nnNOCore);
