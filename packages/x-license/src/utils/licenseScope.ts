7128911a90e6dc45edda302f7d1f3a1b97f710a0
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-06-26 13:04:08 +0200

    [license] Allow usage of charts an tree view pro package for old premium licenses (#13619)
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>

--- packages/x-license/src/utils/licenseScope.ts
+++ packages/x-license/src/utils/licenseScope.ts
@@ -1,20 +1,5 @@
 export const LICENSE_SCOPES = ['pro', 'premium'] as const;
-export const PRODUCT_SCOPES = ['data-grid', 'date-pickers', 'charts', 'tree-view'] as const;
 export const PLAN_VERSIONS = ['initial', 'Q3-2024'] as const;
 
 export type LicenseScope = (typeof LICENSE_SCOPES)[number];
-export type ProductScope = (typeof PRODUCT_SCOPES)[number];
 export type PlanVersion = (typeof PLAN_VERSIONS)[number];
-
-export const extractProductScope = (packageName: string): ProductScope => {
-  // extract the part between "x-" and "-pro"/"-premium"
-  const regex = /x-(.*?)(-pro|-premium)?$/;
-  const match = packageName.match(regex);
-  return match![1] as ProductScope;
-};
-
-export const extractAcceptedScopes = (packageName: string): readonly LicenseScope[] => {
-  return packageName.includes('premium')
-    ? LICENSE_SCOPES.filter((scope) => scope.includes('premium'))
-    : LICENSE_SCOPES;
-};
