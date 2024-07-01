11e939dfa18802c598070706b1c6c48e324f8c4f
Armin Mehinovic <armin@mui.com>
2024-07-01 20:30:59 +0200

    Add custom columns demo with the most common custom renderers

---
+++ docs/data/data-grid/custom-columns/CustomColumnFullExample.js
@@ -0,0 +1,174 @@
+import * as React from 'react';
+import {
+  generateFilledQuantity,
+  randomColor,
+  randomCountry,
+  randomEmail,
+  randomIncoterm,
+  randomInt,
+  randomName,
+  randomRating,
+  randomStatusOptions,
+  renderAvatar,
+  renderCountry,
+  renderEditCountry,
+  renderEditIncoterm,
+  renderEditProgress,
+  renderEditRating,
+  renderEditStatus,
+  renderEmail,
+  renderIncoterm,
+  renderProgress,
+  renderRating,
+  renderStatus,
+} from '@mui/x-data-grid-generator';
+import {
+  COUNTRY_ISO_OPTIONS_SORTED,
+  INCOTERM_OPTIONS,
+  STATUS_OPTIONS,
+} from '@mui/x-data-grid-generator/services/static-data';
+import { SparkLineChart } from '@mui/x-charts';
+import { DataGrid, gridStringOrNumberComparator } from '@mui/x-data-grid';
+
+function GridSparklineCell(params) {
+  if (params.value == null) {
+    return '';
+  }
+
+  return (
+    <SparkLineChart
+      data={params.value}
+      width={params.colDef.computedWidth}
+      plotType="bar"
+    />
+  );
+}
+
+const columns = [
+  {
+    field: 'name',
+    headerName: 'Name',
+    width: 120,
+    editable: true,
+  },
+  {
+    field: 'avatar',
+    headerName: 'Avatar',
+    display: 'flex',
+    renderCell: renderAvatar,
+    valueGetter: (value, row) =>
+      row.name == null || row.avatar == null
+        ? null
+        : { name: row.name, color: row.avatar },
+    sortable: false,
+    filterable: false,
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    renderCell: renderEmail,
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'rating',
+    headerName: 'Rating',
+    display: 'flex',
+    renderCell: renderRating,
+    renderEditCell: renderEditRating,
+    width: 180,
+    type: 'number',
+    editable: true,
+    availableAggregationFunctions: ['avg', 'min', 'max', 'size'],
+  },
+  {
+    field: 'country',
+    headerName: 'Country',
+    type: 'singleSelect',
+    valueOptions: COUNTRY_ISO_OPTIONS_SORTED,
+    valueFormatter: (value) => value?.label,
+    renderCell: renderCountry,
+    renderEditCell: renderEditCountry,
+    sortComparator: (v1, v2, param1, param2) =>
+      gridStringOrNumberComparator(v1.label, v2.label, param1, param2),
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'salary',
+    headerName: 'Salary',
+    type: 'number',
+    valueFormatter: (value) => {
+      if (!value || typeof value !== 'number') {
+        return value;
+      }
+      return `${value.toLocaleString()}$`;
+    },
+    editable: true,
+  },
+  {
+    field: 'monthlyActivity',
+    headerName: 'Monthly activity',
+    type: 'custom',
+    resizable: false,
+    filterable: false,
+    sortable: false,
+    editable: false,
+    groupable: false,
+    display: 'flex',
+    renderCell: GridSparklineCell,
+    width: 150,
+    valueGetter: (value, row) => row.monthlyActivity,
+  },
+  {
+    field: 'budget',
+    headerName: 'Budget left',
+    renderCell: renderProgress,
+    renderEditCell: renderEditProgress,
+    availableAggregationFunctions: ['min', 'max', 'avg', 'size'],
+    type: 'number',
+    width: 120,
+    editable: true,
+  },
+  {
+    field: 'status',
+    headerName: 'Status',
+    renderCell: renderStatus,
+    renderEditCell: renderEditStatus,
+    type: 'singleSelect',
+    valueOptions: STATUS_OPTIONS,
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'incoTerm',
+    headerName: 'Incoterm',
+    renderCell: renderIncoterm,
+    renderEditCell: renderEditIncoterm,
+    type: 'singleSelect',
+    valueOptions: INCOTERM_OPTIONS,
+    editable: true,
+  },
+];
+
+const rows = Array.from({ length: 10 }, (_, index) => ({
+  id: index,
+  name: randomName({}, {}),
+  avatar: randomColor(),
+  email: randomEmail(),
+  rating: randomRating(),
+  country: randomCountry(),
+  salary: randomInt(35000, 80000),
+  monthlyActivity: Array.from({ length: 30 }, () => randomInt(1, 25)),
+  budget: generateFilledQuantity({ quantity: 100 }),
+  status: randomStatusOptions(),
+  incoTerm: randomIncoterm(),
+}));
+
+export default function CustomColumnsFullExample() {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
+  );
+}
