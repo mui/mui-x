742a58aa597d6c01ea747829f444818057d27411
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2024-07-01 15:43:48 +0200

    [charts] Add Heatmap (#13209)

--- packages/x-charts/src/internals/index.ts
+++ packages/x-charts/src/internals/index.ts
@@ -7,6 +7,7 @@ export * from '../ResponsiveChartContainer/ResizableContainer';
 // hooks
 export { useReducedMotion } from '../hooks/useReducedMotion';
 export { useSeries } from '../hooks/useSeries';
+export { useInteractionItemProps } from '../hooks/useInteractionItemProps';
 export { useDrawingArea } from '../hooks/useDrawingArea';
 export { useChartContainerHooks } from '../ChartContainer/useChartContainerHooks';
 export { useScatterChartProps } from '../ScatterChart/useScatterChartProps';
@@ -16,6 +17,7 @@ export { useBarChartProps } from '../BarChart/useBarChartProps';
 // utils
 export * from './defaultizeValueFormatter';
 export * from './configInit';
+export * from './getLabel';
 export * from './getSVGPoint';
 
 // contexts
