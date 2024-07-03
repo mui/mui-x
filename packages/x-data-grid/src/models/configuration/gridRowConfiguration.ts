0380e54163a07d3322142ff590d5760c4b1cf694
Armin Mehinovic <armin@mui.com>
2024-07-03 14:50:40 +0200

    Fix lint issue

--- packages/x-data-grid/src/models/configuration/gridRowConfiguration.ts
+++ packages/x-data-grid/src/models/configuration/gridRowConfiguration.ts
@@ -5,6 +5,7 @@ export type GridRowInternalHook = () => {
    * Get the ARIA attributes for a row
    * @param {GridRowId} rowId The id of the row
    * @param {number} index The position index of the row
+   * @returns {Record<string, string | number | boolean>} The ARIA attributes
    */
   getRowAriaAttributes: (
     rowId: GridRowId,
