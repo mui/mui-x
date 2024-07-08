acd9c07d18cb521572d54727e7615e4a05f2c808
noraleonte <noraleonte00@gmail.com>
2024-07-08 12:46:39 +0300

    fix

--- docs/src/modules/components/overview/WorldMapSvg.tsx
+++ docs/src/modules/components/overview/WorldMapSvg.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { styled, alpha, useTheme } from '@mui/material/styles';
+import { styled, useTheme } from '@mui/material/styles';
 import clsx from 'clsx';
 
 const Continent = styled('g')(({ theme }) => ({
