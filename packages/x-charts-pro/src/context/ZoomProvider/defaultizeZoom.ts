3442bc6645a1c4e2b071dd64248e9e840ead574e
Jose Quintas <juniorquintas@gmail.com>
2024-07-10 16:34:34 +0200

    Remove error handling

--- packages/x-charts-pro/src/context/ZoomProvider/defaultizeZoom.ts
+++ packages/x-charts-pro/src/context/ZoomProvider/defaultizeZoom.ts
@@ -33,8 +33,6 @@ export const defaultizeZoom = (
         };
       }
 
-      checkZoomOptionsErrors(v.zoom);
-
       return {
         axisId: v.id,
         axisDirection,
@@ -46,36 +44,3 @@ export const defaultizeZoom = (
 
   return defaultized;
 };
-
-function checkZoomOptionsErrors(options: ZoomOptions) {
-  const start = options.start ?? defaultZoomOptions.start;
-  const end = options.end ?? defaultZoomOptions.end;
-  const step = options.step ?? defaultZoomOptions.step;
-  const minSpan = options.minSpan ?? defaultZoomOptions.minSpan;
-  const maxSpan = options.maxSpan ?? defaultZoomOptions.maxSpan;
-
-  isBetween(start, 0, 100, 'start');
-  isBetween(end, 0, 100, 'end');
-  isBetween(minSpan, 1, 100, 'minSpan');
-  isBetween(maxSpan, 1, 100, 'maxSpan');
-
-  if (end <= start) {
-    throw new Error('MUI X Charts: The end value must be greater than the start value.');
-  }
-
-  if (step < 1) {
-    throw new Error('MUI X Charts: The step value must be greater than 1.');
-  }
-
-  if (minSpan > maxSpan) {
-    throw new Error(
-      'MUI X Charts: The minSpan value must be less than or equal to the maxSpan value.',
-    );
-  }
-}
-
-function isBetween(value: number, min: number, max: number, name: string) {
-  if (value < min || value > max) {
-    throw new Error(`MUI X Charts: The ${name} value must be between ${min} and ${max}.`);
-  }
-}
