efdacf9b3c653e254a1f831a7bb68e3c286caa97
noraleonte <noraleonte00@gmail.com>
2024-07-11 14:16:46 +0300

    add callback demo

---
+++ docs/data/tree-view/rich-tree-view/editing/EditingCallback.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
+import { TreeItem2 } from '@mui/x-tree-view/TreeItem2';
+import { MUI_X_PRODUCTS } from './products';
+
+export default function EditingCallback() {
+  const [lastEditedItem, setLastEditedItem] = React.useState(null);
+
+  return (
+    <Stack spacing={2} sx={{ width: 400 }}>
+      <Typography>{lastEditedItem || 'No item has been edited yet'}</Typography>
+      <Box sx={{ minHeight: 352, minWidth: 250 }}>
+        <RichTreeView
+          items={MUI_X_PRODUCTS}
+          slots={{ item: TreeItem2 }}
+          isItemEditable={(item) => Boolean(item?.editable)}
+          defaultExpandedItems={['grid', 'pickers']}
+          onItemLabelChange={(itemId, label) => {
+            setLastEditedItem(
+              `The label of item ${itemId} has been edited to ${label}`,
+            );
+          }}
+        />
+      </Box>
+    </Stack>
+  );
+}
--- docs/data/tree-view/rich-tree-view/editing/editing.md
+++ docs/data/tree-view/rich-tree-view/editing/editing.md
@@ -31,6 +31,12 @@ You can limit the editing to just the leaves of the tree.
 
 <!-- {{"demo": "EditLeaves.js"}} -->
 
+## Track item label change
+
+Use the `onItemLabelChange` prop to trigger an action when the label of an item changes.
+
+{{"demo": "EditingCallback.js"}}
+
 ## Change the default behavior
 
 The default behavior on bluring the tree item that is being edited is to save the new value if there is one. This can be customized using the `slotProps` of the `TreeItem2`
