0b986f39536e14c9d0d55960faa4538ef5fd72ad
Jose Quintas <juniorquintas@gmail.com>
2024-07-12 14:28:24 +0200

    prettier

--- packages/x-charts-pro/src/context/ZoomProvider/useSetupZoom.ts
+++ packages/x-charts-pro/src/context/ZoomProvider/useSetupZoom.ts
@@ -94,7 +94,7 @@ export const useSetupZoom = () => {
         clearTimeout(interactionTimeoutRef.current);
       }
       setIsInteracting(true);
-      // Debounce transition to `isInteractive=false`. 
+      // Debounce transition to `isInteractive=false`.
       // Useful because wheel events don't have an "end" event.
       interactionTimeoutRef.current = window.setTimeout(() => {
         setIsInteracting(false);
