402cdea07ae2aef9e71e47a3a115b09d23a72ea0
Carlos De Dios <karudedios@users.noreply.github.com>
2024-07-15 13:43:38 -0600

    [DataGrid] Support `date` and `datetime-local` input types in `GridFilterInputMultipleValue` type (#13411)
    Signed-off-by: Carlos De Dios <karudedios@users.noreply.github.com>
    Co-authored-by: Andrew Cherniavskyi <andrew@mui.com>

--- packages/x-data-grid/src/components/panel/filterPanel/GridFilterInputMultipleValue.tsx
+++ packages/x-data-grid/src/components/panel/filterPanel/GridFilterInputMultipleValue.tsx
@@ -6,7 +6,7 @@ import { useGridRootProps } from '../../../hooks/utils/useGridRootProps';
 import { GridFilterInputValueProps } from './GridFilterInputValueProps';
 
 export type GridFilterInputMultipleValueProps = {
-  type?: 'text' | 'number';
+  type?: 'text' | 'number' | 'date' | 'datetime-local';
 } & GridFilterInputValueProps &
   Omit<AutocompleteProps<string, true, false, true>, 'options' | 'renderInput'>;
 
@@ -113,7 +113,7 @@ GridFilterInputMultipleValue.propTypes = {
     operator: PropTypes.string.isRequired,
     value: PropTypes.any,
   }).isRequired,
-  type: PropTypes.oneOf(['number', 'text']),
+  type: PropTypes.oneOf(['date', 'datetime-local', 'number', 'text']),
 } as any;
 
 export { GridFilterInputMultipleValue };
