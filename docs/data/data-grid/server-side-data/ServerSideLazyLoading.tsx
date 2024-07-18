3de276716a3069ba2ebf7a8ce1ea3c8bc82f9e4c
Armin Mehinovic <armin@mui.com>
2024-07-18 09:27:46 +0200

    Add basic (dev) demo for server side lazy loading

---
+++ docs/data/data-grid/server-side-data/ServerSideLazyLoading.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import {
+  DataGridPro,
+  GridDataSource,
+  GridGetRowsParams,
+} from '@mui/x-data-grid-pro';
+import { useMockServer } from '@mui/x-data-grid-generator';
+
+function ServerSideLazyLoading() {
+  const { columns, fetchRows } = useMockServer(
+    {},
+    { useCursorPagination: false, minDelay: 300, maxDelay: 800 },
+  );
+
+  const dataSource: GridDataSource = React.useMemo(
+    () => ({
+      getRows: async (params: GridGetRowsParams) => {
+        const urlParams = new URLSearchParams({
+          filterModel: encodeURIComponent(JSON.stringify(params.filterModel)),
+          sortModel: encodeURIComponent(JSON.stringify(params.sortModel)),
+          firstRowToRender: encodeURIComponent(JSON.stringify(params.start)),
+          lastRowToRender: encodeURIComponent(JSON.stringify(params.end)),
+        });
+        const getRowsResponse = await fetchRows(
+          `https://mui.com/x/api/data-grid?${urlParams.toString()}`,
+        );
+
+        return {
+          rows: getRowsResponse.rows,
+          rowCount: getRowsResponse.rowCount,
+        };
+      },
+      lazyLoaded: true,
+    }),
+    [fetchRows],
+  );
+
+  return (
+    <div style={{ width: '100%', height: 400 }}>
+      <DataGridPro columns={columns} unstable_dataSource={dataSource} />
+    </div>
+  );
+}
+
+export default ServerSideLazyLoading;
