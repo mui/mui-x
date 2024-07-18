9d1a489a456c4aedd1f67f716bd9be62e20745a7
Armin Mehinovic <armin@mui.com>
2024-07-18 09:29:30 +0200

    Update API docs

--- packages/x-data-grid-premium/src/DataGridPremium/DataGridPremium.tsx
+++ packages/x-data-grid-premium/src/DataGridPremium/DataGridPremium.tsx
@@ -1091,6 +1091,7 @@ DataGridPremiumRaw.propTypes = {
     getChildrenCount: PropTypes.func,
     getGroupKey: PropTypes.func,
     getRows: PropTypes.func.isRequired,
+    lazyLoaded: PropTypes.bool,
     updateRow: PropTypes.func,
   }),
   unstable_dataSourceCache: PropTypes.shape({
--- packages/x-data-grid-pro/src/DataGridPro/DataGridPro.tsx
+++ packages/x-data-grid-pro/src/DataGridPro/DataGridPro.tsx
@@ -990,6 +990,7 @@ DataGridProRaw.propTypes = {
     getChildrenCount: PropTypes.func,
     getGroupKey: PropTypes.func,
     getRows: PropTypes.func.isRequired,
+    lazyLoaded: PropTypes.bool,
     updateRow: PropTypes.func,
   }),
   unstable_dataSourceCache: PropTypes.shape({
