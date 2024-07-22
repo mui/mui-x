3af96db06792cf9532f458806d07975a2ad4f47c
alexandre <alex.fauquette@gmail.com>
2024-07-22 14:47:29 +0200

    fix internal usage of "key"

--- packages/x-charts/src/BarChart/checkScaleErrors.ts
+++ packages/x-charts/src/BarChart/checkScaleErrors.ts
@@ -2,22 +2,22 @@ import { DEFAULT_X_AXIS_KEY, DEFAULT_Y_AXIS_KEY } from '../constants';
 import { AxisDefaultized, AxisId, isBandScaleConfig, isPointScaleConfig } from '../models/axis';
 import { SeriesId } from '../models/seriesType/common';
 
-const getAxisMessage = (axisDirection: 'x' | 'y', axisKey: AxisId) => {
+const getAxisMessage = (axisDirection: 'x' | 'y', axisId: AxisId) => {
   const axisName = `${axisDirection}-axis`;
-  const axisKeyName = `${axisDirection}Axis`;
+  const axisIdName = `${axisDirection}Axis`;
   const axisDefaultKey = axisDirection === 'x' ? DEFAULT_X_AXIS_KEY : DEFAULT_Y_AXIS_KEY;
-  return axisKey === axisDefaultKey
-    ? `The first \`${axisKeyName}\``
-    : `The ${axisName} with id "${axisKey}"`;
+  return axisId === axisDefaultKey
+    ? `The first \`${axisIdName}\``
+    : `The ${axisName} with id "${axisId}"`;
 };
 
 export function checkScaleErrors(
   verticalLayout: boolean,
   seriesId: SeriesId,
   xAxisId: AxisId,
-  xAxis: { [axisKey: AxisId]: AxisDefaultized },
+  xAxis: { [axisId: AxisId]: AxisDefaultized },
   yAxisId: AxisId,
-  yAxis: { [axisKey: AxisId]: AxisDefaultized },
+  yAxis: { [axisId: AxisId]: AxisDefaultized },
 ): void {
   const xAxisConfig = xAxis[xAxisId];
   const yAxisConfig = yAxis[yAxisId];
