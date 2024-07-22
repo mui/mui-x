248b69a0b18d7878903d800a365168fec6ce8e39
Armin Mehinovic <armin@mui.com>
2024-07-22 11:22:38 +0200

    Lazy loading flag moved to the root props instead of it being a part of the data source model

--- packages/x-data-grid-pro/src/DataGridPro/DataGridPro.tsx
+++ packages/x-data-grid-pro/src/DataGridPro/DataGridPro.tsx
@@ -484,6 +484,13 @@ DataGridProRaw.propTypes = {
    */
   keepNonExistentRowsSelected: PropTypes.bool,
   /**
+   * Used together with `unstable_dataSource` to enable lazy loading.
+   * If enabled, the grid will stop adding `paginationModel` to the data requests (`getRows`) and start sending `start` and `end` values depending on the scroll position.
+   * A new request will be made whenever the user scrolls to the area that has skeleton rows.
+   * @default false
+   */
+  lazyLoading: PropTypes.bool,
+  /**
    * If `true`, a loading overlay is displayed.
    * @default false
    */
@@ -998,7 +1005,6 @@ DataGridProRaw.propTypes = {
     getChildrenCount: PropTypes.func,
     getGroupKey: PropTypes.func,
     getRows: PropTypes.func.isRequired,
-    lazyLoaded: PropTypes.bool,
     updateRow: PropTypes.func,
   }),
   unstable_dataSourceCache: PropTypes.shape({
