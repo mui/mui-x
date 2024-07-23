ee5ffabf8b876f42a38920bdc5625f21684861ac
Jose Quintas <juniorquintas@gmail.com>
2024-07-23 13:00:24 +0200

    create shared config

--- test/unit-date-pickers/.mocharc.js
+++ test/unit-date-pickers/.mocharc.js
@@ -1,18 +1,13 @@
+const mochaConfig = require('../shared/setup/mochaConfig');
+
+const { resolvePackagesDir, sharedConfig } = mochaConfig;
+
 module.exports = {
-  extension: ['js', 'ts', 'tsx'],
-  ignore: ['**/build/**', '**/node_modules/**'],
-  recursive: true,
-  timeout: (process.env.CIRCLECI === 'true' ? 5 : 2) * 1000, // Circle CI has low-performance CPUs.
-  reporter: 'dot',
+  ...sharedConfig,
   require: [
     require.resolve('../shared/setup/setupBabel'),
     require.resolve('../shared/setup/setupJSDOM'),
     require.resolve('./setup/setupJSDOM'),
   ],
-  'watch-ignore': [
-    '**/node_modules/**',
-    // Unrelated directories with a large number of files
-    '**/build/**',
-  ],
-  spec: ['../../packages/x-date-pickers{,-pro}/**/*.test.{js,ts,tsx}'],
+  spec: [resolvePackagesDir('./packages/x-date-pickers{,-pro}/**/*.test.{js,ts,tsx}', __dirname)],
 };
