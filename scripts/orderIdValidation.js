eb4e0be066d0f403f2485e0256e1eabafefb4cba
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2024-07-24 16:35:35 +0200

    [infra] Fix order id validator action (#13971)
    Signed-off-by: Michel Engelen <32863416+michelengelen@users.noreply.github.com>
    Co-authored-by: Jose C Quintas Jr <juniorquintas@gmail.com>

--- scripts/orderIdValidation.js
+++ scripts/orderIdValidation.js
@@ -1,33 +1,19 @@
 /**
- * We import these just for type checking as the actual imports
- * are passed as arguments to the function
- * https://github.com/marketplace/actions/github-script#run-a-separate-file
+ * @param {Object} params
+ * @param {import("@actions/core")} params.core
+ * @param {ReturnType<import("@actions/github").getOctokit>} params.github
+ * @param {import("@actions/github").context} params.context
  */
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const core = require('@actions/core');
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const github = require('@actions/github');
-
-/**
- *
- * @param {core} core
- * @param {github} github
- */
-// eslint-disable-next-line @typescript-eslint/no-shadow
-module.exports = async (core, github) => {
+module.exports = async ({ core, context, github }) => {
   try {
-    const owner = process.env.OWNER;
-    const repo = process.env.REPO;
-    const issueNumber = process.env.ISSUE_NUMBER;
-    const token = process.env.TOKEN;
+    const owner = context.repo.owner;
+    const repo = context.repo.repo;
+    const issueNumber = context.issue.number;
 
     const orderApiToken = process.env.ORDER_API_TOKEN;
     const orderApi = 'https://store-wp.mui.com/wp-json/wc/v3/orders/';
 
-    const octokit = github.getOctokit(token);
-
-    const issue = octokit.rest.issues.get({
+    const issue = await github.rest.issues.get({
       owner,
       repo,
       issue_number: issueNumber,
@@ -59,13 +45,13 @@ module.exports = async (core, github) => {
       const planName = plan.match(/\b(pro|premium)\b/i)[0].toLowerCase();
       const labelName = `support: ${planName}`;
 
-      const label = octokit.rest.issues.getLabel({
+      const label = await github.rest.issues.getLabel({
         owner,
         repo,
         name: labelName,
       });
 
-      octokit.rest.issues.addLabels({
+      await github.rest.issues.addLabels({
         owner,
         repo,
         issue_number: issueNumber,
