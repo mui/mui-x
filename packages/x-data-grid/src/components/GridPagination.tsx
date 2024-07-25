696beeac22f843f90d4bd1cd95f5bb8a2c96d640
Rom Grk <romgrk.cc@gmail.com>
2024-07-25 15:19:59 -0400

    fix: pagination overflow

--- packages/x-data-grid/src/components/GridPagination.tsx
+++ packages/x-data-grid/src/components/GridPagination.tsx
@@ -16,6 +16,8 @@ import {
 } from '../hooks/features/pagination/gridPaginationSelector';
 
 const GridPaginationRoot = styled(TablePagination)(({ theme }) => ({
+  maxHeight: '100%',
+
   [`& .${tablePaginationClasses.selectLabel}`]: {
     display: 'none',
     [theme.breakpoints.up('sm')]: {
