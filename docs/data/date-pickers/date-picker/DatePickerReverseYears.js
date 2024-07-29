487fc9bfe50efa59a9e9bafb96335950ae74f1af
Thomas Moon <thomas.moon@teamit.fi>
2024-07-30 00:31:25 +0300

    [pickers] Improve picker example, revert to year view only

--- docs/data/date-pickers/date-picker/DatePickerReverseYears.js
+++ docs/data/date-pickers/date-picker/DatePickerReverseYears.js
@@ -1,17 +1,21 @@
 import * as React from 'react';
+import dayjs from 'dayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 export default function DatePickerReverseYears() {
+  const currentYear = dayjs()
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DatePicker']}>
         <DatePicker
           label="Years in reverse"
+          maxDate={currentYear}
           openTo="year"
           reverseYears
+          views={["year"]}
         />
       </DemoContainer>
     </LocalizationProvider>
