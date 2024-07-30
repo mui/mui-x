acfcb3b90fedad413dc15ec4cdbb8fdd83b35f27
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-07-30 13:53:30 +0200

    [TreeView] Add new prop `onItemClick` on the Tree View components (#14018)

--- docs/data/tree-view/rich-tree-view/selection/TrackItemSelectionToggle.tsx
+++ docs/data/tree-view/rich-tree-view/selection/TrackItemSelectionToggle.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
 import { TreeViewBaseItem } from '@mui/x-tree-view/models';
-import Typography from '@mui/material/Typography';
 
 const MUI_X_PRODUCTS: TreeViewBaseItem[] = [
   {
--- docs/data/tree-view/simple-tree-view/items/OnItemClick.js
+++ docs/data/tree-view/simple-tree-view/items/OnItemClick.js
@@ -5,24 +5,18 @@ import Typography from '@mui/material/Typography';
 import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 
-export default function TrackItemSelectionToggle() {
-  const [lastSelectedItem, setLastSelectedItem] = React.useState(null);
-
-  const handleItemSelectionToggle = (event, itemId, isSelected) => {
-    if (isSelected) {
-      setLastSelectedItem(itemId);
-    }
-  };
+export default function OnItemClick() {
+  const [lastClickedItem, setLastClickedItem] = React.useState(null);
 
   return (
     <Stack spacing={2}>
       <Typography>
-        {lastSelectedItem == null
-          ? 'No item selection recorded'
-          : `Last selected item: ${lastSelectedItem}`}
+        {lastClickedItem == null
+          ? 'No item click recorded'
+          : `Last clicked item: ${lastClickedItem}`}
       </Typography>
       <Box sx={{ minHeight: 352, minWidth: 300 }}>
-        <SimpleTreeView onItemSelectionToggle={handleItemSelectionToggle}>
+        <SimpleTreeView onItemClick={(event, itemId) => setLastClickedItem(itemId)}>
           <TreeItem itemId="grid" label="Data Grid">
             <TreeItem itemId="grid-community" label="@mui/x-data-grid" />
             <TreeItem itemId="grid-pro" label="@mui/x-data-grid-pro" />
