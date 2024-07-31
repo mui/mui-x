e7f7393a3328c7d663d7c1a45a94d625d03fcae6
Jose Quintas <juniorquintas@gmail.com>
2024-07-31 13:19:21 +0200

    fix types

--- packages/x-charts/src/context/PluginProvider/usePluginsMerge.ts
+++ packages/x-charts/src/context/PluginProvider/usePluginsMerge.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ChartsPluginType, ColorProcessorsConfig, ExtremumGettersConfig } from '../../models';
+import { ChartsPluginType, ColorProcessorsConfig, ExtremumGettersConfig } from './Plugin.types';
 import { ChartSeriesType } from '../../models/seriesType/config';
 import { SeriesFormatterConfig } from '../SeriesProvider';
 import { defaultPlugins } from '../../ChartContainer/defaultPlugins';
@@ -18,16 +18,16 @@ export function usePluginsMerge<T extends ChartSeriesType>(plugins?: ChartsPlugi
 
       // To remove those any we will need to solve this union discrimination issue:
       // https://www.typescriptlang.org/play/?#code/FDAuE8AcFMAIDkCuBbARtATgYQPYDsAzASwHNYBeWAb2FlgGsi8ATALlgHI8V0MOBuWrBwwMAQ1A4M7ABQAPdtzSYAlBQB8sJb0EBfEBBiwAyqAxMSuQqQrUhjFuw4BnMxYFCRmCVNkLYruZ4JGrkmoEWeiAAxviuWqhWxCTsSMrY+Mm2VAxMbLAARNqYBQA0wqI+0rByGrAATLAAVDWw+rF48YFJpOymQZaZNpQ5DvkFEcFlFd6S1bVhsAAG9S0AJFRyukttMXGgsB3JzrYA2niJQyTl3VcAugZQcADylXPOALJikJAW2ULFDAAflSPEwPRIpw4XnEcw4d1KQkmJBBJjcwQhUJhVXhiN0gmAHXi2LmXx+FnYr1mUk+31+wWy+JABCksBkABtoAcjjYcARDldnGoaCA6AB6MWwADqUnoJxw9FgRH5AHc4L9ooroGJogALQ5iZxwPJEABuRGYiDE7PASJVRFAerZPJIADoxsKhHRooa4FwwXxWF66DNYVIyfTIS73Xk7rZoySpIIQyHUBhtfRkyGfUbOMiOEGU3RExgIxZTtGxnHKAm3kng8xoAQxIh2aBC0W0xms-pvftqLkWOUS2141chBLYABJDimuB4HBKxtiWBiVA4RAHXU4FWwSSwTkHAAqxlgiBYmFcYhYAusbrGq5vtepGFX6YPTHo0GYnjrpbp5ZVrYJZ6EAA
-      seriesFormatters[plugin.seriesType] = plugin.seriesFormatter as any;
+      seriesFormatters[plugin.seriesType as ChartSeriesType] = plugin.seriesFormatter as any;
 
-      colorProcessors[plugin.seriesType] = plugin.colorProcessor as any;
+      colorProcessors[plugin.seriesType as ChartSeriesType] = plugin.colorProcessor as any;
 
       if (plugin.xExtremumGetter) {
-        xExtremumGetters[plugin.seriesType] = plugin.xExtremumGetter as any;
+        xExtremumGetters[plugin.seriesType as ChartSeriesType] = plugin.xExtremumGetter as any;
       }
 
       if (plugin.yExtremumGetter) {
-        yExtremumGetters[plugin.seriesType] = plugin.yExtremumGetter as any;
+        yExtremumGetters[plugin.seriesType as ChartSeriesType] = plugin.yExtremumGetter as any;
       }
     }
     return {
