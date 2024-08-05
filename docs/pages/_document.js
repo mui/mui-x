73b64a7bc4347812ec6f0f675b8e1b177d1a9f68
Andrew Cherniavskyi <andrew@mui.com>
2024-08-05 13:10:54 +0200

    fix import order

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -1,6 +1,7 @@
-import MyDocument from '@mui/monorepo/docs/pages/_document';
 import packageJson from '@mui/joy/package.json';
 
 console.log('@mui/joy/package.json', packageJson);
+// eslint-disable-next-line import/first
+import MyDocument from '@mui/monorepo/docs/pages/_document';
 
 export default MyDocument;
