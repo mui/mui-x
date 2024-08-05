6c45d83ddf7d363e71cf9ae80462fa3cd6f00d39
Andrew Cherniavskyi <andrew@mui.com>
2024-08-05 13:56:18 +0200

    add InitColorSchemeScript import

--- docs/pages/_document2.js
+++ docs/pages/_document2.js
@@ -1,4 +1,7 @@
 import packageJson from '@mui/joy/package.json' with { type: 'json' };
+import JoyInitColorSchemeScript from '@mui/joy/InitColorSchemeScript';
 
 // eslint-disable-next-line no-console
 console.log('@mui/joy/package.json', packageJson);
+// eslint-disable-next-line no-console
+console.log('JoyInitColorSchemeScript', typeof JoyInitColorSchemeScript);
