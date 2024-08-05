0a72fd5515379653af190d242340199497f5304d
Andrew Cherniavskyi <andrew@mui.com>
2024-08-05 13:47:16 +0200

    log @mui/joy/package.json

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -1,7 +1,3 @@
-import packageJson from '@mui/joy/package.json';
-
-console.log('@mui/joy/package.json', packageJson);
-// eslint-disable-next-line import/first
 import MyDocument from '@mui/monorepo/docs/pages/_document';
 
 export default MyDocument;
---
+++ docs/pages/_document2.js
@@ -0,0 +1,4 @@
+import packageJson from '@mui/joy/package.json' with { type: 'json' };
+
+// eslint-disable-next-line no-console
+console.log('@mui/joy/package.json', packageJson);
