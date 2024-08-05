9e546d814d818c6af60e3e034f90abe3e424c513
Andrew Cherniavskyi <andrew@mui.com>
2024-08-05 13:04:57 +0200

    log @mui/joy/package.json

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -1,3 +1,6 @@
 import MyDocument from '@mui/monorepo/docs/pages/_document';
+import packageJson from '@mui/joy/package.json';
+
+console.log('@mui/joy/package.json', packageJson);
 
 export default MyDocument;
