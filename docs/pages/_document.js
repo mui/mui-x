cbaa80716e532fa44c6aa9d7606bf956598048f0
Andrew Cherniavskyi <andrew@mui.com>
2024-08-05 14:52:00 +0200

    try importing InitColorSchemeScript from MUI X _document

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -1,3 +1,8 @@
+import JoyInitColorSchemeScript from '@mui/joy/InitColorSchemeScript';
+// eslint-disable-next-line no-console
+console.log('_document JoyInitColorSchemeScript', typeof JoyInitColorSchemeScript);
+
+// eslint-disable-next-line import/first
 import MyDocument from '@mui/monorepo/docs/pages/_document';
 
 export default MyDocument;
