1dd97fdb1f97875887f3bc42071d08d0c816222c
Arthur Balduini <arthurbalduini@Arthurs-MacBook-Pro.local>
2024-08-08 09:58:45 +0200

    prettier

--- docs/data/date-pickers/validation/DateValidationDisableNonContiguousRange.js
+++ docs/data/date-pickers/validation/DateValidationDisableNonContiguousRange.js
@@ -4,22 +4,21 @@ import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
 const shouldDisableDate = (value) => value.date() === 6;
 const start = dayjs().date(4);
 const end = dayjs().date(7);
 
-export default function DateValidationShouldDisableYear() {
+export default function DateValidationDisableNonContiguousRange() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DateRangePicker']}>
         <DemoItem label="DateRangePicker">
-          <DateRangePicker 
+          <DateRangePicker
             defaultValue={[start, end]}
-            disableNonContiguousRanges={true}
-            shouldDisableDate={shouldDisableDate} 
-          />        
+            disableNonContiguousRanges
+            shouldDisableDate={shouldDisableDate}
+          />
         </DemoItem>
       </DemoContainer>
     </LocalizationProvider>
