a633c1d061122aca16dc29d2d5ae1b770f3bd01c
Armin Mehinovic <armin@mui.com>
2024-08-08 08:40:57 +0200

    Add hook to the premium package

--- packages/x-data-grid-premium/src/DataGridPremium/useDataGridPremiumComponent.tsx
+++ packages/x-data-grid-premium/src/DataGridPremium/useDataGridPremiumComponent.tsx
@@ -61,6 +61,7 @@ import {
   columnGroupsStateInitializer,
   useGridLazyLoader,
   useGridLazyLoaderPreProcessors,
+  useGridDataSourceLazyLoader,
   headerFilteringStateInitializer,
   useGridHeaderFiltering,
   virtualizationStateInitializer,
@@ -175,6 +176,7 @@ export const useDataGridPremiumComponent = (
   useGridScroll(apiRef, props);
   useGridInfiniteLoader(apiRef, props);
   useGridLazyLoader(apiRef, props);
+  useGridDataSourceLazyLoader(apiRef, props);
   useGridColumnMenu(apiRef);
   useGridCsvExport(apiRef, props);
   useGridPrintExport(apiRef, props);
