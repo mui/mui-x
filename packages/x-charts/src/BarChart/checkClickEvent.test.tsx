bea8ef630019ffc59fd81dfd444bbdb651a188c0
alex <alex.fauquette@gmail.com>
2024-08-08 16:14:16 +0200

    add tests

--- packages/x-charts/src/BarChart/checkClickEvent.test.tsx
+++ packages/x-charts/src/BarChart/checkClickEvent.test.tsx
@@ -14,21 +14,21 @@ function firePointerEvent(
     x: 0,
     y: 0,
     bottom: 0,
-    height: 400,
+    height: 0,
     left: 0,
     right: 0,
     top: 0,
-    width: 400,
+    width: 0,
     toJSON() {
       return {
         x: 0,
         y: 0,
         bottom: 0,
-        height: 400,
+        height: 0,
         left: 0,
         right: 0,
         top: 0,
-        width: 400,
+        width: 0,
       };
     },
   });
@@ -62,22 +62,68 @@ const config = {
 // | X X X X
 // ---A---B-
 
+const isJSDOM = /jsdom/.test(window.navigator.userAgent);
+
 describe('BarChart - click event', () => {
   const { render } = createRenderer();
 
-  it('should provide the accurate context as second argument', function test() {
-    if (/jsdom/.test(window.navigator.userAgent)) {
-      // can't do Pointer event with JSDom https://github.com/jsdom/jsdom/issues/2527
-      this.skip();
-    }
-    const onAxisClick = spy();
-    render(
-      <div
-        style={{
-          width: 400,
-          height: 400,
-        }}
-      >
+  describe('onAxisClick', () => {
+    it('should provide the accurate context as second argument', function test() {
+      if (isJSDOM) {
+        // can't do Pointer event with JSDom https://github.com/jsdom/jsdom/issues/2527
+        this.skip();
+      }
+      const onAxisClick = spy();
+      render(
+        <div
+          style={{
+            margin: -8, // No idea why, but that make the SVG coordinates match the HTML coordinates
+            width: 400,
+            height: 400,
+          }}
+        >
+          <BarChart
+            {...config}
+            series={[
+              { dataKey: 'v1', id: 's1' },
+              { dataKey: 'v2', id: 's2' },
+            ]}
+            xAxis={[{ scaleType: 'band', dataKey: 'x' }]}
+            onAxisClick={onAxisClick}
+          />
+        </div>,
+      );
+      const svg = document.querySelector<HTMLElement>('svg')!;
+
+      firePointerEvent(svg, 'pointermove', {
+        clientX: 198,
+        clientY: 60,
+      });
+      fireEvent.click(svg);
+
+      expect(onAxisClick.lastCall.args[1]).to.deep.equal({
+        dataIndex: 0,
+        axisValue: 'A',
+        seriesValues: { s1: 4, s2: 2 },
+      });
+
+      firePointerEvent(svg, 'pointermove', {
+        clientX: 201,
+        clientY: 60,
+      });
+      fireEvent.click(svg);
+
+      expect(onAxisClick.lastCall.args[1]).to.deep.equal({
+        dataIndex: 1,
+        axisValue: 'B',
+        seriesValues: { s1: 1, s2: 1 },
+      });
+    });
+  });
+
+  describe('onItemClick', () => {
+    it('should add cursor="pointer" to bar elements', function test() {
+      render(
         <BarChart
           {...config}
           series={[
@@ -85,22 +131,64 @@ describe('BarChart - click event', () => {
             { dataKey: 'v2', id: 's2' },
           ]}
           xAxis={[{ scaleType: 'band', dataKey: 'x' }]}
-          onAxisClick={onAxisClick}
-        />
-      </div>,
-    );
-    const svg = document.querySelector<HTMLElement>('svg')!;
+          onItemClick={() => {}}
+        />,
+      );
+      const rectangles = document.querySelectorAll<HTMLElement>('rect.MuiBarElement-root');
 
-    firePointerEvent(svg, 'pointermove', {
-      clientX: 201,
-      clientY: 60,
+      expect(
+        Array.from(rectangles).map((rectangle) => rectangle.getAttribute('cursor')),
+      ).to.deep.equal(['pointer', 'pointer', 'pointer', 'pointer']);
     });
-    fireEvent.click(svg);
 
-    expect(onAxisClick.lastCall.args[1]).to.deep.equal({
-      dataIndex: 0,
-      axisValue: 'A',
-      seriesValues: { s1: 4, s2: 2 },
+    it('should provide the accurate context as second argument', function test() {
+      if (isJSDOM) {
+        // can't do Pointer event with JSDom https://github.com/jsdom/jsdom/issues/2527
+        this.skip();
+      }
+      const onItemClick = spy();
+      render(
+        <div
+          style={{
+            margin: -8, // No idea why, but that make the SVG coordinates match the HTML coordinates
+            width: 400,
+            height: 400,
+          }}
+        >
+          <BarChart
+            {...config}
+            series={[
+              { dataKey: 'v1', id: 's1' },
+              { dataKey: 'v2', id: 's2' },
+            ]}
+            xAxis={[{ scaleType: 'band', dataKey: 'x' }]}
+            onItemClick={onItemClick}
+          />
+        </div>,
+      );
+
+      const rectangles = document.querySelectorAll<HTMLElement>('rect.MuiBarElement-root');
+
+      fireEvent.click(rectangles[0]);
+      expect(onItemClick.lastCall.args[1]).to.deep.equal({
+        type: 'bar',
+        seriesId: 's1',
+        dataIndex: 0,
+      });
+
+      fireEvent.click(rectangles[1]);
+      expect(onItemClick.lastCall.args[1]).to.deep.equal({
+        type: 'bar',
+        seriesId: 's1',
+        dataIndex: 1,
+      });
+
+      fireEvent.click(rectangles[2]);
+      expect(onItemClick.lastCall.args[1]).to.deep.equal({
+        type: 'bar',
+        seriesId: 's2',
+        dataIndex: 0,
+      });
     });
   });
 });
