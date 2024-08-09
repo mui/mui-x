7c9e96a19545110df4ac3ac9e19ee48fe84233ca
Jose Quintas <juniorquintas@gmail.com>
2024-08-09 15:31:11 +0200

    Fix scripts

--- docs/data/charts/lines/AreaBaseline.js
+++ docs/data/charts/lines/AreaBaseline.js
@@ -7,7 +7,7 @@ export default function AreaBaseline() {
       xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
       series={[
         {
-          data: [2, -5.5, 2, -8.5, 1.5, 5],
+          data: [2, -5.5, 2, -7.5, 1.5, 6],
           area: true,
           baseline: -10,
         },
