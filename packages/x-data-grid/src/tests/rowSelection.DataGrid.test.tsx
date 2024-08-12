080e02cb6f1fa9d954a913824383f9fc43425b4f
Andrew Cherniavskyi <andrew@mui.com>
2024-08-12 17:58:51 +0200

    fix test

--- packages/x-data-grid/src/tests/rowSelection.DataGrid.test.tsx
+++ packages/x-data-grid/src/tests/rowSelection.DataGrid.test.tsx
@@ -545,7 +545,9 @@ describe('<DataGrid /> - Row selection', () => {
         fireEvent.keyDown(cell, { key: 'ArrowLeft' });
         fireEvent.keyDown(getCell(1, 0).querySelector('input')!, { key: 'ArrowUp' });
         clock.runToLast(); // Wait for transition
-        expect(document.querySelectorAll('.MuiTouchRipple-rippleVisible')).to.have.length(0);
+        waitFor(() =>
+          expect(document.querySelectorAll('.MuiTouchRipple-rippleVisible')).to.have.length(1),
+        );
       });
     });
   });
