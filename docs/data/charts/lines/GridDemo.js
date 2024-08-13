c9bf6fc79d3d70c76cb7f9607d1a89e890079056
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2024-08-13 15:09:53 +0200

    [docs] Extract dataset in the Line chart docs (#14034)

--- docs/data/charts/lines/GridDemo.js
+++ docs/data/charts/lines/GridDemo.js
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
+import { dataset } from './basicDataset';
 
 export default function GridDemo() {
   return (
     <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
+      dataset={dataset}
+      xAxis={[{ dataKey: 'x' }]}
+      series={[{ dataKey: 'y' }]}
       height={300}
       margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
       grid={{ vertical: true, horizontal: true }}
