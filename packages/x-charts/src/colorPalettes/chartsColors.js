00572df7edcdbea23483aaeb6c5b8f827dd61cfc
noraleonte <noraleonte00@gmail.com>
2024-08-15 16:43:19 +0300

    add palettes

---
+++ packages/x-charts/src/colorPalettes/chartsColors.js
@@ -0,0 +1,160 @@
+// Sequential
+// Cold tones
+
+const purples = [
+  '#CAD4EE',
+  '#98ADE5',
+  '#577EE3',
+  '#4254FB',
+  '#2638DF',
+  '#222FA6',
+  '#111C7F',
+  '#091159',
+];
+const blues = [
+  '#BDDEFF',
+  '#99CCFF',
+  '#66B2FF',
+  '#2E96FF',
+  '#0064D6',
+  '#0D47A1',
+  '#0A367B',
+  '#072555',
+];
+const greens = [
+  '#CDEBDD',
+  '#B2E2CB',
+  '#8FD8B5',
+  '#54C690',
+  '#31B375',
+  '#359F6D',
+  '#0F7746',
+  '#065731',
+];
+const cyans = [
+  '#CFE9E8',
+  '#A3DAD8',
+  '#7ED0CE',
+  '#44BDBA',
+  '#299896',
+  '#137370',
+  '#0E5A58',
+  '#073938',
+];
+
+// Warm tones
+const pinks = [
+  '#F7D2E1',
+  '#F6BED5',
+  '#F4A0C3',
+  '#F6619F',
+  '#EE448B',
+  '#E32977',
+  '#B6215F',
+  '#8B1F4C',
+];
+const reds = [
+  '#FAE0E0',
+  '#F7C0BF',
+  '#F3A2A0',
+  '#EF5350',
+  '#E53935',
+  '#DC2B27',
+  '#860B08',
+  '#560503 ',
+];
+
+// categorical
+
+// blueberry twilight - default (same as dark)
+const blueberryTwilightPalette = [
+  '#6061fc',
+  '#2DA2FF',
+  '#e778f9',
+  '#ebd28c',
+  '#84c3ff',
+  '#6d5cd4',
+  '#E7A3F2',
+  '#44BDBA',
+  '#aaabf7',
+  '#AFAFAF',
+  '#179EA7',
+  '#414FCF',
+];
+
+// banana milkshake (same as dark)
+const bananaMilkshake = [
+  '#577EE3',
+  '#F66170',
+  '#FFC13B',
+  '#44BDBA',
+  '#414FCF',
+  '#F3CD80',
+  '#F287B3',
+  '#FE9C41',
+  '#F1BF91',
+  '#A89787',
+];
+
+// rainbow surge (same as dark)
+const rainbowSurgePaletteLight = [
+  '#4254FB',
+  '#2E96FF',
+  '#54C690',
+  '#FFB219',
+  '#FF7A19',
+  '#F73B4B',
+  '#F287B3',
+];
+
+// mango fusion -> did not change
+const mangoFusionPaletteLight = [
+  '#FF7E35',
+  '#DB2671',
+  '#577EE3',
+  '#FFB03C',
+  '#9C2A59',
+  '#4254FB',
+  '#F6619F',
+  '#2EC4C2',
+  '#F3CD80',
+  '#202B93',
+];
+const mangoFusionPaletteDark = [
+  '#FFD771',
+  '#DE196B',
+  '#2E96FF',
+  '#41698F',
+  '#DA00FF',
+  '#72CCFF',
+  '#9001CB',
+  '#19D0CD',
+  '#FC5F5C',
+  '#3B48E0',
+];
+
+// cheerful fiesta -> did not change
+const cheerfulFiestaPaletteDark = [
+  '#0059B2',
+  '#2E96FF',
+  '#FFC24C',
+  '#FF9F0E',
+  '#F38200',
+  '#2ABFDE',
+  '#1F94AD',
+  '#BD2C38',
+  '#FF3143',
+  '#FF8282',
+];
+const cheerfulFiestaPaletteLight = [
+  '#003A75',
+  '#007FFF',
+  '#FFC24C',
+  '#FF9D09',
+  '#CA6C00',
+  '#127D94',
+  '#1F94AD',
+  '#C82634',
+  '#FF3143',
+  '#FF7E7E',
+];
