c15ddf72346707f54c596cfc6a24ad78fe7b6fd0
alex <alex.fauquette@gmail.com>
2024-08-19 11:21:22 +0200

    imporve example

--- docs/data/charts/zoom-and-pan/ZoomFilterMode.js
+++ docs/data/charts/zoom-and-pan/ZoomFilterMode.js
@@ -1,54 +1,27 @@
 import * as React from 'react';
 import { BarChartPro } from '@mui/x-charts-pro/BarChartPro';
+import { dataset, valueFormatter } from './letterFrequency';
 
 export default function ZoomFilterMode() {
   return (
     <BarChartPro
+      dataset={dataset}
       xAxis={[
         {
           scaleType: 'band',
-          data: axisData,
+          data: dataset.map((item) => item.letter),
+          // dataKey: 'letter',
           zoom: { filterMode: 'discard' },
         },
       ]}
+      yAxis={[{ valueFormatter }]}
+      series={[{ label: 'Letter Frequency', dataKey: 'frequency', valueFormatter }]}
       {...chartProps}
     />
   );
 }
 
-const axisData = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-];
-
 const chartProps = {
   width: 600,
   height: 300,
-  series: [
-    {
-      label: 'Series A',
-      data: Array.from({ length: 10 }).flatMap((v, i) => [
-        (i + i + 1) ** 2,
-        (i + i + 1) ** 2 + 5,
-      ]),
-    },
-  ],
 };
