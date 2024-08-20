e2a4605c750e0e67028fade868e4f04a3cfee1bd
Bilal Shafi <bilalshafidev@gmail.com>
2024-08-20 17:06:45 +0500

    Add a couple of demos

---
+++ docs/data/data-grid/row-spanning/RowSpanningCustom.js
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
+export default function RowSpanningCustom() {
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        showCellVerticalBorder
+        pageSizeOptions={[10]}
+        disableRowSelectionOnClick
+        unstable_rowSpanning
+        disableVirtualization
+        sx={{
+          '& .MuiDataGrid-row.Mui-hovered': {
+            backgroundColor: 'transparent',
+          },
+          '& .MuiDataGrid-row:hover': {
+            backgroundColor: 'transparent',
+          },
+        }}
+      />
+    </Box>
+  );
+}
+
+const columns = [
+  {
+    field: 'name',
+    headerName: 'Name',
+    width: 200,
+    editable: true,
+  },
+  {
+    field: 'designation',
+    headerName: 'Designation',
+    width: 200,
+    editable: true,
+  },
+  {
+    field: 'department',
+    headerName: 'Department',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 100,
+    valueGetter: (value) => {
+      return `${value} yo`;
+    },
+    rowSpanValueGetter: (value, row) => {
+      console.log(row);
+      return row ? `${row.name}-${row.age}` : value;
+    },
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    name: 'George Floyd',
+    designation: 'React Engineer',
+    department: 'Engineering',
+    age: 25,
+  },
+  {
+    id: 2,
+    name: 'George Floyd',
+    designation: 'Technical Interviewer',
+    department: 'Human resource',
+    age: 25,
+  },
+  {
+    id: 3,
+    name: 'Cynthia Duke',
+    designation: 'Technical Team Lead',
+    department: 'Engineering',
+    age: 25,
+  },
+  {
+    id: 4,
+    name: 'Jordyn Black',
+    designation: 'React Engineer',
+    department: 'Engineering',
+    age: 31,
+  },
+  {
+    id: 5,
+    name: 'Rene Glass',
+    designation: 'Ops Lead',
+    department: 'Operations',
+    age: 31,
+  },
+];
--- docs/data/data-grid/row-spanning/row-spanning.md
+++ docs/data/data-grid/row-spanning/row-spanning.md
@@ -1,17 +1,34 @@
 # Data Grid - Row spanning
 
-<p class="description">Span cells across several columns.</p>
+<p class="description">Span cells across several rows.</p>
 
-Each cell takes up the width of one row.
+Each cell takes up the height of one row.
 Row spanning lets you change this default behavior, so cells can span multiple rows.
 This is very close to the "row spanning" in an HTML `<table>`.
 
 To enable, pass the `unstable_rowSpanning` prop to the Data Grid.
 
 The Data Grid will automatically merge cells with the same value in a specified column.
-Additionally, you could customize the value used in row spanning computation using `colDef.rowSpanValueGetter` prop and both the value used in row spanning computation and the value used in cell using `colDef.valueGetter` prop.
+
+In the following example, the row spanning causes the cells with the same values in a column to be merged.
 
 {{"demo": "RowSpanning.js", "bg": "inline", "defaultCodeOpen": false}}
+
+## Customizing row spanned cells
+
+You could customize the value used in row spanning computation using `colDef.rowSpanValueGetter` prop and both the value used in row spanning computation and the value used in cell using `colDef.valueGetter` prop.
+
+This could be useful when there _are_ some repeating values but they belong to different groups.
+
+In the following example, `rowSpanValueGetter` is used to avoid merging `age` cells that do not belong to the same person.
+
+{{"demo": "RowSpanningCustom.js", "bg": "inline", "defaultCodeOpen": false}}
+
+## Demo
+
+Here's the calender demo that you can see in the column spanning [documentation](/x/react-data-grid/column-spanning/#function-signature), but implemented with row spanning.
+
+{{"demo": "RowSpanningCalender.js", "bg": "inline", "defaultCodeOpen": false}}
 
 ## API
 
