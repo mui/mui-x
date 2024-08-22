40b62b9f9536cd8ceeb497987686950022058109
Jose Quintas <juniorquintas@gmail.com>
2024-08-22 18:11:18 +0200

    Improve duplicate other

--- docs/data/charts/pie/webUsageStats.ts
+++ docs/data/charts/pie/webUsageStats.ts
@@ -55,8 +55,16 @@ export const platforms = [
 const normalize = (v: number, v2: number) => Number.parseFloat(((v * v2) / 100).toFixed(2));
 
 export const mobileAndDesktopOS = [
-  ...mobileOS.map((v) => ({ ...v, value: normalize(v.value, platforms[0].value) })),
-  ...desktopOS.map((v) => ({ ...v, value: normalize(v.value, platforms[1].value) })),
+  ...mobileOS.map((v) => ({
+    ...v,
+    label: v.label === 'Other' ? 'Other (Mobile)' : v.label,
+    value: normalize(v.value, platforms[0].value),
+  })),
+  ...desktopOS.map((v) => ({
+    ...v,
+    label: v.label === 'Other' ? 'Other (Desktop)' : v.label,
+    value: normalize(v.value, platforms[1].value),
+  })),
 ];
 
 export const valueFormatter = (item: { value: number }) => `${item.value}%`;
