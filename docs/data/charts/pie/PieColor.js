85674395c026b6a771cdd5bab7b0c3edde4c2218
Jose Quintas <juniorquintas@gmail.com>
2024-08-22 18:07:40 +0200

    update charts

--- docs/data/charts/pie/PieColor.js
+++ docs/data/charts/pie/PieColor.js
@@ -3,37 +3,36 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { PieChart } from '@mui/x-charts/PieChart';
+import { platforms } from './webUsageStats';
 
-const pieParams = { height: 200, margin: { right: 5 } };
-const palette = ['red', 'blue', 'green'];
+const palette = ['lightcoral', 'slateblue'];
+
+const colorPerItem = [
+  { ...platforms[0], color: 'orange' },
+  { ...platforms[1], color: 'gray' },
+];
 
 export default function PieColor() {
   return (
     <Stack direction="row" width="100%" textAlign="center" spacing={2}>
       <Box flexGrow={1}>
         <Typography>Default</Typography>
-        <PieChart
-          series={[{ data: [{ value: 10 }, { value: 15 }, { value: 20 }] }]}
-          {...pieParams}
-        />
+        <PieChart series={[{ data: platforms }]} {...pieParams} />
       </Box>
       <Box flexGrow={1}>
         <Typography>Palette</Typography>
-        <PieChart
-          colors={palette}
-          series={[{ data: [{ value: 10 }, { value: 15 }, { value: 20 }] }]}
-          {...pieParams}
-        />
+        <PieChart colors={palette} series={[{ data: platforms }]} {...pieParams} />
       </Box>
       <Box flexGrow={1}>
         <Typography>Item</Typography>
-        <PieChart
-          series={[
-            { data: [{ value: 10, color: 'orange' }, { value: 15 }, { value: 20 }] },
-          ]}
-          {...pieParams}
-        />
+        <PieChart series={[{ data: colorPerItem }]} {...pieParams} />
       </Box>
     </Stack>
   );
 }
+
+const pieParams = {
+  height: 200,
+  margin: { right: 5 },
+  slotProps: { legend: { hidden: true } },
+};
