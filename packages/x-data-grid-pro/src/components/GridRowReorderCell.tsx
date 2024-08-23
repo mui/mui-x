5d2bf7cf266060f3b04b4a49f1a1d8b9fe3d61f0
Gene Arch <archgene@johndeere.com>
2024-08-23 09:25:55 -0500

    run pnpm proptypes

--- packages/x-data-grid-pro/src/components/GridRowReorderCell.tsx
+++ packages/x-data-grid-pro/src/components/GridRowReorderCell.tsx
@@ -100,7 +100,7 @@ function GridRowReorderCell(params: GridRenderCellParams) {
 GridRowReorderCell.propTypes = {
   // ----------------------------- Warning --------------------------------
   // | These PropTypes are generated from the TypeScript type definitions |
-  // | To update them edit the TypeScript types and run "yarn proptypes"  |
+  // | To update them edit the TypeScript types and run "pnpm proptypes"  |
   // ----------------------------------------------------------------------
   /**
    * GridApi that let you manipulate the grid.
