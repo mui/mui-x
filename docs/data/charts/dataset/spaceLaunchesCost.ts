bad6db885bd35d4d01b94c98f75b92fa68538997
Jose Quintas <juniorquintas@gmail.com>
2024-08-25 19:59:02 +0200

    Add multiple dataset examples

---
+++ docs/data/charts/dataset/spaceLaunchesCost.ts
@@ -0,0 +1,87 @@
+export const spaceLaunchesCost = [
+  { name: 'Angara', launchYear: 2014, costPerKG: 4500, launchClass: 'Heavy' },
+  { name: 'Antares', launchYear: 2013, costPerKG: 13600, launchClass: 'Medium' },
+  { name: 'Ariane 44', launchYear: 1988, costPerKG: 18300, launchClass: 'Medium' },
+  { name: 'Ariane 5G', launchYear: 1997, costPerKG: 10200, launchClass: 'Heavy' },
+  { name: 'Athena 1', launchYear: 1997, costPerKG: 19200, launchClass: 'Small' },
+  { name: 'Atlas Centaur', launchYear: 1963, costPerKG: 29500, launchClass: 'Medium' },
+  { name: 'Atlas II', launchYear: 1991, costPerKG: 18700, launchClass: 'Medium' },
+  { name: 'Atlas III', launchYear: 2000, costPerKG: 16000, launchClass: 'Medium' },
+  { name: 'Atlas V', launchYear: 2002, costPerKG: 8100, launchClass: 'Medium' },
+  { name: 'Delta 3000-Series', launchYear: 1975, costPerKG: 21400, launchClass: 'Small' },
+  { name: 'Delta 3910', launchYear: 1980, costPerKG: 32800, launchClass: 'Small' },
+  { name: 'Delta E', launchYear: 1965, costPerKG: 177900, launchClass: 'Medium' },
+  { name: 'Delta II', launchYear: 1990, costPerKG: 38800, launchClass: 'Medium' },
+  { name: 'Delta III', launchYear: 1999, costPerKG: 18000, launchClass: 'Medium' },
+  { name: 'Delta IV', launchYear: 2002, costPerKG: 10400, launchClass: 'Medium' },
+  { name: 'Delta IV Heavy', launchYear: 2004, costPerKG: 11600, launchClass: 'Heavy' },
+  { name: 'Dnepr', launchYear: 1999, costPerKG: 9600, launchClass: 'Medium' },
+  { name: 'Electron', launchYear: 2018, costPerKG: 23100, launchClass: 'Small' },
+  { name: 'Epsilon', launchYear: 2013, costPerKG: 34500, launchClass: 'Small' },
+  { name: 'Falcon 1', launchYear: 2008, costPerKG: 12600, launchClass: 'Small' },
+  { name: 'Falcon 9', launchYear: 2010, costPerKG: 2600, launchClass: 'Medium' },
+  { name: 'Falcon Heavy', launchYear: 2018, costPerKG: 1500, launchClass: 'Heavy' },
+  { name: 'GSLV', launchYear: 2001, costPerKG: 10000, launchClass: 'Medium' },
+  { name: 'H-II', launchYear: 1994, costPerKG: 10500, launchClass: 'Medium' },
+  { name: 'Kosmos', launchYear: 1962, costPerKG: 14900, launchClass: 'Small' },
+  { name: 'Kuaizhou', launchYear: 2013, costPerKG: 10600, launchClass: 'Small' },
+  { name: 'LVM3', launchYear: 2017, costPerKG: 8000, launchClass: 'Medium' },
+  { name: 'Long March 11', launchYear: 2015, costPerKG: 10600, launchClass: 'Small' },
+  { name: 'Long March 2A', launchYear: 1975, costPerKG: 17500, launchClass: 'Medium' },
+  { name: 'Long March 2C', launchYear: 1982, costPerKG: 8300, launchClass: 'Medium' },
+  { name: 'Long March 2D', launchYear: 1992, costPerKG: 9100, launchClass: 'Medium' },
+  { name: 'Long March 2E', launchYear: 1990, costPerKG: 9900, launchClass: 'Medium' },
+  { name: 'Long March 3A', launchYear: 1994, costPerKG: 8700, launchClass: 'Medium' },
+  { name: 'Long March 3B', launchYear: 1997, costPerKG: 6200, launchClass: 'Medium' },
+  { name: 'Long March 4B', launchYear: 1999, costPerKG: 7600, launchClass: 'Medium' },
+  { name: 'Long March 5', launchYear: 2016, costPerKG: 7900, launchClass: 'Heavy' },
+  { name: 'M-V', launchYear: 1997, costPerKG: 45800, launchClass: 'Small' },
+  { name: 'Minotaur I', launchYear: 2000, costPerKG: 73100, launchClass: 'Small' },
+  { name: 'Minotaur IV', launchYear: 2010, costPerKG: 30500, launchClass: 'Small' },
+  { name: 'PSLV', launchYear: 1994, costPerKG: 8500, launchClass: 'Medium' },
+  { name: 'Pegasus', launchYear: 1990, costPerKG: 41100, launchClass: 'Small' },
+  { name: 'Pegasus XL', launchYear: 1996, costPerKG: 50600, launchClass: 'Small' },
+  { name: 'Proton', launchYear: 1965, costPerKG: 8200, launchClass: 'Heavy' },
+  { name: 'R-36 / Cyclone', launchYear: 1966, costPerKG: 8400, launchClass: 'Medium' },
+  { name: 'Rokot', launchYear: 1994, costPerKG: 20600, launchClass: 'Small' },
+  { name: 'Saturn V', launchYear: 1967, costPerKG: 5400, launchClass: 'Heavy' },
+  { name: 'Scout', launchYear: 1961, costPerKG: 118500, launchClass: 'Small' },
+  { name: 'Shavit', launchYear: 1988, costPerKG: 31100, launchClass: 'Small' },
+  { name: 'Shian Quxian', launchYear: 2019, costPerKG: 17300, launchClass: 'Small' },
+  { name: 'Shtil', launchYear: 1998, costPerKG: 19100, launchClass: 'Small' },
+  { name: 'Soyuz', launchYear: 1966, costPerKG: 17900, launchClass: 'Medium' },
+  { name: 'Space Shuttle', launchYear: 1981, costPerKG: 65400, launchClass: 'Heavy' },
+  { name: 'Start', launchYear: 1993, costPerKG: 23600, launchClass: 'Small' },
+  { name: 'Strela', launchYear: 2003, costPerKG: 11200, launchClass: 'Small' },
+  { name: 'Taurus', launchYear: 1994, costPerKG: 34600, launchClass: 'Small' },
+  { name: 'Titan II', launchYear: 1964, costPerKG: 30600, launchClass: 'Medium' },
+  { name: 'Titan III+', launchYear: 1965, costPerKG: 21000, launchClass: 'Medium' },
+  { name: 'Titan IV', launchYear: 1989, costPerKG: 30800, launchClass: 'Heavy' },
+  { name: 'Vega', launchYear: 2012, costPerKG: 20000, launchClass: 'Small' },
+  { name: 'Zenit 2', launchYear: 1985, costPerKG: 5100, launchClass: 'Medium' },
+  { name: 'Zenit 3SL', launchYear: 1999, costPerKG: 8900, launchClass: 'Medium' },
+];
+
+export const heavyLaunchesScatter = spaceLaunchesCost
+  .filter((launch) => launch.launchClass === 'Heavy')
+  .map((launch) => ({
+    x: launch.launchYear,
+    y: launch.costPerKG,
+    id: launch.name,
+  }));
+
+export const mediumLaunchesScatter = spaceLaunchesCost
+  .filter((launch) => launch.launchClass === 'Medium')
+  .map((launch) => ({
+    x: launch.launchYear,
+    y: launch.costPerKG,
+    id: launch.name,
+  }));
+
+export const smallLaunchesScatter = spaceLaunchesCost
+  .filter((launch) => launch.launchClass === 'Small')
+  .map((launch) => ({
+    x: launch.launchYear,
+    y: launch.costPerKG,
+    id: launch.name,
+  }));
