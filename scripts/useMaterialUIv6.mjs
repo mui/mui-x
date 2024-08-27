9234725d21e7af84a16aaff9b677671932b46430
Lukas <llukas.tyla@gmail.com>
2024-08-27 15:25:40 +0300

    Use `6.x` in the script for material v6

--- scripts/useMaterialUIv6.mjs
+++ scripts/useMaterialUIv6.mjs
@@ -2,7 +2,7 @@ import childProcess from 'child_process';
 
 const pnpmUpdate = childProcess.spawnSync(
   'pnpm',
-  ['update', '-r', '@mui/material@next', '@mui/system@next', '@mui/icons-material@next'],
+  ['update', '-r', '@mui/material@6.x', '@mui/system@6.x', '@mui/icons-material@6.x'],
   {
     shell: true,
     stdio: ['inherit', 'inherit', 'inherit'],
