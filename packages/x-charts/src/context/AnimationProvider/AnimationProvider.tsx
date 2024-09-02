6fcab56cf7399ae1d3ca272626c237ec46b075c3
Jose Quintas <juniorquintas@gmail.com>
2024-09-02 14:07:07 +0200

    Fix re-rendering issue in jsdom

--- packages/x-charts/src/context/AnimationProvider/AnimationProvider.tsx
+++ packages/x-charts/src/context/AnimationProvider/AnimationProvider.tsx
@@ -4,17 +4,26 @@ import { AnimationProviderProps } from './Animation.types';
 import { AnimationContext } from './AnimationContext';
 
 function AnimationProvider(props: AnimationProviderProps) {
+  // Part of the behavior was taken from:
+  // https://github.com/pmndrs/react-spring/blob/fd65b605b85c3a24143c4ce9dd322fdfca9c66be/packages/shared/src/hooks/useReducedMotion.ts
   const { children, skipAnimation: inSkipAnimation } = props;
 
-  // Taken from: https://github.com/pmndrs/react-spring/blob/fd65b605b85c3a24143c4ce9dd322fdfca9c66be/packages/shared/src/hooks/useReducedMotion.ts
-  const [skipAnimation, setSkipAnimation] = React.useState<true | undefined>(undefined);
+  // Skip animation test/jsdom
+  const isAnimationDisabledEnvironment = !window?.matchMedia;
+
+  // We use the value of `isAnimationDisabledEnvironment` as the initial value of `skipAnimation` to avoid
+  // re-rendering the component on environments where matchMedia is not supported, hence skipAnimation will always be true.
+  const [skipAnimation, setSkipAnimation] = React.useState<true | undefined>(
+    isAnimationDisabledEnvironment || undefined,
+  );
 
   useIsomorphicLayoutEffect(() => {
-    // Skip animation test/jsdom
-    const shouldSkipAnimation = !window?.matchMedia;
+    if (isAnimationDisabledEnvironment) {
+      return undefined;
+    }
 
     const handleMediaChange = (e: { matches: boolean | undefined }) => {
-      // Modification to the react-spring implementation such that this hook can remove animation but never activate it.
+      // This hook can remove animation but never activate it.
       const inputValue = e.matches || undefined;
       setSkipAnimation(inputValue);
       Globals.assign({
@@ -22,20 +31,15 @@ function AnimationProvider(props: AnimationProviderProps) {
       });
     };
 
-    if (shouldSkipAnimation) {
-      handleMediaChange({ matches: true });
-      return undefined;
-    }
-
     const mql = window.matchMedia('(prefers-reduced-motion)');
 
     handleMediaChange(mql);
 
     // MatchMedia is not fully supported in all environments, so we need to check if it exists before calling addEventListener
-    mql.addEventListener?.('change', handleMediaChange);
+    mql?.addEventListener?.('change', handleMediaChange);
 
     return () => {
-      mql.removeEventListener?.('change', handleMediaChange);
+      mql?.removeEventListener?.('change', handleMediaChange);
     };
   }, []);
 
