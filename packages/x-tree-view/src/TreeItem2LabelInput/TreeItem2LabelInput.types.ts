1aeb92f0d21970f809f7c6df88f5d622aa34e509
delangle <flaviendelangle@gmail.com>
2024-09-02 10:55:07 +0200

    Review Nora

--- packages/x-tree-view/src/TreeItem2LabelInput/TreeItem2LabelInput.types.ts
+++ packages/x-tree-view/src/TreeItem2LabelInput/TreeItem2LabelInput.types.ts
@@ -3,6 +3,9 @@ import { MuiCancellableEventHandler } from '../internals/models/MuiCancellableEv
 
 export interface TreeItem2LabelInputProps {
   value?: string;
+  /**
+   * Used to determine if the target of keydown or blur events is the input and prevent the event from propagating to the root.
+   */
   'data-element'?: 'labelInput';
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
   onKeyDown?: MuiCancellableEventHandler<React.KeyboardEvent<HTMLInputElement>>;
