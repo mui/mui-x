5a85866674136fec5c7f399df8bd828e79f5777e
Rajat <robertdoweny2301@gmail.com>
2024-09-04 16:04:31 +0530

    Remove the re-export from @mui/x-license

--- packages/x-date-pickers-pro/src/index.ts
+++ packages/x-date-pickers-pro/src/index.ts
@@ -1,10 +1,3 @@
-import { LicenseInfo as LicenseInfoExport } from '@mui/x-license';
-/**
- * @deprecated Use `@mui/x-license` package instead:
- * @example import { LicenseInfo } from '@mui/x-license';
- */
-export class LicenseInfo extends LicenseInfoExport {}
-
 export * from '@mui/x-date-pickers';
 
 export * from './DateRangePickerDay';
