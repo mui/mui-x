72d2eb7ecf1cc29e4cd179bb6d589a1243d0e9b2
Andrew Cherniavskyi <andrew@mui.com>
2024-09-05 21:26:54 +0200

    lint

--- packages/x-data-grid/src/components/cell/GridEditInputCell.tsx
+++ packages/x-data-grid/src/components/cell/GridEditInputCell.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import {
   unstable_composeClasses as composeClasses,
   unstable_useEnhancedEffect as useEnhancedEffect,
--- packages/x-data-grid/src/components/toolbar/GridToolbar.tsx
+++ packages/x-data-grid/src/components/toolbar/GridToolbar.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import {
   GridToolbarContainer,
