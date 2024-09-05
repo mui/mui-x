13acded0c88bc898cdbfab4372961d2198708897
Andrew Cherniavskyi <andrew@mui.com>
2024-09-05 21:26:03 +0200

    proptypes

--- packages/x-data-grid/src/components/toolbar/GridToolbarExport.tsx
+++ packages/x-data-grid/src/components/toolbar/GridToolbarExport.tsx
@@ -92,42 +92,6 @@ function GridPrintExportMenuItem(props: GridPrintExportMenuItemProps) {
   );
 }
 
-GridPrintExportMenuItem.propTypes = {
-  // ----------------------------- Warning --------------------------------
-  // | These PropTypes are generated from the TypeScript type definitions |
-  // | To update them edit the TypeScript types and run "pnpm proptypes"  |
-  // ----------------------------------------------------------------------
-  hideMenu: PropTypes.func,
-  options: PropTypes.shape({
-    allColumns: PropTypes.bool,
-    bodyClassName: PropTypes.string,
-    copyStyles: PropTypes.bool,
-    disableToolbarButton: PropTypes.bool,
-    fields: PropTypes.arrayOf(PropTypes.string),
-    fileName: PropTypes.string,
-    getRowsToExport: PropTypes.func,
-    hideFooter: PropTypes.bool,
-    hideToolbar: PropTypes.bool,
-    includeCheckboxes: PropTypes.bool,
-    pageStyle: PropTypes.oneOfType([
-      PropTypes.shape({
-        '__@hasInstance@645': PropTypes.func.isRequired,
-        '__@metadata@647': PropTypes.any,
-        apply: PropTypes.func.isRequired,
-        arguments: PropTypes.any.isRequired,
-        bind: PropTypes.func.isRequired,
-        call: PropTypes.func.isRequired,
-        caller: PropTypes.object.isRequired,
-        length: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-        prototype: PropTypes.any.isRequired,
-        toString: PropTypes.func.isRequired,
-      }),
-      PropTypes.string,
-    ]),
-  }),
-} as any;
-
 export { GridPrintExportMenuItem };
 
 const GridToolbarExport = React.forwardRef<HTMLButtonElement, GridToolbarExportProps>(
