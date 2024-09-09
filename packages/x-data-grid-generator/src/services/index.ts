520d992d0ac145a48ce832dc53dad7a08a1c1799
Andrew Cherniavskyi <andrew@mui.com>
2024-09-09 14:42:06 +0200

    revert generator exports

--- packages/x-data-grid-generator/src/services/index.ts
+++ packages/x-data-grid-generator/src/services/index.ts
@@ -2,4 +2,3 @@ export * from './gridColDefGenerator';
 export * from './random-generator';
 export * from './real-data-service';
 export * from './basic-data-service';
-export * from './static-data';
