d522dab0792bbe7defb8cc55c8a06af09e7c3a63
Jose Quintas <juniorquintas@gmail.com>
2024-09-10 17:43:29 +0200

    easy config

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,31 +1,35 @@
 import { defineWorkspace } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-export default defineWorkspace([
-  {
-    extends: './vitest.config.mts',
-    plugins: [react()],
-    test: {
-      include: ['packages/x-charts/**/*.test.tsx'],
-      name: 'charts/browser',
-      browser: {
-        enabled: true,
-        name: 'chromium',
-        provider: 'playwright',
-        headless: true,
-        // https://playwright.dev
-        providerOptions: {},
-        screenshotFailures: false,
+const packages = ['charts'];
+
+export default defineWorkspace(
+  packages.flatMap((name) => [
+    {
+      extends: './vitest.config.mts',
+      plugins: [react()],
+      test: {
+        include: [`packages/x-${name}/**/*.test.tsx`],
+        name: `${name}/browser`,
+        browser: {
+          enabled: true,
+          name: 'chromium',
+          provider: 'playwright',
+          headless: true,
+          // https://playwright.dev
+          providerOptions: {},
+          screenshotFailures: false,
+        },
       },
     },
-  },
-  {
-    extends: './vitest.config.mts',
-    plugins: [react()],
-    test: {
-      include: ['packages/x-charts/**/*.test.tsx'],
-      name: 'charts/jsdom',
-      environment: 'jsdom',
+    {
+      extends: './vitest.config.mts',
+      plugins: [react()],
+      test: {
+        include: [`packages/x-${name}/**/*.test.tsx`],
+        name: `${name}/jsdom`,
+        environment: 'jsdom',
+      },
     },
-  },
-]);
+  ]),
+);
