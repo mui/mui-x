27777729762d6a120111e0791c4eeff165c6b783
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2024-09-16 08:11:26 +0100

    [DataGridPro] Fix pinned columns in RTL mode (#14586)

--- packages/x-data-grid/src/hooks/features/virtualization/useGridVirtualScroller.tsx
+++ packages/x-data-grid/src/hooks/features/virtualization/useGridVirtualScroller.tsx
@@ -466,7 +466,6 @@ export const useGridVirtualScroller = () => {
       const offsetLeft = computeOffsetLeft(
         columnPositions,
         currentRenderContext,
-        isRtl,
         pinnedColumns.left.length,
       );
       const showBottomBorder = isLastVisibleInSection && params.position === 'top';
@@ -939,14 +938,11 @@ export function areRenderContextsEqual(context1: GridRenderContext, context2: Gr
 export function computeOffsetLeft(
   columnPositions: number[],
   renderContext: GridColumnsRenderContext,
-  isRtl: boolean,
   pinnedLeftLength: number,
 ) {
-  const factor = isRtl ? -1 : 1;
   const left =
-    factor * (columnPositions[renderContext.firstColumnIndex] ?? 0) -
+    (columnPositions[renderContext.firstColumnIndex] ?? 0) -
     (columnPositions[pinnedLeftLength] ?? 0);
-
   return Math.abs(left);
 }
 
