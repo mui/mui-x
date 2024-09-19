fde39ca920372f9923107ed90dfa026ccb6c9209
Lukas Tyla <llukas.tyla@gmail.com>
2024-09-19 12:18:55 +0300

    Apply suggestions from code review
    Signed-off-by: Lukas Tyla <llukas.tyla@gmail.com>

--- docs/data/date-pickers/date-picker/DatePickerYearsOrder.js
+++ docs/data/date-pickers/date-picker/DatePickerYearsOrder.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import dayjs from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -10,15 +9,14 @@ const currentYear = dayjs();
 export default function DatePickerYearsOrder() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DatePicker']}>
-        <DatePicker
-          label="Years in descending order"
-          maxDate={currentYear}
-          openTo="year"
-          views={['year', 'month']}
-          yearsOrder="desc"
-        />
-      </DemoContainer>
+      <DatePicker
+        label="Years in descending order"
+        maxDate={currentYear}
+        openTo="year"
+        views={['year', 'month']}
+        yearsOrder="desc"
+        sx={{ minWidth: 250 }}
+      />
     </LocalizationProvider>
   );
 }
