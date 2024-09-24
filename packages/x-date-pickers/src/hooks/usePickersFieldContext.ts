9837ebf940de8026bb410549cd954e3797c612c5
flavien <flaviendelangle@gmail.com>
2024-09-24 12:05:00 +0200

    Fix

--- packages/x-date-pickers/src/hooks/usePickersFieldContext.ts
+++ packages/x-date-pickers/src/hooks/usePickersFieldContext.ts
@@ -1,28 +1,18 @@
 import * as React from 'react';
-import {
-  PickersFieldContext,
-  PickersFieldContextValue,
-} from '../internals/components/PickersFieldProvider';
+import { PickersFieldContext } from '../internals/components/PickersFieldProvider';
 
 /**
  * Returns the context passed by the picker that wraps the current field.
- * If the field is used as a standalone component, the context will be `null`.
  */
-export const usePickersFieldContext = <TOptional extends boolean = false>(
-  params: { isOptional: TOptional } = { isOptional: false as TOptional },
-) => {
+export const usePickersFieldContext = () => {
   const value = React.useContext(PickersFieldContext);
-  if (!params.isOptional && value == null) {
+  if (value == null) {
     throw new Error(
       [
-        'MUI X: The `usePickersFieldContext` hook was used outside of the component tree of a picker.',
-        'If your field component is usable as a standalone component, then to use the hook as follow:',
-        '`usePickersFieldContext({ isOptional: true })`',
+        'MUI X: The `usePickersFieldContext` can only be called in fields that are used as a slot of a picker component',
       ].join('\n'),
     );
   }
 
-  return value as TOptional extends false
-    ? PickersFieldContextValue
-    : PickersFieldContextValue | null;
+  return value;
 };
