717c8bce816b6ec9cf99bb8de6ab011f6c32ed53
alexandre <alex.fauquette@gmail.com>
2024-09-26 10:30:42 +0100

    prettier

--- packages/x-charts/src/LineChart/AppearingMask.tsx
+++ packages/x-charts/src/LineChart/AppearingMask.tsx
@@ -5,42 +5,40 @@ import { cleanId } from '../internals/cleanId';
 import { useChartId, useDrawingArea } from '../hooks';
 import { SeriesId } from '../models/seriesType/common';
 
-
-
 interface AppearingMaskProps {
-    id: SeriesId;
-    skipAnimation?: boolean;
-    children: React.ReactNode;
+  id: SeriesId;
+  skipAnimation?: boolean;
+  children: React.ReactNode;
 }
 export function AppearingMask(props: AppearingMaskProps) {
-    const drawingArea = useDrawingArea();
-    const chartId = useChartId();
+  const drawingArea = useDrawingArea();
+  const chartId = useChartId();
 
-    const transitionAppear = useTransition<typeof drawingArea, { animatedWidth: number }>(
-        [drawingArea],
-        {
-            from: (v) => ({ animatedWidth: v.left }),
-            enter: (v) => ({ animatedWidth: v.width + v.left + v.right }),
-            leave: (v) => ({ animatedWidth: v.width + v.left + v.right }),
-            reset: false,
-            immediate: props.skipAnimation,
-        },
-    );
+  const transitionAppear = useTransition<typeof drawingArea, { animatedWidth: number }>(
+    [drawingArea],
+    {
+      from: (v) => ({ animatedWidth: v.left }),
+      enter: (v) => ({ animatedWidth: v.width + v.left + v.right }),
+      leave: (v) => ({ animatedWidth: v.width + v.left + v.right }),
+      reset: false,
+      immediate: props.skipAnimation,
+    },
+  );
 
-    const clipId = cleanId(`${chartId}-${props.id}`);
-    return (
-        <React.Fragment>
-            <clipPath id={clipId}>
-                {transitionAppear((style) => (
-                    <animated.rect
-                        x={0}
-                        y={0}
-                        width={style.animatedWidth}
-                        height={drawingArea.top + drawingArea.height + drawingArea.bottom}
-                    />
-                ))}
-            </clipPath>
-            <g clipPath={`url(#${clipId})`}>{props.children}</g>
-        </React.Fragment>
-    )
+  const clipId = cleanId(`${chartId}-${props.id}`);
+  return (
+    <React.Fragment>
+      <clipPath id={clipId}>
+        {transitionAppear((style) => (
+          <animated.rect
+            x={0}
+            y={0}
+            width={style.animatedWidth}
+            height={drawingArea.top + drawingArea.height + drawingArea.bottom}
+          />
+        ))}
+      </clipPath>
+      <g clipPath={`url(#${clipId})`}>{props.children}</g>
+    </React.Fragment>
+  );
 }
