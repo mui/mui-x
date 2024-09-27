7ff9d9f1c5dd59fe8873c7e40fdfdbf0d5c57d7c
Andrew Cherniavskyi <andrew@mui.com>
2024-09-27 09:32:03 +0200

    warn if render function is passed to a slot

--- packages/x-data-grid/src/internals/utils/computeSlots.ts
+++ packages/x-data-grid/src/internals/utils/computeSlots.ts
@@ -1,3 +1,5 @@
+import { warnOnce } from '@mui/x-internals/warning';
+
 export function computeSlots<SlotComponents extends object>({
   defaultSlots,
   slots,
@@ -16,6 +18,18 @@ export function computeSlots<SlotComponents extends object>({
     const k = key as keyof typeof overrides;
 
     if (overrides[k] !== undefined) {
+      if (process.env.NODE_ENV !== 'production') {
+        const slot = overrides[k] as Function;
+        if (slot.name !== undefined && slot.name === key) {
+          // This is a render function, not a component.
+          warnOnce(
+            `MUI X: Slots only accept React components, but \`${String(k)}\` slot received a render function.
+See https://mui.com/x/common-concepts/custom-components/#correct-usage for more details.`,
+            'warning',
+          );
+        }
+      }
+
       result[k] = overrides[k] as any;
     }
   });
