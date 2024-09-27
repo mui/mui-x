7f9e80dcafacfe0b634ca960b636c67d162c3fab
flavien <flaviendelangle@gmail.com>
2024-09-27 10:04:33 +0200

    [TreeView] Rework how items are being rendering in Rich Tree View components

--- packages/x-tree-view/src/internals/plugins/useTreeViewItems/index.ts
+++ packages/x-tree-view/src/internals/plugins/useTreeViewItems/index.ts
@@ -4,5 +4,6 @@ export type {
   UseTreeViewItemsParameters,
   UseTreeViewItemsDefaultizedParameters,
   UseTreeViewItemsState,
+  TreeViewItemToRenderProps,
 } from './useTreeViewItems.types';
 export { buildSiblingIndexes, TREE_VIEW_ROOT_PARENT_ID } from './useTreeViewItems.utils';
