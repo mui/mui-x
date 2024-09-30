a168a06c43819d922034d988a49fad97978f8b26
Arthur Balduini <arthurbalduini@Arthurs-MacBook-Pro.local>
2024-09-30 12:09:47 +0200

    prettier and keyCount

--- packages/x-date-pickers/src/locales/csCZ.ts
+++ packages/x-date-pickers/src/locales/csCZ.ts
@@ -78,7 +78,7 @@ const csCZPickers: Partial<PickersLocaleText<any>> = {
   fieldYearPlaceholder: (params) => 'Y'.repeat(params.digitAmount),
   fieldMonthPlaceholder: (params) => (params.contentType === 'letter' ? 'MMMM' : 'MM'),
   fieldDayPlaceholder: () => 'DD',
-  fieldWeekDayPlaceholder: params => params.contentType === 'letter' ? 'EEEE' : 'EE',
+  fieldWeekDayPlaceholder: (params) => (params.contentType === 'letter' ? 'EEEE' : 'EE'),
   fieldHoursPlaceholder: () => 'hh',
   fieldMinutesPlaceholder: () => 'mm',
   fieldSecondsPlaceholder: () => 'ss',
