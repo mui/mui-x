299a96c42a68e7da7e72ad8da1a6bdcc36099219
flavien <flaviendelangle@gmail.com>
2024-09-30 12:17:08 +0200

    Work

---
+++ docs/data/date-pickers/custom-field/custom-behavior/ReadOnlyMaterialTextField.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { useValidation, validateDate } from '@mui/x-date-pickers/validation';
+import {
+  useSplitFieldProps,
+  useFieldPlaceholder,
+  usePickersFieldContext,
+} from '@mui/x-date-pickers/hooks';
+
+function ReadOnlyDateField(props) {
+  const { internalProps, forwardedProps } = useSplitFieldProps(props, 'date');
+  const { value, timezone, format } = internalProps;
+  const { InputProps, slotProps, slots, ...other } = forwardedProps;
+
+  const pickerContext = usePickersFieldContext();
+
+  const placeholder = useFieldPlaceholder(internalProps);
+  const { hasValidationError } = useValidation({
+    validator: validateDate,
+    value,
+    timezone,
+    props: internalProps,
+  });
+
+  return (
+    <TextField
+      {...other}
+      value={value == null ? '' : value.format(format)}
+      placeholder={placeholder}
+      InputProps={{ ...InputProps, readOnly: true }}
+      error={hasValidationError}
+      onClick={pickerContext.onOpen}
+    />
+  );
+}
+
+function ReadOnlyFieldDatePicker(props) {
+  return (
+    <DatePicker slots={{ ...props.slots, field: ReadOnlyDateField }} {...props} />
+  );
+}
+
+export default function ReadOnlyMaterialTextField() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ReadOnlyFieldDatePicker />
+    </LocalizationProvider>
+  );
+}
