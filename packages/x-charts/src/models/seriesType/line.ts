6038beb77929e83aaee55ad51028350282e93e4a
Jose C Quintas Jr <juniorquintas@gmail.com>
2024-10-01 14:59:34 +0200

    Update packages/x-charts/src/models/seriesType/line.ts
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>
    Signed-off-by: Jose C Quintas Jr <juniorquintas@gmail.com>

--- packages/x-charts/src/models/seriesType/line.ts
+++ packages/x-charts/src/models/seriesType/line.ts
@@ -63,7 +63,6 @@ export interface LineSeriesType
   /**
    * The type of curve to use for the line. Read more about curves at
    * [line interpolation](https://mui.com/x/react-charts/lines/#interpolation).
-   *
    * @default 'monotoneX'
    */
   curve?: CurveType;
