8434b41c2cd10f41d086d9c1c8d46642be0e391d
Jose Quintas <juniorquintas@gmail.com>
2024-10-04 15:37:13 +0200

    use x-package for names

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,7 +1,7 @@
-import { defineWorkspace } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { defineWorkspace } from 'vitest/config';
 
-const packages = ['charts', 'date-pickers'];
+const packages = ['x-charts', 'x-date-pickers'];
 
 // Ideally we move the configuration to each package.
 // Currently it doesn't work because vitest doesn't detect two different configurations in the same package.
@@ -14,10 +14,10 @@ export default defineWorkspace(
         extends: './vitest.config.mts',
         plugins: [react()],
         test: {
-          include: [`packages/x-${name}/src/**/*.test.?(c|m)[jt]s?(x)`],
+          include: [`packages/${name}/src/**/*.test.?(c|m)[jt]s?(x)`],
           exclude: [
-            `packages/x-${name}/src/**/*.jsdom.test.?(c|m)[jt]s?(x)`,
-            `packages/x-${name}/src/**/*V3.test.?(c|m)[jt]s?(x)`,
+            `packages/${name}/src/**/*.jsdom.test.?(c|m)[jt]s?(x)`,
+            `packages/${name}/src/**/*V3.test.?(c|m)[jt]s?(x)`,
           ],
           name: `browser/${name}`,
           env: {
@@ -38,8 +38,8 @@ export default defineWorkspace(
         extends: './vitest.config.mts',
         plugins: [react()],
         test: {
-          include: [`packages/x-${name}/src/**/*.test.?(c|m)[jt]s?(x)`],
-          exclude: [`packages/x-${name}/src/**/*.browser.test.?(c|m)[jt]s?(x)`],
+          include: [`packages/${name}/src/**/*.test.?(c|m)[jt]s?(x)`],
+          exclude: [`packages/${name}/src/**/*.browser.test.?(c|m)[jt]s?(x)`],
           name: `jsdom/${name}`,
           environment: 'jsdom',
           env: {
