d0d5bffd57565ea3747b16c783a92ffd84dfa433
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-10-07 11:16:10 +0200

    [pickers] Move the `DateFieldInPickerProps` interface to the `DatePicker` folder and rename it `DatePickerFieldProps` (same for time and date time) (#14828)

--- docs/data/date-pickers/experimentation/CustomField.tsx
+++ docs/data/date-pickers/experimentation/CustomField.tsx
@@ -4,12 +4,11 @@ import TextField from '@mui/material/TextField';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DatePicker, DatePickerFieldProps } from '@mui/x-date-pickers/DatePicker';
 import { useValidation, validateDate } from '@mui/x-date-pickers/validation';
 import { useSplitFieldProps } from '@mui/x-date-pickers/hooks';
-import { DateFieldInPickerProps } from '@mui/x-date-pickers/DateField';
 
-function ReadOnlyField(props: DateFieldInPickerProps<Dayjs, false>) {
+function ReadOnlyField(props: DatePickerFieldProps<Dayjs, false>) {
   const { internalProps, forwardedProps } = useSplitFieldProps(props, 'date');
 
   const { value, timezone, format } = internalProps;
