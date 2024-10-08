448f209d39ceb08da1c0a7a1318fbae7cc13d9e1
flavien <flaviendelangle@gmail.com>
2024-10-08 13:28:20 +0200

    Work

--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -621,8 +621,9 @@ async function initializeEnvironment(
         });
 
         // assertion for: https://github.com/mui/mui-x/issues/12652
-        it('should allow field editing after opening and closing the picker', async () => {
+        it.only('should allow field editing after opening and closing the picker', async () => {
           await renderFixture('DatePicker/BasicClearableDesktopDatePicker');
+
           // open picker
           await page.getByRole('button').click();
           await page.waitForSelector('[role="dialog"]', { state: 'attached' });
@@ -630,11 +631,18 @@ async function initializeEnvironment(
           await page.getByRole('button', { name: 'Choose date' }).click();
           await page.waitForSelector('[role="dialog"]', { state: 'detached' });
 
-          // click on the input to focus it
-          await page.getByRole('textbox').click();
+          // // open picker
+          // await page.getByRole('button').click();
+          // await page.waitForSelector('[role="dialog"]', { state: 'attached' });
+          // // close picker
+          // await page.getByRole('button', { name: 'Choose date' }).click();
+          // await page.waitForSelector('[role="dialog"]', { state: 'detached' });
 
-          // test that the input value is set after focus
-          expect(await page.getByRole('textbox').inputValue()).to.equal('MM/DD/YYYY');
+          // // click on the input to focus it
+          // await page.getByRole('textbox').click();
+
+          // // test that the input value is set after focus
+          // expect(await page.getByRole('textbox').inputValue()).to.equal('MM/DD/YYYY');
         });
 
         it('should allow filling in a value and clearing a value', async () => {
@@ -752,7 +760,7 @@ async function initializeEnvironment(
         });
 
         it('should submit a form when clicking "Enter" key', async () => {
-          await renderFixture('DatePicker/DesktopDatePickerForm');
+          await renderFixture('DatePicker/DesktopDatePickerFormLegacyDOMStructure');
 
           const textbox = page.getByRole('textbox');
           await textbox.focus();
