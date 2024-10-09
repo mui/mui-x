a82a340f255f274503b8b082827606abf9b26266
Armin Mehinovic <armin@mui.com>
2024-10-09 14:39:36 +0200

    Use different scrollTop numbers

--- packages/x-data-grid-pro/src/tests/infiniteLoader.DataGridPro.test.tsx
+++ packages/x-data-grid-pro/src/tests/infiniteLoader.DataGridPro.test.tsx
@@ -41,7 +41,7 @@ describe('<DataGridPro /> - Infnite loader', () => {
     const { container, setProps } = render(<TestCase rows={baseRows} />);
     const virtualScroller = container.querySelector('.MuiDataGrid-virtualScroller')!;
     // arbitrary number to make sure that the bottom of the grid window is reached.
-    virtualScroller.scrollTop = 12345;
+    virtualScroller.scrollTop = 10000;
     virtualScroller.dispatchEvent(new Event('scroll'));
     await waitFor(() => {
       expect(handleRowsScrollEnd.callCount).to.equal(1);
@@ -56,7 +56,7 @@ describe('<DataGridPro /> - Infnite loader', () => {
     // Trigger a scroll again to notify the grid that we're not in the bottom area anymore
     virtualScroller.dispatchEvent(new Event('scroll'));
     expect(handleRowsScrollEnd.callCount).to.equal(1);
-    virtualScroller.scrollTop = 12345;
+    virtualScroller.scrollTop = 15000;
     virtualScroller.dispatchEvent(new Event('scroll'));
     await waitFor(() => {
       expect(handleRowsScrollEnd.callCount).to.equal(2);
