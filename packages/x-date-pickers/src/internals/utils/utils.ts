cbb72167a94bb60d81682d06a9372ecc61e472ed
arthurbalduini <arthurbalduini2@gmail.com>
2024-10-09 15:00:07 +0200

    adds jsDocs for getFocusedListItemIndex

--- packages/x-date-pickers/src/internals/utils/utils.ts
+++ packages/x-date-pickers/src/internals/utils/utils.ts
@@ -47,6 +47,12 @@ export const getActiveElement = (root: Document | ShadowRoot = document): Elemen
   return activeEl;
 };
 
+/**
+ * Gets the index of the focused list item in a given ul list element.
+ *
+ * @param {HTMLUListElement} listElement - The list element to search within.
+ * @returns {number} The index of the focused list item, or -1 if none is focused.
+ */
 export const getFocusedListItemIndex = (listElement: HTMLUListElement): number => {
   const children = listElement.children;
   return Array.from(children).findIndex((child) => child === getActiveElement(document));
