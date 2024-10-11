96ae6ef17120b5175c0f31554f2dd8daaa2a0162
flavien <flaviendelangle@gmail.com>
2024-10-11 13:47:03 +0200

    Work

---
+++ packages/x-codemod/src/v8.0.0/preset-safe/expected.spec.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import * as React from 'react';
+import { TreeItem } from '@mui/x-tree-view/TreeItem';
+import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
+
+const className = treeViewClasses.root;
+
+// prettier-ignore
+<React.Fragment>
+  <SimpleTreeView>
+    <TreeItem nodeId="1" label="Item 1" />
+  </SimpleTreeView>
+</React.Fragment>
