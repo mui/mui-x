462de3e8b18e988128b84313332221dcdb87df5a
Rotem Bar-Sela <44998514+rotembarsela@users.noreply.github.com>
2024-10-16 12:42:33 +0300

    [data grid][pickers][tree-view] Fix shortcut with localization keyboard (#14220)
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>

--- packages/x-data-grid/src/hooks/features/rowSelection/useGridRowSelection.ts
+++ packages/x-data-grid/src/hooks/features/rowSelection/useGridRowSelection.ts
@@ -677,7 +677,7 @@ export const useGridRowSelection = (
         return;
       }
 
-      if (event.key === 'a' && (event.ctrlKey || event.metaKey)) {
+      if (String.fromCharCode(event.keyCode) === 'A' && (event.ctrlKey || event.metaKey)) {
         event.preventDefault();
         selectRows(apiRef.current.getAllRowIds(), true);
       }
