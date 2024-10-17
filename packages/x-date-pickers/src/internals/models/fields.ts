86f9162f601be94b0ef70590be39f36c4e9bdd25
flavien <flaviendelangle@gmail.com>
2024-10-17 10:18:50 +0200

    Fix

--- packages/x-date-pickers/src/internals/models/fields.ts
+++ packages/x-date-pickers/src/internals/models/fields.ts
@@ -2,7 +2,6 @@ import * as React from 'react';
 import type { UseFieldInternalProps } from '../hooks/useField';
 import { FieldSection, PickerValidDate } from '../../models';
 import type { ExportedUseClearableFieldProps } from '../../hooks/useClearableField';
-import { MakeRequired } from './helpers';
 
 export interface BaseFieldProps<
   TValue,
@@ -10,12 +9,13 @@ export interface BaseFieldProps<
   TSection extends FieldSection,
   TEnableAccessibleFieldDOMStructure extends boolean,
   TError,
-> extends MakeRequired<
+> extends Omit<
       UseFieldInternalProps<TValue, TDate, TSection, TEnableAccessibleFieldDOMStructure, TError>,
-      'value'
+      'format'
     >,
     ExportedUseClearableFieldProps {
   className?: string;
+  format?: string;
   disabled?: boolean;
   ref?: React.Ref<HTMLDivElement>;
   ownerState?: any;
