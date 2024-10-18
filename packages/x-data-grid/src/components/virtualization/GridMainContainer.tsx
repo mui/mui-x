f991f4c5c9002362e34a169feb6e65f6232a5afd
Rajat <robertdoweny2301@gmail.com>
2024-10-19 01:03:51 +0530

    fix

--- packages/x-data-grid/src/components/virtualization/GridMainContainer.tsx
+++ packages/x-data-grid/src/components/virtualization/GridMainContainer.tsx
@@ -8,6 +8,7 @@ const GridPanelAnchor = styled('div')({
   position: 'absolute',
   top: `var(--DataGrid-headersTotalHeight)`,
   left: 0,
+  width: '100%',
 });
 
 type OwnerState = DataGridProcessedProps;
