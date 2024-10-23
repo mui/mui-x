ea248ba440ed55462af29850024a76d5693587f8
alex <alex.fauquette@gmail.com>
2024-10-23 15:59:15 +0200

    Add codemod for legend

---
+++ packages/x-codemod/src/v8.0.0/charts/preset-safe/expected.spec.tsx
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import * as React from 'react';
+import { PieChart } from '@mui/x-charts/PieChart';
+
+<div>
+  <PieChart
+    slotProps={{
+      legend: { hidden: true }
+    }} />
+  <PieChart
+    slotProps={{
+      tooltip: { trigger: 'axis' },
+      legend: { hidden: true }
+    }} />
+</div>;
