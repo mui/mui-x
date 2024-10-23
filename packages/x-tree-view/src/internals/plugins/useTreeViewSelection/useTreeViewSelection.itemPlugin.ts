893d74bfd37e95d7778f81bbd3e53236f548ec2b
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-10-23 09:34:51 +0200

    [TreeView] Automatic parents and children selection (#14899)

---
+++ packages/x-tree-view/src/internals/plugins/useTreeViewSelection/useTreeViewSelection.itemPlugin.ts
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import type { TreeItem2Props } from '../../../TreeItem2';
+import type { TreeItemProps } from '../../../TreeItem';
+import {
+  TreeViewItemId,
+  TreeViewSelectionPropagation,
+  TreeViewCancellableEvent,
+} from '../../../models';
+import { useTreeViewContext } from '../../TreeViewProvider';
+import { TreeViewInstance, TreeViewItemPlugin } from '../../models';
+import {
+  UseTreeItem2CheckboxSlotPropsFromSelection,
+  UseTreeViewSelectionSignature,
+} from './useTreeViewSelection.types';
+import { UseTreeViewItemsSignature } from '../useTreeViewItems';
+
+function getCheckboxStatus({
+  itemId,
+  instance,
+  selectionPropagation,
+  selected,
+}: {
+  itemId: TreeViewItemId;
+  instance: TreeViewInstance<[UseTreeViewItemsSignature, UseTreeViewSelectionSignature]>;
+  selectionPropagation: TreeViewSelectionPropagation;
+  selected: boolean;
+}) {
+  if (selected) {
+    return {
+      indeterminate: false,
+      checked: true,
+    };
+  }
+
+  const children = instance.getItemOrderedChildrenIds(itemId);
+  if (children.length === 0) {
+    return {
+      indeterminate: false,
+      checked: false,
+    };
+  }
+
+  let hasSelectedDescendant = false;
+  let hasUnSelectedDescendant = false;
+
+  const traverseDescendants = (itemToTraverseId: TreeViewItemId) => {
+    if (itemToTraverseId !== itemId) {
+      if (instance.isItemSelected(itemToTraverseId)) {
+        hasSelectedDescendant = true;
+      } else {
+        hasUnSelectedDescendant = true;
+      }
+    }
+
+    instance.getItemOrderedChildrenIds(itemToTraverseId).forEach(traverseDescendants);
+  };
+
+  traverseDescendants(itemId);
+
+  return {
+    indeterminate:
+      (hasSelectedDescendant && hasUnSelectedDescendant) || (!hasUnSelectedDescendant && !selected),
+    checked: selectionPropagation.parents ? hasSelectedDescendant : selected,
+  };
+}
+
+export const useTreeViewSelectionItemPlugin: TreeViewItemPlugin<TreeItemProps | TreeItem2Props> = ({
+  props,
+}) => {
+  const { itemId } = props;
+
+  const {
+    instance,
+    selection: { disableSelection, checkboxSelection, selectionPropagation },
+  } = useTreeViewContext<[UseTreeViewItemsSignature, UseTreeViewSelectionSignature]>();
+  return {
+    propsEnhancers: {
+      checkbox: ({
+        externalEventHandlers,
+        interactions,
+        status,
+      }): UseTreeItem2CheckboxSlotPropsFromSelection => {
+        const handleChange = (
+          event: React.ChangeEvent<HTMLInputElement> & TreeViewCancellableEvent,
+        ) => {
+          externalEventHandlers.onChange?.(event);
+          if (event.defaultMuiPrevented) {
+            return;
+          }
+
+          if (disableSelection || status.disabled) {
+            return;
+          }
+
+          interactions.handleCheckboxSelection(event);
+        };
+
+        const checkboxStatus = getCheckboxStatus({
+          instance,
+          itemId,
+          selectionPropagation,
+          selected: status.selected,
+        });
+
+        return {
+          visible: checkboxSelection,
+          disabled: disableSelection || status.disabled,
+          tabIndex: -1,
+          onChange: handleChange,
+          ...checkboxStatus,
+        };
+      },
+    },
+  };
+};
