0d45819a1d6d7df514d6ecdbd1ad5f7a55cbd4a5
alex <alex.fauquette@gmail.com>
2024-10-24 11:26:40 +0200

    fix test unit

--- packages/x-codemod/src/v8.0.0/charts/preset-safe/actual.spec.tsx
+++ packages/x-codemod/src/v8.0.0/charts/preset-safe/actual.spec.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
 
+// prettier-ignore
 <div>
   <PieChart legend={{ hidden: true }} />
   <PieChart legend={{ hidden: true }} slotProps={{ tooltip: { trigger: 'axis' } }} />
--- packages/x-codemod/src/v8.0.0/charts/preset-safe/expected.spec.tsx
+++ packages/x-codemod/src/v8.0.0/charts/preset-safe/expected.spec.tsx
@@ -2,16 +2,15 @@
 import * as React from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
 
+// prettier-ignore
 <div>
   <PieChart
     slotProps={{
       legend: { hidden: true }
-    }}
-  />
+    }} />
   <PieChart
     slotProps={{
       tooltip: { trigger: 'axis' },
       legend: { hidden: true }
-    }}
-  />
+    }} />
 </div>;
