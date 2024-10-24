ef298c58928b224a5f7a96825e49f4a878112634
alex <alex.fauquette@gmail.com>
2024-10-24 10:17:55 +0200

    prettier

--- packages/x-codemod/src/v8.0.0/charts/preset-safe/expected.spec.tsx
+++ packages/x-codemod/src/v8.0.0/charts/preset-safe/expected.spec.tsx
@@ -5,11 +5,13 @@ import { PieChart } from '@mui/x-charts/PieChart';
 <div>
   <PieChart
     slotProps={{
-      legend: { hidden: true }
-    }} />
+      legend: { hidden: true },
+    }}
+  />
   <PieChart
     slotProps={{
       tooltip: { trigger: 'axis' },
-      legend: { hidden: true }
-    }} />
+      legend: { hidden: true },
+    }}
+  />
 </div>;
