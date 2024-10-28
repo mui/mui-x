693e223648a9f9e848c28b00fb3374d2f2d2d902
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-10-28 15:54:23 +0100

    Update packages/x-date-pickers/src/internals/hooks/usePickersPrivateContext.ts
    Co-authored-by: Lukas Tyla <llukas.tyla@gmail.com>
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>

--- packages/x-date-pickers/src/internals/hooks/usePickersPrivateContext.ts
+++ packages/x-date-pickers/src/internals/hooks/usePickersPrivateContext.ts
@@ -9,9 +9,7 @@ export const usePickersPrivateContext = () => {
   const value = React.useContext(PickersContext) as PickersPrivateContextValue | null;
   if (value == null) {
     throw new Error(
-      [
-        'MUI X: The `usePickersPrivateContext` can only be called in components that are used inside a picker component',
-      ].join('\n'),
+        'MUI X: The `usePickersPrivateContext` can only be called in components that are used inside a picker component.',
     );
   }
 
