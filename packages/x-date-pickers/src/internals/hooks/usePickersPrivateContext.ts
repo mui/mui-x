4915d890b4013daa363f2ec3f5a22e30aebee139
flavien <flaviendelangle@gmail.com>
2024-10-29 18:36:14 +0100

    Fix

--- packages/x-date-pickers/src/internals/hooks/usePickersPrivateContext.ts
+++ packages/x-date-pickers/src/internals/hooks/usePickersPrivateContext.ts
@@ -1,12 +1,12 @@
 'use client';
 import * as React from 'react';
-import { PickersContext, PickersPrivateContextValue } from '../components/PickersProvider';
+import { PickersPrivateContext, PickersPrivateContextValue } from '../components/PickersProvider';
 
 /**
  * Returns the private context passed by the picker that wraps the current component.
  */
 export const usePickersPrivateContext = () => {
-  const value = React.useContext(PickersContext) as PickersPrivateContextValue | null;
+  const value = React.useContext(PickersPrivateContext) as PickersPrivateContextValue | null;
   if (value == null) {
     throw new Error(
       'MUI X: The `usePickersPrivateContext` can only be called in components that are used inside a picker component.',
