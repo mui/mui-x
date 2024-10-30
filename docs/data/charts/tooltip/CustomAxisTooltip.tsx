c0b2c77151991154d47afe781f317894157644d1
alex <alex.fauquette@gmail.com>
2024-10-30 10:44:58 +0100

    [charts] Fix tooltip follow mouse

--- docs/data/charts/tooltip/CustomAxisTooltip.tsx
+++ docs/data/charts/tooltip/CustomAxisTooltip.tsx
@@ -5,7 +5,6 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { useAxisTooltip } from '@mui/x-charts/ChartsTooltip';
 import { useSvgRef } from '@mui/x-charts/hooks';
-import { generateVirtualElement } from './generateVirtualElement';
 
 type PointerState = {
   isActive: boolean;
@@ -16,7 +15,7 @@ type PointerState = {
 function usePointer(): PointerState & Pick<PopperProps, 'popperRef' | 'anchorEl'> {
   const svgRef = useSvgRef();
   const popperRef: PopperProps['popperRef'] = React.useRef(null);
-  const virtualElement = React.useRef(generateVirtualElement({ x: 0, y: 0 }));
+  const positionRef = React.useRef({ x: 0, y: 0 });
 
   // Use a ref to avoid rerendering on every mousemove event.
   const [pointer, setPointer] = React.useState<PointerState>({
@@ -49,10 +48,10 @@ function usePointer(): PointerState & Pick<PopperProps, 'popperRef' | 'anchorEl'
     };
 
     const handleMove = (event: PointerEvent) => {
-      virtualElement.current = generateVirtualElement({
+      positionRef.current = {
         x: event.clientX,
         y: event.clientY,
-      });
+      };
       popperRef.current?.update();
     };
 
@@ -67,7 +66,23 @@ function usePointer(): PointerState & Pick<PopperProps, 'popperRef' | 'anchorEl'
     };
   }, [svgRef]);
 
-  return { ...pointer, popperRef, anchorEl: virtualElement.current };
+  return {
+    ...pointer,
+    popperRef,
+    anchorEl: {
+      getBoundingClientRect: () => ({
+        x: positionRef.current.x,
+        y: positionRef.current.y,
+        top: positionRef.current.y,
+        left: positionRef.current.x,
+        right: positionRef.current.x,
+        bottom: positionRef.current.y,
+        width: 0,
+        height: 0,
+        toJSON: () => '',
+      }),
+    },
+  };
 }
 
 export function CustomAxisTooltip() {
