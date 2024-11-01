354cbb9a8b08414a800e464b686656afbc9a0a28
Andrew Cherniavskyi <andrew@mui.com>
2024-11-01 14:00:13 +0100

    add skeleton overlays to visual test

--- test/regressions/data-grid/DataGridOverlays.js
+++ test/regressions/data-grid/DataGridOverlays.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
+function FlexParent({ children, style }) {
+  return <div style={{ display: 'flex', flexDirection: 'column', ...style }}>{children}</div>;
+}
+
 function FlexParentNoRowsOverlay() {
   return (
     <div>
       FlexParentNoRowsOverlay
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <FlexParent>
         <DataGrid columns={[{ field: 'id' }]} />
-      </div>
+      </FlexParent>
     </div>
   );
 }
@@ -16,9 +20,9 @@ function FlexParentMinHeightNoRowsOverlay() {
   return (
     <div>
       FlexParentMinHeightNoRowsOverlay
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: 300 }}>
+      <FlexParent style={{ minHeight: 300 }}>
         <DataGrid columns={[{ field: 'id' }]} />
-      </div>
+      </FlexParent>
     </div>
   );
 }
@@ -27,9 +31,9 @@ function FlexParentMaxHeightNoRowsOverlay() {
   return (
     <div>
       FlexParentMaxHeightNoRowsOverlay
-      <div style={{ display: 'flex', flexDirection: 'column', maxHeight: 200 }}>
+      <FlexParent style={{ maxHeight: 200 }}>
         <DataGrid columns={[{ field: 'id' }]} />
-      </div>
+      </FlexParent>
     </div>
   );
 }
@@ -67,6 +71,82 @@ function PredefinedSizeNoRowsLoadingOverlay() {
   );
 }
 
+function FlexParentSkeletonOverlay() {
+  return (
+    <div>
+      FlexParentSkeletonOverlay
+      <FlexParent>
+        <DataGrid
+          columns={[{ field: 'id' }]}
+          slotProps={{ loadingOverlay: { variant: 'skeleton', noRowsVariant: 'skeleton' } }}
+          loading
+        />
+      </FlexParent>
+    </div>
+  );
+}
+
+function FlexParentMinHeightSkeletonOverlay() {
+  return (
+    <div>
+      FlexParentMinHeightSkeletonOverlay
+      <FlexParent style={{ minHeight: 300 }}>
+        <DataGrid
+          columns={[{ field: 'id' }]}
+          slotProps={{ loadingOverlay: { variant: 'skeleton', noRowsVariant: 'skeleton' } }}
+          loading
+        />
+      </FlexParent>
+    </div>
+  );
+}
+
+function FlexParentMaxHeightSkeletonOverlay() {
+  return (
+    <div>
+      FlexParentMaxHeightSkeletonOverlay
+      <FlexParent style={{ maxHeight: 200 }}>
+        <DataGrid
+          columns={[{ field: 'id' }]}
+          slotProps={{ loadingOverlay: { variant: 'skeleton', noRowsVariant: 'skeleton' } }}
+          loading
+        />
+      </FlexParent>
+    </div>
+  );
+}
+
+function AutoHeightSkeletonOverlay() {
+  return (
+    <div>
+      AutoHeightSkeletonOverlay
+      <div style={{ width: '100%' }}>
+        <DataGrid
+          columns={[{ field: 'id' }]}
+          autoHeight
+          slotProps={{ loadingOverlay: { variant: 'skeleton', noRowsVariant: 'skeleton' } }}
+          loading
+        />
+      </div>
+    </div>
+  );
+}
+
+function PredefinedSizeSkeletonOverlay() {
+  return (
+    <div>
+      PredefinedSizeSkeletonOverlay
+      <div style={{ width: '100%', height: 300 }}>
+        <DataGrid
+          columns={[{ field: 'id' }]}
+          loading
+          slotProps={{ loadingOverlay: { variant: 'skeleton', noRowsVariant: 'skeleton' } }}
+        />
+      </div>
+    </div>
+  );
+}
+
 export default function DataGridFlexParentOverlay() {
   return (
     <div style={{ width: '100%' }}>
@@ -76,6 +156,12 @@ export default function DataGridFlexParentOverlay() {
       <AutoHeightNoRowsOverlay />
       <AutoHeightLoadingOverlay />
       <PredefinedSizeNoRowsLoadingOverlay />
+
+      <FlexParentSkeletonOverlay />
+      <FlexParentMinHeightSkeletonOverlay />
+      <FlexParentMaxHeightSkeletonOverlay />
+      <AutoHeightSkeletonOverlay />
+      <PredefinedSizeSkeletonOverlay />
     </div>
   );
 }
