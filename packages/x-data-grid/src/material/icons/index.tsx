b7c8f44a9323d180997497875415cc19cd0bb894
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2024-11-04 13:25:38 +0000

    update column icon

--- packages/x-data-grid/src/material/icons/index.tsx
+++ packages/x-data-grid/src/material/icons/index.tsx
@@ -46,7 +46,7 @@ export const GridCheckCircleIcon = createSvgIcon(
 );
 
 export const GridColumnIcon = createSvgIcon(
-  <path d="M6 5H3c-.55 0-1 .45-1 1v12c0 .55.45 1 1 1h3c.55 0 1-.45 1-1V6c0-.55-.45-1-1-1zm14 0h-3c-.55 0-1 .45-1 1v12c0 .55.45 1 1 1h3c.55 0 1-.45 1-1V6c0-.55-.45-1-1-1zm-7 0h-3c-.55 0-1 .45-1 1v12c0 .55.45 1 1 1h3c.55 0 1-.45 1-1V6c0-.55-.45-1-1-1z" />,
+  <path d="M14.67 5v14H9.33V5zm1 14H21V5h-5.33zm-7.34 0V5H3v14z" />,
   'ColumnIcon',
 );
 
