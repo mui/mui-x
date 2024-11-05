b43dc971700788f58179f739e4989b75174f82ca
alex <alex.fauquette@gmail.com>
2024-11-05 14:43:59 +0100

    build

--- packages/x-charts-vendor/lib/d3-color.js
+++
@@ -1,6 +0,0 @@
-
-// `x-charts-vendor/d3-color` (CommonJS)
-// See upstream license: https://github.com/d3/d3-color/blob/main/LICENSE
-//
-// Our CommonJS package relies on transpiled vendor files in `lib-vendor/d3-color`
-module.exports = require("../lib-vendor/d3-color/src/index.js");
