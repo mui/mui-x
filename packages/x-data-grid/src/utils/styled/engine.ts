136525df124943c8ba3f0dba9af90d98dbec2a87
Rom Grk <romgrk.cc@gmail.com>
2024-11-05 15:41:57 -0500

    refactor

--- packages/x-data-grid/src/utils/styled/engine.ts
+++ packages/x-data-grid/src/utils/styled/engine.ts
@@ -1,8 +1,5 @@
 import type { CSSObject } from '@mui/system';
-import unitLessProperties from './unitLessProperties';
-
-const SPECIAL_CHAR = /#|\.|\s|>|&/
-const UPPERCASE_LETTERS = /[A-Z]/g
+import { stylesToString } from './stylesToString';
 
 let element = undefined as HTMLStyleElement | undefined
 
@@ -10,52 +7,13 @@ export function injectStyles(selector: string, styles: CSSObject) {
   if (typeof document === 'undefined') { return }
   const style = setup()
   if (process.env.NODE_ENV === 'development') {
-    style.innerHTML += stylesToString(selector, styles) + '\n';
+    style.innerHTML += stylesToString(selector, styles).join('\n') + '\n';
   } else {
-    style.sheet?.insertRule(stylesToString(selector, styles));
-  }
-}
-
-function stylesToString(selector: string, styles: CSSObject, parents: string[] = []) {
-  let output = `${transformSelector(selector, parents)} { `
-
-  for (let key in styles) {
-    const isSubStyles = SPECIAL_CHAR.test(key)
-
-    if (isSubStyles) {
-      output += stylesToString(key, styles[key] as any, parents.concat(selector))
-    } else {
-      const cssKey = key.replaceAll(UPPERCASE_LETTERS, uppercaseToDashLowercase)
-      const cssValue = transformValue(cssKey, styles[key])
-
-      output += cssKey + ':' + cssValue + ';'
+    const rules = stylesToString(selector, styles);
+    for (let i = 0; i < rules.length; i += 1) {
+      style.sheet!.insertRule(rules[i]);
     }
   }
-
-  output += ' } '
-
-  return output
-}
-
-function uppercaseToDashLowercase(char: string) {
-  return '-' + char.toLowerCase()
-}
-
-function transformSelector(selector: string, parents: string[]) {
-  if (selector.includes('&')) {
-    return selector.replaceAll('&', parents.join(' '))
-  }
-  return parents.join(' ') + ' ' + selector
-}
-
-function transformValue(cssKey: string, value: any) {
-  if (typeof value !== 'number') {
-    return value
-  }
-  if (unitLessProperties.has(cssKey)) {
-    return String(value)
-  }
-  return String(value) + 'px'
 }
 
 function setup() {
---
+++ packages/x-data-grid/src/utils/styled/stylesToString.ts
@@ -0,0 +1,83 @@
+import type { CSSObject } from '@mui/system';
+import unitLessProperties from './unitLessProperties';
+
+const SPECIAL_CHAR = /#|\.|\s|>|&/
+const UPPERCASE_LETTERS = /[A-Z]/g
+
+const stack = [] as any[]
+export function stylesToString(selector: string, styles: CSSObject) {
+  stack.length = 0
+  stack.push(selector, styles, '')
+
+  const rules = [] as string[]
+
+  while (stack.length > 0) {
+    const parents = stack.pop()
+    const styles = stack.pop()
+    const selector = stack.pop()
+
+    let output = `${transformSelector(selector, parents)} { `
+
+    for (let key in styles) {
+      const isSubStyles = SPECIAL_CHAR.test(key)
+
+      if (isSubStyles) {
+        stack.push(key, styles[key], parents + ' ' + key)
+      } else {
+        const cssKey = key.replaceAll(UPPERCASE_LETTERS, uppercaseToDashLowercase)
+        const cssValue = transformValue(cssKey, styles[key])
+
+        output += cssKey + ':' + cssValue + ';'
+      }
+    }
+
+    output += ' } '
+
+    rules.push(output)
+  }
+
+  return rules
+}
+
+// TODO: Use native nested CSS if available
+// function stylesToStringNested(selector: string, styles: CSSObject, parents: string[] = []) {
+//   let output = `${transformSelector(selector, parents)} { `
+//
+//   for (let key in styles) {
+//     const isSubStyles = SPECIAL_CHAR.test(key)
+//
+//     if (isSubStyles) {
+//       output += stylesToStringNested(key, styles[key] as any, parents.concat(selector))
+//     } else {
+//       const cssKey = key.replaceAll(UPPERCASE_LETTERS, uppercaseToDashLowercase)
+//       const cssValue = transformValue(cssKey, styles[key])
+//
+//       output += cssKey + ':' + cssValue + ';'
+//     }
+//   }
+//
+//   output += ' } '
+//
+//   return output
+// }
+
+function uppercaseToDashLowercase(char: string) {
+  return '-' + char.toLowerCase()
+}
+
+function transformSelector(selector: string, parents: string[]) {
+  if (selector.includes('&')) {
+    return selector.replaceAll('&', parents.join(' '))
+  }
+  return parents.join(' ') + ' ' + selector
+}
+
+function transformValue(cssKey: string, value: any) {
+  if (typeof value !== 'number') {
+    return value
+  }
+  if (unitLessProperties.has(cssKey)) {
+    return String(value)
+  }
+  return String(value) + 'px'
+}
