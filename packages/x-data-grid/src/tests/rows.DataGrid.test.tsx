1da2274ca9fe2b5cfac7c1a0ddf1e08c802ace02
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2024-11-05 06:46:09 +0000

    fix test

--- packages/x-data-grid/src/tests/rows.DataGrid.test.tsx
+++ packages/x-data-grid/src/tests/rows.DataGrid.test.tsx
@@ -21,6 +21,7 @@ import {
   GridRenderCellParams,
   useGridApiRef,
   GridApi,
+  gridClasses,
 } from '@mui/x-data-grid';
 import { getBasicGridData } from '@mui/x-data-grid-generator';
 import {
@@ -31,6 +32,7 @@ import {
   getActiveCell,
   getCell,
   microtasks,
+  $$,
 } from 'test/utils/helperFn';
 import { fireUserEvent } from 'test/utils/fireUserEvent';
 import Dialog from '@mui/material/Dialog';
