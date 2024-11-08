b141174626f162c4cdaeeeb484daff26f5532e80
Jose Quintas <juniorquintas@gmail.com>
2024-11-08 16:54:46 +0100

    try using globalThis for release info

--- packages/x-charts-pro/src/internals/utils/releaseInfo.ts
+++ packages/x-charts-pro/src/internals/utils/releaseInfo.ts
@@ -2,7 +2,7 @@ import { ponyfillGlobal } from '@mui/utils';
 
 export const getReleaseInfo = () => {
   const releaseInfo = '__RELEASE_INFO__';
-  if (process.env.NODE_ENV !== 'production') {
+  if (globalThis.process.env.NODE_ENV !== 'production') {
     // A simple hack to set the value in the test environment (has no build step).
     // eslint-disable-next-line no-useless-concat
     if (releaseInfo === '__RELEASE' + '_INFO__') {
