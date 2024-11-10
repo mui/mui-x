d1cb4110b14fffee303677dfce6c5933af0ec9ec
CarlosLopezLg <101943016+CarlosLopezLg@users.noreply.github.com>
2024-11-10 14:26:37 -0600

    Update esES.ts
    Signed-off-by: CarlosLopezLg <101943016+CarlosLopezLg@users.noreply.github.com>

--- packages/x-data-grid/src/locales/esES.ts
+++ packages/x-data-grid/src/locales/esES.ts
@@ -58,9 +58,9 @@ const esESGrid: Partial<GridLocaleText> = {
 
   // Filter operators text
   filterOperatorContains: 'contiene',
-  // filterOperatorDoesNotContain: 'does not contain',
+  filterOperatorDoesNotContain: 'no contiene',
   filterOperatorEquals: 'es igual',
-  // filterOperatorDoesNotEqual: 'does not equal',
+  filterOperatorDoesNotEqual: 'es diferente a',
   filterOperatorStartsWith: 'comienza con',
   filterOperatorEndsWith: 'termina con',
   filterOperatorIs: 'es',
@@ -81,9 +81,9 @@ const esESGrid: Partial<GridLocaleText> = {
 
   // Header filter operators text
   headerFilterOperatorContains: 'Contiene',
-  // headerFilterOperatorDoesNotContain: 'Does not contain',
+  headerFilterOperatorDoesNotContain: 'No contiene',
   headerFilterOperatorEquals: 'Es igual a',
-  // headerFilterOperatorDoesNotEqual: 'Does not equal',
+  headerFilterOperatorDoesNotEqual: 'Es diferente a',
   headerFilterOperatorStartsWith: 'Comienza con',
   headerFilterOperatorEndsWith: 'Termina con',
   headerFilterOperatorIs: 'Es',
