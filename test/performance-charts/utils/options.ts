327529afc426240510472a93a6f58dd285272450
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2024-11-11 13:40:56 +0100

    [charts] Filter item outside the drawing area for perf (#14281)
    Co-authored-by: Jose Quintas <juniorquintas@gmail.com>

--- test/performance-charts/utils/options.ts
+++ test/performance-charts/utils/options.ts
@@ -1,7 +1,7 @@
 import { BenchOptions } from 'vitest';
 
-const iterations = process.env.BENCHMARK_ITERATIONS
-  ? parseInt(process.env.BENCHMARK_ITERATIONS, 10)
+const iterations = globalThis.process?.env?.BENCHMARK_ITERATIONS
+  ? parseInt(globalThis.process.env.BENCHMARK_ITERATIONS, 10)
   : 1;
 
 export const options: BenchOptions = {
