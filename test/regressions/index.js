efb0f87af0086d2532007bfcf2cbf61b3bdf6c94
Lukas <llukas.tyla@gmail.com>
2024-11-12 15:12:29 +0200

    Try with v7 flags toggled on

--- test/regressions/index.js
+++ test/regressions/index.js
@@ -142,7 +142,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         {tests.map((test) => {
           const path = computePath(test);
