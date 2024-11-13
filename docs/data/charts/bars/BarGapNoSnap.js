55436a9d39d6ce47023c0173eaac51a2556594b1
Jose C Quintas Jr <juniorquintas@gmail.com>
2024-11-13 10:30:07 +0100

    Update docs/data/charts/bars/BarGapNoSnap.js
    Signed-off-by: Jose C Quintas Jr <juniorquintas@gmail.com>

--- docs/data/charts/bars/BarGapNoSnap.js
+++ docs/data/charts/bars/BarGapNoSnap.js
@@ -72,6 +72,7 @@ export default function BarGapNoSnap() {
           `      categoryGapRatio: ${props.categoryGapRatio}`,
           `      barGapRatio: ${props.barGapRatio}`,
           `    }`,
+          `  ]}`,
           `  maxBarSize={${props.maxBarSize}}`,
           '/>',
         ].join('\n');
