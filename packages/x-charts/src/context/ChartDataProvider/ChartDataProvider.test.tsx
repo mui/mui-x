8b8327a7f379bbe9ad3d60cf213b0b44d4d7944c
Jose Quintas <juniorquintas@gmail.com>
2024-11-13 18:04:28 +0100

    remove irrelevant test

--- packages/x-charts/src/context/ChartDataProvider/ChartDataProvider.test.tsx
+++ packages/x-charts/src/context/ChartDataProvider/ChartDataProvider.test.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { createRenderer, describeConformance } from '@mui/internal-test-utils';
 import { ChartDataProvider } from '@mui/x-charts/context/ChartDataProvider';
-import { ChartsSurface } from '../../ChartsSurface';
 
 describe('<ChartDataProvider />', () => {
   const { render } = createRenderer();
@@ -14,12 +13,10 @@ describe('<ChartDataProvider />', () => {
     testComponentPropWith: 'div',
     refInstanceof: window.SVGSVGElement,
     skip: [
-      // Need `<ChartsSurface />` to work
       'mergeClassName',
       'propsSpread',
       'rootClass',
       'refForwarding',
-      // X
       'componentProp',
       'componentsProp',
       'slotPropsProp',
@@ -31,21 +28,4 @@ describe('<ChartDataProvider />', () => {
       'themeCustomPalette',
     ],
   }));
-
-  describe('with ChartsSurface', () => {
-    describeConformance(
-      <ChartDataProvider height={100} width={100} series={[]}>
-        <ChartsSurface />
-      </ChartDataProvider>,
-      () => ({
-        classes: {} as any,
-        inheritComponent: 'svg',
-        render,
-        muiName: 'MuiChartDataProvider',
-        testComponentPropWith: 'div',
-        refInstanceof: window.SVGSVGElement,
-        only: ['refForwarding', 'mergeClassName', 'propsSpread', 'rootClass'],
-      }),
-    );
-  });
 });
