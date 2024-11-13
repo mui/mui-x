c6aa132e596ad990d36f3211138f0dc1f9d8112e
flavien <flaviendelangle@gmail.com>
2024-11-13 17:30:55 +0100

    [docs] Add proper documentation on how to create custom fields

---
+++ docs/data/date-pickers/custom-field/behavior-tutorial/MaterialDatePicker.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import {
+  DatePicker,
+  DatePickerProps,
+  DatePickerFieldProps,
+} from '@mui/x-date-pickers/DatePicker';
+import { useSplitFieldProps, useParsedFormat } from '@mui/x-date-pickers/hooks';
+import { useValidation, validateDate } from '@mui/x-date-pickers/validation';
+
+function CustomDateField(props: DatePickerFieldProps) {
+  // TextField does not support slots and slotProps before `@mui/material` v6.0
+  const { slots, slotProps, ...other } = props;
+  const { internalProps, forwardedProps } = useSplitFieldProps(other, 'date');
+
+  const { format, timezone, value, onChange } = internalProps;
+  const [inputValue, setInputValue] = React.useState(() =>
+    createInputValue(value, format),
+  );
+
+  const { hasValidationError, getValidationErrorForNewValue } = useValidation({
+    value,
+    timezone,
+    props: internalProps,
+    validator: validateDate,
+  });
+
+  React.useEffect(() => {
+    if (value && value.isValid()) {
+      setInputValue(createInputValue(value, format));
+    }
+  }, [format, value]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newInputValue = event.target.value;
+    const newValue = dayjs(newInputValue, format);
+    setInputValue(newInputValue);
+    onChange(newValue, { validationError: getValidationErrorForNewValue(newValue) });
+  };
+
+  const placeholder = useParsedFormat(internalProps);
+
+  return (
+    <TextField
+      {...forwardedProps}
+      placeholder={placeholder}
+      value={inputValue}
+      onChange={handleChange}
+      error={hasValidationError}
+    />
+  );
+}
+
+function createInputValue(value: Dayjs | null, format: string) {
+  if (value == null) {
+    return '';
+  }
+
+  return value.isValid() ? value.format(format) : '';
+}
+
+function CustomFieldDatePicker(props: DatePickerProps) {
+  return (
+    <DatePicker slots={{ ...props.slots, field: CustomDateField }} {...props} />
+  );
+}
+
+export default function MaterialDatePicker() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <CustomFieldDatePicker />
+    </LocalizationProvider>
+  );
+}
