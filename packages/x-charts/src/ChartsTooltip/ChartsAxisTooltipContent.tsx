ff1d54bad2bd8d80febce4af1b8fca3f6501cb1b
alex <alex.fauquette@gmail.com>
2024-11-14 13:38:41 +0100

    ts-cleaning

--- packages/x-charts/src/ChartsTooltip/ChartsAxisTooltipContent.tsx
+++ packages/x-charts/src/ChartsTooltip/ChartsAxisTooltipContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { SxProps, Theme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import clsx from 'clsx';
@@ -14,18 +15,15 @@ import {
 import { useAxisTooltip } from './useAxisTooltip';
 import { useXAxis, useYAxis } from '../hooks';
 
-export type ChartsAxisContentProps = {
+export interface ChartsAxisTooltipContentProps {
   /**
    * Override or extend the styles applied to the component.
    */
-  classes: ChartsTooltipClasses;
+  classes?: Partial<ChartsTooltipClasses>;
   sx?: SxProps<Theme>;
-};
+}
 
-function ChartsAxisTooltipContent(props: {
-  sx?: SxProps<Theme>;
-  classes: ChartsAxisContentProps['classes'];
-}) {
+function ChartsAxisTooltipContent(props: ChartsAxisTooltipContentProps) {
   const { classes: propClasses, sx } = props;
   const tootlipData = useAxisTooltip();
   const xAxis = useXAxis();
@@ -78,5 +76,21 @@ function ChartsAxisTooltipContent(props: {
     </ChartsTooltipPaper>
   );
 }
+
+ChartsAxisTooltipContent.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "pnpm proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * Override or extend the styles applied to the component.
+   */
+  classes: PropTypes.object,
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+} as any;
 
 export { ChartsAxisTooltipContent };
--- packages/x-charts/src/ChartsTooltip/ChartsItemTooltipContent.tsx
+++ packages/x-charts/src/ChartsTooltip/ChartsItemTooltipContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { SxProps, Theme } from '@mui/material/styles';
 import { ChartsTooltipClasses, useUtilityClasses } from './chartsTooltipClasses';
@@ -13,8 +14,11 @@ import {
 } from './ChartsTooltipTable';
 
 export interface ChartsItemTooltipContentProps {
+  /**
+   * Override or extend the styles applied to the component.
+   */
+  classes?: Partial<ChartsTooltipClasses>;
   sx?: SxProps<Theme>;
-  classes?: ChartsTooltipClasses;
 }
 
 function ChartsItemTooltipContent(props: ChartsItemTooltipContentProps) {
@@ -48,5 +52,21 @@ function ChartsItemTooltipContent(props: ChartsItemTooltipContentProps) {
     </ChartsTooltipPaper>
   );
 }
+
+ChartsItemTooltipContent.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "pnpm proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * Override or extend the styles applied to the component.
+   */
+  classes: PropTypes.object,
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+} as any;
 
 export { ChartsItemTooltipContent };
