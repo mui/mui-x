049a6afed640a37df5ee9163ff8e0e52ccb0a548
Armin Mehinovic <armin@mui.com>
2024-11-14 19:44:40 +0100

    lint

--- packages/x-data-grid-premium/src/components/promptControl/GridToolbarPromptControl.tsx
+++ packages/x-data-grid-premium/src/components/promptControl/GridToolbarPromptControl.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
 import composeClasses from '@mui/utils/composeClasses';
 import useEventCallback from '@mui/utils/useEventCallback';
