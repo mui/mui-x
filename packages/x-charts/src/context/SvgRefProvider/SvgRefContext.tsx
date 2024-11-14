bb2ae830f015aabf59a8eb709e805c7f1fa6eaf6
Jose Quintas <juniorquintas@gmail.com>
2024-11-14 11:41:00 +0100

    Split drawing and svg ref providers

---
+++ packages/x-charts/src/context/SvgRefProvider/SvgRefContext.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Initializable } from '../context.types';
+import { SvgRefState } from './SvgRef.types';
+
+export const SvgRefContext = React.createContext<Initializable<SvgRefState>>({
+  isInitialized: false,
+  data: { current: null },
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  SvgRefContext.displayName = 'SvgRefContext';
+}
--- packages/x-charts/src/internals/index.ts
+++ packages/x-charts/src/internals/index.ts
@@ -27,7 +27,7 @@ export * from './computeAxisValue';
 // contexts
 
 export * from '../context/CartesianProvider';
-export * from '../context/DrawingProvider';
+export * from '../context/DrawingAreaProvider';
 export * from '../context/InteractionProvider';
 export * from '../context/SeriesProvider';
 export * from '../context/ZAxisContextProvider';
