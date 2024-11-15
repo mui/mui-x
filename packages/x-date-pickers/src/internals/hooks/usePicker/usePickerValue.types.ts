d1ca6a413c8b9196e12edb0ac11fe079ed44308c
flavien <flaviendelangle@gmail.com>
2024-11-15 11:36:23 +0100

    Fix

--- packages/x-date-pickers/src/internals/hooks/usePicker/usePickerValue.types.ts
+++ packages/x-date-pickers/src/internals/hooks/usePicker/usePickerValue.types.ts
@@ -2,11 +2,6 @@ import { FieldChangeHandlerContext, UseFieldInternalProps } from '../useField';
 import { Validator } from '../../../validation';
 import { WrapperVariant } from '../../models/common';
 import {
-<<<<<<< HEAD
-  FieldValueType,
-=======
-  FieldSection,
->>>>>>> master
   TimezoneProps,
   MuiPickersAdapter,
   PickersTimezone,
@@ -46,24 +41,14 @@ export interface PickerValueManager<TIsRange extends boolean, TError> {
    * @template TIsRange `true` if the value comes from a range picker, `false` otherwise.
    * @param {MuiPickersAdapter} utils The adapter.
    * @param {PickersTimezone} timezone The current timezone.
-<<<<<<< HEAD
-   * @param {FieldValueType} valueType The type of the value being edited.
-   * @returns {InferPickerValue<TIsRange>} The value to set when clicking the "Today" button.
-=======
    * @param {PickerValueType} valueType The type of the value being edited.
-   * @returns {TValue} The value to set when clicking the "Today" button.
->>>>>>> master
+   * @returns {InferPickerValue<TIsRange>} The value to set when clicking the "Today" button.
    */
   getTodayValue: (
     utils: MuiPickersAdapter,
     timezone: PickersTimezone,
-<<<<<<< HEAD
-    valueType: FieldValueType,
-  ) => InferPickerValue<TIsRange>;
-=======
     valueType: PickerValueType,
-  ) => TValue;
->>>>>>> master
+  ) => InferPickerValue<TIsRange>;
   /**
    * @template TIsRange `true` if the value comes from a range picker, `false` otherwise.
    * Method returning the reference value to use when mounting the component.
@@ -301,13 +286,8 @@ export interface UsePickerValueParams<
   TExternalProps extends UsePickerValueProps<TIsRange, any>,
 > {
   props: TExternalProps;
-<<<<<<< HEAD
   valueManager: PickerValueManager<TIsRange, InferError<TExternalProps>>;
-  valueType: FieldValueType;
-=======
-  valueManager: PickerValueManager<TValue, InferError<TExternalProps>>;
   valueType: PickerValueType;
->>>>>>> master
   wrapperVariant: WrapperVariant;
   validator: Validator<TIsRange, InferError<TExternalProps>, TExternalProps>;
 }
