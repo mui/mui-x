886e7af72ab5965660e5a675bdf5dc5a4ba8e567
alex <alex.fauquette@gmail.com>
2024-11-15 10:33:03 +0100

    fix-dash-line

--- docs/data/charts/line-demo/LineWithPrediction.js
+++ docs/data/charts/line-demo/LineWithPrediction.js
@@ -40,11 +40,11 @@ function CustomAnimatedLine(props) {
           height={top + height + bottom}
         />
       </clipPath>
-      <g clipPath={`url(#${clipIdleft})`}>
-        <AnimatedLine {...other} sx={sxBefore} />
+      <g clipPath={`url(#${clipIdleft})`} className="line-before">
+        <AnimatedLine {...other} />
       </g>
-      <g clipPath={`url(#${clipIdRight})`}>
-        <AnimatedLine {...other} sx={sxAfter} />
+      <g clipPath={`url(#${clipIdRight})`} className="line-after">
+        <AnimatedLine {...other} />
       </g>
     </React.Fragment>
   );
@@ -64,7 +64,8 @@ export default function LineWithPrediction() {
       height={200}
       width={400}
       slots={{ line: CustomAnimatedLine }}
-      slotProps={{ line: { limit: 5, sxAfter: { strokeDasharray: '10 5' } } }}
+      slotProps={{ line: { limit: 5 } }}
+      sx={{ '& .line-after path': { strokeDasharray: '10 5' } }}
     />
   );
 }
