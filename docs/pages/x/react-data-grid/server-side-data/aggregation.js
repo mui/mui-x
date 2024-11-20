340acbde76bbad3d7925f3c374cb9f23a8d7f924
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-11-20 01:55:57 +0100

    [docs][data grid] Polish Server-side data section (#15330)

--- docs/pages/x/react-data-grid/server-side-data/aggregation.js
+++ docs/pages/x/react-data-grid/server-side-data/aggregation.js
@@ -3,5 +3,5 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 import * as pageProps from 'docsx/data/data-grid/server-side-data/aggregation.md?muiMarkdown';
 
 export default function Page() {
-  return <MarkdownDocs {...pageProps} />;
+  return <MarkdownDocs disableAd {...pageProps} />;
 }
