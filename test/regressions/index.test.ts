e040b42b04fa02ce68c0054f37f1be576a4403fe
Jose Quintas <juniorquintas@gmail.com>
2024-11-20 12:24:30 +0100

    use more native behaviours

--- test/regressions/index.test.ts
+++ test/regressions/index.test.ts
@@ -2,7 +2,7 @@ import * as fse from 'fs-extra';
 import { expect } from 'chai';
 import * as path from 'path';
 import * as childProcess from 'child_process';
-import { chromium } from '@playwright/test';
+import { chromium, Page } from '@playwright/test';
 import materialPackageJson from '@mui/material/package.json';
 
 function sleep(timeoutMS: number | undefined) {
@@ -15,6 +15,19 @@ const isMaterialUIv6 = materialPackageJson.version.startsWith('6.');
 
 // Tests that need a longer timeout.
 const timeSensitiveSuites = ['ColumnAutosizingAsync', 'DensitySelectorGrid'];
+
+async function navigateToTest(page: Page, testIndex: number) {
+  const css = `#tests li:nth-of-type(${testIndex}) a`;
+  try {
+    await page.locator(css).click();
+  } catch (error) {
+    // When one demo crashes, the page becomes empty and there are no links to demos,
+    // so navigation to the next demo throws an error.
+    // Reloading the page fixes this.
+    await page.reload();
+    await page.locator(css).click();
+  }
+}
 
 const isConsoleWarningIgnored = (msg?: string) => {
   if (
@@ -86,16 +99,6 @@ async function main() {
   // prepare screenshots
   await fse.emptyDir(screenshotDir);
 
-  function navigateToTest(testIndex: number) {
-    // Use client-side routing which is much faster than full page navigation via page.goto().
-    // Could become an issue with test isolation.
-    // If tests are flaky due to global pollution switch to page.goto(route);
-    // puppeteers built-in click() times out
-    return page.$eval(`#tests li:nth-of-type(${testIndex}) a`, (link) => {
-      (link as HTMLAnchorElement).click();
-    });
-  }
-
   describe('visual regressions', () => {
     after(async () => {
       await browser.close();
@@ -124,13 +127,13 @@ async function main() {
         }
 
         try {
-          await navigateToTest(index + 1);
+          await navigateToTest(page, index);
         } catch (error) {
           // When one demo crashes, the page becomes empty and there are no links to demos,
           // so navigation to the next demo throws an error.
           // Reloading the page fixes this.
           await page.reload();
-          await navigateToTest(index + 1);
+          await navigateToTest(page, index);
         }
         // Move cursor offscreen to not trigger unwanted hover effects.
         await page.mouse.move(0, 0);
@@ -192,9 +195,7 @@ async function main() {
       await fse.ensureDir(path.dirname(screenshotPath));
 
       const testcaseIndex = routes.indexOf(route);
-      await page.$eval(`#tests li:nth-of-type(${testcaseIndex + 1}) a`, (link) => {
-        (link as HTMLAnchorElement).click();
-      });
+      await navigateToTest(page, testcaseIndex);
 
       const testcase = await page.waitForSelector(
         '[data-testid="testcase"]:not([aria-busy="true"])',
@@ -222,14 +223,10 @@ async function main() {
       await fse.ensureDir(path.dirname(screenshotPath));
 
       const testcaseIndex = routes.indexOf(route);
-      await page.$eval(`#tests li:nth-of-type(${testcaseIndex + 1}) a`, (link) => {
-        (link as HTMLAnchorElement).click();
-      });
+      await navigateToTest(page, testcaseIndex);
 
       // Click the export button in the toolbar.
-      await page.$eval(`button[aria-label="Export"]`, (exportButton) => {
-        (exportButton as HTMLAnchorElement).click();
-      });
+      await page.getByAltText('Export').click();
 
       // Click the print export option from the export menu in the toolbar.
       await page.$eval(`li[role="menuitem"]:last-child`, (printButton) => {
