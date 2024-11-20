34364ebd503b2e4a40baaf424a9f7df20de42e82
Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>
2024-11-20 09:10:15 +0100

    Improve mock prompt processor query match
    Co-authored-by: Andrew Cherniavskii <andrew.cherniavskii@gmail.com>
    Signed-off-by: Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>

--- packages/x-data-grid-generator/src/services/prompt-resolver.ts
+++ packages/x-data-grid-generator/src/services/prompt-resolver.ts
@@ -2,7 +2,7 @@ import { unstable_PromptResponse as PromptResponse } from '@mui/x-data-grid-prem
 import { mockPrompts } from '../constants/prompts';
 
 export const mockPromptResolver = (_: string, query: string) => {
-  const resolved = mockPrompts.get(query);
+  const resolved = mockPrompts.get(query.toLowerCase().trim());
 
   return new Promise<PromptResponse>((resolve, reject) => {
     setTimeout(() => {
