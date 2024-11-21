fa9d058c728061a9e53456807f4391bb619c3e14
Jose C Quintas Jr <juniorquintas@gmail.com>
2024-11-21 15:51:22 +0100

    [code-infra] Add documentation to internal types (#15540)

--- packages/x-internals/src/types/index.ts
+++ packages/x-internals/src/types/index.ts
@@ -1,17 +1,4 @@
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
-
-export type MakeRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
-
-export type DefaultizedProps<
-  P extends {},
-  RequiredProps extends keyof P,
-  AdditionalProps extends {} = {},
-> = Omit<P, RequiredProps | keyof AdditionalProps> &
-  Required<Pick<P, RequiredProps>> &
-  AdditionalProps;
-
-export type SlotComponentPropsFromProps<
-  TProps extends {},
-  TOverrides extends {},
-  TOwnerState extends {},
-> = (Partial<TProps> & TOverrides) | ((ownerState: TOwnerState) => Partial<TProps> & TOverrides);
+export * from './DefaultizedProps';
+export * from './MakeOptional';
+export * from './MakeRequired';
+export * from './SlotComponentPropsFromProps';
