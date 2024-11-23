7675db26dc4f76b0880a0a8af40b5a42f47a0cfa
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-11-23 11:43:50 +0100

    complete it

--- packages/x-data-grid/src/locales/ptPT.ts
+++ packages/x-data-grid/src/locales/ptPT.ts
@@ -56,7 +56,7 @@ const ptPTGrid: Partial<GridLocaleText> = {
   columnsManagementNoColumns: 'Sem colunas',
   columnsManagementShowHideAllText: 'Mostrar/Ocultar Todas',
   columnsManagementReset: 'Repor',
-  // columnsManagementDeleteIconLabel: 'Clear',
+  columnsManagementDeleteIconLabel: 'Limpar',
 
   // Filter panel text
   filterPanelAddFilter: 'Adicionar filtro',
