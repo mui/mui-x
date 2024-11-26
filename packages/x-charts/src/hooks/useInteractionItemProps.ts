6d224a3f4ee7ae6ce0789688113f2293c89104c5
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2024-11-26 12:28:11 +0100

    [charts] Prevent invalid `releasePointerCapture` (#15602)

--- packages/x-charts/src/hooks/useInteractionItemProps.ts
+++ packages/x-charts/src/hooks/useInteractionItemProps.ts
@@ -28,8 +28,29 @@ export const useInteractionItemProps = (skip?: boolean) => {
       });
     };
     const onPointerLeave = (event: React.PointerEvent) => {
-      event.currentTarget.releasePointerCapture(event.pointerId);
-      dispatchInteraction({ type: 'leaveItem', data });
+      if (event.currentTarget.hasPointerCapture(event.pointerId)) {
+        event.currentTarget.releasePointerCapture(event.pointerId);
+      }
+
+      store.update((prev) => {
+        const prevItem = prev.interaction.item;
+        if (
+          prevItem === null ||
+          Object.keys(data).some(
+            (key) => data[key as keyof typeof data] !== prevItem[key as keyof typeof prevItem],
+          )
+        ) {
+          // The item is already something else, no need to clean it.
+          return prev;
+        }
+        return {
+          ...prev,
+          interaction: {
+            ...prev.interaction,
+            item: null,
+          },
+        };
+      });
       clearHighlighted();
     };
     return {
