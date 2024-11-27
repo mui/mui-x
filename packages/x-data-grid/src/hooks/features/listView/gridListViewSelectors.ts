d8c11e0bf3c210f36dd80e61b02537350e470152
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2024-11-27 13:54:10 +0000

    do not document gridListColumnSelector

--- packages/x-data-grid/src/hooks/features/listView/gridListViewSelectors.ts
+++ packages/x-data-grid/src/hooks/features/listView/gridListViewSelectors.ts
@@ -3,5 +3,6 @@ import { GridStateCommunity } from '../../../models/gridStateCommunity';
 /**
  * Get the list column definition
  * @category List View
+ * @ignore - Do not document
  */
 export const gridListColumnSelector = (state: GridStateCommunity) => state.listViewColumn;
