d9d7ddd546782ce070739225bf60bc401f137f6e
alex <alex.fauquette@gmail.com>
2024-11-27 13:28:27 +0100

    [chart] Fix custom Tooltip demos

--- docs/data/charts/tooltip/CustomItemTooltip.tsx
+++ docs/data/charts/tooltip/CustomItemTooltip.tsx
@@ -7,38 +7,35 @@ import { ChartsTooltipContainer, useItemTooltip } from '@mui/x-charts/ChartsTool
 export function CustomItemTooltip() {
   const tooltipData = useItemTooltip();
 
-  if (!tooltipData) {
-    // No data to display
-    return null;
-  }
-
   return (
     <ChartsTooltipContainer trigger="item">
-      <Paper
-        elevation={0}
-        sx={{
-          m: 1,
-          p: 1.5,
-          border: 'solid',
-          borderWidth: 2,
-          borderColor: 'divider',
-        }}
-      >
-        <Stack direction="row" alignItems="center">
-          <div
-            style={{
-              width: 11,
-              height: 11,
-              borderRadius: '50%',
-              backgroundColor: tooltipData.color,
-            }}
-          />
-          <Typography sx={{ ml: 2 }} fontWeight="light">
-            {tooltipData.label}
-          </Typography>
-          <Typography sx={{ ml: 2 }}>{tooltipData.formattedValue}</Typography>
-        </Stack>
-      </Paper>
+      {tooltipData && (
+        <Paper
+          elevation={0}
+          sx={{
+            m: 1,
+            p: 1.5,
+            border: 'solid',
+            borderWidth: 2,
+            borderColor: 'divider',
+          }}
+        >
+          <Stack direction="row" alignItems="center">
+            <div
+              style={{
+                width: 11,
+                height: 11,
+                borderRadius: '50%',
+                backgroundColor: tooltipData.color,
+              }}
+            />
+            <Typography sx={{ ml: 2 }} fontWeight="light">
+              {tooltipData.label}
+            </Typography>
+            <Typography sx={{ ml: 2 }}>{tooltipData.formattedValue}</Typography>
+          </Stack>
+        </Paper>
+      )}
     </ChartsTooltipContainer>
   );
 }
