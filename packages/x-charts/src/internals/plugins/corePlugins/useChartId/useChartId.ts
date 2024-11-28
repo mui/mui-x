4e806a5b37e7ac1cb544ffba348aa987579da9c3
alex <alex.fauquette@gmail.com>
2024-11-28 14:04:14 +0100

    test perf

--- packages/x-charts/src/internals/plugins/corePlugins/useChartId/useChartId.ts
+++ packages/x-charts/src/internals/plugins/corePlugins/useChartId/useChartId.ts
@@ -1,21 +1,24 @@
-import * as React from 'react';
+// import * as React from 'react';
+import useId from '@mui/utils/useId';
 import { ChartPlugin } from '../../models';
 import { UseChartIdSignature } from './useChartId.types';
-import { createChartDefaultId } from './useChartId.utils';
+// import { createChartDefaultId } from './useChartId.utils';
 
 export const useChartId: ChartPlugin<UseChartIdSignature> = ({ params, store }) => {
-  React.useEffect(() => {
-    store.update((prevState) => {
-      if (params.id === prevState.id.providedChartId && prevState.id.chartId !== undefined) {
-        return prevState;
-      }
+  const id = useId();
 
-      return {
-        ...prevState,
-        id: { ...prevState.id, chartId: params.id ?? createChartDefaultId() },
-      };
-    });
-  }, [store, params.id]);
+  // React.useEffect(() => {
+  store.update((prevState) => {
+    if (params.id === prevState.id.providedChartId && prevState.id.chartId !== undefined) {
+      return prevState;
+    }
+
+    return {
+      ...prevState,
+      id: { ...prevState.id, chartId: params.id ?? `mui-chart-${id}u` },
+    };
+  });
+  // }, [store, params.id]);
   return {};
 };
 
