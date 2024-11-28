f544cd7063d0488f81c8d2e2b45103f6821ce00f
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2024-11-28 16:57:07 +0000

    add class to toggle button group

--- packages/x-data-grid/src/components/grid/toolbar/GridToolbarToggleButtonGroup.tsx
+++ packages/x-data-grid/src/components/grid/toolbar/GridToolbarToggleButtonGroup.tsx
@@ -1,15 +1,37 @@
 import * as React from 'react';
+import clsx from 'clsx';
 import { ToggleButtonGroupProps } from '@mui/material/ToggleButtonGroup';
+import composeClasses from '@mui/utils/composeClasses';
 import { useGridRootProps } from '../../../hooks/utils/useGridRootProps';
+import { DataGridProcessedProps } from '../../../models/props/DataGridProps';
+import { getDataGridUtilityClass } from '../../../constants/gridClasses';
 
 export type GridToolbarToggleButtonGroupProps = ToggleButtonGroupProps;
 
+type OwnerState = DataGridProcessedProps;
+
+const useUtilityClasses = (ownerState: OwnerState) => {
+  const { classes } = ownerState;
+
+  const slots = {
+    root: ['toolbarToggleButtonGroup'],
+  };
+
+  return composeClasses(slots, getDataGridUtilityClass, classes);
+};
+
 function GridToolbarToggleButtonGroup(props: GridToolbarToggleButtonGroupProps) {
   const rootProps = useGridRootProps();
-  const { children, ...other } = props;
+  const classes = useUtilityClasses(rootProps);
+  const { children, className, ...other } = props;
 
   return (
-    <rootProps.slots.baseToggleButtonGroup color="primary" size="small" {...other}>
+    <rootProps.slots.baseToggleButtonGroup
+      color="primary"
+      size="small"
+      className={clsx(classes.root, className)}
+      {...other}
+    >
       {children}
     </rootProps.slots.baseToggleButtonGroup>
   );
