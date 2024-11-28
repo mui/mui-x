6d5f099d46cf6d6db451e8c0df814a25e0c69616
Jose C Quintas Jr <juniorquintas@gmail.com>
2024-11-28 16:13:22 +0100

    [charts] Remove `labelFontSize` and `tickFontSize` props (#15633)
    Signed-off-by: Jose C Quintas Jr <juniorquintas@gmail.com>
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>

--- packages/x-codemod/src/v8.0.0/charts/rename-label-and-tick-font-size/expected.spec.tsx
+++ packages/x-codemod/src/v8.0.0/charts/rename-label-and-tick-font-size/expected.spec.tsx
@@ -1,24 +1,9 @@
 // @ts-nocheck
 import * as React from 'react';
-import { PieChart } from '@mui/x-charts/PieChart';
-import { BarPlot } from '@mui/x-charts/BarChart';
-import { ChartContainer } from '@mui/x-charts/ChartContainer';
 import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
 
 // prettier-ignore
 <div>
-  <PieChart
-    slotProps={{
-      legend: { hidden: true }
-    }} />
-  <PieChart
-    slotProps={{
-      tooltip: { trigger: 'axis' },
-      legend: { hidden: true }
-    }} />
-  <ChartContainer>
-    <BarPlot />
-  </ChartContainer>
   <ChartsXAxis
     labelStyle={{
       fontSize: 18
