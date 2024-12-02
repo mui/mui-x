957e54593b7df4c77ea7ba5bd3b480bb10aae720
Jose C Quintas Jr <juniorquintas@gmail.com>
2024-12-02 17:05:47 +0100

    [code-infra] Import Pickers `preset-safe` into global codemod config (#15659)

--- packages/x-codemod/src/v8.0.0/preset-safe/index.ts
+++ packages/x-codemod/src/v8.0.0/preset-safe/index.ts
@@ -1,9 +1,11 @@
 import transformTreeView from '../tree-view/preset-safe';
+import transformPickers from '../pickers/preset-safe';
 import transformCharts from '../charts/preset-safe';
 import { JsCodeShiftAPI, JsCodeShiftFileInfo } from '../../types';
 
 export default function transformer(file: JsCodeShiftFileInfo, api: JsCodeShiftAPI, options: any) {
   file.source = transformTreeView(file, api, options);
+  file.source = transformPickers(file, api, options);
   file.source = transformCharts(file, api, options);
 
   return file.source;
--- packages/x-codemod/src/v8.0.0/preset-safe/preset-safe.test.ts
+++ packages/x-codemod/src/v8.0.0/preset-safe/preset-safe.test.ts
@@ -1,27 +1,58 @@
 import path from 'path';
 import { expect } from 'chai';
 import jscodeshift from 'jscodeshift';
+import fs from 'fs';
 import transform from '.';
 import readFile from '../../util/readFile';
 
 function read(fileName) {
-  return readFile(path.join(__dirname, fileName));
+  return readFile(fileName);
+}
+
+function getAllDirs() {
+  return fs
+    .readdirSync(path.resolve(__dirname, '..'))
+    .filter(
+      (file) =>
+        fs.statSync(path.resolve(__dirname, '..', file)).isDirectory() && file !== 'preset-safe',
+    );
 }
 
 describe('v8.0.0', () => {
+  const MOD_DIRS = getAllDirs();
+
   describe('preset-safe', () => {
-    it('transforms code as needed', () => {
-      const actual = transform({ source: read('./actual.spec.js') }, { jscodeshift }, {});
+    MOD_DIRS.forEach((testDir) => {
+      const actualPath = path.resolve(__dirname, '..', testDir, 'preset-safe', 'actual.spec.tsx');
+      const expectedPath = path.resolve(
+        __dirname,
+        '..',
+        testDir,
+        'preset-safe',
+        'expected.spec.tsx',
+      );
 
-      const expected = read('./expected.spec.js');
-      expect(actual).to.equal(expected, 'The transformed version should be correct');
-    });
+      describe(`${testDir.replace(/-/g, ' ')}`, () => {
+        it('transforms code as needed', () => {
+          const actual = transform(
+            { source: read(actualPath) },
+            { jscodeshift: jscodeshift.withParser('tsx') },
+            {},
+          );
+          const expected = read(expectedPath);
+          expect(actual).to.equal(expected, 'The transformed version should be correct');
+        });
 
-    it('should be idempotent', () => {
-      const actual = transform({ source: read('./expected.spec.js') }, { jscodeshift }, {});
-
-      const expected = read('./expected.spec.js');
-      expect(actual).to.equal(expected, 'The transformed version should be correct');
+        it('should be idempotent', () => {
+          const actual = transform(
+            { source: read(expectedPath) },
+            { jscodeshift: jscodeshift.withParser('tsx') },
+            {},
+          );
+          const expected = read(expectedPath);
+          expect(actual).to.equal(expected, 'The transformed version should be correct');
+        });
+      });
     });
   });
 });
