6f89780d1028995a571a31272fb5b68c820edcdc
Armin Mehinovic <armin@mui.com>
2024-12-02 10:47:12 +0100

    Remove flag. Add codemod

--- packages/x-data-grid-premium/src/DataGridPremium/DataGridPremium.tsx
+++ packages/x-data-grid-premium/src/DataGridPremium/DataGridPremium.tsx
@@ -329,7 +329,6 @@ DataGridPremiumRaw.propTypes = {
    * For each feature, if the flag is not explicitly set to `true`, then the feature is fully disabled, and neither property nor method calls will have any effect.
    */
   experimentalFeatures: PropTypes.shape({
-    ariaV8: PropTypes.bool,
     warnIfFocusStateIsNotSynced: PropTypes.bool,
   }),
   /**
