92efaec7c0dc464cfe69f90e45cd76e9d5290fc4
Jose Quintas <juniorquintas@gmail.com>
2024-12-04 11:47:18 +0100

    use pattern transform to scale pattern

--- docs/data/charts/styling/PatternPie.tsx
+++ docs/data/charts/styling/PatternPie.tsx
@@ -29,7 +29,7 @@ export default function PatternPie() {
         patternUnits="userSpaceOnUse"
         width="20"
         height="40"
-        patternTransform="scale(2) rotate(0)"
+        patternTransform="scale(0.5)"
       >
         <rect x="0" y="0" width="100%" height="100%" fill="#123456" />
         <path
