5cc425f78bc0bcd40fd87c5cf91214e688c50b87
lauri865 <lauri.lehtmaa@gmail.com>
2024-12-05 01:13:16 +0100

    test

--- packages/x-data-grid/src/hooks/utils/useGridSelector.ts
+++ packages/x-data-grid/src/hooks/utils/useGridSelector.ts
@@ -98,7 +98,7 @@ export const useGridSelector = <Api extends GridApiCommon, T>(
   refs.current.selector = selector;
 
   useOnMount(() => {
-    return apiRef.current.store.subscribe((state) => {
+    return apiRef.current.store.subscribe(() => {
       const newState = applySelector(apiRef, refs.current.selector);
       if (!refs.current.equals(refs.current.state, newState)) {
         refs.current.state = newState;
