b5e389cefb53d08054e53fd33f9f52fbe2358bca
flavien <flaviendelangle@gmail.com>
2024-12-06 15:02:16 +0100

    [pickers] Use the new ownerState in the toolbar components

--- packages/x-date-pickers/src/internals/index.ts
+++ packages/x-date-pickers/src/internals/index.ts
@@ -92,6 +92,8 @@ export type {
   UseStaticPickerSlots,
   UseStaticPickerSlotProps,
 } from './hooks/useStaticPicker';
+export { useToolbarOwnerState } from './hooks/useToolbarOwnerState';
+export type { PickerToolbarOwnerState } from './hooks/useToolbarOwnerState';
 export { useLocalizationContext, useDefaultDates, useUtils, useNow } from './hooks/useUtils';
 export type { ExportedUseViewsOptions, UseViewsOptions } from './hooks/useViews';
 export { useViews } from './hooks/useViews';
