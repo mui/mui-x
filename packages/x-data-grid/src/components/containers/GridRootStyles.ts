f643319b203f4d533dd25fb09d24c51baa6bedf8
Rom Grk <romgrk.cc@gmail.com>
2024-12-09 11:29:08 -0500

    lint

--- packages/x-data-grid/src/components/containers/GridRootStyles.ts
+++ packages/x-data-grid/src/components/containers/GridRootStyles.ts
@@ -212,7 +212,7 @@ export const GridRootStyles = styled('div', {
     borderStyle: 'solid',
     borderColor: vars.colors.border.base,
     borderRadius: 'var(--unstable_DataGrid-radius)',
-    color: t.palette.text.primary,
+    color: vars.colors.foreground.base,
     ...vars.typography.body,
     outline: 'none',
     height: '100%',
