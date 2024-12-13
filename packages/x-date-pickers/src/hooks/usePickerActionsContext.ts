41cfc402e1509f40b91ccb6df7641743d0e8ef56
Flavien DELANGLE <flaviendelangle@gmail.com>
2024-12-13 18:30:31 +0100

    Update packages/x-date-pickers/src/hooks/usePickerActionsContext.ts
    Co-authored-by: Michel Engelen <32863416+michelengelen@users.noreply.github.com>
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>

--- packages/x-date-pickers/src/hooks/usePickerActionsContext.ts
+++ packages/x-date-pickers/src/hooks/usePickerActionsContext.ts
@@ -4,7 +4,7 @@ import { PickerActionsContext } from '../internals/components/PickerProvider';
 
 /**
  * Returns a subset of the context passed by the picker wrapping the current component.
- * If only contain the actions and never causes a re-render of the component using it.
+ * It only contains the actions and never causes a re-render of the component using it.
  */
 export const usePickerActionsContext = () => {
   const value = React.useContext(PickerActionsContext);
