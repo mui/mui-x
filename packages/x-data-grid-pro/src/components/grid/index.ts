5b8ee6976dc79370c079de6dcd7353efa588d777
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2024-12-18 09:44:26 +0000

    update pro import

--- packages/x-data-grid-pro/src/components/grid/index.ts
+++ packages/x-data-grid-pro/src/components/grid/index.ts
@@ -1,10 +1,7 @@
 /* eslint-disable no-restricted-imports */
 
 // Components without pro features
-export * from '@mui/x-data-grid/components/grid/columnsPanel';
-export * from '@mui/x-data-grid/components/grid/filterPanel';
-export * from '@mui/x-data-grid/components/grid/toolbar';
-export * from '@mui/x-data-grid/components/grid/export';
+export * from '@mui/x-data-grid/components/grid';
 
 // Components with pro features
 // export * from './some-component';
