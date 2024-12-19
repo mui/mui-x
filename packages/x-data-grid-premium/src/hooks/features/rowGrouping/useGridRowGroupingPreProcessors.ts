68b9f2a9dc791afdaa27a0972a476a120e63c064
Armin Mehinovic <armin@mui.com>
2024-12-19 20:56:34 +0100

    Simplify logic

--- packages/x-data-grid-premium/src/hooks/features/rowGrouping/useGridRowGroupingPreProcessors.ts
+++ packages/x-data-grid-premium/src/hooks/features/rowGrouping/useGridRowGroupingPreProcessors.ts
@@ -116,14 +116,13 @@ export const useGridRowGroupingPreProcessors = (
       const groupingColDefs = getGroupingColDefs(columnsState);
       let newColumnFields: string[] = [];
       const newColumnsLookup: GridColumnRawLookup = {};
-      const prevGroupingfields = columnsState.orderedFields.filter((field) =>
-        isGroupingColumn(field),
-      );
-      const currentGroupingfields: string[] = [];
+      const prevGroupingfields: string[] = [];
 
       // We only keep the non-grouping columns
       columnsState.orderedFields.forEach((field) => {
-        if (!isGroupingColumn(field)) {
+        if (isGroupingColumn(field)) {
+          prevGroupingfields.push(field);
+        } else {
           newColumnFields.push(field);
           newColumnsLookup[field] = columnsState.lookup[field];
         }
@@ -132,7 +131,6 @@ export const useGridRowGroupingPreProcessors = (
       // We add the grouping column
       groupingColDefs.forEach((groupingColDef) => {
         const matchingGroupingColDef = columnsState.lookup[groupingColDef.field];
-        currentGroupingfields.push(groupingColDef.field);
         if (matchingGroupingColDef) {
           groupingColDef.width = matchingGroupingColDef.width;
           groupingColDef.flex = matchingGroupingColDef.flex;
@@ -141,7 +139,7 @@ export const useGridRowGroupingPreProcessors = (
         newColumnsLookup[groupingColDef.field] = groupingColDef;
       });
 
-      if (prevGroupingfields.length !== currentGroupingfields.length) {
+      if (prevGroupingfields.length !== groupingColDefs.length) {
         const startIndex = newColumnFields[0] === GRID_CHECKBOX_SELECTION_FIELD ? 1 : 0;
         newColumnFields = [
           ...newColumnFields.slice(0, startIndex),
