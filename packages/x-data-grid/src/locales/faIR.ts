85ceee21bdabeebcd40862d54e7a1a157f075a47
Mostafa Roosta <106796626+mostafaRoosta74@users.noreply.github.com>
2024-12-21 09:20:07 +0330

    Update faIR.ts
    Signed-off-by: Mostafa Roosta <106796626+mostafaRoosta74@users.noreply.github.com>

--- packages/x-data-grid/src/locales/faIR.ts
+++ packages/x-data-grid/src/locales/faIR.ts
@@ -43,7 +43,7 @@ const faIRGrid: Partial<GridLocaleText> = {
   columnsManagementNoColumns: 'بدون سطر',
   columnsManagementShowHideAllText: 'نمایش/مخفی کردن همه',
   columnsManagementReset: 'بازنشانی',
-  // columnsManagementDeleteIconLabel: 'Clear',
+  columnsManagementDeleteIconLabel: 'پاک کردن',
 
   // Filter panel text
   filterPanelAddFilter: 'افزودن فیلتر',
@@ -59,9 +59,9 @@ const faIRGrid: Partial<GridLocaleText> = {
 
   // Filter operators text
   filterOperatorContains: 'شامل',
-  // filterOperatorDoesNotContain: 'does not contain',
+  filterOperatorDoesNotContain: 'شامل نمیشود',
   filterOperatorEquals: 'مساوی',
-  // filterOperatorDoesNotEqual: 'does not equal',
+  filterOperatorDoesNotEqual: 'برابر نیست',
   filterOperatorStartsWith: 'شروع با',
   filterOperatorEndsWith: 'پایان با',
   filterOperatorIs: 'هست',
@@ -82,9 +82,9 @@ const faIRGrid: Partial<GridLocaleText> = {
 
   // Header filter operators text
   headerFilterOperatorContains: 'شامل',
-  // headerFilterOperatorDoesNotContain: 'Does not contain',
+  headerFilterOperatorDoesNotContain: 'شامل نمیشود',
   headerFilterOperatorEquals: 'مساوی',
-  // headerFilterOperatorDoesNotEqual: 'Does not equal',
+  headerFilterOperatorDoesNotEqual: 'برابر نیست',
   headerFilterOperatorStartsWith: 'شروع با',
   headerFilterOperatorEndsWith: 'پایان با',
   headerFilterOperatorIs: 'هست',
