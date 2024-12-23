fb81d41ba60accf427e75eaf191765a1702cc4dd
Andrew Cherniavskyi <andrew@mui.com>
2024-12-23 19:53:11 +0100

    fix types

--- packages/x-data-grid-premium/src/components/sidebar/GridSidebarCloseButton.tsx
+++ packages/x-data-grid-premium/src/components/sidebar/GridSidebarCloseButton.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import { useGridRootProps } from '@mui/x-data-grid';
 import { IconButtonProps } from '@mui/material/IconButton';
+import { useGridRootProps } from '../../hooks/utils/useGridRootProps';
 
 // TODO: Figure out what the types should be for this component
 // If users can provide a custom icon button, this could be something other than IconButtonProps
 export function GridSidebarCloseButton(props: IconButtonProps) {
   const rootProps = useGridRootProps();
-  const { slots } = rootProps;
+  const { slots, pivotParams } = rootProps;
 
   return (
     <slots.baseIconButton
       size="small"
       {...props}
-      onClick={() => rootProps.pivotParams?.onPivotSettingsOpenChange(false)}
+      onClick={() => pivotParams?.onPivotSettingsOpenChange(false)}
     >
       {/* Replace with a new slots.closeButtonIcon or similar */}
       <slots.filterPanelDeleteIcon fontSize="small" />
