1caecd5e91ce7a56a6f801df3ca0d6d42b229853
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-12-29 16:44:10 +0100

    fix script generation

--- scripts/releaseChangelog.mjs
+++ scripts/releaseChangelog.mjs
@@ -339,7 +339,7 @@ async function main(argv) {
   };
 
   const changelog = `
-## __VERSION__
+## v__VERSION__
 <!-- generated comparing ${lastRelease}..${release} -->
 _${nowFormatted}_
 
@@ -396,7 +396,7 @@ Same changes as in \`@mui/x-charts@__VERSION__\`${chartsProCommits.length > 0 ? 
 ${logChangelogSection(chartsProCommits)}${chartsProCommits.length > 0 ? '\n' : ''}
 ### Tree View
 ${logChangelogMessages('TreeView')}
-#### \`@mui/x-tree-view@__VERSION__\` 
+#### \`@mui/x-tree-view@__VERSION__\`
 ${logChangelogSection(treeViewProCommits) || `No changes since \`@mui/x-tree-view-pro@${lastRelease}\`.`}
 
 #### \`@mui/x-tree-view-pro@__VERSION__\` [![pro](https://mui.com/r/x-pro-svg)](https://mui.com/r/x-pro-svg-link 'Pro plan')
