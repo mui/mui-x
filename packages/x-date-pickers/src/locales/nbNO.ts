4189b60b1b5f80d4fd7062ff27e1148051d64199
Jostein Hauge <josteinjhauge@me.com>
2025-01-07 09:29:25 +0100

    [l10n] Improve Norwegian (nb-NO) locale (#16083)
    Signed-off-by: Jostein Hauge <josteinjhauge@me.com>

--- packages/x-date-pickers/src/locales/nbNO.ts
+++ packages/x-date-pickers/src/locales/nbNO.ts
@@ -25,10 +25,10 @@ const nbNOPickers: Partial<PickersLocaleText<any>> = {
   // DateRange labels
   start: 'Start',
   end: 'Slutt',
-  // startDate: 'Start date',
-  // startTime: 'Start time',
-  // endDate: 'End date',
-  // endTime: 'End time',
+  startDate: 'Startdato',
+  startTime: 'Starttid',
+  endDate: 'Sluttdato',
+  endTime: 'Slutttid',
 
   // Action bar
   cancelButtonLabel: 'Avbryt',
@@ -67,7 +67,7 @@ const nbNOPickers: Partial<PickersLocaleText<any>> = {
     formattedTime || (value !== null && utils.isValid(value))
       ? `Velg tid, valgt tid er ${formattedTime ?? utils.format(value, 'fullTime')}`
       : 'Velg tid',
-  // fieldClearLabel: 'Clear',
+  fieldClearLabel: 'Slett',
 
   // Table labels
   timeTableLabel: 'velg tid',
@@ -84,17 +84,17 @@ const nbNOPickers: Partial<PickersLocaleText<any>> = {
   fieldMeridiemPlaceholder: () => 'aa',
 
   // View names
-  // year: 'Year',
-  // month: 'Month',
-  // day: 'Day',
-  // weekDay: 'Week day',
-  // hours: 'Hours',
-  // minutes: 'Minutes',
-  // seconds: 'Seconds',
-  // meridiem: 'Meridiem',
+  year: 'År',
+  month: 'Måned',
+  day: 'Dag',
+  weekDay: 'Ukedag',
+  hours: 'Timer',
+  minutes: 'Minutter',
+  seconds: 'Sekunder',
+  meridiem: 'Meridiem',
 
   // Common
-  // empty: 'Empty',
+  empty: 'Tøm',
 };
 
 export const nbNO = getPickersLocalization(nbNOPickers);
