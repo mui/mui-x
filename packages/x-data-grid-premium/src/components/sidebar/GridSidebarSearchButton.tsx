90ec99d7ffef675cb75d7c88e5ce45655cd29872
Andrew Cherniavskyi <andrew@mui.com>
2025-01-08 22:09:42 +0100

    fix ts error

--- packages/x-data-grid-premium/src/components/sidebar/GridSidebarSearchButton.tsx
+++ packages/x-data-grid-premium/src/components/sidebar/GridSidebarSearchButton.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { useGridRootProps } from '@mui/x-data-grid';
-import { IconButtonProps } from '@mui/material/IconButton';
+import { GridSlotProps } from '@mui/x-data-grid';
 import SearchIcon from '@mui/icons-material/Search';
+import { useGridRootProps } from '../../hooks/utils/useGridRootProps';
 
 // TODO: Figure out what the types should be for this component
 // If users can provide a custom icon button, this could be something other than IconButtonProps
-export function GridSidebarSearchButton(props: IconButtonProps) {
+export function GridSidebarSearchButton(props: GridSlotProps['baseIconButton']) {
   const rootProps = useGridRootProps();
   const { slots } = rootProps;
 
