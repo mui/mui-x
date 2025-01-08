21c4fe564f2aac2b50c3528ae3f8740bd59b0a69
Jose Quintas <juniorquintas@gmail.com>
2025-01-08 18:14:07 +0100

    rename param

--- packages/x-charts/src/hooks/useChartGradientId.tsx
+++ packages/x-charts/src/hooks/useChartGradientId.tsx
@@ -4,54 +4,55 @@ import { useChartId } from './useChartId';
 import { AxisId } from '../models/axis';
 
 /**
- * Returns a function that generates a gradient id for the given axis and direction.
+ * Returns a function that generates a gradient id for the given axis id and direction.
  */
 export function useChartGradientIdBuilder() {
   const chartId = useChartId();
   return React.useCallback(
-    (axisId: AxisId, direction: 'x' | 'y' | 'z') => `${chartId}-gradient-${direction}-${axisId}`,
+    (axisId: AxisId, axisDirection: 'x' | 'y' | 'z') =>
+      `${chartId}-gradient-${axisDirection}-${axisId}`,
     [chartId],
   );
 }
 
 /**
- * Returns a function that generates a gradient id for the given axis and direction.
+ * Returns a function that generates a gradient id for the given axis id and direction.
  */
 export function useChartGradientIdObjectBoundBuilder() {
   const chartId = useChartId();
   return React.useCallback(
-    (axisId: AxisId, direction: 'x' | 'y' | 'z') =>
-      `${chartId}-gradient-${direction}-${axisId}-object-bound`,
+    (axisId: AxisId, axisDirection: 'x' | 'y' | 'z') =>
+      `${chartId}-gradient-${axisDirection}-${axisId}-object-bound`,
     [chartId],
   );
 }
 
 /**
- * Returns a gradient id for the given axis and direction.
+ * Returns a gradient id for the given axis id and direction.
  *
  * Can be useful when reusing the same gradient on custom components.
  *
  * For a gradient that respects the coordinates of the object on which it is applied, use `useChartGradientIdObjectBound` instead.
  *
  * @param axisId the axis id
- * @param direction the direction of the axis
+ * @param axisDirection the direction of the axis
  * @returns the gradient id
  */
-export function useChartGradientId(axisId: AxisId, direction: 'x' | 'y' | 'z') {
-  return useChartGradientIdBuilder()(axisId, direction);
+export function useChartGradientId(axisId: AxisId, axisDirection: 'x' | 'y' | 'z') {
+  return useChartGradientIdBuilder()(axisId, axisDirection);
 }
 
 /**
- * Returns a gradient id for the given axis and direction.
+ * Returns a gradient id for the given axis id and direction.
  *
  * Can be useful when reusing the same gradient on custom components.
  *
  * This gradient differs from `useChartGradientId` in that it respects the coordinates of the object on which it is applied.
  *
  * @param axisId the axis id
- * @param direction the direction of the axis
+ * @param axisDirection the direction of the axis
  * @returns the gradient id
  */
-export function useChartGradientIdObjectBound(axisId: AxisId, direction: 'x' | 'y' | 'z') {
-  return useChartGradientIdObjectBoundBuilder()(axisId, direction);
+export function useChartGradientIdObjectBound(axisId: AxisId, axisDirection: 'x' | 'y' | 'z') {
+  return useChartGradientIdObjectBoundBuilder()(axisId, axisDirection);
 }
