7297707c8e2e04e07936b983b86ee76217ce9795
Bilal Shafi <bilalshafidev@gmail.com>
2025-01-09 17:13:03 +0500

    [DataGridPremium] Server-side aggregation with data source (#15741)
    Co-authored-by: Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>

--- packages/x-data-grid-pro/src/hooks/features/dataSource/cache.ts
+++ packages/x-data-grid-pro/src/hooks/features/dataSource/cache.ts
@@ -1,4 +1,4 @@
-import { GridGetRowsParams, GridGetRowsResponse } from '../../../models';
+import { GridGetRowsParams, GridGetRowsResponse } from '@mui/x-data-grid/internals';
 
 export type GridDataSourceCacheDefaultConfig = {
   /**
@@ -7,9 +7,16 @@ export type GridDataSourceCacheDefaultConfig = {
    * @default 300000 (5 minutes)
    */
   ttl?: number;
+  /**
+   * Function to generate a cache key from the params.
+   * @param {GridGetRowsParams} params The params to generate the cache key from.
+   * @returns {string} The cache key.
+   * @default `getKeyDefault()`
+   */
+  getKey?: (params: GridGetRowsParams) => string;
 };
 
-export function getKey(params: GridGetRowsParams) {
+export function getKeyDefault(params: GridGetRowsParams) {
   return JSON.stringify([
     params.filterModel,
     params.sortModel,
@@ -25,19 +32,22 @@ export class GridDataSourceCacheDefault {
 
   private ttl: number;
 
-  constructor({ ttl = 300000 }: GridDataSourceCacheDefaultConfig) {
+  private getKey: (params: GridGetRowsParams) => string;
+
+  constructor({ ttl = 300000, getKey = getKeyDefault }: GridDataSourceCacheDefaultConfig) {
     this.cache = {};
     this.ttl = ttl;
+    this.getKey = getKey;
   }
 
   set(key: GridGetRowsParams, value: GridGetRowsResponse) {
-    const keyString = getKey(key);
+    const keyString = this.getKey(key);
     const expiry = Date.now() + this.ttl;
     this.cache[keyString] = { value, expiry };
   }
 
   get(key: GridGetRowsParams): GridGetRowsResponse | undefined {
-    const keyString = getKey(key);
+    const keyString = this.getKey(key);
     const entry = this.cache[keyString];
     if (!entry) {
       return undefined;
