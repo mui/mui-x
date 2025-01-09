76f76ae3749b297c6610253303680c3d740618a6
Armin Mehinovic <armin@mui.com>
2025-01-09 13:34:32 +0100

    Fix tests

--- packages/x-data-grid-pro/src/tests/dataSourceLazyLoader.DataGridPro.test.tsx
+++ packages/x-data-grid-pro/src/tests/dataSourceLazyLoader.DataGridPro.test.tsx
@@ -229,6 +229,11 @@ describeSkipIf(isJSDOM)('<DataGridPro /> - Data source lazy loader', () => {
 
       apiRef.current?.scrollToIndexes({ rowIndex: 9 });
 
+      // wait until the debounced fetch
+      await waitFor(() => {
+        expect(fetchRowsSpy.callCount).to.equal(2);
+      });
+
       // wait until the rows are rendered
       await waitFor(() => expect(getRow(10)).not.to.be.undefined);
 
@@ -237,6 +242,10 @@ describeSkipIf(isJSDOM)('<DataGridPro /> - Data source lazy loader', () => {
       expect(beforeSortingSearchParams.get('end')).to.not.equal('9');
 
       apiRef.current?.sortColumn(mockServer.columns[0].field, 'asc');
+
+      await waitFor(() => {
+        expect(fetchRowsSpy.callCount).to.equal(3);
+      });
 
       const afterSortingSearchParams = new URL(fetchRowsSpy.lastCall.args[0]).searchParams;
       // last row is the end of the first page
@@ -249,6 +258,11 @@ describeSkipIf(isJSDOM)('<DataGridPro /> - Data source lazy loader', () => {
       await waitFor(() => expect(getRow(0)).not.to.be.undefined);
 
       apiRef.current?.scrollToIndexes({ rowIndex: 9 });
+
+      // wait until the debounced fetch
+      await waitFor(() => {
+        expect(fetchRowsSpy.callCount).to.equal(2);
+      });
 
       // wait until the rows are rendered
       await waitFor(() => expect(getRow(10)).not.to.be.undefined);
@@ -265,6 +279,10 @@ describeSkipIf(isJSDOM)('<DataGridPro /> - Data source lazy loader', () => {
             operator: 'contains',
           },
         ],
+      });
+
+      await waitFor(() => {
+        expect(fetchRowsSpy.callCount).to.equal(3);
       });
 
       const afterFilteringSearchParams = new URL(fetchRowsSpy.lastCall.args[0]).searchParams;
@@ -288,7 +306,9 @@ describeSkipIf(isJSDOM)('<DataGridPro /> - Data source lazy loader', () => {
       setProps({ rowCount: 100 });
 
       // The 11th row should be a skeleton
-      expect(getRow(10).dataset.id).to.equal('auto-generated-skeleton-row-root-10');
+      await waitFor(() =>
+        expect(getRow(10).dataset.id).to.equal('auto-generated-skeleton-row-root-10'),
+      );
     });
 
     it('should reset the grid if the rowCount becomes unknown', async () => {
