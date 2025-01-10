91d5f652f5f713b76e5440e437171d47bf3897a2
Lukas Tyla <llukas.tyla@gmail.com>
2025-01-10 11:51:53 +0200

    Update scripts/localeNames.js
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>
    Signed-off-by: Lukas Tyla <llukas.tyla@gmail.com>

--- scripts/localeNames.js
+++ scripts/localeNames.js
@@ -1,9 +1,6 @@
 module.exports = {
   'am-ET': 'Amharic',
-  ar: 'Arabic',
-  'ar-EG': 'Arabic (Egypt)',
-  'ar-SA': 'Arabic (Saudi Arabia)',
-  'ar-SD': 'Arabic (Sudan)',
+  ar: 'Standard Arabic',
   'hy-AM': 'Armenian',
   'az-AZ': 'Azerbaijani',
   'bn-BD': 'Bangla',
