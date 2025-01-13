585e71df6d7e94540bb4d43986c0101992c92434
Bilal Shafi <bilalshafidev@gmail.com>
2025-01-13 13:57:50 +0300

    Run script

--- docs/data/data-grid/filtering/HeaderFilteringInlineClearDataGridPro.js
+++ docs/data/data-grid/filtering/HeaderFilteringInlineClearDataGridPro.js
@@ -2,10 +2,13 @@ import * as React from 'react';
 import { DataGridPro } from '@mui/x-data-grid-pro';
 import { useDemoData } from '@mui/x-data-grid-generator';
 
+const VISIBLE_FIELDS = ['name', 'rating', 'website'];
+
 export default function HeaderFilteringInlineClearDataGridPro() {
   const { data, loading } = useDemoData({
     dataSet: 'Employee',
     rowLength: 100,
+    visibleFields: VISIBLE_FIELDS,
   });
 
   return (
@@ -15,10 +18,13 @@ export default function HeaderFilteringInlineClearDataGridPro() {
         loading={loading}
         initialState={{
           ...data.initialState,
-          columns: {
-            columnVisibilityModel: {
-              avatar: false,
-              id: false,
+          filter: {
+            filterModel: {
+              items: [
+                { field: 'name', operator: 'contains', value: 'a' },
+                { field: 'website', operator: 'contains', value: 'http://' },
+                { field: 'rating', operator: '>', value: 2 },
+              ],
             },
           },
         }}
