92957c40a94b3217ea5bb3e155dcff9e5b038b27
Lukas <llukas.tyla@gmail.com>
2025-01-13 11:45:35 +0200

    Set the appropriate flag in the script

--- scripts/useReactVersion.mjs
+++ scripts/useReactVersion.mjs
@@ -1,1 +1,14 @@
-import '@mui/monorepo/scripts/useReactVersion.mjs';
+import childProcess from 'child_process';
+
+const [version = process.env.REACT_VERSION] = process.argv.slice(2);
+
+if (typeof version !== 'string') {
+  throw new TypeError(`expected version: string but got '${version}'`);
+}
+
+if (version !== 'stable') {
+  // ensure the flag is set in `.npmrc` before calling `useReactVersion` from `@mui/monorepo`
+  childProcess.execSync('pnpm config set auto-install-peers false --location project');
+}
+
+import('@mui/monorepo/scripts/useReactVersion.mjs');
