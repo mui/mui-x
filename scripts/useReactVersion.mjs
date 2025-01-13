bbaa20d271d9835de7ec14f93a8e274b8cad97cc
Lukas <llukas.tyla@gmail.com>
2025-01-13 11:46:44 +0200

    Remove no longer relevant step from CI config

--- scripts/useReactVersion.mjs
+++ scripts/useReactVersion.mjs
@@ -6,6 +6,7 @@ if (typeof version !== 'string') {
   throw new TypeError(`expected version: string but got '${version}'`);
 }
 
+// Change pnpm setting to not install peer dependencies
 if (version !== 'stable') {
   // ensure the flag is set in `.npmrc` before calling `useReactVersion` from `@mui/monorepo`
   childProcess.execSync('pnpm config set auto-install-peers false --location project');
