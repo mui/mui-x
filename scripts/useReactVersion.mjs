b26503363e75628da78ac7b9bd178615b9262ede
Lukas <llukas.tyla@gmail.com>
2025-01-14 14:21:40 +0200

    Revert `useReactVersion` changes

--- scripts/useReactVersion.mjs
+++ scripts/useReactVersion.mjs
@@ -1,15 +1,1 @@
-import childProcess from 'child_process';
-
-const [version = process.env.REACT_VERSION] = process.argv.slice(2);
-
-if (typeof version !== 'string') {
-  throw new TypeError(`expected version: string but got '${version}'`);
-}
-
-// Change pnpm setting to not install peer dependencies
-if (version !== 'stable') {
-  // ensure the flag is set in `.npmrc` before calling `useReactVersion` from `@mui/monorepo`
-  childProcess.execSync('pnpm config set auto-install-peers false --location project');
-}
-
-import('@mui/monorepo/scripts/useReactVersion.mjs');
+import '@mui/monorepo/scripts/useReactVersion.mjs';
