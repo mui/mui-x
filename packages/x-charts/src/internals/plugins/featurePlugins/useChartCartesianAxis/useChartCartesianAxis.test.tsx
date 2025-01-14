9581ffc19203404b7e9387df9cd33432ea2d27dd
Jose Quintas <juniorquintas@gmail.com>
2025-01-14 23:14:35 +0100

    [charts] Fix flaky charts test

--- packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/useChartCartesianAxis.test.tsx
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/useChartCartesianAxis.test.tsx
@@ -3,9 +3,14 @@ import { expect } from 'chai';
 import { createRenderer, reactMajor } from '@mui/internal-test-utils';
 import { testSkipIf, isJSDOM } from 'test/utils/skipIf';
 import { BarChart } from '@mui/x-charts/BarChart';
+import { clearWarningsCache } from '@mui/x-internals/warning';
 
 describe('useChartCartesianAxis', () => {
   const { render } = createRenderer();
+
+  beforeEach(() => {
+    clearWarningsCache();
+  });
 
   // can't catch render errors in the browser for unknown reason
   // tried try-catch + error boundary + window onError preventDefault
@@ -17,7 +22,15 @@ describe('useChartCartesianAxis', () => {
 
     expect(() =>
       render(
-        <BarChart xAxis={[{ id: 'qwerty' }, { id: 'qwerty' }]} series={[{ data: [1, 2, 3] }]} />,
+        <BarChart
+          xAxis={[
+            { scaleType: 'band', id: 'qwerty', data: ['a', 'b', 'c'] },
+            { scaleType: 'band', id: 'qwerty', data: ['a', 'b', 'c'] },
+          ]}
+          series={[{ data: [1, 2, 3] }]}
+          height={100}
+          width={100}
+        />,
       ),
     ).toErrorDev(expectedError);
   });
@@ -26,7 +39,7 @@ describe('useChartCartesianAxis', () => {
   // tried try-catch + error boundary + window onError preventDefault
   testSkipIf(!isJSDOM)(
     'should throw an error when axis have duplicate ids across different directions (x,y)',
-    () => {
+    async () => {
       const errorMessage1 = 'MUI X: The following axis ids are duplicated: qwerty.';
       const errorMessage2 = 'Please make sure that each axis has a unique id.';
       const expectedError =
@@ -35,9 +48,11 @@ describe('useChartCartesianAxis', () => {
       expect(() =>
         render(
           <BarChart
-            xAxis={[{ id: 'qwerty' }]}
+            xAxis={[{ scaleType: 'band', id: 'qwerty', data: ['a', 'b', 'c'] }]}
             yAxis={[{ id: 'qwerty' }]}
             series={[{ data: [1, 2, 3] }]}
+            height={100}
+            width={100}
           />,
         ),
       ).toErrorDev(expectedError);
