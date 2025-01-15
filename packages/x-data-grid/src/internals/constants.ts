014db976673b2f632f53d9221d2af5b1b219cd6c
Andrew Cherniavskii <andrew@mui.com>
2025-01-15 16:48:06 +0100

    [Data Grid] Fix row, cell and header memoizations (#15666) (#16195)
    Co-authored-by: Rom Grk <romgrk.cc@gmail.com>

--- packages/x-data-grid/src/internals/constants.ts
+++ packages/x-data-grid/src/internals/constants.ts
@@ -1,3 +1,9 @@
 export const GRID_TREE_DATA_GROUPING_FIELD = '__tree_data_group__';
 export const GRID_ROW_GROUPING_SINGLE_GROUPING_FIELD = '__row_group_by_columns_group__';
 export const GRID_DETAIL_PANEL_TOGGLE_FIELD = '__detail_panel_toggle__';
+export enum PinnedColumnPosition {
+  NONE,
+  LEFT,
+  RIGHT,
+  VIRTUAL,
+}
