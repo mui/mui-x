d174d102dcae1d78c3a6b28cff095f3208c45551
lauri865 <lauri.lehtmaa@gmail.com>
2025-01-20 14:16:16 +0100

    don't try to mount filter button with

--- packages/x-data-grid/src/hooks/features/preferencesPanel/gridPreferencePanelSelector.ts
+++ packages/x-data-grid/src/hooks/features/preferencesPanel/gridPreferencePanelSelector.ts
@@ -1,4 +1,17 @@
 import { GridStateCommunity } from '../../../models/gridStateCommunity';
+import { createSelector } from '../../../utils/createSelector';
+import { GridPreferencePanelsValue } from './gridPreferencePanelsValue';
 
 export const gridPreferencePanelStateSelector = (state: GridStateCommunity) =>
   state.preferencePanel;
+
+export const gridPreferencePanelSelectorWithLabel = createSelector(
+  gridPreferencePanelStateSelector,
+  (panel, labelId: string) => {
+    if (panel.open && panel.labelId === labelId) {
+      return true;
+    }
+
+    return false;
+  },
+);
