111f9b3a86d4bc194d044143c2e028598e2ac3f0
lauri865 <lauri.lehtmaa@gmail.com>
2025-01-20 14:43:38 +0100

    fix

--- packages/x-data-grid/src/hooks/features/preferencesPanel/gridPreferencePanelSelector.ts
+++ packages/x-data-grid/src/hooks/features/preferencesPanel/gridPreferencePanelSelector.ts
@@ -1,6 +1,5 @@
 import { GridStateCommunity } from '../../../models/gridStateCommunity';
 import { createSelector } from '../../../utils/createSelector';
-import { GridPreferencePanelsValue } from './gridPreferencePanelsValue';
 
 export const gridPreferencePanelStateSelector = (state: GridStateCommunity) =>
   state.preferencePanel;
