c4d0b2375f1d4ddf923267d9db99005d3f26c40d
Armin Mehinovic <armin@mui.com>
2025-01-21 09:06:53 +0100

    Add comments

--- packages/x-internals/src/apiRef/apiRef.tsx
+++ packages/x-internals/src/apiRef/apiRef.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import reactMajor from '../reactMajor';
 
+// in React 19 useRef requires a parameter, so () => infer R will not match anymore
 export type ApiRef<T> = typeof React.useRef<T> extends () => infer R
   ? R extends React.MutableRefObject<infer M | undefined>
-    ? React.MutableRefObject<M>
+    ? // In v7 we remove the possibility of undefined for MutableRefObject (see https://github.com/mui/mui-x/issues/16135#issuecomment-2589395230)
+      React.MutableRefObject<M>
     : R
   : React.RefObject<T>;
 
