44358f3bed8761bc1a7322c9cacc1139a0de6a1f
alex <alex.fauquette@gmail.com>
2025-01-22 15:51:13 +0100

    test

--- packages/x-charts/src/LineChart/CircleMarkElement.tsx
+++ packages/x-charts/src/LineChart/CircleMarkElement.tsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
 import { animated, useSpring } from '@react-spring/web';
 import { useInteractionItemProps } from '../hooks/useInteractionItemProps';
-import { useItemHighlighted } from '../context';
+import { useItemHighlighted } from '../hooks/useItemHighlighted';
 import { MarkElementOwnerState, useUtilityClasses } from './markElementClasses';
 import { useSelector } from '../internals/store/useSelector';
 import { selectorChartsInteractionXAxis } from '../internals/plugins/featurePlugins/useChartInteraction';
