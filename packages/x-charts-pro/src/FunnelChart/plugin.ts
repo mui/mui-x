ea4516c9bf03c5341473309cac1252aa0654fe81
Jose Quintas <juniorquintas@gmail.com>
2025-01-23 14:27:57 +0100

    funnel legend

--- packages/x-charts-pro/src/FunnelChart/plugin.ts
+++ packages/x-charts-pro/src/FunnelChart/plugin.ts
@@ -2,10 +2,12 @@ import type { ChartSeriesTypeConfig } from '@mui/x-charts/internals';
 import { getExtremumX, getExtremumY } from './extremums';
 import formatter from './formatter';
 import getColor from './getColor';
+import legendGetter from './legend';
 
 export const plugin: ChartSeriesTypeConfig<'funnel'> = {
   seriesProcessor: formatter,
   colorProcessor: getColor,
+  legendGetter,
   xExtremumGetter: getExtremumX,
   yExtremumGetter: getExtremumY,
 };
