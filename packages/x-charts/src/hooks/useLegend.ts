717bffd9c5cc357653e4150bc6e4b028df3ca592
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2025-01-23 13:33:49 +0100

    [charts] Move legend getter to series config (#16307)

--- packages/x-charts/src/hooks/useLegend.ts
+++ packages/x-charts/src/hooks/useLegend.ts
@@ -1,25 +1,23 @@
 'use client';
 import { ChartSeriesType } from '../models/seriesType/config';
-import getBarLegend from '../BarChart/legend';
-import getScatterLegend from '../ScatterChart/legend';
-import getLineLegend from '../LineChart/legend';
-import getPieLegend from '../PieChart/legend';
-import { ProcessedSeries } from '../internals/plugins/corePlugins/useChartSeries/useChartSeries.types';
-import { LegendGetter } from '../internals/plugins/models/seriesConfig/seriesProcessor.types';
+import {
+  ProcessedSeries,
+  UseChartSeriesSignature,
+  selectorChartSeriesConfig,
+} from '../internals/plugins/corePlugins/useChartSeries';
 import { useSeries } from './useSeries';
 import type { LegendItemParams } from '../ChartsLegend';
+import { useStore } from '../internals/store/useStore';
+import { useSelector } from '../internals/store/useSelector';
+import { ChartSeriesConfig } from '../internals/plugins/models/seriesConfig';
 
-const legendGetter: { [T in ChartSeriesType]?: LegendGetter<T> } = {
-  bar: getBarLegend,
-  scatter: getScatterLegend,
-  line: getLineLegend,
-  pie: getPieLegend,
-};
-
-function getSeriesToDisplay(series: ProcessedSeries) {
+function getSeriesToDisplay(
+  series: ProcessedSeries,
+  seriesConfig: ChartSeriesConfig<ChartSeriesType>,
+) {
   return (Object.keys(series) as ChartSeriesType[]).flatMap(
     <T extends ChartSeriesType>(seriesType: T) => {
-      const getter = legendGetter[seriesType as T];
+      const getter = seriesConfig[seriesType as T].legendGetter;
       return getter === undefined ? [] : getter(series[seriesType as T]!);
     },
   );
@@ -36,7 +34,10 @@ function getSeriesToDisplay(series: ProcessedSeries) {
  */
 export function useLegend(): { items: LegendItemParams[] } {
   const series = useSeries();
+  const store = useStore<[UseChartSeriesSignature]>();
+  const seriesConfig = useSelector(store, selectorChartSeriesConfig);
+
   return {
-    items: getSeriesToDisplay(series),
+    items: getSeriesToDisplay(series, seriesConfig),
   };
 }
