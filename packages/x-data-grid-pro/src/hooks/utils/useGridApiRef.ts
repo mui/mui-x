d67b6cbc8a914efe666d8d732565f562571d2dd5
Armin Mehinovic <armin@mui.com>
2025-01-24 15:55:00 +0100

    Explicitly type return type of useGridApiRef as ReactRef from the internals package

--- packages/x-data-grid-pro/src/hooks/utils/useGridApiRef.ts
+++ packages/x-data-grid-pro/src/hooks/utils/useGridApiRef.ts
@@ -1,4 +1,7 @@
-import { useGridApiRef as useCommunityGridApiRef } from '@mui/x-data-grid';
+import { RefObject } from '@mui/x-internals/types';
+import { GridApiCommon, useGridApiRef as useCommunityGridApiRef } from '@mui/x-data-grid';
 import { GridApiPro } from '../../models/gridApiPro';
 
-export const useGridApiRef = useCommunityGridApiRef<GridApiPro>;
+export const useGridApiRef = useCommunityGridApiRef as <
+  Api extends GridApiCommon = GridApiPro,
+>() => RefObject<Api>;
