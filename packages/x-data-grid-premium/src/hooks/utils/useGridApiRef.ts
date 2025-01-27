215aab15b6d6241e20b55fdea249bdb3c1904157
Armin Mehinovic <armin@mui.com>
2025-01-27 08:53:38 +0100

    Type ref without casting

--- packages/x-data-grid-premium/src/hooks/utils/useGridApiRef.ts
+++ packages/x-data-grid-premium/src/hooks/utils/useGridApiRef.ts
@@ -2,6 +2,5 @@ import { RefObject } from '@mui/x-internals/types';
 import { GridApiCommon, useGridApiRef as useCommunityGridApiRef } from '@mui/x-data-grid';
 import { GridApiPremium } from '../../models/gridApiPremium';
 
-export const useGridApiRef = useCommunityGridApiRef as <
-  Api extends GridApiCommon = GridApiPremium,
->() => RefObject<Api>;
+export const useGridApiRef: <Api extends GridApiCommon = GridApiPremium>() => RefObject<Api> =
+  useCommunityGridApiRef;
--- packages/x-data-grid-pro/src/hooks/utils/useGridApiRef.ts
+++ packages/x-data-grid-pro/src/hooks/utils/useGridApiRef.ts
@@ -2,6 +2,5 @@ import { RefObject } from '@mui/x-internals/types';
 import { GridApiCommon, useGridApiRef as useCommunityGridApiRef } from '@mui/x-data-grid';
 import { GridApiPro } from '../../models/gridApiPro';
 
-export const useGridApiRef = useCommunityGridApiRef as <
-  Api extends GridApiCommon = GridApiPro,
->() => RefObject<Api>;
+export const useGridApiRef: <Api extends GridApiCommon = GridApiPro>() => RefObject<Api> =
+  useCommunityGridApiRef;
