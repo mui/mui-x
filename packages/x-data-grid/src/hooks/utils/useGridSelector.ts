fc18204ab3c654ac5fa40a09be8407b68202d590
lauri865 <lauri.lehtmaa@gmail.com>
2025-01-27 23:10:25 +0100

    lint

--- packages/x-data-grid/src/hooks/utils/useGridSelector.ts
+++ packages/x-data-grid/src/hooks/utils/useGridSelector.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/utils';
+import useEnhancedEffect from '@mui/utils/useEnhancedEffect';
 import { RefObject } from '@mui/x-internals/types';
 import { fastObjectShallowCompare } from '@mui/x-internals/fastObjectShallowCompare';
 import { warnOnce } from '@mui/x-internals/warning';
