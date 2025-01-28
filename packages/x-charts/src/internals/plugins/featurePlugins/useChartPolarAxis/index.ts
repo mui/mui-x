21d0c35c020e115822a1f359876096faa95ceba9
alex <alex.fauquette@gmail.com>
2025-01-28 09:45:22 +0100

    create a polar plugin

---
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartPolarAxis/index.ts
@@ -0,0 +1,5 @@
+export { useChartPolarAxis } from './useChartPolarAxis';
+export type * from './useChartPolarAxis.types';
+export * from './useChartPolarAxis.selectors';
+export { defaultizeAxis } from './defaultizeAxis';
+export * from './computeAxisValue';
