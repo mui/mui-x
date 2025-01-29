07201fb37499b1c1b48d05baa96d2323e1d1040f
Jose C Quintas Jr <juniorquintas@gmail.com>
2025-01-29 14:35:02 +0100

    [charts] Replace `legend.position.horizontal` from `"left" | "middle" | "right"` to `"start" | "center" | "end"` (#16315)
    Signed-off-by: Jose C Quintas Jr <juniorquintas@gmail.com>
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>

---
+++ packages/x-codemod/src/v8.0.0/charts/rename-legend-position-type/actual-root-imports.spec.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-restricted-imports */
+import { LegendPosition } from '@mui/x-charts';
+import { LegendPosition as LegendPositionPro } from '@mui/x-charts-pro';
+
+const legendPosition: LegendPosition = {};
+type LegendPositionType = {} & LegendPosition;
+interface LegendPositionProps extends LegendPosition {}
+
+const legendPositionPro: LegendPositionPro = {};
+type LegendPositionProType = {} & LegendPositionPro;
+interface LegendPositionProProps extends LegendPositionPro {}
