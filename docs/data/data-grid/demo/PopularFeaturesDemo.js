f8eaf22592b28b97d9e8d11ee3e9027fdb74ef44
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2025-01-30 15:48:13 +0000

    [docs] Fix border on corners of popular features demo (#16401)

--- docs/data/data-grid/demo/PopularFeaturesDemo.js
+++ docs/data/data-grid/demo/PopularFeaturesDemo.js
@@ -480,9 +480,6 @@ export default function PopularFeaturesDemo() {
         flexDirection: 'column',
         minHeight: 1000,
         width: '100%',
-        border: '1px solid',
-        borderColor: 'divider',
-        borderRadius: 3,
       }}
     >
       <DataGridPremium
@@ -527,7 +524,7 @@ export default function PopularFeaturesDemo() {
             {
               outline: 'none',
             },
-          border: 0,
+          borderRadius: 2,
         }}
         rows={featuresSet}
         columns={columns}
