21fa10dddbc00ae31d1b149f00e47a7a8fe00529
alex <alex.fauquette@gmail.com>
2025-01-31 12:08:34 +0100

    docs

---
+++ docs/data/charts/radar/CompositionExample.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import {
+  Unstable_RadarDataProvider as RadarDataProvider,
+  RadarGrid,
+  RadarSeriesArea,
+  RadarSeriesMarks,
+} from '@mui/x-charts/RadarChart';
+import { ChartsSurface } from '@mui/x-charts/ChartsSurface';
+
+export default function CompositionExample() {
+  return (
+    <RadarDataProvider
+      height={300}
+      series={[
+        {
+          id: 'usa-id',
+          label: 'USA',
+          data: [6.65, 2.76, 5.15, 0.19, 0.07, 0.12],
+        },
+        {
+          id: 'australia-id',
+          label: 'Australia',
+          data: [5.52, 5.5, 3.19, 0.51, 0.15, 0.11],
+        },
+      ]}
+      radar={{
+        metrics: ['Oil', 'Coal', 'Gas', 'Flaring', 'Other industry', 'Cement'],
+      }}
+    >
+      <ChartsSurface disableAxisListener>
+        <RadarGrid divisionNumber={3} />
+        <RadarSeriesArea />
+        <RadarSeriesMarks seriesId="usa-id" />
+      </ChartsSurface>
+    </RadarDataProvider>
+  );
+}
---
+++ docs/data/charts/radar/DemoRadarNoSnap.js
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import ChartsUsageDemo from 'docsx/src/modules/components/ChartsUsageDemo';
+import { Unstable_RadarChart as RadarChart } from '@mui/x-charts/RadarChart';
+
+export default function DemoRadarNoSnap() {
+  return (
+    <ChartsUsageDemo
+      componentName="Alert"
+      data={[
+        {
+          propName: 'startAngle',
+          knob: 'number',
+          defaultValue: 0,
+          min: -180,
+          max: 180,
+        },
+
+        {
+          propName: 'divisionNumber',
+          knob: 'number',
+          defaultValue: 5,
+          min: 0,
+          max: 20,
+        },
+      ]}
+      renderDemo={(props) => (
+        <Box sx={{ width: '100%', maxWidth: 400 }}>
+          <RadarChart
+            height={250}
+            margin={{ top: 20 }}
+            series={[
+              {
+                // label: 'Lisa',
+                data: [120, 98, 86, 99, 85, 65],
+              },
+            ]}
+            divisionNumber={props.divisionNumber}
+            radar={{
+              max: 120,
+              startAngle: props.startAngle,
+              metrics: [
+                'Math',
+                'Chinese',
+                'English',
+                'Geography',
+                'Physics',
+                'History',
+              ],
+            }}
+          />
+        </Box>
+      )}
+      getCode={({ props }) =>
+        [
+          `import { Unstable_RadarChart as RadarChart } from '@mui/x-charts/RadarChart';`,
+          '',
+          `<RadarChart`,
+          '  {/** ... */}',
+          `  divisionNumber={${props.divisionNumber}}`,
+          `  radar={{`,
+          `    startAngle: ${props.startAngle},`,
+          `    metrics: [...],`,
+          '  }}',
+          '/>',
+        ].join('\n')
+      }
+    />
+  );
+}
