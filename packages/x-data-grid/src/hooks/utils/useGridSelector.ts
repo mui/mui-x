d92c4d886606890d835a57ca573d3ed8da8870c1
lauri865 <lauri.lehtmaa@gmail.com>
2025-01-31 10:57:01 +0100

    use `useSyncExternalStore` for `useGridSelector`

--- packages/x-data-grid/src/hooks/utils/useGridSelector.ts
+++ packages/x-data-grid/src/hooks/utils/useGridSelector.ts
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { RefObject } from '@mui/x-internals/types';
 import { fastObjectShallowCompare } from '@mui/x-internals/fastObjectShallowCompare';
 import { warnOnce } from '@mui/x-internals/warning';
+import { useSyncExternalStore } from 'use-sync-external-store/shim';
 import type { GridApiCommon } from '../../models/api/gridApiCommon';
 import type { OutputSelector } from '../../utils/createSelector';
 import { useLazyRef } from './useLazyRef';
@@ -49,9 +50,38 @@ export const argsEqual = (prev: any, curr: any) => {
   return fn(prev, curr);
 };
 
-const createRefs = () => ({ state: null, equals: null, selector: null, args: null }) as any;
+const createRefs = () =>
+  ({ state: null, equals: null, selector: null, args: null, argsId: 0 }) as any;
 
 const EMPTY = [] as unknown[];
+
+type Refs<T> = {
+  state: T;
+  equals: <U = T>(a: U, b: U) => boolean;
+  selector: Selector<any, any, T>;
+  args: any;
+  argsId: number;
+};
+
+const getState = <Api extends GridApiCommon, T>(
+  apiRef: RefObject<Api>,
+  refs: RefObject<Refs<T>>,
+  listener?: (state: T) => void,
+) => {
+  const newState = applySelector(
+    apiRef,
+    refs.current.selector,
+    refs.current.args,
+    apiRef.current.instanceId,
+  ) as T;
+  if (!refs.current.equals(refs.current.state, newState)) {
+    refs.current.state = newState;
+    if (listener) {
+      return listener(newState);
+    }
+  }
+  return refs.current.state;
+};
 
 export const useGridSelector = <Api extends GridApiCommon, Args, T>(
   apiRef: RefObject<Api>,
@@ -68,55 +98,31 @@ export const useGridSelector = <Api extends GridApiCommon, Args, T>(
     }
   }
 
-  const refs = useLazyRef<
-    {
-      state: T;
-      equals: typeof equals;
-      selector: typeof selector;
-      args: typeof args;
-    },
-    never
-  >(createRefs);
+  const refs = useLazyRef<Refs<T>, never>(createRefs);
   const didInit = refs.current.selector !== null;
 
-  const [state, setState] = React.useState<T>(
-    // We don't use an initialization function to avoid allocations
-    (didInit ? null : applySelector(apiRef, selector, args, apiRef.current.instanceId)) as T,
-  );
-
-  refs.current.state = state;
   refs.current.equals = equals;
   refs.current.selector = selector;
   const prevArgs = refs.current.args;
   refs.current.args = args;
 
   if (didInit && !argsEqual(prevArgs, args)) {
-    const newState = applySelector(
-      apiRef,
-      refs.current.selector,
-      refs.current.args,
-      apiRef.current.instanceId,
-    ) as T;
-    if (!refs.current.equals(refs.current.state, newState)) {
-      refs.current.state = newState;
-      setState(newState);
-    }
+    refs.current.argsId += 1;
   }
 
-  React.useInsertionEffect(() => {
-    return apiRef.current.store.subscribe(() => {
-      const newState = applySelector(
-        apiRef,
-        refs.current.selector,
-        refs.current.args,
-        apiRef.current.instanceId,
-      ) as T;
-      if (!refs.current.equals(refs.current.state, newState)) {
-        refs.current.state = newState;
-        setState(newState);
-      }
-    });
-  }, EMPTY);
+  const getSnapShot = React.useCallback(
+    () => getState(apiRef, refs),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [apiRef, refs.current.argsId],
+  );
 
+  const subscribe = React.useCallback(
+    (listener: (state: T) => void) =>
+      apiRef.current.store.subscribe(() => getState(apiRef, refs, listener)),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    EMPTY,
+  );
+
+  const state = useSyncExternalStore(subscribe, getSnapShot, getSnapShot);
   return state;
 };
