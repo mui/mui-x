78ae1768f383470d7c345da246393fe9c4b9b6d4
Bilal Shafi <bilalshafidev@gmail.com>
2025-02-06 21:56:13 +0500

    Simplify the selector

--- packages/x-data-grid/src/hooks/features/dataSource/gridDataSourceSelector.ts
+++ packages/x-data-grid/src/hooks/features/dataSource/gridDataSourceSelector.ts
@@ -1,26 +1,18 @@
-import type { GridPaginationModel } from '../../../models';
 import { gridFilterModelSelector } from '../filter/gridFilterSelector';
 import { gridSortModelSelector } from '../sorting/gridSortingSelector';
 import { gridPaginationModelSelector } from '../pagination/gridPaginationSelector';
 import { createSelector } from '../../../utils/createSelector';
 
-const computeStartEnd = (paginationModel: GridPaginationModel) => {
-  const start = paginationModel.page * paginationModel.pageSize;
-  const end = start + paginationModel.pageSize - 1;
-  return { start, end };
-};
-
 export const gridGetRowsParamsSelector = createSelector(
   gridFilterModelSelector,
   gridSortModelSelector,
   gridPaginationModelSelector,
-  (filterModel, sortModel, paginationModel) => {
-    return {
-      groupKeys: [],
-      paginationModel,
-      sortModel,
-      filterModel,
-      ...computeStartEnd(paginationModel),
-    };
-  },
+  (filterModel, sortModel, paginationModel) => ({
+    groupKeys: [],
+    paginationModel,
+    sortModel,
+    filterModel,
+    start: paginationModel.page * paginationModel.pageSize,
+    end: paginationModel.page * paginationModel.pageSize + paginationModel.pageSize - 1,
+  }),
 );
