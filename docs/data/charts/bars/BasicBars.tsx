6bb3bf1c07f0152407e535ac6749d5b78f2ab03e
alex <alex.fauquette@gmail.com>
2025-02-07 11:23:32 +0100

    ci

--- docs/data/charts/bars/BasicBars.tsx
+++ docs/data/charts/bars/BasicBars.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import { BarChartPro } from '@mui/x-charts-pro/BarChartPro';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 export default function BasicBars() {
   return (
-    <BarChartPro
-      xAxis={[
-        { scaleType: 'band', data: ['group A', 'group B', 'group C'], zoom: true },
-      ]}
+    <BarChart
+      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
       series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
       width={500}
       height={300}
