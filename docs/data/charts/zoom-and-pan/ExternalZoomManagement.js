bb972f5432e65ef774d0baf56795ff23eb74259d
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2025-02-17 14:59:23 +0100

    [charts] Add back zoom control (#16550)

--- docs/data/charts/zoom-and-pan/ExternalZoomManagement.js
+++ docs/data/charts/zoom-and-pan/ExternalZoomManagement.js
@@ -3,57 +3,42 @@ import { LineChartPro } from '@mui/x-charts-pro/LineChartPro';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-import { BarChartPro } from '@mui/x-charts-pro/BarChartPro';
 import { randomData } from './randomData';
-
-const lineAxis = [
-  {
-    zoom: true,
-    scaleType: 'point',
-    id: 'shared-x-axis',
-    data: randomData.map((v, i) => i),
-  },
-];
-
-const barAxis = [
-  {
-    zoom: true,
-    scaleType: 'band',
-    id: 'shared-x-axis',
-    data: randomData.map((v, i) => i),
-  },
-];
 
 const initialZoomData = [
   {
-    axisId: 'shared-x-axis',
+    axisId: 'my-x-axis',
     start: 20,
     end: 40,
   },
 ];
 
-export default function ZoomControlled() {
+export default function ExternalZoomManagement() {
+  const apiRef = React.useRef(undefined);
+
   const [zoomData, setZoomData] = React.useState(initialZoomData);
 
   return (
     <Stack direction={'column'} alignItems={'center'}>
       <LineChartPro
         {...chartProps}
+        initialZoom={initialZoomData}
+        apiRef={apiRef}
         onZoomChange={(newZoomData) => setZoomData(newZoomData)}
-        zoomData={zoomData}
-        xAxis={lineAxis}
-      />
-      <BarChartPro
-        {...chartProps}
-        onZoomChange={(newZoomData) => setZoomData(newZoomData)}
-        zoomData={zoomData}
-        xAxis={barAxis}
+        xAxis={[
+          {
+            zoom: true,
+            scaleType: 'point',
+            id: 'my-x-axis',
+            data: randomData.map((v, i) => i),
+          },
+        ]}
       />
       <pre>{JSON.stringify(zoomData, null, 2)}</pre>
       <Button
         variant="contained"
         onClick={() =>
-          setZoomData([{ axisId: 'shared-x-axis', start: 0, end: 100 }])
+          apiRef.current.setZoomData([{ axisId: 'my-x-axis', start: 0, end: 100 }])
         }
       >
         Reset zoom
