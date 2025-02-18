4a2ffb298c0c11a03cebe9a8158832dc1df0799d
Jose Quintas <juniorquintas@gmail.com>
2025-02-18 17:11:43 +0100

    fix readonly

--- packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/defaultizeAxis.ts
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/defaultizeAxis.ts
@@ -6,17 +6,17 @@ import { DatasetType } from '../../../../models/seriesType/config';
 
 export function defaultizeAxis(
   inAxis: readonly MakeOptional<AxisConfig<ScaleName, any, ChartsXAxisProps>, 'id'>[] | undefined,
-  dataset: DatasetType | undefined,
+  dataset: Readonly<DatasetType> | undefined,
   axisName: 'x',
 ): AxisConfig<ScaleName, any, ChartsXAxisProps>[];
 export function defaultizeAxis(
   inAxis: readonly MakeOptional<AxisConfig<ScaleName, any, ChartsYAxisProps>, 'id'>[] | undefined,
-  dataset: DatasetType | undefined,
+  dataset: Readonly<DatasetType> | undefined,
   axisName: 'y',
 ): AxisConfig<ScaleName, any, ChartsYAxisProps>[];
 export function defaultizeAxis(
   inAxis: readonly MakeOptional<AxisConfig, 'id'>[] | undefined,
-  dataset: DatasetType | undefined,
+  dataset: Readonly<DatasetType> | undefined,
   axisName: 'x' | 'y',
 ): AxisConfig[] {
   const DEFAULT_AXIS_KEY = axisName === 'x' ? DEFAULT_X_AXIS_KEY : DEFAULT_Y_AXIS_KEY;
