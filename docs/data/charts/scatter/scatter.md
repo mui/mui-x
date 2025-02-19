9832ba3f4ad77dc9bec132e960ba495d385678c1
Jose Quintas <juniorquintas@gmail.com>
2025-02-19 14:44:46 +0100

    fix scripts

--- docs/data/charts/scatter/scatter.md
+++ docs/data/charts/scatter/scatter.md
@@ -1,7 +1,7 @@
 ---
 title: React Scatter chart
 productId: x-charts
-components: ScatterChart, ScatterChartPro, ScatterPlot, ChartsGrid
+components: ScatterChart, ScatterChartPro, ScatterPlot, ChartsGrid, ScatterMarker
 ---
 
 # Charts - Scatter
--- packages/x-charts/src/ScatterChart/ScatterMarker.tsx
+++ packages/x-charts/src/ScatterChart/ScatterMarker.tsx
@@ -5,13 +5,37 @@ import { consumeSlots } from '../internals/consumeSlots';
 import { ScatterMarkerSlotExtension } from './ScatterMarker.types';
 
 export interface ScatterMarkerProps {
+  /**
+   * The series ID.
+   */
   seriesId: SeriesId;
+  /**
+   * The index of the data point.
+   */
   dataIndex: number;
+  /**
+   * The x coordinate of the data point.
+   */
   x: number;
+  /**
+   * The y coordinate of the data point.
+   */
   y: number;
+  /**
+   * The fill color of the marker.
+   */
   color: string;
+  /**
+   * The size of the marker.
+   */
   size: number;
+  /**
+   * If `true`, the marker is highlighted.
+   */
   isHighlighted: boolean;
+  /**
+   * If `true`, the marker is faded.
+   */
   isFaded: boolean;
   /**
    * Callback fired when clicking on a scatter item.
