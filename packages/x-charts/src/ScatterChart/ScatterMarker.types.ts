e1464ca12c66440ed30607228af0490cad324ab7
Jose Quintas <juniorquintas@gmail.com>
2025-02-19 14:04:26 +0100

    use consumeslots

--- packages/x-charts/src/ScatterChart/ScatterMarker.types.ts
+++ packages/x-charts/src/ScatterChart/ScatterMarker.types.ts
@@ -1,21 +1,5 @@
 import * as React from 'react';
-import { useInteractionItemProps } from '../hooks/useInteractionItemProps';
-import { DefaultizedScatterSeriesType } from '../models';
-
-export interface ScatterMarkerProps {
-  series: DefaultizedScatterSeriesType;
-  dataIndex: number;
-  x: number;
-  y: number;
-  color: string;
-  isHighlighted: boolean;
-  isFaded: boolean;
-  /**
-   * Callback fired when clicking on a scatter item.
-   * @param {MouseEvent} event Mouse event recorded on the `<svg/>` element.
-   */
-  onClick?: (event: React.MouseEvent<SVGElement, MouseEvent>) => void;
-}
+import type { ScatterMarkerProps } from './ScatterMarker';
 
 export interface ScatterMarkerSlots {
   /**
@@ -29,13 +13,15 @@ export interface ScatterMarkerSlotProps {
   marker?: ScatterMarkerProps;
 }
 
-export interface ScatterMarkerElementProps
-  extends Pick<
-    ScatterMarkerProps,
-    'series' | 'dataIndex' | 'x' | 'y' | 'color' | 'isHighlighted' | 'isFaded'
-  > {
-  slots?: ScatterMarkerSlots;
+export interface ScatterMarkerSlotExtension {
+  /**
+   * The props used for each component slot.
+   * @default {}
+   */
   slotProps?: ScatterMarkerSlotProps;
-  onItemClick: ScatterMarkerProps['onClick'];
-  interactionProps: ReturnType<ReturnType<typeof useInteractionItemProps>>;
+  /**
+   * Overridable component slots.
+   * @default {}
+   */
+  slots?: ScatterMarkerSlots;
 }
--- packages/x-charts/src/ScatterChart/index.ts
+++ packages/x-charts/src/ScatterChart/index.ts
@@ -1,7 +1,5 @@
 export * from './ScatterChart';
-
 export * from './ScatterPlot';
-
 export * from './Scatter';
-
 export * from './ScatterMarker.types';
+export * from './ScatterMarker';
