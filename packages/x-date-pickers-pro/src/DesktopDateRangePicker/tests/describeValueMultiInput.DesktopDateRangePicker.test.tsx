af8e372d8a31554b8cf15624ee9416831f9a68d6
Jose Quintas <juniorquintas@gmail.com>
2025-02-20 19:14:49 +0100

    separate files

--- packages/x-date-pickers-pro/src/DesktopDateRangePicker/tests/describeValueMultiInput.DesktopDateRangePicker.test.tsx
+++ packages/x-date-pickers-pro/src/DesktopDateRangePicker/tests/describeValueMultiInput.DesktopDateRangePicker.test.tsx
@@ -4,7 +4,6 @@ import {
   createPickerRenderer,
   expectFieldValueV7,
   describeValue,
-  getFieldInputRoot,
   getFieldSectionsContainer,
 } from 'test/utils/pickers';
 import { DesktopDateRangePicker } from '@mui/x-date-pickers-pro/DesktopDateRangePicker';
@@ -43,65 +42,6 @@ describe('<DesktopDateRangePicker /> - Describe Value', () => {
         ? adapterToUse.format(expectedValues[1], 'keyboardDate')
         : 'MM/DD/YYYY';
       expectFieldValueV7(endSectionsContainer, expectedEndValueStr);
-    },
-    setNewValue: async (
-      value,
-      _,
-      { isOpened, applySameValue, setEndDate = false, selectSection, pressKey },
-    ) => {
-      let newValue: PickerNonNullableRangeValue;
-      if (applySameValue) {
-        newValue = value;
-      } else if (setEndDate) {
-        newValue = [value[0], adapterToUse.addDays(value[1], 1)];
-      } else {
-        newValue = [adapterToUse.addDays(value[0], 1), value[1]];
-      }
-
-      if (isOpened) {
-        fireEvent.click(
-          screen.getAllByRole('gridcell', {
-            name: adapterToUse.getDate(newValue[setEndDate ? 1 : 0]).toString(),
-          })[0],
-        );
-      } else {
-        selectSection('day');
-        pressKey(undefined, 'ArrowUp');
-      }
-
-      return newValue;
-    },
-  }));
-
-  // With single input field
-  describeValue<PickerRangeValue, 'picker'>(DesktopDateRangePicker, () => ({
-    render,
-    componentFamily: 'picker',
-    type: 'date-range',
-    variant: 'desktop',
-    initialFocus: 'start',
-    fieldType: 'single-input',
-    values: [
-      // initial start and end dates
-      [adapterToUse.date('2018-01-01'), adapterToUse.date('2018-01-04')],
-      // start and end dates after `setNewValue`
-      [adapterToUse.date('2018-01-02'), adapterToUse.date('2018-01-05')],
-    ],
-    emptyValue: [null, null],
-    assertRenderedValue: (expectedValues: any[]) => {
-      const fieldRoot = getFieldInputRoot(0);
-
-      const expectedStartValueStr = expectedValues[0]
-        ? adapterToUse.format(expectedValues[0], 'keyboardDate')
-        : 'MM/DD/YYYY';
-
-      const expectedEndValueStr = expectedValues[1]
-        ? adapterToUse.format(expectedValues[1], 'keyboardDate')
-        : 'MM/DD/YYYY';
-
-      const expectedValueStr = `${expectedStartValueStr} – ${expectedEndValueStr}`;
-
-      expectFieldValueV7(fieldRoot, expectedValueStr);
     },
     setNewValue: async (
       value,
