fcd60d8af026565deac08e5930e59df4920b1b2d
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-02-20 07:59:20 +0100

    Add unit test

--- packages/x-charts/src/PieChart/PieChart.test.tsx
+++ packages/x-charts/src/PieChart/PieChart.test.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { createRenderer } from '@mui/internal-test-utils/createRenderer';
+import { createRenderer, screen } from '@mui/internal-test-utils/createRenderer';
 import { describeConformance } from 'test/utils/describeConformance';
 import { PieChart } from '@mui/x-charts/PieChart';
+import { expect } from 'chai';
 
 describe('<PieChart />', () => {
   const { render } = createRenderer();
@@ -38,7 +39,10 @@ describe('<PieChart />', () => {
     }),
   );
 
-  it('does not crash when missing series', () => {
-    render(<PieChart series={[]} width={400} height={200} />);
+  it('should render "No Data" overlay when series prop is an empty array', () => {
+    render(<PieChart height={100} width={100} series={[]} />);
+
+    const noDataOverlay = screen.getByText('No data to display');
+    expect(noDataOverlay).toBeVisible();
   });
 });
