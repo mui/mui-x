8fdf0524eba9d03915bb6f91ded0febde50eb93a
Jose Quintas <juniorquintas@gmail.com>
2025-02-24 18:41:40 +0100

    remove key

--- test/performance-charts/tests/FunnelChart.bench.tsx
+++ test/performance-charts/tests/FunnelChart.bench.tsx
@@ -3,11 +3,10 @@ import * as React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { bench, describe } from 'vitest';
 import { FunnelChart } from '@mui/x-charts-pro/FunnelChart';
-import { LicenseInfo, generateLicense } from '@mui/x-license';
 import { options } from '../utils/options';
 
 describe('FunnelChart', () => {
-  const dataLength = 100;
+  const dataLength = 10;
   const series = [
     {
       data: Array.from({ length: dataLength }, (_, i) => ({ value: dataLength / (i + 1) })),
@@ -17,16 +16,6 @@ describe('FunnelChart', () => {
   bench(
     'FunnelChart with big data amount',
     async () => {
-      const licenseKey = generateLicense({
-        expiryDate: new Date(3001, 0, 0, 0, 0, 0, 0),
-        orderNumber: 'MUI-123',
-        planScope: 'pro',
-        licenseModel: 'subscription',
-        planVersion: 'Q3-2024',
-      });
-
-      LicenseInfo.setLicenseKey(licenseKey);
-
       const { findByText } = render(<FunnelChart series={series} width={500} height={300} />);
 
       await findByText(dataLength.toLocaleString(), { ignore: 'span' });
