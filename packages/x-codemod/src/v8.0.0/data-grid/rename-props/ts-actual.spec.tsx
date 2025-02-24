958e24426bfec884c04c3db62334f03e680ddccb
Bilal Shafi <bilalshafidev@gmail.com>
2025-02-24 17:49:22 +0500

    [codemod] Add a few Data Grid codemods

---
+++ packages/x-codemod/src/v8.0.0/data-grid/rename-props/ts-actual.spec.tsx
@@ -0,0 +1,24 @@
+// @ts-nocheck
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { DataGridPro } from '@mui/x-data-grid-pro';
+import { DataGridPremium } from '@mui/x-data-grid-premium';
+
+// prettier-ignore
+function App() {
+  return (
+    <React.Fragment>
+      <DataGrid
+        unstable_rowSpanning
+      />
+      <DataGridPro
+        unstable_rowSpanning
+      />
+      <DataGridPremium
+        unstable_rowSpanning
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
