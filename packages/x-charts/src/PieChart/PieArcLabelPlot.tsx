1613993896045cbb7fecbbc848066143368c284b
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-02-27 08:37:17 +0100

    [charts] Accept component in `labelMarkType` (#16739)
    Signed-off-by: Bernardo Belchior <Bernardo.belchior1@gmail.com>
    Co-authored-by: Jose C Quintas Jr <juniorquintas@gmail.com>

--- packages/x-charts/src/PieChart/PieArcLabelPlot.tsx
+++ packages/x-charts/src/PieChart/PieArcLabelPlot.tsx
@@ -206,7 +206,10 @@ PieArcLabelPlot.propTypes = {
       id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
       index: PropTypes.number.isRequired,
       label: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-      labelMarkType: PropTypes.oneOf(['circle', 'line', 'square']),
+      labelMarkType: PropTypes.oneOfType([
+        PropTypes.oneOf(['circle', 'line', 'square']),
+        PropTypes.func,
+      ]),
       padAngle: PropTypes.number.isRequired,
       startAngle: PropTypes.number.isRequired,
       value: PropTypes.number.isRequired,
