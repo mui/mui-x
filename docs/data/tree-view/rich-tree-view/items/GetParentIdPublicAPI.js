bc2e1c57da8111a3b77ea61089cd1e0e3dbfd395
noraleonte <noraleonte00@gmail.com>
2025-03-04 16:33:48 +0200

    document getParentId

---
+++ docs/data/tree-view/rich-tree-view/items/GetParentIdPublicAPI.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
+import { useTreeViewApiRef } from '@mui/x-tree-view/hooks';
+import { MUI_X_PRODUCTS } from './products';
+
+export default function GetParentIdPublicAPI() {
+  const apiRef = useTreeViewApiRef();
+  const [selectedItemParent, setSelectedItemParent] = React.useState();
+
+  const handleSelectedItemsChange = (_event, id) => {
+    if (id) {
+      const parentId = apiRef.current?.getParentId(id);
+      if (parentId) {
+        setSelectedItemParent(parentId);
+      }
+    }
+  };
+
+  return (
+    <Stack spacing={2}>
+      {selectedItemParent && (
+        <Alert severity="info">Selected child of {selectedItemParent}</Alert>
+      )}
+
+      <Box sx={{ minHeight: 352, minWidth: 250 }}>
+        <RichTreeView
+          apiRef={apiRef}
+          items={MUI_X_PRODUCTS}
+          defaultSelectedItems="tree-view"
+          onSelectedItemsChange={handleSelectedItemsChange}
+        />
+      </Box>
+    </Stack>
+  );
+}
