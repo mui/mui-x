e9cec83332a973f9c441b0f56aeabe09945d3b0d
alex <alex.fauquette@gmail.com>
2025-03-05 16:06:54 +0100

    Fix dark mode

--- packages/x-charts/src/RadarChart/RadarGrid/RadarGrid.tsx
+++ packages/x-charts/src/RadarChart/RadarGrid/RadarGrid.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
 import { useRadarGridData } from './useRadarGridData';
 
 export interface RadarGridProps {
@@ -14,12 +15,20 @@ function RadarGrid(props: RadarGridProps) {
   const { divisions = 5 } = props;
   const { center, corners } = useRadarGridData();
 
+  const theme = useTheme();
+
   const divisionRatio = Array.from({ length: divisions }, (_, index) => (index + 1) / divisions);
 
   return (
     <React.Fragment>
       {corners.map(({ x, y }, i) => (
-        <path key={i} d={`M ${center.x} ${center.y} L ${x} ${y}`} stroke="black" />
+        <path
+          key={i}
+          d={`M ${center.x} ${center.y} L ${x} ${y}`}
+          stroke={(theme.vars || theme).palette.text.primary}
+          strokeWidth={1}
+          fill="none"
+        />
       ))}
       {divisionRatio.map((ratio) => (
         <path
@@ -30,7 +39,8 @@ function RadarGrid(props: RadarGridProps) {
                 `${center.x * (1 - ratio) + ratio * x} ${center.y * (1 - ratio) + ratio * y}`,
             )
             .join(' L ')} Z`}
-          stroke="black"
+          stroke={(theme.vars || theme).palette.text.primary}
+          strokeWidth={1}
           fill="none"
         />
       ))}
