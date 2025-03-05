fa6a19e75ae68328cbd2b1a3546cae6cdbbb7481
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-03-05 12:27:50 +0100

    Address PR feedback

--- docs/data/charts/axis/AxisTextCustomization.js
+++ docs/data/charts/axis/AxisTextCustomization.js
@@ -16,13 +16,13 @@ export default function AxisTextCustomization() {
         angle: { knob: 'number', defaultValue: 45, min: -180, max: 180 },
         textAnchor: {
           knob: 'select',
-          defaultValue: 'unset',
-          options: ['unset', 'start', 'middle', 'end'],
+          defaultValue: 'undefined',
+          options: ['undefined', 'start', 'middle', 'end'],
         },
         dominantBaseline: {
           knob: 'select',
-          defaultValue: 'unset',
-          options: ['unset', 'auto', 'central', 'hanging'],
+          defaultValue: 'undefined',
+          options: ['undefined', 'auto', 'central', 'hanging'],
         },
         fontSize: { knob: 'number', defaultValue: 12 },
         labelFontSize: { knob: 'number', defaultValue: 14 },
@@ -42,10 +42,10 @@ export default function AxisTextCustomization() {
                 },
                 tickLabelStyle: {
                   angle: props.angle,
-                  ...(props.textAnchor === 'unset'
+                  ...(props.textAnchor === 'undefined'
                     ? {}
                     : { textAnchor: props.textAnchor }),
-                  ...(props.dominantBaseline === 'unset'
+                  ...(props.dominantBaseline === 'undefined'
                     ? {}
                     : { dominantBaseline: props.dominantBaseline }),
                   fontSize: props.fontSize,
@@ -73,12 +73,12 @@ export default function AxisTextCustomization() {
       },
       tickLabelStyle: {
         angle: ${props.angle},${
-          props.textAnchor === 'unset'
+          props.textAnchor === 'undefined'
             ? ''
             : `
         textAnchor: '${props.textAnchor}',`
         }${
-          props.dominantBaseline === 'unset'
+          props.dominantBaseline === 'undefined'
             ? ''
             : `
         dominantBaseline: '${props.dominantBaseline}',`
--- docs/data/charts/axis/AxisTextCustomization.tsx
+++ docs/data/charts/axis/AxisTextCustomization.tsx
@@ -17,13 +17,13 @@ export default function AxisTextCustomization() {
           angle: { knob: 'number', defaultValue: 45, min: -180, max: 180 },
           textAnchor: {
             knob: 'select',
-            defaultValue: 'unset',
-            options: ['unset', 'start', 'middle', 'end'],
+            defaultValue: 'undefined',
+            options: ['undefined', 'start', 'middle', 'end'],
           },
           dominantBaseline: {
             knob: 'select',
-            defaultValue: 'unset',
-            options: ['unset', 'auto', 'central', 'hanging'],
+            defaultValue: 'undefined',
+            options: ['undefined', 'auto', 'central', 'hanging'],
           },
           fontSize: { knob: 'number', defaultValue: 12 },
           labelFontSize: { knob: 'number', defaultValue: 14 },
@@ -44,10 +44,10 @@ export default function AxisTextCustomization() {
                 },
                 tickLabelStyle: {
                   angle: props.angle,
-                  ...(props.textAnchor === 'unset'
+                  ...(props.textAnchor === 'undefined'
                     ? {}
                     : { textAnchor: props.textAnchor }),
-                  ...(props.dominantBaseline === 'unset'
+                  ...(props.dominantBaseline === 'undefined'
                     ? {}
                     : { dominantBaseline: props.dominantBaseline }),
                   fontSize: props.fontSize,
@@ -75,12 +75,12 @@ export default function AxisTextCustomization() {
       },
       tickLabelStyle: {
         angle: ${props.angle},${
-          props.textAnchor === 'unset'
+          props.textAnchor === 'undefined'
             ? ''
             : `
         textAnchor: '${props.textAnchor}',`
         }${
-          props.dominantBaseline === 'unset'
+          props.dominantBaseline === 'undefined'
             ? ''
             : `
         dominantBaseline: '${props.dominantBaseline}',`
