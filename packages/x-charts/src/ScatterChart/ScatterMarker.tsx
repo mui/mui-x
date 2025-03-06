7cfd130fe0d8a198ba0c97266c0bfb1f42af3145
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-03-06 17:25:11 +0100

    Inline consumeSlots

--- packages/x-charts/src/ScatterChart/ScatterMarker.tsx
+++ packages/x-charts/src/ScatterChart/ScatterMarker.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { useThemeProps } from '@mui/material/styles';
+import useSlotProps from '@mui/utils/useSlotProps';
 import { SeriesId } from '../models/seriesType/common';
-import { consumeSlots } from '../internals/consumeSlots';
 import { ScatterMarkerSlotExtension } from './ScatterMarker.types';
 
 export interface ScatterMarkerProps {
@@ -44,32 +45,95 @@ export interface ScatterMarkerProps {
   onClick?: (event: React.MouseEvent<SVGElement, MouseEvent>) => void;
 }
 
+function DefaultScatterMarker(props: ScatterMarkerProps) {
+  const { seriesId, isFaded, isHighlighted, x, y, color, size, dataIndex, ...other } = props;
+
+  return (
+    <circle
+      cx={0}
+      cy={0}
+      r={(isHighlighted ? 1.2 : 1) * size}
+      transform={`translate(${x}, ${y})`}
+      fill={color}
+      opacity={isFaded ? 0.3 : 1}
+      cursor={other.onClick ? 'pointer' : 'unset'}
+      {...other}
+    />
+  );
+}
+DefaultScatterMarker.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "pnpm proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * The fill color of the marker.
+   */
+  color: PropTypes.string.isRequired,
+  /**
+   * The index of the data point.
+   */
+  dataIndex: PropTypes.number.isRequired,
+  /**
+   * If `true`, the marker is faded.
+   */
+  isFaded: PropTypes.bool.isRequired,
+  /**
+   * If `true`, the marker is highlighted.
+   */
+  isHighlighted: PropTypes.bool.isRequired,
+  /**
+   * Callback fired when clicking on a scatter item.
+   * @param {MouseEvent} event Mouse event recorded on the `<svg/>` element.
+   */
+  onClick: PropTypes.func,
+  /**
+   * The series ID.
+   */
+  seriesId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  /**
+   * The size of the marker.
+   */
+  size: PropTypes.number.isRequired,
+  /**
+   * The x coordinate of the data point.
+   */
+  x: PropTypes.number.isRequired,
+  /**
+   * The y coordinate of the data point.
+   */
+  y: PropTypes.number.isRequired,
+} as any;
+
 /**
  * The marker that is rendered for each data point in the scatter chart.
  * By default, it is a circle.
  */
-const ScatterMarker = consumeSlots(
-  'MuiScatterMarker',
-  'marker',
-  {
-    // Currently required to make the prop types work
-    classesResolver: (_: ScatterMarkerProps & ScatterMarkerSlotExtension) => ({}),
-  },
-  function ScatterMarker(props: ScatterMarkerProps) {
-    const { seriesId, isFaded, isHighlighted, x, y, color, size, dataIndex, ...other } = props;
+const ScatterMarker = React.forwardRef<{}, ScatterMarkerProps & ScatterMarkerSlotExtension>(
+  function ScatterMarkerSlotWrapper(props, ref) {
+    const themedProps = useThemeProps({
+      props,
+      // eslint-disable-next-line material-ui/mui-name-matches-component-name
+      name: 'MuiScatterMarker',
+    });
+    const slotPropName = 'marker';
 
-    return (
-      <circle
-        cx={0}
-        cy={0}
-        r={(isHighlighted ? 1.2 : 1) * size}
-        transform={`translate(${x}, ${y})`}
-        fill={color}
-        opacity={isFaded ? 0.3 : 1}
-        cursor={other.onClick ? 'pointer' : 'unset'}
-        {...other}
-      />
-    );
+    const { slots, slotProps, ...other } = themedProps as {
+      slots?: Record<string, any>;
+      slotProps?: Record<string, any>;
+    };
+
+    // Can be a function component or a forward ref component.
+    const Component = slots?.[slotPropName] ?? DefaultScatterMarker;
+
+    const { ownerState, ...outProps } = useSlotProps({
+      elementType: Component,
+      externalSlotProps: slotProps?.[slotPropName],
+      additionalProps: other,
+      ownerState: {},
+    });
+
+    return <Component {...outProps} ref={ref} />;
   },
 );
 
@@ -78,16 +142,52 @@ ScatterMarker.propTypes = {
   // | These PropTypes are generated from the TypeScript type definitions |
   // | To update them edit the TypeScript types and run "pnpm proptypes"  |
   // ----------------------------------------------------------------------
+  /**
+   * The fill color of the marker.
+   */
   color: PropTypes.string.isRequired,
-  seriesId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  /**
+   * The index of the data point.
+   */
   dataIndex: PropTypes.number.isRequired,
+  /**
+   * If `true`, the marker is faded.
+   */
   isFaded: PropTypes.bool.isRequired,
+  /**
+   * If `true`, the marker is highlighted.
+   */
   isHighlighted: PropTypes.bool.isRequired,
-  onItemClick: PropTypes.func,
-  series: PropTypes.object.isRequired,
+  /**
+   * Callback fired when clicking on a scatter item.
+   * @param {MouseEvent} event Mouse event recorded on the `<svg/>` element.
+   */
+  onClick: PropTypes.func,
+  /**
+   * The series ID.
+   */
+  seriesId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  /**
+   * The size of the marker.
+   */
+  size: PropTypes.number.isRequired,
+  /**
+   * The props used for each component slot.
+   * @default {}
+   */
   slotProps: PropTypes.object,
+  /**
+   * Overridable component slots.
+   * @default {}
+   */
   slots: PropTypes.object,
+  /**
+   * The x coordinate of the data point.
+   */
   x: PropTypes.number.isRequired,
+  /**
+   * The y coordinate of the data point.
+   */
   y: PropTypes.number.isRequired,
 } as any;
 
