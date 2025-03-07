6d80993a4fdc5264e91ae271a1fe5bbf9a9778d4
Jose Quintas <juniorquintas@gmail.com>
2025-03-07 19:19:29 +0100

    add chart interaction using @use-gesture/react

--- packages/x-charts/src/internals/plugins/corePlugins/useChartInteraction/useChartInteraction.ts
+++ packages/x-charts/src/internals/plugins/corePlugins/useChartInteraction/useChartInteraction.ts
@@ -1,15 +1,73 @@
 'use client';
 import * as React from 'react';
+import { Handler, useGesture } from '@use-gesture/react';
 import { ChartPlugin } from '../../models';
-import { UseChartInteractionSignature, AddInteractionListener } from './useChartInteraction.types';
+import {
+  UseChartInteractionSignature,
+  AddInteractionListener,
+  ChartInteraction,
+} from './useChartInteraction.types';
+
+type ListenerRef = Map<ChartInteraction, Set<Handler<any>>>;
 
 export const useChartInteraction: ChartPlugin<UseChartInteractionSignature> = ({ svgRef }) => {
-  const addInteractionListener: AddInteractionListener = React.useCallback(
-    (interaction, callback, options) => {
-      svgRef.current?.addEventListener(interaction, callback, options);
+  const listenersRef = React.useRef<ListenerRef>(new Map());
+
+  const retriggerEvent = React.useCallback((interaction: ChartInteraction, state: any) => {
+    const listeners = listenersRef.current.get(interaction);
+    if (listeners) {
+      listeners.forEach((callback) => callback(state));
+    }
+  }, []);
+
+  useGesture(
+    {
+      onDrag: (state) => retriggerEvent('drag', state),
+      onDragStart: (state) => retriggerEvent('dragStart', state),
+      onDragEnd: (state) => retriggerEvent('dragEnd', state),
+      onPinch: (state) => retriggerEvent('pinch', state),
+      onPinchStart: (state) => retriggerEvent('pinchStart', state),
+      onPinchEnd: (state) => retriggerEvent('pinchEnd', state),
+      onWheel: (state) => retriggerEvent('wheel', state),
+      onWheelStart: (state) => retriggerEvent('wheelStart', state),
+      onWheelEnd: (state) => retriggerEvent('wheelEnd', state),
+      onMove: (state) => retriggerEvent('move', state),
+      onMoveStart: (state) => retriggerEvent('moveStart', state),
+      onMoveEnd: (state) => retriggerEvent('moveEnd', state),
     },
-    [svgRef],
+    {
+      target: svgRef,
+    },
   );
+
+  const addInteractionListener: AddInteractionListener = React.useCallback(
+    (interaction, callback) => {
+      const listeners = listenersRef.current.get(interaction);
+
+      if (!listeners) {
+        const newSet = new Set<Handler<any>>();
+        newSet.add(callback);
+        listenersRef.current.set(interaction, newSet);
+      } else {
+        listenersRef.current.set(interaction, listeners.add(callback));
+      }
+
+      return () => {
+        if (listeners) {
+          listeners.delete(callback);
+        }
+      };
+    },
+    [],
+  );
+
+  React.useEffect(() => {
+    const ref = listenersRef.current;
+
+    return () => {
+      ref.clear();
+    };
+  }, [svgRef]);
 
   return {
     instance: {
--- packages/x-charts/src/internals/plugins/corePlugins/useChartInteraction/useChartInteraction.types.ts
+++ packages/x-charts/src/internals/plugins/corePlugins/useChartInteraction/useChartInteraction.types.ts
@@ -1,32 +1,40 @@
+import { Handler, WebKitGestureEvent } from '@use-gesture/react';
 import { ChartPluginSignature } from '../../models';
 
 export type ChartInteraction =
-  | 'pan'
-  | 'panStart'
-  | 'panEnd'
+  | 'drag'
+  | 'dragStart'
+  | 'dragEnd'
   | 'pinch'
   | 'pinchStart'
   | 'pinchEnd'
-  | 'scroll'
-  | 'scrollStart'
-  | 'scrollEnd'
+  | 'wheel'
+  | 'wheelStart'
+  | 'wheelEnd'
   | 'move'
   | 'moveStart'
   | 'moveEnd';
 
-export type ChartInteractionOptions = {
-  capture?: boolean;
-  once?: boolean;
-  passive?: boolean;
-  signal: AbortSignal;
-};
+export type RemoveInteractionListener = () => void;
 
-export type AddInteractionListener = (
-  interaction: ChartInteraction,
-  // TODO: Custom Event type/data
-  callback: () => void,
-  options: ChartInteractionOptions,
-) => void;
+export type AddInteractionListener = {
+  (
+    interaction: 'drag' | 'dragStart' | 'dragEnd',
+    callback: Handler<'drag', PointerEvent | MouseEvent | TouchEvent | KeyboardEvent>,
+  ): RemoveInteractionListener;
+  (
+    interaction: 'pinch' | 'pinchStart' | 'pinchEnd',
+    callback: Handler<'pinch', PointerEvent | TouchEvent | WheelEvent | WebKitGestureEvent>,
+  ): RemoveInteractionListener;
+  (
+    interaction: 'wheel' | 'wheelStart' | 'wheelEnd',
+    callback: Handler<'wheel', WheelEvent>,
+  ): RemoveInteractionListener;
+  (
+    interaction: 'move' | 'moveStart' | 'moveEnd',
+    callback: Handler<'move', PointerEvent>,
+  ): RemoveInteractionListener;
+};
 
 export interface UseChartInteractionParameters {}
 
@@ -38,31 +46,8 @@ export interface UseChartInteractionInstance {
   /**
    * Adds an interaction listener to the SVG element.
    *
-   * An interrupt signal is required to stop the interaction listener.
-   * We don't provide a way to remove the listener.
-   *
-   * @example
-   * ```tsx
-   * const { instance } = useChartInteraction();
-   *
-   * useEffect(() => {
-   *   const abortController = new AbortController();
-   *
-   *   instance.addInteractionListener(
-   *     'move',
-   *     () => console.log('Move'),
-   *     { signal: abortController.signal }
-   *   );
-   *
-   *   return () => {
-   *     abortController.abort();
-   *   };
-   * }, []);
-   * ```
-   *
    * @param interaction The interaction to listen to.
    * @param callback The callback to call when the interaction occurs.
-   * @param options The options to use when adding the event listener.
    */
   addInteractionListener: AddInteractionListener;
 }
