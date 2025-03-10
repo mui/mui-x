63968d49352a20b6890688246143b33e716fb9e2
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2025-03-10 10:36:33 +0000

    fix wording

--- packages/x-data-grid/src/components/quickFilter/QuickFilterTrigger.tsx
+++ packages/x-data-grid/src/components/quickFilter/QuickFilterTrigger.tsx
@@ -19,7 +19,7 @@ export type QuickFilterTriggerProps = Omit<GridSlotProps['baseButton'], 'classNa
 };
 
 /**
- * A button that opens the quick filter.
+ * A button that expands the quick filter.
  * It renders the `baseButton` slot.
  *
  * Demos:
