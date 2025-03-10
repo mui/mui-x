5edd36929c805f377297b6533dad4b5bd90d4674
Flavien DELANGLE <flaviendelangle@gmail.com>
2025-03-10 11:33:27 +0100

    [DateRangeCalendar] Do not update the previewed day when hover a day and the value is empty (#16819)

--- packages/x-date-pickers-pro/src/DateRangeCalendar/DateRangeCalendar.tsx
+++ packages/x-date-pickers-pro/src/DateRangeCalendar/DateRangeCalendar.tsx
@@ -25,6 +25,9 @@ import {
   DEFAULT_DESKTOP_MODE_MEDIA_QUERY,
   useControlledValueWithTimezone,
   useViews,
+  PickerRangeValue,
+  usePickerPrivateContext,
+  areDatesEqual,
 } from '@mui/x-date-pickers/internals';
 import { warnOnce } from '@mui/x-internals/warning';
 import { PickerValidDate } from '@mui/x-date-pickers/models';
@@ -440,11 +443,18 @@ const DateRangeCalendar = React.forwardRef(function DateRangeCalendar<
   });
 
   const handleDayMouseEnter = useEventCallback(
-    (event: React.MouseEvent<HTMLDivElement>, newPreviewRequest: TDate) => {
-      if (!isWithinRange(utils, newPreviewRequest, valueDayRange)) {
-        setRangePreviewDay(newPreviewRequest);
+    (event: React.MouseEvent<HTMLDivElement>, newRangePreviewDay: PickerValidDate) => {
+      let cleanNewRangePreviewDay: PickerValidDate | null;
+      if (valueDayRange[0] == null && valueDayRange[1] == null) {
+        cleanNewRangePreviewDay = null;
+      } else if (isWithinRange(utils, newRangePreviewDay, valueDayRange)) {
+        cleanNewRangePreviewDay = null;
       } else {
-        setRangePreviewDay(null);
+        cleanNewRangePreviewDay = newRangePreviewDay;
+      }
+
+      if (!areDatesEqual(utils, cleanNewRangePreviewDay, rangePreviewDay)) {
+        setRangePreviewDay(cleanNewRangePreviewDay);
       }
     },
   );
