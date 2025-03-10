c6c064a4c8262858d0598cec55eb2931975892c7
Jose Quintas <juniorquintas@gmail.com>
2025-03-10 21:39:46 +0100

    removve comments

--- test/performance-charts/utils/directRender.ts
+++ test/performance-charts/utils/directRender.ts
@@ -14,20 +14,5 @@ export const directRender = (fn: React.JSX.Element) => {
   return {
     unmount: () => reactRoot.unmount(),
     ...screen,
-    // findByText: async (text: string) => {
-    //   const el = document.querySelector(
-    //     `#root *:not(script):not(style):not(link):not(meta):not(title):not(base):not(template):not(noscript):not(head):not(html):not(body):not(#root)`,
-    //   );
-    //   if (!el) {
-    //     throw new Error('Element not found');
-    //   }
-    //   if (!el.textContent) {
-    //     throw new Error('Element has no text content');
-    //   }
-    //   if (!el.textContent.includes(text)) {
-    //     throw new Error('Text not found');
-    //   }
-    //   return el;
-    // },
   };
 };
