2b4d46a8f4ab79f330a0878b39f14b5afa11fa54
alex <alex.fauquette@gmail.com>
2025-03-12 11:24:03 +0100

    basic implementation

--- packages/x-charts/src/RadarChart/RadarSeriesPlot/RadarSeriesPlot.tsx
+++ packages/x-charts/src/RadarChart/RadarSeriesPlot/RadarSeriesPlot.tsx
@@ -3,29 +3,41 @@ import PropTypes from 'prop-types';
 import { useRadarSeriesData } from './useRadarSeriesData';
 import { RadarSeriesPlotProps } from './RadarSeriesPlot.types';
 import { getAreaPath } from './getAreaPath';
+import { useInteractionAllItemProps } from '../../hooks/useInteractionItemProps';
+import { useItemHighlightedGetter } from '../../hooks/useItemHighlightedGetter';
 
 function RadarSeriesPlot(props: RadarSeriesPlotProps) {
   const seriesCoordinates = useRadarSeriesData(props.seriesId);
 
+  const interactionProps = useInteractionAllItemProps(seriesCoordinates);
+  const { isFaded, isHighlighted } = useItemHighlightedGetter();
   return (
     <React.Fragment>
-      {seriesCoordinates?.map(({ seriesId, points, color, showMark }) => (
-        <g key={seriesId}>
-          {
-            <path
-              key={seriesId}
-              d={getAreaPath(points)}
-              fill={color}
-              stroke={color}
-              fillOpacity={0.4}
-            />
-          }
-          {showMark &&
-            points.map((point, index) => (
-              <circle key={index} cx={point.x} cy={point.y} r={5} fill={color} stroke={color} />
-            ))}
-        </g>
-      ))}
+      {seriesCoordinates?.map(({ seriesId, points, color, showMark }, seriesIndex) => {
+        const isItemHighlighted = isHighlighted({ seriesId });
+        const isItemFaded = !isItemHighlighted && isFaded({ seriesId });
+
+        return (
+          <g key={seriesId}>
+            {
+              <path
+                key={seriesId}
+                d={getAreaPath(points)}
+                {...interactionProps[seriesIndex]}
+                fill={color}
+                stroke={color}
+                filter={isItemHighlighted ? 'brightness(120%)' : undefined}
+                strokeOpacity={isItemFaded ? 0.5 : 1}
+                fillOpacity={isItemFaded ? 0.1 : 0.4}
+              />
+            }
+            {showMark &&
+              points.map((point, index) => (
+                <circle key={index} cx={point.x} cy={point.y} r={5} fill={color} stroke={color} />
+              ))}
+          </g>
+        );
+      })}
     </React.Fragment>
   );
 }
