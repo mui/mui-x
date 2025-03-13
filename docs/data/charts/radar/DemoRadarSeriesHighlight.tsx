d73ccfb375fd9a05d6f8e3d8dff348de50e1fd64
alex <alex.fauquette@gmail.com>
2025-03-13 10:33:21 +0100

    refine axes labels

--- docs/data/charts/radar/DemoRadarSeriesHighlight.tsx
+++ docs/data/charts/radar/DemoRadarSeriesHighlight.tsx
@@ -78,5 +78,5 @@ const series = [
   },
 ];
 const radar = {
-  metrics: ['Oil', 'Coal', 'Gas', 'Flaring', 'Other industry', 'Cement'],
+  metrics: ['Oil', 'Coal', 'Gas', 'Flaring', 'Other\nindustry', 'Cement'],
 };
