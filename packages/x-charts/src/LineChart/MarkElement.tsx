19185790be52d605f621b90df8829b6f11f06f1a
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-03-14 10:45:54 +0100

    Gen stuff

--- packages/x-charts/src/LineChart/MarkElement.tsx
+++ packages/x-charts/src/LineChart/MarkElement.tsx
@@ -121,9 +121,8 @@ MarkElement.propTypes = {
     .isRequired,
   /**
    * If `true`, animations are skipped.
-   * @default false
    */
-  skipAnimation: PropTypes.bool,
+  skipAnimation: PropTypes.bool.isRequired,
 } as any;
 
 export { MarkElement };
