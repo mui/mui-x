c2d7329f62c2a210a55ce42765415323b9e822c1
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-03-14 18:08:27 +0100

    Attempt at animation hook

--- packages/x-charts/src/LineChart/AnimatedLine.tsx
+++ packages/x-charts/src/LineChart/AnimatedLine.tsx
@@ -19,45 +19,125 @@ export interface AnimatedLineProps extends React.ComponentPropsWithoutRef<'path'
   skipAnimation?: boolean;
 }
 
+const TRANSITION_NAME = 'MuiAnimatedLine-transition';
 const DURATION = 200;
-function useAnimatePath(props: Pick<AnimatedLineProps, 'd' | 'skipAnimation'>) {
-  const lastDRef = React.useRef(props.d);
+function useAnimatePath(props: Pick<AnimatedLineProps, 'd'>, { skip }: { skip?: boolean }) {
+  const lastInterpolatedDRef = React.useRef(props.d);
   const transitionRef = React.useRef<Transition<SVGPathElement, unknown, null, undefined>>(null);
-  const [path, setPath] = React.useState<SVGPathElement | null>(null);
+
+  /*
+   * We need to interrupt the animation when:
+   * - the element is removed from the DOM
+   * - the `skipAnimation` prop becomes true
+   * - the `d` prop changes and `skipAnimation` is not true and there is an element
+   *
+   * We start a new animation when:
+   * - a new element is added to the DOM
+   * - the `skipAnimation` prop becomes false
+   * - the `d` prop changes and `skipAnimation` is not true and there is an element
+   *   */
+
+  const elementRef = React.useRef<SVGPathElement>(null);
+  const elementUnmounted = React.useRef(false);
+
+  /* Stop the transition if the component that calls this hook is unmounted. */
   React.useLayoutEffect(() => {
-    /* If we're not skipping animation, we need to set the attribute to override React's changes.
-     * Still need to figure out if this is better than asking the user not to pass the `d` prop to the component.
-     * The problem with that is that SSR might not look good. */
-    if (!props.skipAnimation) {
-      path?.setAttribute('d', lastDRef.current);
+    return () => {
+      const lastElement = elementRef.current;
+
+      interrupt(lastElement, TRANSITION_NAME);
+    };
+  }, []);
+
+  /* Handles `skip` changing.
+   * If it happens while the element is transitioning, it should jump to the finished state of the transition
+   * immediately. */
+  React.useLayoutEffect(() => {
+    const transition = transitionRef.current;
+    const element = elementRef.current;
+
+    if (element && transition) {
+      if (skip) {
+        interrupt(element, TRANSITION_NAME);
+      } else {
+        // TODO: start animation
+      }
     }
-  }, [path, props.skipAnimation]);
+  }, [skip]);
+
+  /* `elementUnmounted` is when to true when a `setRef` is called with a non-null element. This is needed because
+   * `setRef` can be called because the component using this hook re-renders, i.e., it isn't guaranteed to be called only
+   *  when the underlying element changes.
+   *  When `elementUnmounted` is true, it means `setRef` wasn't called with an element, so we must interrupt the
+   *  transition.
+   *
+   * This runs on every render, so it must be light. */
+  React.useLayoutEffect(() => {
+    if (elementUnmounted.current) {
+      const lastElement = elementRef.current;
+
+      interrupt(lastElement, TRANSITION_NAME);
+    }
+  });
+
+  const animate = React.useCallback(
+    (element: SVGPathElement) => {
+      const lastInterpolatedD = lastInterpolatedDRef.current;
+      const stringInterpolator = interpolateString(lastInterpolatedD, props.d);
+
+      transitionRef.current = select(element)
+        .transition(TRANSITION_NAME)
+        .duration(DURATION)
+        .ease(easeLinear)
+        .attrTween('d', () => (t) => {
+          const interpolatedD = stringInterpolator(t);
+
+          lastInterpolatedDRef.current = interpolatedD;
+
+          return interpolatedD;
+        });
+    },
+    [props.d],
+  );
 
   React.useLayoutEffect(() => {
-    // TODO: What if we set skipAnimation to true in the middle of the animation?
-    if (path === null || props.skipAnimation) {
-      return undefined;
+    const element = elementRef.current;
+
+    if (element && !skip) {
+      interrupt(element, TRANSITION_NAME);
+      // Also, start the animation
+      animate(element);
     }
+  }, [animate, skip]);
 
-    const lastD = lastDRef.current;
-    const stringInterpolator = interpolateString(lastD, props.d);
+  const setRef = React.useCallback(
+    (element: SVGPathElement | null) => {
+      elementUnmounted.current = element === null;
 
-    transitionRef.current = select(path)
-      .transition()
-      .duration(DURATION)
-      .ease(easeLinear)
-      .attrTween('d', () => (t) => {
-        const interpolatedD = stringInterpolator(t);
+      if (element === null) {
+        return;
+      }
 
-        lastDRef.current = interpolatedD;
+      // If it's the same element, there's nothing to do.
+      if (elementRef.current === element) {
+        return;
+      }
 
-        return interpolatedD;
-      });
+      if (!skip) {
+        /* If we're not skipping animation, we need to set the attribute to override React's changes.
+         * Still need to figure out if this is better than asking the user not to pass the `d` prop to the component.
+         * The problem with that is that SSR might not look good. */
+        element.setAttribute('d', lastInterpolatedDRef.current);
+        // Also, start the animation
+        animate(element);
+      }
 
-    return () => interrupt(path);
-  }, [path, props.d, props.skipAnimation]);
+      elementRef.current = element;
+    },
+    [animate, skip],
+  );
 
-  return setPath;
+  return setRef;
 }
 
 /**
@@ -74,7 +154,7 @@ const AnimatedLine = React.forwardRef<SVGPathElement, AnimatedLineProps>(
   function AnimatedLine(props, ref) {
     const { d, skipAnimation, ownerState, ...other } = props;
 
-    const animateRef = useAnimatePath(props);
+    const animateRef = useAnimatePath(props, { skip: props.skipAnimation });
     const forkRef = useForkRef(ref, animateRef);
 
     return (
