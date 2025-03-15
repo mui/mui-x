c912a720fe9bbf685c4b94e2ea915feb7cae2506
Armin Mehinovic <armin@mui.com>
2025-03-15 16:15:54 +0100

    Small adjustments

--- docs/data/pages.ts
+++ docs/data/pages.ts
@@ -164,6 +164,7 @@ const pages: MuiPage[] = [
               },
             ],
           },
+          { pathname: '/x/react-data-grid/prompt', plan: 'premium', unstable: true },
         ],
       },
       {
