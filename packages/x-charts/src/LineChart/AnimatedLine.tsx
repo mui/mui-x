f39848a7d7ab60a4fedd6fb15cb1a41031256921
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-03-17 16:51:34 +0100

    Attempt to create generic function

--- packages/x-charts/src/LineChart/AnimatedLine.tsx
+++ packages/x-charts/src/LineChart/AnimatedLine.tsx
@@ -5,6 +5,7 @@ import { interpolateString } from '@mui/x-charts-vendor/d3-interpolate';
 import { select } from '@mui/x-charts-vendor/d3-selection';
 import { interrupt, Transition } from '@mui/x-charts-vendor/d3-transition';
 import useForkRef from '@mui/utils/useForkRef';
+import useId from '@mui/utils/useId';
 import { ANIMATION_DURATION_MS, ANIMATION_TIMING_FUNCTION_JS } from '../constants';
 import { AppearingMask } from './AppearingMask';
 import type { LineElementOwnerState } from './LineElement';
@@ -19,16 +20,34 @@ export interface AnimatedLineProps extends React.ComponentPropsWithoutRef<'path'
   skipAnimation?: boolean;
 }
 
-const TRANSITION_NAME = 'MuiAnimatedLine-transition';
-function useAnimatePath(props: Pick<AnimatedLineProps, 'd'>, { skip }: { skip?: boolean }) {
+function useAnimate<Props extends {}, Elem extends Element>(
+  props: Props,
+  {
+    createInterpolator,
+    applyProps,
+    skip,
+  }: {
+    createInterpolator: (lastProps: Props, newProps: Props) => (t: number) => Props;
+    applyProps: (element: Elem, props: Props) => void;
+    skip?: boolean;
+  },
+) {
+  const transitionName = `MuiUseAnimate-${useId()}`;
   /* A transition is interrupted when:
    * T1. The transitioned element changes;
    * T2. The transitioned element is unmounted;
    * T3. The component calling this hook is unmounted. */
-  const lastInterpolatedDRef = React.useRef(props.d);
-  const transitionRef = React.useRef<Transition<SVGPathElement, unknown, null, undefined>>(null);
-  const elementRef = React.useRef<SVGPathElement>(null);
+  const lastInterpolatedProps = React.useRef(props);
+  const transitionRef = React.useRef<Transition<Elem, unknown, null, undefined>>(null);
+  const elementRef = React.useRef<Elem>(null);
   const elementUnmounted = React.useRef(false);
+  const createInterpolatorRef = React.useRef(createInterpolator);
+  const applyPropsRef = React.useRef(applyProps);
+
+  React.useLayoutEffect(() => {
+    createInterpolatorRef.current = createInterpolator;
+    applyPropsRef.current = applyProps;
+  });
 
   React.useLayoutEffect(() => {
     return () => {
@@ -36,10 +55,10 @@ function useAnimatePath(props: Pick<AnimatedLineProps, 'd'>, { skip }: { skip?: 
       const lastElement = elementRef.current;
 
       if (lastElement) {
-        interrupt(lastElement, TRANSITION_NAME);
+        interrupt(lastElement, transitionName);
       }
     };
-  }, []);
+  }, [transitionName]);
 
   /* T2. Interrupt the transition if the element is unmounted.
    *
@@ -54,28 +73,30 @@ function useAnimatePath(props: Pick<AnimatedLineProps, 'd'>, { skip }: { skip?: 
     if (elementUnmounted.current) {
       const lastElement = elementRef.current;
 
-      interrupt(lastElement, TRANSITION_NAME);
+      interrupt(lastElement, transitionName);
     }
   });
 
   const animate = React.useCallback(
-    (element: SVGPathElement) => {
-      const lastInterpolatedD = lastInterpolatedDRef.current;
-      const stringInterpolator = interpolateString(lastInterpolatedD, props.d);
-
+    (element: Elem) => {
       transitionRef.current = select(element)
-        .transition(TRANSITION_NAME)
+        .transition(transitionName)
         .duration(skip ? 0 : ANIMATION_DURATION_MS)
         .ease(ANIMATION_TIMING_FUNCTION_JS)
-        .attrTween('d', () => (t) => {
-          const interpolatedD = stringInterpolator(t);
+        .tween('animate', () => {
+          const lastProps = lastInterpolatedProps.current;
+          const interpolate = createInterpolatorRef.current(lastProps, props);
 
-          lastInterpolatedDRef.current = interpolatedD;
+          return function animateAt(t) {
+            const interpolatedProps = interpolate(t);
 
-          return interpolatedD;
+            lastInterpolatedProps.current = interpolatedProps;
+
+            applyPropsRef.current(this, interpolatedProps);
+          };
         });
     },
-    [props.d, skip],
+    [props, skip, transitionName],
   );
 
   React.useLayoutEffect(() => {
@@ -87,7 +108,7 @@ function useAnimatePath(props: Pick<AnimatedLineProps, 'd'>, { skip }: { skip?: 
   }, [animate]);
 
   const setRef = React.useCallback(
-    (element: SVGPathElement | null) => {
+    (element: Elem | null) => {
       elementUnmounted.current = element === null;
 
       if (element === null) {
@@ -103,17 +124,25 @@ function useAnimatePath(props: Pick<AnimatedLineProps, 'd'>, { skip }: { skip?: 
 
       // T1. If it's a different element, interrupt the transition of the last element.
       if (lastElement) {
-        interrupt(lastElement, TRANSITION_NAME);
+        interrupt(lastElement, transitionName);
       }
 
       animate(element);
 
       elementRef.current = element;
     },
-    [animate],
+    [animate, transitionName],
   );
 
   return setRef;
+}
+
+function useAnimatePath(props: Pick<AnimatedLineProps, 'd'>, { skip }: { skip?: boolean }) {
+  return useAnimate(props.d, {
+    createInterpolator: (lastD, newD) => interpolateString(lastD, newD),
+    applyProps: (element: SVGPathElement, d) => element.setAttribute('d', d),
+    skip,
+  });
 }
 
 /**
