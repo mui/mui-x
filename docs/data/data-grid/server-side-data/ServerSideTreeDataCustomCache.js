8060024b5480eb907672797e8deb913202fe4fd4
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2025-03-17 15:01:30 +0000

    [docs] Remove `showQuickFilter: true` toolbar prop from demos

--- docs/data/data-grid/server-side-data/ServerSideTreeDataCustomCache.js
+++ docs/data/data-grid/server-side-data/ServerSideTreeDataCustomCache.js
@@ -97,7 +97,6 @@ export default function ServerSideTreeDataCustomCache() {
           pageSizeOptions={pageSizeOptions}
           initialState={initialState}
           showToolbar
-          slotProps={{ toolbar: { showQuickFilter: true } }}
         />
       </div>
     </div>
