3996fa97478d3d5d4af1013828a81ad673437d82
Jose Quintas <juniorquintas@gmail.com>
2025-03-17 15:57:19 +0100

    Revert "tree shake"
    This reverts commit c89dabdf8baa86e7cdba7843ef1f86887976557f.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,12 @@ module.exports = function setKarmaConfig(config) {
         served: true,
         included: true,
       },
+      {
+        pattern: 'test/karma.datagrid.tests.js',
+        watched: true,
+        served: true,
+        included: true,
+      },
     ],
     plugins: (process.env.PARALLEL === 'true' ? ['karma-parallel'] : []).concat([
       'karma-mocha',
