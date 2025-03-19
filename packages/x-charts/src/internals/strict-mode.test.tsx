a1effe5dbffdb72b83e043eb1e3f550c8e4bcd8b
Bernardo Belchior <bernardo@mui.com>
2025-03-19 16:09:13 +0100

    Remove ref callback

--- packages/x-charts/src/internals/strict-mode.test.tsx
+++ packages/x-charts/src/internals/strict-mode.test.tsx
@@ -2,15 +2,12 @@ import { configure, createRenderer } from '@mui/internal-test-utils';
 import { expect } from 'chai';
 import * as React from 'react';
 
-const reactMajor = parseInt(React.version, 10);
-
 describe('Strict Mode', () => {
   const { render } = createRenderer({ strict: true, strictEffects: true });
 
   it('does not work when `configure` is not called, even if `createRenderer` was created with `strict: true` and `strictEffects: true`', async () => {
     let mounted = 0;
     let unmounted = 0;
-    let refCallback = 0;
 
     function TestComponent() {
       React.useEffect(() => {
@@ -21,22 +18,13 @@ describe('Strict Mode', () => {
         };
       }, []);
 
-      return (
-        <div
-          ref={() => {
-            refCallback += 1;
-          }}
-        />
-      );
+      return <div />;
     }
 
     render(<TestComponent />, { strict: true, strictEffects: true });
 
     expect(mounted).to.equal(2);
     expect(unmounted).to.equal(1);
-
-    // Double ref callback call were introduced in React 19: https://react.dev/blog/2024/04/25/react-19-upgrade-guide#strict-mode-improvements
-    expect(refCallback).to.equal(reactMajor >= 19 ? 3 : 1);
   });
 
   it('works when `configure({ reactStrictMode: true })` is called', async () => {
@@ -44,7 +32,6 @@ describe('Strict Mode', () => {
 
     let mounted = 0;
     let unmounted = 0;
-    let refCallback = 0;
 
     function TestComponent() {
       React.useEffect(() => {
@@ -55,21 +42,12 @@ describe('Strict Mode', () => {
         };
       }, []);
 
-      return (
-        <div
-          ref={() => {
-            refCallback += 1;
-          }}
-        />
-      );
+      return <div />;
     }
 
     render(<TestComponent />, { strict: true, strictEffects: true });
 
     expect(mounted).to.equal(2);
     expect(unmounted).to.equal(1);
-
-    // Double ref callback call were introduced in React 19: https://react.dev/blog/2024/04/25/react-19-upgrade-guide#strict-mode-improvements
-    expect(refCallback).to.equal(reactMajor >= 19 ? 3 : 1);
   });
 });
