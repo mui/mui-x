18af239e367e61a2451f7aa3963f3a5784cfffc2
Bernardo Belchior <bernardo@mui.com>
2025-03-20 15:59:04 +0100

    Remove d3-transition

--- packages/x-charts-vendor/lib-vendor/d3-transition/src/selection/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _index = require("../../../../lib-vendor/d3-selection/src/index.js");
-var _interrupt = _interopRequireDefault(require("./interrupt.js"));
-var _transition = _interopRequireDefault(require("./transition.js"));
-_index.selection.prototype.interrupt = _interrupt.default;
-_index.selection.prototype.transition = _transition.default;
