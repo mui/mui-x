b099dc7f7408b19d7f7f1919357ec2687fd123b6
Rom Grk <romgrk.cc@gmail.com>
2025-03-21 16:09:24 -0400

    build(e2e): tsx

--- test/e2e/.mocharc.js
+++ test/e2e/.mocharc.js
@@ -1,8 +1,8 @@
 module.exports = {
   extension: ['js', 'ts', 'tsx'],
   recursive: true,
-  slow: 500,
+  require: 'tsx',
   timeout: (process.env.CIRCLECI === 'true' ? 5 : 2) * 1000, // Circle CI has low-performance CPUs.
+  slow: 500,
   reporter: 'dot',
-  require: [require.resolve('../utils/setupBabel')],
 };
