c1597ec3d20b59aad4ddd945a834631b1ca60a12
Bernardo Belchior <bernardo@mui.com>
2025-03-21 10:13:10 +0100

    Fix types

--- packages/x-charts/src/LineChart/AnimatedLine.tsx
+++ packages/x-charts/src/LineChart/AnimatedLine.tsx
@@ -24,7 +24,10 @@ type LineAnimatedProps = Pick<AnimatedLineProps, 'd' | 'skipAnimation'> & {
 /** Animates a line of a line chart using a `path` element.
  * The props object also accepts a `ref` which will be merged with the ref returned from this hook. This means you can
  * pass the ref returned by this hook to the `path` element and the `ref` provided as argument will also be called. */
-export function useAnimateLine(props: LineAnimatedProps) {
+export function useAnimateLine(props: LineAnimatedProps): {
+  ref: React.Ref<SVGPathElement>;
+  d: string;
+} {
   const ref = useAnimate(
     { d: props.d },
     {
