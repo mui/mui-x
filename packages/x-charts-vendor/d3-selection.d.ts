3eed220c2f0d363b267839af08ec01792d9dfc26
Bernardo Belchior <bernardo@mui.com>
2025-03-21 10:42:10 +0100

    Remove unnecessary dependencies

--- packages/x-charts-vendor/d3-selection.d.ts
+++
@@ -1,5 +0,0 @@
-
-// `x-charts-vendor/d3-selection` (TypeScript)
-//
-// Export the type definitions for this package:
-export * from "d3-selection";
--- packages/x-charts-vendor/es/d3-selection.mjs
+++
@@ -1,6 +0,0 @@
-
-// `x-charts-vendor/d3-selection` (ESM)
-// See upstream license: https://github.com/d3/d3-selection/blob/main/LICENSE
-//
-// Our ESM package uses the underlying installed dependencies of `node_modules/d3-selection`
-export * from "d3-selection";
