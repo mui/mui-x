efa9c015414f3396f6a945c64a51bf5e8a370a3e
Jose C Quintas Jr <juniorquintas@gmail.com>
2025-03-25 09:39:54 +0100

    [charts] Add pie benchmark (#17115)

---
+++ test/performance-charts/tests/PieChart.bench.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+// eslint-disable-next-line no-restricted-imports
+import { render, cleanup } from '@testing-library/react';
+import { bench, describe } from 'vitest';
+import { PieChart } from '@mui/x-charts/PieChart';
+import { options } from '../utils/options';
+
+describe('PieChart', () => {
+  const dataLength = 50;
+  const data = Array.from({ length: dataLength + 1 }).map((_, i) => ({
+    value: 50 + Math.sin(i / 5) * 1000,
+  }));
+
+  bench(
+    'PieChart with big data amount',
+    async () => {
+      const { findByText } = render(
+        <PieChart
+          series={[
+            {
+              data,
+              arcLabel: (v) => v.value.toFixed(0),
+            },
+          ]}
+          width={500}
+          height={300}
+        />,
+      );
+
+      const result = 1050;
+      await findByText(result.toLocaleString());
+
+      cleanup();
+    },
+    options,
+  );
+});
