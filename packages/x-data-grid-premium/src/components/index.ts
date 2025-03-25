260b735f4f1bbeacac101be8bfb34b5d75eeb9ca
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2025-03-25 15:44:32 +0000

    compasble prompt field

--- packages/x-data-grid-premium/src/components/index.ts
+++ packages/x-data-grid-premium/src/components/index.ts
@@ -2,6 +2,7 @@ export * from './GridExcelExportMenuItem';
 export * from '../material/icons';
 export * from './GridColumnMenuAggregationItem';
 export * from './promptControl';
+export * from './promptField';
 export { GridColumnMenuGroupingItem } from './GridPremiumColumnMenu';
 export * from './export';
 export * from './GridEmptyPivotOverlay';
