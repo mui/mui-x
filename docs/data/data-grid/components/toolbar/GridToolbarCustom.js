7e90a84014985168e1543ae9ac02f83de5831d21
Lukas <llukas.tyla@gmail.com>
2025-03-25 17:55:56 +0200

    docs:typescript:formatted

--- docs/data/data-grid/components/toolbar/GridToolbarCustom.js
+++ docs/data/data-grid/components/toolbar/GridToolbarCustom.js
@@ -17,6 +17,7 @@ import { useDemoData } from '@mui/x-data-grid-generator';
 import { TailwindDemoContainer } from '@mui/x-data-grid/internals';
 import SearchIcon from '@mui/icons-material/Search';
 import CancelIcon from '@mui/icons-material/Cancel';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Button(props) {
   return (
@@ -105,6 +106,7 @@ function CustomToolbar() {
     </Toolbar>
   );
 }
+const theme = createTheme({ colorSchemes: { light: true, dark: true } });
 
 export default function GridToolbarCustom({ window }) {
   const { data, loading } = useDemoData({
@@ -118,14 +120,16 @@ export default function GridToolbarCustom({ window }) {
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <TailwindDemoContainer documentBody={documentBody}>
-        <DataGrid
-          {...data}
-          loading={loading}
-          slots={{ toolbar: CustomToolbar }}
-          showToolbar
-        />
-      </TailwindDemoContainer>
+      <ThemeProvider theme={theme}>
+        <TailwindDemoContainer documentBody={documentBody}>
+          <DataGrid
+            {...data}
+            loading={loading}
+            slots={{ toolbar: CustomToolbar }}
+            showToolbar
+          />
+        </TailwindDemoContainer>
+      </ThemeProvider>
     </div>
   );
 }
