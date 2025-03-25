6e5681777c5d684945b0a82931c54077e393ae35
Bernardo Belchior <bernardo@mui.com>
2025-03-25 15:10:38 +0100

    Remove react spring from area chart.

--- packages/x-charts/src/BarChart/BarLabel/BarLabelPlot.tsx
+++ packages/x-charts/src/BarChart/BarLabel/BarLabelPlot.tsx
@@ -1,30 +1,6 @@
 import * as React from 'react';
-import { useTransition } from '@react-spring/web';
-import type { AnimationData, CompletedBarData } from '../types';
+import type { CompletedBarData } from '../types';
 import { BarLabelItem, BarLabelItemProps } from './BarLabelItem';
-
-const leaveStyle = ({ layout, yOrigin, x, width, y, xOrigin, height }: AnimationData) => ({
-  ...(layout === 'vertical'
-    ? {
-        y: yOrigin,
-        x: x + width / 2,
-        height: 0,
-        width,
-      }
-    : {
-        y: y + height / 2,
-        x: xOrigin,
-        height,
-        width: 0,
-      }),
-});
-
-const enterStyle = ({ x, width, y, height }: AnimationData) => ({
-  x: x + width / 2,
-  y: y + height / 2,
-  height,
-  width,
-});
 
 type BarLabelPlotProps = {
   bars: CompletedBarData[];
