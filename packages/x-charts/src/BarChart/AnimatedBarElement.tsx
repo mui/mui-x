4abf30c7cc75e1f53550f82a438aca6724c6c015
Bernardo Belchior <bernardo@mui.com>
2025-03-26 13:14:55 +0100

    Fix exports and types.

--- packages/x-charts/src/BarChart/AnimatedBarElement.tsx
+++ packages/x-charts/src/BarChart/AnimatedBarElement.tsx
@@ -1,9 +1,17 @@
 'use client';
 import * as React from 'react';
+import { SeriesId } from '../models/seriesType/common';
 import { BarElementOwnerState } from './barElementClasses';
 import { useAnimateBar } from '../hooks/animation/useAnimateBar';
 
-export interface BarProps extends Omit<React.SVGProps<SVGRectElement>, 'id' | 'color' | 'ref'> {
+export interface BarProps
+  extends Omit<
+    React.SVGProps<SVGRectElement>,
+    'id' | 'color' | 'ref' | 'x' | 'y' | 'height' | 'width'
+  > {
+  id: SeriesId;
+  dataIndex: number;
+  color: string;
   ownerState: BarElementOwnerState;
   x: number;
   y: number;
@@ -14,7 +22,7 @@ export interface BarProps extends Omit<React.SVGProps<SVGRectElement>, 'id' | 'c
 }
 
 export function AnimatedBarElement(props: BarProps) {
-  const { ownerState, skipAnimation, ...other } = props;
+  const { ownerState, skipAnimation, id, dataIndex, ...other } = props;
 
   const animatedProps = useAnimateBar(props);
 
