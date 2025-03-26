c646fd9004b1b7b1e8c4dbd5e9b0e88b7226b444
alex <alex.fauquette@gmail.com>
2025-03-26 12:05:25 +0100

    update

--- packages/x-charts/src/internals/plugins/featurePlugins/useChartPolarAxis/getAxisValue.ts
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartPolarAxis/getAxisValue.ts
@@ -4,7 +4,7 @@ import { clampAngleRad } from '../../../clampAngle';
 
 /**
  * For a pointer coordinate, this function returns the value and dataIndex associated.
- * Returns `null` if the coordinate is outside of values.
+ * Returns `-1` if the coordinate does not match a value.
  */
 export function getAxisIndex(axisConfig: PolarAxisDefaultized, pointerValue: number): number {
   const { scale, data: axisData, reverse } = axisConfig;
@@ -13,13 +13,17 @@ export function getAxisIndex(axisConfig: PolarAxisDefaultized, pointerValue: num
     throw new Error('MUI X: getAxisValue is not implemented for polare continuous axes.');
   }
 
+  if (!axisData) {
+    return -1;
+  }
+
   const angleGap = clampAngleRad(pointerValue - Math.min(...scale.range()));
   const dataIndex =
     scale.bandwidth() === 0
       ? Math.floor((angleGap + scale.step() / 2) / scale.step()) % axisData!.length
       : Math.floor(angleGap / scale.step());
 
-  if (dataIndex < 0 || dataIndex >= axisData!.length) {
+  if (dataIndex < 0 || dataIndex >= axisData.length) {
     return -1;
   }
 
