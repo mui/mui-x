93a4f625a56fd14e571f049cc270d4f3fe604a57
Flavien DELANGLE <flaviendelangle@gmail.com>
2025-03-28 10:27:29 +0100

    Update packages/x-date-pickers/src/internals/hooks/useStaticPicker/useStaticPicker.types.ts
    Co-authored-by: Lukas Tyla <llukas.tyla@gmail.com>
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>

--- packages/x-date-pickers/src/internals/hooks/useStaticPicker/useStaticPicker.types.ts
+++ packages/x-date-pickers/src/internals/hooks/useStaticPicker/useStaticPicker.types.ts
@@ -58,7 +58,7 @@ export interface UseStaticPickerParams<
   props: TExternalProps;
   /**
    * Steps available for the picker.
-   * This will be used to generate the "previous" and "next" actions.
+   * This will be used to define the behavior of navigation actions.
    * If null, the picker will not have any step navigation.
    */
   steps: PickerStep[] | null;
