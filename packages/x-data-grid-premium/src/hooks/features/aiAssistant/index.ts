b8e7af3536cb074dfefa7fd58f8c1fbd09d1e130
Armin Mehinovic <armin@mui.com>
2025-03-31 14:59:36 +0200

    fix exports

--- packages/x-data-grid-premium/src/hooks/features/aiAssistant/index.ts
+++ packages/x-data-grid-premium/src/hooks/features/aiAssistant/index.ts
@@ -1,3 +1,2 @@
 export { gridDefaultPromptResolver as unstable_gridDefaultPromptResolver } from './api';
-export type { PromptResponse as unstable_PromptResponse } from './gridAiAssistantInterfaces';
-export { useGridAiAssistant } from './useGridAiAssistant';
+export type { PromptResponse, PromptHistory } from './gridAiAssistantInterfaces';
