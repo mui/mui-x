ef49694d30a6ae63be372b1d8dc9b74d1469d3be
Jose Quintas <juniorquintas@gmail.com>
2025-03-31 10:59:08 +0200

    useeffect

--- packages/x-charts-pro/src/internals/plugins/useChartProZoom/useChartProZoom.ts
+++ packages/x-charts-pro/src/internals/plugins/useChartProZoom/useChartProZoom.ts
@@ -1,6 +1,5 @@
 'use client';
 import * as React from 'react';
-import useEnhancedEffect from '@mui/utils/useEnhancedEffect';
 import {
   ChartPlugin,
   AxisId,
@@ -47,8 +46,7 @@ export const useChartProZoom: ChartPlugin<UseChartProZoomSignature> = ({
   const isZoomEnabled = Object.keys(optionsLookup).length > 0;
 
   // Manage controlled state
-
-  useEnhancedEffect(() => {
+  React.useEffect(() => {
     if (paramsZoomData === undefined) {
       return undefined;
     }
