17afdbc7628c5e8dc16e6b810972aa4d83a2472e
flavien <flaviendelangle@gmail.com>
2025-04-01 08:12:23 +0200

    Fix test

--- test/utils/pickers/describeValue/testPickerActionBar.tsx
+++ test/utils/pickers/describeValue/testPickerActionBar.tsx
@@ -156,7 +156,7 @@ export const testPickerActionBar: DescribeValueTestSuite<any, 'picker'> = (
           onClose,
           open: true,
           defaultValue: values[0],
-          slotProps: { actionBar: { actions: ['nextOrAccept'] } },
+          slotProps: { actionBar: { actions: ['accept', 'nextOrAccept'] } },
           closeOnSelect: false,
         });
 
@@ -164,7 +164,7 @@ export const testPickerActionBar: DescribeValueTestSuite<any, 'picker'> = (
         setNewValue(values[0], { isOpened: true, selectSection, pressKey });
 
         // Accept the modifications
-        fireEvent.click(screen.getByText(/ok/i));
+        fireEvent.click(screen.getAllByRole('button', { name: 'OK' })[0]);
         expect(onChange.callCount).to.equal(
           getExpectedOnChangeCount(componentFamily, pickerParams),
         ); // The accepted value as already been committed, don't call onChange again
