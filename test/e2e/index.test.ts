ae2e125e50059610b7891fd39dd937dbde4c4b5f
Rom Grk <romgrk.cc@gmail.com>
2025-04-03 05:21:47 -0400

    build: fix e2e timeout

--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -109,7 +109,7 @@ async function initializeEnvironment(
     ...contextOptions,
   });
   // Circle CI has low-performance CPUs.
-  context.setDefaultTimeout((process.env.CIRCLECI === 'true' ? 4 : 2) * 1000);
+  context.setDefaultTimeout((process.env.CIRCLECI === 'true' ? 8 : 4) * 1000);
   page = await context.newPage();
   // taken from: https://github.com/microsoft/playwright/issues/6347#issuecomment-1085850728
   // Update the Date accordingly in your test pages
