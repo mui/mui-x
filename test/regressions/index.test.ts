b80973a27ecd75170fd370ebbb941b6c01ac2aac
Lukas <llukas.tyla@gmail.com>
2025-04-03 12:45:52 +0300

    Align `Date` mocking approach with regression tests

--- test/regressions/index.test.ts
+++ test/regressions/index.test.ts
@@ -62,6 +62,9 @@ const isConsoleWarningIgnored = (msg?: string) => {
   return false;
 };
 
+// Pick the new/fake "now" for you test pages.
+const fakeNow = new Date('Mon Aug 18 14:11:54 2014 -0500').valueOf();
+
 async function main() {
   const baseUrl = 'http://localhost:5001';
   const screenshotDir = path.resolve(__dirname, './screenshots/chrome');
@@ -77,6 +80,25 @@ async function main() {
   // reuse viewport from `vrtest`
   // https://github.com/nathanmarks/vrtest/blob/1185b852a6c1813cedf5d81f6d6843d9a241c1ce/src/server/runner.js#L44
   const page = await browser.newPage({ viewport: { width: 1000, height: 700 } });
+
+  // taken from: https://github.com/microsoft/playwright/issues/6347#issuecomment-1085850728
+  // Update the Date accordingly in your test pages
+  await page.addInitScript(`{
+    // Extend Date constructor to default to fakeNow
+    Date = class extends Date {
+      constructor(...args) {
+        if (args.length === 0) {
+          super(${fakeNow});
+        } else {
+          super(...args);
+        }
+      }
+    }
+    // Override Date.now() to start from fakeNow
+    const __DateNowOffset = ${fakeNow} - Date.now();
+    const __DateNow = Date.now;
+    Date.now = () => __DateNow() + __DateNowOffset;
+  }`);
 
   // Block images since they slow down tests (need download).
   // They're also most likely decorative for documentation demos
