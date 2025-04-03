485333ccdf5541d1016b89d4c86310ce9c1d623c
noraleonte <noraleonte00@gmail.com>
2025-04-03 15:10:01 +0300

    update label demo

--- docs/src/modules/components/overview/tree-view/advancedFeatures/LabelEditingDemo.tsx
+++ docs/src/modules/components/overview/tree-view/advancedFeatures/LabelEditingDemo.tsx
@@ -3,16 +3,45 @@ import { HighlightedCode } from '@mui/docs/HighlightedCode';
 import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
 import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
+import { TreeItem, TreeItemContent, TreeItemProps } from '@mui/x-tree-view/TreeItem';
+import { UseTreeItemContentSlotOwnProps } from '@mui/x-tree-view/useTreeItem';
 import { MUI_X_PRODUCTS } from './products';
 import DemoWrapper from '../../DemoWrapper';
+
+function CustomContent(props: UseTreeItemContentSlotOwnProps & { itemId: string }) {
+  if (props?.itemId === 'grid-community' && !props.status?.editing) {
+    return (
+      <Tooltip placement="left" open arrow title="Double click to edit">
+        <TreeItemContent {...props} />
+      </Tooltip>
+    );
+  }
+  return <TreeItemContent {...props} />;
+}
+
+const CustomTreeItem = React.forwardRef(function CustomTreeItem(
+  props: TreeItemProps,
+  ref: React.Ref<HTMLLIElement>,
+) {
+  return (
+    <TreeItem
+      {...props}
+      ref={ref}
+      slots={{ content: CustomContent }}
+      slotProps={{
+        content: {
+          itemId: props.itemId,
+        } as any,
+      }}
+    />
+  );
+});
 
 export default function LabelEditingDemo() {
   const brandingTheme = useTheme();
   const theme = createTheme({ palette: { mode: brandingTheme.palette.mode } });
-  const [showIndicator, setShowIndicator] = React.useState(true);
 
   return (
     <DemoWrapper link="/x/react-tree-view/rich-tree-view/editing/">
@@ -20,9 +49,6 @@ export default function LabelEditingDemo() {
         spacing={1}
         sx={{ width: '100%', padding: 2, minHeight: '600px' }}
         justifyContent="space-between"
-        onMouseEnter={() => {
-          setShowIndicator(false);
-        }}
       >
         <Box
           sx={{
@@ -40,19 +66,9 @@ export default function LabelEditingDemo() {
               isItemEditable
               defaultExpandedItems={['grid', 'pickers']}
               defaultSelectedItems={'grid-community'}
+              slots={{ item: CustomTreeItem }}
             />
           </ThemeProvider>
-          {showIndicator && (
-            <Paper
-              variant="outlined"
-              sx={{ position: 'absolute', top: 70, left: -95, px: 2, py: 0.5 }}
-            >
-              {/* eslint-disable-next-line material-ui/no-hardcoded-labels */}
-              <Typography variant="caption" color="text.secondary" fontWeight={500}>
-                Double click me to edit
-              </Typography>
-            </Paper>
-          )}
         </Box>
 
         <HighlightedCode
