9e17002787a65f07375204d87ac23b1755787ff9
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2025-04-07 16:40:35 +0100

    update demo colors

--- docs/data/data-grid/pivoting/pivotHighlightStyles.js
+++ docs/data/data-grid/pivoting/pivotHighlightStyles.js
@@ -1,39 +1,51 @@
-const colors = {
-  rows: 'rgba(255, 200, 196, 0.5)',
-  columns: 'rgba(232, 201, 250, 0.5)',
-  values: 'rgba(208, 233, 255, 0.5)',
+const getColors = (colorScheme) => {
+  if (colorScheme === 'light') {
+    return {
+      rows: 'hsl(355, 98%, 97%)',
+      columns: 'hsl(144, 72%, 95%)',
+      values: 'hsl(210, 100%, 96%)',
+    };
+  }
+  return {
+    rows: 'hsl(355, 98%, 10%)',
+    columns: 'hsl(144, 72%, 9%)',
+    values: 'hsl(210, 100%, 11%)',
+  };
 };
 
-export const pivotHeightlightStyles = {
-  '& .MuiDataGrid-virtualScrollerContent .MuiDataGrid-cell[data-field="__row_group_by_columns_group__"]':
-    {
+export const pivotHeightlightStyles = (theme) => {
+  const colors = getColors(theme.palette.mode);
+  return {
+    '& .MuiDataGrid-virtualScrollerContent .MuiDataGrid-cell[data-field="__row_group_by_columns_group__"]':
+      {
+        backgroundColor: colors.rows,
+      },
+    '& .MuiDataGrid-columnHeader[data-field="__row_group_by_columns_group__"]': {
       backgroundColor: colors.rows,
     },
-  '& .MuiDataGrid-columnHeader[data-field="__row_group_by_columns_group__"]': {
-    backgroundColor: colors.rows,
-  },
-  '& .MuiDataGrid-pivotPanelSections .MuiDataGrid-pivotPanelSection:nth-of-type(1)':
-    {
-      backgroundColor: colors.rows,
-    },
-  '& .MuiDataGrid-pivotPanelSections .MuiDataGrid-pivotPanelSection:nth-of-type(2)':
-    {
-      backgroundColor: colors.columns,
-    },
-  '& .MuiDataGrid-columnHeader.MuiDataGrid-columnHeader--filledGroup:not([data-field="__row_group_by_columns_group__"]):not(.MuiDataGrid-columnHeader--emptyGroup)':
-    {
-      backgroundColor: colors.columns,
-    },
-  '& .MuiDataGrid-pivotPanelSections .MuiDataGrid-pivotPanelSection:nth-of-type(3)':
-    {
-      backgroundColor: colors.values,
-    },
-  '& .MuiDataGrid-columnHeader:not([data-field="__row_group_by_columns_group__"]):not(.MuiDataGrid-columnHeader--filledGroup):not(.MuiDataGrid-columnHeader--emptyGroup)':
-    {
-      backgroundColor: colors.values,
-    },
-  '& .MuiDataGrid-virtualScrollerContent .MuiDataGrid-cell:not([data-field="__row_group_by_columns_group__"]):not(.MuiDataGrid-cellEmpty)':
-    {
-      backgroundColor: colors.values,
-    },
+    '& .MuiDataGrid-pivotPanelSections .MuiDataGrid-pivotPanelSection:nth-of-type(1)':
+      {
+        backgroundColor: colors.rows,
+      },
+    '& .MuiDataGrid-pivotPanelSections .MuiDataGrid-pivotPanelSection:nth-of-type(2)':
+      {
+        backgroundColor: colors.columns,
+      },
+    '& .MuiDataGrid-columnHeader.MuiDataGrid-columnHeader--filledGroup:not([data-field="__row_group_by_columns_group__"]):not(.MuiDataGrid-columnHeader--emptyGroup)':
+      {
+        backgroundColor: colors.columns,
+      },
+    '& .MuiDataGrid-pivotPanelSections .MuiDataGrid-pivotPanelSection:nth-of-type(3)':
+      {
+        backgroundColor: colors.values,
+      },
+    '& .MuiDataGrid-columnHeader:not([data-field="__row_group_by_columns_group__"]):not(.MuiDataGrid-columnHeader--filledGroup):not(.MuiDataGrid-columnHeader--emptyGroup)':
+      {
+        backgroundColor: colors.values,
+      },
+    '& .MuiDataGrid-virtualScrollerContent .MuiDataGrid-cell:not([data-field="__row_group_by_columns_group__"]):not(.MuiDataGrid-cellEmpty)':
+      {
+        backgroundColor: colors.values,
+      },
+  };
 };
