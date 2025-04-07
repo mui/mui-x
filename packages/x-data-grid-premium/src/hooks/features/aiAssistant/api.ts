ee6702a3a29baca0634d750c541ced54f415437a
Armin Mehinovic <armin@mui.com>
2025-04-07 12:53:31 +0200

    small fixes

--- packages/x-data-grid-premium/src/hooks/features/aiAssistant/api.ts
+++ packages/x-data-grid-premium/src/hooks/features/aiAssistant/api.ts
@@ -8,6 +8,7 @@ type Result<T> = { ok: false; message: string } | { ok: true; data: T };
  * @param {string} query The query to be processed.
  * @param {string} context The prompt context containing necessary information about the current columns definition.
  * Either use the `context` parameter of the `onPrompt` callback or the return value of the `unstable_getPromptContext()` API method.
+ * @param {string} conversationId The id of the conversation the prompt is part of. If not passed, prompt response will return a new conversation id that can be used to continue the newly started conversation.
  * @param {string} additionalContext Optional, additional context to make the processing results more accurate.
  * @returns {Promise<PromptResponse>} The grid state updates to be applied.
  */
@@ -15,6 +16,7 @@ export function gridDefaultPromptResolver(
   url: string,
   query: string,
   context: string,
+  conversationId?: string,
   additionalContext = '',
 ) {
   return fetch(url, {
@@ -28,6 +30,7 @@ export function gridDefaultPromptResolver(
       context,
       query,
       additionalContext,
+      conversationId,
     }),
   })
     .then((result) => result.json())
