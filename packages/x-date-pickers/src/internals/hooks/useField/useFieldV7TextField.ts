8a38d47b1f7fdfc220964d9c8909e7da2e7caa4e
Lukas <llukas.tyla@gmail.com>
2025-04-08 09:16:06 +0300

    Another cleanup pass

--- packages/x-date-pickers/src/internals/hooks/useField/useFieldV7TextField.ts
+++ packages/x-date-pickers/src/internals/hooks/useField/useFieldV7TextField.ts
@@ -16,7 +16,7 @@ import { PickersSectionElement, PickersSectionListRef } from '../../../PickersSe
 import { useFieldCharacterEditing } from './useFieldCharacterEditing';
 import { useFieldState } from './useFieldState';
 import { useFieldInternalPropsWithDefaults } from './useFieldInternalPropsWithDefaults';
-import { FieldRangeSection, PickerValidValue } from '../../models';
+import { PickerValidValue } from '../../models';
 import { syncSelectionToDOM } from './syncSelectionToDOM';
 import { useFieldRootProps } from './useFieldRootProps';
 import { useFieldHiddenInputProps } from './useFieldHiddenInputProps';
@@ -195,7 +195,6 @@ export const useFieldV7TextField = <
       },
       after: {
         children: section.endSeparator,
-        'data-range-position': (section as FieldRangeSection).dateName,
       },
     }));
   }, [state.sections, createSectionContainerProps, createSectionContentProps]);
