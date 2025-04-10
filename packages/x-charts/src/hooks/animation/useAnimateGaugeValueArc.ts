2f384fa499e19a5e5c760d52b91c121150ce3e1f
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-04-10 09:55:32 +0100

    Update packages/x-charts/src/hooks/animation/useAnimateGaugeValueArc.ts
    Signed-off-by: Bernardo Belchior <Bernardo.belchior1@gmail.com>

--- packages/x-charts/src/hooks/animation/useAnimateGaugeValueArc.ts
+++ packages/x-charts/src/hooks/animation/useAnimateGaugeValueArc.ts
@@ -44,7 +44,7 @@ function gaugeValueArcPropsInterpolator(
   };
 }
 
-/** Animates a arc of a gauge chart by increasing the start and end angles from the middle angle to their final values.
+/** Animates a arc of a gauge chart by increasing the `endAngle` from the start angle to the end angle.
  * The props object also accepts a `ref` which will be merged with the ref returned from this hook. This means you can
  * pass the ref returned by this hook to the `path` element and the `ref` provided as argument will also be called. */
 export function useAnimateGaugeValueArc(
