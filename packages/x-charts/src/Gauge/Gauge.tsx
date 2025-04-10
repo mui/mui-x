4fd93d839cef18f57a69c5eb527f55ca4b6b1985
Bernardo Belchior <Bernardo.belchior1@gmail.com>
2025-04-10 10:19:59 +0100

    [charts] Animate gauge chart (#17304)
    Signed-off-by: Bernardo Belchior <Bernardo.belchior1@gmail.com>

--- packages/x-charts/src/Gauge/Gauge.tsx
+++ packages/x-charts/src/Gauge/Gauge.tsx
@@ -28,12 +28,13 @@ const useUtilityClasses = (props: GaugeProps) => {
 };
 
 const Gauge = React.forwardRef(function Gauge(props: GaugeProps, ref: React.Ref<SVGSVGElement>) {
-  const { text, children, classes: propsClasses, className, ...other } = props;
+  const { text, children, classes: propsClasses, className, skipAnimation, ...other } = props;
   const classes = useUtilityClasses(props);
+
   return (
     <GaugeContainer {...other} className={clsx(classes.root, className)} ref={ref}>
       <GaugeReferenceArc className={classes.referenceArc} />
-      <GaugeValueArc className={classes.valueArc} />
+      <GaugeValueArc className={classes.valueArc} skipAnimation={skipAnimation} />
       <GaugeValueText className={classes.valueText} text={text} />
       {children}
     </GaugeContainer>
