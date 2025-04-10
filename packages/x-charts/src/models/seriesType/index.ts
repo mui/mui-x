aea81319af7b4ca1bb1430f5c4776b63cef8185f
Bernardo Belchior <bernardo@mui.com>
2025-04-10 14:42:03 +0100

    Re-add exported types with deprecation message

--- packages/x-charts/src/models/seriesType/index.ts
+++ packages/x-charts/src/models/seriesType/index.ts
@@ -1,12 +1,36 @@
-import { ChartSeriesType, ChartsSeriesConfig } from './config';
+import { BarSeriesType, DefaultizedBarSeriesType } from './bar';
+import {
+  CartesianChartSeriesType,
+  ChartSeriesType,
+  ChartsSeriesConfig,
+  StackableChartSeriesType,
+} from './config';
 
 // Series definition
 
 type AllSeriesType<T extends ChartSeriesType = ChartSeriesType> =
   ChartsSeriesConfig[T]['seriesProp'];
 
+/**
+ * @deprecated We do not use this type in v8. If it's useful for you use case, please open an issue explaining why.
+ * Otherwise, it will be removed in next major.
+ */
+type CartesianSeriesType = AllSeriesType<CartesianChartSeriesType>;
+
 type DefaultizedSeriesType<T extends ChartSeriesType = ChartSeriesType> =
   ChartsSeriesConfig[T]['series'];
+
+/**
+ * @deprecated We do not use this type in v8. If it's useful for you use case, please open an issue explaining why.
+ * Otherwise, it will be removed in next major.
+ */
+type DefaultizedCartesianSeriesType = DefaultizedSeriesType<CartesianChartSeriesType>;
+
+/**
+ * @deprecated We do not use this type in v8. If it's useful for you use case, please open an issue explaining why.
+ * Otherwise, it will be removed in next major.
+ */
+type StackableSeriesType = DefaultizedSeriesType<StackableChartSeriesType>;
 
 // item identifier
 
@@ -18,4 +42,30 @@ export * from './bar';
 export * from './scatter';
 export * from './pie';
 export * from './radar';
-export type { AllSeriesType, DefaultizedSeriesType };
+export type {
+  AllSeriesType,
+  DefaultizedSeriesType,
+  CartesianSeriesType,
+  DefaultizedCartesianSeriesType,
+  StackableSeriesType,
+};
+
+// Helpers
+
+/**
+ * @deprecated We do not use this function in v8. If it's useful for you use case, please open an issue explaining why.
+ * Otherwise, it will be removed in next major.
+ */
+export function isDefaultizedBarSeries(
+  series: DefaultizedSeriesType,
+): series is DefaultizedBarSeriesType {
+  return series.type === 'bar';
+}
+
+/**
+ * @deprecated We do not use this function in v8. If it's useful for you use case, please open an issue explaining why.
+ * Otherwise, it will be removed in next major.
+ */
+export function isBarSeries(series: AllSeriesType): series is BarSeriesType {
+  return series.type === 'bar';
+}
