dbf370b1328263a3ca8596c39367c6076d32adc9
Andrew Cherniavskii <andrew@mui.com>
2025-04-10 14:27:47 +0200

    [test] Fix flaky data source aggregation test (take 2) (#17311)

--- packages/x-data-grid-premium/src/tests/dataSourceAggregation.DataGridPremium.test.tsx
+++ packages/x-data-grid-premium/src/tests/dataSourceAggregation.DataGridPremium.test.tsx
@@ -104,9 +104,7 @@ describeSkipIf(isJSDOM)('<DataGridPremium /> - Data source aggregation', () => {
       expect(fetchRowsSpy.callCount).to.be.greaterThan(0);
     });
     await user.click(within(getColumnHeaderCell(0)).getByLabelText('id column menu'));
-    await waitFor(async () => {
-      expect(await screen.findByLabelText('Aggregation')).not.to.equal(null);
-    });
+    await screen.findByLabelText('Aggregation');
   });
 
   it('should not show aggregation option in the column menu when no aggregation function is defined', async () => {
