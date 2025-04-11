47a9138828d3ca707ca23023797b7554d16fc440
Lukas <llukas.tyla@gmail.com>
2025-04-11 16:30:34 +0300

    Add tests asserting expected form behavior

--- packages/x-date-pickers/src/DatePicker/tests/DatePicker.test.tsx
+++ packages/x-date-pickers/src/DatePicker/tests/DatePicker.test.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { expect } from 'chai';
+import { spy } from 'sinon';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { screen } from '@mui/internal-test-utils/createRenderer';
+import { screen } from '@mui/internal-test-utils';
 import { createPickerRenderer, stubMatchMedia } from 'test/utils/pickers';
 
 describe('<DatePicker />', () => {
@@ -17,5 +18,31 @@ describe('<DatePicker />', () => {
     expect(screen.queryByRole('dialog')).to.not.equal(null);
 
     window.matchMedia = originalMatchMedia;
+  });
+
+  it('should submit the form when "Enter" is pressed on the input', async () => {
+    function TestComponent({ onSubmit }) {
+      return (
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            onSubmit(new window.FormData(event.target as any));
+          }}
+        >
+          <DatePicker name="testDate" defaultValue={new Date('2022-04-17')} />
+          <button type="submit">Submit</button>
+        </form>
+      );
+    }
+
+    const handleSubmit = spy();
+    const { user } = render(<TestComponent onSubmit={handleSubmit} />);
+
+    // focus the input
+    await user.keyboard('{Tab}');
+    await user.keyboard('{Enter}');
+
+    expect(handleSubmit.callCount).to.equal(1);
+    expect([...handleSubmit.lastCall.args[0]][0]).to.deep.equal(['testDate', '04/17/2022']);
   });
 });
