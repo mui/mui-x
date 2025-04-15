cecf4d0854b55b407db19b0912b404eab6596c0a
Lukas <llukas.tyla@gmail.com>
2025-04-15 18:56:40 +0300

    Fix TS error by aligning slotProps types

--- packages/x-date-pickers/src/internals/components/PickerFieldUI.tsx
+++ packages/x-date-pickers/src/internals/components/PickerFieldUI.tsx
@@ -460,7 +460,7 @@ interface UseFieldTextFieldPropsParameters {
   slotProps:
     | {
         textField?: SlotComponentPropsFromProps<
-          PickersTextFieldProps | TextFieldProps,
+          Omit<TextFieldProps, 'onKeyDown'> | PickersTextFieldProps,
           {},
           FieldOwnerState
         >;
