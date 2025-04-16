bd7ae1151cbfbed88191e121da235f10f371a797
flavien <flaviendelangle@gmail.com>
2025-04-16 11:37:51 +0200

    Fix

--- docs/data/scheduler/overview/BasicScheduler.js
+++ docs/data/scheduler/overview/BasicScheduler.js
@@ -1,4 +1,6 @@
-import { Scheduler } from '@mui/x-scheduler/joy';
+import * as React from 'react';
+import { Scheduler } from '@mui/x-scheduler/joy/scheduler';
+import '@mui/x-scheduler/joy/index.css';
 
 export default function BasicScheduler() {
   return <Scheduler />;
