f1f5532a16a644f40161fb4ab301d958ad86d1c7
Bernardo Belchior <bernardo@mui.com>
2025-04-17 09:51:41 +0100

    Update export composition example

--- docs/data/charts/export/ExportCompositionNoSnap.js
+++ docs/data/charts/export/ExportCompositionNoSnap.js
@@ -8,14 +8,23 @@ import Button from '@mui/material/Button';
 import { Stack } from '@mui/system';
 import { ChartDataProviderPro } from '@mui/x-charts-pro/ChartDataProviderPro';
 import { ChartsSurface } from '@mui/x-charts/ChartsSurface';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
+import { ChartsLegend } from '@mui/x-charts/ChartsLegend';
 
 function CustomChartWrapper({ children }) {
   const chartRootRef = useChartRootRef();
 
-  return <div ref={chartRootRef}>{children}</div>;
+  return (
+    <div
+      ref={chartRootRef}
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      {children}
+    </div>
+  );
 }
 
-export default function ExportComposition() {
+export default function ExportCompositionNoSnap() {
   const apiRef = React.useRef(undefined);
 
   return (
@@ -27,10 +36,12 @@ export default function ExportComposition() {
           {
             type: 'bar',
             data: [1, 2, 3, 2, 1],
+            label: 'Bar',
           },
           {
             type: 'line',
             data: [4, 3, 1, 3, 4],
+            label: 'Line',
           },
         ]}
         xAxis={[
@@ -38,16 +49,19 @@ export default function ExportComposition() {
             data: ['A', 'B', 'C', 'D', 'E'],
             scaleType: 'band',
             id: 'x-axis-id',
-            height: 45,
+            height: 24,
           },
         ]}
+        yAxis={[{ width: 20 }]}
       >
         <CustomChartWrapper>
+          <ChartsLegend direction="horizontal" />
           <ChartsSurface>
             <BarPlot />
             <LinePlot />
             <MarkPlot />
-            <ChartsXAxis label="X axis" axisId="x-axis-id" />
+            <ChartsXAxis axisId="x-axis-id" />
+            <ChartsYAxis />
           </ChartsSurface>
         </CustomChartWrapper>
       </ChartDataProviderPro>
