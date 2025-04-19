dfa690d25aead77319283f1e5765352c330952fe
Rom Grk <romgrk.cc@gmail.com>
2025-04-19 07:55:40 -0400

    refactor: GridRowCount

--- packages/x-data-grid/src/components/GridRowCount.tsx
+++ packages/x-data-grid/src/components/GridRowCount.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import composeClasses from '@mui/utils/composeClasses';
 import { styled, SxProps, Theme } from '@mui/system';
+import { css } from '@mui/x-internals/css';
 import { forwardRef } from '@mui/x-internals/forwardRef';
 import { vars } from '../constants/cssVariables';
+import { composeGridStyles } from '../utils/composeGridStyles';
 import { useGridApiContext } from '../hooks/utils/useGridApiContext';
-import { getDataGridUtilityClass } from '../constants/gridClasses';
 import { useGridRootProps } from '../hooks/utils/useGridRootProps';
 import { DataGridProcessedProps } from '../models/props/DataGridProps';
 
@@ -22,32 +22,26 @@ export type GridRowCountProps = React.HTMLAttributes<HTMLDivElement> &
 
 type OwnerState = DataGridProcessedProps;
 
-const useUtilityClasses = (ownerState: OwnerState) => {
-  const { classes } = ownerState;
-
-  const slots = {
-    root: ['rowCount'],
-  };
-
-  return composeClasses(slots, getDataGridUtilityClass, classes);
-};
-
 const GridRowCountRoot = styled('div', {
   name: 'MuiDataGrid',
   slot: 'RowCount',
   overridesResolver: (props, styles) => styles.rowCount,
-})<{ ownerState: OwnerState }>({
-  alignItems: 'center',
-  display: 'flex',
-  margin: vars.spacing(0, 2),
+})<{ ownerState: OwnerState }>(null);
+
+const styles = css('MuiDataGrid-rowCount', {
+  root: {
+    alignItems: 'center',
+    display: 'flex',
+    margin: vars.spacing(0, 2),
+  },
 });
 
 const GridRowCount = forwardRef<HTMLDivElement, GridRowCountProps>(
   function GridRowCount(props, ref) {
     const { className, rowCount, visibleRowCount, ...other } = props;
     const apiRef = useGridApiContext();
-    const ownerState = useGridRootProps();
-    const classes = useUtilityClasses(ownerState);
+    const rootProps = useGridRootProps();
+    const classes = composeGridStyles(styles, rootProps.classes);
 
     if (rowCount === 0) {
       return null;
@@ -61,7 +55,7 @@ const GridRowCount = forwardRef<HTMLDivElement, GridRowCountProps>(
     return (
       <GridRowCountRoot
         className={clsx(classes.root, className)}
-        ownerState={ownerState}
+        ownerState={rootProps}
         {...other}
         ref={ref}
       >
