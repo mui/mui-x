2fb0db73d4b11fdea2d939a234717194abaa6e37
Bernardo Belchior <bernardo@mui.com>
2025-04-22 15:31:43 +0100

    Fix lint

--- packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/defaultizeZoom.ts
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/defaultizeZoom.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_ZOOM_BRUSH_SIZE } from '../../../../constants';
 import { AxisId } from '../../../../models/axis';
 import { DefaultizedZoomOptions } from './useChartCartesianAxis.types';
 import { ZoomOptions } from './zoom.types';
@@ -10,7 +11,7 @@ const defaultZoomOptions = {
   maxSpan: 100,
   panning: true,
   filterMode: 'keep',
-  overview: { enabled: false },
+  overview: { enabled: false, size: DEFAULT_ZOOM_BRUSH_SIZE },
 } satisfies Omit<DefaultizedZoomOptions, 'axisId' | 'axisDirection'>;
 
 export const defaultizeZoom = (
