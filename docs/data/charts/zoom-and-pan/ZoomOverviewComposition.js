410fdc00a89bf0376be9cf518a8e593bca571ffd
Bernardo Belchior <bernardo@mui.com>
2025-04-22 15:43:55 +0100

    Update composition example

--- docs/data/charts/zoom-and-pan/ZoomOverviewComposition.js
+++ docs/data/charts/zoom-and-pan/ZoomOverviewComposition.js
@@ -4,23 +4,28 @@ import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
 import { LinePlot } from '@mui/x-charts/LineChart';
 import { ChartContainerPro } from '@mui/x-charts-pro/ChartContainerPro';
 import { ChartOverview } from '@mui/x-charts-pro/ChartOverview';
+import { ChartsClipPath } from '@mui/x-charts/ChartsClipPath';
+
+const xAxisData = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
+const firstSeriesData = Array.from({ length: 26 }, () =>
+  Math.floor(Math.random() * 11),
+);
+const secondSeriesData = Array.from({ length: 26 }, () =>
+  Math.floor(Math.random() * 11),
+);
 
 export default function ZoomOverviewComposition() {
+  const clipPathId = React.useId();
+
   return (
     <ChartContainerPro
       series={[
-        {
-          type: 'line',
-          data: [1, 2, 3, 2, 1],
-        },
-        {
-          type: 'line',
-          data: [4, 3, 1, 3, 4],
-        },
+        { type: 'line', data: firstSeriesData },
+        { type: 'line', data: secondSeriesData },
       ]}
       xAxis={[
         {
-          data: ['A', 'B', 'C', 'D', 'E'],
+          data: xAxisData,
           scaleType: 'band',
           id: 'x-axis-id',
           height: 45,
@@ -29,10 +34,13 @@ export default function ZoomOverviewComposition() {
       ]}
       height={200}
     >
-      <BarPlot />
-      <LinePlot />
+      <g clipPath={`url(#${clipPathId})`}>
+        <BarPlot />
+        <LinePlot />
+      </g>
       <ChartsXAxis label="X axis" axisId="x-axis-id" />
       <ChartOverview />
+      <ChartsClipPath id={clipPathId} />
     </ChartContainerPro>
   );
 }
