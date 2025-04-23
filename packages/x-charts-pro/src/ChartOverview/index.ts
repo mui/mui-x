397d3a9d3049479cf2b704aa746f626f41353637
Bernardo Belchior <bernardo@mui.com>
2025-04-23 18:05:36 +0100

    Fix export

--- packages/x-charts-pro/src/ChartOverview/index.ts
+++ packages/x-charts-pro/src/ChartOverview/index.ts
@@ -1,2 +1,6 @@
 export * from './ChartOverview';
-export * from './chartAxisZoomOverviewHandleClasses';
+export {
+  ChartAxisZoomOverviewHandleClasses,
+  ChartAxisZoomOverviewHandleClassKey,
+  chartAxisZoomOverviewHandleClasses,
+} from './chartAxisZoomOverviewHandleClasses';
