9417d97e1b268f6bf8f49e072d44ceda06bf17f2
Jose Quintas <juniorquintas@gmail.com>
2025-04-26 17:20:20 +0200

    scroll on pan

--- packages/x-charts-pro/src/internals/plugins/useChartProZoom/gestureHooks/usePanOnDrag.ts
+++ packages/x-charts-pro/src/internals/plugins/useChartProZoom/gestureHooks/usePanOnDrag.ts
@@ -9,8 +9,35 @@ import {
 } from '@mui/x-charts/internals';
 import { rafThrottle } from '@mui/x-internals/rafThrottle';
 import { PanEvent } from '@web-gestures/core';
+import { isDeepEqual } from '@mui/x-internals/isDeepEqual';
 import { UseChartProZoomSignature } from '../useChartProZoom.types';
 import { translateZoom } from './useZoom.utils';
+
+function getScrollParent(element: Element | null, includeHidden: boolean = false): Element {
+  if (!element) {
+    return document.body;
+  }
+
+  let style = getComputedStyle(element);
+  const excludeStaticParent = style.position === 'absolute';
+  const overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
+
+  if (style.position === 'fixed') {
+    return document.body;
+  }
+  // eslint-disable-next-line no-cond-assign
+  for (let parent: Element | null = element; (parent = parent.parentElement); ) {
+    style = getComputedStyle(parent);
+    if (excludeStaticParent && style.position === 'static') {
+      continue;
+    }
+    if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX)) {
+      return parent;
+    }
+  }
+
+  return document.body;
+}
 
 export const usePanOnDrag = (
   {
@@ -22,6 +49,8 @@ export const usePanOnDrag = (
 ) => {
   const drawingArea = useSelector(store, selectorChartDrawingArea);
   const optionsLookup = useSelector(store, selectorChartZoomOptionsLookup);
+  const isChangingRef = React.useRef(false);
+  const isChangingTimeoutRef = React.useRef<ReturnType<typeof setTimeout> | null>(null);
 
   // Add event for chart panning
   const isPanEnabled = React.useMemo(
@@ -41,12 +70,26 @@ export const usePanOnDrag = (
     // instead of always calculating the new zoom data based on the current zoom data
     const panStartHandler = instance.addInteractionListener('panStart', (event) => {
       event.detail.customData.zoomData = store.getSnapshot().zoom.zoomData;
+      const scrollParent = getScrollParent(event.target as Element);
+      event.detail.customData.scrollParent = scrollParent;
+      event.detail.customData.scrollParentOriginalPosition = {
+        top: scrollParent.scrollTop,
+        left: scrollParent.scrollLeft,
+      };
     });
 
     const rafThrottledCallback = rafThrottle(
-      (event: PanEvent<{ zoomData: readonly ZoomData[] }>) => {
+      (
+        event: PanEvent<{
+          zoomData: readonly ZoomData[];
+          scrollParent: Element;
+          scrollParentOriginalPosition: { top: number; left: number };
+        }>,
+      ) => {
+        const zoomData = event.detail.customData.zoomData;
+
         const newZoomData = translateZoom(
-          event.detail.customData.zoomData,
+          zoomData,
           { x: event.detail.deltaX, y: -event.detail.deltaY },
           {
             width: drawingArea.width,
@@ -55,19 +98,63 @@ export const usePanOnDrag = (
           optionsLookup,
         );
 
+        // This handles preventing the default behavior of the wheel event
+        // when the zoom data is changing.
+        const isEqual = isDeepEqual(zoomData, newZoomData);
+        if (!isEqual || isChangingTimeoutRef.current) {
+          isChangingRef.current = true;
+          if (isChangingTimeoutRef.current) {
+            clearTimeout(isChangingTimeoutRef.current);
+          }
+          isChangingTimeoutRef.current = setTimeout(() => {
+            isChangingRef.current = false;
+            isChangingTimeoutRef.current = null;
+          }, 100);
+        }
+
+        if (!isChangingRef.current && event.detail.srcEvent.pointerType !== 'mouse') {
+          const canScrollVertical =
+            event.detail.customData.scrollParent.scrollHeight >
+            event.detail.customData.scrollParent.clientHeight;
+          const canScrollHorizontal =
+            event.detail.customData.scrollParent.scrollWidth >
+            event.detail.customData.scrollParent.clientWidth;
+          event.detail.customData.scrollParent.scrollTo({
+            ...(canScrollHorizontal
+              ? {
+                  left:
+                    event.detail.customData.scrollParentOriginalPosition.left + event.detail.deltaX,
+                }
+              : {}),
+            ...(canScrollVertical
+              ? {
+                  top:
+                    event.detail.customData.scrollParentOriginalPosition.top - event.detail.deltaY,
+                }
+              : {}),
+            behavior: 'instant',
+          });
+        }
+
         setZoomDataCallback(newZoomData);
       },
     );
 
-    const panHandler = instance.addInteractionListener<{ zoomData: readonly ZoomData[] }>(
-      'pan',
-      rafThrottledCallback,
-    );
+    const panHandler = instance.addInteractionListener<{
+      zoomData: readonly ZoomData[];
+      scrollParent: Element;
+      scrollParentOriginalPosition: { top: number; left: number };
+    }>('pan', rafThrottledCallback);
 
     return () => {
       panStartHandler.cleanup();
       panHandler.cleanup();
       rafThrottledCallback.clear();
+      if (isChangingTimeoutRef.current) {
+        clearTimeout(isChangingTimeoutRef.current);
+        isChangingTimeoutRef.current = null;
+      }
+      isChangingRef.current = false;
     };
   }, [
     instance,
