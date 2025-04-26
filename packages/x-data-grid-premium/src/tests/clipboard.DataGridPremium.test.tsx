4884ea0cba18ac169523312742b516a7b70a6f07
Jose Quintas <juniorquintas@gmail.com>
2025-04-26 19:41:51 +0200

    revert

--- packages/x-data-grid-premium/src/tests/clipboard.DataGridPremium.test.tsx
+++ packages/x-data-grid-premium/src/tests/clipboard.DataGridPremium.test.tsx
@@ -12,6 +12,7 @@ import { expect } from 'chai';
 import { SinonSpy, spy, stub, SinonStub } from 'sinon';
 import { getCell, getColumnValues, includeRowSelection, sleep } from 'test/utils/helperFn';
 import { getBasicGridData } from '@mui/x-data-grid-generator';
+import { describeSkipIf, isJSDOM } from 'test/utils/skipIf';
 
 describe('<DataGridPremium /> - Clipboard', () => {
   const { render } = createRenderer();
@@ -169,7 +170,7 @@ describe('<DataGridPremium /> - Clipboard', () => {
   });
 
   // These test are flaky in JSDOM
-  describe('paste', () => {
+  describeSkipIf(isJSDOM)('paste', () => {
     function paste(cell: HTMLElement, pasteText: string) {
       const pasteEvent = new Event('paste');
 
