f746f590751012b05a68612dcb3a5aac463e5925
Bernardo Belchior <bernardo@mui.com>
2025-04-29 10:26:01 +0100

    [charts] Add docs on tooltip style

---
+++ docs/data/charts/tooltip/TooltipStyle.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
+import { chartsTooltipClasses } from '@mui/x-charts/ChartsTooltip';
+
+const params = {
+  xAxis: [{ data: [1, 2, 3, 5, 8, 10] }],
+  series: [{ data: [2, 5.5, 2, 8.5, 1.5, 5] }],
+  height: 300,
+  axisHighlight: { x: 'line' },
+};
+
+export default function TooltipStyle() {
+  return (
+    <LineChart
+      {...params}
+      slotProps={{
+        tooltip: {
+          sx: {
+            [`.${chartsTooltipClasses.valueCell}`]: {
+              color: 'red',
+            },
+          },
+        },
+      }}
+    />
+  );
+}
