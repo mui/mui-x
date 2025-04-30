be9dc0c92a6d68bf5f2d2d8d8e4af8508c3a4a9e
Jose Quintas <juniorquintas@gmail.com>
2025-04-30 15:21:51 +0200

    subdivide colors

--- docs/data/charts/styling/MuiColorTemplate.tsx
+++ docs/data/charts/styling/MuiColorTemplate.tsx
@@ -15,7 +15,19 @@ import {
   blueberryTwilightPalette,
   mangoFusionPalette,
   cheerfulFiestaPalette,
+  strawberrySkyPalette,
+  rainbowSurgePalette,
+  bluePalette,
+  greenPalette,
+  purplePalette,
+  redPalette,
+  orangePalette,
+  yellowPalette,
+  cyanPalette,
+  pinkPalette,
 } from '@mui/x-charts/colorPalettes';
+import Divider from '@mui/material/Divider';
+import ListSubheader from '@mui/material/ListSubheader';
 
 const chance = new Chance(42);
 
@@ -67,10 +79,27 @@ const series = [
     v && `(${v.x.toFixed(1)}, ${v.y.toFixed(1)})`,
 }));
 
-const categories = {
+const categorical = {
+  rainbowSurge: rainbowSurgePalette,
   blueberryTwilight: blueberryTwilightPalette,
   mangoFusion: mangoFusionPalette,
   cheerfulFiesta: cheerfulFiestaPalette,
+} as const;
+const sequential = {
+  strawberrySky: strawberrySkyPalette,
+  purple: purplePalette,
+  blue: bluePalette,
+  cyan: cyanPalette,
+  green: greenPalette,
+  yellow: yellowPalette,
+  orange: orangePalette,
+  red: redPalette,
+  pink: pinkPalette,
+} as const;
+
+const categories = {
+  ...categorical,
+  ...sequential,
 } as const;
 
 type PaletteKey = keyof typeof categories;
@@ -118,9 +147,42 @@ export default function MuiColorTemplate() {
               value={colorScheme}
               onChange={(event) => setColorScheme(event.target.value as PaletteKey)}
             >
-              {Object.entries(categories).map(([name, colors]) => (
+              <ListSubheader>Categorical</ListSubheader>
+              {Object.entries(categorical).map(([name, colors]) => (
                 <MenuItem key={name} value={name}>
-                  <Stack direction="row" alignItems="center">
+                  <Stack
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    width={'100%'}
+                  >
+                    <Typography sx={{ mr: 2 }}>{name}</Typography>
+                    <div style={{ width: 200, height: 20 }}>
+                      {colors(colorMode).map((c) => (
+                        <div
+                          key={c}
+                          style={{
+                            width: 20,
+                            height: 20,
+                            backgroundColor: c,
+                            display: 'inline-block',
+                          }}
+                        />
+                      ))}
+                    </div>
+                  </Stack>
+                </MenuItem>
+              ))}
+              <Divider />
+              <ListSubheader>Sequential</ListSubheader>
+              {Object.entries(sequential).map(([name, colors]) => (
+                <MenuItem key={name} value={name}>
+                  <Stack
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    width={'100%'}
+                  >
                     <Typography sx={{ mr: 2 }}>{name}</Typography>
                     <div style={{ width: 200, height: 20 }}>
                       {colors(colorMode).map((c) => (
