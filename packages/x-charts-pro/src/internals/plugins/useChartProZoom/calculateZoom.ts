cc96656182187395891a0c4af66c1dc7cf52289b
Bernardo Belchior <bernardo@mui.com>
2025-04-30 10:27:07 +0100

    Update docs

--- packages/x-charts-pro/src/internals/plugins/useChartProZoom/calculateZoom.ts
+++ packages/x-charts-pro/src/internals/plugins/useChartProZoom/calculateZoom.ts
@@ -2,6 +2,10 @@ import { DefaultizedZoomOptions, ZoomData } from '@mui/x-charts/internals';
 
 /**
  * Calculates the new zoom range based on the current zoom, step, and constraints.
+ *
+ * A step of 0.1 or -0.1 means that 10% of the current range will be subtracted/added, respectively, and assuming no
+ * constraints (e.g., minSpan, maxEnd) are faced.
+ *
  * @param zoom Current zoom range with start and end values.
  * @param step Percentage of the current zoom range to adjust (positive to zoom in, negative to zoom out). Ranges from 0 to 1.
  * @param minSpan Minimum allowed span between start and end values.
