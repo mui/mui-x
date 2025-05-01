deef8140586368801d49a13dee74ccb11ded164c
Jose Quintas <juniorquintas@gmail.com>
2025-05-01 09:56:23 +0200

    export type

--- packages/x-charts-pro/src/FunnelChart/index.ts
+++ packages/x-charts-pro/src/FunnelChart/index.ts
@@ -4,6 +4,6 @@ export * from './FunnelPlot';
 export * from './funnel.types';
 export * from './categoryAxis.types';
 export * from './funnelSlots.types';
-export { FunnelCurveType } from './curves';
+export type { FunnelCurveType } from './curves';
 export { funnelSectionClasses } from './funnelSectionClasses';
 export type { FunnelSectionClasses } from './funnelSectionClasses';
