161b235673f5a801f638cbdac087718925d616e9
Jose Quintas <juniorquintas@gmail.com>
2025-05-01 02:46:31 +0200

    improvements

--- packages/x-charts-pro/src/FunnelChart/curves/getFunnelCurve.ts
+++ packages/x-charts-pro/src/FunnelChart/curves/getFunnelCurve.ts
@@ -4,16 +4,20 @@ import { FunnelStep } from './funnelStep';
 import { Linear } from './linear';
 import { Bump } from './bump';
 
+const curveConstructor = (curve: FunnelCurveType | undefined) => {
+  if (curve === 'step') {
+    return FunnelStep;
+  }
+  if (curve === 'bump') {
+    return Bump;
+  }
+  return Linear;
+};
+
 export const getFunnelCurve = (
   curve: FunnelCurveType | undefined,
   isHorizontal: boolean,
   gap: number = 0,
 ): CurveFactory => {
-  const constructorMap = {
-    linear: Linear,
-    step: FunnelStep,
-    bump: Bump,
-  };
-
-  return (context) => new constructorMap[curve ?? 'linear'](context as any, isHorizontal, gap);
+  return (context) => new (curveConstructor(curve))(context as any, isHorizontal, gap);
 };
