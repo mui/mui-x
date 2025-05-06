e8c54e9e1857011a3fb3f71bcb4219e30c46a52c
Armin Mehinovic <armin@mui.com>
2025-05-06 10:47:21 +0200

    Do not update row count when loading new children with the data source

--- packages/x-data-grid-pro/src/hooks/features/dataSource/useGridDataSourceBasePro.ts
+++ packages/x-data-grid-pro/src/hooks/features/dataSource/useGridDataSourceBasePro.ts
@@ -129,7 +129,6 @@ export const useGridDataSourceBasePro = <Api extends GridPrivateApiPro>(
         const rows = cachedData.rows;
         nestedDataManager.setRequestSettled(id);
         apiRef.current.updateNestedRows(rows, rowNode.path);
-        apiRef.current.setRowCount(cachedData.rowCount === undefined ? -1 : cachedData.rowCount);
         apiRef.current.setRowChildrenExpansion(id, true);
         apiRef.current.dataSource.setChildrenLoading(id, false);
         return;
@@ -158,9 +157,6 @@ export const useGridDataSourceBasePro = <Api extends GridPrivateApiPro>(
           cache.set(key, response);
         });
 
-        apiRef.current.setRowCount(
-          getRowsResponse.rowCount === undefined ? -1 : getRowsResponse.rowCount,
-        );
         // Remove existing outdated rows before setting the new ones
         const rowsToDelete: GridRowModelUpdate[] = [];
         getRowsResponse.rows.forEach((row) => {
