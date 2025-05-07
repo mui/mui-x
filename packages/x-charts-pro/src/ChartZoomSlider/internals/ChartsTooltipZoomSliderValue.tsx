34e0ecf78803cba69efdcb43eee02ea97a6b8063
Bernardo Belchior <bernardo@mui.com>
2025-05-07 08:52:15 +0100

    Refactor

--- packages/x-charts-pro/src/ChartZoomSlider/internals/ChartsTooltipZoomSliderValue.tsx
+++ packages/x-charts-pro/src/ChartZoomSlider/internals/ChartsTooltipZoomSliderValue.tsx
@@ -1,38 +1,27 @@
 import * as React from 'react';
 import NoSsr from '@mui/material/NoSsr';
-import { styled } from '@mui/material/styles';
-import Popper, { PopperProps } from '@mui/material/Popper';
-import { ChartsTooltipPaper } from '@mui/x-charts/ChartsTooltip';
+import { PopperProps } from '@mui/material/Popper';
+import { ChartsTooltipPaper, ChartsTooltipRoot } from '@mui/x-charts/ChartsTooltip';
 import Typography from '@mui/material/Typography';
-
-const ChartsTooltipRoot = styled(Popper, {
-  name: 'MuiChartsTooltip',
-  slot: 'Root',
-})(({ theme }) => ({
-  pointerEvents: 'none',
-  zIndex: theme.zIndex.modal,
-}));
 
 const MODIFIERS = [
   {
     name: 'offset',
-    options: {
-      offset: [0, 4],
-    },
+    options: { offset: [0, 4] },
   },
 ];
 
-interface ChartsZoomValueTooltipProps
+interface ChartsTooltipZoomSliderValueProps
   extends Pick<PopperProps, 'anchorEl' | 'open' | 'modifiers' | 'placement'>,
     React.PropsWithChildren {}
 
-export function ChartsZoomValueTooltip({
+export function ChartsTooltipZoomSliderValue({
   anchorEl,
   open,
   placement,
   modifiers = MODIFIERS,
   children,
-}: ChartsZoomValueTooltipProps) {
+}: ChartsTooltipZoomSliderValueProps) {
   return (
     <NoSsr>
       {open ? (
