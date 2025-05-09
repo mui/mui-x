6f9cc8ec692f1f295f1ed14af89642eae40f6f15
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-05-09 13:18:44 +0200

    add docs for API Method SetEditedItem

---
+++ docs/data/tree-view/rich-tree-view/editing/ApiMethodSetEditedItem.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
+import { useTreeViewApiRef } from '@mui/x-tree-view/hooks';
+
+export default function ApiMethodSetEditedItem() {
+  const [items, setItems] = React.useState([
+    { id: '1', label: 'Jane Doe', editable: true },
+  ]);
+  const apiRef = useTreeViewApiRef();
+
+  const handleAddFolder = () => {
+    const newId = String(items.length + 1);
+    const newItem = { id: newId, label: '', editable: true };
+
+    setItems((prev) => [...prev, newItem]);
+
+    requestAnimationFrame(() => {
+      apiRef.current!.setEditedItem(newId);
+    });
+  };
+
+  const handleItemLabelChange = (itemId: string, newLabel: string) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, label: newLabel } : item
+      )
+    );
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Stack spacing={2} direction="row">
+        <Button onClick={handleAddFolder}>+ Add Folder</Button>
+      </Stack>
+      <Box sx={{ minHeight: 352, minWidth: 260 }}>
+        <RichTreeView
+          items={items}
+          apiRef={apiRef}
+          isItemEditable={(item) => item.editable}
+          onItemLabelChange={handleItemLabelChange}
+        />
+      </Box>
+    </Stack>
+  );
+}
