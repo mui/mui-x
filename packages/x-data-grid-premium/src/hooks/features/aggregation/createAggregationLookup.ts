b4026823c2bee6bea5456882e99129ee2a77bd70
Andrew Cherniavskyi <andrew@mui.com>
2025-05-22 01:30:03 +0200

    fix

--- packages/x-data-grid-premium/src/hooks/features/aggregation/createAggregationLookup.ts
+++ packages/x-data-grid-premium/src/hooks/features/aggregation/createAggregationLookup.ts
@@ -36,10 +36,6 @@ const getGroupAggregatedValue = (
   const filteredRowsLookup = gridFilteredRowsLookupSelector(apiRef);
 
   rowIds.forEach((rowId) => {
-    if (aggregationRowsScope === 'filtered' && filteredRowsLookup[rowId] === false) {
-      return;
-    }
-
     // If the row is a group, we want to aggregate based on its children
     // For instance in the following tree, we want the aggregated values of A to be based on A.A, A.B.A and A.B.B but not A.B
     // A
@@ -69,7 +65,9 @@ const getGroupAggregatedValue = (
         };
       }
 
-      if (typeof aggregationFunction.getCellValue === 'function') {
+      if (aggregationRowsScope === 'filtered' && filteredRowsLookup[rowId] === false) {
+        aggregatedValues[j].values.push(0);
+      } else if (typeof aggregationFunction.getCellValue === 'function') {
         aggregatedValues[j].values.push(aggregationFunction.getCellValue({ row }));
       } else {
         const colDef = apiRef.current.getColumn(field);
