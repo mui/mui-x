ddfe0423416bb9a818c36525db29872250d4bd29
flavien <flaviendelangle@gmail.com>
2025-05-21 10:20:25 +0200

    Rita review: Hardcode dates

--- docs/data/scheduler/overview/BasicEventCalendar.tsx
+++ docs/data/scheduler/overview/BasicEventCalendar.tsx
@@ -1,58 +1,49 @@
 import * as React from 'react';
-import { DateTime, WeekdayNumbers, MinuteNumbers, HourNumbers } from 'luxon';
+import { DateTime } from 'luxon';
 import { EventCalendar } from '@mui/x-scheduler/joy/event-calendar';
 import { CalendarEvent } from '@mui/x-scheduler/joy/models/events';
-
-const startOfWeek = DateTime.now().startOf('week');
-const createDate = (
-  weekday: WeekdayNumbers,
-  hour: HourNumbers,
-  minute: MinuteNumbers,
-) => {
-  return startOfWeek.set({ weekday, hour, minute });
-};
 
 const events: CalendarEvent[] = [
   {
     id: '1',
-    start: createDate(1, 7, 30),
-    end: createDate(1, 8, 15),
+    start: DateTime.fromISO('2025-05-26T07:30:00'),
+    end: DateTime.fromISO('2025-05-26T08:15:00'),
     title: 'Footing',
   },
   {
     id: '2',
-    start: createDate(1, 16, 0),
-    end: createDate(1, 17, 0),
+    start: DateTime.fromISO('2025-05-26T16:00:00'),
+    end: DateTime.fromISO('2025-05-26T17:00:00'),
     title: 'Weekly',
   },
   {
     id: '3',
-    start: createDate(2, 10, 0),
-    end: createDate(2, 11, 0),
+    start: DateTime.fromISO('2025-05-27T10:00:00'),
+    end: DateTime.fromISO('2025-05-27T11:00:00'),
     title: 'Backlog grooming',
   },
   {
     id: '4',
-    start: createDate(2, 19, 0),
-    end: createDate(2, 22, 0),
+    start: DateTime.fromISO('2025-05-27T19:00:00'),
+    end: DateTime.fromISO('2025-05-27T22:00:00'),
     title: 'Pizza party',
   },
   {
     id: '5',
-    start: createDate(3, 8, 0),
-    end: createDate(3, 17, 0),
+    start: DateTime.fromISO('2025-05-28T08:00:00'),
+    end: DateTime.fromISO('2025-05-28T17:00:00'),
     title: 'Scheduler deep dive',
   },
   {
     id: '1',
-    start: createDate(4, 7, 30),
-    end: createDate(4, 8, 15),
+    start: DateTime.fromISO('2025-05-29T07:30:00'),
+    end: DateTime.fromISO('2025-05-29T08:15:00'),
     title: 'Footing',
   },
   {
     id: '1',
-    start: createDate(5, 15, 0),
-    end: createDate(5, 15, 45),
+    start: DateTime.fromISO('2025-05-30T15:00:00'),
+    end: DateTime.fromISO('2025-05-30T15:45:00'),
     title: 'Retrospective',
   },
 ];
