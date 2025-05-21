ed2fa5898ca3a2c2d374570f0ee8f3cb238b2c7f
flavien <flaviendelangle@gmail.com>
2025-05-21 12:01:44 +0200

    [scheduler][joy] Create the basic layout of the EventCalendar component

--- packages/x-scheduler/src/joy/event-calendar/EventCalendar.tsx
+++ packages/x-scheduler/src/joy/event-calendar/EventCalendar.tsx
@@ -12,9 +12,21 @@ export const EventCalendar = React.forwardRef(function EventCalendar(
   const { events, onEventsChange, className, ...other } = props;
 
   return (
-    <div className="mui light">
-      <div className={clsx(className, 'EventCalendar')} ref={forwardedRef} {...other}>
-        HELLO WORLD ({events.length} events loaded)
+    <div
+      className={clsx(className, 'EventCalendarRoot', 'mui', 'light')}
+      ref={forwardedRef}
+      {...other}
+    >
+      <div className="EventCalendarSidePanel">
+        <span style={{ height: 72, paddingTop: 18 }}>TODO: Time nav</span>
+        <div className="EventCalendarMonthCalendarPlaceholder">Month Calendar</div>
+        <span style={{ height: 200, paddingTop: 24 }}>TODO: Resource legend</span>
+      </div>
+      <div className="EventCalendarMainPanel">
+        <div className="EventCalendarToolbar">
+          <span>TODO: View switch</span>
+        </div>
+        <div className="EventCalendarContent">Content: {events.length} events loaded</div>
       </div>
     </div>
   );
