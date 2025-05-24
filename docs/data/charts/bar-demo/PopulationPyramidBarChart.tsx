353b5d1ed0cecc4939a7ea52dcbe8c1950d04c4b
Olivier Tassinari <olivier.tassinari@gmail.com>
2025-05-25 00:43:47 +0200

    [charts][docs] Fix Population pyramid demo

--- docs/data/charts/bar-demo/PopulationPyramidBarChart.tsx
+++ docs/data/charts/bar-demo/PopulationPyramidBarChart.tsx
@@ -4,37 +4,39 @@ import Typography from '@mui/material/Typography';
 import { BarChart } from '@mui/x-charts/BarChart';
 
 const ageGroups = [
-  '0-4 yrs',
-  '5-9 yrs',
-  '10-14 yrs',
-  '15-19 yrs',
-  '20-24 yrs',
-  '25-29 yrs',
-  '30-34 yrs',
-  '35-39 yrs',
-  '40-44 yrs',
-  '45-49 yrs',
-  '50-54 yrs',
-  '55-59 yrs',
-  '60-64 yrs',
-  '65-69 yrs',
-  '70-74 yrs',
-  '75-79 yrs',
-  '80-84 yrs',
-  '85-89 yrs',
-  '90-94 yrs',
-  '95-99 yrs',
   '100+ yrs',
+  '95-99 yrs',
+  '90-94 yrs',
+  '85-89 yrs',
+  '80-84 yrs',
+  '75-79 yrs',
+  '70-74 yrs',
+  '65-69 yrs',
+  '60-64 yrs',
+  '55-59 yrs',
+  '50-54 yrs',
+  '45-49 yrs',
+  '40-44 yrs',
+  '35-39 yrs',
+  '30-34 yrs',
+  '25-29 yrs',
+  '20-24 yrs',
+  '15-19 yrs',
+  '10-14 yrs',
+  '5-9 yrs',
+  '0-4 yrs',
 ];
+
 const male = [
-  766227, 1097221, 1189663, 1183580, 1620461, 1933726, 1796779, 1808706, 2067075,
-  2061862, 2258061, 2068112, 2042614, 1478069, 1012668, 696606, 476263, 201240,
-  50323, 8291, 1139,
+  1139, 8291, 50323, 201240, 476263, 696606, 1012668, 1478069, 2042614, 2068112,
+  2258061, 2061862, 2067075, 1808706, 1796779, 1933726, 1620461, 1183580, 1189663,
+  1097221, 766227,
 ];
+
 const female = [
-  727814, 1044863, 1122176, 1104293, 1484776, 1695058, 1593655, 1673805, 1967944,
-  2000130, 2231491, 2045845, 2105499, 1585781, 1152098, 899933, 762492, 445118,
-  168603, 36739, 5770,
+  5770, 36739, 168603, 445118, 762492, 899933, 1152098, 1585781, 2105499, 2045845,
+  2231491, 2000130, 1967944, 1673805, 1593655, 1695058, 1484776, 1104293, 1122176,
+  1044863, 727814,
 ];
 
 const numberFormatter = Intl.NumberFormat('en-US', {
@@ -46,18 +48,21 @@ const numberWithSuffixFormatter = new Intl.NumberFormat('en-US', {
 
 export default function PopulationPyramidBarChart() {
   return (
-    <Stack width="100%">
-      <Typography variant="h6" textAlign="center">
+    <Stack width="100%" sx={{ mx: [0, 4] }}>
+      <Typography variant="h6" component="span" textAlign="center">
         South Korea Population Pyramid - 2022
       </Typography>
       <BarChart
         height={500}
+        layout="horizontal"
+        margin={{ right: 0, left: 0 }}
         series={[
           {
-            data: male.map((d) => -d),
+            data: male.map((population) => -population),
             label: 'Male',
             type: 'bar',
-            valueFormatter: (d: number | null) => `${numberFormatter.format(-d!)}`,
+            valueFormatter: (population: number | null) =>
+              `${numberFormatter.format(-population!)}`,
             stack: 'stack',
           },
           {
@@ -70,21 +75,19 @@ export default function PopulationPyramidBarChart() {
         yAxis={[
           {
             data: ageGroups,
-            width: 70,
+            width: 60,
             disableLine: true,
             disableTicks: true,
           },
         ]}
         xAxis={[
           {
-            valueFormatter: (d: number) =>
-              numberWithSuffixFormatter.format(Math.abs(d)),
+            valueFormatter: (population: number) =>
+              numberWithSuffixFormatter.format(Math.abs(population)),
             disableLine: true,
             disableTicks: true,
           },
         ]}
-        layout="horizontal"
-        margin={{ right: 70 }}
       />
       <Typography variant="caption">Source: KOSIS</Typography>
     </Stack>
