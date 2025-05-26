14e66130afae9583ff285832c2de49dbc74b40a9
flavien <flaviendelangle@gmail.com>
2025-05-26 14:25:48 +0200

    Remove unused code

--- packages/x-scheduler/src/primitives/day-grid/root/DayGridRoot.tsx
+++ packages/x-scheduler/src/primitives/day-grid/root/DayGridRoot.tsx
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import { useRenderElement } from '../../../base-ui-copy/utils/useRenderElement';
 import { BaseUIComponentProps } from '../../../base-ui-copy/utils/types';
-import { SchedulerValidDate } from '../../utils/adapter/types';
 
 export const DayGridRoot = React.forwardRef(function DayGridRoot(
   componentProps: DayGridRoot.Props,
@@ -31,27 +30,4 @@ export namespace DayGridRoot {
   export interface State {}
 
   export interface Props extends BaseUIComponentProps<'div', State> {}
-
-  export interface ChildrenParameters {
-    /**
-     * Returns the weeks in the given month.
-     * The returned value is an array where each element is a date object representing the first day of the week.
-     * @param {GetWeeksInMonthParameters} parameters The parameters for the function.
-     * @returns {SchedulerValidDate[]} The weeks in the given month.
-     */
-    getWeeksInMonth: (parameters: GetWeeksInMonthParameters) => SchedulerValidDate[];
-  }
-
-  export interface GetWeeksInMonthParameters {
-    /**
-     * The month to calculate the weeks for.
-     */
-    month: SchedulerValidDate;
-    /**
-     * The amount of weeks to return.
-     * The method will add as many weeks as needed after the end of the month to match this value.
-     * Put it to 6 to have a fixed number of weeks across months in Gregorian calendars.
-     */
-    fixedWeekNumber?: number;
-  }
 }
