6dff2fe301a471c9b9d1973980ece801e77457e7
Armin Mehinovic <armin@mui.com>
2025-05-27 12:27:28 +0200

    proptypes

--- packages/x-charts-pro/src/ChartsRenderer/ChartsRenderer.tsx
+++ packages/x-charts-pro/src/ChartsRenderer/ChartsRenderer.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { BarChart } from '@mui/x-charts/BarChart';
 import { LineChart } from '@mui/x-charts/LineChart';
 import { PieChart } from '@mui/x-charts/PieChart';
@@ -9,7 +10,7 @@ export interface ChartsRendererProps {
   chartType: string;
 }
 
-export function ChartsRenderer({ categories, series, chartType }: ChartsRendererProps) {
+function ChartsRenderer({ categories, series, chartType }: ChartsRendererProps) {
   if (chartType === 'bar') {
     // TODO: instead of returning charts directly, each chart can have a helper that will get the configuration and set the props (and add the defaults)
     return (
@@ -71,3 +72,15 @@ export function ChartsRenderer({ categories, series, chartType }: ChartsRenderer
 
   return <span>No chart renderer found for the chart type: {chartType}</span>;
 }
+
+ChartsRenderer.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "pnpm proptypes"  |
+  // ----------------------------------------------------------------------
+  categories: PropTypes.arrayOf(PropTypes.string).isRequired,
+  chartType: PropTypes.string.isRequired,
+  series: PropTypes.arrayOf(PropTypes.object).isRequired,
+} as any;
+
+export { ChartsRenderer };
