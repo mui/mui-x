92dd4b046de8ef2a551b087cbb4a966407bf5ce8
alex <alex.fauquette@gmail.com>
2025-05-27 16:57:46 +0200

    [docs-infra][draft] Test Full width demo

--- docs/pages/x/react-data-grid/pivoting.js
+++ docs/pages/x/react-data-grid/pivoting.js
@@ -3,5 +3,31 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 import * as pageProps from 'docsx/data/data-grid/pivoting/pivoting.md?muiMarkdown';
 
 export default function Page() {
-  return <MarkdownDocs {...pageProps} disableAd />;
+  return (
+    <MarkdownDocs
+      {...pageProps}
+      disableAd
+      sx={{
+        '& .MuiDocs-footer-block': {
+          // marginLeft: 'auto',
+          // marginRight: 'auto',
+          // maxWidth: 'var(--MuiDocs-text-width)',
+        },
+        // '& .MuiDocs-content-block.MuiDocs-api-content-block': {
+        //   marginLeft: 0,
+        //   marginRight: 'auto',
+        //   maxWidth: 'calc(2 * var(--MuiDocs-text-width))',
+        // },
+        '& .MuiDocs-content-block': {
+          marginLeft: 0,
+          marginRight: 'auto',
+          maxWidth: 'var(--MuiDocs-text-width)',
+        },
+        '& .MuiDocs-content-block.MuiDocs-demo-block': {
+          maxWidth: 'unset',
+        },
+        //
+      }}
+    />
+  );
 }
--- docs/pages/x/react-scheduler/index.js
+++ docs/pages/x/react-scheduler/index.js
@@ -3,5 +3,30 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 import * as pageProps from 'docsx/data/scheduler/overview/overview.md?muiMarkdown';
 
 export default function Page() {
-  return <MarkdownDocs {...pageProps} />;
+  return (
+    <MarkdownDocs
+      {...pageProps}
+      sx={{
+        '& .MuiDocs-footer-block': {
+          // marginLeft: 'auto',
+          // marginRight: 'auto',
+          // maxWidth: 'var(--MuiDocs-text-width)',
+        },
+        // '& .MuiDocs-content-block.MuiDocs-api-content-block': {
+        //   marginLeft: 0,
+        //   marginRight: 'auto',
+        //   maxWidth: 'calc(2 * var(--MuiDocs-text-width))',
+        // },
+        '& .MuiDocs-content-block': {
+          marginLeft: 0,
+          marginRight: 'auto',
+          maxWidth: 'var(--MuiDocs-text-width)',
+        },
+        '& .MuiDocs-content-block.MuiDocs-demo-block': {
+          maxWidth: 'unset',
+        },
+        //
+      }}
+    />
+  );
 }
