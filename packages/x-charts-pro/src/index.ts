d4a3325689bd7237aecc54653647d0bb9a6f58f3
Armin Mehinovic <armin@mui.com>
2025-05-27 12:06:54 +0200

    Fix export

--- packages/x-charts-pro/src/index.ts
+++ packages/x-charts-pro/src/index.ts
@@ -48,4 +48,4 @@ export * from './FunnelChart';
 export * from './RadarChartPro';
 export * from './ChartZoomSlider';
 export * from './ChartsToolbarPro';
-export * from './ChartRenderer';
+export * from './ChartsRenderer';
