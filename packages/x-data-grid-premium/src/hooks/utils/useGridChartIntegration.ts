69e2d4f6b73cb74c8b8b6b02d846cd6f40ed2bd4
Armin Mehinovic <armin@mui.com>
2025-05-27 11:27:03 +0200

    make integration optional

--- packages/x-data-grid-premium/src/hooks/utils/useGridChartIntegration.ts
+++ packages/x-data-grid-premium/src/hooks/utils/useGridChartIntegration.ts
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { GridChartsIntegrationContext } from '../../components/chartsIntegration/GridChartsIntegrationContext';
 import { GridChartsIntegrationContextValue } from '../../models/gridChartsIntegration';
 
-export const useGridChartsIntegrationContext = () => {
+export const useGridChartsIntegrationContext = (ignoreError = false) => {
   const context = React.useContext(GridChartsIntegrationContext);
-  if (!context) {
+  if (!context && !ignoreError) {
     throw new Error(
       [
         'MUI X: Could not find the Data Grid charts integration context.',
