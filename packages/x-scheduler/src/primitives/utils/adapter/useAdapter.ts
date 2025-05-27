c5ac8196b943625478a808f38f0616517db54361
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-05-27 11:57:56 +0200

    avoid memoization issues

--- packages/x-scheduler/src/primitives/utils/adapter/useAdapter.ts
+++ packages/x-scheduler/src/primitives/utils/adapter/useAdapter.ts
@@ -2,6 +2,8 @@ import { getAdapter } from './getAdapter';
 
 export function useAdapter() {
   // Temporary hook that mimics the behavior of a future `useAdapter` hook,
-  // to ease migration once the provider setup is ready.
-  return getAdapter();
+  // to ease migration once (the provider setup is ready.
+  const adapter = getAdapter();
+
+  return adapter;
 }
