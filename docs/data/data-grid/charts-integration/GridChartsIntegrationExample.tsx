5cad79a8dce82ca8ba58e5d541e145b9d15c4a2f
Armin Mehinovic <armin@mui.com>
2025-05-27 11:59:21 +0200

    adjust example

--- docs/data/data-grid/charts-integration/GridChartsIntegrationExample.tsx
+++ docs/data/data-grid/charts-integration/GridChartsIntegrationExample.tsx
@@ -40,8 +40,8 @@ const columns: GridColDef[] = [
 export default function GridChartsIntegrationExample() {
   return (
     <GridChartsIntegrationContextProvider>
-      <div style={{ gap: 32 }}>
-        <div style={{ height: 400 }}>
+      <div style={{ gap: 32, width: '100%' }}>
+        <div style={{ height: 370 }}>
           <DataGridPremium
             rows={rows}
             columns={columns}
