9bd2cb7199831d5c610a95748ab81db542b8691d
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-05-28 16:05:25 +0200

    fix conflict

--- docs/data/scheduler/event-calendar/FullEventCalendar.js
+++ docs/data/scheduler/event-calendar/FullEventCalendar.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { DateTime } from 'luxon';
 import { EventCalendar } from '@mui/x-scheduler/joy/event-calendar';
 
-import classes from './BasicEventCalendar.module.css';
+import classes from './FullEventCalendar.module.css';
 
 const events = [
   {
