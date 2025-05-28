b90517514e749e5753b24d8274159838452eaac7
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-05-28 10:34:46 +0200

    make layout more flexible

--- docs/data/scheduler/overview/BasicEventCalendar.tsx
+++ docs/data/scheduler/overview/BasicEventCalendar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { DateTime } from 'luxon';
 import { EventCalendar } from '@mui/x-scheduler/joy/event-calendar';
 import { CalendarEvent } from '@mui/x-scheduler/joy/models/events';
+import classes from './BasicEventCalendar.module.css';
 
 const events: CalendarEvent[] = [
   {
@@ -49,5 +50,5 @@ const events: CalendarEvent[] = [
 ];
 
 export default function BasicEventCalendar() {
-  return <EventCalendar events={events} />;
+  return <EventCalendar events={events} className={classes.Container} />;
 }
