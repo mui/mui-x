e8fe0d9ca9f79c1a017f84b3663cf272d9583c9e
Bernardo Belchior <bernardo@mui.com>
2025-05-29 16:06:14 +0200

    Fix lint

--- packages/x-charts/src/Toolbar/Toolbar.types.ts
+++ packages/x-charts/src/Toolbar/Toolbar.types.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ChartsToolbarProps } from './Toolbar';
+import { ChartsToolbarProps } from './internals/ChartsToolbar';
 
 export interface ChartsToolbarSlots {
   /**
