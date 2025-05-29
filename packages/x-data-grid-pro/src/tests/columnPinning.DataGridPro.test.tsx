47a7d9fc3470a53f692f7b0009a5177dd4c04af7
Ali Sasani <a.sasani.b@gmail.com>
2025-05-29 08:43:57 +0100

    [DataGridPro] Fix pinned columns order in column management (#17950)
    Co-authored-by: Armin Mehinovic <armin@mui.com>

--- packages/x-data-grid-pro/src/tests/columnPinning.DataGridPro.test.tsx
+++ packages/x-data-grid-pro/src/tests/columnPinning.DataGridPro.test.tsx
@@ -721,4 +721,104 @@ describe('<DataGridPro /> - Column pinning', () => {
       expect(ageCellColumnGroupHeader.textContent).to.equal('Basic info');
     });
   });
+
+  describe('pinned columns order in column management', () => {
+    it('should keep pinned column order in column management panel when toggling columns', async () => {
+      const { user } = render(
+        <TestCase
+          rows={[{ id: 1, brand: 'Nike' }]}
+          columns={[{ field: 'id' }, { field: 'brand' }]}
+          showToolbar
+          initialState={{
+            pinnedColumns: {
+              left: ['brand'],
+            },
+          }}
+        />,
+      );
+
+      await user.click(screen.getByRole('button', { name: 'Columns' }));
+
+      const columnCheckboxes = screen.getAllByRole('checkbox');
+
+      expect(columnCheckboxes[0]).to.have.attribute('name', 'brand');
+      expect(columnCheckboxes[1]).to.have.attribute('name', 'id');
+
+      await user.click(columnCheckboxes[1]);
+
+      await user.click(screen.getByRole('button', { name: 'Columns' }));
+      await user.click(screen.getByRole('button', { name: 'Columns' }));
+
+      const checkboxesAfterToggle = screen.getAllByRole('checkbox');
+
+      expect(checkboxesAfterToggle[0]).to.have.attribute('name', 'brand');
+      expect(checkboxesAfterToggle[1]).to.have.attribute('name', 'id');
+    });
+
+    it('should keep pinned column order in column management panel when clicking show/hide all checkbox', async () => {
+      const { user } = render(
+        <TestCase
+          rows={[{ id: 0, brand: 'Nike' }]}
+          columns={[{ field: 'id' }, { field: 'brand' }]}
+          showToolbar
+          initialState={{
+            pinnedColumns: {
+              left: ['brand'],
+            },
+          }}
+        />,
+      );
+
+      await user.click(screen.getByRole('button', { name: 'Columns' }));
+
+      const columnCheckboxes = screen.getAllByRole('checkbox');
+
+      expect(columnCheckboxes[0]).to.have.attribute('name', 'brand');
+      expect(columnCheckboxes[1]).to.have.attribute('name', 'id');
+
+      await user.click(columnCheckboxes[columnCheckboxes.length - 1]);
+
+      await user.click(screen.getByRole('button', { name: 'Columns' }));
+      await user.click(screen.getByRole('button', { name: 'Columns' }));
+
+      const checkboxesAfterToggle = screen.getAllByRole('checkbox');
+      expect(checkboxesAfterToggle[0]).to.have.attribute('name', 'brand');
+      expect(checkboxesAfterToggle[1]).to.have.attribute('name', 'id');
+    });
+
+    it('should update column order when pinned columns are updated', async () => {
+      const { user } = render(
+        <TestCase
+          rows={[{ id: 0, brand: 'Nike', price: 100 }]}
+          columns={[{ field: 'id' }, { field: 'brand' }, { field: 'price' }]}
+          showToolbar
+          initialState={{
+            pinnedColumns: {},
+            columns: {
+              columnVisibilityModel: { id: false },
+            },
+          }}
+        />,
+      );
+
+      await user.click(screen.getByRole('button', { name: 'Columns' }));
+
+      const columnCheckboxes = screen.getAllByRole('checkbox');
+
+      expect(columnCheckboxes[0]).to.have.attribute('name', 'id');
+      expect(columnCheckboxes[1]).to.have.attribute('name', 'brand');
+      expect(columnCheckboxes[2]).to.have.attribute('name', 'price');
+
+      await act(() => {
+        apiRef.current?.pinColumn('brand', GridPinnedColumnPosition.LEFT);
+        apiRef.current?.pinColumn('id', GridPinnedColumnPosition.RIGHT);
+      });
+
+      const checkboxesAfterPinning = screen.getAllByRole('checkbox');
+
+      expect(checkboxesAfterPinning[0]).to.have.attribute('name', 'brand');
+      expect(checkboxesAfterPinning[1]).to.have.attribute('name', 'price');
+      expect(checkboxesAfterPinning[2]).to.have.attribute('name', 'id');
+    });
+  });
 });
