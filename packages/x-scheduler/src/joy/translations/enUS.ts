a138864e4ae936d4eab018a58f2ef89e6ade0038
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-06-02 10:02:21 +0200

    add comments on translations files

--- packages/x-scheduler/src/joy/translations/enUS.ts
+++ packages/x-scheduler/src/joy/translations/enUS.ts
@@ -1,11 +1,14 @@
 import { SchedulerTranslations } from '../models/translations';
 
 export const enUS: SchedulerTranslations = {
+  // View navigation
   agenda: 'Agenda',
-  allDay: 'All day',
   day: 'Day',
   month: 'Month',
   other: 'Other',
   today: 'Today',
   week: 'Week',
+
+  // Week view
+  allDay: 'All day',
 };
--- packages/x-scheduler/src/joy/translations/frFR.ts
+++ packages/x-scheduler/src/joy/translations/frFR.ts
@@ -1,11 +1,14 @@
 import { SchedulerTranslations } from '../models/translations';
 
 export const frFR: Partial<SchedulerTranslations> = {
+  // View navigation
   agenda: 'Agenda',
-  allDay: 'Toute la journée',
   day: 'Jour',
   month: 'Mois',
   other: 'Autre',
   today: "Aujourd'hui",
   week: 'Semaine',
+
+  // Week view
+  allDay: 'Toute la journée',
 };
