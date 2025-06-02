2b7881edf06f6e1cc3c008d130250d66a4f4d86f
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2025-06-02 10:24:45 +0100

    improve shadow styles

--- packages/x-data-grid/src/components/GridScrollShadows.tsx
+++ packages/x-data-grid/src/components/GridScrollShadows.tsx
@@ -22,11 +22,17 @@ type OwnerState = Pick<DataGridProcessedProps, 'classes'> & {
   position: 'vertical' | 'horizontal';
 };
 
-const ScrollShadow = styled('div')<{ ownerState: OwnerState }>({
+const ScrollShadow = styled('div')<{ ownerState: OwnerState }>(({ theme }) => ({
   position: 'absolute',
   inset: 0,
   pointerEvents: 'none',
   transition: vars.transition(['box-shadow'], { duration: vars.transitions.duration.short }),
+  '--size': theme.palette.mode === 'dark' ? '10px' : '5px',
+  '--opacity': theme.palette.mode === 'dark' ? '0.6' : '0.18',
+  '--blur': 'var(--size)',
+  '--spread': 'calc(var(--size) * -1)',
+  '--color-start': 'rgba(0, 0, 0, calc(var(--hasScrollStart) * var(--opacity)))',
+  '--color-end': 'rgba(0, 0, 0, calc(var(--hasScrollEnd) * var(--opacity)))',
   variants: [
     {
       props: { position: 'vertical' },
@@ -35,7 +41,7 @@ const ScrollShadow = styled('div')<{ ownerState: OwnerState }>({
         bottom:
           'calc(var(--DataGrid-bottomContainerHeight) + var(--DataGrid-hasScrollX) * var(--DataGrid-scrollbarSize))',
         boxShadow:
-          'inset 0 5px 5px -5px rgba(0,0,0,calc(var(--hasScrollStart) * .18)), inset 0 -5px 5px -5px rgba(0,0,0,calc(var(--hasScrollEnd) * .18))',
+          'inset 0 var(--size) var(--blur) var(--spread) var(--color-start), inset 0 var(--spread) var(--blur) var(--spread) var(--color-end)',
       },
     },
     {
@@ -45,11 +51,11 @@ const ScrollShadow = styled('div')<{ ownerState: OwnerState }>({
         right:
           'calc(var(--DataGrid-rightPinnedWidth) + var(--DataGrid-hasScrollY) * var(--DataGrid-scrollbarSize))',
         boxShadow:
-          'inset 5px 0 5px -5px rgba(0,0,0,calc(var(--hasScrollStart) * .18)), inset -5px 0 5px -5px rgba(0,0,0,calc(var(--hasScrollEnd) * .18))',
+          'inset var(--size) 0 var(--blur) var(--spread) var(--color-start), inset var(--spread) 0 var(--blur) var(--spread) var(--color-end)',
       },
     },
   ],
-});
+}));
 
 function GridScrollShadows(props: GridScrollShadowsProps) {
   const { position } = props;
