bc792f89f43b9ea93e44d6a6fb8b816e8562c483
Bernardo Belchior <bernardo@mui.com>
2025-06-02 12:43:05 +0200

    Use any

--- packages/x-charts/src/internals/defaultValueFormatters.ts
+++ packages/x-charts/src/internals/defaultValueFormatters.ts
@@ -1,18 +1,18 @@
-import { AxisValueFormatterContext, ContinuousScaleName } from '../models/axis';
+import { AxisValueFormatterContext, ScaleName } from '../models/axis';
 
 /**
  * Creates a default formatter function for continuous scales (e.g., linear, sqrt, log).
  * @returns A formatter function for continuous values.
  */
-export function createScalarFormatter<
-  S extends ContinuousScaleName = ContinuousScaleName,
-  V extends any = any,
->(tickNumber: number) {
-  return function defaultScalarValueFormatter(
-    value: V,
+export function createScalarFormatter(tickNumber: number) {
+  return function defaultScalarValueFormatter<S extends ScaleName = ScaleName>(
+    value: any,
     context: AxisValueFormatterContext<S>,
   ): string {
-    if (context.location === 'tick' || context.location === 'zoom-slider-tooltip') {
+    if (
+      (context.location === 'tick' || context.location === 'zoom-slider-tooltip') &&
+      'tickFormat' in context.scale
+    ) {
       return context.scale.tickFormat(tickNumber)(value);
     }
 
