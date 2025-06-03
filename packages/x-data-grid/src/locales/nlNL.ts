581408c0b09c72aebba761817620f21c0baf2560
Armin Mehinovic <armin@mui.com>
2025-06-03 10:57:10 +0200

    l10n

--- packages/x-data-grid/src/locales/nlNL.ts
+++ packages/x-data-grid/src/locales/nlNL.ts
@@ -42,6 +42,7 @@ const nlNLGrid: Partial<GridLocaleText> = {
 
   // Toolbar pivot button
   // toolbarPivot: 'Pivot',
+  // toolbarCharts: 'Configure charts',
 
   // Toolbar AI Assistant button
   // toolbarAssistant: 'AI Assistant',
