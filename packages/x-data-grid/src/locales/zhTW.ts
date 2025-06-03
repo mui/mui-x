eed4a492f641425a22e37afd391be414b4b33a60
Armin Mehinovic <armin@mui.com>
2025-06-03 10:57:10 +0200

    l10n

--- packages/x-data-grid/src/locales/zhTW.ts
+++ packages/x-data-grid/src/locales/zhTW.ts
@@ -41,6 +41,7 @@ const zhTWGrid: Partial<GridLocaleText> = {
 
   // Toolbar pivot button
   // toolbarPivot: 'Pivot',
+  // toolbarCharts: 'Configure charts',
 
   // Toolbar AI Assistant button
   // toolbarAssistant: 'AI Assistant',
