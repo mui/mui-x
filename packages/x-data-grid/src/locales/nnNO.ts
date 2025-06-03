5368e75e45ec340eca4c9f8432f1058a04abacaa
Armin Mehinovic <armin@mui.com>
2025-06-03 10:57:10 +0200

    l10n

--- packages/x-data-grid/src/locales/nnNO.ts
+++ packages/x-data-grid/src/locales/nnNO.ts
@@ -42,6 +42,7 @@ const nnNOGrid: Partial<GridLocaleText> = {
 
   // Toolbar pivot button
   // toolbarPivot: 'Pivot',
+  // toolbarCharts: 'Configure charts',
 
   // Toolbar AI Assistant button
   // toolbarAssistant: 'AI Assistant',
