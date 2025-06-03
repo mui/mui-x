7da5bffbefed018cb8e528f53106135ec5640dd9
Armin Mehinovic <armin@mui.com>
2025-06-03 10:57:10 +0200

    l10n

--- packages/x-data-grid/src/locales/csCZ.ts
+++ packages/x-data-grid/src/locales/csCZ.ts
@@ -49,6 +49,7 @@ const csCZGrid: Partial<GridLocaleText> = {
 
   // Toolbar pivot button
   // toolbarPivot: 'Pivot',
+  // toolbarCharts: 'Configure charts',
 
   // Toolbar AI Assistant button
   // toolbarAssistant: 'AI Assistant',
