a907f466ec0d2402b2d40affa15a4a94d7f70cf9
Armin Mehinovic <armin@mui.com>
2025-06-03 10:57:10 +0200

    l10n

--- packages/x-data-grid/src/locales/bgBG.ts
+++ packages/x-data-grid/src/locales/bgBG.ts
@@ -41,6 +41,7 @@ const bgBGGrid: Partial<GridLocaleText> = {
 
   // Toolbar pivot button
   // toolbarPivot: 'Pivot',
+  // toolbarCharts: 'Configure charts',
 
   // Toolbar AI Assistant button
   // toolbarAssistant: 'AI Assistant',
