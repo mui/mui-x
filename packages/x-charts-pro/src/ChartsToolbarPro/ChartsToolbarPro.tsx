8ea9035796860b089c0e40a6f83986749695ebc0
Bernardo Belchior <bernardo@mui.com>
2025-06-04 09:28:00 +0200

    Handle esc and tab on export menu

--- packages/x-charts-pro/src/ChartsToolbarPro/ChartsToolbarPro.tsx
+++ packages/x-charts-pro/src/ChartsToolbarPro/ChartsToolbarPro.tsx
@@ -78,6 +78,16 @@ export function ChartsToolbarPro({
 
     const closeExportMenu = () => setExportMenuOpen(false);
 
+    const handleListKeyDown = (event: React.KeyboardEvent) => {
+      if (event.key === 'Tab') {
+        event.preventDefault();
+      }
+
+      if (isHideMenuKey(event.key)) {
+        closeExportMenu();
+      }
+    };
+
     children.push(
       <React.Fragment key="export-menu">
         <Tooltip title={localeText.toolbarExport}>
@@ -102,6 +112,7 @@ export function ChartsToolbarPro({
           <MenuList
             id={exportMenuId}
             aria-labelledby={exportMenuTriggerId}
+            onKeyDown={handleListKeyDown}
             autoFocusItem
             {...slotProps?.baseMenuList}
           >
@@ -134,4 +145,8 @@ export function ChartsToolbarPro({
   }
 
   return <Toolbar {...other}>{children}</Toolbar>;
+}
+
+function isHideMenuKey(key: React.KeyboardEvent['key']) {
+  return key === 'Tab' || key === 'Escape';
 }
