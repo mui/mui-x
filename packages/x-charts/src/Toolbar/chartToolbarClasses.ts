004b45834563b133ec93108f4b7ba2cb94a7b4ce
Bernardo Belchior <bernardo@mui.com>
2025-06-04 16:56:45 +0200

    Remove unused class

--- packages/x-charts/src/Toolbar/chartToolbarClasses.ts
+++ packages/x-charts/src/Toolbar/chartToolbarClasses.ts
@@ -3,13 +3,11 @@ import generateUtilityClasses from '@mui/utils/generateUtilityClasses';
 export interface ChartsToolbarClasses {
   /** Styles applied to the root element. */
   root: string;
-  /** Styles applied to the toolbar divider element. */
-  divider: string;
 }
 
 export type ChartsToolbarClassKey = keyof ChartsToolbarClasses;
 
 export const chartsToolbarClasses: ChartsToolbarClasses = generateUtilityClasses(
   'MuiChartsToolbar',
-  ['root', 'divider'],
+  ['root'],
 );
