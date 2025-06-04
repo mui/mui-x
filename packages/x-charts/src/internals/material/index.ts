2ed07101f8ccf47b4df757fe8893ff34fa561320
Bernardo Belchior <bernardo@mui.com>
2025-06-04 16:06:44 +0200

    Fix build

--- packages/x-charts/src/internals/material/index.ts
+++ packages/x-charts/src/internals/material/index.ts
@@ -1,14 +1,12 @@
 import type * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
 import { ChartsBaseSlots } from '../../models/slots/chartsBaseSlots';
 import { ChartsIconSlots } from '../../models/slots/chartsIconSlots';
 
 const baseSlots: ChartsBaseSlots = {
   baseButton: Button,
   baseIconButton: IconButton,
-  baseDivider: Divider,
 };
 
 const iconSlots: ChartsIconSlots = {};
