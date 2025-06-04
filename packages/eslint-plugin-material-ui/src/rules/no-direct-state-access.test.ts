79611bacbab4891da0673761f41c92e41abbdd5d
Jose Quintas <juniorquintas@gmail.com>
2025-06-04 10:20:28 +0200

    one-liner test names

--- packages/eslint-plugin-material-ui/src/rules/no-direct-state-access.test.ts
+++ packages/eslint-plugin-material-ui/src/rules/no-direct-state-access.test.ts
@@ -21,24 +21,34 @@ const ruleTester = new RuleTester({
 
 ruleTester.run('no-direct-state-access', rule, {
   valid: [
-    `
+    {
+      name: 'assigning received grid state to a variable',
+      code: `
 const useCustomHook = (apiRef: GridApiRef) => {
   const state = apiRef.current.state;
 }
     `,
-    `
+    },
+    {
+      name: 'accessing any state directly',
+      code: `
 const useCustomHook = (api: any) => {
   const rows = api.current.state.rows;
 }
     `,
-    `
+    },
+    {
+      name: 'passing any state to a function',
+      code: `
 const useCustomHook = (api: any) => {
   const rows = gridRowsSelector(api.current.state);
 }
     `,
+    },
   ],
   invalid: [
     {
+      name: 'directly accessing variable inside received grid state',
       code: `
 type GridApiRef = React.MutableRefObject<any>;
 const useCustomHook = (apiRef: GridApiRef) => {
@@ -48,6 +58,7 @@ const useCustomHook = (apiRef: GridApiRef) => {
       errors: [{ messageId: 'direct-access', line: 4, column: 16 }],
     },
     {
+      name: 'directly accessing variable inside grid state from the hook context',
       code: `
 type GridApiRef = React.MutableRefObject<any>;
 const useGridApiContext = (): GridApiRef => { return {} };
@@ -59,6 +70,7 @@ const useCustomHook = () => {
       errors: [{ messageId: 'direct-access', line: 6, column: 16 }],
     },
     {
+      name: 'destructuring variable from grid state',
       code: `
 type GridApiRef = React.MutableRefObject<any>;
 const useGridApiContext = (): GridApiRef => { return {} };
