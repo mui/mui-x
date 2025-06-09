c1091e0ce02c3d1e75f7da13bb69dc730791cd4e
Armin Mehinovic <armin@mui.com>
2025-06-09 09:13:56 +0200

    update locale text

--- packages/x-data-grid/src/locales/nbNO.ts
+++ packages/x-data-grid/src/locales/nbNO.ts
@@ -42,7 +42,7 @@ const nbNOGrid: Partial<GridLocaleText> = {
 
   // Toolbar pivot button
   // toolbarPivot: 'Pivot',
-  // toolbarCharts: 'Configure charts',
+  // toolbarCharts: 'Charts',
 
   // Toolbar AI Assistant button
   // toolbarAssistant: 'AI Assistant',
