753e67bc1dfeccc51fca9bb2fae0388046ab5b70
Armin Mehinovic <armin@mui.com>
2025-06-10 14:13:11 +0200

    Render section in the config

--- packages/x-data-grid-premium/src/components/chartsConfigurationPanel/GridChartsConfigurationForm.tsx
+++ packages/x-data-grid-premium/src/components/chartsConfigurationPanel/GridChartsConfigurationForm.tsx
@@ -1,27 +1,31 @@
 import * as React from 'react';
 import { styled } from '@mui/system';
-import { NotRendered } from '@mui/x-data-grid-pro/internals';
-import { GridSlotProps } from '@mui/x-data-grid-pro';
 import { useGridRootProps } from '../../hooks/utils/useGridRootProps';
 import { useGridChartsIntegrationContext } from '../../hooks/utils/useGridChartIntegration';
-import { DataGridPremiumProcessedProps } from '../../models/dataGridPremiumProps';
 import { GridChartsConfigurationSection } from '../../models/gridChartsIntegration';
+import { Collapsible } from '../collapsible/Collapsible';
+import { CollapsibleTrigger } from '../collapsible/CollapsibleTrigger';
+import { CollapsiblePanel } from '../collapsible/CollapsiblePanel';
+import type { DataGridPremiumProcessedProps } from '../../models/dataGridPremiumProps';
 
 type OwnerState = DataGridPremiumProcessedProps;
 
-const GridChartsConfigSwitch = styled(NotRendered<GridSlotProps['baseSwitch']>, {
-  name: 'MuiDataGrid',
-  slot: 'ChartsConfigurationForm',
-})<{ ownerState: OwnerState }>({
-  overflow: 'visible',
-});
-
-const GirdChartsConfigurationForm = styled('form')({
+const GridChartsConfigurationFormRoot = styled('div')({
   display: 'flex',
   flexDirection: 'column',
-  gap: 16,
-  padding: 16,
+  gap: 4,
+  padding: 8,
   overflowY: 'auto',
+});
+
+const GridChartsConfigurationFormPanel = styled(CollapsiblePanel, {
+  name: 'MuiDataGrid',
+  slot: 'ChartsConfigurationPanelSection',
+})<{ ownerState: OwnerState }>({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: 16,
+  gap: 16,
 });
 
 export function GridChartsConfigurationForm() {
@@ -29,7 +33,6 @@ export function GridChartsConfigurationForm() {
   const { chartType, configuration, categories, series, setConfiguration } =
     useGridChartsIntegrationContext();
 
-  // TODO: take sections into account when rendering the form
   const sections: GridChartsConfigurationSection[] = React.useMemo(() => {
     return rootProps.slotProps?.chartsConfigurationPanel?.schema?.[chartType]?.customization || [];
   }, [rootProps.slotProps?.chartsConfigurationPanel?.schema, chartType]);
@@ -44,93 +47,93 @@ export function GridChartsConfigurationForm() {
   }
 
   return (
-    <GirdChartsConfigurationForm>
-      {sections.map((section) =>
-        Object.entries(section.controls).map(([key, optRaw]) => {
-          const opt = optRaw as any;
-
-          const context = { configuration, categories, series };
-          const isHidden = opt.isHidden?.(context) ?? false;
-          if (isHidden) {
-            return null;
-          }
-
-          const isDisabled = opt.isDisabled?.(context) ?? false;
-
-          if (opt.type === 'boolean') {
-            return (
-              <GridChartsConfigSwitch
-                key={key}
-                as={rootProps.slots.baseSwitch}
-                ownerState={rootProps}
-                checked={!!configuration[key]}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                  handleChange(key, event.target.checked)
-                }
-                size="small"
-                label={opt.label}
-                disabled={isDisabled}
-                {...rootProps.slotProps?.baseSwitch}
-              />
-            );
-          }
-          if (opt.type === 'select') {
-            return (
-              <rootProps.slots.baseSelect
-                key={key}
-                fullWidth
-                label={opt.label}
-                value={configuration[key] ?? opt.default}
-                onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
-                  handleChange(key, event.target.value)
-                }
-                disabled={isDisabled}
-                slotProps={{
-                  htmlInput: {
-                    ...opt.htmlAttributes,
-                  },
-                }}
-                {...rootProps.slotProps?.baseSelect}
-              >
-                {(opt.options || []).map((option: { label: string; value: string }) => (
-                  <rootProps.slots.baseSelectOption
-                    key={option.value}
-                    value={option.value}
-                    native={false}
-                  >
-                    {option.label}
-                  </rootProps.slots.baseSelectOption>
-                ))}
-              </rootProps.slots.baseSelect>
-            );
-          }
-          // string or number
-          return (
-            <rootProps.slots.baseTextField
-              key={key}
-              aria-label={opt.label}
-              placeholder={opt.label}
-              label={opt.label}
-              type={opt.type === 'number' ? 'number' : 'text'}
-              fullWidth
-              disabled={isDisabled}
-              slotProps={{
-                htmlInput: {
-                  ...opt.htmlAttributes,
-                },
-              }}
-              {...rootProps.slotProps?.baseTextField}
-              value={configuration[key] ?? opt.default}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                handleChange(
-                  key,
-                  opt.type === 'number' ? Number(event.target.value) : event.target.value,
-                )
+    <GridChartsConfigurationFormRoot>
+      {sections.map((section, index) => (
+        <Collapsible key={section.id} initiallyOpen={index === 0}>
+          <CollapsibleTrigger>{section.label}</CollapsibleTrigger>
+          <GridChartsConfigurationFormPanel ownerState={rootProps}>
+            {Object.entries(section.controls).map(([key, optRaw]) => {
+              const opt = optRaw as any;
+              const context = { configuration, categories, series };
+              const isHidden = opt.isHidden?.(context) ?? false;
+              if (isHidden) {
+                return null;
               }
-            />
-          );
-        }),
-      )}
-    </GirdChartsConfigurationForm>
+              const isDisabled = opt.isDisabled?.(context) ?? false;
+              if (opt.type === 'boolean') {
+                return (
+                  <rootProps.slots.baseSwitch
+                    key={key}
+                    checked={!!configuration[key]}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      handleChange(key, event.target.checked)
+                    }
+                    size="small"
+                    label={opt.label}
+                    disabled={isDisabled}
+                    {...rootProps.slotProps?.baseSwitch}
+                  />
+                );
+              }
+              if (opt.type === 'select') {
+                return (
+                  <rootProps.slots.baseSelect
+                    key={key}
+                    fullWidth
+                    label={opt.label}
+                    value={configuration[key] ?? opt.default}
+                    onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+                      handleChange(key, event.target.value)
+                    }
+                    disabled={isDisabled}
+                    slotProps={{
+                      htmlInput: {
+                        ...opt.htmlAttributes,
+                      },
+                    }}
+                    {...rootProps.slotProps?.baseSelect}
+                  >
+                    {(opt.options || []).map((option: { label: string; value: string }) => (
+                      <rootProps.slots.baseSelectOption
+                        key={option.value}
+                        value={option.value}
+                        native={false}
+                      >
+                        {option.label}
+                      </rootProps.slots.baseSelectOption>
+                    ))}
+                  </rootProps.slots.baseSelect>
+                );
+              }
+              // string or number
+              return (
+                <rootProps.slots.baseTextField
+                  key={key}
+                  aria-label={opt.label}
+                  placeholder={opt.label}
+                  label={opt.label}
+                  type={opt.type === 'number' ? 'number' : 'text'}
+                  fullWidth
+                  disabled={isDisabled}
+                  slotProps={{
+                    htmlInput: {
+                      ...opt.htmlAttributes,
+                    },
+                  }}
+                  {...rootProps.slotProps?.baseTextField}
+                  value={configuration[key] ?? opt.default}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleChange(
+                      key,
+                      opt.type === 'number' ? Number(event.target.value) : event.target.value,
+                    )
+                  }
+                />
+              );
+            })}
+          </GridChartsConfigurationFormPanel>
+        </Collapsible>
+      ))}
+    </GridChartsConfigurationFormRoot>
   );
 }
