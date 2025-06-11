450590bc3f55d5aec2914ae966e23a7031cdaa15
Jose Quintas <juniorquintas@gmail.com>
2025-06-11 14:55:52 +0200

    setup files

--- scripts/buildApiDocs/chartsSettings/getComponentInfo.ts
+++ scripts/buildApiDocs/chartsSettings/getComponentInfo.ts
@@ -77,6 +77,7 @@ export function getComponentImports(name: string, filename: string) {
 
   if (rootImportPath === '@mui/x-charts') {
     reExportPackage.push('@mui/x-charts-pro');
+    reExportPackage.push('@mui/x-charts-premium');
   }
 
   return [
