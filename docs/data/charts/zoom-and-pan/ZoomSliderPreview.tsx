986484a395d3031081bf86f449366416ff51cd4e
Bernardo Belchior <bernardo@mui.com>
2025-06-12 10:07:32 +0200

    Add US Unemployment Rate chart

--- docs/data/charts/zoom-and-pan/ZoomSliderPreview.tsx
+++ docs/data/charts/zoom-and-pan/ZoomSliderPreview.tsx
@@ -1,60 +1,74 @@
-import { ScatterChartPro } from '@mui/x-charts-pro/ScatterChartPro';
 import * as React from 'react';
+import { LineChartPro } from '@mui/x-charts-pro/LineChartPro';
+import { XAxis } from '@mui/x-charts/models';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import { usUnemploymentRate } from '../dataset/usUnemploymentRate';
 
-const dataLength = 801;
-const data = Array.from({ length: dataLength }).map((_, i) => ({
-  x: i,
-  y: 50 + Math.sin(i / 5) * 25,
-}));
-const series2Data = Array.from({ length: dataLength }).map((_, i) => ({
-  x: i,
-  y: 50 + Math.sin(i / 10) * 25,
-}));
+const data = usUnemploymentRate.map((d) => d.rate / 100);
 
-const xData = data.map((d) => d.x);
+const percentageFormatter = new Intl.NumberFormat(undefined, {
+  style: 'percent',
+  minimumSignificantDigits: 1,
+  maximumSignificantDigits: 3,
+});
+
+const xAxis = {
+  scaleType: 'time',
+  id: 'x',
+  data: usUnemploymentRate.map((d) => d.date),
+  valueFormatter: (v: Date, context) =>
+    v.toLocaleDateString(undefined, {
+      month: context.location === 'tick' ? undefined : 'short',
+      year: 'numeric',
+    }),
+  zoom: {
+    filterMode: 'discard',
+    minSpan: 10,
+    panning: true,
+    slider: { enabled: true, preview: true },
+  },
+} satisfies XAxis;
+
+const settings = {
+  yAxis: [
+    {
+      id: 'y',
+      width: 48,
+      valueFormatter: (v: number | null) => percentageFormatter.format(v!),
+      min: 0,
+    },
+  ],
+  series: [
+    {
+      data,
+      showMark: false,
+      valueFormatter: (v: number | null) => percentageFormatter.format(v!),
+    },
+  ],
+  height: 400,
+} as const;
 
 export default function ZoomSliderPreview() {
   return (
-    <ScatterChartPro
-      xAxis={[
-        {
-          id: 'x',
-          data: xData,
-          zoom: {
-            filterMode: 'discard',
-            minSpan: 10,
-            panning: true,
-            slider: { enabled: true, preview: true },
+    <Stack width="100%">
+      <Typography variant="h6" sx={{ alignSelf: 'center' }}>
+        United States Unemployment Rate (%)
+      </Typography>
+      <LineChartPro
+        {...settings}
+        xAxis={[
+          {
+            ...xAxis,
+            zoom: {
+              slider: { enabled: true, preview: true },
+            },
           },
-          valueFormatter: (v: number) => v.toLocaleString('en-US'),
-        },
-        {
-          id: 'x2',
-          data: series2Data.map((d) => d.x),
-          position: 'top',
-          zoom: {
-            slider: { enabled: true },
-          },
-        },
-      ]}
-      yAxis={[
-        { id: 'y', width: 28, zoom: { slider: { enabled: true } } },
-        {
-          id: 'y2',
-          position: 'right',
-          width: 28,
-          zoom: { slider: { enabled: true } },
-        },
-      ]}
-      series={[{ data }, { data: series2Data, xAxisId: 'x2', yAxisId: 'y2' }]}
-      height={400}
-      margin={{ bottom: 40 }}
-      initialZoom={[
-        { axisId: 'x', start: 45, end: 55 },
-        { axisId: 'x2', start: 30, end: 70 },
-        { axisId: 'y', start: 40, end: 60 },
-        { axisId: 'y2', start: 30, end: 70 },
-      ]}
-    />
+        ]}
+      />
+      <Typography variant="caption">
+        Source: Federal Reserve Bank of St. Louis. Updated: Jun 6, 2025 7:46 AM CDT.
+      </Typography>
+    </Stack>
   );
 }
