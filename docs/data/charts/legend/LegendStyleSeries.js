05c26b373113d9ff8451ab514ae17569af3c0e81
Bernardo Belchior <bernardo@mui.com>
2025-06-17 17:49:54 +0100

    Add docs

---
+++ docs/data/charts/legend/LegendStyleSeries.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+import { legendClasses } from '@mui/x-charts/ChartsLegend';
+
+const highlightScope = { highlight: 'series', fade: 'global' };
+const settings = {
+  series: [
+    { id: '0', data: [10, 15], label: 'Series A', highlightScope },
+    { id: '1', data: [15, 20], label: 'Series B', highlightScope },
+    { id: '2', data: [20, 25], label: 'Series C', highlightScope },
+    { id: '3', data: [10, 15], label: 'Series D', highlightScope },
+  ],
+  xAxis: [{ data: ['Category 1', 'Category 2'] }],
+  height: 200,
+};
+
+export default function LegendStyleSeries() {
+  const [highlightedItem, setHighlightedItem] = React.useState(null);
+
+  console.log(highlightedItem);
+
+  return (
+    <BarChart
+      {...settings}
+      onHighlightChange={setHighlightedItem}
+      sx={
+        highlightedItem
+          ? {
+              [`.${legendClasses.item}:not([data-series="${highlightedItem.seriesId}"])`]:
+                {
+                  opacity: 0.4,
+                },
+            }
+          : undefined
+      }
+    />
+  );
+}
