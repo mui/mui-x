0883f74f0fba6ea6818f67fa42fcb8f906628e44
Armin Mehinovic <armin@mui.com>
2025-06-18 10:40:04 +0200

    support pivoting

---
+++ docs/data/data-grid/charts-integration/GridChartsIntegrationPivoting.js
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import {
+  DataGridPremium,
+  GridChartsConfigurationPanel,
+  GridChartsIntegrationContextProvider,
+  GridChartsRendererProxy,
+  gridColumnGroupsUnwrappedModelSelector,
+  useGridApiRef,
+} from '@mui/x-data-grid-premium';
+import {
+  ChartsRenderer,
+  configurationOptions,
+} from '@mui/x-charts-pro/ChartsRenderer';
+import { useDemoData } from '@mui/x-data-grid-generator';
+
+export default function GridChartsIntegrationPivoting() {
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 1000,
+    editable: true,
+  });
+  const apiRef = useGridApiRef();
+
+  const pivotModel = {
+    rows: [{ field: 'commodity' }],
+    columns: [
+      { field: 'maturityDate-year', sort: 'asc' },
+      { field: 'maturityDate-quarter', sort: 'asc' },
+    ],
+    values: [
+      { field: 'quantity', aggFunc: 'sum' },
+      { field: 'feeRate', aggFunc: 'avg' },
+    ],
+  };
+
+  const initialState = {
+    ...data.initialState,
+    pivoting: {
+      model: pivotModel,
+      enabled: true,
+    },
+    chartsIntegration: {
+      configurationPanel: {
+        open: true,
+      },
+      categories: ['commodity'],
+      chartType: 'column',
+    },
+  };
+
+  const hasInitializedPivotingSeries = React.useRef(false);
+  React.useEffect(() => {
+    const handleColumnVisibilityModelChange = () => {
+      if (hasInitializedPivotingSeries.current) {
+        return;
+      }
+
+      const unwrappedGroupingModel = Object.keys(
+        gridColumnGroupsUnwrappedModelSelector(apiRef),
+      );
+      // wait until pivoting creates column grouping model
+      if (unwrappedGroupingModel.length === 0) {
+        return;
+      }
+
+      hasInitializedPivotingSeries.current = true;
+      // pick up the first 3 dyamically created columns with quantity in the name
+      apiRef.current?.updateSeries(
+        unwrappedGroupingModel
+          .filter((field) => field.endsWith('quantity'))
+          .slice(0, 3),
+      );
+    };
+
+    return apiRef.current?.subscribeEvent(
+      'columnVisibilityModelChange',
+      handleColumnVisibilityModelChange,
+    );
+  }, [apiRef]);
+
+  return (
+    <GridChartsIntegrationContextProvider>
+      <div style={{ gap: 32, width: '100%' }}>
+        <div style={{ height: 575 }}>
+          <DataGridPremium
+            {...data}
+            apiRef={apiRef}
+            showToolbar
+            chartsIntegration
+            slots={{
+              chartsConfigurationPanel: GridChartsConfigurationPanel,
+            }}
+            slotProps={{
+              chartsConfigurationPanel: {
+                schema: configurationOptions,
+              },
+            }}
+            initialState={initialState}
+            checkboxSelection
+            columnGroupHeaderHeight={35}
+          />
+        </div>
+        <GridChartsRendererProxy renderer={ChartsRenderer} />
+      </div>
+    </GridChartsIntegrationContextProvider>
+  );
+}
