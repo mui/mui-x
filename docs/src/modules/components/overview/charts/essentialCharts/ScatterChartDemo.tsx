981930ec8b8b1d1ba7ce652e92bb625818f5905d
alex <alex.fauquette@gmail.com>
2025-06-20 13:44:42 +0200

    add tooltip to the scatter demo

--- docs/src/modules/components/overview/charts/essentialCharts/ScatterChartDemo.tsx
+++ docs/src/modules/components/overview/charts/essentialCharts/ScatterChartDemo.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { ScatterSeriesType } from '@mui/x-charts/models';
 import { ScatterChart } from '@mui/x-charts/ScatterChart';
+import { ChartsTooltipContainer, useItemTooltip } from '@mui/x-charts/ChartsTooltip';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import data from '../data/transistorCPU';
 import ChartDemoWrapper from '../ChartDemoWrapper';
 
@@ -15,7 +20,7 @@ const series: ScatterSeriesType[] = [
   {
     type: 'scatter',
     label: 'Other',
-    // highlightScope: { highlight: 'item', fade: 'global' },
+    highlightScope: { highlight: 'item', fade: 'global' },
     data: data
       .filter((item) => !constructors.includes(item.constructor) && item.density !== null)
       .map((item) => ({ x: item.year, y: item.density as number, id: item.id })),
@@ -24,7 +29,7 @@ const series: ScatterSeriesType[] = [
     (constructor): ScatterSeriesType => ({
       type: 'scatter',
       label: constructor,
-      // highlightScope: { highlight: 'series', fade: 'global' },
+      highlightScope: { highlight: 'item', fade: 'global' },
       data: data
         .filter((item) => item.constructor === constructor && item.density !== null)
         .map((item) => ({ x: item.year, y: item.density as number, id: item.id })),
@@ -32,13 +37,37 @@ const series: ScatterSeriesType[] = [
   ),
 ];
 
+const numberFormatter = new Intl.NumberFormat('en-US').format;
+
+function CustomTooltip() {
+  const item = useItemTooltip<'scatter'>();
+
+  return (
+    <ChartsTooltipContainer trigger="item">
+      <Paper sx={{ p: 1 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <Box
+            sx={{ width: 20, height: 20, backgroundColor: item?.color, borderRadius: 1, mr: 2 }}
+          />
+          <Typography>{item?.label}</Typography>
+        </Box>
+        <Divider sx={{ my: 1 }} />
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Typography sx={{ mr: 3 }}>{item?.value.x}</Typography>
+          <Typography>{item?.value.y == null ? 'NaN' : numberFormatter(item?.value.y)}</Typography>
+        </Box>
+      </Paper>
+    </ChartsTooltipContainer>
+  );
+}
+
 function Scatter() {
   return (
     <ScatterChart
       series={series}
       grid={{ horizontal: true, vertical: true }}
       voronoiMaxRadius={20}
-      slotProps={{ tooltip: { trigger: 'none' } }}
+      slots={{ tooltip: CustomTooltip }}
       {...chartSetting}
     />
   );
