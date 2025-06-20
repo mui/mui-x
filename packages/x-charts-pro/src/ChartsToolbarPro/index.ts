798d3223845cb8f266f28073b80f6cd441784d11
Bernardo Belchior <bernardo@mui.com>
2025-06-20 09:35:03 +0200

    [charts-pro] Add export menu to charts toolbar (#18210)
    Signed-off-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
    Co-authored-by: alex <alex.fauquette@gmail.com>
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
    Co-authored-by: Jose C Quintas Jr <juniorquintas@gmail.com>

--- packages/x-charts-pro/src/ChartsToolbarPro/index.ts
+++ packages/x-charts-pro/src/ChartsToolbarPro/index.ts
@@ -1,3 +1,5 @@
 export * from './ChartsToolbarPro';
 export * from './ChartsToolbarZoomInTrigger';
 export * from './ChartsToolbarZoomOutTrigger';
+export * from './ChartsToolbarPrintExportTrigger';
+export * from './ChartsToolbarImageExportTrigger';
