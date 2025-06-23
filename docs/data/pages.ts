fffc14c8f6410b89400f2d5edfb23479fa770f5a
Bilal Shafi <bilalshafidev@gmail.com>
2025-06-23 16:47:37 +0500

    [DataGridPro] Data source nested pagination recipe (#14777)
    Co-authored-by: Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>

--- docs/data/pages.ts
+++ docs/data/pages.ts
@@ -184,6 +184,9 @@ const pages: MuiPage[] = [
                 pathname: '/x/react-data-grid/server-side-data/aggregation',
                 plan: 'premium',
               },
+              {
+                pathname: '/x/react-data-grid/server-side-data/recipes',
+              },
             ],
           },
           {
