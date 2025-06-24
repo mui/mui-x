4ecbac1a53a4d0f5f6831e9c8dd74ca87fa8948a
alex <alex.fauquette@gmail.com>
2025-06-24 10:54:44 +0200

    fix lint

--- docs/src/modules/components/overview/charts/essentialCharts/ScatterChartDemo.tsx
+++ docs/src/modules/components/overview/charts/essentialCharts/ScatterChartDemo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable material-ui/no-hardcoded-labels */
 import * as React from 'react';
 import { ScatterSeriesType } from '@mui/x-charts/models';
 import { ScatterChart } from '@mui/x-charts/ScatterChart';
