d599e8f5cdb631d442bc478492ef455f03e49694
Bernardo Belchior <bernardo@mui.com>
2025-06-24 17:18:33 +0200

    Remove barclippath export

--- packages/x-charts/src/internals/index.ts
+++ packages/x-charts/src/internals/index.ts
@@ -3,7 +3,6 @@ export * from './components/ChartsAxesGradients';
 export * from './components/ChartsWrapper';
 export * from '../ChartsLabel/ChartsLabelMark';
 export * from './components/NotRendered';
-export * from '../BarChart/BarClipPath';
 
 // hooks
 export { useSeries } from '../hooks/useSeries';
