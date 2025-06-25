7a3aa11c61bbfe1effd97f0bf50ee4381ab48dce
Bernardo Belchior <bernardo@mui.com>
2025-06-25 11:08:21 +0200

    [charts] Benchmark charts using a real browser

--- test/performance-charts/vitest.config.ts
+++ test/performance-charts/vitest.config.ts
@@ -3,16 +3,31 @@ import codspeedPlugin from '@codspeed/vitest-plugin';
 import react from '@vitejs/plugin-react';
 
 const isCI = process.env.CI === 'true';
+const isTrace = !isCI && process.env.TRACE === 'true';
 
 export default defineConfig({
   plugins: [...(isCI ? [codspeedPlugin()] : []), react()],
   test: {
     setupFiles: ['./setup.ts'],
-    environment: 'jsdom',
+    // @ts-expect-error
+    env: process.env,
+    environment: isTrace ? 'node' : 'jsdom',
     browser: {
-      enabled: false,
+      enabled: isTrace,
       headless: true,
-      instances: [{ browser: 'chromium', testTimeout: 60_000 }],
+      instances: [
+        {
+          browser: 'chromium',
+          testTimeout: 60_000,
+          launch: {
+            args: [
+              '--enable-precise-memory-info',
+              '--enable-devtools-experiments',
+              '--disable-web-security',
+            ],
+          },
+        },
+      ],
       provider: 'playwright',
     },
   },
