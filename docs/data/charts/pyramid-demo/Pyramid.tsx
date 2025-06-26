e8341ee7d17a7e2a71c33235970bbd0c2fe74158
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2025-06-26 16:23:14 +0530

    lint

--- docs/data/charts/pyramid-demo/Pyramid.tsx
+++ docs/data/charts/pyramid-demo/Pyramid.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { Unstable_FunnelChart as FunnelChart } from '@mui/x-charts-pro/FunnelChart';
 
-export default function PyramidFunnel() {
+export default function Pyramid() {
   return (
     <Box sx={{ width: '100%', maxWidth: 400 }}>
       <FunnelChart
