cb3f31b902ffc3a0253da4b8cf068cbb297d5289
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-06-27 17:08:25 +0200

    add more events to the docs

--- docs/data/scheduler/event-calendar/StandaloneMonthView.tsx
+++ docs/data/scheduler/event-calendar/StandaloneMonthView.tsx
@@ -1,72 +1,10 @@
 import * as React from 'react';
-import { DateTime } from 'luxon';
-import { CalendarEvent, CalendarResource } from '@mui/x-scheduler/joy';
+
+import { CalendarEvent } from '@mui/x-scheduler/joy';
 import { StandaloneView } from '@mui/x-scheduler/joy/standalone-view';
 import { MonthView } from '@mui/x-scheduler/joy/month-view';
+import { events as initialEvents, resources } from './month-events';
 import classes from './StandaloneMonthView.module.css';
-
-const initialEvents: CalendarEvent[] = [
-  {
-    id: '1',
-    start: DateTime.fromISO('2025-05-26T07:30:00'),
-    end: DateTime.fromISO('2025-05-26T08:15:00'),
-    title: 'Footing',
-    resource: 'workout',
-  },
-  {
-    id: '2',
-    start: DateTime.fromISO('2025-05-26T16:00:00'),
-    end: DateTime.fromISO('2025-05-26T17:00:00'),
-    title: 'Weekly',
-    resource: 'work',
-  },
-  {
-    id: '3',
-    start: DateTime.fromISO('2025-05-27T10:00:00'),
-    end: DateTime.fromISO('2025-05-27T11:00:00'),
-    title: 'Backlog grooming',
-    resource: 'work',
-  },
-  {
-    id: '4',
-    start: DateTime.fromISO('2025-05-27T19:00:00'),
-    end: DateTime.fromISO('2025-05-27T22:00:00'),
-    title: 'Pizza party',
-  },
-  {
-    id: '5',
-    start: DateTime.fromISO('2025-05-28T08:00:00'),
-    end: DateTime.fromISO('2025-05-28T17:00:00'),
-    title: 'Scheduler deep dive',
-    resource: 'work',
-  },
-  {
-    id: '6',
-    start: DateTime.fromISO('2025-05-29T07:30:00'),
-    end: DateTime.fromISO('2025-05-29T08:15:00'),
-    title: 'Footing',
-    resource: 'workout',
-  },
-  {
-    id: '7',
-    start: DateTime.fromISO('2025-05-29T08:15:00'),
-    end: DateTime.fromISO('2025-05-29T08:30:00'),
-    title: 'Standup',
-    resource: 'work',
-  },
-  {
-    id: '8',
-    start: DateTime.fromISO('2025-05-30T15:00:00'),
-    end: DateTime.fromISO('2025-05-30T15:45:00'),
-    title: 'Retrospective',
-    resource: 'work',
-  },
-];
-
-const resources: CalendarResource[] = [
-  { name: 'Work', id: 'work', color: 'red' },
-  { name: 'Workout', id: 'workout', color: 'jade' },
-];
 
 export default function StandaloneMonthView() {
   const [events, setEvents] = React.useState<CalendarEvent[]>(initialEvents);
