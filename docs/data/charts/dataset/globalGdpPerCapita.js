910b777b3b7e38b831704d36780bf04b1bb90d10
Bernardo Belchior <bernardo@mui.com>
2025-06-27 08:56:29 +0200

    Gen static

---
+++ docs/data/charts/dataset/globalGdpPerCapita.js
@@ -0,0 +1,772 @@
+// This data is expressed in international-$ at 2021 prices.
+// Data from 2023. Source: https://ourworldindata.org/grapher/gdp-per-capita-worldbank?tab=table&tableFilter=countries
+export const globalGdpPerCapita = [
+  {
+    code: 'AFG',
+    gdpPerCapita: 1992.4244,
+  },
+  {
+    code: 'ALB',
+    gdpPerCapita: 18244.293,
+  },
+  {
+    code: 'DZA',
+    gdpPerCapita: 15159.324,
+  },
+  {
+    code: 'AND',
+    gdpPerCapita: 64631.297,
+  },
+  {
+    code: 'AGO',
+    gdpPerCapita: 7244.893,
+  },
+  {
+    code: 'ATG',
+    gdpPerCapita: 28967.256,
+  },
+  {
+    code: 'ARG',
+    gdpPerCapita: 27104.98,
+  },
+  {
+    code: 'ARM',
+    gdpPerCapita: 19230.19,
+  },
+  {
+    code: 'ABW',
+    gdpPerCapita: 40516.81,
+  },
+  {
+    code: 'AUS',
+    gdpPerCapita: 59552.88,
+  },
+  {
+    code: 'AUT',
+    gdpPerCapita: 65015.133,
+  },
+  {
+    code: 'AZE',
+    gdpPerCapita: 21262.299,
+  },
+  {
+    code: 'BHS',
+    gdpPerCapita: 33105.605,
+  },
+  {
+    code: 'BHR',
+    gdpPerCapita: 57213.09,
+  },
+  {
+    code: 'BGD',
+    gdpPerCapita: 8242.398,
+  },
+  {
+    code: 'BRB',
+    gdpPerCapita: 19224.463,
+  },
+  {
+    code: 'BLR',
+    gdpPerCapita: 27718.322,
+  },
+  {
+    code: 'BEL',
+    gdpPerCapita: 64185.848,
+  },
+  {
+    code: 'BLZ',
+    gdpPerCapita: 12455.296,
+  },
+  {
+    code: 'BEN',
+    gdpPerCapita: 3721.2246,
+  },
+  {
+    code: 'BMU',
+    gdpPerCapita: 98041.69,
+  },
+  {
+    code: 'BOL',
+    gdpPerCapita: 9843.97,
+  },
+  {
+    code: 'BIH',
+    gdpPerCapita: 20126.38,
+  },
+  {
+    code: 'BWA',
+    gdpPerCapita: 18846.25,
+  },
+  {
+    code: 'BRA',
+    gdpPerCapita: 19018.238,
+  },
+  {
+    code: 'BRN',
+    gdpPerCapita: 76828.4,
+  },
+  {
+    code: 'BGR',
+    gdpPerCapita: 33403.375,
+  },
+  {
+    code: 'BFA',
+    gdpPerCapita: 2482.0723,
+  },
+  {
+    code: 'BDI',
+    gdpPerCapita: 828.8611,
+  },
+  {
+    code: 'KHM',
+    gdpPerCapita: 6690.5894,
+  },
+  {
+    code: 'CMR',
+    gdpPerCapita: 4870.571,
+  },
+  {
+    code: 'CAN',
+    gdpPerCapita: 55918.93,
+  },
+  {
+    code: 'CPV',
+    gdpPerCapita: 9288.402,
+  },
+  {
+    code: 'CYM',
+    gdpPerCapita: 78109.35,
+  },
+  {
+    code: 'CAF',
+    gdpPerCapita: 1135.1362,
+  },
+  {
+    code: 'TCD',
+    gdpPerCapita: 1679.4615,
+  },
+  {
+    code: 'CHL',
+    gdpPerCapita: 29462.64,
+  },
+  {
+    code: 'CHN',
+    gdpPerCapita: 22137.6,
+  },
+  {
+    code: 'COL',
+    gdpPerCapita: 18692.385,
+  },
+  {
+    code: 'COM',
+    gdpPerCapita: 3498.8032,
+  },
+  {
+    code: 'COG',
+    gdpPerCapita: 6172.2876,
+  },
+  {
+    code: 'CRI',
+    gdpPerCapita: 26292.771,
+  },
+  {
+    code: 'CIV',
+    gdpPerCapita: 6485.316,
+  },
+  {
+    code: 'HRV',
+    gdpPerCapita: 41116.87,
+  },
+  {
+    code: 'CUW',
+    gdpPerCapita: 28886.688,
+  },
+  {
+    code: 'CYP',
+    gdpPerCapita: 53420.684,
+  },
+  {
+    code: 'CZE',
+    gdpPerCapita: 49681.06,
+  },
+  {
+    code: 'COD',
+    gdpPerCapita: 1455.8356,
+  },
+  {
+    code: 'DNK',
+    gdpPerCapita: 72097.305,
+  },
+  {
+    code: 'DJI',
+    gdpPerCapita: 6400.62,
+  },
+  {
+    code: 'DMA',
+    gdpPerCapita: 17420.252,
+  },
+  {
+    code: 'DOM',
+    gdpPerCapita: 23087.934,
+  },
+  {
+    code: 'TLS',
+    gdpPerCapita: 4328.508,
+  },
+  {
+    code: 'ECU',
+    gdpPerCapita: 14472.316,
+  },
+  {
+    code: 'EGY',
+    gdpPerCapita: 16691.133,
+  },
+  {
+    code: 'SLV',
+    gdpPerCapita: 11404.334,
+  },
+  {
+    code: 'GNQ',
+    gdpPerCapita: 15688.238,
+  },
+  {
+    code: 'EST',
+    gdpPerCapita: 42471.824,
+  },
+  {
+    code: 'SWZ',
+    gdpPerCapita: 10132.1875,
+  },
+  {
+    code: 'ETH',
+    gdpPerCapita: 2755.457,
+  },
+  {
+    code: 'FJI',
+    gdpPerCapita: 13652.491,
+  },
+  {
+    code: 'FIN',
+    gdpPerCapita: 57063.523,
+  },
+  {
+    code: 'FRA',
+    gdpPerCapita: 55441.188,
+  },
+  {
+    code: 'GAB',
+    gdpPerCapita: 18702.691,
+  },
+  {
+    code: 'GMB',
+    gdpPerCapita: 2932.2805,
+  },
+  {
+    code: 'GEO',
+    gdpPerCapita: 22590.53,
+  },
+  {
+    code: 'DEU',
+    gdpPerCapita: 63578.14,
+  },
+  {
+    code: 'GHA',
+    gdpPerCapita: 6796.4736,
+  },
+  {
+    code: 'GRC',
+    gdpPerCapita: 36905.453,
+  },
+  {
+    code: 'GRD',
+    gdpPerCapita: 16946.473,
+  },
+  {
+    code: 'GTM',
+    gdpPerCapita: 12389.386,
+  },
+  {
+    code: 'GIN',
+    gdpPerCapita: 3949.34,
+  },
+  {
+    code: 'GNB',
+    gdpPerCapita: 2578.8044,
+  },
+  {
+    code: 'GUY',
+    gdpPerCapita: 49315.16,
+  },
+  {
+    code: 'HTI',
+    gdpPerCapita: 2956.4622,
+  },
+  {
+    code: 'HND',
+    gdpPerCapita: 6468.302,
+  },
+  {
+    code: 'HKG',
+    gdpPerCapita: 64467.574,
+  },
+  {
+    code: 'HUN',
+    gdpPerCapita: 40543.992,
+  },
+  {
+    code: 'ISL',
+    gdpPerCapita: 67255.67,
+  },
+  {
+    code: 'IND',
+    gdpPerCapita: 9160.063,
+  },
+  {
+    code: 'IDN',
+    gdpPerCapita: 13889.883,
+  },
+  {
+    code: 'IRN',
+    gdpPerCapita: 15912.03,
+  },
+  {
+    code: 'IRQ',
+    gdpPerCapita: 12711.062,
+  },
+  {
+    code: 'IRL',
+    gdpPerCapita: 114922.39,
+  },
+  {
+    code: 'ISR',
+    gdpPerCapita: 48432.11,
+  },
+  {
+    code: 'ITA',
+    gdpPerCapita: 53312.07,
+  },
+  {
+    code: 'JAM',
+    gdpPerCapita: 10291.3125,
+  },
+  {
+    code: 'JPN',
+    gdpPerCapita: 46157.637,
+  },
+  {
+    code: 'JOR',
+    gdpPerCapita: 9362.588,
+  },
+  {
+    code: 'KAZ',
+    gdpPerCapita: 34703.234,
+  },
+  {
+    code: 'KEN',
+    gdpPerCapita: 5682.9893,
+  },
+  {
+    code: 'KIR',
+    gdpPerCapita: 3195.8704,
+  },
+  {
+    code: 'OWID_KOS',
+    gdpPerCapita: 13642.677,
+  },
+  {
+    code: 'KWT',
+    gdpPerCapita: 46458.2,
+  },
+  {
+    code: 'KGZ',
+    gdpPerCapita: 6403.143,
+  },
+  {
+    code: 'LAO',
+    gdpPerCapita: 8372.156,
+  },
+  {
+    code: 'LVA',
+    gdpPerCapita: 37007.367,
+  },
+  {
+    code: 'LSO',
+    gdpPerCapita: 2595.8296,
+  },
+  {
+    code: 'LBR',
+    gdpPerCapita: 1616.9237,
+  },
+  {
+    code: 'LBY',
+    gdpPerCapita: 12478.144,
+  },
+  {
+    code: 'LTU',
+    gdpPerCapita: 46740.41,
+  },
+  {
+    code: 'LUX',
+    gdpPerCapita: 132846.58,
+  },
+  {
+    code: 'MAC',
+    gdpPerCapita: 104961.64,
+  },
+  {
+    code: 'MDG',
+    gdpPerCapita: 1643.3093,
+  },
+  {
+    code: 'MWI',
+    gdpPerCapita: 1648.4365,
+  },
+  {
+    code: 'MYS',
+    gdpPerCapita: 32812.3,
+  },
+  {
+    code: 'MDV',
+    gdpPerCapita: 22286.98,
+  },
+  {
+    code: 'MLI',
+    gdpPerCapita: 2394.672,
+  },
+  {
+    code: 'MLT',
+    gdpPerCapita: 61125.71,
+  },
+  {
+    code: 'MHL',
+    gdpPerCapita: 6786.696,
+  },
+  {
+    code: 'MRT',
+    gdpPerCapita: 6258.711,
+  },
+  {
+    code: 'MUS',
+    gdpPerCapita: 26590.424,
+  },
+  {
+    code: 'MEX',
+    gdpPerCapita: 22142.607,
+  },
+  {
+    code: 'FSM',
+    gdpPerCapita: 3886.6965,
+  },
+  {
+    code: 'MDA',
+    gdpPerCapita: 15855.273,
+  },
+  {
+    code: 'MNG',
+    gdpPerCapita: 16222.864,
+  },
+  {
+    code: 'MNE',
+    gdpPerCapita: 27869.65,
+  },
+  {
+    code: 'MAR',
+    gdpPerCapita: 8868.712,
+  },
+  {
+    code: 'MOZ',
+    gdpPerCapita: 1511.6365,
+  },
+  {
+    code: 'MMR',
+    gdpPerCapita: 5364.138,
+  },
+  {
+    code: 'NAM',
+    gdpPerCapita: 10105.515,
+  },
+  {
+    code: 'NRU',
+    gdpPerCapita: 12462.841,
+  },
+  {
+    code: 'NPL',
+    gdpPerCapita: 4859.8003,
+  },
+  {
+    code: 'NLD',
+    gdpPerCapita: 71446.695,
+  },
+  {
+    code: 'NZL',
+    gdpPerCapita: 48826.574,
+  },
+  {
+    code: 'NIC',
+    gdpPerCapita: 7486.943,
+  },
+  {
+    code: 'NER',
+    gdpPerCapita: 1703.397,
+  },
+  {
+    code: 'NGA',
+    gdpPerCapita: 5593.0566,
+  },
+  {
+    code: 'MKD',
+    gdpPerCapita: 23584.564,
+  },
+  {
+    code: 'NOR',
+    gdpPerCapita: 90469.81,
+  },
+  {
+    code: 'OMN',
+    gdpPerCapita: 38311.418,
+  },
+  {
+    code: 'PAK',
+    gdpPerCapita: 5439.1875,
+  },
+  {
+    code: 'PLW',
+    gdpPerCapita: 15796.54,
+  },
+  {
+    code: 'PSE',
+    gdpPerCapita: 5313.098,
+  },
+  {
+    code: 'PAN',
+    gdpPerCapita: 35863.88,
+  },
+  {
+    code: 'PNG',
+    gdpPerCapita: 4174.4414,
+  },
+  {
+    code: 'PRY',
+    gdpPerCapita: 15783.115,
+  },
+  {
+    code: 'PER',
+    gdpPerCapita: 15294.257,
+  },
+  {
+    code: 'PHL',
+    gdpPerCapita: 9901.057,
+  },
+  {
+    code: 'POL',
+    gdpPerCapita: 44384.168,
+  },
+  {
+    code: 'PRT',
+    gdpPerCapita: 41755.48,
+  },
+  {
+    code: 'PRI',
+    gdpPerCapita: 42994.785,
+  },
+  {
+    code: 'QAT',
+    gdpPerCapita: 116159.14,
+  },
+  {
+    code: 'ROU',
+    gdpPerCapita: 40665.527,
+  },
+  {
+    code: 'RUS',
+    gdpPerCapita: 39753.457,
+  },
+  {
+    code: 'RWA',
+    gdpPerCapita: 3060.029,
+  },
+  {
+    code: 'KNA',
+    gdpPerCapita: 30409.404,
+  },
+  {
+    code: 'LCA',
+    gdpPerCapita: 23403.46,
+  },
+  {
+    code: 'VCT',
+    gdpPerCapita: 18335.213,
+  },
+  {
+    code: 'WSM',
+    gdpPerCapita: 6304.346,
+  },
+  {
+    code: 'STP',
+    gdpPerCapita: 5541.302,
+  },
+  {
+    code: 'SAU',
+    gdpPerCapita: 55055.4,
+  },
+  {
+    code: 'SEN',
+    gdpPerCapita: 4317.2266,
+  },
+  {
+    code: 'SRB',
+    gdpPerCapita: 26030.021,
+  },
+  {
+    code: 'SYC',
+    gdpPerCapita: 29468.951,
+  },
+  {
+    code: 'SLE',
+    gdpPerCapita: 3034.219,
+  },
+  {
+    code: 'SGP',
+    gdpPerCapita: 127543.555,
+  },
+  {
+    code: 'SXM',
+    gdpPerCapita: 44894.695,
+  },
+  {
+    code: 'SVK',
+    gdpPerCapita: 39290.434,
+  },
+  {
+    code: 'SVN',
+    gdpPerCapita: 48243.94,
+  },
+  {
+    code: 'SLB',
+    gdpPerCapita: 2534.0276,
+  },
+  {
+    code: 'SOM',
+    gdpPerCapita: 1402.47,
+  },
+  {
+    code: 'ZAF',
+    gdpPerCapita: 13690.39,
+  },
+  {
+    code: 'KOR',
+    gdpPerCapita: 50572.25,
+  },
+  {
+    code: 'ESP',
+    gdpPerCapita: 47141.543,
+  },
+  {
+    code: 'LKA',
+    gdpPerCapita: 13029.524,
+  },
+  {
+    code: 'SDN',
+    gdpPerCapita: 2469.1946,
+  },
+  {
+    code: 'SUR',
+    gdpPerCapita: 19043.705,
+  },
+  {
+    code: 'SWE',
+    gdpPerCapita: 63114.68,
+  },
+  {
+    code: 'CHE',
+    gdpPerCapita: 82557.55,
+  },
+  {
+    code: 'TJK',
+    gdpPerCapita: 4472.324,
+  },
+  {
+    code: 'TZA',
+    gdpPerCapita: 3620.7935,
+  },
+  {
+    code: 'THA',
+    gdpPerCapita: 21142.664,
+  },
+  {
+    code: 'TGO',
+    gdpPerCapita: 2767.5166,
+  },
+  {
+    code: 'TTO',
+    gdpPerCapita: 31705.834,
+  },
+  {
+    code: 'TUN',
+    gdpPerCapita: 12552.986,
+  },
+  {
+    code: 'TUR',
+    gdpPerCapita: 34609.914,
+  },
+  {
+    code: 'TKM',
+    gdpPerCapita: 17866.404,
+  },
+  {
+    code: 'TCA',
+    gdpPerCapita: 22305.66,
+  },
+  {
+    code: 'TUV',
+    gdpPerCapita: 5812.393,
+  },
+  {
+    code: 'UGA',
+    gdpPerCapita: 2791.0557,
+  },
+  {
+    code: 'UKR',
+    gdpPerCapita: 15885.226,
+  },
+  {
+    code: 'ARE',
+    gdpPerCapita: 68577.54,
+  },
+  {
+    code: 'GBR',
+    gdpPerCapita: 54542.016,
+  },
+  {
+    code: 'USA',
+    gdpPerCapita: 74577.51,
+  },
+  {
+    code: 'URY',
+    gdpPerCapita: 31019.31,
+  },
+  {
+    code: 'UZB',
+    gdpPerCapita: 10007.686,
+  },
+  {
+    code: 'VUT',
+    gdpPerCapita: 3118.396,
+  },
+  {
+    code: 'VNM',
+    gdpPerCapita: 13491.88,
+  },
+  {
+    code: 'OWID_WRL',
+    gdpPerCapita: 20670.941,
+  },
+  {
+    code: 'ZMB',
+    gdpPerCapita: 3673.4841,
+  },
+  {
+    code: 'ZWE',
+    gdpPerCapita: 3442.2512,
+  },
+];
