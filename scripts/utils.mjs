bec5b14eef19526245d860259a52863a16f8b685
Lukas <llukas.tyla@gmail.com>
2025-06-30 12:52:00 +0300

    Revert JSDoc comment removal

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -1,5 +1,8 @@
 import path from 'path';
 
+/**
+ * Returns the full path of the root directory of this repository.
+ */
 export function getWorkspaceRoot() {
   const workspaceRoot = path.resolve(import.meta.dirname, '..');
   return workspaceRoot;
