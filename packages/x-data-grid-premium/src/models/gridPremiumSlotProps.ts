28242301f400ddafc35b8ce0ee057295ef5a2473
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2025-06-30 10:12:46 +0100

    rename panel directory

--- packages/x-data-grid-premium/src/models/gridPremiumSlotProps.ts
+++ packages/x-data-grid-premium/src/models/gridPremiumSlotProps.ts
@@ -1,5 +1,5 @@
 import { GridProSlotProps } from '@mui/x-data-grid-pro/internals';
-import type { GridChartsConfigurationPanelProps } from '../components/chartsConfigurationPanel/GridChartsConfigurationPanel';
+import type { GridChartsConfigurationPanelProps } from '../components/chartsPanel/GridChartsConfigurationPanel';
 
 // Overrides for module augmentation
 export interface GridChartsConfigurationPanelPropsOverrides {}
