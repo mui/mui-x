a01542e7e6b5b2ab8cb229dcdd48b9e1a7823378
Jose Quintas <juniorquintas@gmail.com>
2025-07-02 17:33:07 +0200

    docs

--- packages/x-charts-pro/src/FunnelChart/funnel.types.ts
+++ packages/x-charts-pro/src/FunnelChart/funnel.types.ts
@@ -89,12 +89,12 @@ export interface FunnelSeriesType
    */
   variant?: 'filled' | 'outlined';
   /**
-   * Denotes if the data is increasing or decreasing.
+   * Denotes if the funnel is increasing or decreasing.
    * Only used in the `pyramid` and `step-pyramid` curves.
    *
-   * - `increasing`, first data point is smaller than the last data point.
-   * - `decreasing`, first data point is greater than the last data point.
-   * - `auto`, the data direction is determined automatically based on the first and last data points.
+   * - `increasing`, funnel is drawn with a point at the top and a wide base.
+   * - `decreasing`, funnel is drawn with a wide top and a point at the base.
+   * - `auto`, the direction is determined automatically based on the first and last data points.
    *
    * @default 'auto'
    */
