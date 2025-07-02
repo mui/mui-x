ed3db84a021a6f4c753399152dbb7d4e777c039b
Jose Quintas <juniorquintas@gmail.com>
2025-07-02 14:20:23 +0200

    [charts-pro] Add tests and classes to zoom slider

---
+++ packages/x-charts-pro/src/ChartZoomSlider/internals/chartAxisZoomSliderTrackClasses.ts
@@ -0,0 +1,42 @@
+import generateUtilityClasses from '@mui/utils/generateUtilityClasses';
+import composeClasses from '@mui/utils/composeClasses';
+import generateUtilityClass from '@mui/utils/generateUtilityClass';
+import type { ChartAxisZoomSliderActiveTrackProps } from './ChartAxisZoomSliderActiveTrack';
+
+export interface ChartAxisZoomSliderTrackClasses {
+  /** Styles applied to the root element. */
+  root: string;
+  /** Styles applied to the root element when it is horizontal. */
+  horizontal: string;
+  /** Styles applied to the root element when it is vertical. */
+  vertical: string;
+  /** Styles applied to the root element to the active part of the track. */
+  active: string;
+  /** Styles applied to the root element to the background part of the track. */
+  background: string;
+}
+
+export type ChartAxisZoomSliderTrackClassKey = keyof ChartAxisZoomSliderTrackClasses;
+
+export const chartAxisZoomSliderTrackClasses: ChartAxisZoomSliderTrackClasses =
+  generateUtilityClasses('MuiChartAxisZoomSliderTrack', [
+    'root',
+    'horizontal',
+    'vertical',
+    'background',
+    'active',
+  ]);
+
+export function getAxisZoomSliderTrackUtilityClass(slot: string) {
+  return generateUtilityClass('MuiChartAxisZoomSliderTrack', slot);
+}
+
+export const useUtilityClasses = (props: Partial<ChartAxisZoomSliderActiveTrackProps>) => {
+  const { axisDirection } = props;
+  const slots = {
+    background: [axisDirection === 'x' ? 'horizontal' : 'vertical', 'background'],
+    active: [axisDirection === 'x' ? 'horizontal' : 'vertical', 'active'],
+  };
+
+  return composeClasses(slots, getAxisZoomSliderTrackUtilityClass);
+};
