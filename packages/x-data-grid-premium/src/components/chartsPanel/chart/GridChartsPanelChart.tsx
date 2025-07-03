7ecacce754097b3a68b5944bb7516f5ff98274b4
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2025-07-03 14:16:45 +0100

    update chart button styles

--- packages/x-data-grid-premium/src/components/chartsPanel/chart/GridChartsPanelChart.tsx
+++ packages/x-data-grid-premium/src/components/chartsPanel/chart/GridChartsPanelChart.tsx
@@ -58,7 +58,9 @@ const GridChartTypeButton = styled('button', {
   shouldForwardProp: (prop) => prop !== 'isSelected',
 })<ChartTypeButtonProps>(({ isSelected }) => {
   return {
-    backgroundColor: isSelected ? vars.colors.interactive.hover : vars.colors.background.base,
+    backgroundColor: isSelected
+      ? `color-mix(in srgb, ${vars.colors.interactive.selected} calc(${vars.colors.interactive.selectedOpacity} * 100%), ${vars.colors.background.base})`
+      : vars.colors.background.base,
     color: isSelected ? vars.colors.interactive.selected : vars.colors.foreground.muted,
     cursor: 'pointer',
     display: 'flex',
@@ -67,15 +69,18 @@ const GridChartTypeButton = styled('button', {
     justifyContent: 'center',
     gap: vars.spacing(0.5),
     padding: vars.spacing(1.5, 1, 1),
-    border: `1px solid ${vars.colors.border.base}`,
+    border: `1px solid ${isSelected ? vars.colors.interactive.selected : vars.colors.border.base}`,
     font: vars.typography.font.small,
+    fontWeight: vars.typography.fontWeight.medium,
     borderRadius: vars.radius.base,
-    transition: vars.transition(['border-color', 'background-color'], {
+    transition: vars.transition(['border-color', 'background-color', 'color'], {
       duration: vars.transitions.duration.short,
       easing: vars.transitions.easing.easeInOut,
     }),
     '&:hover': {
-      backgroundColor: vars.colors.interactive.hover,
+      backgroundColor: isSelected
+        ? `color-mix(in srgb, ${vars.colors.interactive.selected} calc(${vars.colors.interactive.selectedOpacity} * 100%), ${vars.colors.background.base})`
+        : vars.colors.interactive.hover,
     },
   };
 });
