ae66a3846ba28aa7e24ccf6c08aeb2b2b8915a4e
Bernardo Belchior <bernardo@mui.com>
2025-07-04 12:10:43 +0200

    Run charts benchmark

--- test/performance-charts/vitest.config.ts
+++ test/performance-charts/vitest.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vitest/config';
 import codspeedPlugin from '@codspeed/vitest-plugin';
 import react from '@vitejs/plugin-react';
+import { BenchmarkReporter } from 'vitest/reporters';
+import util from 'util';
 
 const isCI = process.env.CI === 'true';
 const isTrace = !isCI && process.env.TRACE === 'true';
+
+const reporter: BenchmarkReporter = {
+  onTestModuleEnd(module) {
+    console.log(
+      util.inspect(
+        module.task.tasks.flatMap((task) =>
+          task.tasks.filter((task) => task.meta.benchmark).map((task) => task.result),
+        ),
+        { depth: null, colors: true },
+      ),
+    );
+  },
+};
 
 export default defineConfig({
   plugins: [...(isCI ? [codspeedPlugin()] : []), react()],
@@ -11,6 +26,7 @@ export default defineConfig({
     setupFiles: ['./setup.ts'],
     env: { TRACE: isTrace ? 'true' : 'false' },
     environment: isTrace ? 'node' : 'jsdom',
+    benchmark: { reporters: ['default', reporter] },
     browser: {
       enabled: isTrace,
       headless: true,
