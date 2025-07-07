bd2308ec97d244d0d60a1b41ecf0225e24cea56b
Bernardo Belchior <bernardo@mui.com>
2025-07-07 10:43:15 +0200

    Fix bar chart bench

--- test/performance-charts/tests/BarChart.bench.tsx
+++ test/performance-charts/tests/BarChart.bench.tsx
@@ -23,7 +23,7 @@ describe('BarChart', () => {
         <BarChart xAxis={[{ data: xData }]} series={[{ data: yData }]} width={500} height={300} />,
       );
 
-      await findByText(dataLength.toLocaleString(), { ignore: 'span' });
+      await findByText('926', { ignore: 'span' });
 
       cleanup();
     },
