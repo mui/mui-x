fa8f6ce9b091cbea4baaa11418a95b1326e7c866
alex <alex.fauquette@gmail.com>
2025-07-07 14:49:23 +0200

    early return

--- packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/getAxisDomainLimit.ts
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/getAxisDomainLimit.ts
@@ -6,20 +6,19 @@ export const getAxisDomainLimit = <T extends CartesianChartSeriesType>(
   axis: AxisConfig,
   axisDirection: 'x' | 'y',
   axisIndex: number,
-  formattedSeries: ProcessedSeries<T | 'line' | 'bar'>,
+  formattedSeries: ProcessedSeries<T | 'line'>,
 ): 'nice' | 'strict' | ((min: number, max: number) => { min: number; max: number }) => {
   if (axis.domainLimit !== undefined) {
     return axis.domainLimit;
   }
 
-  for (const seriesId of formattedSeries.line?.seriesOrder ?? []) {
-    const series = formattedSeries.line!.series[seriesId];
+  if (axisDirection === 'x') {
+    for (const seriesId of formattedSeries.line?.seriesOrder ?? []) {
+      const series = formattedSeries.line!.series[seriesId];
 
-    if (
-      series.xAxisId === axis.id ||
-      (series.xAxisId === undefined && axisIndex === 0 && axisDirection === 'x')
-    ) {
-      return 'strict';
+      if (series.xAxisId === axis.id || (series.xAxisId === undefined && axisIndex === 0)) {
+        return 'strict';
+      }
     }
   }
   return 'nice';
