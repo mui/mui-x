c56703d6d50e34a59522772300a6ec6d0668bca0
Bernardo Belchior <bernardo@mui.com>
2025-07-07 17:37:52 +0200

    [charts] Add symlog scale to charts

---
+++ docs/data/charts/bars/SymlogBars.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+export default function SymlogBars() {
+  return (
+    <BarChart
+      xAxis={[{ data: ['group A', 'group B', 'group C'] }]}
+      yAxis={[{ scaleType: 'symlog', constant: 1 }]}
+      series={[
+        { data: [4_000, 30, 50] },
+        { data: [1, 600, 34] },
+        { data: [20, 5, 60_000] },
+      ]}
+      height={300}
+    />
+  );
+}
