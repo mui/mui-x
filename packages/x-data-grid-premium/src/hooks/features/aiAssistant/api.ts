3e3e2a00ae02bd217fa458e768589c784533d27e
Bharat Kashyap <bharatkashyap@outlook.com>
2025-07-11 01:17:06 -0700

    Update packages/x-data-grid-premium/src/hooks/features/aiAssistant/api.ts
    Co-authored-by: Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>
    Signed-off-by: Bharat Kashyap <bharatkashyap@outlook.com>

--- packages/x-data-grid-premium/src/hooks/features/aiAssistant/api.ts
+++ packages/x-data-grid-premium/src/hooks/features/aiAssistant/api.ts
@@ -9,7 +9,7 @@ type Result<T> = { ok: false; message: string } | { ok: true; data: T };
  * @param {string} context The prompt context containing necessary information about the current columns definition.
  * Either use the `context` parameter of the `onPrompt` callback or the return value of the `unstable_getPromptContext()` API method.
  * @param {string} conversationId The id of the conversation the prompt is part of. If not passed, prompt response will return a new conversation id that can be used to continue the newly started conversation.
- * @param {object} options Optional, includes settings to extend and customize the prompt resolver's behaviour.
+ * @param {PromptResolverOptions} options Optional, includes settings to extend and customize the prompt resolver's behaviour.
  * @returns {Promise<PromptResponse>} The grid state updates to be applied.
  */
 export function gridDefaultPromptResolver(
