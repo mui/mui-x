a4eb06136c6d29811e62b64de95386cd60bebc8b
alex <alex.fauquette@gmail.com>
2025-07-11 13:05:56 +0200

    add tests on axisClick

---
+++ packages/x-charts/src/RadarChart/checkClickEvent.test.tsx
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import { createRenderer } from '@mui/internal-test-utils';
+import { spy } from 'sinon';
+import { RadarChart, RadarChartProps } from '@mui/x-charts/RadarChart';
+import { isJSDOM } from 'test/utils/skipIf';
+
+const config: RadarChartProps = {
+  series: [
+    { id: 's1', data: [2, 3, 4, 5] },
+    { id: 's2', data: [6, 5, 2, 1] },
+  ],
+  radar: {
+    metrics: ['A', 'B', 'C', 'D'],
+  },
+  margin: { top: 0, left: 0, bottom: 0, right: 0 },
+  width: 100,
+  height: 100,
+} as const;
+
+describe('RadarChart - click event', () => {
+  const { render } = createRenderer();
+
+  describe('onAxisClick', () => {
+    // can't do Pointer event with JSDom https://github.com/jsdom/jsdom/issues/2527
+    it.skipIf(isJSDOM)('should provide the right context as second argument', async () => {
+      const onAxisClick = spy();
+      const { user } = render(
+        <div
+          style={{
+            width: 100,
+            height: 100,
+          }}
+        >
+          <RadarChart {...config} onAxisClick={onAxisClick} />
+        </div>,
+      );
+      const svg = document.querySelector<HTMLElement>('svg')!;
+
+      await user.pointer([
+        {
+          keys: '[MouseLeft]',
+          target: svg,
+          coords: { clientX: 45, clientY: 15 },
+        },
+      ]);
+
+      expect(onAxisClick.lastCall.args[1]).to.deep.equal({
+        dataIndex: 0,
+        axisValue: 'A',
+        seriesValues: { s1: 2, s2: 6 },
+      });
+
+      await user.pointer([
+        {
+          keys: '[MouseLeft]',
+          target: svg,
+          coords: { clientX: 80, clientY: 45 },
+        },
+      ]);
+
+      expect(onAxisClick.lastCall.args[1]).to.deep.equal({
+        dataIndex: 1,
+        axisValue: 'B',
+        seriesValues: { s1: 3, s2: 5 },
+      });
+    });
+
+    // can't do Pointer event with JSDom https://github.com/jsdom/jsdom/issues/2527
+    it.skipIf(isJSDOM)(
+      'should provide the right context as second argument with startAngle=90',
+      async () => {
+        const onAxisClick = spy();
+        const { user } = render(
+          <div
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          >
+            <RadarChart
+              {...config}
+              radar={{ ...config.radar, startAngle: 90 }}
+              onAxisClick={onAxisClick}
+            />
+          </div>,
+        );
+        const svg = document.querySelector<HTMLElement>('svg')!;
+
+        await user.pointer([
+          {
+            keys: '[MouseLeft]',
+            target: svg,
+            coords: { clientX: 45, clientY: 15 },
+          },
+        ]);
+
+        expect(onAxisClick.lastCall.args[1]).to.deep.equal({
+          dataIndex: 3,
+          axisValue: 'D',
+          seriesValues: { s1: 5, s2: 1 },
+        });
+
+        await user.pointer([
+          {
+            keys: '[MouseLeft]',
+            target: svg,
+            coords: { clientX: 80, clientY: 45 },
+          },
+        ]);
+
+        expect(onAxisClick.lastCall.args[1]).to.deep.equal({
+          dataIndex: 0,
+          axisValue: 'A',
+          seriesValues: { s1: 2, s2: 6 },
+        });
+      },
+    );
+  });
+});
