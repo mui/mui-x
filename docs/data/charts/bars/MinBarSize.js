9c42731d3164628aa84d5a1d927e584f440abaad
Jose C Quintas Jr <juniorquintas@gmail.com>
2025-07-15 09:54:55 +0200

    [charts] Add `minBarSize` to prevent bars from having 0 height (#18798)

---
+++ docs/data/charts/bars/MinBarSize.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+// Create sparse data where some bars would be very small
+const sparseData = [0.02, 5, 0.05, 7, 0.01, 6, -0.03];
+const categories = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+export default function MinBarSize() {
+  const [minBarSize, setMinBarSize] = React.useState(10);
+
+  const handleChange = (event, newValue) => {
+    if (typeof newValue !== 'number') {
+      return;
+    }
+    setMinBarSize(newValue);
+  };
+
+  return (
+    <Stack spacing={2} justifyContent={'center'} alignItems={'center'}>
+      <Typography id="min-bar-size-slider" gutterBottom>
+        minBarSize: {minBarSize}px
+      </Typography>
+      <Slider
+        value={minBarSize}
+        onChange={handleChange}
+        aria-labelledby="min-bar-size-slider"
+        min={0}
+        max={200}
+        sx={{ width: 300 }}
+      />
+      <BarChart
+        xAxis={[{ data: categories, scaleType: 'band' }]}
+        series={[{ data: sparseData, minBarSize }]}
+        height={300}
+        width={400}
+      />
+    </Stack>
+  );
+}
