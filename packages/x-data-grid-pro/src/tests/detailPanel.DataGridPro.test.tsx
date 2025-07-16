c294f0e94c2527b1bd0f8318265d585e16c83d30
Andrew Cherniavskyi <andrew@mui.com>
2025-07-16 18:25:05 +0200

    fix failing tests in react 18

--- packages/x-data-grid-pro/src/tests/detailPanel.DataGridPro.test.tsx
+++ packages/x-data-grid-pro/src/tests/detailPanel.DataGridPro.test.tsx
@@ -326,7 +326,7 @@ describe('<DataGridPro /> - Detail panel', () => {
     // from React 19 it is:
     //   2x during state initialization
     // + 2x when sortedRowsSet is fired
-    const expectedCallCount = reactMajor >= 19 ? 8 : 8;
+    const expectedCallCount = reactMajor >= 19 ? 8 : 12;
 
     expect(getDetailPanelContent.callCount).to.equal(expectedCallCount);
     await user.click(screen.getByRole('button', { name: 'Expand' }));
@@ -368,7 +368,7 @@ describe('<DataGridPro /> - Detail panel', () => {
     // from React 19 it is:
     //   2x during state initialization
     // + 2x when sortedRowsSet is fired
-    const expectedCallCount = reactMajor >= 19 ? 8 : 8;
+    const expectedCallCount = reactMajor >= 19 ? 8 : 12;
 
     expect(getDetailPanelHeight.callCount).to.equal(expectedCallCount);
     await user.click(screen.getByRole('button', { name: 'Expand' }));
@@ -441,7 +441,7 @@ describe('<DataGridPro /> - Detail panel', () => {
     // from React 19 it is:
     //   1x during state initialization
     // + 1x when sortedRowsSet is fired
-    const expectedCallCount = reactMajor >= 19 ? 4 : 4;
+    const expectedCallCount = reactMajor >= 19 ? 4 : 6;
 
     expect(getDetailPanelHeight.callCount).to.equal(expectedCallCount);
     expect(getDetailPanelHeight.lastCall.args[0].id).to.equal(0);
