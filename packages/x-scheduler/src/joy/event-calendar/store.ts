b70389966e6ec0bc6e25c8454b4412229d756ca2
Flavien DELANGLE <flaviendelangle@gmail.com>
2025-07-24 07:31:17 +0200

    [scheduler][primitives] Allow to re-schedule an event in the Time Grid primitive by dragging it (#18811)

--- packages/x-scheduler/src/joy/event-calendar/store.ts
+++ packages/x-scheduler/src/joy/event-calendar/store.ts
@@ -1,7 +1,7 @@
 import { getAdapter } from '../../primitives/utils/adapter/getAdapter';
 import { createSelector, createSelectorMemoized, Store } from '../../base-ui-copy/utils/store';
 import { SchedulerValidDate } from '../../primitives/models';
-import { CalendarEvent } from '../models/events';
+import { CalendarEvent, CalendarEventId } from '../models/events';
 import { CalendarResource, CalendarResourceId } from '../models/resource';
 import { EventCalendarView } from './EventCalendar.types';
 
@@ -18,6 +18,10 @@ export type State = {
    * A resource is visible if it is registered in this lookup with `true` value or if it is not registered at all.
    */
   visibleResources: Map<CalendarResourceId, boolean>;
+  /**
+   * Whether the items in the calendar are draggable to change their start and end dates without changing the duration.
+   */
+  areEventsDraggable: boolean;
 };
 
 export type EventCalendarStore = Store<State>;
@@ -72,4 +76,11 @@ export const selectors = {
       };
     },
   ),
+  // TODO: Add a new data structure (Map?) to avoid linear complexity here.
+  getEventById: createSelector((state: State, eventId: CalendarEventId | null) =>
+    state.events.find((event) => event.id === eventId),
+  ),
+  isEventDraggable: createSelector((state: State, { readOnly }: { readOnly?: boolean }) => {
+    return !readOnly && state.areEventsDraggable;
+  }),
 };
--- packages/x-scheduler/src/primitives/time-grid/column/TimeGridColumn.tsx
+++ packages/x-scheduler/src/primitives/time-grid/column/TimeGridColumn.tsx
@@ -1,18 +1,28 @@
 'use client';
 import * as React from 'react';
+import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
 import { useRenderElement } from '../../../base-ui-copy/utils/useRenderElement';
 import { BaseUIComponentProps } from '../../../base-ui-copy/utils/types';
 import { TimeGridColumnContext } from './TimeGridColumnContext';
-import { getAdapter } from '../../utils/adapter/getAdapter';
+import { TimeGridColumnPlaceholderContext } from './TimeGridColumnPlaceholderContext';
+import { useAdapter } from '../../utils/adapter/useAdapter';
 import { SchedulerValidDate } from '../../models';
 import { mergeDateAndTime } from '../../utils/date-utils';
-
-const adapter = getAdapter();
+import { useTimeGridRootContext } from '../root/TimeGridRootContext';
+import type { TimeGridEvent } from '../event';
+import type { TimeGridRoot } from '../root';
+import {
+  EVENT_DRAG_PRECISION_MINUTE,
+  getCursorPositionRelativeToElement,
+  isDraggingTimeGridEvent,
+} from '../../utils/drag-utils';
 
 export const TimeGridColumn = React.forwardRef(function TimeGridColumn(
   componentProps: TimeGridColumn.Props,
   forwardedRef: React.ForwardedRef<HTMLDivElement>,
 ) {
+  const adapter = useAdapter();
+
   const {
     // Rendering props
     className,
@@ -25,13 +35,48 @@ export const TimeGridColumn = React.forwardRef(function TimeGridColumn(
     ...elementProps
   } = componentProps;
 
+  const ref = React.useRef<HTMLDivElement>(null);
+  const { onEventChange, setPlaceholder, placeholder } = useTimeGridRootContext();
+
+  const columnStart = React.useMemo(
+    () =>
+      startTime == null ? adapter.startOfDay(value) : mergeDateAndTime(adapter, value, startTime),
+    [adapter, value, startTime],
+  );
+
+  const columnEnd = React.useMemo(
+    () => (endTime == null ? adapter.endOfDay(value) : mergeDateAndTime(adapter, value, endTime)),
+    [adapter, value, endTime],
+  );
+
+  const columnPlaceholder = React.useMemo(() => {
+    if (placeholder == null) {
+      return null;
+    }
+
+    if (
+      adapter.isBefore(placeholder.start, columnStart) ||
+      adapter.isAfter(placeholder.end, columnEnd)
+    ) {
+      return null;
+    }
+
+    return placeholder;
+  }, [adapter, columnStart, columnEnd, placeholder]);
+
   const contextValue: TimeGridColumnContext = React.useMemo(
     () => ({
-      start:
-        startTime == null ? adapter.startOfDay(value) : mergeDateAndTime(adapter, value, startTime),
-      end: endTime == null ? adapter.endOfDay(value) : mergeDateAndTime(adapter, value, endTime),
+      start: columnStart,
+      end: columnEnd,
     }),
-    [value, startTime, endTime],
+    [columnStart, columnEnd],
+  );
+
+  const placeholderContextValue: TimeGridColumnPlaceholderContext = React.useMemo(
+    () => ({
+      placeholder: columnPlaceholder,
+    }),
+    [columnPlaceholder],
   );
 
   const props = React.useMemo(() => ({ role: 'gridcell' }), []);
@@ -40,12 +85,84 @@ export const TimeGridColumn = React.forwardRef(function TimeGridColumn(
 
   const element = useRenderElement('div', componentProps, {
     state,
-    ref: [forwardedRef],
+    ref: [forwardedRef, ref],
     props: [props, elementProps],
   });
 
+  React.useEffect(() => {
+    const domElement = ref.current;
+    if (!domElement) {
+      return () => {};
+    }
+
+    function getEventDropData({
+      data,
+      input,
+    }: {
+      data: TimeGridEvent.EventDragData;
+      input: { clientY: number };
+    }): TimeGridRoot.EventData {
+      const position = getCursorPositionRelativeToElement({ ref, input });
+      const eventTopPosition = position.y - data.position.y;
+
+      const newStartMinuteInDay =
+        Math.round(
+          ((eventTopPosition / domElement!.offsetHeight) * 1440) / EVENT_DRAG_PRECISION_MINUTE,
+        ) * EVENT_DRAG_PRECISION_MINUTE;
+
+      // TODO: Avoid JS Date conversion
+      const eventDuration =
+        (adapter.toJsDate(data.end).getTime() - adapter.toJsDate(data.start).getTime()) /
+        (60 * 1000);
+
+      const newStartDate = adapter.setMinutes(
+        adapter.setHours(value, Math.floor(newStartMinuteInDay / 60)),
+        newStartMinuteInDay % 60,
+      );
+
+      const newEndDate = adapter.addMinutes(newStartDate, eventDuration);
+
+      return { start: newStartDate, end: newEndDate, id: data.id };
+    }
+
+    return dropTargetForElements({
+      element: domElement,
+      canDrop: (arg) => isDraggingTimeGridEvent(arg.source.data),
+      getData: () => ({ type: 'column' }),
+      onDrag: ({ source: { data }, location }) => {
+        if (!isDraggingTimeGridEvent(data)) {
+          return;
+        }
+
+        const newPlaceholder = getEventDropData({
+          data,
+          input: location.current.input,
+        });
+
+        setPlaceholder(newPlaceholder);
+      },
+      onDrop: ({ source: { data }, location }) => {
+        if (!isDraggingTimeGridEvent(data)) {
+          return;
+        }
+
+        const newEvent = getEventDropData({
+          data,
+          input: location.current.input,
+        });
+
+        onEventChange(newEvent);
+        setPlaceholder(null);
+      },
+    });
+  }, [adapter, onEventChange, setPlaceholder, value]);
+
   return (
-    <TimeGridColumnContext.Provider value={contextValue}>{element}</TimeGridColumnContext.Provider>
+    <TimeGridColumnContext.Provider value={contextValue}>
+      <TimeGridColumnPlaceholderContext.Provider value={placeholderContextValue}>
+        {element}
+      </TimeGridColumnPlaceholderContext.Provider>
+    </TimeGridColumnContext.Provider>
   );
 });
 
