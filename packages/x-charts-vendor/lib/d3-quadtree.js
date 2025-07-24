966e9d69c3f248661411cc41513402226493924a
Bernardo Belchior <bernardo@mui.com>
2025-07-24 16:27:07 +0200

    Remove unused code

--- packages/x-charts-vendor/lib/d3-quadtree.js
+++
@@ -1,6 +0,0 @@
-
-// `x-charts-vendor/d3-quadtree` (CommonJS)
-// See upstream license: https://github.com/d3/d3-quadtree/blob/main/LICENSE
-//
-// Our CommonJS package relies on transpiled vendor files in `lib-vendor/d3-quadtree`
-module.exports = require("../lib-vendor/d3-quadtree/src/index.js");
