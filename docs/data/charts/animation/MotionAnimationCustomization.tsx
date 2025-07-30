e890147d8d32f6361fbbcf27f6325a7ae622adea
Jose Quintas <juniorquintas@gmail.com>
2025-07-30 21:27:09 +0200

    [charts] Animation example using `motion` library

---
+++ docs/data/charts/animation/MotionAnimationCustomization.tsx
@@ -0,0 +1,88 @@
+import {
+  LineChart,
+  type AnimatedLineProps,
+  type MarkElementProps,
+} from '@mui/x-charts/LineChart';
+import * as React from 'react';
+import { motion } from 'motion/react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function MotionAnimationCustomization() {
+  const [key, animate] = React.useReducer((v) => v + 1, 0);
+
+  return (
+    <Stack>
+      <LineChart
+        key={key}
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            curve: 'linear',
+          },
+          {
+            data: [3, 4, 6, 7, 9, 11],
+            curve: 'linear',
+          },
+        ]}
+        width={500}
+        height={300}
+        slots={{
+          line: AnimatedLine,
+          mark: AnimatedMark,
+        }}
+      />
+      <Button onClick={() => animate()}>Run Animation</Button>
+    </Stack>
+  );
+}
+
+function AnimatedLine({ d, ownerState }: AnimatedLineProps) {
+  const pathRef = React.useRef<SVGPathElement>(null);
+
+  return (
+    <motion.path
+      ref={pathRef}
+      d={d}
+      fill="transparent"
+      stroke={ownerState.color}
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1.5,
+        ease: 'easeInOut',
+      }}
+    />
+  );
+}
+
+function AnimatedMark(props: MarkElementProps) {
+  const { x, y, color } = props;
+
+  return (
+    <motion.circle
+      cx={x}
+      cy={y}
+      r={5}
+      fill={color}
+      initial={{
+        scale: 0,
+        opacity: 0,
+      }}
+      animate={{
+        scale: 1,
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+        delay: 0.5,
+        ease: 'backOut',
+      }}
+    />
+  );
+}
