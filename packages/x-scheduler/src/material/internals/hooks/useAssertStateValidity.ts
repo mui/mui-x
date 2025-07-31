34d26593979f22cb444dd134bb20d29ca240c1b8
Lukas <llukas.tyla@gmail.com>
2025-07-31 10:14:21 +0300

    Use `warn` from the utils package

--- packages/x-scheduler/src/material/internals/hooks/useAssertStateValidity.ts
+++ packages/x-scheduler/src/material/internals/hooks/useAssertStateValidity.ts
@@ -1,6 +1,6 @@
 'use client';
 import { useSelector } from '@base-ui-components/utils/store';
-import { warn } from '../../../base-ui-copy/utils/warn';
+import { warn } from '@base-ui-components/utils/warn';
 import { EventCalendarStore, selectors } from '../../event-calendar/store';
 
 /**
