624ab9ec4b8f265fd18bc86cddf2078147faaf2e
Bernardo Belchior <bernardo@mui.com>
2025-08-06 11:45:56 +0200

    [charts] add opt-in `FastScatter` component

--- packages/x-charts/src/ScatterChart/index.ts
+++ packages/x-charts/src/ScatterChart/index.ts
@@ -5,3 +5,7 @@ export * from './ScatterMarker.types';
 export * from './ScatterMarker';
 export type { ScatterClasses } from './scatterClasses';
 export { scatterClasses } from './scatterClasses';
+export {
+  FastScatter as Unstable_FastScatter,
+  type FastScatterProps as Unstable_FastScatterProps,
+} from './FastScatter';
