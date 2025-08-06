5bda899e1b6a1f50d28bc8fc5e778ee607d912d3
Bernardo Belchior <bernardo@mui.com>
2025-08-06 11:47:58 +0200

    Add benchmarks

--- test/performance-charts/tests/ScatterChartPro.bench.tsx
+++ test/performance-charts/tests/ScatterChartPro.bench.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { describe } from 'vitest';
 import { ScatterChartPro } from '@mui/x-charts-pro/ScatterChartPro';
+import { FastScatter } from '@mui/x-charts/ScatterChart/FastScatter';
 import { options } from '../utils/options';
 import { bench } from '../utils/bench';
 
@@ -72,4 +73,66 @@ describe('ScatterChartPro', () => {
     },
     options,
   );
+
+  describe('using FastScatter slot', () => {
+    bench(
+      'ScatterChartPro with big data amount',
+      async () => {
+        const { findByText } = render(
+          <ScatterChartPro
+            xAxis={[
+              {
+                id: 'x',
+                data: xData,
+                zoom: { filterMode: 'discard' },
+                valueFormatter: (v: number) => v.toLocaleString('en-US'),
+              },
+            ]}
+            initialZoom={[{ axisId: 'x', start: 20, end: 70 }]}
+            series={[{ data }]}
+            width={500}
+            height={300}
+            slots={{ scatter: FastScatter }}
+          />,
+        );
+
+        await findByText('60', { ignore: 'span' });
+
+        cleanup();
+      },
+      options,
+    );
+
+    bench(
+      'ScatterChartPro with big data amount and zoomed in',
+      async () => {
+        const { findByText } = render(
+          <ScatterChartPro
+            xAxis={[
+              {
+                id: 'x',
+                data: xData,
+                valueFormatter: (v: number) => v.toLocaleString('en-US'),
+                zoom: { minSpan: 0 },
+              },
+            ]}
+            yAxis={[{ id: 'y', zoom: { minSpan: 0 } }]}
+            series={[{ data }]}
+            width={500}
+            height={300}
+            initialZoom={[
+              { axisId: 'x', start: 50, end: 50.1 },
+              { axisId: 'y', start: 50, end: 50.1 },
+            ]}
+            slots={{ scatter: FastScatter }}
+          />,
+        );
+
+        await findByText('50.06', { ignore: 'span' });
+
+        cleanup();
+      },
+      options,
+    );
+  });
 });
