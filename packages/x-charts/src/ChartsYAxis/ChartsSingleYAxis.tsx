7f0b2b1bd3779e2d82c6bb759385095232662653
Jose Quintas <juniorquintas@gmail.com>
2025-08-07 17:23:55 +0200

    useAxisProps

--- packages/x-charts/src/ChartsYAxis/ChartsSingleYAxis.tsx
+++ packages/x-charts/src/ChartsYAxis/ChartsSingleYAxis.tsx
@@ -1,57 +1,43 @@
 'use client';
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import useSlotProps from '@mui/utils/useSlotProps';
-import { useThemeProps, useTheme } from '@mui/material/styles';
-import { useRtl } from '@mui/system/RtlProvider';
 import { useIsHydrated } from '../hooks/useIsHydrated';
-import { getDefaultBaseline, getDefaultTextAnchor } from '../ChartsText/defaultTextPlacement';
 import { getStringSize } from '../internals/domUtils';
 import { useTicks } from '../hooks/useTicks';
 import { useDrawingArea } from '../hooks/useDrawingArea';
 import { ChartsYAxisProps } from '../models/axis';
-import { ChartsText, ChartsTextProps } from '../ChartsText';
-import { isInfinity } from '../internals/isInfinity';
-import { isBandScale } from '../internals/isBandScale';
 import { useChartContext } from '../context/ChartProvider';
-import { useYAxes } from '../hooks';
-import { invertTextAnchor } from '../internals/invertTextAnchor';
 import { shortenLabels } from './shortenLabels';
-import {
-  AXIS_LABEL_TICK_LABEL_GAP,
-  defaultProps,
-  TICK_LABEL_GAP,
-  useUtilityClasses,
-  YAxisRoot,
-} from './utilities';
+import { AXIS_LABEL_TICK_LABEL_GAP, TICK_LABEL_GAP, YAxisRoot } from './utilities';
+import { useAxisProps } from './useAxisProps';
 
 /**
  * @ignore - internal component.
  */
 function ChartsSingleYAxis(inProps: ChartsYAxisProps) {
-  const { yAxisIds, yAxis } = useYAxes();
-  const { scale: yScale, tickNumber, ...settings } = yAxis[inProps.axisId ?? yAxisIds[0]];
-
-  const themedProps = useThemeProps({
-    props: { ...settings, ...inProps },
-    name: 'MuiChartsSingleYAxis',
-  });
-
-  const defaultizedProps = {
-    ...defaultProps,
-    ...themedProps,
-  };
+  const {
+    yScale,
+    defaultizedProps,
+    tickNumber,
+    positionSign,
+    skipAxisRendering,
+    classes,
+    Line,
+    Tick,
+    TickLabel,
+    Label,
+    axisTickLabelProps,
+    axisLabelProps,
+    isRtl,
+  } = useAxisProps(inProps);
 
   const {
     position,
     disableLine,
     disableTicks,
     label,
-    labelStyle,
-    tickLabelStyle,
     tickSize: tickSizeProp,
     valueFormatter,
-    slots,
     slotProps,
     tickPlacement,
     tickLabelPlacement,
@@ -62,15 +48,10 @@ function ChartsSingleYAxis(inProps: ChartsYAxisProps) {
     width: axisWidth,
   } = defaultizedProps;
 
-  const theme = useTheme();
-  const isRtl = useRtl();
-  const isHydrated = useIsHydrated();
-
-  const classes = useUtilityClasses(defaultizedProps);
-
-  const { instance } = useChartContext();
   const drawingArea = useDrawingArea();
   const { left, top, width, height } = drawingArea;
+  const { instance } = useChartContext();
+  const isHydrated = useIsHydrated();
 
   const tickSize = disableTicks ? 4 : tickSizeProp;
 
@@ -84,76 +65,11 @@ function ChartsSingleYAxis(inProps: ChartsYAxisProps) {
     direction: 'y',
   });
 
-  const positionSign = position === 'right' ? 1 : -1;
-
-  const tickFontSize = typeof tickLabelStyle?.fontSize === 'number' ? tickLabelStyle.fontSize : 12;
-
-  const Line = slots?.axisLine ?? 'line';
-  const Tick = slots?.axisTick ?? 'line';
-  const TickLabel = slots?.axisTickLabel ?? ChartsText;
-  const Label = slots?.axisLabel ?? ChartsText;
-
-  const defaultTextAnchor = getDefaultTextAnchor(
-    (position === 'right' ? -90 : 90) - (tickLabelStyle?.angle ?? 0),
-  );
-  const defaultDominantBaseline = getDefaultBaseline(
-    (position === 'right' ? -90 : 90) - (tickLabelStyle?.angle ?? 0),
-  );
-
-  const axisTickLabelProps = useSlotProps({
-    elementType: TickLabel,
-    externalSlotProps: slotProps?.axisTickLabel,
-    additionalProps: {
-      style: {
-        ...theme.typography.caption,
-        fontSize: tickFontSize,
-        textAnchor: isRtl ? invertTextAnchor(defaultTextAnchor) : defaultTextAnchor,
-        dominantBaseline: defaultDominantBaseline,
-        ...tickLabelStyle,
-      },
-    } as Partial<ChartsTextProps>,
-    className: classes.tickLabel,
-    ownerState: {},
-  });
-
-  const axisLabelProps = useSlotProps({
-    elementType: Label,
-    externalSlotProps: slotProps?.axisLabel,
-    additionalProps: {
-      style: {
-        ...theme.typography.body1,
-        lineHeight: 1,
-        fontSize: 14,
-        angle: positionSign * 90,
-        textAnchor: 'middle',
-        dominantBaseline: 'text-before-edge',
-        ...labelStyle,
-      } as Partial<ChartsTextProps>['style'],
-    } as Partial<ChartsTextProps>,
-    ownerState: {},
-  });
-
-  const lineSlotProps = useSlotProps({
-    elementType: Line,
-    externalSlotProps: slotProps?.axisLine,
-    additionalProps: {
-      strokeLinecap: 'square' as const,
-    },
-    ownerState: {},
-  });
-
-  const domain = yScale.domain();
-  const ordinalAxis = isBandScale(yScale);
-
   // Skip axis rendering if no data is available
   // - The domain is an empty array for band/point scales.
   // - The domains contains Infinity for continuous scales.
-  // - The position is 'none'.
-  if (
-    (ordinalAxis && domain.length === 0) ||
-    (!ordinalAxis && domain.some(isInfinity)) ||
-    position === 'none'
-  ) {
+  // - The position is set to 'none'.
+  if (skipAxisRendering) {
     return null;
   }
 
@@ -181,7 +97,7 @@ function ChartsSingleYAxis(inProps: ChartsYAxisProps) {
       sx={sx}
     >
       {!disableLine && (
-        <Line y1={top} y2={top + height} className={classes.line} {...lineSlotProps} />
+        <Line y1={top} y2={top + height} className={classes.line} {...slotProps?.axisLine} />
       )}
 
       {yTicks.map((item, index) => {
