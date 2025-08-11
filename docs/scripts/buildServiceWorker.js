e2005d8633e27fb413473278b0788872d7a8cf34
Bernardo Belchior <bernardo@mui.com>
2025-08-11 18:18:55 +0200

    Replace copy with copyFile

--- docs/scripts/buildServiceWorker.js
+++ docs/scripts/buildServiceWorker.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 const path = require('path');
 const fs = require('fs/promises');
-const fse = require('fs-extra');
 
 async function prepend(file, string) {
   const data = await fs.readFile(file, 'utf8');
@@ -12,7 +11,7 @@ async function run() {
   const swDest = path.join(__dirname, '../export/x/sw.js');
   const swSrc = path.join(__dirname, '../src/sw.js');
 
-  await fse.copy(swSrc, swDest);
+  await fs.copyFile(swSrc, swDest);
   await prepend(
     swDest,
     `
