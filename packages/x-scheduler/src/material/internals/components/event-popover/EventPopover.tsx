38579679563647e16691b1cc97aef8626fd3a4fe
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-08-12 12:28:05 +0200

    remove console log

--- packages/x-scheduler/src/material/internals/components/event-popover/EventPopover.tsx
+++ packages/x-scheduler/src/material/internals/components/event-popover/EventPopover.tsx
@@ -91,10 +91,7 @@ export const EventPopover = React.forwardRef(function EventPopover(
     const endDateValue = form.get('endDate');
     const endTimeValue = form.get('endTime');
     const recurrenceKey = form.get('recurrence') as string;
-
     const recurrenceRule = recurrenceKey ? recurrencePresets[recurrenceKey] : undefined;
-    // eslint-disable-next-line no-console
-    console.log('Submitted Recurrence Rule:', recurrenceRule);
 
     const startISO = startTimeValue
       ? `${startDateValue}T${startTimeValue}`
