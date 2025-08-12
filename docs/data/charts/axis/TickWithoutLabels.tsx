f43ff7fc01a2540e78738e96cf8c29432cf37fec
Bernardo Belchior <bernardo@mui.com>
2025-08-12 10:18:38 +0100

    Improve demo

--- docs/data/charts/axis/TickWithoutLabels.tsx
+++ docs/data/charts/axis/TickWithoutLabels.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import { ScatterChart } from '@mui/x-charts/ScatterChart';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { AxisValueFormatterContext } from '@mui/x-charts/models';
 import { earthquakeData } from '../dataset/earthquakeData';
 
-const otherSetting = {
-  height: 300,
+const settings = {
+  height: 400,
   grid: { horizontal: true },
 };
 const data = Object.entries(earthquakeData).reduce((acc, [magnitude, events]) => {
@@ -17,24 +25,70 @@ const formatter = new Intl.NumberFormat('en-US', {
   maximumFractionDigits: 1,
 });
 
-export default function FormatterDemo() {
-  return (
-    <ScatterChart
-      xAxis={[{ height: 40 }]}
-      yAxis={[
-        {
-          scaleType: 'log',
-          valueFormatter(value, context) {
-            if (context.location === 'tick' && context.defaultTickLabel === '') {
-              return '';
-            }
+function valueFormatterIgnoreEmpty(value: any, context: AxisValueFormatterContext) {
+  if (context.location === 'tick' && context.defaultTickLabel === '') {
+    return '';
+  }
 
-            return formatter.format(value);
-          },
-        },
-      ]}
-      series={[{ label: 'Seoul rainfall', data }]}
-      {...otherSetting}
-    />
+  return formatter.format(value);
+}
+
+function valueFormatterShowAll(value: any) {
+  return formatter.format(value);
+}
+
+export default function TickWithoutLabels() {
+  const [tickFormatter, setTickFormatter] = React.useState(
+    () => valueFormatterIgnoreEmpty,
+  );
+
+  return (
+    <Stack width="100%" direction={{ md: 'row' }} alignItems="center" rowGap={2}>
+      <Stack width="100%">
+        <Typography variant="h6" textAlign="center">
+          Worldwide Earthquake Count and Magnitude, 2020-2024
+        </Typography>
+        <ScatterChart
+          {...settings}
+          xAxis={[{ height: 40, label: 'Magnitude' }]}
+          yAxis={[
+            {
+              scaleType: 'log',
+              valueFormatter: tickFormatter,
+              label: 'Number of events (Log scale)',
+              width: 48,
+            },
+          ]}
+          series={[{ data }]}
+        />
+        <Typography variant="caption">Source: US Geological Survey</Typography>
+      </Stack>
+      <FormControl sx={{ flexShrink: 0 }}>
+        <FormLabel id="tick-format-label">Tick Format</FormLabel>
+        <RadioGroup
+          aria-labelledby="tick-format-label"
+          name="tick-format"
+          value={tickFormatter === valueFormatterIgnoreEmpty ? 'empty' : 'all'}
+          onChange={(event) =>
+            setTickFormatter(() =>
+              event.target.value === 'empty'
+                ? valueFormatterIgnoreEmpty
+                : valueFormatterShowAll,
+            )
+          }
+        >
+          <FormControlLabel
+            value="empty"
+            control={<Radio />}
+            label="Ignore empty ticks"
+          />
+          <FormControlLabel
+            value="all"
+            control={<Radio />}
+            label="Show all values"
+          />
+        </RadioGroup>
+      </FormControl>
+    </Stack>
   );
 }
