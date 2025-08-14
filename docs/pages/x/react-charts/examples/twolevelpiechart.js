8b4970952342190acbf660e6fd1dba67797538a9
alelthomas <alelthommas@gmail.com>
2025-08-14 15:26:50 -0400

    remove unused files

--- docs/pages/x/react-charts/examples/twolevelpiechart.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import * as pageProps from 'docsx/data/charts/examples/twolevelpiechart.md?muiMarkdown';
-
-export default function Page() {
-  return <MarkdownDocs {...pageProps} disableAd />;
-}
