8a2958b8a7dd68e15271306cdac508c9dd52acce
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-08-18 10:31:57 +0200

    make object static

--- packages/x-scheduler/src/primitives/use-event-calendar/useEventCalendar.ts
+++ packages/x-scheduler/src/primitives/use-event-calendar/useEventCalendar.ts
@@ -23,6 +23,7 @@ import { useAssertStateValidity } from '../utils/useAssertStateValidity';
 export const AGENDA_VIEW_DAYS_AMOUNT = 12;
 
 const DEFAULT_VIEWS: CalendarView[] = ['week', 'day', 'month', 'agenda'];
+const DEFAULT_SETTINGS: CalendarSettings = { hideWeekends: false };
 const EMPTY_ARRAY: any[] = [];
 
 export function useEventCalendar(
@@ -47,7 +48,7 @@ export function useEventCalendar(
     areEventsDraggable = false,
     areEventsResizable = false,
     ampm = true,
-    settings: settingsProp = { hideWeekends: false },
+    settings: settingsProp = DEFAULT_SETTINGS,
   } = parameters;
 
   const store = useRefWithInit(
