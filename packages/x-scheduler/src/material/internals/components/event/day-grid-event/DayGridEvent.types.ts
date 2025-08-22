fbda8ce703b96257d0421e4b7e8d1b57fa7e5623
Nora <72460825+noraleonte@users.noreply.github.com>
2025-08-22 16:31:21 +0300

    Apply suggestions from code review - Flavien
    Co-authored-by: Flavien DELANGLE <flaviendelangle@gmail.com>
    Signed-off-by: Nora <72460825+noraleonte@users.noreply.github.com>

--- packages/x-scheduler/src/material/internals/components/event/day-grid-event/DayGridEvent.types.ts
+++ packages/x-scheduler/src/material/internals/components/event/day-grid-event/DayGridEvent.types.ts
@@ -6,13 +6,12 @@ export interface DayGridEventProps extends EventProps {
    * The variant of the event, which determines its styling.
    */
   variant: Extract<EventVariant, 'compact' | 'allDay' | 'invisible'>;
-
   /**
-   * The number of the row the event should be placed on
+   * The index of the row the event should be placed on.
    */
   gridRow?: number;
   /**
-   * The number of columns the event spans across
+   * The amount of columns the event spans across.
    */
   columnSpan?: number;
 }
