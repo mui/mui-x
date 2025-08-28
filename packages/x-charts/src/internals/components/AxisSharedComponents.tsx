6f04e207c07ed13075efe0eabd61bc916855b392
Jose Quintas <juniorquintas@gmail.com>
2025-08-29 01:31:06 +0200

    cleanup

--- packages/x-charts/src/internals/components/AxisSharedComponents.tsx
+++ packages/x-charts/src/internals/components/AxisSharedComponents.tsx
@@ -1,7 +1,6 @@
 import { styled } from '@mui/material/styles';
-import { axisClasses } from '../../ChartsAxis/axisClasses';
 
 export const AxisRoot = styled('g', {
   name: 'MuiChartsAxis',
   slot: 'Root',
-})(({ theme }) => ({}));
+})(() => ({}));
