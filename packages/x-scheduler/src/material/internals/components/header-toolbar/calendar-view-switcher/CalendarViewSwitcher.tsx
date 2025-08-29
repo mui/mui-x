6878dad5762998197d688673f4cdaa624d64229d
Nora <72460825+noraleonte@users.noreply.github.com>
2025-08-29 18:47:36 +0300

    Suggestion - Flavien
    Co-authored-by: Flavien DELANGLE <flaviendelangle@gmail.com>
    Signed-off-by: Nora <72460825+noraleonte@users.noreply.github.com>

--- packages/x-scheduler/src/material/internals/components/header-toolbar/calendar-view-switcher/CalendarViewSwitcher.tsx
+++ packages/x-scheduler/src/material/internals/components/header-toolbar/calendar-view-switcher/CalendarViewSwitcher.tsx
@@ -10,6 +10,7 @@ export function CalendarViewSwitcher(props: React.HTMLAttributes<HTMLDivElement>
 
   const { store } = useEventCalendarContext();
   const views = useStore(store, selectors.views);
+  // TODO: Remove once we decide how to integrate the Timeline in the top level component
   const viewsWithoutTimeline = views.filter((view) => view !== 'timeline');
 
   return <ViewSwitcher views={viewsWithoutTimeline} {...other} />;
