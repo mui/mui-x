33b63a248b3da2492af27e724362f4caea3d4d6e
Flavien DELANGLE <flaviendelangle@gmail.com>
2025-08-29 11:55:55 +0200

    Update packages/x-scheduler/src/primitives/models/preferences.ts
    Co-authored-by: Rita <47579513+rita-codes@users.noreply.github.com>
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>

--- packages/x-scheduler/src/primitives/models/preferences.ts
+++ packages/x-scheduler/src/primitives/models/preferences.ts
@@ -11,5 +11,5 @@ export interface CalendarPreferencesMenuConfig {
    * Whether the menu item to toggle weekend visibility is visible.
    * @default true
    */
-  toggleWeekEndVisibility: boolean;
+  toggleWeekendVisibility: boolean;
 }
