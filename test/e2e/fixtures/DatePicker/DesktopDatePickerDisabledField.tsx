3e4c3b8e2b661718c920e4bb66c3faa1a182544b
Utkarsh Agarwal <agarwalutkarsh58@gmail.com>
2025-09-01 15:34:22 +0530

    added e2e test for checking disabled date-picker

---
+++ test/e2e/fixtures/DatePicker/DesktopDatePickerDisabledField.tsx
@@ -0,0 +1,16 @@
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
+export default function BasicDesktopDatePicker() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DesktopDatePicker']}>
+        <button data-testid="start-btn">Start Button</button>
+        <DesktopDatePicker label="disabled" disabled />
+        <button data-testid="end-btn">End Button</button>
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
