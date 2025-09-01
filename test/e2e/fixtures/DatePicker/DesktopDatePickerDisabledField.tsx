31ccfbff8dda4e99143230f487aae3cf2a2eab4c
Utkarsh Agarwal <agarwalutkarsh58@gmail.com>
2025-09-01 15:48:22 +0530

    fix lint errors

--- test/e2e/fixtures/DatePicker/DesktopDatePickerDisabledField.tsx
+++ test/e2e/fixtures/DatePicker/DesktopDatePickerDisabledField.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
@@ -7,9 +8,13 @@ export default function BasicDesktopDatePicker() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DesktopDatePicker']}>
-        <button data-testid="start-btn">Start Button</button>
+        <button type="button" data-testid="start-btn">
+          Start Button
+        </button>
         <DesktopDatePicker label="disabled" disabled />
-        <button data-testid="end-btn">End Button</button>
+        <button type="button" data-testid="end-btn">
+          End Button
+        </button>
       </DemoContainer>
     </LocalizationProvider>
   );
