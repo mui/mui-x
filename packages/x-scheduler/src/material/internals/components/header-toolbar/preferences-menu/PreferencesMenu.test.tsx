0982f63b3e0d7e21b6ef99edde4beed9024ffd2c
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-09-02 17:13:19 +0200

    add more tests

--- packages/x-scheduler/src/material/internals/components/header-toolbar/preferences-menu/PreferencesMenu.test.tsx
+++ packages/x-scheduler/src/material/internals/components/header-toolbar/preferences-menu/PreferencesMenu.test.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { createSchedulerRenderer } from 'test/utils/scheduler';
 import { StandaloneView } from '@mui/x-scheduler/material/standalone-view';
+import { screen } from '@mui/internal-test-utils';
 import { PreferencesMenu } from './PreferencesMenu';
-import { getPreferencesMenu } from '../../../utils/test-utils';
+import { getPreferencesMenu, openPreferencesMenu } from '../../../utils/test-utils';
 
 describe('<PreferencesMenu />', () => {
   const anchor = document.createElement('button');
@@ -54,5 +55,39 @@ describe('<PreferencesMenu />', () => {
     );
 
     expect(getPreferencesMenu()).to.equal(null);
+  });
+
+  it('should hide hideWeekends option when toggleWeekendVisibility is false', async () => {
+    const { user } = render(
+      <StandaloneView
+        events={[]}
+        preferencesMenuConfig={{ toggleWeekendVisibility: false, toggleWeekNumberVisibility: true }}
+      >
+        <PreferencesMenu />
+      </StandaloneView>,
+    );
+
+    await openPreferencesMenu(user);
+
+    expect(screen.queryByRole('menuitemcheckbox', { name: /hide weekends/i })).to.equal(null);
+    expect(screen.queryByRole('menuitemcheckbox', { name: /hide week number/i })).not.to.equal(
+      null,
+    );
+  });
+
+  it('should hide hideWeekNumber option when toggleWeekNumberVisibility is false', async () => {
+    const { user } = render(
+      <StandaloneView
+        events={[]}
+        preferencesMenuConfig={{ toggleWeekendVisibility: true, toggleWeekNumberVisibility: false }}
+      >
+        <PreferencesMenu />
+      </StandaloneView>,
+    );
+
+    await openPreferencesMenu(user);
+
+    expect(screen.queryByRole('menuitemcheckbox', { name: /hide weekends/i })).not.to.equal(null);
+    expect(screen.queryByRole('menuitemcheckbox', { name: /hide week number/i })).to.equal(null);
   });
 });
