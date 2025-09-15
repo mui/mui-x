0fa8a5751a4ef00079eb21c240bc6371982163e7
Armin Mehinovic <armin@mui.com>
2025-09-15 10:46:41 +0200

    server side pivoting page and demos

---
+++ docs/data/data-grid/server-side-data/ServerSidePivotingGroupExpansion.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { DataGridPremium } from '@mui/x-data-grid-premium';
+import { useMockServer } from '@mui/x-data-grid-generator';
+
+const pivotModel = {
+  rows: [{ field: 'commodity' }, { field: 'traderName' }],
+  columns: [{ field: 'status' }],
+  values: [{ field: 'quantity', aggFunc: 'sum' }],
+};
+
+const aggregationFunctions = {
+  sum: { columnTypes: ['number'] },
+  avg: { columnTypes: ['number'] },
+  min: { columnTypes: ['number', 'date', 'dateTime'] },
+  max: { columnTypes: ['number', 'date', 'dateTime'] },
+  size: {},
+};
+
+export default function ServerSidePivotingGroupExpansion() {
+  const {
+    columns,
+    initialState: initialStateMock,
+    fetchRows,
+  } = useMockServer({
+    rowLength: 20,
+    dataSet: 'Commodity',
+    maxColumns: 20,
+  });
+
+  const dataSource = React.useMemo(
+    () => ({
+      getRows: async (params) => {
+        const urlParams = new URLSearchParams({
+          paginationModel: JSON.stringify(params.paginationModel),
+          filterModel: JSON.stringify(params.filterModel),
+          sortModel: JSON.stringify(params.sortModel),
+          groupKeys: JSON.stringify(params.groupKeys),
+          groupFields: JSON.stringify(params.groupFields),
+          pivotModel: JSON.stringify(params.pivotModel),
+        });
+        const getRowsResponse = await fetchRows(
+          `https://mui.com/x/api/data-grid?${urlParams.toString()}`,
+        );
+        return {
+          rows: getRowsResponse.rows,
+          rowCount: getRowsResponse.rowCount,
+          aggregateRow: getRowsResponse.aggregateRow,
+          pivotColumns: getRowsResponse.pivotColumns,
+        };
+      },
+      getGroupKey: (row) => row.group,
+      getChildrenCount: (row) => row.descendantCount,
+      getAggregatedValue: (row, field) => row[field],
+      getPivotColumnDef: (field, columnGroupPath) => ({
+        field: columnGroupPath
+          .map((path) =>
+            typeof path.value === 'string' ? path.value : path.value[path.field],
+          )
+          .concat(field)
+          .join('>->'),
+      }),
+    }),
+    [fetchRows],
+  );
+
+  const initialState = React.useMemo(
+    () => ({
+      ...initialStateMock,
+      pivoting: {
+        model: pivotModel,
+        enabled: true,
+      },
+    }),
+    [initialStateMock],
+  );
+
+  return (
+    <div style={{ height: 600, width: '100%' }}>
+      <DataGridPremium
+        columns={columns}
+        dataSource={dataSource}
+        showToolbar
+        initialState={initialState}
+        aggregationFunctions={aggregationFunctions}
+        defaultGroupingExpansionDepth={-1}
+      />
+    </div>
+  );
+}
