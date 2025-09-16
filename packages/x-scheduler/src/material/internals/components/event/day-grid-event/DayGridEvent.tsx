e6dae99ea48d1849403f3d45d6a74853c9d4bc78
flavien <flaviendelangle@gmail.com>
2025-09-16 10:35:53 +0200

    Disable drag in day view

--- packages/x-scheduler/src/material/internals/components/event/day-grid-event/DayGridEvent.tsx
+++ packages/x-scheduler/src/material/internals/components/event/day-grid-event/DayGridEvent.tsx
@@ -36,9 +36,10 @@ export const DayGridEvent = React.forwardRef(function DayGridEvent(
   const id = useId(idProp);
   const translations = useTranslations();
   const store = useEventCalendarStoreContext();
-  const isDraggable = useStore(store, selectors.isEventDraggable, occurrence);
+  const isDayView = useStore(store, selectors.isDayView);
+  const isDraggable = useStore(store, selectors.isEventDraggable, occurrence) && !isDayView;
   const isResizable =
-    useStore(store, selectors.isEventResizable, occurrence) && variant === 'allDay';
+    useStore(store, selectors.isEventResizable, occurrence) && variant === 'allDay' && !isDayView;
   const ampm = useStore(store, selectors.ampm);
   const resource = useStore(store, selectors.resource, occurrence.resource);
   const color = useStore(store, selectors.eventColor, occurrence.id);
