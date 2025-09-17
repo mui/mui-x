feadf164db4002a83b60edd364da28ab088d728c
Bernardo Belchior <bernardo@mui.com>
2025-09-17 11:41:29 +0100

    [charts] Fix performance issue with JS animations

---
+++ packages/x-charts/src/internals/animation/Transition.test.ts
@@ -0,0 +1,68 @@
+import { afterAll, beforeAll, expect } from 'vitest';
+import { Transition } from './Transition';
+
+describe('Transition', () => {
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  const duration = 200;
+  const easingFn = (t: number) => t;
+
+  it('starts automatically on creation', () => {
+    let ticks = 0;
+    const transition = new Transition(duration, easingFn, () => {
+      ticks += 1;
+    });
+
+    expect(ticks).to.eq(0);
+    vi.advanceTimersToNextFrame();
+    expect(ticks).to.eq(1);
+    transition.stop();
+  });
+
+  it('stops when `stop()` is called', () => {
+    let ticks = 0;
+    const transition = new Transition(duration, easingFn, () => {
+      ticks += 1;
+    });
+
+    expect(ticks).to.eq(0);
+    transition.stop();
+    vi.advanceTimersToNextFrame();
+    expect(ticks).to.eq(0);
+  });
+
+  it('resumes after stopping', () => {
+    let ticks = 0;
+    const transition = new Transition(duration, easingFn, () => {
+      ticks += 1;
+    });
+
+    expect(ticks).to.eq(0);
+    transition.stop();
+    transition.resume();
+    expect(ticks).to.eq(0);
+    vi.advanceTimersToNextFrame();
+    expect(ticks).to.eq(1);
+
+    transition.stop();
+  });
+
+  it('finishes when `finish()` is called', () => {
+    let ticks = 0;
+    const transition = new Transition(duration, easingFn, () => {
+      ticks += 1;
+    });
+
+    expect(ticks).to.eq(0);
+    transition.finish();
+    vi.advanceTimersToNextFrame();
+    vi.advanceTimersToNextFrame();
+    expect(ticks).to.eq(1);
+  });
+});
