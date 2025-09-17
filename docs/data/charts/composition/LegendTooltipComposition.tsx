a60e86a0c9221c10aa4e7e7a278ff5855025eee3
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2025-09-17 11:53:12 +0300

    review

--- docs/data/charts/composition/LegendTooltipComposition.tsx
+++ docs/data/charts/composition/LegendTooltipComposition.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import { ChartDataProvider } from '@mui/x-charts/ChartDataProvider';
 import { ChartsSurface } from '@mui/x-charts/ChartsSurface';
-import {
-  LinePlot,
-  MarkPlot,
-  lineElementClasses,
-  markElementClasses,
-} from '@mui/x-charts/LineChart';
+import { LinePlot, MarkPlot } from '@mui/x-charts/LineChart';
 import { ChartsLegend } from '@mui/x-charts/ChartsLegend';
 import { ChartsTooltip } from '@mui/x-charts/ChartsTooltip';
 import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
 import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
 import { ChartsAxisHighlight } from '@mui/x-charts/ChartsAxisHighlight';
+import Box from '@mui/material/Box';
 
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const xLabels = [
@@ -26,35 +22,32 @@ const xLabels = [
 
 export default function LegendTooltipComposition() {
   return (
-    <ChartDataProvider
-      width={500}
-      height={300}
-      series={[{ type: 'line', data: pData, label: 'Sales Data' }]}
-      xAxis={[{ scaleType: 'point', data: xLabels }]}
-      margin={{ top: 40, right: 20, bottom: 20, left: 20 }}
+    <Box
+      sx={{
+        width: '100%',
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
     >
-      <ChartsLegend />
-      <ChartsTooltip />
-      <ChartsSurface
-        sx={{
-          [`& .${lineElementClasses.root}`]: {
-            stroke: '#8884d8',
-            strokeWidth: 2,
-          },
-          [`& .${markElementClasses.root}`]: {
-            stroke: '#8884d8',
-            r: 4,
-            fill: '#fff',
-            strokeWidth: 2,
-          },
-        }}
+      <ChartDataProvider
+        height={300}
+        series={[{ type: 'line', data: pData, label: 'Sales Data' }]}
+        xAxis={[{ scaleType: 'point', data: xLabels }]}
+        yAxis={[{ width: 50 }]}
+        margin={{ top: 30, right: 30, bottom: 20, left: 20 }}
       >
-        <ChartsXAxis />
-        <ChartsYAxis />
-        <LinePlot />
-        <MarkPlot />
-        <ChartsAxisHighlight x="line" />
-      </ChartsSurface>
-    </ChartDataProvider>
+        <ChartsLegend />
+        <ChartsTooltip />
+        <ChartsSurface>
+          <ChartsXAxis />
+          <ChartsYAxis />
+          <LinePlot />
+          <MarkPlot />
+          <ChartsAxisHighlight x="line" />
+        </ChartsSurface>
+      </ChartDataProvider>
+    </Box>
   );
 }
