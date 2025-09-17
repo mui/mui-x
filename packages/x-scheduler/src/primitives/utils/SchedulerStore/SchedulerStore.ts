f206fc1f4cee0f9e0b3454118f2ca1f27d065289
flavien <flaviendelangle@gmail.com>
2025-09-17 09:17:39 +0200

    Fix

--- packages/x-scheduler/src/primitives/utils/SchedulerStore/SchedulerStore.ts
+++ packages/x-scheduler/src/primitives/utils/SchedulerStore/SchedulerStore.ts
@@ -1,4 +1,5 @@
 import { Store } from '@base-ui-components/utils/store';
+// TODO: Use the Base UI warning utility once it supports cleanup in tests.
 import { warnOnce } from '@mui/x-internals/warning';
 import {
   CalendarEvent,
