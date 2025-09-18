6165ef7f09ae214118efca910b0ef6829b4b7e3c
Jose Quintas <juniorquintas@gmail.com>
2025-09-18 20:04:55 +0200

    Revert "memo obj"
    This reverts commit 8be2e5b2043f65fc6dbb53f4c1698c44f61a9523.

--- packages/x-charts-pro/src/internals/plugins/useChartProZoom/useChartProZoom.ts
+++ packages/x-charts-pro/src/internals/plugins/useChartProZoom/useChartProZoom.ts
@@ -10,7 +10,6 @@ import {
   selectorChartAxisZoomOptionsLookup,
 } from '@mui/x-charts/internals';
 import debounce from '@mui/utils/debounce';
-import { useMemoObject } from '@mui/x-internals/useMemoObject';
 import { useEventCallback } from '@mui/material/utils';
 import { calculateZoom } from './calculateZoom';
 import { UseChartProZoomSignature } from './useChartProZoom.types';
@@ -25,22 +24,30 @@ export const useChartProZoom: ChartPlugin<UseChartProZoomSignature> = (pluginDat
   const {
     zoomData: paramsZoomData,
     onZoomChange: onZoomChangeProp,
-    zoomInteractionConfig: zoomInteractionConfigProp,
+    zoomInteractionConfig,
   } = params;
 
   const onZoomChange = useEventCallback(onZoomChangeProp ?? (() => {}));
   const optionsLookup = useSelector(store, selectorChartZoomOptionsLookup);
-  const zoomInteractionConfig = useMemoObject(
-    initializeZoomInteractionConfig(zoomInteractionConfigProp),
-  );
 
+  // Check if fixes codspeed performance
   React.useEffect(() => {
     store.update((prevState) => {
+      // Should deep compare prevState.zoom.zoomInteractionConfig with the new initializedZoomConfig
+
+      const initializedZoomConfig = initializeZoomInteractionConfig(zoomInteractionConfig);
+      if (
+        JSON.stringify(prevState.zoom.zoomInteractionConfig) ===
+        JSON.stringify(initializedZoomConfig)
+      ) {
+        return prevState;
+      }
+
       return {
         ...prevState,
         zoom: {
           ...prevState.zoom,
-          zoomInteractionConfig,
+          zoomInteractionConfig: initializedZoomConfig,
         },
       };
     });
