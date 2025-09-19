50b9281e60fd20b3c5939654f3f0daa868f863ce
Janpot <2109932+Janpot@users.noreply.github.com>
2025-09-19 16:38:26 +0200

    run in ci

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,20 +1,3 @@
 export default {
   extends: '@mui/internal-code-infra/stylelint',
-  overrides: [
-    {
-      files: [
-        'docs/data/material/integrations/interoperability/**/*.js',
-        'docs/data/material/integrations/interoperability/**/*.tsx',
-      ],
-      rules: {
-        'nesting-selector-no-missing-scoping-root': null,
-      },
-    },
-    {
-      files: ['docs/data/joy/**/*.js', 'docs/data/joy/**/*.tsx'],
-      rules: {
-        'property-no-deprecated': null,
-      },
-    },
-  ],
 };
