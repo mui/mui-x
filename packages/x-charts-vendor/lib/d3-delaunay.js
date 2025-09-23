94a087849f3d90a5d833485f65cc7177d5bb5bb3
Bernardo Belchior <bernardo@mui.com>
2025-09-23 17:41:58 +0200

    [charts] Use more performant data structure for closest point

--- packages/x-charts-vendor/lib/d3-delaunay.js
+++
@@ -1,6 +0,0 @@
-
-// `x-charts-vendor/d3-delaunay` (CommonJS)
-// See upstream license: https://github.com/d3/d3-delaunay/blob/main/LICENSE
-//
-// Our CommonJS package relies on transpiled vendor files in `lib-vendor/d3-delaunay`
-module.exports = require("../lib-vendor/d3-delaunay/src/index.js");
