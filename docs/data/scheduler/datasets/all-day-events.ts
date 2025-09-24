784746e6a5ff25959b4716fe77abd48113f2054b
flavien <flaviendelangle@gmail.com>
2025-09-24 11:30:23 +0200

    Fix

--- docs/data/scheduler/datasets/all-day-events.ts
+++ docs/data/scheduler/datasets/all-day-events.ts
@@ -3,7 +3,6 @@
 
 import { DateTime } from 'luxon';
 import { CalendarEvent, CalendarResource } from '@mui/x-scheduler/primitives/models';
-import { weekdays } from 'moment';
 
 export const defaultVisibleDate = DateTime.fromISO('2025-07-01T00:00:00');
 
