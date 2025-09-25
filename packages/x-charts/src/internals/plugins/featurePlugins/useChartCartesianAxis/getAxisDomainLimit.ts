d982469f8f30030f50b20ae59320b6a7f4e1850b
sarathkumarsasi <sarathkumar@qburst.com>
2025-09-25 17:07:48 +0530

    [charts] : Fix linting issue.

--- packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/getAxisDomainLimit.ts
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/getAxisDomainLimit.ts
@@ -1,14 +1,17 @@
+import { NumberValue } from '@mui/x-charts-vendor/d3-scale';
 import { AxisConfig } from '../../../../models/axis';
 import { CartesianChartSeriesType } from '../../../../models/seriesType/config';
 import { ProcessedSeries } from '../../corePlugins/useChartSeries';
-import { NumberValue } from '@mui/x-charts-vendor/d3-scale';
 
 export const getAxisDomainLimit = <T extends CartesianChartSeriesType>(
   axis: Pick<AxisConfig, 'id' | 'domainLimit'>,
   axisDirection: 'x' | 'y',
   axisIndex: number,
   formattedSeries: ProcessedSeries<T | 'line'>,
-): 'nice' | 'strict' | ((min: NumberValue, max: NumberValue) => { min: NumberValue; max: NumberValue }) => {
+):
+  | 'nice'
+  | 'strict'
+  | ((min: NumberValue, max: NumberValue) => { min: NumberValue; max: NumberValue }) => {
   if (axis.domainLimit !== undefined) {
     return axis.domainLimit;
   }
