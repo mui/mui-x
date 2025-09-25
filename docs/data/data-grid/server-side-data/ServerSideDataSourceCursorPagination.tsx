39fb891c56cb8c222a8f327fab58fe81a9e49110
siriwatknp <siriwatkunaporn@gmail.com>
2025-09-25 09:30:18 +0700

    add recipe

---
+++ docs/data/data-grid/server-side-data/ServerSideDataSourceCursorPagination.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { DataGrid, GridDataSource } from '@mui/x-data-grid';
+import { useMockServer } from '@mui/x-data-grid-generator';
+
+export default function ServerSideDataSourceCursorPagination() {
+  const { columns, initialState, fetchRows } = useMockServer(
+    {},
+    { useCursorPagination: true },
+  );
+
+  const dataSource: GridDataSource = React.useMemo(
+    () => ({
+      getRows: async (params) => {
+        const cursor = await params.getCursor();
+        const urlParams = new URLSearchParams({
+          paginationModel: JSON.stringify(params.paginationModel),
+          filterModel: JSON.stringify(params.filterModel),
+          sortModel: JSON.stringify(params.sortModel),
+          cursor: String(cursor ?? ''),
+        });
+        const getRowsResponse = await fetchRows(
+          `https://mui.com/x/api/data-grid?${urlParams.toString()}`,
+        );
+        return {
+          rows: getRowsResponse.rows,
+          rowCount: getRowsResponse.rowCount,
+          pageInfo: getRowsResponse.pageInfo,
+        };
+      },
+    }),
+    [fetchRows],
+  );
+
+  return (
+    <div style={{ width: '100%', height: 400 }}>
+      <DataGrid
+        columns={columns}
+        dataSource={dataSource}
+        pagination
+        initialState={{
+          ...initialState,
+          pagination: { paginationModel: { pageSize: 10, page: 0 }, rowCount: 0 },
+        }}
+        pageSizeOptions={[10, 20, 50]}
+      />
+    </div>
+  );
+}
