d200d994eab7b6d901ea59237b3e52f600f1b0e9
delangle <flaviendelangle@gmail.com>
2025-10-01 15:16:37 +0200

    Fix

--- packages/x-scheduler/src/primitives/use-adapter/useAdapter.ts
+++ packages/x-scheduler/src/primitives/use-adapter/useAdapter.ts
@@ -1,8 +1,8 @@
 import { AdapterLuxon } from './AdapterLuxon';
 
-export const DO_NOT_USE_ADAPTER_CLASS = new AdapterLuxon();
+export const DO_NOT_USE_THIS_VARIABLE_ADAPTER_CLASS = new AdapterLuxon();
 
 // TODO: Replace with Base UI adapter when available.
 export function useAdapter() {
-  return DO_NOT_USE_ADAPTER_CLASS;
+  return DO_NOT_USE_THIS_VARIABLE_ADAPTER_CLASS;
 }
