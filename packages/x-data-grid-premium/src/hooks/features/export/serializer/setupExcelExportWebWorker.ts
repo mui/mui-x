2b538c33841c3d5354f725b3e75674bdbd3a0836
Andrew <andrew@mui.com>
2025-10-02 18:55:13 +0200

    use exceljs fork

--- packages/x-data-grid-premium/src/hooks/features/export/serializer/setupExcelExportWebWorker.ts
+++ packages/x-data-grid-premium/src/hooks/features/export/serializer/setupExcelExportWebWorker.ts
@@ -1,4 +1,4 @@
-import type * as Excel from 'exceljs';
+import type * as Excel from '@mui/x-internal-exceljs-fork';
 import type { GridExcelExportOptions } from '../gridExcelExportInterface';
 import {
   addColumnGroupingHeaders,
