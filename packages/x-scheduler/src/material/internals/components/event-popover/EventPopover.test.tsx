943eb68f526ca64e29e9ee35906d6111a7ef4ab9
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-10-06 12:24:03 +0200

    fix

--- packages/x-scheduler/src/material/internals/components/event-popover/EventPopover.test.tsx
+++ packages/x-scheduler/src/material/internals/components/event-popover/EventPopover.test.tsx
@@ -16,6 +16,7 @@ import { EventCalendarStoreContext } from '@mui/x-scheduler/primitives/use-event
 import { EventCalendarProvider } from '@mui/x-scheduler/primitives/event-calendar-provider';
 import { EventPopover } from './EventPopover';
 import { getColorClassName } from '../../utils/color-utils';
+import '@mui/x-scheduler/styles.css';
 
 const occurrence: CalendarEventOccurrence = {
   id: '1',
