7fde8451e920557706a4b707cc168271508fa27d
Bernardo Belchior <bernardo@mui.com>
2025-10-08 13:41:56 +0200

    [charts] Refactor domain/scale selectors (#19832)

--- packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/computeAxisValue.ts
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartCartesianAxis/computeAxisValue.ts
@@ -1,3 +1,4 @@
+import { NumberValue } from '@mui/x-charts-vendor/d3-scale';
 import { createScalarFormatter } from '../../../defaultValueFormatters';
 import { ContinuousScaleName, ScaleName } from '../../../../models';
 import {
@@ -12,25 +13,19 @@ import {
   DefaultedAxis,
   AxisValueFormatterContext,
   ComputedAxis,
+  D3Scale,
 } from '../../../../models/axis';
 import { CartesianChartSeriesType, ChartSeriesType } from '../../../../models/seriesType/config';
 import { getColorScale, getOrdinalColorScale, getSequentialColorScale } from '../../../colorScale';
 import { scaleTickNumberByRange } from '../../../ticks';
 import { getScale } from '../../../getScale';
 import { isDateData, createDateFormatter } from '../../../dateHelpers';
-import { getAxisExtrema } from './getAxisExtrema';
 import type { ChartDrawingArea } from '../../../../hooks';
 import { ChartSeriesConfig } from '../../models/seriesConfig';
-import { ComputedAxisConfig, DefaultizedZoomOptions } from './useChartCartesianAxis.types';
+import { ComputedAxisConfig } from './useChartCartesianAxis.types';
 import { ProcessedSeries } from '../../corePlugins/useChartSeries/useChartSeries.types';
-import { GetZoomAxisFilters, ZoomData } from './zoom.types';
+import { ZoomData } from './zoom.types';
 import { getAxisTriggerTooltip } from './getAxisTriggerTooltip';
-import {
-  applyDomainLimit,
-  getActualAxisExtrema,
-  getDomainLimit,
-  ScaleDefinition,
-} from './getAxisScale';
 import { isBandScale, isOrdinalScale } from '../../../scaleGuards';
 
 function getRange(
@@ -55,17 +50,18 @@ export type ComputeResult<T extends ChartsAxisProps> = {
 };
 
 type ComputeCommonParams<T extends ChartSeriesType = ChartSeriesType> = {
-  scales: Record<AxisId, ScaleDefinition>;
+  scales: Record<AxisId, D3Scale>;
   drawingArea: ChartDrawingArea;
   formattedSeries: ProcessedSeries<T>;
   seriesConfig: ChartSeriesConfig<T>;
   zoomMap?: Map<AxisId, ZoomData>;
-  zoomOptions?: Record<AxisId, DefaultizedZoomOptions>;
-  getFilters?: GetZoomAxisFilters;
-  /**
-   * @deprecated To remove in v9. This is an experimental feature to avoid breaking change.
-   */
-  preferStrictDomainInLineCharts?: boolean;
+  domains: Record<
+    AxisId,
+    {
+      domain: ReadonlyArray<string | NumberValue>;
+      tickNumber?: number;
+    }
+  >;
 };
 
 export function computeAxisValue<T extends ChartSeriesType>(
@@ -88,9 +84,7 @@ export function computeAxisValue<T extends ChartSeriesType>({
   seriesConfig,
   axisDirection,
   zoomMap,
-  zoomOptions,
-  getFilters,
-  preferStrictDomainInLineCharts,
+  domains,
 }: ComputeCommonParams<T> & {
   axis?: DefaultedAxis[];
   axisDirection: 'x' | 'y';
@@ -110,11 +104,9 @@ export function computeAxisValue<T extends ChartSeriesType>({
   );
 
   const completeAxis: ComputedAxisConfig<ChartsAxisProps> = {};
-  allAxis.forEach((eachAxis, axisIndex) => {
+  allAxis.forEach((eachAxis) => {
     const axis = eachAxis as Readonly<DefaultedAxis<ScaleName, any, Readonly<ChartsAxisProps>>>;
-    const scaleDefinition = scales[axis.id];
-    let scale = scaleDefinition.scale;
-    const zoomOption = zoomOptions?.[axis.id];
+    const scale = scales[axis.id];
     const zoom = zoomMap?.get(axis.id);
     const zoomRange: [number, number] = zoom ? [zoom.start, zoom.end] : [0, 100];
     const range = getRange(drawingArea, axisDirection, axis.reverse ?? false);
@@ -179,45 +171,12 @@ export function computeAxisValue<T extends ChartSeriesType>({
       return;
     }
 
-    const rawTickNumber = scaleDefinition.tickNumber!;
+    const rawTickNumber = domains[axis.id].tickNumber!;
     const continuousAxis = axis as Readonly<
       DefaultedAxis<ContinuousScaleName, any, Readonly<ChartsAxisProps>>
     >;
     const scaleType = continuousAxis.scaleType ?? ('linear' as const);
     const tickNumber = scaleTickNumberByRange(rawTickNumber, zoomRange);
-
-    const filter = zoom === undefined && !zoomOption ? getFilters : undefined; // Do not apply filtering if zoom is already defined.
-    if (filter) {
-      const [minData, maxData] = getAxisExtrema(
-        axis,
-        axisDirection,
-        seriesConfig as ChartSeriesConfig<CartesianChartSeriesType>,
-        axisIndex,
-        formattedSeries,
-        filter,
-      );
-      scale = scale.copy();
-      scale.domain([minData, maxData]);
-
-      const domainLimit = getDomainLimit(
-        axis,
-        axisDirection,
-        axisIndex,
-        formattedSeries,
-        preferStrictDomainInLineCharts,
-      );
-
-      const axisExtrema = getActualAxisExtrema(axis, minData, maxData);
-
-      if (typeof domainLimit === 'function') {
-        const { min, max } = domainLimit(minData, maxData);
-        axisExtrema[0] = min;
-        axisExtrema[1] = max;
-      }
-
-      scale.domain(axisExtrema);
-      applyDomainLimit(scale, axis, domainLimit, rawTickNumber);
-    }
 
     completeAxis[axis.id] = {
       offset: 0,
@@ -244,6 +203,7 @@ export function computeAxisValue<T extends ChartSeriesType>({
         ) => string),
     } as ComputedAxis<ContinuousScaleName, any, ChartsAxisProps>;
   });
+
   return {
     axis: completeAxis,
     axisIds: allAxis.map(({ id }) => id),
