2acc3a58f7702c19fdcdebff4d3a4954ae0f08db
Bernardo Belchior <bernardo@mui.com>
2025-10-13 11:11:15 +0200

    Add more cases to test

--- test/regressions/charts/MinBarSize.tsx
+++ test/regressions/charts/MinBarSize.tsx
@@ -8,22 +8,41 @@ export default function MinBarSize(): React.JSX.Element {
   const minBarSize = 40;
 
   return (
-    <Stack direction="row" spacing={2} justifyContent={'center'} alignItems={'center'}>
-      <BarChart
-        xAxis={[{ data: sparseData.map((v) => `${v}`), scaleType: 'band' }]}
-        series={[{ data: sparseData, minBarSize }]}
-        height={300}
-        width={400}
-        borderRadius={16}
-      />
-      <BarChart
-        xAxis={[{ data: sparseData.map((v) => `${v}`), scaleType: 'band' }]}
-        series={[{ data: sparseData, minBarSize }]}
-        yAxis={[{ reverse: true }]}
-        height={300}
-        width={400}
-        borderRadius={16}
-      />
+    <Stack spacing={2} justifyContent="center" alignItems="center">
+      <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+        <BarChart
+          xAxis={[{ data: sparseData.map((v) => `${v}`), scaleType: 'band' }]}
+          series={[{ data: sparseData, minBarSize }]}
+          height={300}
+          width={400}
+          borderRadius={16}
+        />
+        <BarChart
+          xAxis={[{ data: sparseData.map((v) => `${v}`), scaleType: 'band' }]}
+          yAxis={[{ reverse: true }]}
+          series={[{ data: sparseData, minBarSize }]}
+          height={300}
+          width={400}
+          borderRadius={16}
+        />
+      </Stack>
+      <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+        <BarChart
+          yAxis={[{ data: sparseData.map((v) => `${v}`), scaleType: 'band' }]}
+          series={[{ data: sparseData, minBarSize, layout: 'horizontal' }]}
+          height={300}
+          width={400}
+          borderRadius={16}
+        />
+        <BarChart
+          xAxis={[{ reverse: true }]}
+          yAxis={[{ data: sparseData.map((v) => `${v}`), scaleType: 'band' }]}
+          series={[{ data: sparseData, minBarSize, layout: 'horizontal' }]}
+          height={300}
+          width={400}
+          borderRadius={16}
+        />
+      </Stack>
     </Stack>
   );
 }
