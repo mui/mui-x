c9a28bbb38d389bc4d36ad87c7cb659973d0c0e6
Bernardo Belchior <bernardo@mui.com>
2025-10-14 17:46:27 +0200

    [charts] Batch string size measurement

--- packages/x-charts/src/internals/domUtils.ts
+++ packages/x-charts/src/internals/domUtils.ts
@@ -1,5 +1,6 @@
-// DOM utils taken from
+// DOM utils adapted from
 // https://github.com/recharts/recharts/blob/master/src/util/DOMUtils.ts
+import * as React from 'react';
 
 function isSsr(): boolean {
   return typeof window === 'undefined';
@@ -117,7 +118,6 @@ export const getStringSize = (text: string | number, style: React.CSSProperties 
       return styleKey;
     });
 
-    // measurementElem.style.whiteSpace = 'pre';
     measurementElem.textContent = str;
 
     measurementSpanContainer.replaceChildren(measurementElem);
@@ -141,6 +141,74 @@ export const getStringSize = (text: string | number, style: React.CSSProperties 
     return { width: 0, height: 0 };
   }
 };
+
+export function batchMeasureStrings(
+  texts: Iterable<string | number>,
+  style: React.CSSProperties = {},
+) {
+  if (isSsr()) {
+    return new Map<string | number, { width: number; height: number }>(
+      Array.from(texts).map((text) => [text, { width: 0, height: 0 }]),
+    );
+  }
+
+  const sizeMap = new Map<string | number, { width: number; height: number }>();
+  const textToMeasure: Array<string | number> = [];
+  const styleString = getStyleString(style);
+
+  for (const text of texts) {
+    const cacheKey = `${text}-${styleString}`;
+    const size = stringCache.get(cacheKey);
+
+    if (size) {
+      sizeMap.set(text, size);
+    } else {
+      textToMeasure.push(text);
+    }
+  }
+
+  const measurementContainer = getMeasurementContainer();
+  // Need to use CSS Object Model (CSSOM) to be able to comply with Content Security Policy (CSP)
+  // https://en.wikipedia.org/wiki/Content_Security_Policy
+  const measurementSpanStyle: Record<string, any> = { ...style };
+
+  Object.keys(measurementSpanStyle).map((styleKey) => {
+    (measurementContainer!.style as Record<string, any>)[camelToMiddleLine(styleKey)] =
+      autoCompleteStyle(styleKey, measurementSpanStyle[styleKey]);
+    return styleKey;
+  });
+
+  const measurementElems: SVGTextElement[] = [];
+  for (const string of textToMeasure) {
+    const measurementElem = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    measurementElem.textContent = `${string}`;
+    measurementElems.push(measurementElem);
+  }
+
+  measurementContainer.replaceChildren(...measurementElems);
+
+  for (let i = 0; i < textToMeasure.length; i += 1) {
+    const text = textToMeasure[i];
+    const measurementSpan = measurementContainer.children[i] as HTMLSpanElement;
+    const rect = measurementSpan.getBoundingClientRect();
+    const result = { width: rect.width, height: rect.height };
+    const cacheKey = `${text}-${styleString}`;
+
+    stringCache.set(cacheKey, result);
+    sizeMap.set(text, result);
+  }
+
+  if (stringCache.size + 1 > MAX_CACHE_NUM) {
+    stringCache.clear();
+  }
+
+  if (process.env.NODE_ENV === 'test') {
+    // In test environment, we clean the measurement span immediately
+    measurementContainer.replaceChildren();
+  }
+
+  return sizeMap;
+}
 
 /**
  * Get (or create) a hidden span element to measure text size.
