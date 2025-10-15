249c56715995f9c29a6977b6f9cd3684b1cb5df0
lauri865 <lauri.lehtmaa@gmail.com>
2025-10-15 02:23:23 +0200

    fix

--- packages/x-data-grid/src/hooks/utils/useRunOncePerLoop.ts
+++ packages/x-data-grid/src/hooks/utils/useRunOncePerLoop.ts
@@ -1,17 +1,11 @@
 'use client';
 import * as React from 'react';
 
-export function useRunOncePerLoop<T extends (...args: any[]) => void>(
-  callback: T,
-  firstCall?: boolean,
-) {
+export function useRunOncePerLoop<T extends (...args: any[]) => void>(callback: T) {
   const scheduledCallbackRef = React.useRef<(...args: any) => void>(null);
 
   const schedule = React.useCallback(
     (...args: Parameters<T>) => {
-      if (scheduledCallbackRef.current && firstCall) {
-        return;
-      }
       scheduledCallbackRef.current = () => {
         scheduledCallbackRef.current = null;
         callback(...args);
