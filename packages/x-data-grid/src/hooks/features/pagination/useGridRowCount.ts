945bb5b7f15d9a48a7bef2ff9fe825ccb4f31066
lauri865 <lauri.lehtmaa@gmail.com>
2025-10-16 19:16:38 +0200

    lint

--- packages/x-data-grid/src/hooks/features/pagination/useGridRowCount.ts
+++ packages/x-data-grid/src/hooks/features/pagination/useGridRowCount.ts
@@ -7,7 +7,7 @@ import { GridPrivateApiCommunity } from '../../../models/api/gridApiCommunity';
 import { DataGridProcessedProps } from '../../../models/props/DataGridProps';
 import { GridPaginationRowCountApi, GridPaginationState } from './gridPaginationInterfaces';
 import { gridFilteredTopLevelRowCountSelector } from '../filter';
-import { useGridLogger, useGridApiMethod, useGridEvent, useFirstRender } from '../../utils';
+import { useGridLogger, useGridApiMethod, useGridEvent } from '../../utils';
 import { GridPipeProcessor, useGridRegisterPipeProcessor } from '../../core/pipeProcessing';
 import {
   gridPaginationRowCountSelector,
