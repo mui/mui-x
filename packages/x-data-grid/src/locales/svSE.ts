7b68d8ddfbc6dc7305748dbdfc453695b0a67c2a
Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>
2025-10-17 10:08:26 +0200

    [DataGridPremium] [ai-assistant] Support data visualization (#19831)

--- packages/x-data-grid/src/locales/svSE.ts
+++ packages/x-data-grid/src/locales/svSE.ts
@@ -328,6 +328,7 @@ const svSEGrid: Partial<GridLocaleText> = {
   // promptChangePivotRowsLabel: (count: number) => `Rows (${count})`,
   // promptChangePivotValuesLabel: (count: number) => `Values (${count})`,
   // promptChangePivotValuesDescription: (column: string, aggregation: string) => `${column} (${aggregation})`,
+  // promptChangeChartsLabel: (dimensionsCount: number, valuesCount: number) => `Dimensions (${dimensionsCount}), Values (${valuesCount})`,
 };
 
 export const svSE: Localization = getGridLocalization(svSEGrid);
