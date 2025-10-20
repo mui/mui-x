86e9cea110fabc35c2d8555b173e00ff7641b28d
Armin Mehinovic <armin@mui.com>
2025-10-20 20:58:54 +0200

    add data source description in the api docs

--- packages/x-data-grid/src/hooks/features/dataSource/models.ts
+++ packages/x-data-grid/src/hooks/features/dataSource/models.ts
@@ -12,35 +12,37 @@ import type { GridDataSourceCacheDefaultConfig } from './cache';
  */
 export type GridDataSourceFetchRowsParams<T> = Partial<T> & GridGetRowsOptions;
 
-export interface GridDataSourceApiBase {
-  /**
-   * Fetches the rows from the server.
-   * If no `parentId` option is provided, it fetches the root rows.
-   * Any missing parameter from `params` will be filled from the state (sorting, filtering, etc.).
-   * @param {GridRowId} parentId The id of the parent node (default: `GRID_ROOT_GROUP_ID`).
-   * @param {GridDataSourceFetchRowsParams<GridGetRowsParams>} params Request parameters override.
-   */
-  fetchRows: (
-    parentId?: GridRowId,
-    params?: GridDataSourceFetchRowsParams<GridGetRowsParams>,
-  ) => void;
-  /**
-   * The data source cache object.
-   */
-  cache: GridDataSourceCache;
-  /**
-   * Syncs the row with the server and updates in the grid.
-   * @param {GridUpdateRowParams} params The parameters for the edit operation.
-   * @returns {Promise<GridRowModel> | undefined} The updated row or `undefined` if `dataSource.updateRow` is not passed.
-   */
-  editRow: (params: GridUpdateRowParams) => Promise<GridRowModel> | undefined;
-}
-
 export interface GridDataSourceApi {
   /**
    * The data source API.
    */
-  dataSource: GridDataSourceApiBase;
+  dataSource: {
+    /**
+     * Fetches the rows from the server.
+     * If no `parentId` option is provided, it fetches the root rows.
+     * Any missing parameter from `params` will be filled from the state (sorting, filtering, etc.).
+     * @param {GridRowId} parentId The id of the parent node (default: `GRID_ROOT_GROUP_ID`).
+     * @param {GridDataSourceFetchRowsParams<GridGetRowsParams>} params Request parameters override.
+     */
+    fetchRows: (
+      parentId?: GridRowId,
+      params?: GridDataSourceFetchRowsParams<GridGetRowsParams>,
+    ) => void;
+    /**
+     * The data source cache object.
+     */
+    cache: GridDataSourceCache;
+    /**
+     * Syncs the row with the server and updates in the grid.
+     * @param {GridUpdateRowParams} params The parameters for the edit operation.
+     * @returns {Promise<GridRowModel> | undefined} The updated row or `undefined` if `dataSource.updateRow` is not passed.
+     */
+    editRow: (params: GridUpdateRowParams) => Promise<GridRowModel> | undefined;
+  };
+}
+
+export interface GridDataSourceApiBase {
+  dataSource: GridDataSourceApi['dataSource'];
 }
 
 export interface GridDataSourceBaseOptions {
