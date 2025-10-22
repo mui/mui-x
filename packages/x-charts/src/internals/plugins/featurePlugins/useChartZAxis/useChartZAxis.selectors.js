98d0bc36b2bcf95e8c01eee6875da8ef7b1f601f
Rom Grk <romgrk.cc@gmail.com>
2025-10-22 06:22:46 -0400

    Revert "Add support for all canvas text layout CSS properties"
    This reverts commit 6c0958b150f75b04da48c3cc38a3c12e8354c15e.

--- packages/x-charts/src/internals/plugins/featurePlugins/useChartZAxis/useChartZAxis.selectors.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.selectorChartZAxis = void 0;
-var selectors_1 = require("../../utils/selectors");
-var selectRootState = function (state) { return state; };
-exports.selectorChartZAxis = (0, selectors_1.createSelector)([selectRootState], function (state) { return state.zAxis; });
--- packages/x-internals/src/fastObjectShallowCompare/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fastObjectShallowCompare = void 0;
-var fastObjectShallowCompare_1 = require("./fastObjectShallowCompare");
-Object.defineProperty(exports, "fastObjectShallowCompare", { enumerable: true, get: function () { return fastObjectShallowCompare_1.fastObjectShallowCompare; } });
