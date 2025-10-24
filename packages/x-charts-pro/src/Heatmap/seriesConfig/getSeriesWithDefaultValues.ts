e21f010207eb21242a9b3e3e6f355279e9fc5e85
Bernardo Belchior <bernardo@mui.com>
2025-10-24 16:50:57 +0100

    [charts] Handle `undefined` id and color in series (#20087)

--- packages/x-charts-pro/src/Heatmap/seriesConfig/getSeriesWithDefaultValues.ts
+++ packages/x-charts-pro/src/Heatmap/seriesConfig/getSeriesWithDefaultValues.ts
@@ -6,9 +6,9 @@ const getSeriesWithDefaultValues: GetSeriesWithDefaultValues<'heatmap'> = (
   colors,
 ) => {
   return {
-    id: seriesData.id ?? `auto-generated-id-${seriesIndex}`,
     color: colors[seriesIndex % colors.length],
     ...seriesData,
+    id: seriesData.id ?? `auto-generated-id-${seriesIndex}`,
   };
 };
 
