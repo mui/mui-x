a8899d6d4fd703104684d0d2b65a5e5134c1b7ed
Bernardo Belchior <bernardo@mui.com>
2025-10-28 10:00:34 +0000

    Remove unnecessary nonce setting

--- packages/x-charts-pro/src/internals/plugins/useChartProExport/exportImage.ts
+++ packages/x-charts-pro/src/internals/plugins/useChartProExport/exportImage.ts
@@ -44,7 +44,6 @@ export async function exportImage(
     resolve = res;
   });
 
-  iframe.nonce = nonce;
   iframe.onload = async () => {
     const exportDoc = iframe.contentDocument!;
     const elementClone = element.cloneNode(true) as HTMLElement;
