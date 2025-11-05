a64722db58b7af68c0af3f15234de664db0bd8be
Bernardo Belchior <bernardo@mui.com>
2025-11-05 11:21:48 +0000

    [charts] Add `barLabel` to bar series. Deprecate `barLabel` in `BarPlot`. (#20184)

--- packages/x-charts/src/BarChart/BarChart.tsx
+++ packages/x-charts/src/BarChart/BarChart.tsx
@@ -176,6 +176,7 @@ BarChart.propTypes = {
     y: PropTypes.oneOf(['band', 'line', 'none']),
   }),
   /**
+   * @deprecated Use `barLabel` in the chart series instead.
    * If provided, the function will be used to format the label of the bar.
    * It can be set to 'value' to display the current value.
    * @param {BarItem} item The item to format.
