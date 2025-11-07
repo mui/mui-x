81192be38f66ccacbfa58b3104ee04b59d146fff
Bernardo Belchior <bernardo@mui.com>
2025-11-07 10:43:25 +0000

    Fix color getter in bar range chart

--- packages/x-charts/src/BarChart/seriesConfig/barRange/getColor.ts
+++ packages/x-charts/src/BarChart/seriesConfig/barRange/getColor.ts
@@ -1,25 +1,39 @@
 import { ColorProcessor } from '../../../internals/plugins/models';
+import { getSeriesColorFn } from '../../../internals/getSeriesColorFn';
 
 const getColor: ColorProcessor<'barRange'> = (series, xAxis, yAxis) => {
   const verticalLayout = series.layout === 'vertical';
 
   const bandColorScale = verticalLayout ? xAxis?.colorScale : yAxis?.colorScale;
   const bandValues = verticalLayout ? xAxis?.data : yAxis?.data;
+  const getSeriesColor = getSeriesColorFn(series);
 
   if (bandColorScale && bandValues) {
     return (dataIndex?: number) => {
       if (dataIndex === undefined) {
         return series.color;
       }
+
       const value = bandValues[dataIndex];
-      const color = value === null ? series.color : bandColorScale(value);
+      const color = value === null ? getSeriesColor({ value, dataIndex }) : bandColorScale(value);
+
       if (color === null) {
-        return series.color;
+        return getSeriesColor({ value, dataIndex });
       }
+
       return color;
     };
   }
-  return () => series.color;
+
+  return (dataIndex?: number) => {
+    if (dataIndex === undefined) {
+      return series.color;
+    }
+
+    const value = series.data[dataIndex];
+
+    return getSeriesColor({ value, dataIndex });
+  };
 };
 
 export default getColor;
