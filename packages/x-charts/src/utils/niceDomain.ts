aa64d578db67681c42b157e80974cd752e5ac104
Bernardo Belchior <bernardo@mui.com>
2025-11-07 17:26:12 +0000

    Add tests and better docs

--- packages/x-charts/src/utils/niceDomain.ts
+++ packages/x-charts/src/utils/niceDomain.ts
@@ -5,13 +5,26 @@ import { getScale } from '../internals/getScale';
 /**
  * Returns a nice domain for the given scale type and domain.
  * Does not modify the original domain.
+ *
+ * Providing a count improves the nice domain calculation by trying to align tick values to round
+ * numbers or dates.
+ *
+ * For example, if you have a domain of [29, 72] and there are 5 ticks, the nice domain will be
+ * [20, 80] so that the ticks can be at [20, 35, 50, 65, 80].
+ * However, if there are 11 ticks, the nice domain will be [25, 75] so that the ticks can be at
+ * [25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75].
+ *
+ * @param scaleType The type of the scale (e.g., 'linear', 'log', 'time', etc.).
+ * @param domain The domain to be made nicer.
+ * @param count An optional number of ticks to improve the nice domain calculation. Defaults to 5.
  */
 export function niceDomain<Domain extends NumberValue>(
   scaleType: ContinuousScaleName,
   domain: Iterable<Domain>,
+  count: number = 5,
 ): number[] | Date[] {
   const scale = getScale<Domain, any>(scaleType, domain, [0, 1]);
 
-  scale.nice();
+  scale.nice(count);
   return scale.domain();
 }
