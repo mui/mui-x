e269b35fe0d713e654dd1972d170b4bd303278e9
Jose Quintas <juniorquintas@gmail.com>
2025-11-10 13:08:19 +0100

    node args

--- test/performance-charts/vitest.config.ts
+++ test/performance-charts/vitest.config.ts
@@ -36,18 +36,6 @@ export default defineConfig({
             '--enable-precise-memory-info',
             '--enable-devtools-experiments',
             '--disable-web-security',
-            // Suggested by Codspeed: https://github.com/CodSpeedHQ/codspeed-node/blob/174369c7c28b320f79cabd4bd7fb2b6a3cdef6dd/packages/core/src/introspection.ts#L57
-            '--interpreted-frames-native-stack',
-            '--allow-natives-syntax',
-            '--hash-seed=1',
-            '--random-seed=1',
-            '--no-opt',
-            '--predictable',
-            '--predictable-gc-schedule',
-            '--expose-gc',
-            '--no-concurrent-sweeping',
-            '--max-old-space-size=4096',
-            // End of suggested by Codspeed
           ],
         },
       }),
