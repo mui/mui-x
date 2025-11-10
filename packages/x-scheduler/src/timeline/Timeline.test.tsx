853c4dc4a4ac757f19c4ba75a7c83aa5c6dd63ba
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-11-10 14:01:43 +0100

    refactor

--- packages/x-scheduler/src/timeline/Timeline.test.tsx
+++ packages/x-scheduler/src/timeline/Timeline.test.tsx
@@ -15,23 +15,17 @@ const baseResources: CalendarResource[] = [
   { id: 'resource-2', title: 'Design', eventColor: 'jade' },
 ];
 
-const baseEvents: SchedulerEvent[] = [
-  EventBuilder.new()
-    .title('Spec Review')
-    .singleDay('2025-07-03T09:00:00Z')
-    .resource('resource-1')
-    .build(),
-  EventBuilder.new()
-    .title('UX Sync')
-    .span('2025-07-03T11:00:00Z', '2025-07-06T12:00:00Z')
-    .resource('resource-2')
-    .build(),
-  EventBuilder.new()
-    .title('Architecture Session')
-    .span('2025-07-04T13:00:00Z', '2025-08-04T14:30:00Z')
-    .resource('resource-1')
-    .build(),
-];
+const event1 = EventBuilder.new().singleDay('2025-07-03T09:00:00Z').resource('resource-1').build();
+const event2 = EventBuilder.new()
+  .span('2025-07-03T11:00:00Z', '2025-07-06T12:00:00Z')
+  .resource('resource-2')
+  .build();
+const event3 = EventBuilder.new()
+  .span('2025-07-04T13:00:00Z', '2025-08-04T14:30:00Z')
+  .resource('resource-1')
+  .build();
+
+const baseEvents = [event1, event2, event3];
 
 describe('<Timeline />', () => {
   const { render } = createSchedulerRenderer({
@@ -86,20 +80,17 @@ describe('<Timeline />', () => {
     });
 
     it('does not render events out of range', () => {
-      const extendedEvents: SchedulerEvent[] = [
-        ...baseEvents,
-        EventBuilder.new()
-          .title('Out of range')
-          .span('2050-07-04T13:00:00Z', '2050-08-04T14:30:00Z')
-          .build(),
-      ];
+      const outOfRangeEvent = EventBuilder.new()
+        .title('Out of range')
+        .span('2050-07-04T13:00:00Z', '2050-08-04T14:30:00Z')
+        .build();
 
-      renderTimeline({ events: extendedEvents });
+      renderTimeline({ events: [...baseEvents, outOfRangeEvent] });
       baseEvents.forEach((eventItem) => {
         expect(screen.getByText(eventItem.title)).not.to.equal(null);
       });
 
-      expect(screen.queryByText('Out of range')).to.equal(null);
+      expect(screen.queryByText(outOfRangeEvent.title)).to.equal(null);
     });
 
     it('should keep events visible after rerender', () => {
@@ -123,7 +114,7 @@ describe('<Timeline />', () => {
       const hourBoundaries = { start: 9 * 64, end: 10 * 64 }; // 9:00 - 10:00
       renderTimeline({ view: 'time' });
 
-      const event = screen.getByText('Spec Review');
+      const event = screen.getByText(event1.title);
       expect(event).not.to.equal(null);
       const xPositioning = event.style.getPropertyValue('--x-position');
 
@@ -138,7 +129,7 @@ describe('<Timeline />', () => {
       const dayBoundaries = { start: 1 * 120, end: 2 * 120 }; // 4th - 5th
       renderTimeline({ view: 'days' });
 
-      const event = screen.getByText('Architecture Session');
+      const event = screen.getByText(event3.title);
       expect(event).not.to.equal(null);
       const xPositioning = event.style.getPropertyValue('--x-position');
 
@@ -156,7 +147,7 @@ describe('<Timeline />', () => {
 
       renderTimeline({ view: 'weeks' });
 
-      const event = screen.getByText('Spec Review');
+      const event = screen.getByText(event1.title);
       expect(event).not.to.equal(null);
       const xPositioning = event.style.getPropertyValue('--x-position');
 
@@ -167,29 +158,27 @@ describe('<Timeline />', () => {
     });
 
     it('should render events correctly in the month view', () => {
-      const extendedEvents: SchedulerEvent[] = [
-        ...baseEvents,
-        EventBuilder.new()
-          .title('Next month')
-          .span('2025-08-04T13:00:00Z', '2025-09-04T14:30:00Z')
-          .resource('resource-1')
-          .build(),
-      ];
+      const nextMonthEvent = EventBuilder.new()
+        .title('Next month')
+        .span('2025-08-04T13:00:00Z', '2025-09-04T14:30:00Z')
+        .resource('resource-1')
+        .build();
+      const extendedEvents: SchedulerEvent[] = [...baseEvents, nextMonthEvent];
 
       renderTimeline({ events: extendedEvents, view: 'months' });
 
       const totalWidth = 2160; // 12 months * 180px
-      const event1 = screen.getByText('Spec Review');
+      const event1Element = screen.getByText(event1.title);
       expect(event1).not.to.equal(null);
-      const xPositioning = event1.style.getPropertyValue('--x-position');
+      const xPositioning = event1Element.style.getPropertyValue('--x-position');
 
       const eventPosition = (totalWidth * parseFloat(xPositioning)) / 100;
 
       expect(eventPosition).to.be.lessThanOrEqual(180); // first month
 
-      const event2 = screen.getByText('Next month');
-      expect(event2).not.to.equal(null);
-      const xPositioning2 = event2.style.getPropertyValue('--x-position');
+      const nextMonthEventElement = screen.getByText('Next month');
+      expect(nextMonthEventElement).not.to.equal(null);
+      const xPositioning2 = nextMonthEventElement.style.getPropertyValue('--x-position');
 
       const eventPosition2 = (totalWidth * parseFloat(xPositioning2)) / 100;
 
@@ -197,33 +186,29 @@ describe('<Timeline />', () => {
       expect(eventPosition2).to.be.lessThanOrEqual(360); // second month
     });
     it('should render events correctly in the month view', () => {
-      const extendedEvents: SchedulerEvent[] = [
-        EventBuilder.new()
-          .title('This year')
-          .span('2025-08-03T13:00:00Z', '2025-09-04T14:30:00Z')
-          .resource('resource-1')
-          .build(),
-        EventBuilder.new()
-          .title('Next year')
-          .span('2026-08-03T13:00:00Z', '2026-09-04T14:30:00Z')
-          .resource('resource-1')
-          .build(),
-      ];
+      const thisYearEvent = EventBuilder.new()
+        .span('2025-08-03T13:00:00Z', '2025-09-04T14:30:00Z')
+        .resource('resource-1')
+        .build();
+      const nextYearEvent = EventBuilder.new()
+        .span('2026-08-03T13:00:00Z', '2026-09-04T14:30:00Z')
+        .resource('resource-1')
+        .build();
 
-      renderTimeline({ events: extendedEvents, view: 'years' });
+      renderTimeline({ events: [thisYearEvent, nextYearEvent], view: 'years' });
 
       const totalWidth = 800;
-      const event1 = screen.getByText('This year');
-      expect(event1).not.to.equal(null);
-      const xPositioning = event1.style.getPropertyValue('--x-position');
+      const thisYearEventElement = screen.getByText(thisYearEvent.title);
+      expect(thisYearEventElement).not.to.equal(null);
+      const xPositioning = thisYearEventElement.style.getPropertyValue('--x-position');
 
       const eventPosition = (totalWidth * parseFloat(xPositioning)) / 100;
 
       expect(eventPosition).to.be.lessThanOrEqual(200); // 2025
 
-      const event2 = screen.getByText('Next year');
-      expect(event2).not.to.equal(null);
-      const xPositioning2 = event2.style.getPropertyValue('--x-position');
+      const nextYearEventElement = screen.getByText(nextYearEvent.title);
+      expect(nextYearEventElement).not.to.equal(null);
+      const xPositioning2 = nextYearEventElement.style.getPropertyValue('--x-position');
 
       const eventPosition2 = (totalWidth * parseFloat(xPositioning2)) / 100;
 
