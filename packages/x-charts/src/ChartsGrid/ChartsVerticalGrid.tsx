a6628b644c1346d32021a9b51b5b68414e0ff493
Bernardo Belchior <bernardo@mui.com>
2025-11-11 16:33:53 +0000

    [charts] Add tick spacing property

--- packages/x-charts/src/ChartsGrid/ChartsVerticalGrid.tsx
+++ packages/x-charts/src/ChartsGrid/ChartsVerticalGrid.tsx
@@ -17,9 +17,15 @@ interface ChartsGridVerticalProps {
 export function ChartsGridVertical(props: ChartsGridVerticalProps) {
   const { axis, start, end, classes } = props;
 
-  const { scale, tickNumber, tickInterval } = axis;
+  const { scale, tickNumber, tickInterval, tickSpacing } = axis;
 
-  const xTicks = useTicks({ scale, tickNumber, tickInterval, direction: 'x' });
+  const xTicks = useTicks({
+    scale,
+    tickNumber,
+    tickInterval,
+    tickSpacing,
+    direction: 'x',
+  });
 
   return (
     <React.Fragment>
