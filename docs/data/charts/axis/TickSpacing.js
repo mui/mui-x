35a591205e1509913ea83c277f1287032b2ee34a
Bernardo Belchior <bernardo@mui.com>
2025-11-11 16:36:17 +0000

    Add demo

---
+++ docs/data/charts/axis/TickSpacing.js
@@ -0,0 +1,23 @@
+import { BarChart } from '@mui/x-charts/BarChart';
+import data from '../dataset/sp500-intraday.json';
+
+const tickLabelDateFormatter = new Intl.DateTimeFormat(undefined, {
+  month: 'short',
+  day: 'numeric',
+});
+
+export default function TickSpacing() {
+  return (
+    <BarChart
+      xAxis={[
+        {
+          data: data.map((d) => new Date(Date.parse(d.date))),
+          valueFormatter: (v) => tickLabelDateFormatter.format(v),
+          tickSpacing: 50,
+        },
+      ]}
+      series={[{ data: data.map((d) => d.close), label: 'Close' }]}
+      height={300}
+    />
+  );
+}
