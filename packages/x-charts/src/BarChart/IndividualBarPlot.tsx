ede675c59139dd7c5f157b759d00ae7a9bc3a2ee
Bernardo Belchior <bernardo@mui.com>
2025-11-12 15:40:18 +0000

    [charts] Add batch bar plot

---
+++ packages/x-charts/src/BarChart/IndividualBarPlot.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { BarPlotSlotProps, BarPlotSlots } from './BarPlot';
+import { BarItemIdentifier } from '../models';
+import { BarElement } from './BarElement';
+import { ProcessedBarSeriesData } from './types';
+import { useUtilityClasses } from './barClasses';
+
+interface IndividualBarPlotProps {
+  completedData: ProcessedBarSeriesData[];
+  borderRadius?: number;
+  skipAnimation?: boolean;
+  onItemClick?: (
+    event: React.MouseEvent<SVGElement, MouseEvent>,
+    barItemIdentifier: BarItemIdentifier,
+  ) => void;
+  slotProps?: BarPlotSlotProps;
+  slots?: BarPlotSlots;
+}
+
+export function IndividualBarPlot({
+  completedData,
+  borderRadius,
+  onItemClick,
+  skipAnimation,
+  ...other
+}: IndividualBarPlotProps) {
+  const classes = useUtilityClasses();
+  const withoutBorderRadius = !borderRadius || borderRadius <= 0;
+
+  return (
+    <React.Fragment>
+      {completedData.map(({ seriesId, layout, xOrigin, yOrigin, data }) => {
+        return (
+          <g key={seriesId} data-series={seriesId} className={classes.series}>
+            {data.map(({ dataIndex, color, maskId, x, y, width, height }) => {
+              const barElement = (
+                <BarElement
+                  key={dataIndex}
+                  id={seriesId}
+                  dataIndex={dataIndex}
+                  color={color}
+                  skipAnimation={skipAnimation ?? false}
+                  layout={layout ?? 'vertical'}
+                  x={x}
+                  xOrigin={xOrigin}
+                  y={y}
+                  yOrigin={yOrigin}
+                  width={width}
+                  height={height}
+                  {...other}
+                  onClick={
+                    onItemClick &&
+                    ((event) => {
+                      onItemClick(event, { type: 'bar', seriesId, dataIndex });
+                    })
+                  }
+                />
+              );
+
+              if (withoutBorderRadius) {
+                return barElement;
+              }
+
+              return (
+                <g key={dataIndex} clipPath={`url(#${maskId})`}>
+                  {barElement}
+                </g>
+              );
+            })}
+          </g>
+        );
+      })}
+    </React.Fragment>
+  );
+}
