bb88ca2169b2ab81bb6d5bd9f8aa58521bd03047
Bernardo Belchior <bernardo@mui.com>
2025-11-12 15:40:18 +0000

    [charts] Add batch bar plot
    Fix naming collision
    
    Fix border radius
    
    Handle border radii greater than width/height
    
    Add example and renderer prop
    
    Remove unused code
    
    WIP: Use flatbush for finding the clicked bar
    
    Add simple animation
    
    Split
    
    Render series separately
    
    Implement highlight and fading
    
    Import fixing. Remove unused prop.
    
    Update docs
    
    Slight performance improvements
    
    Share appendAtKey
    
    Fix layout-dependent animation
    
    Return layout and origin in bar plot data
    
    Fix rebase
    
    Remove unused prop
    
    Fix imports
    
    Abstract position in getClosestPoint
    
    Fix type issues
    
    Handle both layouts when creating bar flatbush
    
    Fix flatbush with nulls
    
    Fix renderer prop not being forwarded
    
    Fix animation
    
    Remove disableHover mention
    
    Try to implement onItemClick
    
    Investigate flatbush issue
    
    Remove flatbush usage
    
    Add on item click
    
    Update docs. Cleanup batch bar after on item click working.
    
    Some more progress
    
    Remove memo for now
    
    Add fading and highlighting (missing animation)
    
    Remove animation from highlight/fade
    
    Fix type imports
    
    Fix pointer leave
    
    Rename
    
    Add brightness filter to highlighted item
    
    Revert demos
    
    Fix types
    
    Fix rebase
    
    Address feedback
    
    Use ordinal time ticks
    
    Gen stuff
    
    Use stroke and stroke-width to fix sub-pixel rendering in large number of bars

---
+++ packages/x-charts/src/BarChart/getBarItemAtPosition.ts
@@ -0,0 +1,114 @@
+import { type Store } from '@mui/x-internals/store';
+import type { BarItemIdentifier, SeriesId } from '../models';
+import {
+  selectorChartXAxis,
+  selectorChartYAxis,
+} from '../internals/plugins/featurePlugins/useChartCartesianAxis';
+import { type UseChartHighlightSignature } from '../internals/plugins/featurePlugins/useChartHighlight';
+import { type UseChartCartesianAxisSignature } from '../internals/plugins/featurePlugins/useChartCartesianAxis';
+import type { ComputedAxis } from '../models/axis';
+import { selectorChartSeriesProcessed } from '../internals/plugins/corePlugins/useChartSeries';
+import { getBandSize } from '../internals/getBandSize';
+import { isBandScale } from '../internals/scaleGuards';
+import { type ChartState } from '../internals/plugins/models';
+import { getDataIndexForOrdinalScaleValue } from '../internals/invertScale';
+
+export function getBarItemAtPosition(
+  store: Store<ChartState<[UseChartCartesianAxisSignature, UseChartHighlightSignature]>>,
+  svgPoint: Pick<DOMPoint, 'x' | 'y'>,
+): BarItemIdentifier | undefined {
+  const { series, stackingGroups = [] } =
+    selectorChartSeriesProcessed(store.getSnapshot())?.bar ?? {};
+  const { axis: xAxes, axisIds: xAxisIds } = selectorChartXAxis(store.getSnapshot());
+  const { axis: yAxes, axisIds: yAxisIds } = selectorChartYAxis(store.getSnapshot());
+  const defaultXAxisId = xAxisIds[0];
+  const defaultYAxisId = yAxisIds[0];
+
+  let item: { dataIndex: number; seriesId: SeriesId } | undefined = undefined;
+
+  for (let stackIndex = 0; stackIndex < stackingGroups.length; stackIndex += 1) {
+    const group = stackingGroups[stackIndex];
+    const seriesIds = group.ids;
+
+    for (const seriesId of seriesIds) {
+      const aSeries = (series ?? {})[seriesId];
+
+      const xAxisId = aSeries.xAxisId ?? defaultXAxisId;
+      const yAxisId = aSeries.yAxisId ?? defaultYAxisId;
+
+      const xAxis = xAxes[xAxisId];
+      const yAxis = yAxes[yAxisId];
+
+      const bandAxis = aSeries.layout === 'horizontal' ? yAxis : xAxis;
+      const continuousAxis = aSeries.layout === 'horizontal' ? xAxis : yAxis;
+      const bandScale = bandAxis.scale;
+      const svgPointBandCoordinate = aSeries.layout === 'horizontal' ? svgPoint.y : svgPoint.x;
+
+      if (!isBandScale(bandScale)) {
+        continue;
+      }
+
+      const dataIndex = getDataIndexForOrdinalScaleValue(bandScale, svgPointBandCoordinate);
+
+      const { barWidth, offset } = getBandSize(
+        bandScale.bandwidth(),
+        stackingGroups.length,
+        (bandAxis as ComputedAxis<'band'>).barGapRatio,
+      );
+
+      const barOffset = stackIndex * (barWidth + offset);
+      const bandValue = bandAxis.data?.[dataIndex];
+
+      if (bandValue == null) {
+        continue;
+      }
+
+      const bandStart = bandScale(bandValue);
+
+      if (bandStart == null) {
+        continue;
+      }
+
+      const bandBarStart = bandStart + barOffset;
+      const bandBarEnd = bandBarStart + barWidth;
+      const bandBarMin = Math.min(bandBarStart, bandBarEnd);
+      const bandBarMax = Math.max(bandBarStart, bandBarEnd);
+
+      if (svgPointBandCoordinate >= bandBarMin && svgPointBandCoordinate <= bandBarMax) {
+        // The point is inside the band for this series
+        const svgPointContinuousCoordinate =
+          aSeries.layout === 'horizontal' ? svgPoint.x : svgPoint.y;
+        const bar = aSeries.stackedData[dataIndex];
+        const start = continuousAxis.scale(bar[0]);
+        const end = continuousAxis.scale(bar[1]);
+
+        if (start == null || end == null) {
+          continue;
+        }
+
+        const continuousMin = Math.min(start, end);
+        const continuousMax = Math.max(start, end);
+
+        if (
+          svgPointContinuousCoordinate >= continuousMin &&
+          svgPointContinuousCoordinate <= continuousMax
+        ) {
+          item = {
+            seriesId,
+            dataIndex,
+          };
+        }
+      }
+    }
+  }
+
+  if (item) {
+    return {
+      type: 'bar',
+      seriesId: item.seriesId,
+      dataIndex: item.dataIndex,
+    };
+  }
+
+  return undefined;
+}
