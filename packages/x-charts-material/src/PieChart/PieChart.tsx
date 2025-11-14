07ab055d3e300d7b7b7d9ce8abb9e29e2c682ab4
Jose Quintas <juniorquintas@gmail.com>
2025-11-14 10:51:48 +0100

    material

---
+++ packages/x-charts-material/src/PieChart/PieChart.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export const PieChart = React.forwardRef(function PieChart(
+  props: any,
+  ref: React.Ref<SVGSVGElement>,
+) {
+  return <div>PieChartMaterial</div>;
+});
