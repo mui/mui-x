003654ca61c7ca2403d57b4cd6313710a320aec8
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-11-17 11:58:02 +0100

    add test useweeklist

---
+++ packages/x-scheduler-headless/src/use-week-list/useWeekList.test.ts
@@ -0,0 +1,52 @@
+import { renderHook } from '@mui/internal-test-utils';
+import {
+  adapter,
+  DEFAULT_TESTING_VISIBLE_DATE,
+  DEFAULT_TESTING_VISIBLE_DATE_STR,
+} from 'test/utils/scheduler';
+import { useWeekList } from './useWeekList';
+
+describe('useWeekList', () => {
+  function testHook(date: string, amount: number | 'end-of-month') {
+    const { result } = renderHook(() => useWeekList());
+    return result.current({ date: adapter.date(date), amount });
+  }
+
+  it('should throw an error when amount is a non positive number', () => {
+    expect(() => testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, 0)).to.throw(/amount.*positive number/);
+    expect(() => testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, -2)).to.throw(
+      /amount.*positive number/,
+    );
+  });
+
+  it('should return one week when amount=1', () => {
+    const weeks = testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, 1);
+
+    expect(weeks).to.have.length(1);
+
+    const start = adapter.startOfWeek(DEFAULT_TESTING_VISIBLE_DATE);
+    expect(weeks[0]).to.toEqualDateTime(start);
+  });
+
+  it('should return the correct number of consecutive weeks', () => {
+    const weeks = testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, 4);
+
+    expect(weeks).to.have.length(4);
+
+    const start = adapter.startOfWeek(DEFAULT_TESTING_VISIBLE_DATE);
+    for (let i = 0; i < 4; i += 1) {
+      expect(weeks[i]).to.toEqualDateTime(adapter.addWeeks(start, i));
+    }
+  });
+
+  it('should compute end correctly when amount=end-of-month', () => {
+    const start = adapter.startOfWeek(DEFAULT_TESTING_VISIBLE_DATE);
+    const endOfMonth = adapter.endOfMonth(DEFAULT_TESTING_VISIBLE_DATE);
+    const lastWeekStart = adapter.startOfWeek(endOfMonth);
+
+    const weeks = testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, 'end-of-month');
+
+    expect(weeks[0]).to.toEqualDateTime(start);
+    expect(weeks[weeks.length - 1]).to.toEqualDateTime(lastWeekStart);
+  });
+});
