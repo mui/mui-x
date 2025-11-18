c2c5d7718a57dabac693fc786cd03e15dcca5ad0
Jose Quintas <juniorquintas@gmail.com>
2025-11-18 11:52:57 +0100

    sankeys dont have legend

--- packages/x-charts-pro/src/SankeyChart/SankeyChart.plugins.ts
+++ packages/x-charts-pro/src/SankeyChart/SankeyChart.plugins.ts
@@ -2,8 +2,6 @@ import {
   ConvertSignaturesIntoPlugins,
   useChartInteraction,
   UseChartInteractionSignature,
-  useChartVisibleSeries,
-  UseChartVisibleSeriesSignature,
 } from '@mui/x-charts/internals';
 import {
   useChartProExport,
@@ -14,13 +12,11 @@ import { useSankeyHighlight, type UseSankeyHighlightSignature } from './plugins'
 export type SankeyChartPluginSignatures = [
   UseChartInteractionSignature,
   UseSankeyHighlightSignature,
-  UseChartVisibleSeriesSignature,
   UseChartProExportSignature,
 ];
 
 export const SANKEY_CHART_PLUGINS: ConvertSignaturesIntoPlugins<SankeyChartPluginSignatures> = [
   useChartInteraction,
   useSankeyHighlight,
-  useChartVisibleSeries,
   useChartProExport,
 ];
