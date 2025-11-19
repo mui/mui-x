bfa73b1465f8451f116e554773aabcb407557af2
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-11-19 18:42:37 +0100

    fix jsdoc

--- packages/x-scheduler-headless/src/models/timezone.ts
+++ packages/x-scheduler-headless/src/models/timezone.ts
@@ -1,5 +1,7 @@
 /**
  * The timezone used by the scheduler.
- * It can be an IANA timezone name (e.g., "America/New_York") or a fixed-offset timezone (e.g., "UTC+02:00").
+ * It must be an IANA timezone name (e.g. "America/New_York", "Europe/Paris")
+ * or 'default' to use the default timezone of the date adapter.
+ * @default 'default'
  */
 export type SchedulerTimezone = string;
