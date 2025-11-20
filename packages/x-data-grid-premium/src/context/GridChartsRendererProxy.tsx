5c854642d849a56539d3417db0d49913a9ecc423
Armin Mehinovic <armin@mui.com>
2025-11-20 16:34:54 +0100

    revert type change

--- packages/x-data-grid-premium/src/context/GridChartsRendererProxy.tsx
+++ packages/x-data-grid-premium/src/context/GridChartsRendererProxy.tsx
@@ -11,13 +11,13 @@ export type GridChartsRendererProxyRendererCallback = (
   Component: React.ComponentType<any>,
 ) => React.ReactNode;
 
-type GridChartsRendererProxyRenderer = (props: {
+type GridChartsRendererProxyRenderer = React.ComponentType<{
   dimensions: ChartState['dimensions'];
   values: ChartState['values'];
   chartType: ChartState['type'];
   configuration: ChartState['configuration'];
   onRender?: GridChartsRendererProxyRendererCallback;
-}) => any;
+}>;
 
 export interface GridChartsRendererProxyProps {
   /**
