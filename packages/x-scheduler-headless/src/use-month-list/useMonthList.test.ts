96e5c6228d0adb3bf9d400d7c11c9285226ec1c5
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2025-11-21 10:22:38 +0100

    review flavien

--- packages/x-scheduler-headless/src/use-month-list/useMonthList.test.ts
+++ packages/x-scheduler-headless/src/use-month-list/useMonthList.test.ts
@@ -1,26 +1,21 @@
 import { renderHook } from '@mui/internal-test-utils';
-import {
-  adapter,
-  DEFAULT_TESTING_VISIBLE_DATE,
-  DEFAULT_TESTING_VISIBLE_DATE_STR,
-} from 'test/utils/scheduler';
+import { adapter, DEFAULT_TESTING_VISIBLE_DATE } from 'test/utils/scheduler';
 import { useMonthList } from './useMonthList';
+import { SchedulerValidDate } from '../models';
 
 describe('useMonthList', () => {
-  function testHook(date: string, amount: number | 'end-of-year') {
+  function testHook(date: SchedulerValidDate, amount: number | 'end-of-year') {
     const { result } = renderHook(() => useMonthList());
     return result.current({ date: adapter.date(date, 'default'), amount });
   }
 
   it('should throw an error when amount is a non positive number', () => {
-    expect(() => testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, 0)).to.throw(/amount.*positive number/);
-    expect(() => testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, -3)).to.throw(
-      /amount.*positive number/,
-    );
+    expect(() => testHook(DEFAULT_TESTING_VISIBLE_DATE, 0)).to.throw(/amount.*positive number/);
+    expect(() => testHook(DEFAULT_TESTING_VISIBLE_DATE, -3)).to.throw(/amount.*positive number/);
   });
 
   it('should return one month when amount=1', () => {
-    const months = testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, 1);
+    const months = testHook(DEFAULT_TESTING_VISIBLE_DATE, 1);
 
     const start = adapter.startOfMonth(DEFAULT_TESTING_VISIBLE_DATE);
 
@@ -29,7 +24,7 @@ describe('useMonthList', () => {
   });
 
   it('should return consecutive months', () => {
-    const months = testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, 4);
+    const months = testHook(DEFAULT_TESTING_VISIBLE_DATE, 4);
 
     expect(months).to.have.length(4);
 
@@ -46,7 +41,7 @@ describe('useMonthList', () => {
     const endOfYear = adapter.endOfYear(DEFAULT_TESTING_VISIBLE_DATE);
     const lastMonthStart = adapter.startOfMonth(endOfYear);
 
-    const months = testHook(DEFAULT_TESTING_VISIBLE_DATE_STR, 'end-of-year');
+    const months = testHook(DEFAULT_TESTING_VISIBLE_DATE, 'end-of-year');
 
     expect(months[0].value).to.toEqualDateTime(start);
     expect(months[months.length - 1].value).to.toEqualDateTime(lastMonthStart);
