7f8d687f5600cc1b200b76c768c455d185442bfb
delangle <flaviendelangle@gmail.com>
2025-11-25 11:19:22 +0100

    Test

--- packages/x-scheduler-headless/src/event-calendar-selectors/eventCalendarViewSelectors.test.ts
+++ packages/x-scheduler-headless/src/event-calendar-selectors/eventCalendarViewSelectors.test.ts
@@ -1,43 +1,43 @@
-// import { getEventCalendarStateFromParameters } from 'test/utils/scheduler';
-// import { eventCalendarViewSelectors } from './eventCalendarViewSelectors';
+import { getEventCalendarStateFromParameters } from 'test/utils/scheduler';
+import { eventCalendarViewSelectors } from './eventCalendarViewSelectors';
 
 describe('eventCalendarViewSelectors', () => {
-  // describe('view', () => {
-  //   it('should return the view from state', () => {
-  //     const state = getEventCalendarStateFromParameters({
-  //       events: [],
-  //       view: 'month',
-  //     });
-  //     const view = eventCalendarViewSelectors.view(state);
-  //     expect(view).toBe('month');
-  //   });
-  // });
-  // describe('views', () => {
-  //   it('should return the views from state', () => {
-  //     const state = getEventCalendarStateFromParameters({
-  //       events: [],
-  //       views: ['day', 'week', 'month'],
-  //     });
-  //     const views = eventCalendarViewSelectors.views(state);
-  //     expect(views).toEqual(['day', 'week', 'month']);
-  //   });
-  // });
-  // describe('hasDayView', () => {
-  //   it('should return true if views include day', () => {
-  //     const state = getEventCalendarStateFromParameters({
-  //       events: [],
-  //       views: ['day', 'week', 'month'],
-  //     });
-  //     const hasDayView = eventCalendarViewSelectors.hasDayView(state);
-  //     expect(hasDayView).toBe(true);
-  //   });
-  //   it('should return false if views do not include day', () => {
-  //     const state = getEventCalendarStateFromParameters({
-  //       events: [],
-  //       views: ['week', 'month'],
-  //     });
-  //     const hasDayView = eventCalendarViewSelectors.hasDayView(state);
-  //     expect(hasDayView).toBe(false);
-  //   });
-  // });
+  describe('view', () => {
+    it('should return the view from state', () => {
+      const state = getEventCalendarStateFromParameters({
+        events: [],
+        view: 'month',
+      });
+      const view = eventCalendarViewSelectors.view(state);
+      expect(view).toBe('month');
+    });
+  });
+  describe('views', () => {
+    it('should return the views from state', () => {
+      const state = getEventCalendarStateFromParameters({
+        events: [],
+        views: ['day', 'week', 'month'],
+      });
+      const views = eventCalendarViewSelectors.views(state);
+      expect(views).toEqual(['day', 'week', 'month']);
+    });
+  });
+  describe('hasDayView', () => {
+    it('should return true if views include day', () => {
+      const state = getEventCalendarStateFromParameters({
+        events: [],
+        views: ['day', 'week', 'month'],
+      });
+      const hasDayView = eventCalendarViewSelectors.hasDayView(state);
+      expect(hasDayView).toBe(true);
+    });
+    it('should return false if views do not include day', () => {
+      const state = getEventCalendarStateFromParameters({
+        events: [],
+        views: ['week', 'month'],
+      });
+      const hasDayView = eventCalendarViewSelectors.hasDayView(state);
+      expect(hasDayView).toBe(false);
+    });
+  });
 });
