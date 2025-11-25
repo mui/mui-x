3d4308e4a6b2e8f1201d9601a750ef3f42cd388b
Andrew Cherniavskii <andrew@mui.com>
2025-11-25 14:03:09 +0100

    [data grid] Initialize data grid core packages (#20276)

---
+++ examples/data-grid-headless/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/x-data-grid-headless': path.resolve(
+        __dirname,
+        '../../packages/x-data-grid-headless/src',
+      ),
+      '@mui/x-data-grid-headless-pro': path.resolve(
+        __dirname,
+        '../../packages/x-data-grid-headless-pro/src',
+      ),
+      '@mui/x-data-grid-headless-premium': path.resolve(
+        __dirname,
+        '../../packages/x-data-grid-headless-premium/src',
+      ),
+    },
+  },
+  server: {
+    port: 3003,
+  },
+});
