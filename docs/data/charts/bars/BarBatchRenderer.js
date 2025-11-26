b97674525ea0f529707605502f129656cc75c103
Bernardo Belchior <bernardo@mui.com>
2025-11-26 15:53:16 +0100

    Add simple animation

--- docs/data/charts/bars/BarBatchRenderer.js
+++ docs/data/charts/bars/BarBatchRenderer.js
@@ -1,4 +1,4 @@
-import { BarChart } from '@mui/x-charts/BarChart';
+import { BarChartPro } from '@mui/x-charts-pro/BarChartPro';
 import data from '../dataset/sp500-intraday.json';
 
 const tickLabelDateFormatter = new Intl.DateTimeFormat(undefined, {
@@ -8,11 +8,12 @@ const tickLabelDateFormatter = new Intl.DateTimeFormat(undefined, {
 
 export default function BarBatchRenderer() {
   return (
-    <BarChart
+    <BarChartPro
       xAxis={[
         {
           data: data.map((d) => new Date(Date.parse(d.date))),
           valueFormatter: (v) => tickLabelDateFormatter.format(v),
+          zoom: true,
         },
       ]}
       series={[
