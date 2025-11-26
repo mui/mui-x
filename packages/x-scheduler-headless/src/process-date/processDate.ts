e14f65d3a4013afb947a69ad41f9c81943e9d1c2
delangle <flaviendelangle@gmail.com>
2025-11-26 13:45:55 +0100

    Work

--- packages/x-scheduler-headless/src/process-date/processDate.ts
+++ packages/x-scheduler-headless/src/process-date/processDate.ts
@@ -3,7 +3,7 @@ import { Adapter } from '../use-adapter';
 import { getDateKey } from '../utils/date-utils';
 
 /**
- * Creates a CalendarProcessedDate object from a date object.
+ * Creates a processed date object from a date object.
  */
 export function processDate(date: SchedulerValidDate, adapter: Adapter): SchedulerProcessedDate {
   return {
