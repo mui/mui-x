94edf95b2acfc621f29d9bcbd0bb27516fbfefd8
Rom Grk <romgrk.cc@gmail.com>
2025-11-28 04:12:15 -0500

    fix: use uSESWS in R18

--- packages/x-internals/src/store/useStore.ts
+++ packages/x-internals/src/store/useStore.ts
@@ -2,7 +2,13 @@ import * as React from 'react';
 /* We need to import the shim because React 17 does not support the `useSyncExternalStore` API.
  * More info: https://github.com/mui/mui-x/issues/18303#issuecomment-2958392341 */
 import { useSyncExternalStore } from 'use-sync-external-store/shim';
+import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/shim/with-selector';
+import reactMajor from '../reactMajor';
 import type { ReadonlyStore } from './Store';
+
+/* Some tests fail in R18 with the raw useSyncExternalStore. It may be possible to make it work
+ * but for now we only enable it for R19+. */
+const canUseRawUseSyncExternalStore = reactMajor >= 19;
 
 export function useStore<State, Value>(
   store: ReadonlyStore<State>,
@@ -33,10 +39,18 @@ export function useStore(
   a2?: unknown,
   a3?: unknown,
 ): unknown {
-  const getSelection = React.useMemo(
-    () => () => selector(store.getSnapshot(), a1, a2, a3),
-    [store, selector, a1, a2, a3],
-  );
-
-  return useSyncExternalStore(store.subscribe, getSelection, getSelection);
+  if (canUseRawUseSyncExternalStore) {
+    const getSelection = React.useMemo(
+      () => () => selector(store.getSnapshot(), a1, a2, a3),
+      [store, selector, a1, a2, a3],
+    );
+    return useSyncExternalStore(store.subscribe, getSelection, getSelection);
+  } else {
+    return useSyncExternalStoreWithSelector(
+      store.subscribe,
+      store.getSnapshot,
+      store.getSnapshot,
+      (state) => selector(state, a1, a2, a3),
+    );
+  }
 }
