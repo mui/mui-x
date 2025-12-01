1064adf09f2c29a6a71c01603cc5b39cf8304ca0
Bernardo Belchior <bernardo@mui.com>
2025-12-01 16:53:52 +0100

    [charts] Refactor bar chart components in preparation for range bar chart  (#20521)

--- packages/x-charts/src/BarChart/BarLabel/BarLabelPlot.tsx
+++ packages/x-charts/src/BarChart/BarLabel/BarLabelPlot.tsx
@@ -1,20 +1,42 @@
-import type { ProcessedBarSeriesData } from '../types';
+import * as React from 'react';
+import { AnimationData } from '../types';
 import { BarLabelItem, BarLabelItemProps } from './BarLabelItem';
-import { useUtilityClasses } from '../barClasses';
+import type { SeriesId } from '../../models/seriesType/common';
+import { BarSeriesType, BarValueType } from '../../models/seriesType/bar';
+import { BarLabelFunction } from './BarLabel.types';
 
-type BarLabelPlotProps = {
-  processedSeries: ProcessedBarSeriesData;
+interface BarLabelPlotProps<V extends BarValueType | null = BarValueType | null> {
+  processedSeries: ProcessedBarLabelSeriesData<V>;
+  className: string;
   skipAnimation?: boolean;
-  barLabel?: BarLabelItemProps['barLabel'];
-};
+  barLabel?: BarLabelItemProps<V | null>['barLabel'];
+}
+
+export interface ProcessedBarLabelSeriesData<V extends BarValueType | null> {
+  seriesId: SeriesId;
+  data: ProcessedBarLabelData<V>[];
+  barLabel?: 'value' | BarLabelFunction<V>;
+  barLabelPlacement?: BarSeriesType['barLabelPlacement'];
+  layout?: 'vertical' | 'horizontal';
+  xOrigin: number;
+  yOrigin: number;
+}
+
+export interface ProcessedBarLabelData<V extends BarValueType | null> extends AnimationData {
+  seriesId: SeriesId;
+  dataIndex: number;
+  color: string;
+  value: V;
+}
 
 /**
  * @ignore - internal component.
  */
-function BarLabelPlot(props: BarLabelPlotProps) {
-  const { processedSeries, skipAnimation, ...other } = props;
+function BarLabelPlot<V extends BarValueType | null = BarValueType | null>(
+  props: BarLabelPlotProps<V>,
+) {
+  const { processedSeries, className, skipAnimation, ...other } = props;
   const { seriesId, data, layout, xOrigin, yOrigin } = processedSeries;
-  const classes = useUtilityClasses();
 
   const barLabel = processedSeries.barLabel ?? props.barLabel;
 
@@ -23,7 +45,7 @@ function BarLabelPlot(props: BarLabelPlotProps) {
   }
 
   return (
-    <g key={seriesId} className={classes.seriesLabels} data-series={seriesId}>
+    <g key={seriesId} className={className} data-series={seriesId}>
       {data.map(({ x, y, dataIndex, color, value, width, height }) => (
         <BarLabelItem
           key={dataIndex}
