c42ea530801f0745f515c672f60b79456b465bc3
Bernardo Belchior <bernardo@mui.com>
2025-12-03 12:05:32 +0100

    Re-add new badge

--- docs/data/pages.ts
+++ docs/data/pages.ts
@@ -530,13 +530,14 @@ const pages: MuiPage[] = [
           {
             pathname: '/x/react-charts-bars',
             title: 'Bars',
+            newFeature: true,
             children: [
               { pathname: '/x/react-charts/bars', title: 'Bars overview' },
               {
                 pathname: '/x/react-charts/range-bar',
                 title: 'Range bar',
                 plan: 'pro',
-                unstable: true,
+                newFeature: true,
               },
               { pathname: '/x/react-charts/bar-demo', title: 'Demos' },
             ],
