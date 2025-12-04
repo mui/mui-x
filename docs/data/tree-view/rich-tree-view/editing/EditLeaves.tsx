1d8c5133677561c06cf049c265d84036875f41bf
Flavien DELANGLE <flaviendelangle@gmail.com>
2025-12-04 14:20:15 +0100

    [tree view] Expose one hook per component to initialize the `apiRef` (#20235)

--- docs/data/tree-view/rich-tree-view/editing/EditLeaves.tsx
+++ docs/data/tree-view/rich-tree-view/editing/EditLeaves.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
-import { useTreeViewApiRef } from '@mui/x-tree-view/hooks';
+import { useRichTreeViewApiRef } from '@mui/x-tree-view/hooks';
 import { TreeViewBaseItem } from '@mui/x-tree-view/models';
 
 type ExtendedTreeItemProps = {
@@ -43,7 +43,7 @@ const MUI_X_PRODUCTS: TreeViewBaseItem<ExtendedTreeItemProps>[] = [
 ];
 
 export default function EditLeaves() {
-  const apiRef = useTreeViewApiRef();
+  const apiRef = useRichTreeViewApiRef();
   return (
     <Box sx={{ minHeight: 352, minWidth: 260 }}>
       <RichTreeView
