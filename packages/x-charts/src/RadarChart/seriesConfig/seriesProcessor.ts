ccb3f4f916f70c9bbf845b8a3b7c317e346c61c1
Jose Quintas <juniorquintas@gmail.com>
2025-12-04 15:02:24 +0100

    [charts] General type improvements

---
+++ packages/x-charts/src/RadarChart/seriesConfig/seriesProcessor.ts
@@ -0,0 +1,29 @@
+import { type SeriesProcessor } from '../../internals/plugins/models/seriesConfig';
+import type { DefaultizedRadarSeriesType } from '../../models';
+import type { SeriesId } from '../../models/seriesType/common';
+
+const radarValueFormatter = ((v) =>
+  v == null ? '' : v.toLocaleString()) as DefaultizedRadarSeriesType['valueFormatter'];
+
+const seriesProcessor: SeriesProcessor<'radar'> = (params, _) => {
+  const { seriesOrder, series: seriesMap } = params;
+
+  const completedSeries: Record<SeriesId, DefaultizedRadarSeriesType> = {};
+
+  seriesOrder.forEach((seriesId) => {
+    const series = seriesMap[seriesId];
+
+    completedSeries[seriesId] = {
+      labelMarkType: 'square',
+      valueFormatter: radarValueFormatter,
+      ...series,
+    };
+  });
+
+  return {
+    seriesOrder,
+    series: completedSeries,
+  };
+};
+
+export default seriesProcessor;
