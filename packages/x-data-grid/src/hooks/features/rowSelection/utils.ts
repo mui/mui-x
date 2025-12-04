c102d8e58265ab5f87baeb28305afb025768aee7
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2025-12-04 23:43:11 +0100

    Update utils.ts
    Co-authored-by: Andrew Cherniavskii <andrew.cherniavskii@gmail.com>
    Signed-off-by: Michel Engelen <32863416+michelengelen@users.noreply.github.com>

--- packages/x-data-grid/src/hooks/features/rowSelection/utils.ts
+++ packages/x-data-grid/src/hooks/features/rowSelection/utils.ts
@@ -111,13 +111,7 @@ export const checkboxPropsSelector = createSelector(
         node.children.forEach(traverseDescendants);
       }
       // Check if row is selectable before considering it for parent selection state
-      const params: GridRowParams = {
-        id: itemToTraverseId,
-        row: rowsLookup[itemToTraverseId],
-        columns,
-      };
-      const rowIsSelectable =
-        typeof isRowSelectable === 'function' ? isRowSelectable(params) : true;
+      const rowIsSelectable = apiRef.current.isRowSelectable(itemToTraverseId);
 
       // Only consider selectable rows when determining parent selection state
       if (rowIsSelectable) {
