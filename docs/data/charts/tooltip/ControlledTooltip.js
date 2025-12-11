a57afcfc95aa4ccadc5375987c2a0c9982cff586
alex <alex.fauquette@gmail.com>
2025-12-11 13:49:13 +0100

    script

--- docs/data/charts/tooltip/ControlledTooltip.js
+++ docs/data/charts/tooltip/ControlledTooltip.js
@@ -77,6 +77,7 @@ export default function ControlledTooltip() {
         </Stack>
         <BarChart
           {...barChartsProps}
+          slotProps={{ tooltip: { trigger: 'item' } }}
           tooltipItem={tooltipItem}
           onTooltipItemChange={setTooltipItem}
         />
