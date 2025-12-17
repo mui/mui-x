43a6d570bb9df8f239e56f48ca41f753c744ee99
Jose Quintas <juniorquintas@gmail.com>
2025-12-17 16:26:39 +0100

    bit saner types

--- packages/x-charts/src/internals/stacking/offset/offsetDiverging.ts
+++ packages/x-charts/src/internals/stacking/offset/offsetDiverging.ts
@@ -1,9 +1,11 @@
+import type { Series } from '@mui/x-charts-vendor/d3-shape';
+
 /**
  * Adapted from D3.js's offsetDiverging function https://github.com/d3/d3-shape/blob/main/src/offset/diverging.js
  * The original function return 0 values stacked at 0, this implementation put them either
  * at the positive or negative sum according to the original data.
  */
-export function offsetDiverging(series: any[], order: Iterable<number>) {
+export function offsetDiverging(series: Series<any, any>, order: Iterable<number>) {
   if (series.length === 0) {
     return;
   }
@@ -17,7 +19,8 @@ export function offsetDiverging(series: any[], order: Iterable<number>) {
     let negativeSum = 0;
 
     for (let seriesIndex = 0; seriesIndex < seriesCount; seriesIndex += 1) {
-      const dataPoint = series[numericOrder[seriesIndex]][pointIndex];
+      const currentSeries = series[numericOrder[seriesIndex]] as any as Series<any, any>;
+      const dataPoint = currentSeries[pointIndex];
       const difference = dataPoint[1] - dataPoint[0];
 
       if (difference > 0) {
@@ -28,10 +31,10 @@ export function offsetDiverging(series: any[], order: Iterable<number>) {
         dataPoint[1] = negativeSum;
         negativeSum += difference;
         dataPoint[0] = negativeSum;
-      } else if (dataPoint.data[series[numericOrder[seriesIndex]].key] > 0) {
+      } else if (dataPoint.data[currentSeries.key] > 0) {
         dataPoint[0] = positiveSum;
         dataPoint[1] = positiveSum;
-      } else if (dataPoint.data[series[numericOrder[seriesIndex]].key] < 0) {
+      } else if (dataPoint.data[currentSeries.key] < 0) {
         dataPoint[1] = negativeSum;
         dataPoint[0] = negativeSum;
       } else {
