0886d2b685ca94a1f90b5858244f1ffa9549ae76
Lukas <llukas.tyla@gmail.com>
2025-12-19 15:51:34 +0200

    Ad missing import

--- packages/x-date-pickers/src/DesktopTimePicker/tests/DesktopTimePicker.test.tsx
+++ packages/x-date-pickers/src/DesktopTimePicker/tests/DesktopTimePicker.test.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { spy } from 'sinon';
 import { screen, waitFor } from '@mui/internal-test-utils';
 import { DesktopTimePicker } from '@mui/x-date-pickers/DesktopTimePicker';
