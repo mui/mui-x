e817dd6eaafde616d2683068ca1f15c1b0f0188c
Flavien DELANGLE <flaviendelangle@gmail.com>
2025-12-29 14:20:49 +0100

    [tree view] Introduce a Tree View Store to clean the internals (#20051)
    Signed-off-by: Flavien DELANGLE <flaviendelangle@gmail.com>

---
+++ packages/x-tree-view/src/internals/hooks/useTreeViewRootProps.ts
@@ -0,0 +1,43 @@
+'use client';
+import * as React from 'react';
+import { useStore } from '@mui/x-internals/store';
+import { EventHandlers } from '@mui/utils/types';
+import { TreeViewCancellableEvent } from '../../models';
+import { idSelectors } from '../plugins/id';
+import { selectionSelectors } from '../plugins/selection';
+import { itemsSelectors } from '../plugins/items';
+import { TreeViewAnyStore } from '../models';
+
+export function useTreeViewRootProps<TStore extends TreeViewAnyStore>(
+  store: TStore,
+  forwardedProps: React.HTMLAttributes<HTMLUListElement>,
+  ref: React.Ref<HTMLUListElement | null> | undefined,
+) {
+  const treeId = useStore(store, idSelectors.treeId);
+  const itemChildrenIndentation = useStore(store, itemsSelectors.itemChildrenIndentation);
+  const isMultiSelectEnabled = useStore(store, selectionSelectors.isMultiSelectEnabled);
+
+  return (otherHandlers: EventHandlers) => ({
+    ref,
+    role: 'tree',
+    id: treeId,
+    'aria-multiselectable': isMultiSelectEnabled,
+    ...forwardedProps,
+    ...otherHandlers,
+    style: {
+      ...forwardedProps.style,
+      '--TreeView-itemChildrenIndentation':
+        typeof itemChildrenIndentation === 'number'
+          ? `${itemChildrenIndentation}px`
+          : itemChildrenIndentation,
+    } as React.CSSProperties,
+    onFocus: (event: React.FocusEvent<HTMLUListElement> & TreeViewCancellableEvent) => {
+      otherHandlers.onFocus?.(event);
+      store.focus.handleRootFocus(event);
+    },
+    onBlur: (event: React.FocusEvent<HTMLUListElement> & TreeViewCancellableEvent) => {
+      otherHandlers.onBlur?.(event);
+      store.focus.handleRootBlur(event);
+    },
+  });
+}
