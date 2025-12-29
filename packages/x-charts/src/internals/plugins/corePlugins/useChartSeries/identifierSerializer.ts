03ceaad5302cc905e2bfd3cffc149e977dea40cc
Jose Quintas <juniorquintas@gmail.com>
2025-12-29 15:58:30 +0100

    init identifier serializer

---
+++ packages/x-charts/src/internals/plugins/corePlugins/useChartSeries/identifierSerializer.ts
@@ -0,0 +1,17 @@
+import type { SeriesId } from '../../../../models';
+
+export const typeSerializer = (type: string) => `Type(${type})`;
+export const seriesIdSerializer = (id: SeriesId) => `Series(${id})`;
+export const dataIndexSerializer = (dataIndex?: number) => `Index(${dataIndex})`;
+
+export const identifierSerializerSeriesIdDataIndex = <
+  T extends {
+    type: string;
+    seriesId: SeriesId;
+    dataIndex?: number;
+  },
+>(
+  identifier: T,
+) => {
+  return `${typeSerializer(identifier.type)}${seriesIdSerializer(identifier.seriesId)}${dataIndexSerializer(identifier.dataIndex)}`;
+};
