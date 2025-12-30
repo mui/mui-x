39850ba6408737233dcce0626ac9d1579cb7eaa1
Jose Quintas <juniorquintas@gmail.com>
2025-12-30 11:21:40 +0100

    fix

--- packages/x-charts/src/internals/identifierSerializer.ts
+++ packages/x-charts/src/internals/identifierSerializer.ts
@@ -5,14 +5,10 @@ export const seriesIdSerializer = (id: SeriesId) => `Series(${id})`;
 export const dataIndexSerializer = (dataIndex?: number) =>
   dataIndex === undefined ? '' : `Index(${dataIndex})`;
 
-export const identifierSerializerSeriesIdDataIndex = <
-  T extends {
-    type: string;
-    seriesId: SeriesId;
-    dataIndex?: number;
-  },
->(
-  identifier: T,
-) => {
+export const identifierSerializerSeriesIdDataIndex = (identifier: {
+  type: string;
+  seriesId: SeriesId;
+  dataIndex?: number;
+}) => {
   return `${typeSerializer(identifier.type)}${seriesIdSerializer(identifier.seriesId)}${dataIndexSerializer(identifier.dataIndex)}`;
 };
