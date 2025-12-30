cb4655c5de45fad1f85095531de3c6f1b94fc2bd
Jose C Quintas Jr <juniorquintas@gmail.com>
2025-12-30 10:57:00 +0100

    Update packages/x-charts/src/internals/identifierSerializer.ts
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
    Signed-off-by: Jose C Quintas Jr <juniorquintas@gmail.com>

--- packages/x-charts/src/internals/identifierSerializer.ts
+++ packages/x-charts/src/internals/identifierSerializer.ts
@@ -2,7 +2,7 @@ import type { SeriesId } from '../models';
 
 export const typeSerializer = (type: string) => `Type(${type})`;
 export const seriesIdSerializer = (id: SeriesId) => `Series(${id})`;
-export const dataIndexSerializer = (dataIndex?: number) => `Index(${dataIndex})`;
+export const dataIndexSerializer = (dataIndex?: number) => dataIndex ==== undefined ? '' : `Index(${dataIndex})`;
 
 export const identifierSerializerSeriesIdDataIndex = <
   T extends {
