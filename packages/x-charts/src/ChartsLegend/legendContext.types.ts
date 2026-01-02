5afbf6d88cab552d202ac064c12fa0ad2a7de9b6
Jose C Quintas Jr <juniorquintas@gmail.com>
2026-01-02 19:16:09 +0100

    Update packages/x-charts/src/ChartsLegend/legendContext.types.ts
    Co-authored-by: Bernardo Belchior <Bernardo.belchior1@gmail.com>
    Signed-off-by: Jose C Quintas Jr <juniorquintas@gmail.com>

--- packages/x-charts/src/ChartsLegend/legendContext.types.ts
+++ packages/x-charts/src/ChartsLegend/legendContext.types.ts
@@ -26,6 +26,7 @@ export interface LegendItemParams
   id: number | string;
   /**
    * The type of the series
+   * This property is always defined, but marked as optional to avoid breaking changes. It will become required in a future major version.
    */
   type?: ChartSeriesType;
   markType: ChartsLabelMarkProps['type'];
