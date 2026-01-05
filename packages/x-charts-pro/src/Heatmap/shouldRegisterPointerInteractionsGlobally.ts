7e4a136be909563c850665c32530a309a4933618
Bernardo Belchior <bernardo@mui.com>
2026-01-05 06:55:17 +0000

    Add comment

--- packages/x-charts-pro/src/Heatmap/shouldRegisterPointerInteractionsGlobally.ts
+++ packages/x-charts-pro/src/Heatmap/shouldRegisterPointerInteractionsGlobally.ts
@@ -1,6 +1,8 @@
 import { type HeatmapPlotProps } from './HeatmapPlot';
 import { HeatmapCell } from './internals/HeatmapCell';
 
+/* Global pointer interactions should be registered when we're using the default HeatmapCell.
+ * We only want to return false when a custom slot is being used to avoid breaking changes. */
 export function shouldRegisterPointerInteractionsGlobally(
   slots: HeatmapPlotProps['slots'],
 ): boolean {
