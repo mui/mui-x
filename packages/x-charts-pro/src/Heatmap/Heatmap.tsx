bad1265ba45e603afb2baf5dde41bdefdda93398
alex <alex.fauquette@gmail.com>
2026-01-05 15:44:47 +0100

    deprecate axis click for heatmap

--- packages/x-charts-pro/src/Heatmap/Heatmap.tsx
+++ packages/x-charts-pro/src/Heatmap/Heatmap.tsx
@@ -76,6 +76,7 @@ export interface HeatmapSlotProps
 }
 
 export type HeatmapSeries = MakeOptional<HeatmapSeriesType, 'type'>;
+
 export interface HeatmapProps
   extends
     Omit<
@@ -90,9 +91,19 @@ export interface HeatmapProps
       | 'experimentalFeatures'
       | 'highlightedAxis'
       | 'onHighlightedAxisChange'
+      | 'onAxisClick'
     >,
     Omit<ChartsAxisProps, 'slots' | 'slotProps'>,
     Omit<ChartsOverlayProps, 'slots' | 'slotProps'> {
+  /**
+   * The function called for onClick events.
+   * The second argument contains information about all line/bar elements at the current mouse position.
+   * @param {MouseEvent} event The mouse event recorded on the `<svg/>` element.
+   * @param {null | ChartsAxisData} data The data about the clicked axis and items associated with it.
+   *
+   * @deprecated Use `onCellClick` instead to get access to both x- and y-axis values.
+   */
+  onAxisClick?: ChartContainerProProps<'heatmap', HeatmapPluginSignatures>['onAxisClick'];
   /**
    * The configuration of the x-axes.
    * If not provided, a default axis config is used.
