c7fc4c2c610dad001774073fdd19098fb2bf2b2d
Bernardo Belchior <bernardo@mui.com>
2026-01-14 16:19:18 +0100

    Add test for line and scatter charts

--- packages/x-charts-pro/src/ChartZoomSlider/internals/ChartAxisZoomSliderPreview.test.tsx
+++ packages/x-charts-pro/src/ChartZoomSlider/internals/ChartAxisZoomSliderPreview.test.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { BarChartPro } from '@mui/x-charts-pro/BarChartPro';
 import { render } from '@mui/internal-test-utils';
+import { LineChartPro } from '@mui/x-charts-pro/LineChartPro';
+import { ScatterChartPro } from '@mui/x-charts-pro/ScatterChartPro';
 
 describe('ChartAxisZoomSliderPreview', () => {
-  it('handles two axes with zoom slider preview', () => {
+  it('handles two axes with zoom slider preview in a bar chart', () => {
     const { container } = render(
       <BarChartPro
         xAxis={[
@@ -17,6 +19,60 @@ describe('ChartAxisZoomSliderPreview', () => {
           },
         ]}
         series={[{ data: [1, 2, 3] }, { xAxisId: 'x2', data: [1, 2, 3] }]}
+        height={400}
+      />,
+    );
+
+    expect(container.querySelector('svg')).not.toBeNull();
+  });
+
+  it('handles two axes with zoom slider preview in an area chart', () => {
+    const { container } = render(
+      <LineChartPro
+        xAxis={[
+          { data: [1, 2, 4], zoom: { slider: { enabled: true, preview: true } } },
+          {
+            id: 'x2',
+            position: 'bottom',
+            data: [1, 2, 4],
+            zoom: { slider: { enabled: true, preview: true } },
+          },
+        ]}
+        series={[
+          { data: [1, 2, 4], area: true },
+          { xAxisId: 'x2', data: [1, 2, 4], area: true },
+        ]}
+        height={400}
+      />,
+    );
+
+    expect(container.querySelector('svg')).not.toBeNull();
+  });
+
+  it('handles two axes with zoom slider preview in a scatter chart', () => {
+    const { container } = render(
+      <ScatterChartPro
+        xAxis={[
+          { zoom: { slider: { enabled: true, preview: true } } },
+          { id: 'x2', position: 'bottom', zoom: { slider: { enabled: true, preview: true } } },
+        ]}
+        series={[
+          {
+            data: [
+              { x: 1, y: 1 },
+              { x: 2, y: 2 },
+              { x: 3, y: 3 },
+            ],
+          },
+          {
+            xAxisId: 'x2',
+            data: [
+              { x: 1, y: 1 },
+              { x: 2, y: 2 },
+              { x: 3, y: 3 },
+            ],
+          },
+        ]}
         height={400}
       />,
     );
