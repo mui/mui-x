0a381c910af793c56df5f8c0fa99d0ba93e5264f
Janpot <2109932+Janpot@users.noreply.github.com>
2026-01-14 18:36:38 +0100

    more fixes

--- packages/x-charts-pro/src/SankeyChart/index.ts
+++ packages/x-charts-pro/src/SankeyChart/index.ts
@@ -14,4 +14,8 @@ export * from './sankeyHighlightHooks';
 export * from '../hooks/useSankeySeries';
 export { type SankeyPlotClasses, sankeyPlotClasses } from './sankeyClasses';
 export { SankeyTooltip, SankeyTooltipContent } from './SankeyTooltip';
-export type { SankeyTooltipProps } from './SankeyTooltip';
+export type {
+  SankeyTooltipProps,
+  SankeyTooltipClasses,
+  SankeyTooltipContentClasses,
+} from './SankeyTooltip';
