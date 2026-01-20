8d1f3a55085da990fe7c0cbf7ccf5495a59a06ec
Jose C Quintas Jr <juniorquintas@gmail.com>
2026-01-20 14:22:42 +0100

    [code-infra] Fix `renameImports` codemod not preserving comments (#20977)

--- packages/x-codemod/src/v8.0.0/pickers/rename-adapter-date-fns-imports/expected-community-import.spec.tsx
+++ packages/x-codemod/src/v8.0.0/pickers/rename-adapter-date-fns-imports/expected-community-import.spec.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { AdapterDateFns as DateFns } from '@mui/x-date-pickers/AdapterDateFnsV2';
 import { AdapterDateFnsJalali } from '@mui/x-date-pickers/AdapterDateFnsJalali';
