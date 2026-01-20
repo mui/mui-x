b9bd0f0957489237a62ba8858ee677e1e372569e
Flavien DELANGLE <flaviendelangle@gmail.com>
2026-01-20 09:35:17 +0100

    [tree view] Fix `props.id` not passed to the root element (#20976)

--- packages/x-tree-view/src/internals/plugins/id/selectors.ts
+++ packages/x-tree-view/src/internals/plugins/id/selectors.ts
@@ -2,18 +2,22 @@ import { createSelector } from '@mui/x-internals/store';
 import { MinimalTreeViewState } from '../../MinimalTreeViewStore';
 import { TreeViewItemId } from '../../../models';
 
+const treeIdSelector = createSelector(
+  (state: MinimalTreeViewState<any, any>) => state.providedTreeId ?? state.treeId,
+);
+
 export const idSelectors = {
   /**
    * Get the id attribute of the tree view.
    */
-  treeId: createSelector((state: MinimalTreeViewState<any, any>) => state.treeId),
+  treeId: treeIdSelector,
   /**
    * Generate the id attribute (i.e.: the `id` attribute passed to the DOM element) of a Tree Item.
    * If the user explicitly defined an id attribute, it will be returned.
    * Otherwise, the method creates a unique id for the item based on the Tree View id attribute and the item `itemId`
    */
   treeItemIdAttribute: createSelector(
-    (state: MinimalTreeViewState<any, any>) => state.treeId,
+    treeIdSelector,
     (treeId, itemId: TreeViewItemId, providedIdAttribute: string | undefined) => {
       if (providedIdAttribute != null) {
         return providedIdAttribute;
