1f79ed1b684df42123b67c810f6cf0902d8e1121
Andrew <andrew@mui.com>
2026-01-21 14:24:20 +0100

    remove unnecessary plugin exports

--- packages/x-data-grid-headless/src/index.ts
+++ packages/x-data-grid-headless/src/index.ts
@@ -1,20 +1,10 @@
 // Hooks
 export { useDataGrid } from './hooks/useDataGrid';
 
-// Plugins
-export { default as sortingPlugin } from './plugins/sorting';
-export { default as paginationPlugin } from './plugins/pagination';
-
 export { createPlugin } from './plugins/core/plugin';
 export type { Plugin, AnyPlugin, ExtractPluginApi } from './plugins/core/plugin';
 export type { PluginsApi, PluginsOptions, PluginsState } from './plugins/core/helpers';
 export { PluginRegistry } from './plugins/core/pluginRegistry';
-
-// Sorting types
-export type { SortModel } from './plugins/sorting';
-
-// Pagination types
-export type { PaginationModel } from './plugins/pagination';
 
 // Column definition types
 export type { ColumnDef } from './columnDef/columnDef';
