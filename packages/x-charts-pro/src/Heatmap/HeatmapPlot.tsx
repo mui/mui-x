e3f50ad70f8437529333513afda5f9c00ed683c8
Bernardo Belchior <bernardo@mui.com>
2026-01-22 15:35:03 +0100

    Fix props not being generated

--- packages/x-charts-pro/src/Heatmap/HeatmapPlot.tsx
+++ packages/x-charts-pro/src/Heatmap/HeatmapPlot.tsx
@@ -1,9 +1,34 @@
 'use client';
+import * as React from 'react';
+import PropTypes from 'prop-types';
 import { HeatmapSVGPlot } from './HeatmapSVGPlot';
 import { type HeatmapRendererPlotProps } from './Heatmap.types';
 
 export interface HeatmapPlotProps extends HeatmapRendererPlotProps {}
 
-export function HeatmapPlot({ borderRadius, ...props }: HeatmapPlotProps) {
+function HeatmapPlot({ borderRadius, ...props }: HeatmapPlotProps): React.ReactNode {
   return <HeatmapSVGPlot borderRadius={borderRadius} {...props} />;
 }
+
+HeatmapPlot.propTypes = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // | To update them edit the TypeScript types and run "pnpm proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * The border radius of the heatmap cells in pixels.
+   */
+  borderRadius: PropTypes.number,
+  /**
+   * The props used for each component slot.
+   * @default {}
+   */
+  slotProps: PropTypes.object,
+  /**
+   * Overridable component slots.
+   * @default {}
+   */
+  slots: PropTypes.object,
+} as any;
+
+export { HeatmapPlot };
