352daac164442ebd5e2d71adc2e07cdaa9c77d75
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-01-23 12:52:26 +0530

    Refactor babel config and dependencies

--- babel.config.js
+++ babel.config.js
@@ -9,34 +9,38 @@ const generateReleaseInfo = require('./packages/x-license/generateReleaseInfo');
 module.exports = function getBabelConfig(api) {
   const baseConfig = getBaseConfig(api);
 
-  const plugins = [
-    ['@babel/plugin-transform-object-rest-spread', { loose: true }],
-    [
-      'babel-plugin-transform-react-remove-prop-types',
-      {
-        mode: 'unsafe-wrap',
-        ignoreFilenames: ['DataGrid.tsx', 'DataGridPro.tsx'],
-      },
-    ],
-    [
-      '@mui/internal-babel-plugin-display-name',
-      {
-        allowedCallees: {
-          '@mui/x-internals/forwardRef': ['forwardRef'],
-        },
-      },
-      'mui-x-display-name',
-    ],
-  ];
+  const removePropTypePlugin = baseConfig.plugins.find(
+    (p) => p[2] === 'babel-plugin-transform-react-remove-prop-types',
+  );
+  if (removePropTypePlugin) {
+    removePropTypePlugin[1] ??= {};
+    removePropTypePlugin[1].mode = 'unsafe-wrap';
+    removePropTypePlugin[1].ignoreFilenames ??= [];
+    removePropTypePlugin[1].ignoreFilenames.push('DataGrid.tsx', 'DataGridPro.tsx');
+  }
+  const displayNamePlugin = baseConfig.plugins.find(
+    (p) => p[2] === '@mui/internal-babel-plugin-display-name',
+  );
+
+  if (displayNamePlugin) {
+    displayNamePlugin[1] ??= {};
+    displayNamePlugin[1].allowedCallees = {
+      ...displayNamePlugin[1].allowedCallees,
+      '@mui/x-internals/forwardRef': ['forwardRef'],
+    };
+  }
 
   if (process.env.NODE_ENV === 'production') {
     if (!process.env.TEST_BUILD) {
-      plugins.push(['babel-plugin-react-remove-properties', { properties: ['data-testid'] }]);
+      baseConfig.plugins.push([
+        'babel-plugin-react-remove-properties',
+        { properties: ['data-testid'] },
+      ]);
     }
 
     if (process.env.BABEL_ENV) {
-      plugins.push([
-        'search-and-replace',
+      baseConfig.plugins.push([
+        'babel-plugin-search-and-replace',
         {
           rules: [
             {
@@ -50,21 +54,15 @@ module.exports = function getBabelConfig(api) {
   }
 
   if (process.env.BABEL_ENV || process.env.NODE_ENV === 'test') {
-    plugins.push([
-      'transform-replace-expressions',
+    baseConfig.plugins.push([
+      'babel-plugin-transform-replace-expressions',
       {
         replace: [['LICENSE_DISABLE_CHECK', 'false']],
       },
     ]);
   }
 
-  baseConfig.plugins = baseConfig.plugins
-    .filter(
-      ([, , pluginName]) =>
-        pluginName !== 'babel-plugin-transform-react-remove-prop-types' &&
-        pluginName !== '@mui/internal-babel-plugin-display-name',
-    )
-    .concat(plugins);
+  baseConfig.plugins.push(['@babel/plugin-transform-object-rest-spread', { loose: true }]);
   delete baseConfig.assumptions.setSpreadProperties;
 
   return baseConfig;
