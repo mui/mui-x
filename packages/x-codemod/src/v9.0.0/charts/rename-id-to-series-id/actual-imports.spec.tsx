c13a08d5b3fc44412a193ae393949c17d71a6f36
alex <alex.fauquette@gmail.com>
2026-01-23 18:01:39 +0100

    add codemod

---
+++ packages/x-codemod/src/v9.0.0/charts/rename-id-to-series-id/actual-imports.spec.tsx
@@ -0,0 +1,16 @@
+// @ts-nocheck
+
+<div>
+  <PieArc id="test" />
+  <PieArcPlot id={'test'} />
+  <PieArcLabel id="test" />
+  <PieArcLabelPlot id="test" />
+  <AnimatedPieArcElement id={'test'} />
+  <BarElement id="test" />
+  <AnimatedArea id="test" />
+  <AnimatedLine id="test" />
+  <MarkElement id={'test'} />
+  <AreaElement id="test" />
+  <LineElement id="test" />
+  <LineHighlightElement id={'test'} />
+</div>;
