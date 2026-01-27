44162dade4b3d1e5b9cc70647d4c133e914fc66b
Jose Quintas <juniorquintas@gmail.com>
2026-01-27 14:41:12 +0100

    revert NODE_ENV

--- packages/x-codemod/src/util/readFile.ts
+++ packages/x-codemod/src/util/readFile.ts
@@ -2,9 +2,6 @@ import fs, { PathOrFileDescriptor } from 'fs';
 import { EOL } from 'os';
 
 export default function readFile(filePath: PathOrFileDescriptor) {
-  if (process.env.NODE_ENV !== 'test') {
-    return '';
-  }
   const fileContents = fs.readFileSync(filePath, 'utf8').toString();
   if (EOL !== '\n') {
     return fileContents.replace(/\n/g, EOL);
