48ad38210e568329936eda749a6b03759a733962
Bernardo Belchior <bernardo@mui.com>
2026-01-27 11:34:04 +0100

    Rectangles are showing

--- packages/x-charts-premium/src/CandlestickChart/CandlestickPlot.tsx
+++ packages/x-charts-premium/src/CandlestickChart/CandlestickPlot.tsx
@@ -1,8 +1,20 @@
 'use client';
 import * as React from 'react';
-import { useDrawingArea, useStore, useWebGLContext, WebGLProvider } from '@mui/x-charts/internals';
+import {
+  type ContinuousScaleName,
+  useDrawingArea,
+  useWebGLContext,
+  WebGLProvider,
+} from '@mui/x-charts/internals';
 import { useXScale, useYScale } from '@mui/x-charts/hooks';
-import { compileShader, logWebGLErrors, uploadQuadBuffer } from '../HeatmapPremium/webgl/utils';
+import { type DefaultizedOHLCSeriesType } from '../models';
+import { useOHLCSeriesContext } from '../hooks/useOHLCSeries';
+import {
+  bindQuadBuffer,
+  compileShader,
+  logWebGLErrors,
+  uploadQuadBuffer,
+} from '../HeatmapPremium/webgl/utils';
 import { useWebGLResizeObserver } from '../HeatmapPremium/webgl/useWebGLResizeObserver';
 import { candlestickFragmentShader, candlestickVertexShader } from './shaders';
 
@@ -18,12 +30,15 @@ export function CandlestickPlot() {
 
 function CandlestickWebGLPlot() {
   const gl = useWebGLContext();
+  const series = useOHLCSeriesContext();
 
-  if (!gl) {
+  const seriesToDisplay = series?.series[series.seriesOrder[0]];
+
+  if (!gl || !seriesToDisplay) {
     return null;
   }
 
-  return <CandlestickWebGLPlotImpl gl={gl} />;
+  return <CandlestickWebGLPlotImpl gl={gl} series={seriesToDisplay} />;
 }
 
 function initializeProgram(gl: WebGL2RenderingContext) {
@@ -42,20 +57,24 @@ function initializeProgram(gl: WebGL2RenderingContext) {
     console.error(`Fragment shader info-log: ${gl.getShaderInfoLog(fragmentShader)}`);
   }
 
-  gl.useProgram(program);
-  logWebGLErrors(gl);
-
   return program;
 }
 
-function CandlestickWebGLPlotImpl({ gl }: { gl: WebGL2RenderingContext }) {
+const CANDLESTICK_WIDTH = 10;
+
+function CandlestickWebGLPlotImpl({
+  gl,
+  series,
+}: {
+  gl: WebGL2RenderingContext;
+  series: DefaultizedOHLCSeriesType;
+}) {
   const drawingArea = useDrawingArea();
-  const xScale = useXScale();
-  const yScale = useYScale();
-  const store = useStore();
+  // TODO: Validate this earlier in the processing pipeline
+  const xScale = useXScale<ContinuousScaleName>();
+  const yScale = useYScale<ContinuousScaleName>();
 
   const [program] = React.useState<WebGLProgram>(() => initializeProgram(gl));
-  const [quadBuffer] = React.useState(() => uploadQuadBuffer(gl));
   const dataLength = series.data.length;
   const renderScheduledRef = React.useRef<boolean>(false);
 
@@ -83,25 +102,91 @@ function CandlestickWebGLPlotImpl({ gl }: { gl: WebGL2RenderingContext }) {
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
   }, [gl]);
 
-  const setupResolutionUniform = React.useCallback(() => {
+  React.useEffect(() => {
+    // This isn't a hook
+    // eslint-disable-next-line react-compiler/react-compiler
+    gl.useProgram(program);
+    logWebGLErrors(gl);
+  }, [gl, program]);
+
+  React.useEffect(() => {
+    // Setup constants
+    bindQuadBuffer(gl, program, uploadQuadBuffer(gl));
+
+    gl.uniform1f(gl.getUniformLocation(program, 'u_width'), CANDLESTICK_WIDTH);
+  }, [gl, program]);
+
+  React.useEffect(() => {
     gl.uniform2f(
       gl.getUniformLocation(program, 'u_resolution'),
       drawingArea.width,
       drawingArea.height,
     );
-  }, [drawingArea.height, drawingArea.width, gl, program]);
 
-  const setupAttributes = React.useCallback(() => {}, []);
+    scheduleRender();
+  }, [drawingArea.height, drawingArea.width, gl, program, scheduleRender]);
 
   React.useEffect(() => {
-    setupResolutionUniform();
-    scheduleRender();
-  }, [setupResolutionUniform, scheduleRender]);
+    const centers = new Float32Array(series.data.length * 2);
+    const heights = new Float32Array(series.data.length);
+    const colors = new Float32Array(series.data.length * 4);
 
-  React.useEffect(() => {
-    setupAttributes();
+    for (let dataIndex = 0; dataIndex < series.data.length; dataIndex += 1) {
+      const [xValue, open, high, low, close] = series.data[dataIndex];
+      const x = xScale(xValue);
+      const [bottom, top] = [yScale(open), yScale(close)].sort();
+
+      centers[dataIndex * 2] = x + CANDLESTICK_WIDTH / 2;
+      centers[dataIndex * 2 + 1] = (top + bottom) / 2;
+      heights[dataIndex] = top - bottom;
+
+      if (close >= open) {
+        // Bullish - green
+        colors[dataIndex * 4] = 0.0;
+        colors[dataIndex * 4 + 1] = 1.0;
+        colors[dataIndex * 4 + 2] = 0.0;
+        colors[dataIndex * 4 + 3] = 1.0;
+      } else {
+        // Bearish - red
+        colors[dataIndex * 4] = 1.0;
+        colors[dataIndex * 4 + 1] = 0.0;
+        colors[dataIndex * 4 + 2] = 0.0;
+        colors[dataIndex * 4 + 3] = 1.0;
+      }
+    }
+
+    // Setup center attribute
+    const centerBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, centerBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, centers, gl.STATIC_DRAW);
+
+    const centerLocation = gl.getAttribLocation(program, 'a_center');
+    gl.enableVertexAttribArray(centerLocation);
+    gl.vertexAttribPointer(centerLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribDivisor(centerLocation, 1); // One per instance
+
+    // Setup height attribute
+    const heightBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, heightBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, heights, gl.STATIC_DRAW);
+
+    const heightLocation = gl.getAttribLocation(program, 'a_height');
+    gl.enableVertexAttribArray(heightLocation);
+    gl.vertexAttribPointer(heightLocation, 1, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribDivisor(heightLocation, 1); // One per instance
+
+    // Setup color attribute
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+    const colorLocation = gl.getAttribLocation(program, 'a_color');
+    gl.enableVertexAttribArray(colorLocation);
+    gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribDivisor(colorLocation, 1); // One per instance
+
     scheduleRender();
-  }, [scheduleRender, setupAttributes]);
+  }, [gl, program, scheduleRender, series.data, xScale, yScale]);
 
   React.useEffect(() => {
     if (renderScheduledRef.current) {
