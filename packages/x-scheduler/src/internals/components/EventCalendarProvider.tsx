d37283e06827c9d3fe052b25be9f6e168834c5f2
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2026-01-29 17:15:33 +0100

    add tokens to the provider so standalone components have tokens available

--- packages/x-scheduler/src/internals/components/EventCalendarProvider.tsx
+++ packages/x-scheduler/src/internals/components/EventCalendarProvider.tsx
@@ -1,9 +1,23 @@
 'use client';
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import { EventCalendarProvider as HeadlessEventCalendarProvider } from '@mui/x-scheduler-headless/event-calendar-provider';
 import { eventCalendarClasses } from '../../event-calendar/eventCalendarClasses';
 import { EventCalendarClassesContext } from '../../event-calendar/EventCalendarClassesContext';
 import { EventDialogClassesContext } from './event-draggable-dialog/EventDialogClassesContext';
+import { schedulerTokens } from '../utils/tokens';
+
+/**
+ * Root wrapper for standalone views that provides CSS variable tokens.
+ * This ensures event colors work correctly outside of EventCalendar.
+ */
+const StandaloneViewRoot = styled('div', {
+  name: 'MuiEventCalendar',
+  slot: 'StandaloneViewRoot',
+})({
+  ...schedulerTokens,
+  display: 'contents',
+});
 
 export function EventCalendarProvider<TEvent extends object, TResource extends object>(
   props: HeadlessEventCalendarProvider.Props<TEvent, TResource>,
@@ -14,7 +28,7 @@ export function EventCalendarProvider<TEvent extends object, TResource extends o
     <HeadlessEventCalendarProvider {...other}>
       <EventCalendarClassesContext.Provider value={eventCalendarClasses}>
         <EventDialogClassesContext.Provider value={eventCalendarClasses}>
-          {children}
+          <StandaloneViewRoot>{children}</StandaloneViewRoot>
         </EventDialogClassesContext.Provider>
       </EventCalendarClassesContext.Provider>
     </HeadlessEventCalendarProvider>
