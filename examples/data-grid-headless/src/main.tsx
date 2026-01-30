92bf111ace1da35524285de15aa145986bf94454
Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>
2026-01-30 10:54:51 +0100

    [internal] Headless sorting plugin (#21089)

--- examples/data-grid-headless/src/main.tsx
+++ examples/data-grid-headless/src/main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
