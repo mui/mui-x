9713585c1462c5cad38085a06da02e16e600f1b6
alex <alex.fauquette@gmail.com>
2026-01-30 11:43:15 +0100

    Let TS protect us

--- packages/x-charts/src/hooks/useInteractionItemProps.ts
+++ packages/x-charts/src/hooks/useInteractionItemProps.ts
@@ -40,11 +40,7 @@ export const useInteractionItemProps = <ChartSeries extends ChartSeriesType>(
     interactionActive.current = true;
     instance.setLastUpdateSource('pointer');
     instance.setTooltipItem(data);
-    // TODO: uniformize sankey and other types to get a single plugin
-    instance.setHighlight(
-      // @ts-ignore
-      data.type === 'sankey' ? data : { seriesId: data.seriesId, dataIndex: data.dataIndex },
-    );
+    instance.setHighlight(data);
   });
 
   const onPointerLeave = useEventCallback(() => {
@@ -79,7 +75,7 @@ export function getInteractionItemProps<SeriesType extends ChartSeriesType>(
   instance: ChartInstance<
     [UseChartInteractionSignature, UseChartHighlightSignature<SeriesType>, UseChartTooltipSignature]
   >,
-  item: SeriesItemIdentifier<ChartSeriesType>,
+  item: SeriesItemIdentifier<SeriesType>,
 ): {
   onPointerEnter?: () => void;
   onPointerLeave?: () => void;
@@ -91,10 +87,7 @@ export function getInteractionItemProps<SeriesType extends ChartSeriesType>(
     }
     instance.setLastUpdateSource('pointer');
     instance.setTooltipItem(item);
-    instance.setHighlight(
-      // @ts-ignore
-      item.type === 'sankey' ? item : { seriesId: item.seriesId, dataIndex: item.dataIndex },
-    );
+    instance.setHighlight(item);
   }
 
   function onPointerLeave() {
