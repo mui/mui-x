d48321547ca9df636c7ca2f108ed22e9a07aa066
Bernardo Belchior <bernardo@mui.com>
2026-02-04 16:18:54 +0100

    Start structure

---
+++ test/benchmark-tool/bench/ScatterBench.tsx
@@ -0,0 +1,20 @@
+import { ScatterChart } from '@mui/x-charts';
+
+const dataLength = 1_400;
+const data = Array.from({ length: dataLength }).map((_, i) => ({
+  x: i,
+  y: 50 + Math.sin(i / 5) * 25,
+}));
+
+const xData = data.map((d) => d.x);
+
+export default function Bench() {
+  return (
+    <ScatterChart
+      xAxis={[{ data: xData, valueFormatter: (v: number) => v.toLocaleString('en-US') }]}
+      series={[{ data }]}
+      width={500}
+      height={300}
+    />
+  );
+}
---
+++ test/benchmark-tool/vite.config.mts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const profiling = isProduction && {
+  'react-dom/client': 'react-dom/profiling',
+};
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    minifyIdentifiers: false,
+  resolve: {
+    alias: {
+      ...profiling,
+    },
+  },
+});
