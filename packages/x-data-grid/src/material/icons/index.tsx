02cb204e278f9f13669bf23d19cc7f4ecd551843
Clayton <claytonlin1110@gmail.com>
2026-02-05 10:35:38 -0600

    fix(data-grid): icon usage enhancement

--- packages/x-data-grid/src/material/icons/index.tsx
+++ packages/x-data-grid/src/material/icons/index.tsx
@@ -154,3 +154,18 @@ export const GridLongTextCellCollapseIcon = createSvgIcon(
   <path d="M5 15h2V8.41L18.59 20 20 18.59 8.41 7H15V5H5z" />,
   'NorthWest',
 );
+
+export const GridFirstPageIcon = createSvgIcon(
+  <path d="M18.41 16.59L13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z" />,
+  'FirstPage',
+);
+
+export const GridLastPageIcon = createSvgIcon(
+  <path d="M5.59 7.41L10.18 12l-4.59 4.59L7 18l6-6-6-6zM16 6h2v12h-2z" />,
+  'LastPage',
+);
+
+export const GridKeyboardArrowLeft = createSvgIcon(
+  <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6z" />,
+  'KeyboardArrowLeft',
+);
