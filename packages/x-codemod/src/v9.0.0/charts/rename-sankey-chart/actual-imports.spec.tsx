acc1c3175c2901ac29ef3b3fbd5b4270fff98420
Bernardo Belchior <bernardo@mui.com>
2026-02-10 16:10:28 +0100

    Apply suggestions from code review
    Signed-off-by: Bernardo Belchior <Bernardo.belchior1@gmail.com>

--- packages/x-codemod/src/v9.0.0/charts/rename-sankey-chart/actual-imports.spec.tsx
+++ packages/x-codemod/src/v9.0.0/charts/rename-sankey-chart/actual-imports.spec.tsx
@@ -1,12 +1,12 @@
 // @ts-nocheck
-import { Unstable_SankeyChart } from '@mui/x-charts-pro/SankeyChart';
+import { Unstable_SankeyChart as SankeyChart } from '@mui/x-charts-pro/SankeyChart';
 import { Unstable_SankeyChart as SankeyChartPro } from '@mui/x-charts-premium/SankeyChart';
 // Should rename Unstable_SankeyChart to SankeyChart
 // prettier-ignore
 import { Unstable_SankeyChart as MySankeyChart, SankeyChartProps } from '@mui/x-charts-pro/SankeyChart';
 
 function MyComponent() {
-  return <Unstable_SankeyChart />;
+  return <SankeyChart />;
 }
 
 function MyComponentWithAlias() {
