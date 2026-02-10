caf98642676405ce6b3f4119fcd83ecdd729e937
delangle <flaviendelangle@gmail.com>
2026-02-10 10:37:20 +0100

    Fix CI

--- docs/pages/x/react-scheduler/event-calendar/day-view.js
+++ docs/pages/x/react-scheduler/event-calendar/day-view.js
@@ -1,5 +1,5 @@
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import * as pageProps from 'docsx/data/scheduler/day-view/day-view.md?muiMarkdown';
+import * as pageProps from 'docsx/data/scheduler/event-calendar/day-view/day-view.md?muiMarkdown';
 
 export default function Page() {
   return <MarkdownDocs {...pageProps} />;
