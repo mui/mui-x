f502941be8aeba51758470f71e0186fe33a100f7
alex <alex.fauquette@gmail.com>
2026-02-10 12:58:01 +0100

    fix

--- docs/data/charts/bars/BarAnimation.tsx
+++ docs/data/charts/bars/BarAnimation.tsx
@@ -5,7 +5,7 @@ import Slider from '@mui/material/Slider';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { BarChart } from '@mui/x-charts/BarChart';
-import { HighlightScope } from '@mui/x-charts/context';
+import { HighlightScope } from '@mui/x-charts/models';
 
 export default function BarAnimation() {
   const [seriesNb, setSeriesNb] = React.useState(2);
