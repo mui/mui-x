abece720fc27ef518f7d925b6977df33d2ae0d4c
Janpot <2109932+Janpot@users.noreply.github.com>
2026-02-10 15:37:41 +0100

    fix

--- mui-env.d.ts
+++ mui-env.d.ts
@@ -2,8 +2,7 @@ export {}; // Ensure this file is treated as a module to avoid global scope TS e
 
 declare global {
   interface MUIEnv {
-    NODE_ENV?: 'development' | 'production';
-    IS_TEST_ENV?: 'true';
+    NODE_ENV?: 'production' | 'development';
   }
 
   interface Process {
