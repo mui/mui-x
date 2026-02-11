c64d5604230ca04314d09b75693d125c6997d6b8
Flavien DELANGLE <flaviendelangle@gmail.com>
2026-02-11 14:52:22 +0100

    [scheduler] Rename `EventDraggableDialog` => `EventDialog` (#21295)

--- packages/x-scheduler/src/theme-augmentation/overrides.ts
+++ packages/x-scheduler/src/theme-augmentation/overrides.ts
@@ -1,10 +1,10 @@
 import { EventCalendarClassKey } from '../event-calendar/eventCalendarClasses';
-import { EventDialogClassKey } from '../internals/components/event-draggable-dialog/eventDialogClasses';
+import { EventDialogClassKey } from '../internals/components/event-dialog/eventDialogClasses';
 
 // prettier-ignore
 export interface SchedulerComponentNameToClassKey {
   MuiEventCalendar: EventCalendarClassKey;
-  MuiEventDraggableDialog: EventDialogClassKey;
+  MuiEventDialog: EventDialogClassKey;
 }
 
 declare module '@mui/material/styles' {
