dda897cf8804e7cabe11584f35341da79123f377
Bernardo Belchior <bernardo@mui.com>
2026-02-11 12:27:15 +0100

    Update WIP layering example

--- docs/data/charts/composition/Layering.tsx
+++ docs/data/charts/composition/Layering.tsx
@@ -1,96 +1,72 @@
 import * as React from 'react';
-import { ChartDataProvider } from '@mui/x-charts/ChartDataProvider';
-import { BarPlot } from '@mui/x-charts/BarChart';
+import { ChartDataProviderPremium } from '@mui/x-charts-premium/ChartDataProviderPremium';
 import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
 import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
 import { ChartsGrid } from '@mui/x-charts/ChartsGrid';
-import { ChartsLegend } from '@mui/x-charts/ChartsLegend';
 import { ChartsTooltip } from '@mui/x-charts/ChartsTooltip';
 import { ChartsAxisHighlight } from '@mui/x-charts/ChartsAxisHighlight';
-import { ChartsLayerContainer, ChartsSvgLayer, ChartsWrapper } from '@mui/x-charts';
+import { ChartsWrapper } from '@mui/x-charts/ChartsWrapper';
+import { ChartsLayerContainer } from '@mui/x-charts/ChartsLayerContainer';
+import { ChartsSvgLayer } from '@mui/x-charts/ChartsSvgLayer';
+import { ChartsWebGlLayer } from '@mui/x-charts/ChartsWebGlLayer';
+import { CandlestickPlot } from '@mui/x-charts-premium/CandlestickChart';
+import { OHLCValueType } from '@mui/x-charts-premium/models';
 
-const data = [
-  { name: 'Barking and Dagenham', value: 232747 },
-  { name: 'Barnet', value: 405050 },
-  { name: 'Bexley', value: 256434 },
-  { name: 'Brent', value: 352976 },
-  { name: 'Bromley', value: 335319 },
-  { name: 'Camden', value: 216943 },
-  { name: 'City of London', value: 15111 },
-  { name: 'City of Westminster', value: 209996 },
-  { name: 'Croydon', value: 409342 },
-  { name: 'Ealing', value: 385985 },
-  { name: 'Enfield', value: 327434 },
-  { name: 'Greenwich', value: 299528 },
-  { name: 'Hackney', value: 266758 },
-  { name: 'Hammersmith and Fulham', value: 188687 },
-  { name: 'Haringey', value: 263850 },
-  { name: 'Harrow', value: 270724 },
-  { name: 'Havering', value: 276274 },
-  { name: 'Hillingdon', value: 329185 },
-  { name: 'Hounslow', value: 299424 },
-  { name: 'Islington', value: 223024 },
-  { name: 'Kensington and Chelsea', value: 144518 },
-  { name: 'Kingston upon Thames', value: 172692 },
-  { name: 'Lambeth', value: 316920 },
-  { name: 'Lewisham', value: 301255 },
-  { name: 'Merton', value: 218539 },
-  { name: 'Newham', value: 374523 },
-  { name: 'Redbridge', value: 321231 },
-  { name: 'Richmond upon Thames', value: 196678 },
-  { name: 'Southwark', value: 314786 },
-  { name: 'Sutton', value: 214525 },
-  { name: 'Tower Hamlets', value: 331886 },
-  { name: 'Waltham Forest', value: 279737 },
-  { name: 'Wandsworth', value: 337655 },
+const ohlcData: OHLCValueType[] = [
+  [100, 110, 95, 105],
+  [105, 115, 100, 112],
+  [112, 120, 108, 118],
+  [118, 125, 115, 120],
+  [120, 128, 118, 125],
+  [125, 130, 120, 122],
+  [122, 128, 118, 126],
+  [126, 135, 124, 132],
+  [132, 140, 130, 138],
+  [138, 145, 135, 135],
+  [135, 148, 138, 145],
+  [145, 152, 142, 148],
+  [148, 155, 145, 150],
+  [150, 158, 148, 152],
+  [152, 162, 151, 151],
+  [151, 165, 151, 157],
+  [157, 168, 153, 153],
+  [153, 160, 151, 152],
+  [152, 155, 150, 154],
+  [154, 158, 153, 157],
 ];
 
-function formatPopulation(value: number) {
-  return value === 0 ? '0' : `${(value / 1000).toFixed(0)}k`;
-}
+const xAxisData = ohlcData.map((_, index) => `Day ${index + 1}`);
 
 export default function Layering() {
   return (
-    <ChartDataProvider
+    <ChartDataProviderPremium
       height={600}
       series={[
         {
-          type: 'bar',
-          data: data.map((d) => d.value),
-          label: 'Population (thousands)',
+          type: 'ohlc',
+          data: ohlcData,
+          label: 'Stock Price',
         },
       ]}
-      xAxis={[
-        {
-          scaleType: 'band',
-          height: 100,
-          data: data.map((d) => d.name),
-          tickLabelStyle: {
-            angle: -45,
-          },
-        },
-      ]}
-      yAxis={[
-        {
-          label: 'Population',
-          width: 54,
-          valueFormatter: formatPopulation,
-        },
-      ]}
+      xAxis={[{ scaleType: 'band', data: xAxisData }]}
+      yAxis={[{ label: 'Price' }]}
     >
       <ChartsWrapper>
-        <ChartsLegend />
         <ChartsLayerContainer>
           <ChartsSvgLayer>
             <ChartsGrid horizontal />
+          </ChartsSvgLayer>
+          <ChartsWebGlLayer>
+            <CandlestickPlot />
+          </ChartsWebGlLayer>
+          <ChartsSvgLayer>
             <ChartsXAxis />
             <ChartsYAxis />
-            <BarPlot />
-            <ChartsAxisHighlight x="band" />
-            <ChartsTooltip />
+            <ChartsAxisHighlight x="line" y="line" />
           </ChartsSvgLayer>
+          <ChartsTooltip />
         </ChartsLayerContainer>
       </ChartsWrapper>
-    </ChartDataProvider>
+    </ChartDataProviderPremium>
   );
 }
