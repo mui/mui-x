b7588ab3b13455d509b69c36646a00a0f1329cdb
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2026-02-11 12:13:06 +0100

    refactor

--- docs/data/scheduler/timezone-support/TimezoneDatasetInstantBased.js
+++ docs/data/scheduler/timezone-support/TimezoneDatasetInstantBased.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { EventCalendar } from '@mui/x-scheduler/event-calendar';
-
 import {
   defaultVisibleDate,
   initialEvents,
@@ -8,23 +7,6 @@ import {
 } from '../datasets/timezone-instant-based-events';
 
 export default function TimezoneDatasetInstantBased() {
-  const eventModelStructure = {
-    start: {
-      getter: (event) => event.start,
-      setter: (event, newValue) => {
-        event.start = newValue;
-        return event;
-      },
-    },
-    end: {
-      getter: (event) => event.end,
-      setter: (event, newValue) => {
-        event.end = newValue;
-        return event;
-      },
-    },
-  };
-
   const [events, setEvents] = React.useState(initialEvents);
 
   return (
@@ -38,7 +20,6 @@ export default function TimezoneDatasetInstantBased() {
         displayTimezone="Europe/Paris"
         areEventsDraggable
         areEventsResizable
-        eventModelStructure={eventModelStructure}
       />
     </div>
   );
--- docs/data/scheduler/timezone-support/TimezoneDatasetInstantBased.tsx
+++ docs/data/scheduler/timezone-support/TimezoneDatasetInstantBased.tsx
@@ -1,31 +1,12 @@
 import * as React from 'react';
 import { EventCalendar } from '@mui/x-scheduler/event-calendar';
-import { SchedulerEventModelStructure } from '@mui/x-scheduler-headless/models';
 import {
   defaultVisibleDate,
   initialEvents,
   resources,
-  TimezoneEvent,
 } from '../datasets/timezone-instant-based-events';
 
 export default function TimezoneDatasetInstantBased() {
-  const eventModelStructure: SchedulerEventModelStructure<TimezoneEvent> = {
-    start: {
-      getter: (event) => event.start,
-      setter: (event, newValue) => {
-        event.start = newValue;
-        return event;
-      },
-    },
-    end: {
-      getter: (event) => event.end,
-      setter: (event, newValue) => {
-        event.end = newValue;
-        return event;
-      },
-    },
-  };
-
   const [events, setEvents] = React.useState(initialEvents);
 
   return (
@@ -39,7 +20,6 @@ export default function TimezoneDatasetInstantBased() {
         displayTimezone="Europe/Paris"
         areEventsDraggable
         areEventsResizable
-        eventModelStructure={eventModelStructure}
       />
     </div>
   );
