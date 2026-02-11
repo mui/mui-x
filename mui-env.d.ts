9f78fdc2d98e39136cf5d0447a4590ab5d4e5a97
Janpot <2109932+Janpot@users.noreply.github.com>
2026-02-11 11:06:19 +0100

    Update mui-env.d.ts

--- mui-env.d.ts
+++ mui-env.d.ts
@@ -2,6 +2,12 @@ export {}; // Ensure this file is treated as a module to avoid global scope TS e
 
 declare global {
   interface MUIEnv {
+    /*
+     * The only values available are `production` and `development`.
+     * Other values are invalid, if you need to detect a test environment use `globalThis.MUI_TEST_ENV` instead.
+     * In public packaged it's recommended to combine with `process.env.NODE_ENV !== 'production'` to avoid bundle overhead.
+     *   if (process.env.NODE_ENV !== 'production' && globalThis.MUI_TEST_ENV) {
+     */
     NODE_ENV?: 'production' | 'development';
   }
 
