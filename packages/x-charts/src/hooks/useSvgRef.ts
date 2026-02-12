97f5eb634a64a5c297039f7ab67a222a3e6895c7
Bernardo Belchior <bernardo@mui.com>
2026-02-12 15:47:55 +0100

    Use ChartsLayerContainer + ChartsSvgLayer inside ChartsSurface

--- packages/x-charts/src/hooks/useSvgRef.ts
+++ packages/x-charts/src/hooks/useSvgRef.ts
@@ -6,10 +6,8 @@ import { useChartContext } from '../context/ChartProvider';
  * Get the ref for the SVG element.
  * @returns The SVG ref.
  */
-export function useSvgRef<
-  E extends HTMLElement | SVGElement = SVGSVGElement,
->(): React.RefObject<E | null> {
+export function useSvgRef(): React.RefObject<HTMLDivElement | null> {
   const { instance } = useChartContext();
 
-  return instance.svgRef as React.RefObject<E | null>;
+  return instance.svgRef;
 }
