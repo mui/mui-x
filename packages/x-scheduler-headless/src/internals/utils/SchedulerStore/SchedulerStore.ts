4a12074fe0890f5b8482ba751331e558840e2c0e
delangle <flaviendelangle@gmail.com>
2026-02-13 13:01:57 +0100

    [docs] Add various fixes for the premium version of the scheduler

--- packages/x-scheduler-headless/src/internals/utils/SchedulerStore/SchedulerStore.ts
+++ packages/x-scheduler-headless/src/internals/utils/SchedulerStore/SchedulerStore.ts
@@ -558,7 +558,7 @@ export class SchedulerStore<
   };
 
   /**
-   * Builds an object containing the methods that should be exposed publicly by the Scheduler components.
+   * Builds an object containing the methods that should be exposed publicly by the scheduler components.
    */
   public buildPublicAPI() {
     return {
