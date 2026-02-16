a661661341770321e969c5ad86c56bfd140575e2
Andrew <andrew@mui.com>
2026-02-16 21:30:17 +0100

    fix broken demo

--- packages/x-data-grid-headless/src/plugins/filtering/index.ts
+++ packages/x-data-grid-headless/src/plugins/filtering/index.ts
@@ -10,7 +10,7 @@ export type {
   FilteringOptions,
   FilteringApi,
 } from './types';
-export { getDefaultFilterOperators } from './filteringUtils';
+export { getDefaultFilterOperators, isFilterGroup, isFilterCondition } from './filteringUtils';
 export { filteringSelectors } from './selectors';
 export { buildFilterApplier, cleanFilterModel, removeDiacritics } from './filteringUtils';
 export {
