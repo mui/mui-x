ad4c0ba19678364422445be8c4a2b36916943f32
alex <alex.fauquette@gmail.com>
2026-02-18 14:43:03 +0100

    pnpm prettier

--- packages/x-charts/src/ChartsLayerContainer/ChartsLayerContainer.test.tsx
+++ packages/x-charts/src/ChartsLayerContainer/ChartsLayerContainer.test.tsx
@@ -8,20 +8,17 @@ import { ChartProvider } from '../context/ChartProvider';
 describe('<ChartsLayerContainer />', () => {
   const { render } = createRenderer();
 
-  it.skipIf(!isJSDOM)(
-    'should warn when ChartsSurface is used inside ChartsLayerContainer',
-    () => {
-      expect(() =>
-        render(
-          <ChartProvider pluginParams={{ width: 100, height: 100, series: [] }}>
-            <ChartsLayerContainer>
-              <ChartsSurface />
-            </ChartsLayerContainer>
-          </ChartProvider>,
-        ),
-      ).toErrorDev(
-        'MUI X Charts: ChartsSurface should not be used inside ChartsLayerContainer. Render a ChartsSvgLayer instead.',
-      );
-    },
-  );
+  it.skipIf(!isJSDOM)('should warn when ChartsSurface is used inside ChartsLayerContainer', () => {
+    expect(() =>
+      render(
+        <ChartProvider pluginParams={{ width: 100, height: 100, series: [] }}>
+          <ChartsLayerContainer>
+            <ChartsSurface />
+          </ChartsLayerContainer>
+        </ChartProvider>,
+      ),
+    ).toErrorDev(
+      'MUI X Charts: ChartsSurface should not be used inside ChartsLayerContainer. Render a ChartsSvgLayer instead.',
+    );
+  });
 });
