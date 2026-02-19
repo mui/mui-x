f8b5832d4c71eafb8706750c14e94553a1505d42
delangle <flaviendelangle@gmail.com>
2026-02-19 12:00:17 +0100

    Fix

--- docs/src/modules/components/ExperimentLayout.js
+++ docs/src/modules/components/ExperimentLayout.js
@@ -5,9 +5,7 @@ export function ExperimentLayout({ children }) {
   return (
     <React.Fragment>
       <style>{'body { margin: 0; }'}</style>
-      <div style={{ display: 'flex', height: '100dvh', boxSizing: 'border-box' }}>
-        {children}
-      </div>
+      <div style={{ display: 'flex', height: '100dvh', boxSizing: 'border-box' }}>{children}</div>
     </React.Fragment>
   );
 }
