0abe6dbd8df64a1073867a0c7d95b3538c4079a0
sai6855 <fayazceg@gmail.com>
2026-02-19 19:26:04 +0530

    fix funnel

--- packages/x-charts-premium/src/BarChartPremium/RangeBar/seriesConfig/keyboardFocusHandler.ts
+++ packages/x-charts-premium/src/BarChartPremium/RangeBar/seriesConfig/keyboardFocusHandler.ts
@@ -2,33 +2,9 @@ import {
   type KeyboardFocusHandler,
   type ComposableCartesianChartSeriesType,
   composableCartesianSeriesTypes,
-  //  ChartSeriesType,
-  // createGetNextIndexFocusedItem,
-  // createGetPreviousIndexFocusedItem,
-  // createGetPreviousSeriesFocusedItem,
-  // createGetNextSeriesFocusedItem,
   createCommonKeyboardFocusHandler,
 } from '@mui/x-charts/internals';
 
-// export function createCommonKeyboardFocusHandler<
-//   TSeriesType extends Exclude<ChartSeriesType, 'sankey'>,
-// >(outSeriesTypes: Set<TSeriesType>, allowCycles?: boolean) {
-//   const keyboardFocusHandler = (event: KeyboardEvent) => {
-//     switch (event.key) {
-//       case 'ArrowRight':
-//         return createGetNextIndexFocusedItem(outSeriesTypes, allowCycles);
-//       case 'ArrowLeft':
-//         return createGetPreviousIndexFocusedItem(outSeriesTypes, allowCycles);
-//       case 'ArrowDown':
-//         return createGetPreviousSeriesFocusedItem(outSeriesTypes);
-//       case 'ArrowUp':
-//         return createGetNextSeriesFocusedItem(outSeriesTypes);
-//       default:
-//         return null;
-//     }
-//   };
-//   return keyboardFocusHandler;
-// }
 
 const keyboardFocusHandler: KeyboardFocusHandler<'rangeBar', ComposableCartesianChartSeriesType> =
   createCommonKeyboardFocusHandler(
