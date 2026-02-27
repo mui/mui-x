4096a35291ea1297fe7fc556ca80ac974a43d8c9
alex <alex.fauquette@gmail.com>
2026-02-27 17:30:50 +0100

    pnpm prettier

--- packages/x-internals/src/types/DistributiveOmit.ts
+++ packages/x-internals/src/types/DistributiveOmit.ts
@@ -1,8 +1,8 @@
 /**
  * Distribute the `Omit` to an union.
- * `DistributiveOmit<A | B, 'key'>` returns `Omit<A, 'key'> | Omit<B, 'key'>` 
+ * `DistributiveOmit<A | B, 'key'>` returns `Omit<A, 'key'> | Omit<B, 'key'>`
  * @see {@link https://tkdodo.eu/blog/omit-for-discriminated-unions-in-type-script this blog post} for more info.
- * 
+ *
  * @template T - The original union type to distribute the `Omit` over.
  * @template K - The keys to omit.
  */
