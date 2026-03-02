20c90b5d5474f4a6bcfaebdc6118463b614bd8af
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2026-03-02 09:38:34 +0100

    [code-infra] Avoid static props for pageContent (#21038)

--- docs/pages/x/api/charts/scatter-series.js
+++ docs/pages/x/api/charts/scatter-series.js
@@ -5,9 +5,9 @@ import mapApiPageTranslations from 'docs/src/modules/utils/mapApiPageTranslation
 import jsonPageContent from './scatter-series.json';
 
 export default function Page(props) {
-  const { descriptions, pageContent } = props;
+  const { descriptions } = props;
   return (
-    <InterfaceApiPage {...layoutConfig} descriptions={descriptions} pageContent={pageContent} />
+    <InterfaceApiPage {...layoutConfig} descriptions={descriptions} pageContent={jsonPageContent} />
   );
 }
 
@@ -18,11 +18,5 @@ export async function getStaticProps() {
     /\.\/scatter-series.*.json$/,
   );
   const descriptions = mapApiPageTranslations(req);
-
-  return {
-    props: {
-      descriptions,
-      pageContent: jsonPageContent,
-    },
-  };
+  return { props: { descriptions } };
 }
