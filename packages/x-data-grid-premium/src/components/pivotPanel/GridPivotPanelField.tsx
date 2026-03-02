c0412b86f6a6b549986bf11504ce5d97441610e9
sai6855 <fayazceg@gmail.com>
2026-03-02 15:15:30 +0530

    remove import

--- packages/x-data-grid-premium/src/components/pivotPanel/GridPivotPanelField.tsx
+++ packages/x-data-grid-premium/src/components/pivotPanel/GridPivotPanelField.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import {
   getDataGridUtilityClass,
-  gridClasses,
   type GridColDef,
   GridMenu,
   type GridSlotProps,
