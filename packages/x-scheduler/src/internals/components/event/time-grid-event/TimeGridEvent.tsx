9bc013e87eb65fbb1307cbc60df1898333197ed8
Rita <47579513+rita-codes@users.noreply.github.com>
2026-03-05 11:14:19 +0100

    Apply suggestion from @rita-codes
    Signed-off-by: Rita <47579513+rita-codes@users.noreply.github.com>

--- packages/x-scheduler/src/internals/components/event/time-grid-event/TimeGridEvent.tsx
+++ packages/x-scheduler/src/internals/components/event/time-grid-event/TimeGridEvent.tsx
@@ -47,7 +47,6 @@ const TimeGridEventRoot = styled(CalendarGrid.TimeEvent, {
   '&[data-dragging], &[data-resizing]': {
     opacity: 0.5,
   },
-  },
   '&[data-under-hour="true"]': {
     flexDirection: 'row',
   },
