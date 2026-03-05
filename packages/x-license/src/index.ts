cd3662250ced659bb46b9b70b743c2cc1b58c4f5
aemartos <aemartos@gmail.com>
2026-03-05 12:15:27 +0100

    [x-license] consolidate internal exports into /internals entry point

--- packages/x-license/src/index.ts
+++ packages/x-license/src/index.ts
@@ -2,16 +2,5 @@ export {
   Unstable_LicenseInfoProvider,
   type Unstable_LicenseInfoProviderProps,
 } from './Unstable_LicenseInfoProvider';
-export { LicenseInfo, PLAN_SCOPES, PLAN_VERSIONS, LICENSE_MODELS, APP_TYPES } from './utils';
-export type {
-  LicenseStatus,
-  MuiLicenseInfo,
-  AppType,
-  KeyVersion,
-  LicenseDetails,
-  LicenseModel,
-  NullableLicenseDetails,
-  PlanScope,
-  PlanVersion,
-} from './utils';
+export { LicenseInfo } from './utils';
 export { muiXTelemetrySettings } from '@mui/x-telemetry';
