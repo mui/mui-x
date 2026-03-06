0ba3e735d3a4e9898f234336e58e4849c14fec5f
michel <jsnerdic@gmail.com>
2026-03-06 16:06:45 +0100

    pickers: add codemod to rename unstableFieldRef props to stable alternatives
    Signed-off-by: michel <jsnerdic@gmail.com>

---
+++ packages/x-codemod/src/v9.0.0/pickers/rename-field-ref/expected.spec.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { MultiInputDateRangeField } from '@mui/x-date-pickers-pro/MultiInputDateRangeField';
+
+function App() {
+  const fieldRef = React.useRef(null);
+  const startRef = React.useRef(null);
+  const endRef = React.useRef(null);
+
+  return (
+    <div>
+      <DatePicker fieldRef={fieldRef} />
+      <DateRangePicker fieldRef={fieldRef} />
+      <MultiInputDateRangeField
+        startFieldRef={startRef}
+        endFieldRef={endRef}
+      />
+    </div>
+  );
+}
