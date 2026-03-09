aef54642c1a881b08c47389ba27137b550b8cf12
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2026-03-09 13:14:02 +0100

    use theme in the docs examples since it's the recommended approach

--- docs/data/scheduler/event-calendar/localization/DateLocaleCalendar.tsx
+++ docs/data/scheduler/event-calendar/localization/DateLocaleCalendar.tsx
@@ -1,28 +1,31 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { fr } from 'date-fns/locale/fr';
 import { SchedulerEvent } from '@mui/x-scheduler/models';
 import { EventCalendar } from '@mui/x-scheduler/event-calendar';
-import { frFR } from '@mui/x-scheduler/locales';
+import { frFR, createDateLocaleTheme } from '@mui/x-scheduler/locales';
 import {
   initialEvents,
   defaultVisibleDate,
   resources,
 } from '../../datasets/personal-agenda';
 
+const theme = createTheme(frFR, createDateLocaleTheme(fr));
+
 export default function DateLocaleCalendar() {
   const [events, setEvents] = React.useState<SchedulerEvent[]>(initialEvents);
 
   return (
-    <div style={{ height: '600px', width: '100%' }}>
-      <EventCalendar
-        events={events}
-        resources={resources}
-        defaultVisibleDate={defaultVisibleDate}
-        onEventsChange={setEvents}
-        localeText={frFR.components.MuiEventCalendar.defaultProps.localeText}
-        dateLocale={fr}
-        defaultPreferences={{ isSidePanelOpen: false }}
-      />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div style={{ height: '600px', width: '100%' }}>
+        <EventCalendar
+          events={events}
+          resources={resources}
+          defaultVisibleDate={defaultVisibleDate}
+          onEventsChange={setEvents}
+          defaultPreferences={{ isSidePanelOpen: false }}
+        />
+      </div>
+    </ThemeProvider>
   );
 }
--- docs/data/scheduler/event-timeline/localization/DateLocaleTimeline.tsx
+++ docs/data/scheduler/event-timeline/localization/DateLocaleTimeline.tsx
@@ -1,28 +1,31 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { fr } from 'date-fns/locale/fr';
 import { SchedulerEvent } from '@mui/x-scheduler/models';
 import { EventTimelinePremium } from '@mui/x-scheduler-premium/event-timeline-premium';
-import { frFR } from '@mui/x-scheduler/locales';
+import { frFR, createDateLocaleTheme } from '@mui/x-scheduler/locales';
 import {
   initialEvents,
   defaultVisibleDate,
   resources,
 } from '../../datasets/company-roadmap';
 
+const theme = createTheme(frFR, createDateLocaleTheme(fr));
+
 export default function DateLocaleTimeline() {
   const [events, setEvents] = React.useState<SchedulerEvent[]>(initialEvents);
 
   return (
-    <div style={{ height: '500px', width: '100%', overflow: 'auto' }}>
-      <EventTimelinePremium
-        events={events}
-        resources={resources}
-        defaultVisibleDate={defaultVisibleDate}
-        onEventsChange={setEvents}
-        localeText={frFR.components.MuiEventTimeline.defaultProps.localeText}
-        dateLocale={fr}
-        defaultView="months"
-      />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div style={{ height: '500px', width: '100%', overflow: 'auto' }}>
+        <EventTimelinePremium
+          events={events}
+          resources={resources}
+          defaultVisibleDate={defaultVisibleDate}
+          onEventsChange={setEvents}
+          defaultView="months"
+        />
+      </div>
+    </ThemeProvider>
   );
 }
