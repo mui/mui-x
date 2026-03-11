8823b0936aaa181eccfe7e145a23c047f6717571
alex <alex.fauquette@gmail.com>
2026-03-11 16:57:29 +0100

    [charts] Use different shapes by default

--- packages/x-charts/src/LineChart/seriesConfig/getSeriesWithDefaultValues.ts
+++ packages/x-charts/src/LineChart/seriesConfig/getSeriesWithDefaultValues.ts
@@ -1,4 +1,15 @@
 import type { GetSeriesWithDefaultValues } from '../../internals/plugins/corePlugins/useChartSeriesConfig';
+import type { MarkShape } from '../../models/seriesType/line';
+
+const defaultShapes: MarkShape[] = [
+  'circle',
+  'square',
+  'diamond',
+  'cross',
+  'star',
+  'triangle',
+  'wye',
+];
 
 const getSeriesWithDefaultValues: GetSeriesWithDefaultValues<'line'> = (
   seriesData,
@@ -9,6 +20,7 @@ const getSeriesWithDefaultValues: GetSeriesWithDefaultValues<'line'> = (
     ...seriesData,
     id: seriesData.id ?? `auto-generated-id-${seriesIndex}`,
     color: seriesData.color ?? colors[seriesIndex % colors.length],
+    shape: seriesData.shape ?? defaultShapes[seriesIndex % defaultShapes.length],
   };
 };
 
