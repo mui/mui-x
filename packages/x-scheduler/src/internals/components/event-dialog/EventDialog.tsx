f2d18313185c05ef44d45403a05c9df6ce32d7bf
siriwatknp <siriwatkunaporn@gmail.com>
2026-03-11 16:39:27 +0700

    fix passing component

--- packages/x-scheduler/src/internals/components/event-dialog/EventDialog.tsx
+++ packages/x-scheduler/src/internals/components/event-dialog/EventDialog.tsx
@@ -121,7 +121,7 @@ export const EventDialogContent = React.forwardRef(function EventDialogContent(
 ) {
   // eslint-disable-next-line mui/material-ui-name-matches-component-name
   const props = useThemeProps({ props: inProps, name: 'MuiEventDialog' });
-  const { style, anchorRef, occurrence, onClose, open, slots, slotProps, ...other } = props;
+  const { style, anchorRef, occurrence, onClose, open, slotProps, ...other } = props;
   // Context hooks
   const store = useSchedulerStoreContext();
   const { classes } = useEventDialogStyledContext();
@@ -140,9 +140,13 @@ export const EventDialogContent = React.forwardRef(function EventDialogContent(
       aria-labelledby="event-dialog-title"
       aria-modal="false"
       className={classes.eventDialog}
-      slots={{ paper: PaperComponent, ...slots }}
       slotProps={{
-        paper: { className: classes.eventDialogPaper, anchorRef, dragHandlerRef } as PaperProps,
+        paper: {
+          component: PaperComponent,
+          className: classes.eventDialogPaper,
+          anchorRef,
+          dragHandlerRef,
+        } as PaperProps,
         ...slotProps,
       }}
       {...other}
