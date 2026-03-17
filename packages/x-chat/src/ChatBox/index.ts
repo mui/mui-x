e9fda689b6748a9856454d784f00e4ad9ceb24c0
Vadym Raksha <hello@raksha.bio>
2026-03-17 12:37:49 +0100

    [chat] Add ChatBox one-liner API

---
+++ packages/x-chat/src/ChatBox/index.ts
@@ -0,0 +1,14 @@
+export { ChatBox } from './ChatBox';
+export { chatBoxClasses, getChatBoxUtilityClass } from './chatBoxClasses';
+
+export type {
+  ChatBoxClassKey,
+  ChatBoxClasses,
+} from './chatBoxClasses';
+export type {
+  ChatBoxOwnerState,
+  ChatBoxProps,
+  ChatBoxSlotProps,
+  ChatBoxSlots,
+  ChatBoxStateOwnerState,
+} from './ChatBox';
