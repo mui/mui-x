3ae8eec96671581eaf3d7f0481c8d44c88b81911
alex <alex.fauquette@gmail.com>
2026-03-19 11:12:02 +0100

    [charts] Remove innerRadius and outerRadius from PieArclLabel

--- packages/x-charts/src/PieChart/PieArcLabel.tsx
+++ packages/x-charts/src/PieChart/PieArcLabel.tsx
@@ -114,8 +114,6 @@ export type PieArcLabelProps = PieArcLabelOwnerState &
   Omit<React.SVGProps<SVGTextElement>, 'ref' | 'color'> & {
     startAngle: number;
     endAngle: number;
-    innerRadius: number;
-    outerRadius: number;
     arcLabelRadius: number;
     cornerRadius: number;
     paddingAngle: number;
@@ -134,8 +132,6 @@ const PieArcLabel = React.forwardRef<SVGTextElement, PieArcLabelProps>(
       endAngle,
       paddingAngle,
       arcLabelRadius,
-      innerRadius,
-      outerRadius,
       cornerRadius,
       formattedArcLabel,
       isHighlighted,
@@ -161,8 +157,7 @@ const PieArcLabel = React.forwardRef<SVGTextElement, PieArcLabelProps>(
       cornerRadius,
       startAngle,
       endAngle,
-      innerRadius: arcLabelRadius,
-      outerRadius: arcLabelRadius,
+      arcLabelRadius,
       paddingAngle,
       skipAnimation,
       ref,
