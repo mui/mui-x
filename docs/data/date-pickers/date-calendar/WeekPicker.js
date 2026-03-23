abc2d03babc162d13625040beffab42af1e31232
michel <jsnerdic@gmail.com>
2026-03-23 11:20:32 +0100

    fixed docs preview
    Signed-off-by: michel <jsnerdic@gmail.com>

--- docs/data/date-pickers/date-calendar/WeekPicker.js
+++ docs/data/date-pickers/date-calendar/WeekPicker.js
@@ -13,7 +13,6 @@ const CustomPickersDay = styled(PickersDay, {
   shouldForwardProp: (prop) => prop !== 'isSelected' && prop !== 'isHovered',
 })(({ theme, isSelected, isHovered, day }) => ({
   borderRadius: 0,
-  display: 'flex',
   ...(isSelected && {
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
