6b5e9bcb44f26e1df6d35706ddca2df232241d79
alex <alex.fauquette@gmail.com>
2026-03-23 16:22:10 +0100

    [charts] Add l10n to the bar accessibility

--- packages/x-charts/src/BarChart/seriesConfig/bar/descriptionGetter.ts
+++ packages/x-charts/src/BarChart/seriesConfig/bar/descriptionGetter.ts
@@ -2,7 +2,7 @@ import { getLabel } from '../../../internals/getLabel';
 import type { DescriptionGetter } from '../../../internals/plugins/corePlugins/useChartSeriesConfig';
 
 const descriptionGetter: DescriptionGetter<'bar'> = (params) => {
-  const { identifier, series, xAxis, yAxis } = params;
+  const { identifier, series, xAxis, yAxis, localeText } = params;
 
   const label = getLabel(series.label, 'tooltip');
   const value = series.data[identifier.dataIndex];
@@ -11,28 +11,19 @@ const descriptionGetter: DescriptionGetter<'bar'> = (params) => {
   const categoryAxis = isHorizontal ? yAxis : xAxis;
   const categoryValue = categoryAxis.data?.[identifier.dataIndex];
 
-  const formattedValue =
-    value != null ? series.valueFormatter(value, { dataIndex: identifier.dataIndex }) : null;
+  const formattedValue = series.valueFormatter(value, { dataIndex: identifier.dataIndex });
+  const formattedCategory = categoryAxis.valueFormatter?.(categoryValue, {
+    location: 'tooltip',
+    scale: categoryAxis.scale,
+  });
 
-  const parts: string[] = [];
-
-  if (label) {
-    parts.push(label);
-  }
-
-  if (categoryValue != null) {
-    const formattedCategory = categoryAxis.valueFormatter?.(categoryValue, {
-      location: 'tooltip',
-      scale: categoryAxis.scale,
-    });
-    parts.push(formattedCategory ?? String(categoryValue));
-  }
-
-  if (formattedValue != null) {
-    parts.push(formattedValue);
-  }
-
-  return parts.join(', ');
+  return localeText.barDescription({
+    value,
+    formattedValue: formattedValue ?? '',
+    categoryValue,
+    formattedCategoryValue: formattedCategory ?? '',
+    seriesLabel: label,
+  });
 };
 
 export default descriptionGetter;
