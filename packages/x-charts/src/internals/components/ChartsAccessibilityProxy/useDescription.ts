8e50e1612f7a7cc3adaaa6b166a9e491bea663ea
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2026-03-23 16:06:40 +0100

    [charts] Add voiceover component (#21344)

---
+++ packages/x-charts/src/internals/components/ChartsAccessibilityProxy/useDescription.ts
@@ -0,0 +1,56 @@
+import { useRadiusAxes, useRotationAxes, useSeries, useXAxes, useYAxes } from '../../../hooks';
+import { useStore } from '../../store/useStore';
+import { selectorChartSeriesConfig } from '../../plugins/corePlugins/useChartSeriesConfig';
+import { isCartesianSeries } from '../../isCartesian';
+import { isPolarSeriesType } from '../../isPolar';
+import { selectorChartsFocusedOrToFocusedItem } from '../../plugins/featurePlugins/useChartKeyboardNavigation';
+
+/**
+ * Get the message associated to the focused item.
+ * @returns {string | null} the accessibility description linked to the focused item
+ */
+export function useDescription(): string | null {
+  const store = useStore();
+  const focusedItem = store.use(selectorChartsFocusedOrToFocusedItem);
+  const seriesConfig = store.use(selectorChartSeriesConfig);
+
+  const seriesState = useSeries();
+
+  const { xAxis, xAxisIds } = useXAxes();
+  const { yAxis, yAxisIds } = useYAxes();
+  const { rotationAxis, rotationAxisIds } = useRotationAxes();
+  const { radiusAxis, radiusAxisIds } = useRadiusAxes();
+
+  if (focusedItem === null) {
+    return null;
+  }
+  const { type, seriesId } = focusedItem;
+  const focusedSeries = seriesState[type]?.series[seriesId];
+
+  if (!focusedSeries) {
+    return null;
+  }
+
+  const descriptionGetter = seriesConfig[type]?.descriptionGetter;
+
+  if (!descriptionGetter) {
+    return null;
+  }
+
+  const descriptionParams: Record<string, unknown> = {
+    identifier: focusedItem,
+    series: focusedSeries,
+  };
+
+  if (isCartesianSeries(focusedSeries)) {
+    const xAxisId = focusedSeries.xAxisId ?? xAxisIds[0];
+    const yAxisId = focusedSeries.yAxisId ?? yAxisIds[0];
+    descriptionParams.xAxis = xAxis[xAxisId];
+    descriptionParams.yAxis = yAxis[yAxisId];
+  } else if (isPolarSeriesType(type)) {
+    descriptionParams.rotationAxis = rotationAxis[rotationAxisIds[0]];
+    descriptionParams.radiusAxis = radiusAxis[radiusAxisIds[0]];
+  }
+
+  return (descriptionGetter as (params: Record<string, unknown>) => string)(descriptionParams);
+}
