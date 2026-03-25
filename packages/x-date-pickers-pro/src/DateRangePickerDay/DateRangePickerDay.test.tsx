20a920bf420461cec90153eca78ba4acfe25dae8
michel <jsnerdic@gmail.com>
2026-03-25 12:19:57 +0100

    added style overrides support and tests in `DateRangePickerDay` component
    Signed-off-by: michel <jsnerdic@gmail.com>

--- packages/x-date-pickers-pro/src/DateRangePickerDay/DateRangePickerDay.test.tsx
+++ packages/x-date-pickers-pro/src/DateRangePickerDay/DateRangePickerDay.test.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {
   DateRangePickerDay,
   dateRangePickerDayClasses as classes,
@@ -33,4 +35,115 @@ describe('<DateRangePickerDay />', () => {
       skip: ['componentProp', 'componentsProp', 'themeVariants'],
     }),
   );
+
+  describe('styleOverrides', () => {
+    it('should apply custom styleOverrides', () => {
+      const theme = createTheme({
+        components: {
+          MuiDateRangePickerDay: {
+            styleOverrides: {
+              startOfMonth: {
+                opacity: '0.1',
+              },
+              endOfMonth: {
+                opacity: '0.2',
+              },
+              dayWithoutMargin: {
+                opacity: '0.3',
+              },
+              previewed: {
+                opacity: '0.4',
+              },
+            },
+          },
+        },
+      });
+
+      const day = adapterToUse.date('2018-01-01');
+      const { container: container1 } = render(
+        <ThemeProvider theme={theme}>
+          <DateRangePickerDay
+            day={day}
+            onDaySelect={() => {}}
+            outsideCurrentMonth={false}
+            isHighlighting={false}
+            isPreviewing={false}
+            isStartOfPreviewing={false}
+            isEndOfPreviewing={false}
+            isStartOfHighlighting={false}
+            isEndOfHighlighting={false}
+            isFirstVisibleCell={false}
+            isLastVisibleCell={false}
+          />
+        </ThemeProvider>,
+      );
+
+      // 2018-01-01 is start of month
+      expect(container1.firstChild).to.have.style('opacity', '0.1');
+
+      const { container: container2 } = render(
+        <ThemeProvider theme={theme}>
+          <DateRangePickerDay
+            day={adapterToUse.date('2018-01-31')}
+            onDaySelect={() => {}}
+            outsideCurrentMonth={false}
+            isHighlighting={false}
+            isPreviewing={false}
+            isStartOfPreviewing={false}
+            isEndOfPreviewing={false}
+            isStartOfHighlighting={false}
+            isEndOfHighlighting={false}
+            isFirstVisibleCell={false}
+            isLastVisibleCell={false}
+          />
+        </ThemeProvider>,
+      );
+
+      // 2018-01-31 is end of month
+      expect(container2.firstChild).to.have.style('opacity', '0.2');
+
+      const { container: container3 } = render(
+        <ThemeProvider theme={theme}>
+          <DateRangePickerDay
+            day={adapterToUse.date('2018-01-15')}
+            onDaySelect={() => {}}
+            outsideCurrentMonth={false}
+            isHighlighting={false}
+            isPreviewing={false}
+            isStartOfPreviewing={false}
+            isEndOfPreviewing={false}
+            isStartOfHighlighting={false}
+            isEndOfHighlighting={false}
+            isFirstVisibleCell={false}
+            isLastVisibleCell={false}
+            disableMargin
+          />
+        </ThemeProvider>,
+      );
+
+      // 2018-01-15 is NOT start/end of month, but has disableMargin
+      expect(container3.firstChild).to.have.style('opacity', '0.3');
+
+      const { container: container4 } = render(
+        <ThemeProvider theme={theme}>
+          <DateRangePickerDay
+            day={adapterToUse.date('2018-01-15')}
+            onDaySelect={() => {}}
+            outsideCurrentMonth={false}
+            isHighlighting={false}
+            isPreviewing
+            isStartOfPreviewing={false}
+            isEndOfPreviewing={false}
+            isStartOfHighlighting={false}
+            isEndOfHighlighting={false}
+            isFirstVisibleCell={false}
+            isLastVisibleCell={false}
+          />
+        </ThemeProvider>,
+      );
+
+      // 2018-01-15 is NOT start/end of month, but is previewed
+      expect(container4.firstChild).to.have.style('opacity', '0.4');
+    });
+  });
 });
