45acd035dca730ae9c0b71f0e82451397c071d20
Andrew <andrew@mui.com>
2026-03-25 11:23:00 +0100

    remove headless data grid packages

--- packages/x-data-grid-headless/src/plugins/filtering/selectors.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector } from '@base-ui/utils/store';
-import type { FilteringState } from './types';
-
-export const selectFilterModel = createSelector((state: FilteringState) => state.filtering.model);
-
-export const filteringSelectors = {
-  model: selectFilterModel,
-};
