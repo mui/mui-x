35cc8041d694ad89dba22c696384ce558f8b8406
michel <jsnerdic@gmail.com>
2026-03-27 16:18:41 +0100

    [pickers] Standardize and clarify `DateRangePickerDayClasses` property names

--- packages/x-date-pickers-pro/src/DateRangePickerDay/dateRangePickerDayClasses.ts
+++ packages/x-date-pickers-pro/src/DateRangePickerDay/dateRangePickerDayClasses.ts
@@ -4,27 +4,39 @@ import generateUtilityClasses from '@mui/utils/generateUtilityClasses';
 export interface DateRangePickerDayClasses {
   /** Styles applied to the root element. */
   root: string;
-  /** Styles applied to the root element if `outsideCurrentMonth=true` and `showDaysOutsideCurrentMonth=true`. */
+  /** Styles applied to the root element if `isDayOutsideMonth=true` and `showDaysOutsideCurrentMonth=true`. */
   dayOutsideMonth: string;
-  /** Styles applied to the root element if `outsideCurrentMonth=true` and `showDaysOutsideCurrentMonth=false`. */
+  /** Styles applied to the root element if `isDayOutsideMonth=true` and `showDaysOutsideCurrentMonth=false`. */
   fillerCell: string;
-  /** Styles applied to the root element if `disableHighlightToday=false` and `today=true`. */
+  /** Styles applied to the root element if `disableHighlightToday=false` and `isDayCurrent=true`. */
   today: string;
-  /** State class applied to the root element if `disabled=true`. */
+  /** State class applied to the root element if `isDayDisabled=true`. */
   disabled: string;
-  /** State class applied to the root element if `selected=true`. */
+  /** State class applied to the root element if `isDaySelected=true`. */
   selected: string;
+  /** Styles applied to the root element if `isDayPreviewStart=true`. */
   previewStart: string;
+  /** Styles applied to the root element if `isDayPreviewEnd=true`. */
   previewEnd: string;
+  /** Styles applied to the root element if `isDayInsidePreview=true`. */
   insidePreviewing: string;
+  /** Styles applied to the root element if `isDaySelectionStart=true`. */
   selectionStart: string;
+  /** Styles applied to the root element if `isDaySelectionEnd=true`. */
   selectionEnd: string;
+  /** Styles applied to the root element if `isDayInsideSelection=true`. */
   insideSelection: string;
+  /** Styles applied to the root element if `isDayStartOfWeek=true`. */
   startOfWeek: string;
+  /** Styles applied to the root element if `isDayEndOfWeek=true`. */
   endOfWeek: string;
+  /** Styles applied to the root element if `isDayStartOfMonth=true`. */
   startOfMonth: string;
+  /** Styles applied to the root element if `isDayEndOfMonth=true`. */
   endOfMonth: string;
+  /** Styles applied to the root element if `isDayPreviewed=true`. */
   previewed: string;
+  /** Styles applied to the root element if `isDayDraggable=true`. */
   draggable: string;
 }
 
