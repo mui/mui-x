b4fd36441201a948d40303b3d90eea577f9f4036
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2026-03-30 17:55:29 +0530

    [test] Remove `componentsProp` test from `describeConformance` (#21897)

--- packages/x-date-pickers-pro/src/DateTimeRangePicker/tests/describeConformance.DateTimeRangePicker.test.tsx
+++ packages/x-date-pickers-pro/src/DateTimeRangePicker/tests/describeConformance.DateTimeRangePicker.test.tsx
@@ -12,7 +12,6 @@ describe('<DateTimeRangePicker /> - Describe Conformance', () => {
     refInstanceof: window.HTMLDivElement,
     skip: [
       'componentProp',
-      'componentsProp',
       'themeDefaultProps',
       'themeStyleOverrides',
       'themeVariants',
--- packages/x-date-pickers/src/PickerDay2/PickerDay2.test.tsx
+++ packages/x-date-pickers/src/PickerDay2/PickerDay2.test.tsx
@@ -24,7 +24,7 @@ describe('<PickerDay2 />', () => {
       muiName: 'MuiPickerDay2',
       refInstanceof: window.HTMLButtonElement,
       testVariantProps: { variant: 'disableMargin' },
-      skip: ['componentProp', 'componentsProp'],
+      skip: ['componentProp'],
     }),
   );
 
