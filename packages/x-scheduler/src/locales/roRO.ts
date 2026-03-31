15deac111ba8e002c7c7f04bb15cb665b62549d9
Rita <47579513+rita-codes@users.noreply.github.com>
2026-03-31 10:59:57 +0200

    [l10n][scheduler] Add Spanish (es-ES) locale (#21900)

--- packages/x-scheduler/src/locales/roRO.ts
+++ packages/x-scheduler/src/locales/roRO.ts
@@ -18,7 +18,6 @@ const roRODialog: Partial<EventDialogLocaleText> = {
   // closeButtonLabel: 'Close',
   // deleteEvent: 'Delete event',
   // descriptionLabel: 'Description',
-  // editDisabledNotice: 'Editing is currently unavailable for recurrent events',
   // endDateLabel: 'End date',
   // endTimeLabel: 'End time',
   // eventTitleAriaLabel: 'Event title',
@@ -65,8 +64,6 @@ const roRODialog: Partial<EventDialogLocaleText> = {
   // radioGroupAriaLabel: 'Editing recurring events scope',
   // thisAndFollowing: 'This and following events',
   // title: 'Apply this change to:',
-  // General
-  // loading: 'Loading...',
 };
 
 const roROCalendar: Partial<Omit<EventCalendarLocaleText, keyof EventDialogLocaleText>> = {
