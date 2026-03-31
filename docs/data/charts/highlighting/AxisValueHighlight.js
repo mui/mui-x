19a8bca748c15d451176593020adc514ddf8697b
alex <alex.fauquette@gmail.com>
2026-03-31 16:47:51 +0200

    pnpm docs:typescript:formatted

--- docs/data/charts/highlighting/AxisValueHighlight.js
+++ docs/data/charts/highlighting/AxisValueHighlight.js
@@ -3,6 +3,16 @@ import { ChartsReferenceLine } from '@mui/x-charts/ChartsReferenceLine';
 import { ChartsAxisHighlightValue } from '@mui/x-charts/ChartsAxisHighlightValue';
 import data from '../dataset/random/scatterParallel.json';
 
+function valueFormatter(value) {
+  if (typeof value === 'number') {
+    return Math.round(value).toString();
+  }
+  if (value instanceof Date) {
+    return value.toLocaleDateString();
+  }
+  return value;
+}
+
 export default function AxisValueHighlight() {
   return (
     <div style={{ width: 400, height: 400, margin: 'auto' }}>
@@ -19,11 +29,49 @@ export default function AxisValueHighlight() {
           },
         ]}
         // highlightedAxis={[{axis}]}
+        slotProps={{ tooltip: { trigger: 'none' } }}
         axisHighlight={{ x: 'line', y: 'line' }}
       >
-        <ChartsReferenceLine x={200} lineStyle={{ stroke: 'red', strokeWidth: 1 }} />
-        <ChartsAxisHighlightValue axisDirection="x" labelPosition="both" />
-        <ChartsAxisHighlightValue axisDirection="y" labelPosition="both" />
+        <ChartsReferenceLine
+          y={200}
+          lineStyle={{ stroke: 'red', strokeWidth: 1, strokeDasharray: '4 2' }}
+        />
+        <ChartsReferenceLine y={0} lineStyle={{ stroke: 'green', strokeWidth: 1 }} />
+        <ChartsReferenceLine
+          y={500}
+          lineStyle={{ stroke: 'green', strokeWidth: 1 }}
+        />
+        <ChartsAxisHighlightValue
+          axisDirection="y"
+          labelPosition="end"
+          value={200}
+          valueFormatter={valueFormatter}
+          sx={{ borderRadius: '0 8px 8px 0', bgcolor: 'rgba(255,0,0,0.2)' }}
+        />
+        <ChartsAxisHighlightValue
+          axisDirection="y"
+          labelPosition="end"
+          value={500}
+          valueFormatter={valueFormatter}
+          sx={{ borderRadius: '0 8px 8px 0', bgcolor: 'rgba(0,255,0,0.2)' }}
+        />
+        <ChartsAxisHighlightValue
+          axisDirection="y"
+          labelPosition="end"
+          value={0}
+          valueFormatter={valueFormatter}
+          sx={{ borderRadius: '0 8px 8px 0', bgcolor: 'rgba(0,255,0,0.2)' }}
+        />
+        <ChartsAxisHighlightValue
+          axisDirection="x"
+          labelPosition="both"
+          valueFormatter={valueFormatter}
+        />
+        <ChartsAxisHighlightValue
+          axisDirection="y"
+          labelPosition="both"
+          valueFormatter={valueFormatter}
+        />
       </ScatterChart>
     </div>
   );
