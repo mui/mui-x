106d5556470d732cca7de4f308e00f37421d78d2
Jose Quintas <juniorquintas@gmail.com>
2026-04-02 11:17:31 -0300

    improve demo

--- docs/data/charts/toolbar/ChartsToolbarCustomRangeButtons.js
+++ docs/data/charts/toolbar/ChartsToolbarCustomRangeButtons.js
@@ -12,33 +12,31 @@ const values = dates.map(
   (_, i) => 100 + 50 * Math.sin(i / 30) + 20 * Math.sin(i / 7) + i * 0.05,
 );
 
-function CustomToolbar() {
-  return (
-    <Toolbar>
-      <ChartsToolbarRangeButtonTrigger value={{ unit: 'month' }}>
-        1M
-      </ChartsToolbarRangeButtonTrigger>
-      <ChartsToolbarRangeButtonTrigger value={{ unit: 'month', step: 6 }}>
-        6M
-      </ChartsToolbarRangeButtonTrigger>
-      <ChartsToolbarRangeButtonTrigger value={{ unit: 'year' }}>
-        1Y
-      </ChartsToolbarRangeButtonTrigger>
-      <ChartsToolbarRangeButtonTrigger value={null}>
-        All
-      </ChartsToolbarRangeButtonTrigger>
-    </Toolbar>
-  );
-}
+const chartConfiguration = {
+  xAxis: [{ scaleType: 'time', data: dates, zoom: true }],
+  series: [{ data: values, showMark: false, label: 'Value' }],
+  height: 300,
+};
 
 export default function ChartsToolbarCustomRangeButtons() {
   return (
     <LineChartPro
-      xAxis={[{ scaleType: 'time', data: dates, zoom: true }]}
-      series={[{ data: values, showMark: false, label: 'Value' }]}
-      height={300}
+      {...chartConfiguration}
       showToolbar
-      slots={{ toolbar: CustomToolbar }}
+      slots={{
+        toolbar: () => {
+          return (
+            <Toolbar>
+              <ChartsToolbarRangeButtonTrigger value={{ unit: 'month' }}>
+                1 month
+              </ChartsToolbarRangeButtonTrigger>
+              <ChartsToolbarRangeButtonTrigger value={null}>
+                Everything
+              </ChartsToolbarRangeButtonTrigger>
+            </Toolbar>
+          );
+        },
+      }}
     />
   );
 }
