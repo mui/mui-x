8a413cd979961cbebf930be348c2acbd9513bd44
Lukas <llukas.tyla@gmail.com>
2026-04-02 14:57:02 +0300

    Code review: 3 - add a follow-up TODO

--- packages/x-date-pickers/src/internals/components/PickerFieldUI.tsx
+++ packages/x-date-pickers/src/internals/components/PickerFieldUI.tsx
@@ -419,6 +419,7 @@ export function mergeSlotProps<TProps extends {}, TOwnerState extends FieldOwner
 /**
  * The `textField` slot props cannot be handled inside `PickerFieldUI` because it would be a breaking change to not pass the enriched props to `useField`.
  * Once the non-accessible DOM structure will be removed, we will be able to remove the `textField` slot and clean this logic.
+ * TODO: Address with the needed support for the `textField` slotProps given the change of minimum version of MUI.
  */
 export function useFieldTextFieldProps<
   TProps extends UseFieldOwnerStateParameters & { inputProps?: {}; InputProps?: {} },
