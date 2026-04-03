50f7e1d162901d1980e9ec623ed29d20cc56ef20
siriwatknp <siriwatkunaporn@gmail.com>
2026-04-03 17:37:32 +0700

    revert formatting

--- packages/x-data-grid/src/hooks/utils/useGridSelector.ts
+++ packages/x-data-grid/src/hooks/utils/useGridSelector.ts
@@ -27,13 +27,7 @@ export const argsEqual = (prev: any, curr: any) => {
   return fn(prev, curr);
 };
 
-const createRefs = () =>
-  ({
-    state: null,
-    equals: null,
-    selector: null,
-    args: undefined,
-  }) as any;
+const createRefs = () => ({ state: null, equals: null, selector: null, args: undefined }) as any;
 
 const EMPTY = [] as unknown[];
 
