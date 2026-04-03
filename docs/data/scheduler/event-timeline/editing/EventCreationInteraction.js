ce635c1c500aad6e912df6e05bae3598162e64fe
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2026-04-03 13:03:33 +0200

    fix docs

--- docs/data/scheduler/event-timeline/editing/EventCreationInteraction.js
+++ docs/data/scheduler/event-timeline/editing/EventCreationInteraction.js
@@ -18,7 +18,7 @@ export default function EventCreationInteraction() {
         defaultVisibleDate={defaultVisibleDate}
         onEventsChange={setEvents}
         defaultView="months"
-        eventCreation={{ interaction: 'click' }}
+        eventCreation={{ interaction: 'double-click' }}
       />
     </div>
   );
