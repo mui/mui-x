225b76483b8c7a5af002c2c90e01752039a42c2c
Lukas Tyla <llukas.tyla@gmail.com>
2026-04-08 18:21:52 +0300

    [docs] Add `v9` as root path link and move `v8` under subpath (#22037)

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -209,8 +209,12 @@ function AppWrapper(props) {
       name: 'MUI X',
       versions: [
         {
-          text: `v8`,
+          text: `v9`,
           href: `https://mui.com${languagePrefix}/x/introduction/`,
+        },
+        {
+          text: `v8`,
+          href: `https://v8.mui.com${languagePrefix}/x/introduction/`,
         },
         {
           text: `v${process.env.LIB_VERSION}`,
@@ -228,8 +232,12 @@ function AppWrapper(props) {
         name: 'Data Grid',
         versions: [
           {
-            text: `v8`,
+            text: `v9`,
             href: `https://mui.com${languagePrefix}/x/react-data-grid/`,
+          },
+          {
+            text: `v8`,
+            href: `https://v8.mui.com${languagePrefix}/x/react-data-grid/`,
           },
           {
             text: `v${process.env.DATA_GRID_VERSION}`,
@@ -246,8 +254,12 @@ function AppWrapper(props) {
         name: 'Date Pickers',
         versions: [
           {
-            text: `v8`,
+            text: `v9`,
             href: `https://mui.com${languagePrefix}/x/react-date-pickers/`,
+          },
+          {
+            text: `v8`,
+            href: `https://v8.mui.com${languagePrefix}/x/react-date-pickers/`,
           },
           {
             text: `v${process.env.DATE_PICKERS_VERSION}`,
@@ -269,8 +281,12 @@ function AppWrapper(props) {
         name: 'Charts',
         versions: [
           {
-            text: `v8`,
+            text: `v9`,
             href: `https://mui.com${languagePrefix}/x/react-charts/`,
+          },
+          {
+            text: `v8`,
+            href: `https://v8.mui.com${languagePrefix}/x/react-charts/`,
           },
           {
             text: `v${process.env.CHARTS_VERSION}`,
@@ -285,8 +301,12 @@ function AppWrapper(props) {
         name: 'Tree View',
         versions: [
           {
-            text: `v8`,
+            text: `v9`,
             href: `https://mui.com${languagePrefix}/x/react-tree-view/`,
+          },
+          {
+            text: `v8`,
+            href: `https://v8.mui.com${languagePrefix}/x/react-tree-view/`,
           },
           {
             text: `v${process.env.TREE_VIEW_VERSION}`,
