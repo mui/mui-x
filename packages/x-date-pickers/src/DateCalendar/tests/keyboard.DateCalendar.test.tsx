382d1fe425c1b374b33e31b7274ba1dd4aaa8268
Lukas <llukas.tyla@gmail.com>
2026-04-09 15:48:42 +0300

    Simplify

--- packages/x-date-pickers/src/DateCalendar/tests/keyboard.DateCalendar.test.tsx
+++ packages/x-date-pickers/src/DateCalendar/tests/keyboard.DateCalendar.test.tsx
@@ -13,18 +13,18 @@ describe('<DateCalendar /> keyboard interactions', () => {
     });
 
     [
-      { key: 'End', userEventKey: '{End}', expectFocusedDay: '15' },
-      { key: 'Home', userEventKey: '{Home}', expectFocusedDay: '9' },
-      { key: 'ArrowLeft', userEventKey: '{ArrowLeft}', expectFocusedDay: '12' },
-      { key: 'ArrowUp', userEventKey: '{ArrowUp}', expectFocusedDay: '6' },
-      { key: 'ArrowRight', userEventKey: '{ArrowRight}', expectFocusedDay: '14' },
-      { key: 'ArrowDown', userEventKey: '{ArrowDown}', expectFocusedDay: '20' },
-    ].forEach(({ key, userEventKey, expectFocusedDay }) => {
+      { key: 'End', expectFocusedDay: '15' },
+      { key: 'Home', expectFocusedDay: '9' },
+      { key: 'ArrowLeft', expectFocusedDay: '12' },
+      { key: 'ArrowUp', expectFocusedDay: '6' },
+      { key: 'ArrowRight', expectFocusedDay: '14' },
+      { key: 'ArrowDown', expectFocusedDay: '20' },
+    ].forEach(({ key, expectFocusedDay }) => {
       it(`${key}`, async () => {
         const { user } = render(<DateCalendar defaultValue={adapterToUse.date('2020-08-13')} />);
 
         await act(async () => screen.getByText('13').focus());
-        await user.keyboard(userEventKey);
+        await user.keyboard(`{${key}}`);
 
         // Based on column header, screen reader should pronounce <Day Number> <Week Day>
         // But `toHaveAccessibleName` does not do the link between column header and cell value, so we only get <day number> in test
@@ -37,15 +37,15 @@ describe('<DateCalendar /> keyboard interactions', () => {
 
       await act(async () => screen.getByText('13').focus());
       const interactions = [
-        { userEventKey: '{End}', expectFocusedDay: '15' },
-        { userEventKey: '{ArrowLeft}', expectFocusedDay: '14' },
-        { userEventKey: '{ArrowUp}', expectFocusedDay: '7' },
-        { userEventKey: '{Home}', expectFocusedDay: '2' },
-        { userEventKey: '{ArrowDown}', expectFocusedDay: '9' },
+        { key: 'End', expectFocusedDay: '15' },
+        { key: 'ArrowLeft', expectFocusedDay: '14' },
+        { key: 'ArrowUp', expectFocusedDay: '7' },
+        { key: 'Home', expectFocusedDay: '2' },
+        { key: 'ArrowDown', expectFocusedDay: '9' },
       ];
-      for (const { userEventKey, expectFocusedDay } of interactions) {
+      for (const { key, expectFocusedDay } of interactions) {
         // eslint-disable-next-line no-await-in-loop
-        await user.keyboard(userEventKey);
+        await user.keyboard(`{${key}}`);
 
         // Based on column header, screen reader should pronounce <Day Number> <Week Day>
         // But `toHaveAccessibleName` does not do the link between column header and cell value, so we only get <day number> in test
@@ -55,21 +55,21 @@ describe('<DateCalendar /> keyboard interactions', () => {
 
     [
       // Switch between months
-      { initialDay: '01', key: 'ArrowLeft', userEventKey: '{ArrowLeft}', expectFocusedDay: '31' },
-      { initialDay: '05', key: 'ArrowUp', userEventKey: '{ArrowUp}', expectFocusedDay: '29' },
-      { initialDay: '31', key: 'ArrowRight', userEventKey: '{ArrowRight}', expectFocusedDay: '1' },
-      { initialDay: '30', key: 'ArrowDown', userEventKey: '{ArrowDown}', expectFocusedDay: '6' },
+      { initialDay: '01', key: 'ArrowLeft', expectFocusedDay: '31' },
+      { initialDay: '05', key: 'ArrowUp', expectFocusedDay: '29' },
+      { initialDay: '31', key: 'ArrowRight', expectFocusedDay: '1' },
+      { initialDay: '30', key: 'ArrowDown', expectFocusedDay: '6' },
       // Switch between weeks
-      { initialDay: '10', key: 'ArrowLeft', userEventKey: '{ArrowLeft}', expectFocusedDay: '9' },
-      { initialDay: '09', key: 'ArrowRight', userEventKey: '{ArrowRight}', expectFocusedDay: '10' },
-    ].forEach(({ initialDay, key, userEventKey, expectFocusedDay }) => {
+      { initialDay: '10', key: 'ArrowLeft', expectFocusedDay: '9' },
+      { initialDay: '09', key: 'ArrowRight', expectFocusedDay: '10' },
+    ].forEach(({ initialDay, key, expectFocusedDay }) => {
       it(`${key}`, async () => {
         const { user } = render(
           <DateCalendar defaultValue={adapterToUse.date(`2020-08-${initialDay}`)} />,
         );
 
         await act(async () => screen.getByText(`${Number(initialDay)}`).focus());
-        await user.keyboard(userEventKey);
+        await user.keyboard(`{${key}}`);
 
         // Based on column header, screen reader should pronounce <Day Number> <Week Day>
         // But `toHaveAccessibleName` does not do the link between column header and cell value, so we only get <day number> in test
@@ -88,22 +88,12 @@ describe('<DateCalendar /> keyboard interactions', () => {
         adapterToUse.date('2020-02-01'),
       ];
       [
-        { initialDay: '11', key: 'ArrowLeft', userEventKey: '{ArrowLeft}', expectFocusedDay: '9' },
-        {
-          initialDay: '09',
-          key: 'ArrowRight',
-          userEventKey: '{ArrowRight}',
-          expectFocusedDay: '11',
-        },
+        { initialDay: '11', key: 'ArrowLeft', expectFocusedDay: '9' },
+        { initialDay: '09', key: 'ArrowRight', expectFocusedDay: '11' },
         // Switch between months
-        { initialDay: '03', key: 'ArrowLeft', userEventKey: '{ArrowLeft}', expectFocusedDay: '30' },
-        {
-          initialDay: '30',
-          key: 'ArrowRight',
-          userEventKey: '{ArrowRight}',
-          expectFocusedDay: '2',
-        },
-      ].forEach(({ initialDay, key, userEventKey, expectFocusedDay }) => {
+        { initialDay: '03', key: 'ArrowLeft', expectFocusedDay: '30' },
+        { initialDay: '30', key: 'ArrowRight', expectFocusedDay: '2' },
+      ].forEach(({ initialDay, key, expectFocusedDay }) => {
         it(`${key}`, async () => {
           const { user } = render(
             <DateCalendar
@@ -115,7 +105,7 @@ describe('<DateCalendar /> keyboard interactions', () => {
           );
 
           await act(async () => screen.getByText(`${Number(initialDay)}`).focus());
-          await user.keyboard(userEventKey);
+          await user.keyboard(`{${key}}`);
 
           // Based on column header, screen reader should pronounce <Day Number> <Week Day>
           // But `toHaveAccessibleName` does not do the link between column header and cell value, so we only get <day number> in test
