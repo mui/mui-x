80752700c6c5e004eb5983e98180a948eab15b16
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-04-13 16:02:25 +0530

    [docs-infra] Update monorepo dependency with relevant migration (#22041)

--- docs/pages/x/react-chat/core/examples/message-parts.js
+++ docs/pages/x/react-chat/core/examples/message-parts.js
@@ -1,4 +1,4 @@
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
+import { MarkdownDocs } from '@mui/internal-core-docs/MarkdownDocs';
 import * as pageProps from 'docsx/data/chat/core/examples/message-parts/message-parts.md?muiMarkdown';
 
 export default function Page() {
