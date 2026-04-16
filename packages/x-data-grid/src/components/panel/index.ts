fc4557a70d37a53cc01975df37df4d2a5797685f
sai6855 <fayazceg@gmail.com>
2026-04-16 18:15:35 +0530

    remove grid panel header

--- packages/x-data-grid/src/components/panel/index.ts
+++ packages/x-data-grid/src/components/panel/index.ts
@@ -2,6 +2,5 @@ export * from './GridColumnsPanel';
 export * from './GridPanel';
 export * from './GridPanelContent';
 export * from './GridPanelFooter';
-export * from './GridPanelHeader';
 export * from './GridPanelWrapper';
 export * from './filterPanel';
