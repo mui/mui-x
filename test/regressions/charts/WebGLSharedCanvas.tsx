1303669fd8363a8181d539461bd657f11af238cf
Jose Quintas <juniorquintas@gmail.com>
2026-04-17 10:56:35 +0200

    fix

--- test/regressions/charts/WebGLSharedCanvas.tsx
+++ test/regressions/charts/WebGLSharedCanvas.tsx
@@ -4,7 +4,6 @@ import { ChartsWrapper } from '@mui/x-charts/ChartsWrapper';
 import { ChartsLayerContainer } from '@mui/x-charts/ChartsLayerContainer';
 import { ChartsWebGLLayer } from '@mui/x-charts-premium/ChartsWebGLLayer';
 import { useWebGLLayer } from '@mui/x-charts-premium/internals';
-
 /**
  * Draws a blue rectangle using scissor test.
  * Positioned at the top-left quadrant.
@@ -16,7 +15,7 @@ function BluePlot() {
   drawRef.current = () => {
     const { gl } = layer!;
     gl.enable(gl.SCISSOR_TEST);
-    gl.scissor(2, 2, 20, 20);
+    gl.scissor(10, 10, 80, 80);
     gl.clearColor(0.2, 0.4, 0.9, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.disable(gl.SCISSOR_TEST);
@@ -48,7 +47,7 @@ function RedPlot() {
   drawRef.current = () => {
     const { gl } = layer!;
     gl.enable(gl.SCISSOR_TEST);
-    gl.scissor(10, 10, 20, 20);
+    gl.scissor(50, 50, 80, 80);
     gl.clearColor(0.9, 0.2, 0.2, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.disable(gl.SCISSOR_TEST);
@@ -75,20 +74,22 @@ function RedPlot() {
  */
 export default function WebGLSharedCanvas() {
   return (
-    <ChartsDataProviderPremium
-      height={100}
-      width={100}
-      series={[]}
-      xAxis={[{ id: 'x', data: [1, 2, 3] }]}
-    >
-      <ChartsWrapper>
-        <ChartsLayerContainer>
-          <ChartsWebGLLayer>
-            <BluePlot />
-            <RedPlot />
-          </ChartsWebGLLayer>
-        </ChartsLayerContainer>
-      </ChartsWrapper>
-    </ChartsDataProviderPremium>
+    <div style={{ width: 300, height: 300 }}>
+      <ChartsDataProviderPremium
+        height={200}
+        width={200}
+        series={[]}
+        xAxis={[{ id: 'x', data: [1, 2, 3] }]}
+      >
+        <ChartsWrapper>
+          <ChartsLayerContainer>
+            <ChartsWebGLLayer>
+              <BluePlot />
+              <RedPlot />
+            </ChartsWebGLLayer>
+          </ChartsLayerContainer>
+        </ChartsWrapper>
+      </ChartsDataProviderPremium>
+    </div>
   );
 }
