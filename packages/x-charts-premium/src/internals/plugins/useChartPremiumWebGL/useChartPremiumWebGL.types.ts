5cabeb8451cd419362790066ac7fba61f7aeb9a5
Jose Quintas <juniorquintas@gmail.com>
2026-04-17 09:04:21 +0200

    params

--- packages/x-charts-premium/src/internals/plugins/useChartPremiumWebGL/useChartPremiumWebGL.types.ts
+++ packages/x-charts-premium/src/internals/plugins/useChartPremiumWebGL/useChartPremiumWebGL.types.ts
@@ -8,11 +8,14 @@ export interface UseChartPremiumWebGLInstance {
   webGLContextRef: React.RefObject<WebGL2RenderingContext | null>;
   /**
    * Called by ChartsWebGLLayer to set or clear the WebGL context.
+   * @param {WebGL2RenderingContext | null} gl The WebGL context from the layer, or null if the context was lost.
    */
   setWebGLContext: (gl: WebGL2RenderingContext | null) => void;
   /**
    * Register a draw callback ref. Returns an unregister function.
    * Callbacks are called in registration order (DOM order).
+   * @param {React.RefObject} drawRef A ref object whose current property is a callback function to call on each render, or null if the callback should be unregistered.
+   * @returns {() => void} Unregister function to remove the callback from the render cycle.
    */
   registerWebGLDraw: (drawRef: React.RefObject<(() => void) | null>) => () => void;
   /**
