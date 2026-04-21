a2e971658e67e1076ae361d615352856680963ae
alex <alex.fauquette@gmail.com>
2026-04-21 17:06:07 +0200

    fix JSDocs

--- packages/x-charts-premium/src/hooks/useRadialLineSeries.ts
+++ packages/x-charts-premium/src/hooks/useRadialLineSeries.ts
@@ -12,25 +12,25 @@ export type UseRadialLineSeriesReturnValue = ChartSeriesDefaultized<'radialLine'
 export type UseRadialLineSeriesContextReturnValue = ProcessedSeries['radialLine'];
 
 /**
- * Get access to the internal state of range bar series.
+ * Get access to the internal state of radial line series.
  *
  * @param {SeriesId} seriesId The id of the series to get.
- * @returns {UseRadialLineSeriesReturnValue} the range bar series
+ * @returns {UseRadialLineSeriesReturnValue} the radial line series
  */
 export function useRadialLineSeries(seriesId: SeriesId): UseRadialLineSeriesReturnValue | undefined;
 /**
- * Get access to the internal state of range bar series.
+ * Get access to the internal state of radial line series.
  *
- * When called without arguments, it returns all range bar series.
+ * When called without arguments, it returns all radial line series.
  *
- * @returns {UseRadialLineSeriesReturnValue[]} the range bar series
+ * @returns {UseRadialLineSeriesReturnValue[]} the radial line series
  */
 export function useRadialLineSeries(): UseRadialLineSeriesReturnValue[];
 /**
- * Get access to the internal state of range bar series.
+ * Get access to the internal state of radial line series.
  *
  * @param {SeriesId[]} seriesIds The ids of the series to get. Order is preserved.
- * @returns {UseRadialLineSeriesReturnValue[]} the range bar series
+ * @returns {UseRadialLineSeriesReturnValue[]} the radial line series
  */
 export function useRadialLineSeries(seriesIds: SeriesId[]): UseRadialLineSeriesReturnValue[];
 export function useRadialLineSeries(seriesIds?: SeriesId | SeriesId[]) {
@@ -38,11 +38,11 @@ export function useRadialLineSeries(seriesIds?: SeriesId | SeriesId[]) {
 }
 
 /**
- * Get access to the internal state of range bar series.
+ * Get access to the internal state of radial line series.
  * The returned object contains:
  * - series: a mapping from ids to series attributes.
  * - seriesOrder: the array of series ids.
- * @returns the range bar series
+ * @returns the radial line series
  */
 export function useRadialLineSeriesContext(): UseRadialLineSeriesContextReturnValue {
   return useAllSeriesOfType('radialLine');
