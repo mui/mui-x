7b756b7372847596fa119746c1b143e8630312f2
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2026-04-22 12:39:28 +0200

    [charts-premium] Plot radial line (#22133)

---
+++ packages/x-charts-premium/src/RadialLineChart/RadialLineChart.plugins.ts
@@ -0,0 +1,38 @@
+import {
+  useChartZAxis,
+  type UseChartZAxisSignature,
+  useChartPolarAxis,
+  type UseChartPolarAxisSignature,
+  useChartTooltip,
+  type UseChartTooltipSignature,
+  useChartInteraction,
+  type UseChartInteractionSignature,
+  useChartHighlight,
+  type UseChartHighlightSignature,
+  useChartKeyboardNavigation,
+  type UseChartKeyboardNavigationSignature,
+  useChartVisibilityManager,
+  type UseChartVisibilityManagerSignature,
+  type ConvertSignaturesIntoPlugins,
+} from '@mui/x-charts/internals';
+
+export type RadialLineChartPluginSignatures = [
+  UseChartZAxisSignature,
+  UseChartTooltipSignature<'radialLine'>,
+  UseChartInteractionSignature,
+  UseChartPolarAxisSignature<'radialLine'>,
+  UseChartHighlightSignature<'radialLine'>,
+  UseChartVisibilityManagerSignature<'radialLine'>,
+  UseChartKeyboardNavigationSignature,
+];
+
+export const RADIAL_LINE_CHART_PLUGINS: ConvertSignaturesIntoPlugins<RadialLineChartPluginSignatures> =
+  [
+    useChartZAxis,
+    useChartTooltip,
+    useChartInteraction,
+    useChartPolarAxis,
+    useChartHighlight,
+    useChartVisibilityManager,
+    useChartKeyboardNavigation,
+  ];
