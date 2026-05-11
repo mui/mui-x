d1a2d5db9007e03ea071006b4ca22cedd0a92d83
alex <alex.fauquette@gmail.com>
2026-05-11 12:33:45 +0200

    pnpm prettier

--- packages/x-charts-premium/src/RadialBarChart/RadialBarChart.tsx
+++ packages/x-charts-premium/src/RadialBarChart/RadialBarChart.tsx
@@ -14,11 +14,7 @@ import {
 } from '@mui/x-charts/ChartsRadialGrid';
 import { Unstable_ChartsRotationAxis as ChartsRotationAxis } from '@mui/x-charts/ChartsRotationAxis';
 import { Unstable_ChartsRadiusAxis as ChartsRadiusAxis } from '@mui/x-charts/ChartsRadiusAxis';
-import {
-  ChartsLegend,
-  type ChartsLegendSlots,
-  type ChartsLegendSlotProps
-} from '../ChartsLegend';
+import { ChartsLegend, type ChartsLegendSlots, type ChartsLegendSlotProps } from '../ChartsLegend';
 import { ChartsSurface } from '../ChartsSurface';
 import {
   ChartsTooltip,
