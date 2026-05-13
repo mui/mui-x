c323777060de34d0e186ffecebafd06322cb75d4
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2026-05-13 12:09:19 +0200

    fix test

--- packages/x-scheduler-internals-premium/src/scheduler-selectors-premium/schedulerRecurringEventSelectors.test.ts
+++ packages/x-scheduler-internals-premium/src/scheduler-selectors-premium/schedulerRecurringEventSelectors.test.ts
@@ -12,23 +12,23 @@ describe.for(premiumStoreClasses.map((storeClass) => [storeClass.name, storeClas
 
       it('returns daily, weekly, monthly and yearly presets', () => {
         const start = processDate(adapter.date('2025-08-05T09:00:00Z', 'default'), adapter); // Tuesday
-        const presets = schedulerRecurringEventSelectors.presets(state, start);
+        const presets = schedulerRecurringEventSelectors.presets(state, start)!;
 
-        expect(presets!.DAILY).to.deep.equal({
+        expect(presets.DAILY).to.deep.equal({
           freq: 'DAILY',
           interval: 1,
         });
-        expect(presets!.WEEKLY).to.deep.equal({
+        expect(presets.WEEKLY).to.deep.equal({
           freq: 'WEEKLY',
           interval: 1,
           byDay: [getWeekDayCode(adapter, start.value)],
         });
-        expect(presets!.MONTHLY).to.deep.equal({
+        expect(presets.MONTHLY).to.deep.equal({
           freq: 'MONTHLY',
           interval: 1,
           byMonthDay: [5],
         });
-        expect(presets!.YEARLY).to.deep.equal({
+        expect(presets.YEARLY).to.deep.equal({
           freq: 'YEARLY',
           interval: 1,
         });
@@ -51,13 +51,13 @@ describe.for(premiumStoreClasses.map((storeClass) => [storeClass.name, storeClas
           schedulerRecurringEventSelectors.defaultPresetKey(state, presets!.DAILY, start),
         ).to.equal('DAILY');
         expect(
-          schedulerRecurringEventSelectors.defaultPresetKey(state, presets!.WEEKLY, start),
+          schedulerRecurringEventSelectors.defaultPresetKey(state, presets.WEEKLY, start),
         ).to.equal('WEEKLY');
         expect(
-          schedulerRecurringEventSelectors.defaultPresetKey(state, presets!.MONTHLY, start),
+          schedulerRecurringEventSelectors.defaultPresetKey(state, presets.MONTHLY, start),
         ).to.equal('MONTHLY');
         expect(
-          schedulerRecurringEventSelectors.defaultPresetKey(state, presets!.YEARLY, start),
+          schedulerRecurringEventSelectors.defaultPresetKey(state, presets.YEARLY, start),
         ).to.equal('YEARLY');
       });
 
@@ -81,7 +81,7 @@ describe.for(premiumStoreClasses.map((storeClass) => [storeClass.name, storeClas
 
       it('classifies weekly with extra day as custom', () => {
         const rule: SchedulerProcessedEventRecurrenceRule = {
-          ...presets!.WEEKLY,
+          ...presets.WEEKLY,
           byDay: ['TU', 'WE'],
         };
         expect(schedulerRecurringEventSelectors.defaultPresetKey(state, rule, start)).to.equal(
@@ -91,7 +91,7 @@ describe.for(premiumStoreClasses.map((storeClass) => [storeClass.name, storeClas
 
       it('classifies monthly with different day or with interval>1 as custom', () => {
         const ruleDifferentDay: SchedulerProcessedEventRecurrenceRule = {
-          ...presets!.MONTHLY,
+          ...presets.MONTHLY,
           byMonthDay: [26],
         };
         expect(
@@ -99,7 +99,7 @@ describe.for(premiumStoreClasses.map((storeClass) => [storeClass.name, storeClas
         ).to.equal('custom');
 
         const ruleInterval2: SchedulerProcessedEventRecurrenceRule = {
-          ...presets!.MONTHLY,
+          ...presets.MONTHLY,
           interval: 2,
         };
         expect(
@@ -108,7 +108,7 @@ describe.for(premiumStoreClasses.map((storeClass) => [storeClass.name, storeClas
       });
 
       it('classifies yearly interval>1 as custom', () => {
-        const rule: SchedulerProcessedEventRecurrenceRule = { ...presets!.YEARLY, interval: 2 };
+        const rule: SchedulerProcessedEventRecurrenceRule = { ...presets.YEARLY, interval: 2 };
         expect(schedulerRecurringEventSelectors.defaultPresetKey(state, rule, start)).to.equal(
           'custom',
         );
