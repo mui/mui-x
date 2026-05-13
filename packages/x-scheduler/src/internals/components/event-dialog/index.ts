c8701e82d8b0f4c57779d3fc88fc466542a0ee6f
Rita Iglesias Gandara <ritaiglesiasgandara@gmail.com>
2026-05-13 13:23:58 +0200

    refactor

--- packages/x-scheduler/src/internals/components/event-dialog/index.ts
+++ packages/x-scheduler/src/internals/components/event-dialog/index.ts
@@ -9,5 +9,4 @@ export * from './EventDialogStyledContext';
 export type { ControlledValue, EndsSelection } from './utils';
 export { getEndsSelectionFromRRule } from './utils';
 export { EventDialogTabPanel, EventDialogTabContent } from './EventDialogTabPanel';
-export { EventDialogSlotsContext, useEventDialogSlots } from './EventDialogSlotsContext';
-export type { EventDialogSlots } from './EventDialogSlotsContext';
+export { EventDialogSlotsContext } from './EventDialogSlotsContext';
