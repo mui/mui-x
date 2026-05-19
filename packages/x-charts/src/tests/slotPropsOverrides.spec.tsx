75cd061bab096f4120113a28b63ed5e51734d819
sai6855 <fayazceg@gmail.com>
2026-05-19 19:10:50 +0530

    fix ts

--- packages/x-charts/src/tests/slotPropsOverrides.spec.tsx
+++ packages/x-charts/src/tests/slotPropsOverrides.spec.tsx
@@ -1,5 +1,5 @@
 import { LineChart, type LineChartSlots } from '@mui/x-charts/LineChart';
-import type { PropsFromSlot } from '@mui/x-internals';
+import type { PropsFromSlot } from '@mui/x-charts/models';
 
 declare module '@mui/x-charts' {
   interface TooltipPropsOverrides {
