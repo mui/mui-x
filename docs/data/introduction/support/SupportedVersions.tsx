b9d71f4c758486fc951746d6f541368ead74665c
Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>
2026-05-20 12:03:46 +0200

    Use old import path
    Co-authored-by: Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>
    Signed-off-by: Armin Mehinovic <4390250+arminmeh@users.noreply.github.com>

--- docs/data/introduction/support/SupportedVersions.tsx
+++ docs/data/introduction/support/SupportedVersions.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { MarkdownElement } from '@mui/internal-core-docs/MarkdownDocs';
+import { MarkdownElement } from '@mui/docs/MarkdownElement';
 
 interface Column {
   key: string;
