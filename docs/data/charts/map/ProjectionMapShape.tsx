ce8b49a6620c409a4dd751302202b746e1aeb18a
alex <alex.fauquette@gmail.com>
2026-05-27 09:39:52 +0200

    feedback

--- docs/data/charts/map/ProjectionMapShape.tsx
+++ docs/data/charts/map/ProjectionMapShape.tsx
@@ -161,71 +161,79 @@ export default function ProjectionMapShape() {
           <Typography gutterBottom>
             Rotation: {autoRotation ? 'auto' : `(${rotation[0]}°, ${rotation[1]}°)`}
           </Typography>
-          <FormControlLabel
-            control={
-              <Checkbox
-                size="small"
-                checked={autoRotation}
-                onChange={(event) => setAutoRotation(event.target.checked)}
-              />
-            }
-            label={`auto rotation`}
-          />
-          {autoRotation ? null : (
-            <div>
-              <ButtonGroup
-                variant="outlined"
-                aria-label="outlined button group"
-                fullWidth
-              >
-                {cities.map(({ name, coordinates }) => (
-                  <Button
+          {usaProjection ? (
+            <Typography variant="caption">
+              Rotation is not available for the Albers USA projection.
+            </Typography>
+          ) : (
+            <React.Fragment>
+              <FormControlLabel
+                control={
+                  <Checkbox
                     size="small"
-                    key={name}
-                    onClick={() => {
-                      if ('transverseMercator' === projection) {
-                        return setRotation([0, -coordinates[1]]);
-                      }
-                      if (isCylindrical(projection)) {
-                        return setRotation([-coordinates[0], 0]);
-                      }
-                      return setRotation([-coordinates[0], -coordinates[1]]);
-                    }}
-                    disabled={usaProjection}
+                    checked={autoRotation}
+                    onChange={(event) => setAutoRotation(event.target.checked)}
+                  />
+                }
+                label={`auto rotation`}
+              />
+              {autoRotation ? null : (
+                <div>
+                  <ButtonGroup
+                    variant="outlined"
+                    aria-label="outlined button group"
+                    fullWidth
                   >
-                    {name}
-                  </Button>
-                ))}
-              </ButtonGroup>
-              <Slider
-                value={rotation[0]}
-                min={-180}
-                max={180}
-                step={10}
-                marks={[{ value: 0, label: '' }]}
-                aria-label="longitude"
-                size="small"
-                onChange={(_, value: number) =>
-                  setRotation((prev) => [value, prev[1]])
-                }
-                valueLabelDisplay="auto"
-                disabled={usaProjection}
-              />
-              <Slider
-                value={rotation[1]}
-                min={-180}
-                max={180}
-                step={10}
-                marks={[{ value: 0, label: '' }]}
-                aria-label="latitude"
-                size="small"
-                onChange={(_, value: number) =>
-                  setRotation((prev) => [prev[0], value])
-                }
-                valueLabelDisplay="auto"
-                disabled={usaProjection}
-              />
-            </div>
+                    {cities.map(({ name, coordinates }) => (
+                      <Button
+                        size="small"
+                        key={name}
+                        onClick={() => {
+                          if ('transverseMercator' === projection) {
+                            return setRotation([0, -coordinates[1]]);
+                          }
+                          if (isCylindrical(projection)) {
+                            return setRotation([-coordinates[0], 0]);
+                          }
+                          return setRotation([-coordinates[0], -coordinates[1]]);
+                        }}
+                        disabled={usaProjection}
+                      >
+                        {name}
+                      </Button>
+                    ))}
+                  </ButtonGroup>
+                  <Slider
+                    value={rotation[0]}
+                    min={-180}
+                    max={180}
+                    step={10}
+                    marks={[{ value: 0, label: '' }]}
+                    aria-label="longitude"
+                    size="small"
+                    onChange={(_, value: number) =>
+                      setRotation((prev) => [value, prev[1]])
+                    }
+                    valueLabelDisplay="auto"
+                    disabled={usaProjection}
+                  />
+                  <Slider
+                    value={rotation[1]}
+                    min={-180}
+                    max={180}
+                    step={10}
+                    marks={[{ value: 0, label: '' }]}
+                    aria-label="latitude"
+                    size="small"
+                    onChange={(_, value: number) =>
+                      setRotation((prev) => [prev[0], value])
+                    }
+                    valueLabelDisplay="auto"
+                    disabled={usaProjection}
+                  />
+                </div>
+              )}
+            </React.Fragment>
           )}
         </div>
         <div>
@@ -256,7 +264,6 @@ export default function ProjectionMapShape() {
                   setTranslation((prev) => [value, prev[1]])
                 }
                 valueLabelDisplay="auto"
-                disabled={usaProjection}
               />
               <Slider
                 value={translation[1]}
@@ -269,7 +276,6 @@ export default function ProjectionMapShape() {
                   setTranslation((prev) => [prev[0], value])
                 }
                 valueLabelDisplay="auto"
-                disabled={usaProjection}
               />
             </div>
           )}
@@ -288,9 +294,9 @@ export default function ProjectionMapShape() {
           />
           <Slider
             value={scale}
-            min={10}
-            max={500}
-            step={10}
+            min={100}
+            max={1500}
+            step={100}
             size="small"
             disabled={autoScale}
             onChange={(_, value) => setScale(value as number)}
