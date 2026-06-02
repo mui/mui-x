537aa171e229697872559a2a7d1976850de231a4
sai6855 <fayazceg@gmail.com>
2026-06-02 16:36:14 +0530

    barebones export

--- packages/x-charts-pro/src/internals/plugins/useChartProExport/exportSvg.ts
+++ packages/x-charts-pro/src/internals/plugins/useChartProExport/exportSvg.ts
@@ -1,7 +1,29 @@
 import { type ChartSvgExportOptions } from './useChartProExport.types';
 
-function exportSvg(chartRoot: HTMLElement, svg: SVGSVGElement, options?: ChartSvgExportOptions) {
-  console.log('Exporting chart as SVG with options:', options);
+function exportSvg(
+  chartRoot: HTMLElement,
+  chartContainer: HTMLElement | SVGSVGElement,
+  options?: ChartSvgExportOptions,
+) {
+  const container = chartContainer.querySelector('svg');
+  const clonedContainer = container?.cloneNode(true);
+  if (!clonedContainer) {
+    console.error(
+      'MUI X Charts: No SVG element found in the chart container. Unable to export as SVG.',
+    );
+    return;
+  }
+  const svgString = new XMLSerializer().serializeToString(clonedContainer);
+
+  const blob = new Blob([svgString], { type: 'image/svg+xml' });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = options?.fileName || `${document.title}.svg`;
+
+  a.click();
+  URL.revokeObjectURL(url);
 }
 
 export { exportSvg };
