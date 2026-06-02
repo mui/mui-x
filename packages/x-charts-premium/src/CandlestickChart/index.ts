99d6a7477cb4148b39436c60ae7fa3a0635b5aee
alex <alex.fauquette@gmail.com>
2026-06-02 14:26:11 +0200

    export candle stick without unstable prefix

--- packages/x-charts-premium/src/CandlestickChart/index.ts
+++ packages/x-charts-premium/src/CandlestickChart/index.ts
@@ -1,11 +1,27 @@
+import { CandlestickChart } from './CandlestickChart';
+import { CandlestickPlot } from './CandlestickPlot';
+
+/**
+ * @deprecated candlestick chart is now stable, import `ChartsCandlestickChart` instead
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const Unstable_CandlestickChart = CandlestickChart;
+
+/**
+ * @deprecated candlestick plot is now stable, import `ChartsCandlestickPlot` instead
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const Unstable_CandlestickPlot = CandlestickPlot;
+
 export {
-  CandlestickChart as Unstable_CandlestickChart,
+  CandlestickChart,
   type CandlestickChartProps,
   type CandlestickChartSlots,
   type CandlestickChartSlotProps,
   type OHLCSeries,
 } from './CandlestickChart';
+
 export {
-  CandlestickPlot as Unstable_CandlestickPlot,
+  CandlestickPlot,
   type CandlestickPlotProps,
 } from './CandlestickPlot';
