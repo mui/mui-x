4a559a60884e21c58af19d81744bf0f1af670ced
sai6855 <fayazceg@gmail.com>
2026-06-05 19:15:40 +0530

    fix type

--- packages/x-charts/src/internals/getSeriesColorFn.ts
+++ packages/x-charts/src/internals/getSeriesColorFn.ts
@@ -1,11 +1,11 @@
-import { type CommonSeriesType, type ColorCallbackValue } from '../models/seriesType/common';
+import { type ColorCallbackValue, type SeriesColor } from '../models/seriesType/common';
 import { type ChartSeriesType, type ChartsSeriesConfig } from '../models/seriesType/config';
 
 export function getSeriesColorFn<
   TValue extends ChartsSeriesConfig[ChartSeriesType]['valueType'],
 >(series: {
-  color: NonNullable<CommonSeriesType<ChartSeriesType>['color']>;
-  colorGetter?: CommonSeriesType<ChartSeriesType>['colorGetter'];
+  color: NonNullable<SeriesColor<TValue>['color']>;
+  colorGetter?: SeriesColor<TValue>['colorGetter'];
 }): (data: ColorCallbackValue<TValue>) => string {
   return series.colorGetter ? series.colorGetter : () => series.color;
 }
