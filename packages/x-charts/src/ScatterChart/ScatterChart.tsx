aa81cb5a83fdb2aeb42da95de5bd332ec8563520
alex <alex.fauquette@gmail.com>
2026-06-10 14:05:30 +0200

    pnpm proptypes

--- packages/x-charts/src/ScatterChart/ScatterChart.tsx
+++ packages/x-charts/src/ScatterChart/ScatterChart.tsx
@@ -265,7 +265,9 @@ ScatterChart.propTypes = {
   /**
    * Options to enable features planned for the next major.
    */
-  experimentalFeatures: PropTypes.object,
+  experimentalFeatures: PropTypes.shape({
+    progressiveRendering: PropTypes.bool,
+  }),
   /**
    * Option to display a cartesian grid in the background.
    */
