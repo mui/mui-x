e3fc22c23153924cf1078fa3ed1b875523097dfe
sai6855 <saichand685@gmail.com>
2026-06-10 14:10:57 +0000

    [data grid] Remove unused removePropTypesPlugin variable in Babel config

--- babel.config.mjs
+++ babel.config.mjs
@@ -26,10 +26,6 @@ export default function getBabelConfig(api) {
     },
   ]);
 
-  const removePropTypesPlugin = baseConfig.plugins.find(
-    (p) => p[2] === 'babel-plugin-transform-react-remove-prop-types',
-  );
-
   const displayNamePlugin = baseConfig.plugins.find(
     (p) => p[2] === '@mui/internal-babel-plugin-display-name',
   );
