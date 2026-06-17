bbce889e6bcb4512116c5d604a7bd66873381556
Bilal Shafi <bilalshafidev@gmail.com>
2026-06-17 17:59:49 +0500

    Minor demo update

--- docs/data/data-grid/formulas/FormulaBasic.js
+++ docs/data/data-grid/formulas/FormulaBasic.js
@@ -154,6 +154,7 @@ export default function FormulaBasic() {
         formulaA1Notation
         showCellVerticalBorder
         showColumnVerticalBorder
+        density="compact"
       />
     </div>
   );
