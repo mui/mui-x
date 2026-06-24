8c644d31926d91a465887a95d4fcd17afd9e5215
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-06-24 18:49:54 +0530

    [docs-infra] Pre-render API page descriptions (#22919)

--- docs/pages/x/api/charts/charts-container-pro.js
+++ docs/pages/x/api/charts/charts-container-pro.js
@@ -1,16 +1,8 @@
 import * as React from 'react';
 import { ApiPage } from '@mui/internal-core-docs/ApiPage';
-import { mapApiPageTranslation } from '@mui/internal-core-docs/mapApiPageTranslations';
-import translation from 'docs/translations/api-docs/charts/charts-container-pro/charts-container-pro.json';
+import descriptions from 'docs/translations/api-docs/charts/charts-container-pro/charts-container-pro.json';
 import jsonPageContent from './charts-container-pro.json';
 
-export default function Page(props) {
-  const { descriptions } = props;
+export default function Page() {
   return <ApiPage descriptions={descriptions} pageContent={jsonPageContent} />;
-}
-
-export async function getStaticProps() {
-  const descriptions = mapApiPageTranslation(translation);
-
-  return { props: { descriptions } };
 }
