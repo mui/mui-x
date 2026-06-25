9cd41d6b053ee9f5a5c10aa0c34fd9e14dc05628
alex <alex.fauquette@gmail.com>
2026-06-25 10:42:57 +0200

    zoom-more-somaliland

--- docs/data/charts/map/GeoFeatureKeyMapShape.js
+++ docs/data/charts/map/GeoFeatureKeyMapShape.js
@@ -33,7 +33,7 @@ export default function GeoFeatureKeyMapShape() {
           geoData={countries}
           geoFeatureKey={mergeSomaliland}
           projection="naturalEarth1"
-          initialView={{ zoomLevel: 2, center: [46, 6], translation: [0, 0] }}
+          initialView={{ zoomLevel: 5, center: [46, 6], translation: [0, 0] }}
           height={360}
           series={[
             {
