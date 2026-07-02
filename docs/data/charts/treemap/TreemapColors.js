3daf4a8a964c489f8df7b23442932ee278c49e6f
Jose Quintas <juniorquintas@gmail.com>
2026-07-02 12:29:03 +0200

    colors preview

--- docs/data/charts/treemap/TreemapColors.js
+++ docs/data/charts/treemap/TreemapColors.js
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import { Treemap } from '@mui/x-charts-pro/Treemap';
 
-const data = {
-  id: 'portfolio',
-  children: [
-    { id: 'Stocks', value: 45, color: '#112e81' },
-    { id: 'Bonds', value: 25, color: '#4647ae' },
-    { id: 'Real estate', value: 15, color: '#4382df' },
-    { id: 'Cash', value: 10, color: '#7fb2dd' },
-    { id: 'Crypto', value: 5, color: '#aaccd6' },
-  ],
-};
-
 export default function TreemapColors() {
-  return <Treemap series={{ data }} height={300} />;
+  return (
+    <Treemap
+      series={{
+        data: {
+          id: 'portfolio',
+          children: [
+            { id: 'Stocks', value: 45, color: '#112e81' },
+            { id: 'Bonds', value: 25, color: '#4647ae' },
+            { id: 'Real estate', value: 15, color: '#4382df' },
+            { id: 'Cash', value: 10, color: '#7fb2dd' },
+            { id: 'Crypto', value: 5, color: '#aaccd6' },
+          ],
+        },
+      }}
+      height={300}
+    />
+  );
 }
