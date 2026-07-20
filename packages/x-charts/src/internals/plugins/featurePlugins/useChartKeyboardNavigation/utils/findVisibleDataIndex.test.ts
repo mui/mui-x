3dad2d806e3ee6fc45391e9bcea46af31fa23c2c
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-07-20 10:18:56 +0300

    [charts][accessibility] Fix out of bounds keyboard navigation

---
+++ packages/x-charts/src/internals/plugins/featurePlugins/useChartKeyboardNavigation/utils/findVisibleDataIndex.test.ts
@@ -0,0 +1,28 @@
+import type { ProcessedSeries } from '../../../corePlugins/useChartSeries/useChartSeries.types';
+import { findVisibleDataIndex } from './findVisibleDataIndex';
+
+describe('findVisibleDataIndex', () => {
+  it('does not return an index outside the focused series', () => {
+    const processedSeries = {
+      scatter: {
+        seriesOrder: ['short', 'long'],
+        series: {
+          short: { data: [{ x: 1, y: 1 }, { x: 2, y: 2 }] },
+          long: { data: [{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }] },
+        },
+      },
+    } as unknown as ProcessedSeries<'scatter'>;
+
+    expect(
+      findVisibleDataIndex({
+        processedSeries,
+        type: 'scatter',
+        seriesId: 'short',
+        startIndex: 2,
+        dataLength: 3,
+        direction: 1,
+        allowCycles: false,
+      }),
+    ).to.equal(1);
+  });
+});
