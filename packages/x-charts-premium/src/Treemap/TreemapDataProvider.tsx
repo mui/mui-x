4256ac4cadf594b8a8467a2314cf664378a3ef08
Jose Quintas <juniorquintas@gmail.com>
2026-08-25 10:06:52 +0200

    Regenerate the Treemap proptypes and API docs after the master merge

--- packages/x-charts-premium/src/Treemap/TreemapDataProvider.tsx
+++ packages/x-charts-premium/src/Treemap/TreemapDataProvider.tsx
@@ -67,7 +67,16 @@ TreemapDataProvider.propTypes /* remove-proptypes */ = {
   /**
    * Options to enable features planned for the next major.
    */
-  experimentalFeatures: PropTypes.object,
+  experimentalFeatures: PropTypes.shape({
+    keyboardActivation: PropTypes.bool,
+  }),
+  /**
+   * If `true`, clicking an item immediately shows the keyboard focus indicator on it.
+   * By default, clicking sets the item that keyboard navigation starts from, but the focus
+   * indicator stays hidden until the user presses a key.
+   * @default false
+   */
+  focusItemOnClick: PropTypes.bool,
   /**
    * The height of the chart in px. If not defined, it takes the height of the parent element.
    */
@@ -120,7 +129,7 @@ TreemapDataProvider.propTypes /* remove-proptypes */ = {
   /**
    * The callback fired when an item is clicked.
    *
-   * @param {React.MouseEvent<HTMLDivElement, MouseEvent>} event The click event.
+   * @param {ChartsActivationEvent<HTMLDivElement>} event The click event.
    * @param {SeriesItemIdentifierWithType<SeriesType>} item The clicked item.
    */
   onItemClick: PropTypes.func,
