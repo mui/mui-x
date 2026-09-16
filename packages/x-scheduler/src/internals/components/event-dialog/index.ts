573740c0a5e68f91a2f60a7face519a86a1f2ed9
delangle <flaviendelangle@gmail.com>
2026-09-16 15:38:18 +0200

    [scheduler] Reuse recurrence option labels

--- packages/x-scheduler/src/internals/components/event-dialog/index.ts
+++ packages/x-scheduler/src/internals/components/event-dialog/index.ts
@@ -1,6 +1,6 @@
 export { EventDialogContent, EventDialogProvider } from './EventDialog';
 export * from './eventDialogClasses';
 export type { EndsSelection } from './utils';
-export { getEndsSelectionFromRRule, getWeekdayToken } from './utils';
+export { getEndsSelectionFromRRule, getWeekdayToken, getRecurrenceLabel } from './utils';
 export { EventDialogTabPanel, EventDialogTabContent } from './EventDialogTabPanel';
 export { useEventDialogFormContext } from './form/EventDialogFormContext';
